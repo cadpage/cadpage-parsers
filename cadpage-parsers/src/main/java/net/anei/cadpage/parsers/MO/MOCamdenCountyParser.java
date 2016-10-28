06a539949ce714e37967a652eb4c4c46c0ad50aa
Ken Corbin <ken@cadpage.org>
2016-10-27 20:33:36 -0700

    Update A911 Parser table

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/MO/MOCamdenCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/MO/MOCamdenCountyParser.java
@@ -1,5 +1,6 @@
 package net.anei.cadpage.parsers.MO;
 
+import net.anei.cadpage.parsers.MsgInfo.Data;
 import net.anei.cadpage.parsers.dispatch.DispatchSPKParser;
 
 public class MOCamdenCountyParser extends DispatchSPKParser {
@@ -11,4 +12,13 @@ public class MOCamdenCountyParser extends DispatchSPKParser {
   public String getFilter() {
     return "dispatch@camdenmo.org";
   }
+
+  @Override
+  protected boolean parseHtmlMsg(String subject, String body, Data data) {
+    if (!super.parseHtmlMsg(subject, body, data)) return false;
+    int pt = data.strSupp.indexOf("CONFIDENTIALITY NOTICE:");
+    if (pt >= 0) data.strSupp = data.strSupp.substring(0, pt).trim();
+    return true;
+  }
+  
 }
