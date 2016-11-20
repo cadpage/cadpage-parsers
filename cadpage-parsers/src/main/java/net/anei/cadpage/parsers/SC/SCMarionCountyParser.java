fd98b4e34b1f777c71fc30a3c0212aeb47b37cb4
Ken Corbin <ken@cadpage.org>
2016-11-19 16:39:15 -0800

    Fixed parsing problem with Marion County, SC

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/SC/SCMarionCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/SC/SCMarionCountyParser.java
@@ -1,47 +1,89 @@
 package net.anei.cadpage.parsers.SC;
 
 import net.anei.cadpage.parsers.CodeSet;
-import net.anei.cadpage.parsers.dispatch.DispatchB2Parser;
+import net.anei.cadpage.parsers.MsgInfo.Data;
+import net.anei.cadpage.parsers.dispatch.DispatchA48Parser;
 
 
-public class SCMarionCountyParser extends DispatchB2Parser {
+public class SCMarionCountyParser extends DispatchA48Parser {
 
   public SCMarionCountyParser() {
-    super("E911CENTRALMARIONCOUNTY:", CITY_LIST, "MARION COUNTY", "SC");
+    super(CITY_LIST, "MARION COUNTY", "SC", FieldType.NONE, A48_NO_CODE);
     setupCallList(CALL_LIST);
+    setupMultiWordStreets(MWORD_STREET_LIST);
+  }
+  
+  private String savePlace = null;
+  
+  @Override
+  protected boolean parseMsg(String subject, String body, Data data) {
+    body = stripFieldEnd(body, "SC - - LL");
+    savePlace = null;
+    if (!super.parseMsg(subject, body, data)) return false;
+    if (savePlace != null) data.strPlace = append(savePlace, " - ", data.strPlace);
+    return true;
   }
   
   @Override
-  public String getFilter() {
-    return "tcooper@marionsc.gov";
+  public String fixCallAddress(String field) {
+    int pt = field.indexOf("//");
+    if (pt >= 0) {
+      savePlace = field.substring(pt+2).trim();
+      field = field.substring(0, pt);
+    }
+    return field;
   }
   
+  private static final String[] MWORD_STREET_LIST = new String[]{
+      "BYPASS SAWYER",
+      "HENRY MCGILL",
+      "WEST MULLINS"
+  };
+  
+
   private static final CodeSet CALL_LIST = new CodeSet(
-      "MEDICAL ALARM",
+      "ASTDSS",
       "BRUSH FIRE",
-      "RESPIRATORY DISTRESS",
-      "MOTOR VEHICLE ACCIDENT",
-      "FIRE ALARM COMMERCIAL",
+      "BURGLAR ALARM (RESIDENTIAL)",
       "CARDIAC ARREST",
-      "PAIN",
+      "CHOKING",
+      "CHESTP",
+      "CIVILD",
+      "DIABETIC",
+      "FALL WITH INJURY",
+      "FALVIC",
+      "FIRE ALARM COMMERCIAL",
       "FIRE ALARM RESIDENTIAL",
-      "STRUCTURE FIRE",
+      "GAS LEAK",
+      "INFORMATION",
       "INVESTIGATION",
-      "UNCONSCIOUS-UNREPONSIVE",
-      "STRUCTURE FIRE (COMMERCIAL)"
+      "LIFTAS",
+      "LIFTING ASSISTANCE",
+      "MEDICAL ALARM",
+      "MOTOR VEHICLE ACCIDENT",
+      "PAIN",
+      "REPORT",
+      "RESPIRATORY DISTRESS",
+      "SEIZURE",
+      "STANBY",
+      "STROKE",
+      "STROKE /CVA",
+      "STRUCTURE FIRE (COMMERCIAL)",
+      "STRUCTURE FIRE",
+      "UNCONSCIOUS-UNREPONSIVE"
   );
 
   private static final String[] CITY_LIST = new String[]{
-    "ARIEL CROSSROAD",
-    "BRITTONS NECK",
-    "CENTENARY",
-    "FRIENDSHIP",
-    "GRESHAM",
-    "MARION",
-    "MULLINS",
-    "NICHOLS",
-    "RAINS",
-    "SELLERS",
-    "TEMPERANCE HILL"
+      "ARIEL CROSSROAD",
+      "BRITTONS NECK",
+      "CENTENARY",
+      "FRIENDSHIP",
+      "GRESHAM",
+      "MARION",
+      "MULLINS",
+      "NICHOLS",
+      "RAINS",
+      "SELLERS",
+      "TEMPERANCE HILL"
   };
 }
