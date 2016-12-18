bdb7bdcfdbd0d41f8e2b969b017468aa427ef039
Ken Corbin <ken@cadpage.org>
2016-12-18 09:16:58 -0800

    Fixed parsing problem with Rutherford County, NC

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/NC/NCRutherfordCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/NC/NCRutherfordCountyParser.java
@@ -1,5 +1,7 @@
 package net.anei.cadpage.parsers.NC;
 
+import java.util.regex.Pattern;
+
 import net.anei.cadpage.parsers.FieldProgramParser;
 import net.anei.cadpage.parsers.MsgInfo.Data;
 
@@ -8,7 +10,7 @@ public class NCRutherfordCountyParser extends FieldProgramParser {
   
   public NCRutherfordCountyParser() {
     super("RUTHERFORD COUNTY", "NC",
-           "Location:ADDR! APT/ROME:APT? City:CITY! Call_Type:CALL! Line11:INFO? Units:UNIT!");
+           "Location:ADDR! APT/ROOM:APT? City:CITY! Call_Type:CALL! Line11:INFO? Units:UNIT!");
   }
   
   @Override
@@ -16,9 +18,16 @@ public class NCRutherfordCountyParser extends FieldProgramParser {
     return "paging@rutherfordcountync.gov";
   }
   
+  private static final Pattern KEYWORD_DELIM = Pattern.compile("(?<=Location|APT/ROOM|City|Call Type|Line11|Units)[^*]");
+  
   @Override
   public boolean parseMsg(String subject, String body, Data data) {
+    
+    String[] parts = subject.split("\\|");
+    subject = parts[parts.length-1];
     do {
+      if (subject.equals("Paging")) break;
+      
       if (body.startsWith("Paging:")) {
         body = body.substring(7).trim();
         break;
@@ -26,7 +35,9 @@ public class NCRutherfordCountyParser extends FieldProgramParser {
       if (subject.endsWith("PageGate")) break;
       return false;
     } while (false);
-    body = body.replace("\n", "").replace('=', ':');
+    
+    body = body.replaceAll("\n", "");
+    body = KEYWORD_DELIM.matcher(body).replaceAll(":");
     return super.parseFields(body.split("\\*"), data);
   }
 }
