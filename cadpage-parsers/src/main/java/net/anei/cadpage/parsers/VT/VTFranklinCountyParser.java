c56ac8228a827a0a8717d821e2d5791658ca1475
Ken Corbin <ken@cadpage.org>
2017-01-11 14:47:46 -0800

    Checking in Franklin County, VT

---
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/VT/VTFranklinCountyParser.java
@@ -0,0 +1,7 @@
+package net.anei.cadpage.parsers.VT;
+
+public class VTFranklinCountyParser extends VTLamoilleCountyParser {
+  public VTFranklinCountyParser() {
+    super("FRANKLIN COUNTY", "VT");
+  }
+}
