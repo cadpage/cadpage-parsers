ebdafafe2738c48276085a3c71ff34d5f496f413
Ken Corbin <ken@cadpage.org>
2017-01-10 18:41:16 -0800

    Fixed parsing problem with Poplar Bluff

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/MO/MOPoplarBluffParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/MO/MOPoplarBluffParser.java
@@ -1,5 +1,6 @@
 package net.anei.cadpage.parsers.MO;
 
+import net.anei.cadpage.parsers.MsgInfo.Data;
 import net.anei.cadpage.parsers.dispatch.DispatchA33Parser;
 
 
@@ -13,5 +14,17 @@ public class MOPoplarBluffParser extends DispatchA33Parser {
   public String getFilter() {
     return "INFOR@PBPOLICE.ORG,INFO@PBPOLICE.ORG";
   }
+
+  @Override
+  protected boolean parseMsg(String body, Data data) {
+    if (!super.parseMsg(body, data)) return false;
+    if (data.strCross.equals("MO")) data.strCross = "";
+    else if (data.strCross.endsWith(", MO")) {
+      data.strCity = append(data.strCity, " ", data.strCross.substring(0, data.strCross.length()-4).trim());
+      data.strCross = "";
+    }
+    return true;
+  }
+  
 
 }
