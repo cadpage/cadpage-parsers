2606b3d6c2494ac2fbd32c2ad6208b2044785dfc
Ken Corbin <ken@cadpage.org>
2017-01-30 10:34:36 -0800

    Fixed parsing problem with Albermarl County, VA (B)

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/VA/VAAlbemarleCountyBParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/VA/VAAlbemarleCountyBParser.java
@@ -16,7 +16,8 @@ public class VAAlbemarleCountyBParser extends FieldProgramParser {
   
   public VAAlbemarleCountyBParser() {
     super("ALBEMARLE COUNTY", "VA",
-          "SRC! INC:ID! TYP:CALL! UNITS:UNIT! AD:ADDRCITY% APT:APT% CROSS_STREETS:X% NAME:NAME% NATURE:CALL/SDS% NARRATIVE:INFO/N+ ESN:BOX% DT:DATETIME% END");
+          "( HTML_ADDRCITY NATURE_UNIT_CROSS! INFO/N+ " +
+          "| SRC! INC:ID! TYP:CALL1! UNITS:UNIT! AD:ADDRCITY% APT:APT% CROSS_STREETS:X% NAME:NAME% NATURE:CALL/SDS% NARRATIVE:INFO/N+ ESN:BOX% DT:DATETIME% END )");
     setupProtectedNames("LEWIS AND CLARK");
   }
   
@@ -29,6 +30,7 @@ public class VAAlbemarleCountyBParser extends FieldProgramParser {
   protected boolean parseMsg(String subject, String body, Data data) {
     
     if (!subject.startsWith("Automatic R&R Notification:")) return false;
+    data.strCall = subject.substring(27).trim();
     if (!parseFields(body.split("\n"), data)) return false;
     
     // Apt is usually duplicated at end of address
@@ -40,11 +42,58 @@ public class VAAlbemarleCountyBParser extends FieldProgramParser {
   
   @Override
   public Field getField(String name) {
-    if (name.equals("SRC")) return new SourceField("[A-Za-z ]+", true);
+    if (name.equals("HTML_ADDRCITY")) return new MyHtmlAddressCityField();
+    if (name.equals("NATURE_UNIT_CROSS")) return new MyNatureUnitCrossField();
+    if (name.equals("SRC")) return new SourceField("[A-Za-z0-9 ]+", true);
     if (name.equals("ID")) return new MyIdField();
+    if (name.equals("CALL1")) return new MyCallField();
     if (name.equals("ADDRCITY")) return new MyAddressCityField();
     if (name.equals("DATETIME")) return new MyDateTimeField();
     return super.getField(name);
+  }
+  
+  private static final Pattern HTML_ADDR_CITY_PTN = Pattern.compile("http://maps.google.com/.* AD: *(.*)");
+  private class MyHtmlAddressCityField extends MyAddressCityField {
+    @Override
+    public boolean canFail() {
+      return true;
+    }
+    
+    @Override
+    public boolean checkParse(String field, Data data) {
+      Matcher match = HTML_ADDR_CITY_PTN.matcher(field);
+      if (!match.matches()) return false;
+      super.parse(match.group(1), data);
+      return true;
+    }
+    
+    @Override
+    public void parse(String field, Data data) {
+      if (!checkParse(field, data)) abort(); 
+    }
+    
+    @Override
+    public String getFieldNames() {
+      return "CALL " + super.getFieldNames();
+    }
+  }
+  
+  private static final Pattern NATURE_UNIT_CROSS_PTN = Pattern.compile("([^,]*),(.*),([^,]*),");
+  private class MyNatureUnitCrossField extends Field {
+
+    @Override
+    public void parse(String field, Data data) {
+      Matcher match = NATURE_UNIT_CROSS_PTN.matcher(field);
+      if (!match.matches()) abort();
+      data.strCall = append(data.strCall, " - ", match.group(1).trim());
+      data.strUnit = match.group(2).trim();
+      data.strCross = match.group(3).trim();
+    }
+
+    @Override
+    public String getFieldNames() {
+      return "CALL UNIT X";
+    }
   }
   
   private class MyIdField extends IdField {
@@ -53,6 +102,15 @@ public class VAAlbemarleCountyBParser extends FieldProgramParser {
       field = field.replace("[", "").replace("]", "").trim();
       if (field.startsWith("Incident not yet created")) return;
       super.parse(field, data);
+    }
+  }
+  
+  private class MyCallField extends CallField {
+    @Override
+    public void parse(String field, Data data) {
+      if (!data.strCall.startsWith(field)) {
+        data.strCall = field;
+      }
     }
   }
   
