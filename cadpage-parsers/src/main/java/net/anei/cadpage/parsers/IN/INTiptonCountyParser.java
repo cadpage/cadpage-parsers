879ce322815cfc565be097a3032216446b9597f2
Ken Corbin <ken@cadpage.org>
2017-02-08 09:38:46 -0800

    Ditto

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/IN/INTiptonCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/IN/INTiptonCountyParser.java
@@ -12,7 +12,7 @@ public class INTiptonCountyParser extends DispatchOSSIParser {
   
   @Override
   public String getFilter() {
-    return "CAD@tipco.com.CAD@tiptoncounty.in.gov";
+    return "CAD@tipco.com,CAD@tiptoncounty.in.gov";
   }
   
   @Override
