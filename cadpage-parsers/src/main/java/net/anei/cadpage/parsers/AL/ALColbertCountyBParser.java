854e48ee125bdec044c64cccad3e42565cdcf8fb
Ken Corbin <ken@cadpage.org>
2017-02-10 21:55:34 -0800

    Fixed parsing problem with Colbert COunty, AL (B)

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/AL/ALColbertCountyBParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/AL/ALColbertCountyBParser.java
@@ -9,6 +9,11 @@ public class ALColbertCountyBParser extends FieldProgramParser {
     super("COLBERT COUNTY", "AL", 
           "ID! TYPE:CALL! LOC:ADDR! CITY:CITY! GPS UNIT:UNIT! Comments:INFO/N+");
   }
+  
+  @Override
+  public String getFilter() {
+    return "colbertcoal@911email.net,e-alerts@colbert911.org";
+  }
 
   @Override
   protected boolean parseMsg(String subject, String body, Data data) {
@@ -18,7 +23,7 @@ public class ALColbertCountyBParser extends FieldProgramParser {
   
   @Override
   public Field getField(String name) {
-    if (name.equals("ID")) return new IdField("INCIDENT # +(.*)", true);
+    if (name.equals("ID")) return new IdField("INCIDENT # *(.*)", true);
     if (name.equals("GPS")) return new GPSField("[-+]?\\d{1,3}\\.\\d{6,} *, *[-+]?\\d{1,3}\\.\\d{6,}", true);
     return super.getField(name);
   }
