4d9a07da2d7869ff8f3a82d04578581f3cea478b
Ken Corbin <ken@cadpage.org>
2017-02-14 21:28:54 -0800

    Fixed parsing problem with Calcasieu Parish, LA

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/LA/LACalcasieuParishParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/LA/LACalcasieuParishParser.java
@@ -1,5 +1,6 @@
 package net.anei.cadpage.parsers.LA;
 
+import java.util.Properties;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -10,26 +11,39 @@ public class LACalcasieuParishParser extends FieldProgramParser {
   
   public LACalcasieuParishParser() {
     super("CALCASIEU PARISH", "LA", 
-          "CALL CALL2/S? ADDR PLACE? CITY! X+? INFO+");
+          "CALL CALL2/L? ADDR ( CITY! SKIPH? X+? | PLACE CITY! SKIPH? X+? | ) INFO+");
   }
   
   @Override
   public String getFilter() {
-    return "e911page@cityoflc.us";
+    return "e911page@cityoflc.us,72436050";
   }
+  
+  private static final Pattern DIR_SLASH_PTN1 = Pattern.compile("\\b([NS])/([EW])\\b");
+  private static final Pattern DIR_SLASH_PTN2 = Pattern.compile("/([NSEW])/");
   
   @Override
   public boolean parseMsg(String subject, String body, Data data) {
-    if (!subject.equals("E911400")) return false;
+    do {
+      if (subject.equals("E911400")) break;
+      if (body.startsWith("E911:")) {
+        body = body.substring(4).trim();
+        break;
+      }
+      return false;
+    } while (false);
     body = body.replace("\n", "");
+    body = DIR_SLASH_PTN1.matcher(body).replaceAll("$1$2");
+    body = DIR_SLASH_PTN2.matcher(body).replaceAll("/$1 ");
     return super.parseFields(body.split("/",-1), data);
   }
   
   @Override
   public Field getField(String name) {
-    if (name.equals("CALL2")) return new CallField("RELEASE", true);
+    if (name.equals("CALL2")) return new CallField("ARCING|DETECTOR|PI\\(MED\\)|RELEASE", true);
     if (name.equals("ADDR")) return new MyAddressField();
     if (name.equals("CITY")) return new MyCityField();
+    if (name.equals("SKIPH")) return new SkipField("H");
     return super.getField(name);
   }
   
@@ -41,7 +55,7 @@ public class LACalcasieuParishParser extends FieldProgramParser {
     }
   }
   
-  private static final Pattern UNIT_CITY_PTN = Pattern.compile("([A-Z]+\\d+) (.*)");
+  private static final Pattern MAP_CITY_PTN = Pattern.compile("([A-Z]+\\d+) (.*)");
   private class MyCityField extends CityField {
     @Override
     public boolean canFail() {
@@ -50,10 +64,11 @@ public class LACalcasieuParishParser extends FieldProgramParser {
     
     @Override
     public boolean checkParse(String field, Data data) {
-      Matcher match = UNIT_CITY_PTN.matcher(field);
+      Matcher match = MAP_CITY_PTN.matcher(field);
       if (!match.matches()) return false;
-      data.strUnit = match.group(1);
-      data.strCity = match.group(2).trim();
+      data.strMap = match.group(1);
+      String city = match.group(2).trim();
+      data.strCity = stripFieldStart(city, "AROUND ");
       return true;
     }
     
@@ -64,7 +79,24 @@ public class LACalcasieuParishParser extends FieldProgramParser {
     
     @Override
     public String getFieldNames() {
-      return "UNIT CITY";
+      return "MAP CITY";
     }
   }
+  
+  @Override
+  public String adjustMapCity(String city) {
+    return convertCodes(city, MAP_CITY_TABLE);
+  }
+  
+  private static final Properties MAP_CITY_TABLE = buildCodeTable(new String[]{
+      "DEQUI",          "DEQUINCY",
+      "HOU.R.",         "SULPHER",
+      "L C",            "LAKE CHARLES",
+      "LOWER WARD 3",   "LAKE CHARLES",
+      "MCNEESE UNIV",   "LAKE CHARLES",
+      "SE WARD 5",      "VINTON",
+      "SE WARD 6",      "SULPHER",
+      "SE WARD 7",      "SULPHER",
+      "WESTERN WD2",    "IOWA"
+  }); 
 }
