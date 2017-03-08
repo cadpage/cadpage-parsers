e07d9a21d99b436a503685bdfab8bb3d5debc6a9
Ken Corbin <ken@cadpage.org>
2017-03-08 10:43:38 -0800

    Update DeSoto County, MS (B)

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/MS/MSDeSotoCountyBParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/MS/MSDeSotoCountyBParser.java
@@ -10,8 +10,19 @@ public class MSDeSotoCountyBParser extends DispatchB2Parser {
     setupCallList(CALL_LIST);
     setupMultiWordStreets(
         "ANSLEY PARK",
+        "CEDAR LAKE",
+        "CHARLIE MOORES",
+        "COLONIAL HILLS",
+        "GREAT OAKS",
+        "LAKE SHORE",
+        "MARKET PLACE",
         "MOSS POINT",
+        "OAK CHASE",
+        "PLUM POINT",
+        "SLEEPY HOLLOW",
+        "STAR LANDING",
         "SWINNEA RIDGE",
+        "WE ROSS",
         "WINNERS CIRCLE"
    );
   }
@@ -22,23 +33,35 @@ public class MSDeSotoCountyBParser extends DispatchB2Parser {
   }
   
   private static final CodeSet CALL_LIST = new CodeSet(
+      "ABDOMINAL PAIN",
       "ACCIDENT W/INJURIES",
+      "ALTERED MENTAL STATUS",
       "ASSAULT-SIMPLE",
       "BLOOD PRESSURE",
       "CARDIAC/RESPITORY ARREST",
       "CHEST PAIN",
+      "COMMERCIAL FIRE ALARM",
+      "COMPLAINT-RECKLESS DRIVING",
+      "DIABETIC PROBLEMS",
       "DIFFICULTY BREATHING",
+      "DISTURBANCE",
+      "DOA/DEATH INVESTIGATION",
+      "GENERAL COMPLAINT OF PAIN",
       "FALLS",
       "LIFTING ASSISTANCE NO INJURIES",
       "MEDICAL ALARM",
       "MUTUAL AID DESOTO COUNTY",
+      "MUTUAL AID HERNANDO",
+      "RESIDENTIAL FIRE ALARM",
       "SEIZURES/CONVULSIONS",
       "SICK PARTY",
       "SMELL OF SMOKE INSIDE",
       "STROKE - CVA - TIA",
       "SUICIDE-THREAT",
       "SUSPICIOUS PERSON",
+      "TEST CALL",
       "TRAFFIC STOP",
-      "UNCONSCIOUS PARTY"
+      "UNCONSCIOUS PARTY",
+      "VISIBLE SMOKE OUTSIDE"
   );
 }
