08e0d824cab6ac13b3c95dd3563f56ece417b27e
Ken Corbin <ken@cadpage.org>
2017-03-07 17:49:17 -0800

    Fixed parsing problem with Midland County,  MI

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/MI/MIMidlandCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/MI/MIMidlandCountyParser.java
@@ -17,7 +17,8 @@ public class MIMidlandCountyParser extends FieldProgramParser {
   
   MIMidlandCountyParser(String defCity, String defState) {
     super(defCity, defState,
-          "BUS:PLACE! ADDX:ADDR! APT:APT! CODE:CALL! http:GPS");
+          "( ( UNIT_STAT! BUS:PLACE! | BUS:PLACE! ) ADDX:ADDR! APT:APT! CODE:CALL! http:GPS " +
+          "| CALL_ADDR! COMMENTS! INFO/N+? X? PLACE/SDS+? http:GPS ) END");
   }
   
   @Override
@@ -27,7 +28,7 @@ public class MIMidlandCountyParser extends FieldProgramParser {
   
   @Override
   public String getFilter() {
-    return "@midland911.org,sales@emergencysmc.com,9300";
+    return "@midland911.org,sales@emergencysmc.com,cfs-noreply@ioniacounty.org,9300";
   }
   
   @Override
@@ -59,6 +60,11 @@ public class MIMidlandCountyParser extends FieldProgramParser {
   
   @Override
   public Field getField(String name) {
+    if (name.equals("UNIT_STAT")) return new UnitField("Unit (\\S+) +Status:.*", true);
+    if (name.equals("CALL_ADDR")) return new MyCallAddressField();
+    if (name.equals("COMMENTS")) return new MyCommentsField();
+    if (name.equals("INFO")) return new MyInfoField();
+    if (name.equals("X")) return new MyCrossField();
     if (name.equals("GPS")) return new MyGPSField();
     return super.getField(name);
   }
@@ -70,5 +76,86 @@ public class MIMidlandCountyParser extends FieldProgramParser {
       if (!match.matches()) return;
       setGPSLoc(match.group(1) + ',' + match.group(2), data);
     }
+  }
+  
+  private class MyCallAddressField extends Field {
+    @Override
+    public void parse(String field, Data data) {
+      int pt = field.lastIndexOf(',');
+      if (pt < 0) abort();
+      data.strCall = field.substring(0, pt).trim();
+      parseAddress(field.substring(pt+1), data);
+    }
+
+    @Override
+    public String getFieldNames() {
+      return "CALL ADDR APT";
+    }
+  }
+  
+  private static final Pattern COMMENTS_PTN = Pattern.compile("(\\d\\d?:\\d\\d) *COMMENTS:");
+  private class MyCommentsField extends TimeField {
+    @Override
+    public void parse(String field, Data data) {
+      Matcher match = COMMENTS_PTN.matcher(field);
+      if (!match.matches()) abort();
+      data.strTime = match.group(1);
+    }
+  }
+  
+  private static final Pattern INFO_JUNK_PTN = Pattern.compile("\\[\\d\\d?:\\d\\d:\\d\\d .*\\]");
+  private class MyInfoField extends InfoField {
+    @Override
+    public boolean canFail() {
+      return true;
+    }
+    
+    @Override
+    public boolean checkParse(String field, Data data) {
+      if (field.startsWith("Cross Streets :") || field.startsWith("http:")) {
+        return !INFO_JUNK_PTN.matcher(getRelativeField(+1)).matches();
+      }
+      parse(field, data);
+      return true;
+    }
+    
+    @Override
+    public void parse(String field, Data data) {
+      Matcher match = INFO_JUNK_PTN.matcher(field);
+      if (match.matches()) return;
+      super.parse(field, data);
+    }
+  }
+  
+  private class MyCrossField extends CrossField {
+    @Override
+    public boolean canFail() {
+      return true;
+    }
+    
+    @Override
+    public boolean checkParse(String field, Data data) {
+      if (!field.startsWith("Cross Streets :")) return false;
+      field = field.substring(15).trim();
+      field = field.replace('*', '/');
+      super.parse(field, data);
+      return true;
+    }
+    
+    @Override
+    public void parse(String field, Data data) {
+      if (!checkParse(field, data)) abort();
+    }
+  }
+  
+  private static final Pattern HOUSE_RANGE_PTN = Pattern.compile("\\d+-\\d+ +(.*)");
+
+  @Override
+  public String adjustMapAddress(String address, String city, boolean cross) {
+    if (cross) {
+      Matcher match = HOUSE_RANGE_PTN.matcher(address);
+      if (match.matches()) address = match.group(1);
+    }
+    return address;
   }
 }
