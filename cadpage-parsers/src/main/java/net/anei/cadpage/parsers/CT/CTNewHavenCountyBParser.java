7b7c9e17dad988f93e0dda7a75dc2706cb19e0a8
Ken Corbin <ken@cadpage.org>
2017-03-25 21:19:44 -0700

    Fixed parsing problem with Tolland County, CT (B)

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/CT/CTNewHavenCountyBParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/CT/CTNewHavenCountyBParser.java
@@ -13,7 +13,9 @@ public class CTNewHavenCountyBParser extends FieldProgramParser {
   
   private Properties cityCodes = null;
   
-  private static final String PROGRAM = "ID CALL? PLACE/Z ADDR/Z APT/Z CITY/Z ZIP MAP_X UNIT DATETIME! END";
+  private static final String PROGRAM = 
+      "( SELECT/1 ID CALL? PLACE/Z ADDR1/Z APT/Z CITY/Z ZIP " +
+      "| ID CALL? PLACE/Z ADDR2 ) ( MAP_X UNIT/Z DATETIME! | UNIT/Z DATETIME! | DATETIME! ) INFO/N+";
   
   public CTNewHavenCountyBParser() {
     this(CITY_LIST, CITY_CODES, "NORTH BRANFORD", "CT");
@@ -40,7 +42,9 @@ public class CTNewHavenCountyBParser extends FieldProgramParser {
   public String getFilter() {
     return "FirePaging@hamdenfirefighters.org,paging@branfordfire.com,paging@easthavenfire.com,paging@easthavenpolice.com,paging@mail.nbpolicect.org,paging@nbpolicect.org,noreply@nexgenpss.com,pdpaging@farmington-ct.org,noreply@whpd.com";
   }
-  
+
+  private static final Pattern DELIM1 = Pattern.compile(" *\\| *");
+  private static final Pattern DELIM2 = Pattern.compile(" */ *");
   private static final Pattern MARKER = Pattern.compile("(\\d{10}) +(?:(S\\d{2}) +)?");
   private static final Pattern DATE_TIME_PTN = Pattern.compile(" +(\\d{6}) (\\d\\d:\\d\\d)(?:[ ,]|$)"); 
   private static final Pattern TRUNC_DATE_TIME_PTN = Pattern.compile(" +\\d{6} [\\d:]+$| +\\d{1,6}$"); 
@@ -57,148 +61,159 @@ public class CTNewHavenCountyBParser extends FieldProgramParser {
   @Override
   public boolean parseMsg(String body, Data data) {
     
+    body = body.replace('\n', ' ');
+    
     // See if this is one of the delimited field formats
-    String[] flds = body.split(" \\| ");
+    String[] flds = DELIM1.split(body);
     if (flds.length > 5) {
+      setSelectValue("1");
       if (!parseFields(flds, data)) return false;
     }
     
     else {
-      Matcher match = MARKER.matcher(body);
-      if (!match.lookingAt()) return false;
-      setFieldList("ID SRC CODE CALL PRI ADDR APT PLACE CITY MAP X UNIT DATE TIME INFO");
-      data.strCallId = match.group(1);
-      data.strSource = getOptGroup(match.group(2));
-      body = body.substring(match.end());
-      
-      match =  DATE_TIME_PTN.matcher(body);
-      if (match.find()) {
-        String date = match.group(1);
-        data.strDate = date.substring(2,4) + "/" + date.substring(4,6) + "/" + date.substring(0,2);
-        data.strTime = match.group(2);
-        data.strSupp = body.substring(match.end()).trim();
-        body = body.substring(0,match.start());
-      } else {
-        match = TRUNC_DATE_TIME_PTN.matcher(body);
-        if (match.find()) body = body.substring(0,match.start());
+      flds = DELIM2.split(body);
+      if (flds.length > 5) {
+        setSelectValue("2");
+        if (!parseFields(flds, data)) return false;
       }
       
-      String field = null;
-      boolean noCross = false;
-      match = ADDR_END_MARKER.matcher(body);
-      if (match.find()) {
-        field = body.substring(match.end()).trim();
-        body = body.substring(0,match.start()).trim();
-        String mark = match.group();
-        if (mark.length() > 0) {
-          match = APT_PTN.matcher(field);
-          if (match.lookingAt()) {
-            data.strApt = append(data.strApt, "-", match.group());
-            field = field.substring(match.end()).trim();
-          }
-        }
-      }
-      
-      body = cleanCity(body, data);
-      
-      StartType st = StartType.START_CALL;
-      match = PRI_MARKER.matcher(body);
-      if (match.find()) {
-        st = StartType.START_ADDR;
-        data.strCall = body.substring(0,match.start()).trim();
-        data.strPriority = match.group(1);
-        body = body.substring(match.end()).trim();
-      }
-      else if ((match = ADDR_ST_MARKER.matcher(body)).matches()) {
-        st = StartType.START_ADDR;
-        data.strCall = match.group(1).trim();
-        body = match.group(2);
-      }
-      
-      // Remove I-nn HWY construct that causes problems
-      body = I_NN_HWY_PTN.matcher(body).replaceAll("$1");
-      
-      int flags = FLAG_PAD_FIELD | FLAG_CROSS_FOLLOWS;
-      if (st == StartType.START_CALL) flags |= FLAG_START_FLD_REQ;
-      if (field != null) flags |= FLAG_ANCHOR_END;
-      parseAddress(st, flags, body, data);
-      if (field == null) {
-        field = getLeft();
-        noCross = isMBlankLeft();
-      }
-      
-      // If there is a pad field, treat it as a place or cross street
-      String pad = getPadField();
-      if (pad.contains("/") || isValidAddress(pad)) {
-        data.strCross = append(data.strCross, " / ", stripFieldStart(pad, "/"));
-      }
-      else data.strPlace = pad;
-      
-      match = MAP_PFX_PTN.matcher(field);
-      if (match.lookingAt()) {
-        field = field.substring(match.end());
-        noCross = field.startsWith("   ");
-        field = field.trim();
-        match = MAP_PTN.matcher(field);
-        if (match.lookingAt()) {
-          data.strMap = match.group().trim();
-          field = field.substring(match.end());
-          noCross = field.startsWith("  ");
-          field = field.trim();
-        }
-      }
-      
-      // Now we have to split what is left into a cross street and unit
-      // If there is a premium map marker between them, things get easy
-      if (field.startsWith("/")) field = field.substring(1).trim();
-      match = MAP_EXTRA_PTN.matcher(field);
-      if (match.find()) {
-        data.strCross = append(data.strCross, " / ", field.substring(0, match.start()).trim());
-        field = field.substring(match.end()).trim();
-        if (data.strMap.length() == 0) data.strMap = match.group(1).trim();
-      }
-      
-      // If not, our best approach is to looking for the first multiple blank delimiter.
-      // which is a heck of a lot easier to do now that double blanks are preserved by
-      // the getLeft() method.
       else {
-        if (!noCross) {
-          int pt = field.indexOf("  ");
-          if (pt >= 0) {
-            String cross = field.substring(0,pt);
-            if (data.strCity.length() == 0) {
-              parseAddress(StartType.START_OTHER, FLAG_ONLY_CITY | FLAG_ANCHOR_END, cross, data);
-              cross = getStart();
-            }
-            data.strCross = append(data.strCross, " / ", cross);
-            field = field.substring(pt+2).trim();
-          }
-          
-          // If we didn't find one, we will have to use the smart address parser to figure out where
-          // the cross street information ends
-          else {
-            flags = FLAG_ONLY_CROSS;
-            if (data.strCity.length() == 0) flags |= FLAG_ONLY_CITY;
-            Result res = parseAddress(StartType.START_ADDR, flags, field);
-            if (res.isValid()) {
-              res.getData(data);
-              field = res.getLeft();
+        Matcher match = MARKER.matcher(body);
+        if (!match.lookingAt()) return false;
+        setFieldList("ID SRC CODE CALL PRI ADDR APT PLACE CITY MAP X UNIT DATE TIME INFO");
+        data.strCallId = match.group(1);
+        data.strSource = getOptGroup(match.group(2));
+        body = body.substring(match.end());
+        
+        match =  DATE_TIME_PTN.matcher(body);
+        if (match.find()) {
+          String date = match.group(1);
+          data.strDate = date.substring(2,4) + "/" + date.substring(4,6) + "/" + date.substring(0,2);
+          data.strTime = match.group(2);
+          data.strSupp = body.substring(match.end()).trim();
+          body = body.substring(0,match.start());
+        } else {
+          match = TRUNC_DATE_TIME_PTN.matcher(body);
+          if (match.find()) body = body.substring(0,match.start());
+        }
+        
+        String field = null;
+        boolean noCross = false;
+        match = ADDR_END_MARKER.matcher(body);
+        if (match.find()) {
+          field = body.substring(match.end()).trim();
+          body = body.substring(0,match.start()).trim();
+          String mark = match.group();
+          if (mark.length() > 0) {
+            match = APT_PTN.matcher(field);
+            if (match.lookingAt()) {
+              data.strApt = append(data.strApt, "-", match.group());
+              field = field.substring(match.end()).trim();
             }
           }
         }
+        
+        body = cleanCity(body, data);
+        
+        StartType st = StartType.START_CALL;
+        match = PRI_MARKER.matcher(body);
+        if (match.find()) {
+          st = StartType.START_ADDR;
+          data.strCall = body.substring(0,match.start()).trim();
+          data.strPriority = match.group(1);
+          body = body.substring(match.end()).trim();
+        }
+        else if ((match = ADDR_ST_MARKER.matcher(body)).matches()) {
+          st = StartType.START_ADDR;
+          data.strCall = match.group(1).trim();
+          body = match.group(2);
+        }
+        
+        // Remove I-nn HWY construct that causes problems
+        body = I_NN_HWY_PTN.matcher(body).replaceAll("$1");
+        
+        int flags = FLAG_PAD_FIELD | FLAG_CROSS_FOLLOWS;
+        if (st == StartType.START_CALL) flags |= FLAG_START_FLD_REQ;
+        if (field != null) flags |= FLAG_ANCHOR_END;
+        parseAddress(st, flags, body, data);
+        if (field == null) {
+          field = getLeft();
+          noCross = isMBlankLeft();
+        }
+        
+        // If there is a pad field, treat it as a place or cross street
+        String pad = getPadField();
+        if (pad.contains("/") || isValidAddress(pad)) {
+          data.strCross = append(data.strCross, " / ", stripFieldStart(pad, "/"));
+        }
+        else data.strPlace = pad;
+        
+        match = MAP_PFX_PTN.matcher(field);
+        if (match.lookingAt()) {
+          field = field.substring(match.end());
+          noCross = field.startsWith("   ");
+          field = field.trim();
+          match = MAP_PTN.matcher(field);
+          if (match.lookingAt()) {
+            data.strMap = match.group().trim();
+            field = field.substring(match.end());
+            noCross = field.startsWith("  ");
+            field = field.trim();
+          }
+        }
+        
+        // Now we have to split what is left into a cross street and unit
+        // If there is a premium map marker between them, things get easy
+        if (field.startsWith("/")) field = field.substring(1).trim();
+        match = MAP_EXTRA_PTN.matcher(field);
+        if (match.find()) {
+          data.strCross = append(data.strCross, " / ", field.substring(0, match.start()).trim());
+          field = field.substring(match.end()).trim();
+          if (data.strMap.length() == 0) data.strMap = match.group(1).trim();
+        }
+        
+        // If not, our best approach is to looking for the first multiple blank delimiter.
+        // which is a heck of a lot easier to do now that double blanks are preserved by
+        // the getLeft() method.
+        else {
+          if (!noCross) {
+            int pt = field.indexOf("  ");
+            if (pt >= 0) {
+              String cross = field.substring(0,pt);
+              if (data.strCity.length() == 0) {
+                parseAddress(StartType.START_OTHER, FLAG_ONLY_CITY | FLAG_ANCHOR_END, cross, data);
+                cross = getStart();
+              }
+              data.strCross = append(data.strCross, " / ", cross);
+              field = field.substring(pt+2).trim();
+            }
+            
+            // If we didn't find one, we will have to use the smart address parser to figure out where
+            // the cross street information ends
+            else {
+              flags = FLAG_ONLY_CROSS;
+              if (data.strCity.length() == 0) flags |= FLAG_ONLY_CITY;
+              Result res = parseAddress(StartType.START_ADDR, flags, field);
+              if (res.isValid()) {
+                res.getData(data);
+                field = res.getLeft();
+              }
+            }
+          }
+        }
+        
+        // If we have not found a city, see if there is one here
+        String city = data.strCity;
+        data.strCity = "";
+        parseAddress(StartType.START_ADDR, FLAG_ONLY_CITY, field, data);
+        if (data.strCity.length() > 0) field = getLeft();
+        if (city.length() > 0) data.strCity = city;
+        
+        // Whatever is left becomes the unit
+        data.strUnit = field.replaceAll("  +", " ");
+        
+        if (cityCodes != null) data.strCity = convertCodes(data.strCity, cityCodes);
       }
-      
-      // If we have not found a city, see if there is one here
-      String city = data.strCity;
-      data.strCity = "";
-      parseAddress(StartType.START_ADDR, FLAG_ONLY_CITY, field, data);
-      if (data.strCity.length() > 0) field = getLeft();
-      if (city.length() > 0) data.strCity = city;
-      
-      // Whatever is left becomes the unit
-      data.strUnit = field.replaceAll("  +", " ");
-      
-      if (cityCodes != null) data.strCity = convertCodes(data.strCity, cityCodes);
     }
 
     Matcher match = CALL_CODE_PTN.matcher(data.strCall);
@@ -233,7 +248,8 @@ public class CTNewHavenCountyBParser extends FieldProgramParser {
   public Field getField(String name) {
     if (name.equals("ID")) return new IdField("\\d{10}");
     if (name.equals("PLACE")) return new MyPlaceField();
-    if (name.equals("ADDR")) return new MyAddressField();
+    if (name.equals("ADDR1")) return new MyAddress1Field();
+    if (name.equals("ADDR2")) return new MyAddress2Field();
     if (name.equals("APT")) return new MyAptField();
     if (name.equals("ZIP")) return new SkipField("\\d{5}");
     if (name.equals("MAP_X")) return new MyMapCrossField();
@@ -260,11 +276,42 @@ public class CTNewHavenCountyBParser extends FieldProgramParser {
     }
   }
   
-  private class MyAddressField extends AddressField {
+  private class MyAddress1Field extends AddressField {
     @Override
     public void parse(String field, Data data) {
       field = cleanAddress(field);
       super.parse(field, data);;
+    }
+  }
+
+  private static final Pattern ADDRESS_ZIP_PTN = Pattern.compile("(.*) (\\d{5})");
+  private class MyAddress2Field extends AddressField {
+    @Override
+    public boolean canFail() {
+      return true;
+    }
+    
+    @Override
+    public boolean checkParse(String field, Data data) {
+      field = cleanAddress(field);
+      Matcher match = ADDRESS_ZIP_PTN.matcher(field);
+      if (!match.matches()) return false;
+      field = match.group(1).trim();
+      String zip = match.group(2);
+      
+      parseAddress(StartType.START_ADDR, FLAG_ANCHOR_END, field, data);
+      if (data.strCity.length() == 0) data.strCity = zip;
+      return true;
+    }
+    
+    @Override
+    public void parse(String field, Data data) {
+      if (!checkParse(field, data)) abort();
+    }
+    
+    @Override
+    public String getFieldNames() {
+      return "ADDR APT CITY";
     }
   }
   
