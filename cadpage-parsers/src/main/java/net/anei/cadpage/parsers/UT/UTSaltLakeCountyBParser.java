d12363f01fb44adafaafd20a84d5c92b83779585
Ken Corbin <ken@cadpage.org>
2017-04-11 20:13:31 -0700

    Update A911 parser table

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/UT/UTSaltLakeCountyBParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/UT/UTSaltLakeCountyBParser.java
@@ -5,6 +5,9 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import net.anei.cadpage.parsers.SmartAddressParser;
+import net.anei.cadpage.parsers.StandardCodeTable;
+import net.anei.cadpage.parsers.CodeSet;
+import net.anei.cadpage.parsers.CodeTable;
 import net.anei.cadpage.parsers.MsgInfo.Data;
 
 public class UTSaltLakeCountyBParser extends SmartAddressParser {
@@ -19,7 +22,7 @@ public class UTSaltLakeCountyBParser extends SmartAddressParser {
   }
   
   private static final Pattern MOVE_PTN = Pattern.compile("UNIT#:(\\S+) *, (Move from \\S+ *, To Post: \\S+) *, \\((.*)\\)");
-  private static final Pattern MASTER_PTN = Pattern.compile("UNIT#:(\\S+) RUN#(\\S+) *, LOC:(.*?)APT#:(.*?)BLDG:(.*), ADD LOC:(.*?)CROSS ST:(.*?), (PAT.\\S*) *PRIORITY:(.*?), INIT.BY:.*?, ZIP:(.*?) LAT/LON(.*?)");
+  private static final Pattern MASTER_PTN = Pattern.compile("UNIT#:(\\S+) RUN#(\\S+) *, LOC:(.*?)APT#:(.*?)BLDG:(.*), ADD LOC:(.*?)CROSS ST:(.*?), PAT.COND(\\S*) *PRIORITY:(.*?), INIT.BY:.*?, ZIP:(.*?) LAT/LON(.*?)");
   private static final Pattern GPS_PTN = Pattern.compile("([-+]?\\d{2,3})(\\d{6}) ([-+]?\\d{2,3})(\\d{6})");
 
   @Override
@@ -34,7 +37,7 @@ public class UTSaltLakeCountyBParser extends SmartAddressParser {
     }
     
     if ((match = MASTER_PTN.matcher(body)).matches()) {
-      setFieldList("UNIT ID ADDR CITY APT PLACE INFO X PRI GPS");
+      setFieldList("UNIT ID ADDR CITY APT PLACE INFO X CODE CALL PRI GPS");
       data.strUnit = match.group(1);
       data.strCallId = match.group(2);
       parseAddressCity(match.group(3).trim(), data);
@@ -43,7 +46,10 @@ public class UTSaltLakeCountyBParser extends SmartAddressParser {
       data.strPlace = match.group(5).trim();
       data.strSupp = match.group(6).trim();
       parseAddress(StartType.START_ADDR, FLAG_ONLY_CROSS | FLAG_IMPLIED_INTERSECT| FLAG_NO_CITY | FLAG_ANCHOR_END, match.group(7).trim(), data);
-      // Skip field 8 for now
+      data.strCode = match.group(8);
+      String call = CALL_CODES.getCodeDescription(data.strCode);
+      if (call == null) call = data.strCode;
+      data.strCall = call;
       data.strPriority = match.group(9).trim();
       if (data.strCity.length() == 0) data.strCity = match.group(10).trim();
       String gps = match.group(11).trim();
@@ -57,6 +63,8 @@ public class UTSaltLakeCountyBParser extends SmartAddressParser {
     }
     return false;
   }
+  
+  private static CodeTable CALL_CODES = new StandardCodeTable();
   
   private static final Properties CITY_CODES = buildCodeTable(new String[]{
       "SLC", "SALT LAKE CITY"
