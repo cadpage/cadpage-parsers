a8fa4ce2d31db7d9da8fe9a7a552552dc5387049
Ken Corbin <ken@cadpage.org>
2017-04-11 22:50:11 -0700

    Fixed parsing problem in Suffolk County, NY (G)

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/NY/NYSuffolkCountyGParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/NY/NYSuffolkCountyGParser.java
@@ -1,5 +1,8 @@
 package net.anei.cadpage.parsers.NY;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import net.anei.cadpage.parsers.FieldProgramParser;
 import net.anei.cadpage.parsers.MsgInfo.Data;
 
@@ -8,7 +11,7 @@ public class NYSuffolkCountyGParser extends FieldProgramParser {
   
   public NYSuffolkCountyGParser() {
     super("SUFFOLK COUNTY", "NY",
-          "CALL! TOA:TIMEDATE! ADDR! PLACE APT CS:X SRC ID INFO+? UNIT UNIT+");
+          "CALL! TOA:TIMEDATE/d! ADDR! PLACE APT CS:X SRC ID INFO+? UNIT UNIT+");
     setupMultiWordStreets("INDIAN HEAD");
   }
   
@@ -19,21 +22,24 @@ public class NYSuffolkCountyGParser extends FieldProgramParser {
   
   @Override
   protected boolean parseMsg(String body, Data data) {
-    return parseFields(body.split("\n"), 3, data);
+    String[] flds =  body.split("\n");
+    if (flds.length > 1) return parseFields(body.split("\n"), 3, data);
+    return parseNewFormat(body, data);
   }
   
-  private class MyTimeDateField extends TimeDateField {
-    @Override
-    public void parse(String field, Data data) {
-      field = field.replace('-', '/');
-      super.parse(field, data);
-    }
+  @Override
+  public Field getField(String name) {
+    if (name.equals("ADDR")) return new MyAddressField();
+    if (name.equals("ID")) return new IdField("\\d{4}-\\d{6}", true);
+    if (name.equals("INFO")) return new MyInfoField();
+    if (name.equals("UNIT")) return new MyUnitField();
+    return super.getField(name);
   }
   
   private class MyAddressField extends AddressField {
     @Override
     public void parse(String field, Data data) {
-      if (field.endsWith("'")) field = field.substring(0,field.length()-1).trim();
+      field = stripFieldEnd(field, "'");
       Parser p = new Parser(field);
       String state = p.getLast(',');
       if (!state.equals("NY")) abort();
@@ -68,13 +74,21 @@ public class NYSuffolkCountyGParser extends FieldProgramParser {
     }
   }
   
-  @Override
-  public Field getField(String name) {
-    if (name.equals("TIMEDATE")) return new MyTimeDateField();
-    if (name.equals("ADDR")) return new MyAddressField();
-    if (name.equals("ID")) return new IdField("\\d{4}-\\d{6}", true);
-    if (name.equals("INFO")) return new MyInfoField();
-    if (name.equals("UNIT")) return new MyUnitField();
-    return super.getField(name);
+  private static final Pattern MASTER = Pattern.compile("(.*?) TOA: (\\d\\d:\\d\\d) (\\d\\d-\\d\\d-\\d\\d) ([^a-z]*?) (?:CS: ([^a-z]*?) )?(\\S*[a-z].*?) (\\d{4}-\\d{6})");
+  
+  private boolean parseNewFormat(String body, Data data) {
+    
+    Matcher match = MASTER.matcher(body);
+    if (!match.matches()) return false;
+    setFieldList("CALL TIME DATE ADDR APT PLACE X SRC ID");
+    data.strCall = match.group(1).trim();
+    data.strTime = match.group(2);
+    data.strDate = match.group(3).replace('-', '/');
+    parseAddress(StartType.START_ADDR, match.group(4).trim(), data);
+    data.strPlace = getLeft();
+    data.strCross = getOptGroup(match.group(5));
+    data.strSource = match.group(6).trim();
+    data.strCallId = match.group(7);
+    return true;
   }
 }
