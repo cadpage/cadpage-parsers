367d00b43f8b6167db5fe514ffacd5ab59c1da4e
Ken Corbin <ken@cadpage.org>
2017-04-19 14:18:56 -0700

    Fixed parsing problem with Adams County, CO

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/CO/COAdamsCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/CO/COAdamsCountyParser.java
@@ -4,157 +4,96 @@ import java.util.Properties;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import net.anei.cadpage.parsers.FieldProgramParser;
+import net.anei.cadpage.parsers.MsgParser;
 import net.anei.cadpage.parsers.MsgInfo.Data;
 
 
 
-public class COAdamsCountyParser extends FieldProgramParser {
-  
-  private static final Pattern CAD_MARKER = 
-        Pattern.compile("^(?:Subject:)?IPS I/Page Notifica(?:tion|\\.\\.\\.) (?:/ )?");
-  private static final Pattern INTERSTATE_PTN = Pattern.compile("INTERSTATE \\d+(?: [NSEW]B)?", Pattern.CASE_INSENSITIVE);
-  
+public class COAdamsCountyParser extends MsgParser {
   public COAdamsCountyParser() {
-    super(CITY_TABLE, "ADAMS COUNTY", "CO",
-           "ADDR! TYPE_CODE:CALL! CALLER_NAME:NAME! TIME:TIME% Comments:INFO Disp:UNIT");
+    super("ADAMS COUNTY", "CO");
+    setFieldList("UNIT CALL ADDR APT CITY ID GPS CH INFO");
   }
   
   @Override
   public String getFilter() {
-    return "ipspage@adcom911.org";
+    return "tritechpaging@adcom911.org";
   }
+  
+  private static final Pattern INFO_JUNK_PTN = Pattern.compile("\\[\\d{1,2}\\]| *\\[Shared\\]");
 
   @Override
-  protected boolean parseMsg(String subject, String body, Data data) {
-    
-    if (subject.length() > 0) body = "Subject:" + subject + ' ' + body;
-
-    Matcher match = CAD_MARKER.matcher(body);
-    if (!match.find()) return false;
-    body = body.substring(match.end()).trim();
-    if (super.parseMsg(body, data)) {
-      if (data.strPlace.length() > 0 && INTERSTATE_PTN.matcher(data.strAddress).matches()) {
-        data.strAddress = "";
-        parseAddress(data.strPlace, data);
-        data.strPlace = "";
-      }
+  protected boolean parseMsg(String body, Data data) {
+    FParser fp = new FParser(body);
+    if (fp.check("***NOTIFICATION***")) {
+      if (!fp.check("UNIT:")) return false;
+      data.strUnit = fp.get(30);
+      if (!fp.check("PROBLEM:")) return false;
+      data.strCall = fp.get(26);
+      if (!fp.check("ADDRESS:")) return false;
+      parseAddress(fp.get(30), data);
+      if (!fp.check("APT/BLDG#:")) return false;
+      data.strApt = append(data.strApt, "-", fp.get(8));
+      if (!fp.check("CITY:")) return false;
+      data.strCity = fixCity(fp.get(25));
+      if (!fp.check("ZIP:")) return  false;
+      String zip = fp.get(5);
+      if (data.strCity.length() == 0) data.strCity = zip;
+      if (!fp.check("  INC#:")) return false;
+      data.strCallId = fp.get(20);
+      if (!fp.check("LATITUDE:")) return false;
+      String gps1 = fp.get(8);
+      if (!fp.check("  LONGITUTDE:")) return false;
+      String gps2 = fp.get(9);
+      setGPSLoc(fixCoord(gps1)+','+fixCoord(gps2), data);
+      if (!fp.check(" RADIO CHANNEL:")) return false;
+      data.strChannel = fp.get(7);
+      fp.setOptional();
+      fp.check("   ");
+      if (!fp.check("COMMENTS")) return false;
+      String info = fp.get();
+      data.strSupp = INFO_JUNK_PTN.matcher(info).replaceAll("");
       return true;
     }
-    
-    // Fallback parsing address followed by call description
-    setFieldList("CALL PLACE ADDR APT CITY INFO");
-    data.initialize(this);
-    parseAddress(StartType.START_CALL, FLAG_AT_SIGN_ONLY, body, data);
-    if (!isValidAddress()) return false;
-    if (data.strCall.length() == 0) data.strCall = getLeft();
-    else data.strSupp = getLeft();
-    return (data.strCall.length() > 0);
+    if (!fp.check("UNIT: ")) return false;
+    data.strUnit = fp.get(6);
+    if (!fp.check("PROBLEM: ")) return false;
+    data.strCall = fp.get(26);
+    if (!fp.check("ADDRESS: ")) return false;
+    parseAddress(fp.get(30), data);
+    if (!fp.check("APT/BLDG#: ")) return false;
+    data.strApt = append(data.strApt, "-", fp.get(8));
+    if (!fp.check("CITY: ")) return false;
+    data.strCity = fixCity(fp.get(25));
+    if (!fp.check("ZIP: ")) return  false;
+    String zip = fp.get(5);
+    if (data.strCity.length() == 0) data.strCity = zip;
+    if (!fp.check("  INC#: ")) return false;
+    data.strCallId = fp.get(20);
+    if (!fp.check("LATITUDE: ")) return false;
+    String gps1 = fp.get(9);
+    if (!fp.check("  LONGITUDE: ")) return false;
+    String gps2 = fp.get(9);
+    setGPSLoc(fixCoord(gps1)+','+fixCoord(gps2), data);
+    if (!fp.check(" RADIO CHANNEL: ")) return false;
+    data.strChannel = fp.get(7);
+    fp.setOptional();
+    if (!fp.check("COMMENTS")) return false;
+    String info = fp.get();
+    data.strSupp = INFO_JUNK_PTN.matcher(info).replaceAll("");
+    return true;
   }
   
-  
-  private static final Pattern TIME_MARK = Pattern.compile(" *\\b(\\d\\d:\\d\\d:\\d\\d)$");
-  private class MyAddressField extends AddressField {
-    @Override
-    public void parse(String field, Data data) {
-      field = field.replace("E 470", "E-470");
-    
-      Matcher match = TIME_MARK.matcher(field);
-      if (match.find()) {
-        data.strTime = match.group(1);
-        field = field.substring(0, match.start());
-      }
-      int pt = field.lastIndexOf(':');
-      String sPlace = null;
-      String sPlace2 = null;
-      if (pt >= 0) {
-        sPlace = stripFieldStart(field.substring(pt+1).trim(), "@");
-        field = field.substring(0,pt).trim();
-      }
-      
-      if (sPlace != null) {
-        pt = field.lastIndexOf(':');
-        if (pt >= 0) {
-          sPlace2 = sPlace;
-          sPlace = stripFieldStart(field.substring(pt+1).trim(), "@");
-          field = field.substring(0,pt);
-        }
-      }
-  
-      String city = CITY_TABLE.getProperty(field);
-      if (city != null) {
-        data.strCity = city;
-        field = sPlace;
-        if (field == null) field = "";
-        sPlace = sPlace2;
-        sPlace2 = null;
-      }
-      
-      if (sPlace2 != null && !sPlace.equals(sPlace2)) sPlace = append(sPlace, ": ", sPlace2);
-      
-      pt = field.lastIndexOf(',');
-      if (pt >= 0) {
-        data.strApt = field.substring(pt+1).trim();
-        field = field.substring(0,pt).trim();
-      }
-      if (field.startsWith("MM ")) {
-        data.strAddress = field;
-      } else if (data.strCity.length() > 0) {
-        parseAddress(field, data);
-      } else {
-        parseAddress(StartType.START_ADDR, FLAG_ANCHOR_END, field, data);
-      }
-      if (sPlace != null) {
-        String sCross = null;
-        pt = sPlace.indexOf('/');
-        if (pt >= 0) {
-          String sPart1 = sPlace.substring(0,pt).trim();
-          String sPart2 = sPlace.substring(pt+1).trim();
-          if (sPart1.equals(data.strAddress)) sCross = sPart2;
-          else if (sPart2.equals(data.strAddress)) sCross = sPart1; 
-        }
-        if (sCross != null) data.strAddress = append(data.strAddress, " & ", sCross);
-        else data.strPlace = sPlace;
-      }
-    }
-    
-    @Override
-    public String getFieldNames() {
-      return "ADDR CITY APT PLACE";
-    }
+  private String fixCity(String city) {
+    city = stripFieldStart(city, "UNICORPORATED");
+    if (city.endsWith(" COUNT")) city += "Y";
+    return city;
   }
   
-  private class MyTimeField extends TimeField {
-    @Override
-    public void parse(String field, Data data) {
-      if (data.strTime.length() > 0) return;
-      super.parse(field, data);
-    }
-  }
-  
-  private class MyInfoField extends InfoField {
-    @Override
-    public void parse(String field, Data data) {
-      Matcher match = GPS_PATTERN.matcher(field);
-      if (match.find()) {
-        setGPSLoc(match.group(), data);
-        field = field.substring(match.end()).trim();
-      }
-      super.parse(field, data);
-    }
-    
-    @Override 
-    public String getFieldNames() {
-      return "GPS INFO";
-    }
-  }
-  
-  @Override
-  public Field getField(String name) {
-    if (name.equals("ADDR")) return new MyAddressField();
-    if (name.equals("TIME")) return new MyTimeField();
-    if (name.equals("INFO")) return new MyInfoField();
-    return super.getField(name);
+  private String fixCoord(String coord) {
+    int pt = coord.length()-6;
+    if (pt >= 0)  coord = coord.substring(0, pt) + '.' + coord.substring(pt);
+    return coord;
   }
   
   @Override
@@ -163,20 +102,4 @@ public class COAdamsCountyParser extends FieldProgramParser {
   }
   private Pattern TWO_PT_FIVE = Pattern.compile("\\b2\\.5\\b");
   
-  private static final Properties CITY_TABLE = buildCodeTable(new String[]{
-      "ADAM ADAM", "ADAMS COUNTY",
-      "ADAM ARV",  "ARVADA",
-      "ADAM AUR",  "AURORA",
-      "ADAM BPD",  "BRIGHTON",
-      "ADAM CCPD", "COMMERCE CITY",
-      "ADAM FHPD", "FEDERAL HEIGHTS",
-      "ADAM TPD",  "THORNTON",
-      "ADAM NPD",  "NORTHGLENN",
-      "ADAM WES",  "WESTMINSTER",
-      "ARAP ARAP", "ARAPAHOE COUNTY",
-      "JECO ARV",  "ARVADA",
-      "WELD BPD",  "BRIGHTON",
-      "WELD FTLP", "FORT LUPTON",
-      "WELD WELD", "WELD COUNTY"
-  });
 }
