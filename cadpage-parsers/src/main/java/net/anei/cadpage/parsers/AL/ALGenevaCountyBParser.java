757b4e303c98ecea460a48027d8f444cb2a9b44d
Ken Corbin <ken@cadpage.org>
2017-05-09 09:24:34 -0700

    Fixed parsing problem with Geneva County, AL (B)

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/AL/ALGenevaCountyBParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/AL/ALGenevaCountyBParser.java
@@ -1,6 +1,9 @@
 
 package net.anei.cadpage.parsers.AL;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import net.anei.cadpage.parsers.FieldProgramParser;
 import net.anei.cadpage.parsers.MsgInfo.Data;
 
@@ -12,7 +15,7 @@ public class ALGenevaCountyBParser extends FieldProgramParser {
 
   public ALGenevaCountyBParser() {
     super("GENEVA COUNTY", "AL",
-          "CALL! Location:ADDR! City:CITY! Subject:CALL! Place:PLACE! Details:INFO!");
+          "CALL! Location:ADDR! City:CITY! Subject:CALL/SDS? Place:PLACE? Details:INFO!");
   }
     
   @Override
@@ -25,10 +28,12 @@ public class ALGenevaCountyBParser extends FieldProgramParser {
     return MAP_FLG_SUPPR_LA;
   }
   
+  private static final Pattern MARKER = Pattern.compile("34 ?Central:_ *");
   @Override
   protected boolean parseMsg(String body, Data data) {
-    if (!body.startsWith("34 Central:_")) return false;
-    body = body.substring(12).trim();
+    Matcher match = MARKER.matcher(body);
+    if (!match.lookingAt()) return false;
+    body = body.substring(match.end());
     return super.parseFields(body.split(" //"), data);
   }
 }
