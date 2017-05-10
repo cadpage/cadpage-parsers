2b07a12d80f0c00cb990d3cdaba88d431e3be962
Ken Corbin <ken@cadpage.org>
2017-05-10 16:01:01 -0700

    Update msg doc

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/MS/MSDeSotoCountyAParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/MS/MSDeSotoCountyAParser.java
@@ -2,6 +2,7 @@ package net.anei.cadpage.parsers.MS;
 
 import java.util.regex.Pattern;
 
+import net.anei.cadpage.parsers.CodeSet;
 import net.anei.cadpage.parsers.MsgInfo.Data;
 import net.anei.cadpage.parsers.dispatch.DispatchB3Parser;
 
@@ -11,6 +12,8 @@ public class MSDeSotoCountyAParser extends DispatchB3Parser {
 
   public MSDeSotoCountyAParser() {
     super("911CENTER:", CITY_LIST, "DESOTO COUNTY", "MS", B2_FORCE_CALL_CODE);
+    setupCallList(CALL_LIST);
+    setupMultiWordStreets(MWORD_STREET_LIST);
     setupSaintNames("ANDREWS");
   }
   
@@ -24,6 +27,60 @@ public class MSDeSotoCountyAParser extends DispatchB3Parser {
     body = DIR_BOUND_PTN.matcher(body).replaceAll("$1B");
     return super.parseMsg(subject, body, data);
   }
+  
+  private static final String[] MWORD_STREET_LIST = new String[]{
+    "BUSINESS CENTER",
+    "CAMP CREEK",
+    "CARTER LANE",
+    "CENTER HILL",
+    "CREEK SIDE",
+    "DEVON PARK",
+    "FOX HUNTERS",
+    "GEE GEE",
+    "HACKS CROSS",
+    "HUNTERS HILL",
+    "HWY 178",
+    "HWY 302",
+    "JOE LYON",
+    "LAUREL HILL",
+    "METHODIST HOSPITAL",
+    "PIGEON ROOST",
+    "PLANTATION RIDGE",
+    "PLEASANT HILL",
+    "POLK LANE",
+    "REGAL BEND",
+    "SANDIDGE CENTER",
+    "SHADY GROVE",
+    "ST ANDREWS",
+    "ST MICHAEL THOMAS",
+    "STONEY BROOK",
+    "TIMBER OAKS",
+    "WHISPERING PINES"
+  };
+  
+  private static final CodeSet CALL_LIST = new CodeSet(
+      "10 CHEST PAIN / DISCOMFORT",
+      "12 CONVULSIONS / SEIZURES",
+      "13 DIABETIC PROBLEMS",
+      "17 FALLS WITH INJURY",
+      "19 HEART PROBLEMS / A.I.C.D.",
+      "20 HEAT / COLD EXPOSURE",
+      "21 HEMORRHAGE / LACERATIONS",
+      "26 SICK PERSON (SPECIFIC DIAG)",
+      "29 MVA WITH INJURIES",
+      "30 TRAUMATIC INJURIES",
+      "31 UNCONSCIOUS / FAINTING",
+      "4 ASSAULT / RAPE WITH INJURY",
+      "5 BACK PAIN (NON TRAUMATIC)",
+      "6 BREATHING PROBLEMS",
+      "ELECTRICAL SHORT / NO SMOKE",
+      "FIRE ALARM - COMMERCIAL",
+      "FIRE ALARM - RESIDENTIAL",
+      "GRASS / BRUSH / TREE FIRE",
+      "LIFTING ASSISTANCE (NO INJURY)",
+      "MEDICAL ALARM",
+      "VEHICLE FIRE"
+  );
 
   static final String[] CITY_LIST = new String[]{
     
