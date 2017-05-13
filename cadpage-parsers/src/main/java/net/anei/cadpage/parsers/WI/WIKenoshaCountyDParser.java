46e6120dc0403564a81fdc8358891bfc8200782c
Ken Corbin <ken@cadpage.org>
2017-05-13 10:48:27 -0700

    Fixed another problem with Kenosha COunty, WI (D)

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/WI/WIKenoshaCountyDParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/WI/WIKenoshaCountyDParser.java
@@ -1,5 +1,6 @@
 package net.anei.cadpage.parsers.WI;
 
+import net.anei.cadpage.parsers.MsgInfo.Data;
 import net.anei.cadpage.parsers.dispatch.DispatchA57Parser;
 
 public class WIKenoshaCountyDParser extends DispatchA57Parser {
@@ -12,6 +13,18 @@ public class WIKenoshaCountyDParser extends DispatchA57Parser {
     super(defState, defCity);
   }
   
+  @Override
+  protected boolean parseMsg(String body, Data data) {
+    if (!super.parseMsg(body, data)) return false;
+    int pt = data.strCross.lastIndexOf("Add");
+    if (pt >= 0) {
+      if ("Additional Location Info:".startsWith(data.strCross.substring(pt))) {
+        data.strCross = data.strCross.substring(0,pt).trim();
+      }
+    }
+    return true;
+  }
+
   @Override
   public String getFilter() {
     return "dispatch@kenoshajs.org";
