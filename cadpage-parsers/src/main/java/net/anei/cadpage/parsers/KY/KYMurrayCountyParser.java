b2e2497b4bcf59081fd329a940a1eca1d8fd06fa
Ken Corbin <ken@cadpage.org>
2017-05-13 14:28:42 -0700

    Fixed mapping tables for Murray County, KY

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/KY/KYMurrayCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/KY/KYMurrayCountyParser.java
@@ -1,6 +1,7 @@
 package net.anei.cadpage.parsers.KY;
 
 import java.util.Properties;
+import java.util.regex.Pattern;
 
 import net.anei.cadpage.parsers.dispatch.DispatchSPKParser;
 
@@ -12,17 +13,34 @@ public class KYMurrayCountyParser extends DispatchSPKParser {
 
   @Override
   public String getFilter() {
-    return "MurrayE911CAD@murrayky.gov";
+    return "MurrayE911CAD@murrayky.gov,@active911.com";
   }
   
+  private static final Pattern NORTH_PTN = Pattern.compile("\\bNORTH\\b");
+  private static final Pattern STREET_PTN = Pattern.compile("\\bST\\b");
+  
+  @Override
+  protected String adjustGpsLookupAddress(String address) {
+    address = address.toUpperCase();
+    address = NORTH_PTN.matcher(address).replaceAll("N");
+    address = STREET_PTN.matcher(address).replaceAll("ST");
+    address = address.replace(" 16 ST", " 16TH ST");
+    return address;
+  }
+
   private static final Properties GPS_LOOKUP_TABLE = buildCodeTable(new String[]{
       "1457 CHESTNUT ST",                     "+36.615523,-88.321282",
-      "1459 CHESTNUT STREET",                 "+36.615713,-88.322963",
+      "1459 CHESTNUT ST",                     "+36.615713,-88.322963",
+      "1503 CHESTNUT ST",                     "+36.615713,-88.322963",
+      "0 DORM CIR",                           "+36.618263,-88.320921",
       "58 DORM CIR",                          "+36.617188,-88.322025",
-      "70 DORM CIR",                          "+36.618298,-88.321803",
+      "70 DORM CIR",                          "+36.618292,-88.321608",
+      "140 DORM CIR",                         "+36.617402,-88.320712",
       "176 DORM CIR",                         "+36.617535,-88.319584",
+      "238 DORM CIR",                         "+36.618664,-88.319455",
       "290 DORM CIR",                         "+36.619501,-88.320618",
       "298 DORM CIR",                         "+36.618664,-88.319455",
+      "358 DORM CIR",                         "+36.619541,-88.321911",
       "300 DORM CIR",                         "+36.617402,-88.320712",
       "358 DORM CIR",                         "+36.619541,-88.321911",
       "0 DORM CIR X",                         "+36.618263,-88.320921",
@@ -33,34 +51,46 @@ public class KYMurrayCountyParser extends DispatchSPKParser {
       "125 EDUCATION LOOP",                   "+36.614111,-88.322454",
       "147 EDUCATION LOOP",                   "+36.613406,-88.322511",
       "149 EDUCATION LOOP",                   "+36.613822,-88.322354",
+      "150 EDUCATION LOOP",                   "+36.613526,-88.321467",
       "178 EDUCATION LOOP",                   "+36.613017,-88.323613",
       "180 EDUCATION LOOP",                   "+36.613008,-88.322297",
       "400 EDUCATION LOOP",                   "+36.613526,-88.321467",
+      "500 EDUCATION LOOP",                   "+36.615034,-88.323461",
       "27 GILBERT GRAVES DR",                 "+36.617224,-88.318206",
       "95 GILBERT GRAVES DR",                 "+36.618082,-88.318173",
       "1401 HIGHWAY 121 BYPASS N",            "+36.622671,-88.320007",
       "301 N 14TH ST",                        "+36.612954,-88.320366",
       "311 N 14TH ST",                        "+36.614551,-88.320619",
+      "201 N 15TH ST",                        "+36.611069,-88.322714",
+      "210 N 15TH ST",                        "+36.611576,-88.322281",
+      "213 N 15TH ST",                        "+36.611576,-88.322281",
+      "215 N 15TH ST",                        "+36.611987,-88.322276",
+      "240 N 16TH ST",                        "+36.611044,-88.323723",
       "300 N 16TH ST",                        "+36.611395,-88.323385",
       "302 N 16TH ST",                        "+36.611812,-88.323697",
+      "312 N 16TH ST",                        "+36.612405,-88.323651",
+      "313 N 16TH ST",                        "+36.613455,-88.325841",
+      "315 N 16TH ST",                        "+36.612537,-88.325829",
+      "350 N 16TH ST",                        "+36.612405,-88.323651",
+      "380 N 16TH ST",                        "+36.612537,-88.325829",
       "401 N 16TH ST",                        "+36.613816,-88.324753",
-      "201 NORTH 15 STREET",                  "+36.611069,-88.322714",
-      "213 NORTH 15 STREET",                  "+36.611987,-88.322276",
-      "210 NORTH 15TH STREET",                "+36.611576,-88.322281",
-      "240 NORTH 16 STREET",                  "+36.611044,-88.323723",
-      "350 NORTH 16 STREET",                  "+36.612405,-88.323651",
-      "402 NORTH 16 STREET",                  "+36.613455,-88.325841",
-      "380 NORTH 16TH STREET",                "+36.612537,-88.325829",
-      "500 NORTH 16TH STREET",                "+36.615034,-88.323461",
-      "503 NORTH 16TH STREET",                "+36.615206,-88.324036",
-      "980 NORTH 16TH STREET",                "+36.622371,-88.324093",
+      "402 N 16TH ST",                        "+36.613455,-88.325841",
+      "500 N 16TH ST",                        "+36.615034,-88.323461",
+      "502 N 16TH ST",                        "+36.615206,-88.324036",
+      "503 N 16TH ST",                        "+36.615206,-88.324036",
+      "924 N 16TH ST",                        "+36.622371,-88.324093",
+      "980 N 16TH ST",                        "+36.622371,-88.324093",
+      "1500 OLIVE BLVD",                      "+36.613008,-88.322297",
       "1304 PAYNE ST",                        "+36.615394,-88.317916",
-      "1399 PAYNE STREET",                    "+36.614715,-88.319621",
+      "1317 PAYNE ST",                        "+36.614715,-88.319621",
+      "1399 PAYNE ST",                        "+36.614715,-88.319621",
       "1000 RACER DR",                        "+36.620676,-88.320505",
       "50 REGENTS DR",                        "+36.615853,-88.318301",
+      "1520 UNIVERSITY DR",                   "+36.611069,-88.322714",
       "1525 UNIVERSITY DR",                   "+36.610074,-88.322553",
+      "1570 UUNIVERSITY DR",                  "+36.611044,-88.323723",
       "810 WALDROP DR",                       "+36.618341,-88.322459",
       "906 WALDROP DR",                       "+36.619654,-88.322478"
-      
+     
   });
 }
