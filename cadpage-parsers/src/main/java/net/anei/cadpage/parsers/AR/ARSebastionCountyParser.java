490ee7a27405159ffe185aa5da325a68cdf2a74c
Ken Corbin <ken@cadpage.org>
2017-05-15 11:47:01 -0700

    Added Sebastian County, AR

---
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/AR/ARSebastionCountyParser.java
@@ -0,0 +1,91 @@
+package net.anei.cadpage.parsers.AR;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import net.anei.cadpage.parsers.FieldProgramParser;
+import net.anei.cadpage.parsers.MsgInfo.Data;
+import net.anei.cadpage.parsers.SplitMsgOptions;
+import net.anei.cadpage.parsers.SplitMsgOptionsCustom;
+
+public class ARSebastionCountyParser extends FieldProgramParser {
+  
+  public ARSebastionCountyParser() {
+    super("SEBASTIAN COUNTY", "AR", 
+          "CALL:CODE_CALL! PLACE:PLACE! ADDR:ADDR! CITY:CITY! ID:ID! PRI:PRI! DATE:DATETIME! TIME:SKIP! UNIT:UNIT% INFO:INFO% INFO/N+");
+  }
+  
+  @Override
+  public String getFilter() {
+    return "dispatch@fsems.org";
+  }
+  
+  
+  @Override
+  public SplitMsgOptions getActive911SplitMsgOptions() {
+    return new SplitMsgOptionsCustom();
+  }
+
+  @Override
+  protected boolean parseMsg(String body, Data data) {
+    if (body.length() >= 243 && body.length() <= 265) data.expectMore = true;
+    return super.parseFields(body.split("\n"), data);
+  }
+  
+  @Override
+  public Field getField(String name) {
+    if (name.equals("CODE_CALL")) return new MyCodeCallField();
+    if (name.equals("ADDR")) return new MyAddressField();
+    if (name.equals("PRI")) return new MyPriorityField();
+    if (name.equals("DATETIME")) return new MyDateTimeField();
+    return super.getField(name);
+  }
+
+  private static final Pattern CODE_CALL_PTN = Pattern.compile("(\\S+) - (.*)");
+  private class MyCodeCallField extends Field {
+    @Override
+    public void parse(String field, Data data) {
+      Matcher match = CODE_CALL_PTN.matcher(field);
+      if (match.matches()) {
+        data.strCode = match.group(1);
+        data.strCall = match.group(2).trim();
+      }
+      data.strCall = field;
+    }
+
+    @Override
+    public String getFieldNames() {
+      return "CODE CALL";
+    }
+  }
+  
+  private class MyAddressField extends AddressField {
+    @Override
+    public void parse(String field, Data data) {
+      field = stripFieldEnd(field, ",");
+      super.parse(field, data);
+    }
+  }
+  
+  private class MyPriorityField extends PriorityField {
+    @Override
+    public void parse(String field, Data data) {
+      field = stripFieldStart(field, "Priority");
+      super.parse(field, data);
+    }
+  }
+  
+  private static final Pattern DATE_TIME_PTN = Pattern.compile("(\\d\\d?/\\d\\d?/\\d{4}) (\\d\\d?:\\d\\d:\\d\\d [AP]M)");
+  private static final DateFormat TIME_FMT = new SimpleDateFormat("hh:mm:ss aa");
+  private class MyDateTimeField extends DateTimeField {
+    @Override
+    public void parse(String field, Data data) {
+      Matcher match = DATE_TIME_PTN.matcher(field);
+      if (!match.matches()) abort();
+      data.strDate = match.group(1);
+      setTime(TIME_FMT, match.group(2), data);
+    }
+  }
+}
