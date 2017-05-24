e4b655643a0d040c783b0da08cb86bfbb675c8bc
Ken Corbin <ken@cadpage.org>
2017-05-24 09:17:05 -0700

    Fixed parsing problem with Kenosha County, WI (C)

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/WI/WIKenoshaCountyCParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/WI/WIKenoshaCountyCParser.java
@@ -1,15 +1,30 @@
 package net.anei.cadpage.parsers.WI;
 
+import java.util.Properties;
+
+import net.anei.cadpage.parsers.MsgInfo.Data;
 import net.anei.cadpage.parsers.dispatch.DispatchProphoenixParser;
 
 public class WIKenoshaCountyCParser extends DispatchProphoenixParser {
   
   public WIKenoshaCountyCParser() {
-    super("KENOSHA COUNTY", "WI");
+    super(CITY_CODES, "KENOSHA COUNTY", "WI");
   }
   
   @Override
   public String getFilter() {
     return "CAD@plprairiewi.com";
   }
+
+  @Override
+  protected boolean parseMsg(String body, Data data) {
+    int pt = body.indexOf("\n\n\n");
+    if (pt >= 0) body = body.substring(0, pt).trim();
+    return super.parseMsg(body, data);
+  }
+  
+  private static final Properties CITY_CODES = buildCodeTable(new String[]{
+      "VPP", "Pleasant Prairie"
+  });
+  
 }
