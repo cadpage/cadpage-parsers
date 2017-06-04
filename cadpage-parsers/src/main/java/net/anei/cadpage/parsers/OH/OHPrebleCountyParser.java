ad55b52dd154aca4c9c3adb7ad8e66b0e0e85532
Ken Corbin <ken@cadpage.org>
2017-06-04 15:32:13 -0700

    Fixed parsing problem with Preble County, OH (Added B)

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/OH/OHPrebleCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/OH/OHPrebleCountyParser.java
@@ -1,103 +1,14 @@
 package net.anei.cadpage.parsers.OH;
 
-import java.util.Properties;
+import net.anei.cadpage.parsers.GroupBestParser;
 
-import net.anei.cadpage.parsers.MsgInfo.Data;
-import net.anei.cadpage.parsers.dispatch.DispatchEmergitechParser;
+/*
+ * Preble County, OH
+ */
 
-public class OHPrebleCountyParser extends DispatchEmergitechParser {
+public class OHPrebleCountyParser extends GroupBestParser {
   
   public OHPrebleCountyParser() {
-    super("PREBLESHERIFF:", CITY_LIST, "PREBLE COUNTY", "OH", TrailAddrType.PLACE);
+    super(new OHPrebleCountyAParser(), new OHPrebleCountyBParser());
   }
-  
-  @Override
-  public String getFilter() {
-    return "PREBLESHERIFF@swohio.twcbc.com";
-  }
-
-  @Override
-  protected boolean parseMsg(String body, Data data) {
-    if (!super.parseMsg(body, data)) return false;
-    data.strCode = data.strCall;
-    data.strCall = convertCodes(data.strCode, CALL_CODES);
-    return true;
-  }
-  
-  @Override
-  public String getProgram() {
-    return super.getProgram().replace("CALL", "CODE CALL");
-  }
-
-  private static final Properties CALL_CODES = buildCodeTable(new String[]{
-      "4",  "MVA",
-      "16", "Dead Body",
-      "28", "Fire",
-      "29", "EMS Response",
-      "C4", "MVA"
-  });
-
-  private static final String[] CITY_LIST = new String[]{
-    
-    // City
-    "EATON",
-
-    // Villages
-    "COLLEGE CORNER",
-    "CAMDEN",
-    "ELDORADO",
-    "GRATIS",
-    "LEWISBURG",
-    "NEW PARIS",
-    "VERONA",
-    "WEST ALEXANDRIA",
-    "WEST ELKTON",
-    "WEST MANCHESTER",
-
-    // Townships
-    "DIXON",
-    "GASPER",
-    "GRATIS",
-    "HARRISON",
-    "ISRAEL",
-    "JACKSON",
-    "JEFFERSON",
-    "LANIER",
-    "MONROE",
-    "SOMERS",
-    "TWIN",
-    "WASHINGTON",
-
-    // Unincorporated communities
-
-    "BRENNERSVILLE",
-    "BRINLEY",
-    "BROWNS",
-    "CAMPBELLSTOWN",
-    "CEDAR SPRINGS",
-    "DADSVILLE",
-    "EBENEZER",
-    "ENTERPRISE",
-    "FAIRHAVEN",
-    "GETTYSBURG",
-    "GREENBUSH",
-    "HAMBURG",
-    "INGOMAR",
-    "MORNING SUN",
-    "MUTTONVILLE",
-    "NEW HOPE",
-    "NEW LEXINGTON",
-    "NEW WESTVILLE",
-    "SUGAR VALLEY",
-    "TALAWANDA SPRINGS",
-    "WEST FLORENCE",
-    "WEST SONORA",
-    "WHEATVILLE",
-    
-    // Butler County
-    "SOMERVILLE",
-    
-    // Montgomery County
-    "GERMANTOWN"
-  };
 }
