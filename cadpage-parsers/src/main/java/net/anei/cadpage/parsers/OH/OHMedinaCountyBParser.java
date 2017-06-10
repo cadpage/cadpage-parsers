a826095d8f33ef11b8d024745e53726dc275f607
Ken Corbin <ken@cadpage.org>
2017-06-10 08:56:47 -0700

    Added call code lookup for Medina County, OH(B)

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/OH/OHMedinaCountyBParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/OH/OHMedinaCountyBParser.java
@@ -1,5 +1,8 @@
 package net.anei.cadpage.parsers.OH;
 
+import java.util.Properties;
+
+import net.anei.cadpage.parsers.MsgInfo.Data;
 import net.anei.cadpage.parsers.dispatch.DispatchEmergitechParser;
 
 
@@ -14,6 +17,52 @@ public class OHMedinaCountyBParser extends DispatchEmergitechParser {
   public String getFilter() {
     return "@c-msg.net";
   }
+  
+  @Override
+  protected boolean parseMsg(String body, Data data) {
+    if (!super.parseMsg(body, data)) return false;
+    String call = CALL_CODES.getProperty(data.strCall);
+    if  (call != null) {
+      data.strCode = data.strCall;
+      data.strCall = call;
+    }
+    return true;
+  }
+
+  @Override
+  public String getProgram() {
+    return super.getProgram().replaceAll("CALL", "CODE CALL");
+  }
+
+  private static final Properties CALL_CODES = buildCodeTable(new String[]{
+      "16",       "Dead onArrival",
+      "22",       "Drowning",
+      "28",       "UnknownFire",
+      "2812BC",   "Commercial Fire Alarm",
+      "2812BR",   "Residential Fire Alarm",
+      "28D",      "Dumpster Fire",
+      "28G",      "Grass/Brush Fire",
+      "28SC",     "Commercial Structure Fire",
+      "28SR",     "Residential Structure Fire",
+      "28U",      "Utility/Transformer Fire",
+      "28V",      "Vehicle Fire",
+      "29",       "Squad/Ambulance",
+      "2912B",    "Medical Alarm",
+      "4",        "Accident w/Injuries",
+      "4PP",      "Private Property Injury Crash",
+      "6",        "Airplane Crash",
+      "CAVEIN",   "Cave In/Technical Rescue",
+      "CO",       "CO Detector no symptoms",
+      "CO29",     "CO detector w/symptoms",
+      "GASLEAK",  "Gas Leak Natural/Fuel",
+      "GASSMELL", "Smell of Gas",
+      "GASWELL",  "Gas Well Fire",
+      "HAZMAT",   "Spill",
+      "SMOKE",    "Smoke Investigation",
+      "WEATHER",  "Weather Related Incident",
+      "WIRES",    "Wires Down or Sparking"
+
+  });
 
   private static final String[] CITY_LIST = new String[]{
       
