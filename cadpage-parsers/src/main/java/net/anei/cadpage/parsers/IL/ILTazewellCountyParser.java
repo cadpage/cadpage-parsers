ef859cbd4d77fbd943f6c89eaabb398e342ac8ed
Ken Corbin <ken@cadpage.org>
2017-06-16 09:21:50 -0700

    Fixed parsing problem with Tazewell County, IN

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/IL/ILTazewellCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/IL/ILTazewellCountyParser.java
@@ -1,5 +1,7 @@
 package net.anei.cadpage.parsers.IL;
 
+import java.util.Properties;
+
 import net.anei.cadpage.parsers.MsgInfo.Data;
 import net.anei.cadpage.parsers.dispatch.DispatchOSSIParser;
 
@@ -7,8 +9,9 @@ import net.anei.cadpage.parsers.dispatch.DispatchOSSIParser;
 public class ILTazewellCountyParser extends DispatchOSSIParser {
   
   public ILTazewellCountyParser() {
-    super("TAZEWELL COUNTY", "IL",
-           "FYI SRC DATETIME CALL ADDR! X X INFO+");
+    super(CITY_CODES, "TAZEWELL COUNTY", "IL",
+         "( CANCEL ADDR CITY! " +
+         "| FYI SRC DATETIME CALL ADDR! CITY? X X ) INFO+");
   }
   
   @Override
@@ -18,7 +21,7 @@ public class ILTazewellCountyParser extends DispatchOSSIParser {
   
   @Override
   protected boolean parseMsg(String subject, String body, Data data) {
-    if (subject.length() > 0 && body.startsWith("CAD:;")) {
+    if (subject.length() > 0 && body.startsWith("CAD:")) {
       body = "CAD:" + subject + ": " + body.substring(4);
     }
     return super.parseMsg(body, data);
@@ -29,6 +32,40 @@ public class ILTazewellCountyParser extends DispatchOSSIParser {
     if (name.equals("SRC")) return new SourceField("[A-Z]{4}", true);
     if (name.equals("DATETIME")) return new DateTimeField("\\d\\d/\\d\\d/\\d{4} \\d\\d:\\d\\d:\\d\\d", true);
     return super.getField(name);
-    
   }
+
+  private static Properties CITY_CODES = buildCodeTable(new String[]{
+      "AR", "ARMINGTON",
+      "BA", "BARTONVILLE",
+      "CR", "CREVE COEUR",
+      "CU", "CUBA",
+      "DA", "DANVERS",
+      "DC", "DEER CREEK",
+      "DE", "DELAVAN",
+      "EM", "EMDEN",
+      "EP", "EAST PEORIA",
+      "EU", "EUREKA",
+      "GO", "GOODFIELD",
+      "GR", "GROVELAND",
+      "GT", "GERMANTOWN HILLS",
+      "GV", "GREEN VALLEY",
+      "HO", "HOPEDALE",
+      "MA", "MACKINAW",
+      "ME", "METAMORA",
+      "MH", "MARQUETTE HEIGHTS",
+      "MI", "MINIER",
+      "MN", "MANITO",
+      "MO", "MORTON",
+      "NP", "NORTH PEKIN",
+      "PE", "PEKIN",
+      "PH", "PEORIA HEIGHTS",
+      "PO", "PEORIA",
+      "SB", "SPRING BAY",
+      "SJ", "SAN JOSE",
+      "SP", "SOUTH PEKIN",
+      "ST", "STANFORD",
+      "TR", "TREMONT",
+      "WA", "WASHINGTON"
+ 
+  });
 }
