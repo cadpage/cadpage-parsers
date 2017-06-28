07ea32592ae4409eabb1514cb943b7a7a1c87f74
Ken Corbin <ken@cadpage.org>
2017-06-27 20:02:10 -0700

    Update A911 parser table

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/TX/TXJohnsonCountyAParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/TX/TXJohnsonCountyAParser.java
@@ -1,30 +1,17 @@
 package net.anei.cadpage.parsers.TX;
 
-import net.anei.cadpage.parsers.MsgInfo.Data;
 import net.anei.cadpage.parsers.dispatch.DispatchOSSIParser;
 
 public class TXJohnsonCountyAParser extends DispatchOSSIParser {
 
   public TXJohnsonCountyAParser() {
-    super("JOHNSON COUNTY", "TX", "( CANCEL ADDR | FYI SRC SRC? CALL ADDR X+? INFO+ )");
+    super("JOHNSON COUNTY", "TX", "( CANCEL ADDR | FYI? SRC UNIT? CALL ADDR X+? INFO+ )");
   }
 
   @Override
   public Field getField(String name) {
-    if (name.equals("SRC")) return new MySourceField();
+    if (name.equals("SRC")) return new SourceField("[A-Z0-9]{1,3}", true);
+    if (name.equals("UNIT"))  return new UnitField("[A-Z]+\\d+", true);
     return super.getField(name);
   }
-  
-  private class MySourceField extends SourceField {
-    
-    public MySourceField() {
-      super("(.{0,3})", true);
-    }
-    
-    @Override 
-    public void parse(String field, Data data) {
-      data.strSource = append(data.strSource, " ", field);
-    }
-  }
-  
 }
