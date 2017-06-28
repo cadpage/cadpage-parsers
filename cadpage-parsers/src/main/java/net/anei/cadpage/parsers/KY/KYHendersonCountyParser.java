20a8bbdab1a471cd96c6e175056acc6d994144f9
Ken Corbin <ken@cadpage.org>
2017-06-28 08:31:25 -0700

    Checking in Henderson County,KY

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/KY/KYHendersonCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/KY/KYHendersonCountyParser.java
@@ -1,5 +1,6 @@
 package net.anei.cadpage.parsers.KY;
 
+import net.anei.cadpage.parsers.MsgInfo.Data;
 import net.anei.cadpage.parsers.dispatch.DispatchA1Parser;
 
 public class KYHendersonCountyParser extends DispatchA1Parser {
@@ -13,4 +14,13 @@ public class KYHendersonCountyParser extends DispatchA1Parser {
     return "E911@cityofhendersonky.org";
   }
 
+  @Override
+  protected boolean parseMsg(String subject, String body, Data data) {
+    if (!subject.equals("AUTOMATED MESSAGE DO NOT REPLY")) return false;
+    int pt = body.indexOf('\n');
+    if (pt < 0) return false;
+    subject = body.substring(0, pt).trim();
+    body = body.substring(pt+1).trim();
+    return super.parseMsg(subject, body, data);
+  }
 }
