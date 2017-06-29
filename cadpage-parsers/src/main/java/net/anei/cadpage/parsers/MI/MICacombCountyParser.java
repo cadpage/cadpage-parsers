e3a88d85532ee7386addf25c0df335977318df80
Ken Corbin <ken@cadpage.org>
2017-06-28 21:52:23 -0700

    update A911 parser table

---
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/MI/MICacombCountyParser.java
@@ -0,0 +1,23 @@
+package net.anei.cadpage.parsers.MI;
+
+import net.anei.cadpage.parsers.FieldProgramParser;
+import net.anei.cadpage.parsers.MsgInfo.Data;
+
+public class MICacombCountyParser extends FieldProgramParser {
+  
+  public MICacombCountyParser() {
+    super("MACOMB COUNTY", "MI", 
+          "ADDR:ADDR! CITY:CITY! TYPE:CALL! INFO:INFO!");
+  }
+  
+  @Override
+  public String getFilter() {
+    return "RichmondPaging@comcast.net";
+  }
+  
+  @Override
+  protected boolean parseMsg(String body, Data data) {
+    return parseFields(body.split(" ; "), data);
+  }
+
+}
