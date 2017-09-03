882cd3b6376aa6449969c2920491735c6a91bc93
Ken Corbin <ken@cadpage.org>
2017-09-03 07:43:02 -0700

    Fixed parsing problem with Chester County, PA (L)

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/PA/PAChesterCountyLParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/PA/PAChesterCountyLParser.java
@@ -7,15 +7,13 @@ import net.anei.cadpage.parsers.MsgInfo.Data;
 
 public class PAChesterCountyLParser extends PAChesterCountyBaseParser {
   
-  private static final Pattern MASTER = 
-      Pattern.compile("(\\d\\d:\\d\\d)  +([^\\*]+?)(?: \\*)?  ([^,]+?(?:,[ A-Z0-9]+?)?)  (.+)  (\\d\\d/\\d\\d/\\d\\d)  ([A-Z]{4}\\d{8})\\b(.*)");
-  private static final Pattern SRC_PTN = Pattern.compile("  +(?:[A-Z]+  +)?(\\d+)\\b");
-  private static final Pattern UNIT_PTN = Pattern.compile("  +([A-Z0-9]+),$");
-  
   public PAChesterCountyLParser() {
     super(null);
-    setFieldList("TIME CALL ADDR APT CITY PLACE DATE ID SRC X UNIT");
+    setFieldList("TIME CALL ADDR APT CITY PLACE DATE ID CITY X");
   }
+  
+  private static final Pattern MASTER = 
+      Pattern.compile("(\\d\\d:\\d\\d)  +([^\\*]+?)(?: \\*)?  ([^,]+?(?:,[ A-Z0-9]+?)?)  (.+)  (\\d\\d/\\d\\d/\\d\\d)  ([A-Z]{1,4}\\d{8})  +([A-Z]+)(?:  +(.*))?");
 
   @Override
   protected boolean parseMsg(String subject, String body, Data data) {
@@ -28,24 +26,12 @@ public class PAChesterCountyLParser extends PAChesterCountyBaseParser {
     data.strTime = match.group(1);
     data.strCall = match.group(2).trim();
     parseChesterAddress(match.group(3).trim(), data);
-    data.strPlace = stripFieldEnd(match.group(4).trim(), "-");
+    data.strPlace = stripFieldStart(match.group(4).trim(), "-");
     data.strDate = match.group(5);
     data.strCallId = match.group(6);
-    String left = match.group(7);
+    data.strCity = convertCityCode(match.group(7));
+    data.strCross = getOptGroup(match.group(8));
 
-    match = SRC_PTN.matcher(left);
-    if (match.lookingAt()) {
-      data.strSource = match.group(1);
-      left = left.substring(match.end());
-    }
-    
-    match = UNIT_PTN.matcher(left);
-    if (match.find()) {
-      data.strUnit = match.group(1);
-      left = left.substring(0,match.start());
-    }
-
-    data.strCross = left.trim();
     return true;
   }
 } 
