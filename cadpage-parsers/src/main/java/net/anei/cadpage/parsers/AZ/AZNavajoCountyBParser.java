9132d22a73462fc5fb4b88df13eee9e826ef8f26
Ken Corbin <ken@cadpage.org>
2017-09-09 13:32:33 -0700

    Fixed parsing problem with Navajo County, AZ (B)

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/AZ/AZNavajoCountyBParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/AZ/AZNavajoCountyBParser.java
@@ -1,5 +1,8 @@
 package net.anei.cadpage.parsers.AZ;
 
+import java.util.regex.Pattern;
+
+import net.anei.cadpage.parsers.MsgInfo.Data;
 import net.anei.cadpage.parsers.dispatch.DispatchA20Parser;
 
 /**
@@ -14,5 +17,13 @@ public class AZNavajoCountyBParser extends DispatchA20Parser {
   @Override
   public String getFilter() {
     return "@ci.show-low.ca.us";
+  }
+  
+  private static final Pattern SUBJECT_PTN = Pattern.compile("\\([A-Z]+\\)");
+
+  @Override
+  protected boolean parseMsg(String subject, String body, Data data) {
+    if (!SUBJECT_PTN.matcher(subject).matches()) return false;
+    return super.parseMsg("Dispatched Call ()", body, data);
   }
 }
