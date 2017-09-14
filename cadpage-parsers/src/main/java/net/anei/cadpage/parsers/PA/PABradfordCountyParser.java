4def0c25966722c6c46d5140426468af13e8f3d3
Ken Corbin <ken@cadpage.org>
2017-09-14 15:59:29 -0700

    Update sender filter for Bradford County, PA

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/PA/PABradfordCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/PA/PABradfordCountyParser.java
@@ -20,7 +20,7 @@ public class PABradfordCountyParser extends DispatchA65Parser {
 
   @Override
   public String getFilter() {
-    return "bradfordpa911@911email.net,geoconex@nlamerica.com";
+    return "bradfordpa911@911email.net,geoconex@nlamerica.com,911comm1.info";
   }
 
   @Override
