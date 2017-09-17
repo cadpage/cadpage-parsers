414aa5a277abc018b9385ac3095da733d7f3494d
Ken Corbin <ken@cadpage.org>
2017-09-17 11:09:44 -0700

    Update msg doc

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/IN/INStJosephCountyBParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/IN/INStJosephCountyBParser.java
@@ -46,7 +46,7 @@ public class INStJosephCountyBParser extends HtmlProgramParser {
     if (name.equals("X")) return new MyCrossField();
     if (name.equals("PLACE")) return new MyPlaceField();
     if (name.equals("SIMPLE")) return new SkipField("Simple|Complex", true);
-    if (name.equals("UNIT")) return new UnitField("(?:(?:[A-Z]+\\d+[A-Z]?|BTFDTA)\\b[, ]*)+", true);
+    if (name.equals("UNIT")) return new UnitField("(?:(?:[A-Z]+\\d+[A-Z]?|BTFDTA|Mutual Aid)\\b[, ]*)+", true);
     if (name.equals("DATEMARK")) return new SkipField("\\*{3}\\d\\d?/\\d\\d?/\\d{4}\\*{3}", true);
     if (name.equals("INFO")) return new MyInfoField();
     return super.getField(name);
