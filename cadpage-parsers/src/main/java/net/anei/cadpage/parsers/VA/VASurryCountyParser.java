9bc4e3ff693d5b98750af1589ad46f65be912a85
Ken Corbin <ken@cadpage.org>
2017-09-29 15:03:36 -0700

    Fixed parssing problem with SUrry County, VA

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/VA/VASurryCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/VA/VASurryCountyParser.java
@@ -26,7 +26,6 @@ public class VASurryCountyParser extends DispatchDAPROParser {
     Parser p = new Parser(subject);
     data.strUnit = p.getLast(' ');
     String source = p.get();
-    if (source.length() == 0) return false;
     if (!super.parseMsg(body,  data)) return false;
     data.strBox = data.strSource;
     data.strSource = source;
@@ -39,15 +38,27 @@ public class VASurryCountyParser extends DispatchDAPROParser {
   }
   
   private static final CodeSet CALL_SET = new CodeSet(
+      "ABDOMINAL PAIN",
+      "CHEST PAIN/HEART ATTACK",
+      "CITIZEN ASSIST",
+      "DIFFICULTY BREATHING",
+      "FALL",
       "MVA- MOTOR VEHICLE ACCIDENT",
+      "MVA W/ INJURIES",
       "ODOR OF SMOKE IN STRUCTURE",
       "OTHER/UNKNOWN PROBLEM",
+      "PAIN",
+      "SICK",
       "STRUCTURE FIRE",
       "UNCONSCIOUS/UNRESPONSIVE"
   );
   
   private static final Properties CITY_CODE_TABLE =
     buildCodeTable(new String[]{
-        "DEN", "DENDRON"
+        "DEN", "DENDRON",
+        "ELB", "ELBERON",
+        "SPR", "SPRING GROVE",
+        "SUR", "SURRY",
+        "WAV", "WAVERLY"
     });
 }
