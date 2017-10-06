85f8bffbe9db5b55b4bf6ae1966bc23b14fa8b3e
Ken Corbin <ken@cadpage.org>
2017-10-06 15:57:01 -0700

    Update msg doc

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/MO/MOCamdenCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/MO/MOCamdenCountyParser.java
@@ -1,24 +1,14 @@
 package net.anei.cadpage.parsers.MO;
 
-import net.anei.cadpage.parsers.MsgInfo.Data;
-import net.anei.cadpage.parsers.dispatch.DispatchSPKParser;
+import net.anei.cadpage.parsers.dispatch.DispatchA33Parser;
 
-public class MOCamdenCountyParser extends DispatchSPKParser {
+public class MOCamdenCountyParser extends DispatchA33Parser {
   public MOCamdenCountyParser() {
     super("CAMDEN COUNTY", "MO");
   }
 
   @Override
   public String getFilter() {
-    return "dispatch@camdenmo.org";
+    return "DISPATCH@CAMDENSO-MO.US";
   }
-
-  @Override
-  protected boolean parseHtmlMsg(String subject, String body, Data data) {
-    if (!super.parseHtmlMsg(subject, body, data)) return false;
-    int pt = data.strSupp.indexOf("CONFIDENTIALITY NOTICE:");
-    if (pt >= 0) data.strSupp = data.strSupp.substring(0, pt).trim();
-    return true;
-  }
-  
 }
