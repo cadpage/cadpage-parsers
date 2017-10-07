13d0a6f1a73d1d7cdf73d39dac2f52b03c988e0a
Ken Corbin <ken@cadpage.org>
2017-10-06 21:07:19 -0700

    Fixed parsing problem with Surry County, NC

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/NC/NCSurryCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/NC/NCSurryCountyParser.java
@@ -14,20 +14,33 @@ import net.anei.cadpage.parsers.SmartAddressParser;
  */
 public class NCSurryCountyParser extends SmartAddressParser {
   
-  private static final Pattern MASTER = Pattern.compile("SC911::?(?:Call #|=)(\\d{6,}-\\d{4}) \\[(?:Address|Location)\\] (.*?) \\[X St\\] (.*?) \\[Type\\] (.*)");
+  private static final Pattern MISSING_INFO = Pattern.compile("(?<=[A-Z])(?=\\d\\d/\\d\\d/\\d{4} \\d\\d:\\d\\d:\\d\\d : )");
+  private static final Pattern MASTER = Pattern.compile("SC911::?(?:Call #|=)(\\d{6,}-\\d{4}) \\[(?:Address|Location)\\] (.*?) \\[X St\\] (.*?) \\[Type\\] (.*)\\[Info\\] *(.*)");
+  private static final Pattern INFO_BRK_PTN = Pattern.compile(" {2,}");
+  private static final Pattern INFO_JUNK_PTN = Pattern.compile("\\d\\d/\\d\\d/\\d{4} .*|Cross st.*|EMD .*");
   
   public NCSurryCountyParser() {
     super("SURRY COUNTY", "NC");
-    setFieldList("ID ADDR APT CITY ST X CALL");
+    setFieldList("ID ADDR APT CITY ST X CALL INFO");
   }
   
   @Override
   public String getFilter() {
-    return "SC911:@co.surry.nc.us,sc911@co.surry.nc.us";
+    return "SC911:@co.surry.nc.us,sc911@co.surry.nc.us,Surry911@co.surry.nc.us";
   }
   
   @Override
   public boolean parseMsg(String body, Data data) {
+    
+    // Fix missing [Info] tags
+    if (!body.contains("[Info]")) {
+      Matcher match = MISSING_INFO.matcher(body);
+      if (match.find()) {
+        body =  body.substring(0,match.start()) + "[Info]" + body.substring(match.end());
+      } else {
+        body += "[Info]";
+      }
+    }
     Matcher match = MASTER.matcher(body);
     if (!match.matches()) return false;
     data.strCallId = match.group(1);
@@ -55,6 +68,13 @@ public class NCSurryCountyParser extends SmartAddressParser {
       data.strCross = data.strCross.substring(0,data.strCross.length()-3).trim();
     }
     data.strCall = match.group(4).trim();
+    
+    String info = match.group(5).trim();
+    for (String line : INFO_BRK_PTN.split(info)) {
+      if (INFO_JUNK_PTN.matcher(line).matches()) continue;
+      data.strSupp = append(data.strSupp, "\n", line);
+    }
+    
     return true;
   }
   
