199e5d28617fb9e40240df5d5b12a65c38f37184
Ken Corbin <ken@cadpage.org>
2017-10-08 23:04:29 -0700

    Fixed parsing problem with Fannin County, GA (Added   B)

---
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/GA/GAFanninCountyAParser.java
@@ -0,0 +1,80 @@
+package net.anei.cadpage.parsers.GA;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import net.anei.cadpage.parsers.CodeSet;
+import net.anei.cadpage.parsers.MsgInfo.Data;
+import net.anei.cadpage.parsers.dispatch.DispatchB2Parser;
+
+
+public class GAFanninCountyAParser extends DispatchB2Parser {
+
+  public GAFanninCountyAParser() {
+    super("911-CENTER:", "FANNIN COUNTY", "GA", B2_FORCE_CALL_CODE);
+    setupCallList(CALL_LIST);
+    setupMultiWordStreets(
+        "BETHEL CHURCH",
+        "DRY BRANCH",
+        "FISH TRAP",
+        "FOREST COVE",
+        "GRADY HUNT",
+        "MINERAL BLUFF",
+        "POWER DAM",
+        "SCENIC RIDGE",
+        "WATSON GAP",
+        "WINDY RIDGE"
+    );
+  }
+  
+  @Override
+  public String getFilter() {
+    return "911-CENTER@mydomain.com";
+  }  
+   
+  private static final Pattern BEGIN_PTN = Pattern.compile("BEGINS? +(.*)");
+  private static final Pattern LEAD_DIGIT_PTN = Pattern.compile("(\\d+) *(.*)");
+  
+  @Override
+  protected boolean parseMsg(String body, Data data) {
+    if (!super.parseMsg(body, data)) return false;
+    
+    // Weird cross street convention, which may or may not have prevented
+    // the cross street from being identified as such
+    Matcher match = BEGIN_PTN.matcher(data.strCross);
+    if (match.matches()) {
+      data.strCross = match.group(1);
+    }
+    else if ((match = BEGIN_PTN.matcher(data.strName)).matches()) {
+      parseAddress(StartType.START_ADDR, FLAG_ONLY_CROSS, match.group(1), data);
+      data.strName = getLeft();
+    }
+    match = LEAD_DIGIT_PTN.matcher(data.strName);
+    if (match.matches()) {
+      data.strCross = append(data.strCross, " ", match.group(1));
+      data.strName = match.group(2);
+    }
+    
+    return true;
+  }
+  
+  @Override
+  public String adjustMapAddress(String addr, boolean cross) {
+    if (cross) {
+      Matcher match = LEAD_DIGIT_PTN.matcher(addr);
+      if (match.matches()) addr = match.group(2);
+    }
+    return super.adjustMapAddress(addr);
+  }
+
+  private static final CodeSet CALL_LIST = new CodeSet(
+      "BRUSH,FIELD,GRASS,WOODS FIRE",
+      "CHIMNEY FIRE",
+      "COMMERCIAL FIRE",
+      "ELECTRICAL FIRE",
+      "EXPLOSION",      
+      "RESIDENTIAL-FIRE",
+      "VEHICLE FIRE"
+  );
+
+}
---
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/GA/GAFanninCountyBParser.java
@@ -0,0 +1,72 @@
+package net.anei.cadpage.parsers.GA;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import net.anei.cadpage.parsers.FieldProgramParser;
+import net.anei.cadpage.parsers.MsgInfo.Data;
+
+public class GAFanninCountyBParser extends FieldProgramParser {
+  
+  public GAFanninCountyBParser() {
+    super("FANNIN COUNTY", "GA", 
+          "ID CALL NAME ADDRCITY! INFO/N+");
+  }
+  
+  @Override
+  public String getFilter() {
+    return "fannindispatch@fannincountyga.org";
+  }
+  
+  @Override
+  protected boolean parseMsg(String subject, String body, Data data) {
+    if (!subject.equals("CAD CALL")) return false;
+    return parseFields(body.split(" : "), data);
+  }
+  
+  @Override
+  public Field getField(String name) {
+    if (name.equals("ID")) return new IdField("CFS\\d{7}", true);
+    if (name.equals("NAME")) return new MyNameField();
+    if (name.equals("ADDRCITY")) return new MyAddressCityField();
+    if (name.equals("INFO")) return new MyInfoField();
+    return super.getField(name);
+  }
+  
+  private class MyNameField extends NameField {
+    @Override
+    public void parse(String field, Data data) {
+      if (field.equals("None")) return;
+      super.parse(field, data);
+    }
+  }
+  
+  private static final Pattern ADDR_CITY_ST_PTN = Pattern.compile("(.*), +([A-Z]{2})(?: +(\\d{5}))?");
+  private class MyAddressCityField extends AddressCityField {
+    @Override
+    public void parse(String field, Data data) {
+      String zip = null;
+      Matcher match = ADDR_CITY_ST_PTN.matcher(field);
+      if (match.matches()) {
+        field = match.group(1).trim();
+        data.strState = match.group(2);
+        zip = match.group(3);
+      }
+      super.parse(field, data);
+      if (data.strCity.length() == 0 && zip != null) data.strCity = zip;
+    }
+    
+    @Override
+    public String getFieldNames() {
+      return super.getFieldNames() + " ST";
+    }
+  }
+  
+  private class MyInfoField extends InfoField {
+    @Override
+    public void parse(String field, Data data) {
+      if (field.equals("None")) return;
+      super.parse(field, data);
+    }
+  }
+}
