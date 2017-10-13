5e1a9272444b93869d39c8a539ed32d6b0b51afa
Ken Corbin <ken@cadpage.org>
2017-10-12 21:15:18 -0700

    Fixed parsing problem with Bergen County, NJ (C)

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/NJ/NJBergenCountyCParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/NJ/NJBergenCountyCParser.java
@@ -2,6 +2,8 @@ package net.anei.cadpage.parsers.NJ;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import net.anei.cadpage.parsers.FieldProgramParser;
 import net.anei.cadpage.parsers.MsgInfo.Data;
@@ -30,9 +32,26 @@ public class NJBergenCountyCParser extends FieldProgramParser {
   public Field getField(String name) {
     if (name.equals("ID")) return new IdField("\\d{2}-\\d{5}", true);
     if (name.equals("DATE")) return new DateField("\\d\\d?/\\d\\d?/\\d{4}", true);
-    if (name.equals("TIME")) return new TimeField(TIME_FMT, true);
+    if (name.equals("TIME")) return new MyTimeField();
     if (name.equals("CITY")) return new MyCityField();
     return super.getField(name);
+  }
+  
+  private static final Pattern SHORT_TIME_PTN = Pattern.compile("(\\d\\d?:\\d\\d) ([AP]M)");
+  private class MyTimeField extends TimeField {
+    public MyTimeField() {
+      super(TIME_FMT, true);
+    }
+    
+    @Override
+    public void parse(String field, Data data) {
+      Matcher match = SHORT_TIME_PTN.matcher(field);
+      if (match.matches()) {
+        field = match.group(1)+":00 " + match.group(2);
+      }
+      super.parse(field, data);
+    }
+    
   }
 
   private class MyCityField extends CityField {
