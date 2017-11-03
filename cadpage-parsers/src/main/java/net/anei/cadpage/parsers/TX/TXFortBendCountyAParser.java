12120162d91d268de2d084f71265713500334029
Ken Corbin <ken@cadpage.org>
2017-11-02 22:02:50 -0700

    Fixed parsing problem with Fort Bend COunty, TX (A)

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/TX/TXFortBendCountyAParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/TX/TXFortBendCountyAParser.java
@@ -1,10 +1,21 @@
 package net.anei.cadpage.parsers.TX;
 
+import net.anei.cadpage.parsers.MsgInfo.Data;
 import net.anei.cadpage.parsers.dispatch.DispatchA41Parser;
 
 public class TXFortBendCountyAParser extends DispatchA41Parser {
 
   public TXFortBendCountyAParser() {
-    super(TXFortBendCountyParser.CITY_CODES, "FORT BEND COUNTY", "TX", "[A-Z]{2,3}");
+    super(TXFortBendCountyParser.CITY_CODES, "FORT BEND COUNTY", "TX", "[A-Z]{2,3}\\d{0,2}");
   }
+
+  @Override
+  protected boolean parseMsg(String body, Data data) {
+    if (body.startsWith("CALLALERT:")) {
+      body = "DISPATCH:" + body.substring(10);
+    }
+    return super.parseMsg(body, data);
+  }
+  
+  
 }
