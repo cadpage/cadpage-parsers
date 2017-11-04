4bda644854f38ebd16062f02136ba54f7e761380
Ken Corbin <ken@cadpage.org>
2017-11-03 19:54:26 -0700

    Fixed parsing problem with Monroe County, KY

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/KY/KYMonroeCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/KY/KYMonroeCountyParser.java
@@ -1,5 +1,7 @@
 package net.anei.cadpage.parsers.KY;
 
+import net.anei.cadpage.parsers.SplitMsgOptions;
+import net.anei.cadpage.parsers.SplitMsgOptionsCustom;
 import net.anei.cadpage.parsers.dispatch.DispatchA65Parser;
 
 public class KYMonroeCountyParser extends DispatchA65Parser {
@@ -9,8 +11,16 @@ public class KYMonroeCountyParser extends DispatchA65Parser {
   }
   
   @Override
+  public SplitMsgOptions getActive911SplitMsgOptions() {
+    return new SplitMsgOptionsCustom(){
+      @Override public boolean splitKeepLeadBreak() { return true; }
+      @Override public boolean splitBreakIns() { return true; }
+    };
+  }
+
+  @Override
   public String getFilter() {
-    return "monroecoky@911email.net";
+    return "dispatch@911comm1.info,geoconex@nlamerica.com";
   }
   
   private static final String[] CITY_LIST = new String[]{
@@ -18,8 +28,11 @@ public class KYMonroeCountyParser extends DispatchA65Parser {
       "FOUNTAIN RUN",
       "GAMALIEL",
       "TOMPKINSVILLE",
+      
       "BUGTUSSLE",
-      "MOUNT HERMON"
+      "MOUNT HERMON",
+      
+      "HESTAND"
 
   };
 }
