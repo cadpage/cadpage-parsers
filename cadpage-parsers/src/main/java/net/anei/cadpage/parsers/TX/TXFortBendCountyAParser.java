97f0c308694c7bbe417ad4cd0520589afa6ffd71
Ken Corbin <ken@cadpage.org>
2017-11-05 09:02:37 -0800

    Fixed parsing problem with fort Bend, TX (A)

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/TX/TXFortBendCountyAParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/TX/TXFortBendCountyAParser.java
@@ -6,7 +6,7 @@ import net.anei.cadpage.parsers.dispatch.DispatchA41Parser;
 public class TXFortBendCountyAParser extends DispatchA41Parser {
 
   public TXFortBendCountyAParser() {
-    super(TXFortBendCountyParser.CITY_CODES, "FORT BEND COUNTY", "TX", "[A-Z]{2,3}\\d{0,2}");
+    super(TXFortBendCountyParser.CITY_CODES, "FORT BEND COUNTY", "TX", "[A-Z]{2,3}\\d?|EOUT");
   }
 
   @Override
