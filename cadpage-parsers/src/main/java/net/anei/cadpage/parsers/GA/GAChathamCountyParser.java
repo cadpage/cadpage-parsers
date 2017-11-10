1b49c285d555ebd2825080e0ab62c50b4eab21b7
Ken Corbin <ken@cadpage.org>
2017-11-09 16:34:00 -0800

    Fixed parsing problemwith Chatham County, GA

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/GA/GAChathamCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/GA/GAChathamCountyParser.java
@@ -19,20 +19,22 @@ public class GAChathamCountyParser extends FieldProgramParser {
 
   private static final Pattern CRN_PATTERN
     = Pattern.compile("^((?:YOUR +)?CRN\\s*\\&\\s*TIMES(?: +ARE)?|"
++                         "COMPLETE/REFUSAL FOR|"
 +                         "COMPLETED +CALL\\s*\\/\\s*REFUSAL TIMES|"
-+                         "COMPLETE\\s*\\/\\s*REFUSAL)\\:\\s*CRN\\:\\s*(\\d{4}(?:\\d|[A-Z])\\d{4}) +(.*)");
-  private static final Pattern TIMES_BRK_PTN = Pattern.compile(" +(?=(?:RECEIVED|DISPATCHED|ENROUTE|AT SCENE|DEPART|AT DEST|MILEAGE):)");
++                         "COMPLETE\\s*\\/\\s*REFUSAL)\\:(?:ADDRESS:)?([^:]*?)\\s*CRN\\:\\s*(\\d{4}(?:\\d|[A-Z])\\d{4}) +(.*)");
+  private static final Pattern TIMES_BRK_PTN = Pattern.compile("(?: +|(?<! |^))(?=(?:RECEIVED|DISPATCHED|ENROUTE|AT SCENE|DEPART|AT DEST|MILEAGE|CLEAR/CANC):)");
   private static final Pattern APT_PATTERN = Pattern.compile("(.*?)APT\\#?\\:?(.*)");
   
   @Override
   protected boolean parseMsg(String body, Data data) {
     Matcher m = CRN_PATTERN.matcher(body);
     if (m.matches()) {
-      setFieldList("CALL ID INFO");
+      setFieldList("CALL ADDR APT ID INFO");
       data.msgType = MsgType.RUN_REPORT;
       data.strCall = m.group(1);
-      data.strCallId = m.group(2);
-      data.strSupp = TIMES_BRK_PTN.matcher(m.group(3).trim()).replaceAll("\n");
+      parseAddress(m.group(2).trim(), data);
+      data.strCallId = m.group(3);
+      data.strSupp = TIMES_BRK_PTN.matcher(m.group(4).trim()).replaceAll("\n");
       return true;
     }
     m = APT_PATTERN.matcher(body);
