1cc123ac4a5a59da49b00af39ab7adeb3d4614d8
Ken Corbin <ken@cadpage.org>
2017-12-12 22:24:19 -0800

    Added Columbiana County, OH (B)

---
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/OH/OHColumbianaCountyBParser.java
@@ -0,0 +1,8 @@
+package net.anei.cadpage.parsers.OH;
+
+public class OHColumbianaCountyBParser extends OHMahoningCountyAParser {
+  
+  public OHColumbianaCountyBParser() {
+    super("COLUMBIANA COUNTY", "OH");
+  }
+}
