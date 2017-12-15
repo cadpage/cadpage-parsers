d264fcd47154d9f6ec68f6dc2c78cf2ce69c20b0
Ken Corbin <ken@cadpage.org>
2017-12-14 19:15:04 -0800

    Fixed parsing problem with Cass County, ND

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/ND/NDCassCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/ND/NDCassCountyParser.java
@@ -10,14 +10,10 @@ import net.anei.cadpage.parsers.MsgInfo.Data;
 
 
 public class NDCassCountyParser extends SmartAddressParser {
-  
-  private static final Pattern DATE_TIME_CFS_PTN = Pattern.compile("(?:\\* )?(\\d\\d/\\d\\d/\\d\\d) (\\d\\d:\\d\\d) CFS #:? (\\d+) ");
-  private static final Pattern CALL_PFX_PTN = Pattern.compile("X - |X-SEND FIRE ", Pattern.CASE_INSENSITIVE);
-  private static final Pattern UNIT_PTN = Pattern.compile("(?: \\d{3}| \\d{4}-[A-Z]+)+$");
  
   public NDCassCountyParser() {
     super(CITY_CODES, "CASS COUNTY", "ND");
-    setFieldList("CALL ADDR APT CITY ST PLACE DATE TIME ID INFO UNIT GPS");
+    setFieldList("CALL ADDR APT CITY ST PLACE X DATE TIME ID INFO UNIT GPS MAP");
     setupCallList(CALL_LIST);
     removeWords("ESTATES");
     setupSaintNames("FRANCIS");
@@ -30,7 +26,7 @@ public class NDCassCountyParser extends SmartAddressParser {
   
   @Override
   public String getFilter() {
-    return "dispatch@rrrdc.or,dispatch@cityoffargo.com";
+    return "dispatch@rrrdc.or,dispatch@cityoffargo.com,dispatch@fargond.gov";
   }
   
   @Override
@@ -38,16 +34,24 @@ public class NDCassCountyParser extends SmartAddressParser {
     return MAP_FLG_PREFER_GPS;
   }
   
+  private static final Pattern DATE_TIME_CFS_PTN = Pattern.compile("(?:\\* +)?(?:(\\d\\d/\\d\\d/\\d\\d) (\\d\\d:\\d\\d) )?CFS #:? (\\d+) ");
+  private static final Pattern CALL_PFX_PTN = Pattern.compile("X - |X-SEND FIRE ", Pattern.CASE_INSENSITIVE);
+  private static final Pattern CITY_PTN = Pattern.compile(", ([A-Z]{3,4})\\b");
+  private static final Pattern UNIT_PTN = Pattern.compile("(?: \\d{3}| \\d{4}-[A-Z]+)+$");
+  
   @Override
   protected boolean parseMsg(String body, Data data) {
     
+    body = body.replace('\n', ' ');
     Matcher match = DATE_TIME_CFS_PTN.matcher(body);
     if (!match.find()) return false;
-    data.strDate = match.group(1);
-    data.strTime = match.group(2);
+    data.strDate = getOptGroup(match.group(1));
+    data.strTime = getOptGroup(match.group(2));
     data.strCallId = match.group(3);
     String sAddr = body.substring(0,match.start()).trim();
     String sInfo = body.substring(match.end()).trim();
+    
+    int version =  data.strDate.length() > 0 ? 1 : 2;
     
     String prefix = null;
     match = CALL_PFX_PTN.matcher(sAddr);
@@ -67,8 +71,19 @@ public class NDCassCountyParser extends SmartAddressParser {
       st = StartType.START_ADDR;
       flags = 0;
     }
+    
+    String trail = null;
+    match = CITY_PTN.matcher(sAddr);
+    if (match.find()) {
+      data.strCity = convertCodes(match.group(1), CITY_CODES);
+      trail = sAddr.substring(match.end()).trim();
+      sAddr = sAddr.substring(0,match.start()).trim();
+      flags |= FLAG_ANCHOR_END | FLAG_NO_CITY;
+    }
+    
     sAddr = sAddr.replace("\\", "&");
     flags |= FLAG_EMPTY_ADDR_OK | FLAG_IMPLIED_INTERSECT;
+    if (version >= 2) flags |= FLAG_CROSS_FOLLOWS;
     parseAddress(st, flags, sAddr, data);
     if (prefix != null) data.strCall = prefix + data.strCall;
     
@@ -78,22 +93,32 @@ public class NDCassCountyParser extends SmartAddressParser {
       if (data.strState.equals("ND")) data.strState = "";
       data.strCity = data.strCity.substring(0,pt).trim();
     }
-    
-    String sPlace = getLeft();
-    sPlace = stripFieldStart(sPlace, "*");
-    pt = sPlace.indexOf(" - ");
-    if (pt >= 0) sPlace = sPlace.substring(0,pt).trim();
-    if (sPlace.length() <= 1) sPlace = "";
+
+    if (trail == null) trail = getLeft();
+    trail = stripFieldStart(trail, "*");
+    pt = trail.indexOf(" - ");
+    if (pt >= 0) trail = trail.substring(0,pt).trim();
+    if (trail.length() <= 1) trail = "";
     if (data.strAddress.length() == 0) {
-      parseAddress(sPlace, data);
+      parseAddress(trail, data);
+    } else if (version == 2) {
+      if (!trail.equals("No Cross Streets Found")) data.strCross = trail;
     } else {
-      data.strPlace = sPlace;
+      data.strPlace = trail;
     }
     
-    match = UNIT_PTN.matcher(sInfo);
-    if (match.find()) {
-      data.strUnit = match.group().trim();
-      sInfo = sInfo.substring(0,match.start()).trim();
+    if (version == 1) {
+      match = UNIT_PTN.matcher(sInfo);
+      if (match.find()) {
+        data.strUnit = match.group().trim();
+        sInfo = sInfo.substring(0,match.start()).trim();
+      }
+    } else {
+      pt = sInfo.indexOf(" Quad:");
+      if (pt >= 0) {
+        data.strMap = sInfo.substring(pt+6).trim();
+        sInfo = sInfo.substring(0,pt).trim();
+      }
     }
     
     pt = sInfo.indexOf(" GPS:");
@@ -101,10 +126,26 @@ public class NDCassCountyParser extends SmartAddressParser {
       setGPSLoc(sInfo.substring(pt+5), data);
       sInfo = sInfo.substring(0,pt).trim();
     }
+  
+    pt = sInfo.indexOf(" Units:");
+    if (pt >= 0) {
+      data.strUnit = sInfo.substring(pt+7).trim();
+      sInfo =  sInfo.substring(0,pt).trim();
+    }
     
     for (String info : sInfo.split("Nature Of Call:")) {
       info = info.trim();
-      if (info.length() > data.strSupp.length()) data.strSupp = info;
+      pt = info.indexOf("E911 Info -");
+      if (pt >= 0) {
+        info = info.substring(0, pt).trim();
+        info = stripFieldEnd(info, "-");
+      }
+      if (data.strSupp.startsWith(info)) continue;
+      if (info.startsWith(data.strSupp)) {
+        data.strSupp = info;
+      } else {
+        data.strSupp = append(data.strSupp, " - ", info);
+      }
     }
     return true;
   }
@@ -113,8 +154,10 @@ public class NDCassCountyParser extends SmartAddressParser {
       "01 ABDOMINAL PAIN",
       "01 ABDOMINAL PAIN/PROBLEMS",
       "02 ALLERGIES/ENVENOMATIONS",
+      "05 BACK PAIN -  NON TRAUMATIC",
       "06 BREATHING PROBLEMS",
       "07 BURNS/SCALDS",
+      "08 INHALATION",
       "09 CARDIAC/RESPIRATORY ARREST",
       "09 CARDIAC/RESPIRATORY ARREST",
       "10 CHEST PAIN",
@@ -122,17 +165,21 @@ public class NDCassCountyParser extends SmartAddressParser {
       "12 CONVULSIONS/SEIZURE",
       "13 DIABETIC PROBLEMS",
       "17 FALLS",
+      "18 HEADACHE",
       "19 HEART PROBLEMS",
       "20 HEAT - COLD EXPOSURE",
       "21 HEMORRAHAGE - LACERATIONS",
+      "21 HEMORRHAGE - LACERATIONS",
       "23 OVERDOSE - POISONING",
       "24 PREGNANCY - CHILDBIRTH",
       "26 SICK PERSON",
+      "27 STAB/GUNSHOT/PENETRATING",
       "28 STROKE  - CVA",
       "30 TRAUMATIC INJURIES",
       "31 UNCONSCIOUS - FAINTING",
       "32 UNKNOWN PROBLEM - MAN DOWN",
       "33 TRANSFER INTERFACILITY",
+      "911 HANG UP",
       "ACCIDENT - INJURY",
       "ACCIDENT - PROPERTY",
       "AIRCRAFT CRASH",
@@ -143,8 +190,10 @@ public class NDCassCountyParser extends SmartAddressParser {
       "CARBON MONOXIDE DETECTOR",
       "COMMERCIAL FIRE",
       "COMMERCIAL FIRE ALARM RESET ONLY",
+      "DISTURBANCE",
       "DOMESTIC",
       "DUMPSTER FIRE",
+      "FIRE ALARM ACTIVATION",
       "GAS/FUEL SPILLS",
       "GAS LEAK",
       "GRASS FIRE",
@@ -161,12 +210,15 @@ public class NDCassCountyParser extends SmartAddressParser {
       "MUTUAL AID FIRE",
       "RESCUE",
       "RESIDENTIAL FIRE",
+      "SU",
       "SUICIDAL PERSON",
+      "STRUCTURE FIRE",
       "TEST",
       "TS",
       "VEHICLE FIRE",
       "WATER BREAK/WASHED OUT ROAD",
-      "WATER BREAK/SEWER/WASH OUT ROAD"
+      "WATER BREAK/SEWER/WASH OUT ROAD",
+      "WATER RESCUE"
   );
   
   private static final Properties CITY_CODES = buildCodeTable(new String[]{
