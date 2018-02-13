2eca5a8da9d7fbb9b089e71e961965da93618fca
Ken Corbin <ken@cadpage.org>
2018-02-12 17:25:14 -0800

    Fixed parsing problem with Blount County, AL

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/AL/ALBlountCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/AL/ALBlountCountyParser.java
@@ -1,5 +1,6 @@
 package net.anei.cadpage.parsers.AL;
 
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import net.anei.cadpage.parsers.FieldProgramParser;
@@ -11,7 +12,7 @@ public class ALBlountCountyParser extends FieldProgramParser {
   
   public ALBlountCountyParser() {
     super("BLOUNT COUNTY", "AL", 
-          "CALL:CALL! ADDR:ADDR! CITY:CITY! XY:GPS? ID:ID! PRI:PRI? DATE:DATE! TIME:TIME! UNIT:UNIT INFO:X");
+          "CALL:CALL! ADDR:ADDR! CITY:CITY! XY:GPS? ID:ID! PRI:PRI? DATE:DATE! TIME:TIME! UNIT:UNIT ( X:X INFO:INFO | INFO:XINFO )");
   }
   
   @Override
@@ -37,6 +38,7 @@ public class ALBlountCountyParser extends FieldProgramParser {
     if (name.equals("X")) return new MyCrossField();
     if (name.equals("DATE")) return new DateField("\\d\\d?/\\d\\d?/\\d{4}", true);
     if (name.equals("TIME")) return new TimeField("\\d\\d?:\\d\\d:\\d\\d", true);
+    if (name.equals("XINFO")) return new MyCrossInfoField();
     return super.getField(name);
   }
   
@@ -45,6 +47,24 @@ public class ALBlountCountyParser extends FieldProgramParser {
     public void parse(String field, Data data) {
       field = field.replace('@',  '/').replace("*", "");
       super.parse(field, data);
+    }
+  }
+  
+  private static final Pattern CROSS_INFO_PTN = Pattern.compile("([^a-z]*?)(?:^(?=\\d+ *[A-Z]?[a-z])| (?=[A-Z][a-z])|(?<=[A-Z])(?=[1-9])|(?=[a-z]))(.*)");
+  private class MyCrossInfoField extends MyCrossField {
+    @Override
+    public void parse(String field, Data data) {
+      Matcher match = CROSS_INFO_PTN.matcher(field);
+      if (match.matches()) {
+        field = match.group(1).trim();
+        data.strSupp = match.group(2);
+      }
+      super.parse(field, data);
+    }
+    
+    @Override
+    public String getFieldNames() {
+      return "X INFO";
     }
   }
 }
