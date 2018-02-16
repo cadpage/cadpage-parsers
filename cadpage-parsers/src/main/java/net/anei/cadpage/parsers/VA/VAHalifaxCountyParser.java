570cdbee575afa345fe397fdd017e96e166b369c
Ken Corbin <ken@cadpage.org>
2018-02-16 15:36:53 -0800

    Cleanup Halifax County, VA

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/VA/VAHalifaxCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/VA/VAHalifaxCountyParser.java
@@ -1,5 +1,7 @@
 package net.anei.cadpage.parsers.VA;
 
+import java.util.regex.Pattern;
+
 import net.anei.cadpage.parsers.MsgInfo.Data;
 import net.anei.cadpage.parsers.dispatch.DispatchA47Parser;
 
@@ -14,16 +16,24 @@ public class VAHalifaxCountyParser extends DispatchA47Parser {
     return "halifaxeoc@co.halifax.va.us";
   }
   
+  private static final Pattern INFO_JUNK_PTN = Pattern.compile("(?<=^|\n)ENDCE\nCMDS4= *");
+  
   @Override
   public boolean parseMsg(String subject, String body, Data data) {
     body = body.replace("\n\n", "\n");
     if (!super.parseMsg(subject, body, data)) return false;
     if (data.strCity.toUpperCase().startsWith("TURBEVILLE")) data.strCity = data.strCity.substring(0,10);
+    data.strSupp = INFO_JUNK_PTN.matcher(data.strSupp).replaceFirst("");
     if (data.strPlace.startsWith("911")) {
       data.strSupp = append(data.strPlace, "\n", data.strSupp);
       data.strPlace = "";
     }
     return true;
+  }
+  
+  @Override
+  public String getProgram() {
+    return super.getProgram() + " INFO";
   }
   
   @Override
