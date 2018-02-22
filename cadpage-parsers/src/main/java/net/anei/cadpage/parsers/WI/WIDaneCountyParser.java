ddbfd6de3968ed2fc79728845ed3a39eb6ccafe7
Ken Corbin <ken@cadpage.org>
2018-02-21 16:26:05 -0800

    Fixed parsing problem with Dane County, WI

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/WI/WIDaneCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/WI/WIDaneCountyParser.java
@@ -4,16 +4,22 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import net.anei.cadpage.parsers.MsgInfo.Data;
-import net.anei.cadpage.parsers.SmartAddressParser;
+import net.anei.cadpage.parsers.FieldProgramParser;
 
-public class WIDaneCountyParser extends SmartAddressParser {
+public class WIDaneCountyParser extends FieldProgramParser {
 
   public WIDaneCountyParser() {
-    super("DANE", "WI");
-    setFieldList("CITY PLACE ADDR X PRI CODE CALL UNIT");
+    super("DANE", "WI", 
+          "UNIT! Add:ADDR! Loc:PLACE! Zip:CITY! Nature:CODE_CALL_ID! Lat:GPS1! Long:GPS2! Xst:X! PDC:SKIP! Units:SKIP! END");
   }
   
-  private static Pattern MASTER = Pattern.compile("Response Info Page: Municipality [A-Z]+-(.*?) (?:City|Town|Village)(?: (.*))? At +(.*?) +cross of(?: +(.*?))? +a +(.*?) +response for[ \\.]+(?:(\\d\\w+)-)?(.*?) *\\. Units assigned to alarm: +(.*)");
+  @Override
+  public String getFilter() {
+    return "tritech@countyofdane.com";
+  }
+  
+  private static Pattern MASTER = Pattern.compile("Response Info Page: Municipality (?:[A-Z]+-(.*?) (?:City|Town|Village) )?(?:(.*) )?At +(.*?) +cross of(?: +(.*?))? +a +(.*?) +response for[ \\.]+(?:(\\d\\w+)-)?(.*?) *\\. Units assigned to alarm: +(.*)");
+  
   @Override
   public boolean parseMsg(String subject, String body, Data data) {
     //check subject
@@ -21,25 +27,71 @@ public class WIDaneCountyParser extends SmartAddressParser {
     
     //parse in one swoop
     Matcher mat = MASTER.matcher(body);
-    if (!mat.matches()) return false;
+    if (mat.matches()) {
+      
+      setFieldList("CITY PLACE ADDR APT X PRI CODE CALL UNIT");
+      
+      data.strCity = getOptGroup(mat.group(1));
+      data.strPlace = getOptGroup(mat.group(2));
+      parseAddress(mat.group(3).trim(), data);
+      data.strCross = getOptGroup(mat.group(4));
+      data.strPriority = mat.group(5);
+      data.strCode = getOptGroup(mat.group(6));
+      data.strCall = mat.group(7);
+      data.strUnit = mat.group(8);
+      
+      //CLean up cross street info
+      data.strCross = stripFieldStart(data.strCross, "NO X STREET");
+      data.strCross = stripFieldStart(data.strCross, "/");
+      data.strCross = stripFieldEnd(data.strCross, "NO X STREET");
+      data.strCross = stripFieldEnd(data.strCross, "/");
+      
+      return true;
+    }
     
-    //i think i need to change le pattern or trim all of these
-    data.strCity = mat.group(1).trim();
-    data.strPlace = getOptGroup(mat.group(2));
-    parseAddress(mat.group(3).trim(), data);
-    data.strCross = getOptGroup(mat.group(4));
-    data.strPriority = mat.group(5);
-    data.strCode = getOptGroup(mat.group(6));
-    data.strCall = mat.group(7);
-    data.strUnit = mat.group(8);
+    body = body.replace("Add:", " Add:");
+    return super.parseMsg(body, data);
+  }
+  
+  @Override
+  public Field getField(String name) {
+    if (name.equals("CODE_CALL_ID")) return new MyCodeCallIdField();
+    if (name.equals("GPS1")) return new MyGPSField(1);
+    if (name.equals("GPS2")) return new MyGPSField(2);
+      
+    return super.getField(name);
+  }
+  
+  private static final Pattern CODE_CALL_ID_PTN = Pattern.compile("(?:(\\d{1,2}[A-Z]\\d{1,2}[A-Z]?)-|\\.)?(.*?)Inc# *(\\d{2}-\\d+)");
+  private class MyCodeCallIdField extends Field {
+    @Override
+    public void parse(String field, Data data) {
+      Matcher match = CODE_CALL_ID_PTN.matcher(field);
+      if (!match.matches()) abort();
+      data.strCode = getOptGroup(match.group(1));
+      data.strCall = match.group(2).trim();
+      data.strCallId = match.group(3);
+    }
+
+    @Override
+    public String getFieldNames() {
+      return "CODE CALL ID";
+    }
+  }
+  
+  private static final Pattern GPS_PTN = Pattern.compile("(\\d{2,})(\\d{6})");
+  private class MyGPSField extends GPSField {
     
-    //CLean up cross street info
-    data.strCross = stripFieldStart(data.strCross, "NO X STREET");
-    data.strCross = stripFieldStart(data.strCross, "/");
-    data.strCross = stripFieldEnd(data.strCross, "NO X STREET");
-    data.strCross = stripFieldEnd(data.strCross, "/");
+    public MyGPSField(int type) {
+      super(type);
+    }
     
-    return true;
+    @Override
+    public void parse(String field, Data data) {
+      Matcher match = GPS_PTN.matcher(field);
+      if (!match.matches()) return;
+      super.parse(match.group(1)+'.'+match.group(2), data);
+    }
   }
   
   @Override
