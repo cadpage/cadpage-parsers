9787d66ae6ae304a2642bc163f399c44c7b3e379
Ken Corbin <ken@cadpage.org>
2018-04-21 16:54:16 -0700

    Fixed parsing problem with Brunswick County, NC (Add B)

---
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/NC/NCBrunswickCountyBParser.java
@@ -0,0 +1,47 @@
+package net.anei.cadpage.parsers.NC;
+
+import net.anei.cadpage.parsers.FieldProgramParser;
+import net.anei.cadpage.parsers.MsgInfo.Data;
+
+public class NCBrunswickCountyBParser extends FieldProgramParser {
+  
+  public NCBrunswickCountyBParser() {
+    super("BRUNSWICK COUNTY", "NC", 
+          "CALL:CALL! ADDR:ADDR! CITY:CITY! XY:GPS? ID:ID! PRI:PRI? DATE:DATE! TIME:TIME! UNIT:UNIT! X:X? INFO:INFO/N+");
+  }
+  
+  @Override
+  public int getMapFlags() {
+    return MAP_FLG_PREFER_GPS;
+  }
+  
+  @Override
+  protected boolean parseMsg(String body, Data data) {
+    return parseFields(body.split("\n"), data);
+  }
+  
+  @Override
+  public Field getField(String name) {
+    if (name.equals("DATE")) return new DateField("\\d\\d/\\d\\d/\\d{4}", true);
+    if (name.equals("TIME")) return new TimeField("\\d\\d:\\d\\d:\\d\\d", true);
+    if (name.equals("X")) return new MyCrossField();
+    if (name.equals("UNIT")) return new MyUnitField();
+    return super.getField(name);
+  }
+  
+  private class MyCrossField extends CrossField {
+    @Override
+    public void parse(String field, Data data) {
+      field = field.replace('@',  '/');
+      super.parse(field, data);
+    }
+  }
+  
+  private class MyUnitField extends UnitField {
+    @Override
+    public void parse(String field, Data data) {
+      field = stripFieldEnd(field, ",");
+      super.parse(field, data);
+    }
+  }
+}
