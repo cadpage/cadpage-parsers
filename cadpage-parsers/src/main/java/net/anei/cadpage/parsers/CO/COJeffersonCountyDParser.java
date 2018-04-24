89eafb7d53da8fdefaa56f8f5fa0cffc6d6aa26a
Ken Corbin <ken@cadpage.org>
2018-04-23 20:41:52 -0700

    Fixed parsing problem with Jefferson County, CO (D)

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/CO/COJeffersonCountyDParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/CO/COJeffersonCountyDParser.java
@@ -10,7 +10,7 @@ public class COJeffersonCountyDParser extends FieldProgramParser {
   
   public COJeffersonCountyDParser() {
     super("JEFFERSON COUNTY", "CO", 
-          "CALL ADDR APT CITY X MAP GPS1/d GPS2/d UNIT EMPTY ID TIME! END");
+          "CALL ADDR APT CITY X MAP GPS1/d GPS2/d UNIT UNIT/C+? CH ID TIME! END");
   }
   
   @Override
@@ -31,7 +31,9 @@ public class COJeffersonCountyDParser extends FieldProgramParser {
     
     int pt = body.indexOf("\n\n");
     if (pt >= 0) body = body.substring(0,pt).trim();
-    if (!parseFields(DELIM.split(body), data)) return false;
+    String[] flds = body.split("\n");
+    if (flds.length < 12) flds = DELIM.split(body);
+    if (!parseFields(flds, data)) return false;
     if (data.strCity.equals("UNINC JEFFERSON")) data.strCity = "JEFFERSON COUNTY";
     return true;
   }
