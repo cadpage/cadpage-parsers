1dc8fc8e964bff9e83f0ca0cbe3626cbbb39bd36
Ken Corbin <ken@cadpage.org>
2018-05-05 15:00:35 -0700

    Release v1.9.15-43

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/IA/IAWapelloCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/IA/IAWapelloCountyParser.java
@@ -1,5 +1,6 @@
 package net.anei.cadpage.parsers.IA;
 
+import net.anei.cadpage.parsers.MsgInfo;
 import net.anei.cadpage.parsers.dispatch.DispatchA27Parser;
 
 /**
@@ -15,5 +16,12 @@ public class IAWapelloCountyParser extends DispatchA27Parser {
   @Override
   public String getFilter() {
     return "alerts@ci.ottumwa.ia.us";
+  }
+
+  @Override
+  public boolean parseMsg(String subject, String body, MsgInfo.Data data) {
+    int pt = body.indexOf("\n\nThis message and");
+    if (pt >= 0) body = body.substring(0,pt).trim();
+    return super.parseMsg(subject, body, data);
   }
 }
