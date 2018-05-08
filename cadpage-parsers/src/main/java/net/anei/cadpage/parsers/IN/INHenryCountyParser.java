8dcae23485a5fb88c0c7df4c3ad3e5d0818d557e
Ken Corbin <ken@cadpage.org>
2018-05-07 19:59:21 -0700

    Parsing problem with Henry County, IN

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/IN/INHenryCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/IN/INHenryCountyParser.java
@@ -11,7 +11,7 @@ import net.anei.cadpage.parsers.MsgParser;
  */
 public class INHenryCountyParser extends MsgParser {
   
-  private static final Pattern MASTER1 = Pattern.compile("([- A-Z0-9]+)\\|([^:\n]+):([^\\|\n]*)\\|(.*)");
+  private static final Pattern MASTER1 = Pattern.compile("([- A-Z0-9]+)\\|([^:\n]+):([^\\|\n]*)(?:\\|(.*))?");
   private static final Pattern MASTER2 = Pattern.compile("([- A-Z0-9]+)@([^\\*\n]+)\\*([^\\|\n]*)\\|(.*)");
   
   public INHenryCountyParser() {
@@ -21,13 +21,14 @@ public class INHenryCountyParser extends MsgParser {
   
   @Override
   public String getFilter() {
-    return "hcradio@emgsvcs.net";
+    return "hcradio@emgsvcs.net,ncemsriprun@emgsvcs.net";
   }
   
   @Override
   public boolean parseMsg(String subject, String body, Data data) {
-    if (!subject.startsWith("DISPATCH:")) return false;
-    data.strSource = subject.substring(9).trim();
+    if (subject.startsWith("DISPATCH:")) {
+      data.strSource = subject.substring(9).trim();
+    }
     
     int pt = body.indexOf('\n');
     if (pt >= 0) body = body.substring(0,pt).trim();
@@ -38,7 +39,7 @@ public class INHenryCountyParser extends MsgParser {
     data.strCall = match.group(1).trim();
     parseAddress(match.group(2).trim(), data);
     data.strCity = match.group(3).trim();
-    String call = match.group(4).trim();
+    String call = getOptGroup(match.group(4));
     if (call.length() == 0) data.strCall = "ALERT";
     else if (call.length() <= 40) data.strCall = call;
     else data.strSupp = call;
