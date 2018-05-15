189d197bd653b43c65ce0ece30f39416266b56f5
Ken Corbin <ken@cadpage.org>
2018-05-15 10:51:44 -0700

    Fixed parsing problem with Buffalo, NY

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/NY/NYBuffaloParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/NY/NYBuffaloParser.java
@@ -10,8 +10,8 @@ import net.anei.cadpage.parsers.dispatch.DispatchProQAParser;
 public class NYBuffaloParser extends DispatchProQAParser {
   
   public NYBuffaloParser() {
-    super("BUFFALO", "NY",
-           "PRI CODE_CALL CALL/L+? ADDR/Z CITY ALT_ID/Z TIME! INFO+");
+    super(CITY_LIST, "BUFFALO", "NY",
+          "( PRI | JOB ) CODE_CALL CALL2/L+? ADDR APT APT/L+? CITY ALT_ID INFO/N+? TIME! INFO/N+", true);
   }
   
   @Override
@@ -19,10 +19,29 @@ public class NYBuffaloParser extends DispatchProQAParser {
     return "2002000004,777";
   }
   
+  private static final Pattern PREFIX_PTN = Pattern.compile("([A-Z0-9]+): +(?=RC:)");
+  
+  @Override
+  protected boolean parseMsg(String body, Data data) {
+    Matcher match = PREFIX_PTN.matcher(body);
+    if (match.lookingAt()) {
+      data.strSource = match.group(1);
+      body = body.substring(match.end());
+    }
+    return super.parseMsg(body, data);
+  }
+  
+  @Override
+  public String getProgram() {
+    return "SRC " + super.getProgram();
+  }
+
   @Override
   public Field getField(String name) {
-    if (name.equals("CODE_CALL")) return new MyCodeCallField();
     if (name.equals("PRI")) return new PriorityField("(\\d) .*", true);
+    if (name.equals("JOB")) return new SkipField("\\d{4}-[A-Z]");
+    if (name.equals("CODE_CALL")) return new MyCodeCallField();
+    if (name.equals("CALL2")) return new CallField(".*[a-z].*|GSW|", true);
     if (name.equals("ALT_ID")) return new SkipField("\\d+", true);
     if (name.equals("TIME")) return new TimeField("\\d\\d:\\d\\d", true);
     return super.getField(name);
@@ -45,4 +64,10 @@ public class NYBuffaloParser extends DispatchProQAParser {
       return "CODE CALL";
     }
   }
+  
+  private static final String[] CITY_LIST = new String[]{
+      "BUFFALO",
+      "HAMBURG",
+      "NIAGARA FALLS"
+  };
 }
