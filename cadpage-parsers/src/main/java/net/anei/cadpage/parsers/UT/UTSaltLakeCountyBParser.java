29d4362989c8a2b3dfaf913ae6dd0a8bd7df29c1
Ken Corbin <ken@cadpage.org>
2018-05-18 13:27:39 -0700

    Fixed parsing problem with Salt Lake County, UT (B)

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/UT/UTSaltLakeCountyBParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/UT/UTSaltLakeCountyBParser.java
@@ -6,7 +6,6 @@ import java.util.regex.Pattern;
 
 import net.anei.cadpage.parsers.SmartAddressParser;
 import net.anei.cadpage.parsers.StandardCodeTable;
-import net.anei.cadpage.parsers.CodeSet;
 import net.anei.cadpage.parsers.CodeTable;
 import net.anei.cadpage.parsers.MsgInfo.Data;
 
@@ -18,12 +17,15 @@ public class UTSaltLakeCountyBParser extends SmartAddressParser {
   
   @Override
   public String getFilter() {
-    return "4702193850";
+    return "4702193850,2083399349";
   }
   
   private static final Pattern MOVE_PTN = Pattern.compile("UNIT#:(\\S+) *, (Move from \\S+ *, To Post: \\S+) *, \\((.*)\\)");
-  private static final Pattern MASTER_PTN = Pattern.compile("UNIT#:(\\S+) RUN#(\\S+) *, LOC:(.*?)APT#:(.*?)BLDG:(.*), ADD LOC:(.*?)CROSS ST:(.*?), PAT.COND(\\S*) *PRIORITY:(.*?), INIT.BY:.*?, ZIP:(.*?) LAT/LON(.*?)");
+  private static final Pattern MASTER_PTN1 = Pattern.compile("UNIT# *(\\S+) RUN # (\\S+?) *LOC:(.*?)APT ?#(.*?)BLDG:(.*?)ADD LOC:(.*?)CROSS ST:(.*?)STYPE: *(\\S*) *PRIORITY: *(.*?) *INIT BY:.*");
+  private static final Pattern MASTER_PTN2 = Pattern.compile("UNIT#:(\\S+) RUN#(\\S+) *, LOC:(.*?)APT#:(.*?)BLDG:(.*), ADD LOC:(.*?)CROSS ST:(.*?), PAT.COND(\\S*) *PRIORITY:(.*?), INIT.BY:.*?, ZIP:(.*?) LAT/LON(.*?)");
   private static final Pattern GPS_PTN = Pattern.compile("([-+]?\\d{2,3})(\\d{6}) ([-+]?\\d{2,3})(\\d{6})");
+  private static final Pattern RUN_REPORT_PTN = Pattern.compile("UNIT#(\\S+) RUN#(\\S+) (RCVD:.*)");
+  private static final Pattern RUN_REPORT_BRK = Pattern.compile("(?<=\\d\\d?:\\d\\d:\\d\\d) ");
 
   @Override
   protected boolean parseMsg(String body, Data data) {
@@ -36,7 +38,24 @@ public class UTSaltLakeCountyBParser extends SmartAddressParser {
       return true;
     }
     
-    if ((match = MASTER_PTN.matcher(body)).matches()) {
+    if ((match = MASTER_PTN1.matcher(body)).matches()) {
+      setFieldList("UNIT ID ADDR CITY APT PLACE INFO X CODE CALL PRI");
+      data.strUnit = match.group(1);
+      data.strCallId = match.group(2);
+      parseAddress(match.group(3).trim(), data);
+      data.strApt = append(data.strApt, "-", match.group(4).trim());
+      data.strPlace = match.group(5).trim();
+      data.strSupp = match.group(6).trim();
+      parseAddress(StartType.START_ADDR, FLAG_ONLY_CROSS | FLAG_IMPLIED_INTERSECT| FLAG_NO_CITY | FLAG_ANCHOR_END, match.group(7).trim(), data);
+      data.strCode = match.group(8);
+      String call = CALL_CODES.getCodeDescription(data.strCode);
+      if (call == null) call = data.strCode;
+      data.strCall = call;
+      data.strPriority = match.group(9).trim();
+      return true;
+    }
+    
+    if ((match = MASTER_PTN2.matcher(body)).matches()) {
       setFieldList("UNIT ID ADDR CITY APT PLACE INFO X CODE CALL PRI GPS");
       data.strUnit = match.group(1);
       data.strCallId = match.group(2);
@@ -59,6 +78,14 @@ public class UTSaltLakeCountyBParser extends SmartAddressParser {
           setGPSLoc(match.group(3)+'.'+match.group(4)+','+match.group(1)+'.'+match.group(2), data);
         }
       }
+      return true;
+    }
+    
+    if ((match = RUN_REPORT_PTN.matcher(body)).matches()) {
+      setFieldList("UNIT ID INFO");
+      data.strUnit = match.group(1);
+      data.strCallId = match.group(2);
+      data.strSupp = RUN_REPORT_BRK.matcher(match.group(3)).replaceAll("\n");
       return true;
     }
     return false;
