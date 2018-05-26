b7ada315ff2a867675fc389836708f3f68408d68
Ken Corbin <ken@cadpage.org>
2018-05-26 11:19:16 -0700

    Fixed parsing problem with Noble County, OH

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/OH/OHNobleCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/OH/OHNobleCountyParser.java
@@ -1,5 +1,6 @@
 package net.anei.cadpage.parsers.OH;
 
+import net.anei.cadpage.parsers.MsgInfo.Data;
 import net.anei.cadpage.parsers.dispatch.DispatchA1Parser;
 
 public class OHNobleCountyParser extends DispatchA1Parser {
@@ -12,4 +13,11 @@ public class OHNobleCountyParser extends DispatchA1Parser {
   public String getFilter() {
     return "@noblecountysheriffsoffice.com";
   }
+
+  @Override
+  protected boolean parseMsg(String subject, String body, Data data) {
+    if (subject.length() == 0 && body.startsWith("ALRM LVL:")) subject = "Alert:";
+    return super.parseMsg(subject, body, data);
+  }
+  
 }
