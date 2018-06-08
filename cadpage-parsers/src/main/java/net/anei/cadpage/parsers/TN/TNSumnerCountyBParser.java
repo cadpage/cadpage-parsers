c7cde4debd86da0884a717f282a1e9c44739df39
Ken Corbin <ken@cadpage.org>
2018-06-07 21:35:38 -0700

    Fixed parsing problem with Sumner County, TN

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/TN/TNSumnerCountyBParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/TN/TNSumnerCountyBParser.java
@@ -4,13 +4,13 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import net.anei.cadpage.parsers.MsgInfo.Data;
-import net.anei.cadpage.parsers.MsgParser;
+import net.anei.cadpage.parsers.SmartAddressParser;
 
-public class TNSumnerCountyBParser extends MsgParser {
+public class TNSumnerCountyBParser extends SmartAddressParser {
   
   public TNSumnerCountyBParser() {
-    super("SUMNER COUNTY", "TN");
-    setFieldList("ADDR CALL");
+    super(CITY_LIST, "SUMNER COUNTY", "TN");
+    setFieldList("ADDR CITY CALL");
   }
   
   @Override
@@ -18,13 +18,13 @@ public class TNSumnerCountyBParser extends MsgParser {
     return "2083399423";
   }
   
-  private static final Pattern MASTER = Pattern.compile("Sumner County ECC: CALL ALERT: (.*?) CALL TYPE: (.*)");
+  private static final Pattern MASTER = Pattern.compile("Sumner County ECC: CALL ALERT: (.*?) CALL TYPE:? (.*)");
   
   @Override
   protected boolean parseMsg(String body, Data data) {
     Matcher match = MASTER.matcher(body);
     if (!match.matches()) return false;
-    parseAddress(match.group(1).trim(), data);
+    parseAddress(StartType.START_ADDR, FLAG_ANCHOR_END, match.group(1).trim(), data);
     data.strCall = match.group(2).trim();
     return true;
   }
@@ -36,4 +36,38 @@ public class TNSumnerCountyBParser extends MsgParser {
     addr = PVT_PTN.matcher(addr).replaceAll(" ").trim();
     return super.adjustMapAddress(addr);
   }
+  
+  private static final String[] CITY_LIST = new String[]{
+
+      // Cities
+      "GALLATIN",
+      "GOODLETTSVILLE",
+      "HENDERSONVILLE",
+      "MILLERSVILLE",
+      "MITCHELLVILLE",
+      "PORTLAND",
+      "WHITE HOUSE",
+
+      // Town
+      "WESTMORELAND",
+
+      // Census-designated places
+      "BETHPAGE",
+      "BRANSFORD",
+      "CASTALIAN SPRINGS",
+      "COTTONTOWN",
+      "FAIRFIELD",
+      "GRABALL",
+      "NEW DEAL",
+      "OAK GROVE",
+      "SHACKLE ISLAND",
+      "WALNUT GROVE",
+
+      // Unincorporated communities
+      "BON AIR",
+      "BRACKENTOWN",
+      "CAIRO",
+      "CORINTH"
+      
+  };
 }
