9c87298c0c51c08a843a52362dbe3b4c268aab8c
Ken Corbin <ken@cadpage.org>
2018-07-11 19:21:28 -0700

    Fixed parsing problem with Addison County, VT (Add C)

---
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/VT/VTAddisonCountyCParser.java
@@ -0,0 +1,8 @@
+package net.anei.cadpage.parsers.VT;
+
+public class VTAddisonCountyCParser extends VTChittendenCountyDParser {
+  
+  public VTAddisonCountyCParser() {
+    super("ADDISON COUNTY");
+  }
+}
