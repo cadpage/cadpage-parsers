e20aff3a868cf1b3e0d7bce35fedb0007ac63311
Ken Corbin <ken@cadpage.org>
2018-07-23 20:57:49 -0700

    New Location: Jackson County, IN (B)

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/IN/INJacksonCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/IN/INJacksonCountyParser.java
@@ -1,38 +1,11 @@
 package net.anei.cadpage.parsers.IN;
 
-import net.anei.cadpage.parsers.SmartAddressParser;
-import net.anei.cadpage.parsers.MsgInfo.Data;
+import net.anei.cadpage.parsers.GroupBestParser;
 
-
-public class INJacksonCountyParser extends SmartAddressParser {
+public class INJacksonCountyParser extends GroupBestParser {
   
   public INJacksonCountyParser() {
-    super("JACKSON COUNTY", "IN");
-    setFieldList("SRC CALL ADDR APT INFO");
+    super(new INJacksonCountyAParser(), new INJacksonCountyBParser());
   }
-  
-  @Override
-  public String getFilter() {
-    return "@injacso.com";
-  }
-  
-  @Override
-  public boolean parseMsg(String subject, String body, Data data) {
-    do {
-      if (subject.endsWith("FD")) {
-        data.strSource = subject;
-        break;
-      }
-      if (subject.contains("medical")) break;
-    } while (false);
-    
-    parseAddress(StartType.START_CALL, FLAG_ALLOW_DUAL_DIRECTIONS, body, data);
-    String sExtra = getLeft();
-    if (data.strCall.length() == 0) {
-      data.strCall = sExtra;
-    } else {
-      data.strSupp = sExtra;
-    }
-    return true;
-  }
+
 }
