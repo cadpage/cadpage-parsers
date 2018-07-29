a2026a2e83e399f8e598f7ac7cf2f89dd3bb2a79
Ken Corbin <ken@cadpage.org>
2018-07-28 20:45:37 -0700

    Fixed parsing problem with Delaware COunty, NY

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/NY/NYDelawareCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/NY/NYDelawareCountyParser.java
@@ -1,13 +1,16 @@
 package net.anei.cadpage.parsers.NY;
 
-import net.anei.cadpage.parsers.CodeSet;
-import net.anei.cadpage.parsers.MsgInfo.Data;
-import net.anei.cadpage.parsers.dispatch.DispatchB2Parser;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-public class NYDelawareCountyParser extends DispatchB2Parser {
+import net.anei.cadpage.parsers.FieldProgramParser;
+import net.anei.cadpage.parsers.MsgInfo.Data;
+
+public class NYDelawareCountyParser extends FieldProgramParser {
   
   public NYDelawareCountyParser() {
-    super("DELCO911:||DELCO 911:", CITY_CODES, "DELAWARE COUNTY", "NY");
+    super("DELAWARE COUNTY", "NY", 
+          "CALL ADDRCITY ID! CALLER:NAME! END");
   }
   
   @Override
@@ -16,48 +19,34 @@ public class NYDelawareCountyParser extends DispatchB2Parser {
   }
   
   @Override
-  protected CodeSet buildCallList() {
-    return null;
+  protected boolean parseMsg(String body, Data data) {
+    if (!body.startsWith("FLEISCHMANS FD: ")) return false;
+    body = body.substring(16).trim();
+    return parseFields(body.split("\\|"), 4, data);
   }
-
+  
   @Override
-  protected boolean parseAddrField(String field, Data data) {
-    if (!super.parseAddrField(field, data)) return false;
-    if (data.strCity.equals("FLEISCHMANN")) data.strCity = "FLEISCHMANNS";
-    return true;
+  public Field getField(String name) {
+    if (name.equals("ADDRCITY")) return new MyAddressCityField();
+    if (name.equals("ID")) return new IdField("CAD\\d\\d-\\d{6}", true);
+    return super.getField(name);
   }
-
-  private static final String[] CITY_CODES = new String[]{
-    "ANDES",
-    "BOVINA",
-    "COLCHESTER",
-    "DAVENPORT",
-    "DELHI",
-    "DEPOSIT",
-    "DOWNSVILLE",
-    "FLEISCHMANN",
-    "FLEISCHMANNS",
-    "FLEISCHMANNS VILLAGE",
-    "FRANKLIN",
-    "HAMDEN",
-    "HANCOCK",
-    "HARPERSFIELD",
-    "HOBART",
-    "KORTRIGHT",
-    "MARGARETVILLE",
-    "MASONVILLE",
-    "MEREDITH",
-    "MIDDLETOWN",
-    "ROXBURY",
-    "SIDNEY",
-    "STAMFORD",
-    "TOMPKINS",
-    "WALTON",
-    
-    // Greene County
-    "HALCOTT",
-    
-    // Ulster  County
-    "HARDENBURG"
-  };
+  
+  private static final Pattern ST_ZIP_PTN = Pattern.compile("([A-Z]{2})(?: +(\\d{5}))?");
+  private class MyAddressCityField extends AddressCityField {
+    @Override
+    public void parse(String field, Data data) {
+      Parser p = new Parser(field);
+      String city = p.getLastOptional(',');
+      Matcher match = ST_ZIP_PTN.matcher(city);
+      if (match.matches()) {
+        data.strState = match.group(1);
+        String zip = match.group(2);
+        city = p.getLastOptional(',');
+        if (zip != null && city.length() == 0) city = zip;
+      }
+      data.strCity = city;
+      parseAddress(p.get(), data);
+    }
+  }
 }
