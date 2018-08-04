439b9c83ba54517b0d11af714338b63a43c3bbd8
Ken Corbin <ken@cadpage.org>
2018-08-03 20:07:37 -0700

    Fixed parsing problem with Linn County, OR (B)

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/OR/ORLinnCountyBParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/OR/ORLinnCountyBParser.java
@@ -88,7 +88,7 @@ public class ORLinnCountyBParser extends FieldProgramParser {
     if (name.equals("PLACE_X")) return new MyPlaceCrossField();
     if (name.equals("LCITY")) return new MyLongCityField();
     if (name.equals("MAP")) return new MapField("\\d{3,4}|\\d{3}[A-D]|ALB|CMF|HBRG|LCJ|LCSO|LYO|MILL|SWH", true);
-    if (name.equals("MAPQ")) return new MapField("\\d{3,4}|\\d{3}[A-D]|ALB|CMF|HBRG|LCJ|LCSO|LYO|MILL|SWH", true);
+    if (name.equals("MAPQ")) return new MapField("\\d{3,4}|\\d{3}[A-D]|ALB|CMF|HBRG|LCJ|LCSO|LYO|MILL|SWH|", true);
     if (name.equals("CODE")) return new CodeField("(?i)\\d\\d?[A-Z]\\d\\d?[A-Z]?", true);
     if (name.equals("CODEQ")) return new CodeField("(?i)\\d\\d?[A-Z]\\d\\d?[A-Z]?|", true);
     if (name.equals("UNIT")) return new UnitField("(?:\\b(?:[A-Z]+\\d+[A-Z]?|\\d{3}|[A-Z]{1,3}FD|ST[A-Z]|CE|HBRG|SDIVEL?|SH1ST|SWH)\\b,?)+", true);
