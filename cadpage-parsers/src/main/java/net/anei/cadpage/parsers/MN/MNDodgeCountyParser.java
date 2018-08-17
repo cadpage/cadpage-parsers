c3f260f34d5ab3f33a8005f7eb6f83b6fcd87773
Ken Corbin <ken@cadpage.org>
2018-08-17 14:09:23 -0700

    Cleaned leading zeros from units for Dodge County, MN

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/MN/MNDodgeCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/MN/MNDodgeCountyParser.java
@@ -1,5 +1,8 @@
 package net.anei.cadpage.parsers.MN;
 
+import java.util.regex.Pattern;
+
+import net.anei.cadpage.parsers.MsgInfo.Data;
 import net.anei.cadpage.parsers.dispatch.DispatchA27Parser;
 
 /**
@@ -16,4 +19,14 @@ public class MNDodgeCountyParser extends DispatchA27Parser {
   public String getFilter() {
     return "noreply@cisusa.org,admin.strator@co.dodge.mn.us";
   }
+  
+  private static final Pattern UNIT_ZEROS_PTN = Pattern.compile("(?:^|(?<= ))0+(?=.)");
+
+  @Override
+  public boolean parseMsg(String subject, String body, Data data) {
+    if (!super.parseMsg(subject, body, data)) return false;
+    data.strUnit = UNIT_ZEROS_PTN.matcher(data.strUnit).replaceAll("");
+    return true;
+  }
+  
 }
