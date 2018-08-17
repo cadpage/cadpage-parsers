b2af85c2a7af59ef698488773ba1a3ec7eb59030
Ken Corbin <ken@cadpage.org>
2018-08-17 14:31:29 -0700

    Added GSP lookup table for Red Deer County, AB

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/ZCAAB/ZCAABRedDeerCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/ZCAAB/ZCAABRedDeerCountyParser.java
@@ -1,5 +1,6 @@
 package net.anei.cadpage.parsers.ZCAAB;
 
+import java.util.Properties;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -18,6 +19,7 @@ public class ZCAABRedDeerCountyParser extends DispatchA51Parser {
   
   protected ZCAABRedDeerCountyParser(String defCity) {
     super(defCity, "AB");
+    setupGpsLookupTable(GPS_LOOKUP_TABLE);
   }
   
   @Override
@@ -67,4 +69,21 @@ public class ZCAABRedDeerCountyParser extends DispatchA51Parser {
     return HWY_NN_PTN.matcher(sAddress).replaceAll("ALBERTA $1");  
   }
   private static final Pattern HWY_NN_PTN = Pattern.compile("\\bHWY +(\\d+)\\b", Pattern.CASE_INSENSITIVE);
+  
+  private static final Properties GPS_LOOKUP_TABLE = buildCodeTable(new String[]{
+      "4850 43 AVE",                          "+51.791691,-114.093236",
+      "4902 44 AVE",                          "+51.791463,-114.092998",
+      "5032 44 AVE",                          "+51.790321,-114.092735",
+      "5082 44 AVE",                          "+51.790159,-114.092261",
+      "5150 44 AVE",                          "+51.789459,-114.092203",
+      "4477 47 ST",                           "+51.792728,-114.091225",
+      "4470 48 ST",                           "+51.793672,-114.093157",
+      "4533 48 ST",                           "+51.792578,-114.094670",
+      "4534 49 ST",                           "+51.791512,-114.094278",
+      "4500 50 ST",                           "+51.785962,-114.093721",
+      "4450 53 ST",                           "+51.788396,-114.093292",
+      "4501 53 ST",                           "+51.788254,-114.094224",
+      "4550 53 ST",                           "+51.788715,-114.094699",
+      "50 ST",                                "+51.788461,-114.091142"
+  }); 
 }
