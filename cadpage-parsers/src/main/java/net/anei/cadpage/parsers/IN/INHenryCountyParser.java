862d74d9b124cfda180613c854a86962dc9d2a2e
Ken Corbin <ken@cadpage.org>
2018-09-06 10:55:12 -0700

    Parsing problem with Henry County, IN

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/IN/INHenryCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/IN/INHenryCountyParser.java
@@ -1,54 +1,79 @@
 package net.anei.cadpage.parsers.IN;
 
-import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import net.anei.cadpage.parsers.MsgInfo.Data;
-import net.anei.cadpage.parsers.MsgParser;
+import net.anei.cadpage.parsers.FieldProgramParser;
 
 /**
  * Henry County, IN
  */
-public class INHenryCountyParser extends MsgParser {
+public class INHenryCountyParser extends FieldProgramParser {
   
-  private static final Pattern MASTER1 = Pattern.compile("([- A-Z0-9]+)\\|([^:\n]+):([^\\|\n]*)(?:\\|(.*))?");
-  private static final Pattern MASTER2 = Pattern.compile("([- A-Z0-9]+)@([^\\*\n]+)\\*([^\\|\n]*)\\|(.*)");
+  private static final Pattern DELIM = Pattern.compile("\\|+");
   
   public INHenryCountyParser() {
-    super("HENRY COUNTY", "IN");
-    setFieldList("SRC CALL ADDR APT CITY CALL INFO");
+    super("HENRY COUNTY", "IN",
+          "CALL ADDRCITY! INFO/N+");
   }
   
   @Override
   public String getFilter() {
-    return "hcradio@emgsvcs.net,ncemsriprun@emgsvcs.net";
+    return "hcradio@emgsvcs.net,@henryco911.org";
   }
   
   @Override
   public boolean parseMsg(String subject, String body, Data data) {
-    if (subject.startsWith("DISPATCH:")) {
-      data.strSource = subject.substring(9).trim();
+    
+    if (subject.length() > 0) {
+      subject = stripFieldStart(subject, "DISPATCH:");
+      subject = stripFieldEnd(subject, "(no subject)");
+      subject = stripFieldStart(subject, "[");
+      subject = stripFieldEnd(subject, "]");
+      subject = stripFieldEnd(subject, "riprun");
+      data.strSource = subject;
     }
     
-    int pt = body.indexOf('\n');
-    if (pt >= 0) body = body.substring(0,pt).trim();
-    
-    Matcher match = MASTER1.matcher(body);
-    if (!match.matches()) match = MASTER2.matcher(body);
-    if (!match.matches()) return false;
-    data.strCall = match.group(1).trim();
-    parseAddress(match.group(2).trim(), data);
-    data.strCity = match.group(3).trim();
-    String call = getOptGroup(match.group(4));
-    if (call.length() == 0) data.strCall = "ALERT";
-    else if (call.length() <= 40) data.strCall = call;
-    else data.strSupp = call;
-    
-    return true;
+    return parseFields(DELIM.split(body), data);
   }
   
   @Override
   public String getProgram() {
-    return super.getProgram().replace("CITY", "CITY ST");
+    return "SRC " + super.getProgram();
+  }
+  
+  @Override
+  public Field getField(String name) {
+    if (name.equals("CALL")) return new CallField("[- A-Z]+", true);
+    if (name.equals("ADDRCITY")) return new MyAddressCityField();
+    if (name.equals("INFO")) return new MyInfoField();
+    return super.getField(name);
+  }
+  
+  private class MyAddressCityField extends AddressField {
+    
+    @Override
+    public void parse(String field, Data data) {
+      int pt = field.lastIndexOf(':');
+      if (pt >= 0) {
+        data.strCity = field.substring(pt+1).trim();
+        field = field.substring(0, pt).trim();
+      }
+      super.parse(field,  data);
+    }
+    
+    @Override
+    public String getFieldNames() {
+      return super.getFieldNames() + " CITY";
+    }
+  }
+  
+  private static final Pattern INFO_LAT_LON_PTN = Pattern.compile(" *\\b(?:Lat|Lon): *");
+  private class MyInfoField extends InfoField {
+    @Override
+    public void parse(String field, Data data) {
+      field = INFO_LAT_LON_PTN.matcher(field).replaceAll(" ").trim();
+      super.parse(field, data);
+    }
   }
 }
