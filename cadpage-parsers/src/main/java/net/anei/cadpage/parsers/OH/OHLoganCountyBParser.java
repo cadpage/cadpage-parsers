995167036aab39196d1ce1f0b145529470ddd5db
Ken Corbin <ken@cadpage.org>
2018-09-24 19:01:20 -0700

    Fixed parsing problem with Logan County, OH

---
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/OH/OHLoganCountyBParser.java
@@ -0,0 +1,38 @@
+package net.anei.cadpage.parsers.OH;
+
+import net.anei.cadpage.parsers.FieldProgramParser;
+import net.anei.cadpage.parsers.MsgInfo.Data;
+
+public class OHLoganCountyBParser extends FieldProgramParser {
+  
+  public OHLoganCountyBParser() {
+    super("LOGAN COUNTY", "OH", 
+           "CFS:ID! RUN#:SKIP! CODE:CALL! PLACE:PLACE! LAT:GPS1! LONG:GPS2! ADDR:ADDR! CITY:CITY! CROSS:X! PRI:PRI! DATE:DATE! TIME:TIME! SECT:MAP! DIST:SRC! UNIT:UNIT! DESC:INFO/N+");
+  }
+  
+  @Override
+  public String getFilter() {
+    return "lcso911@co.logan.oh.us";
+  }
+  
+  @Override
+  public int getMapFlags() {
+    return MAP_FLG_PREFER_GPS;
+  }
+  
+  @Override
+  protected boolean parseMsg(String subject, String body, Data data) {
+    if (!subject.startsWith("CFS:")) return false;
+    int pt = body.indexOf("\n\n\n");
+    if (pt >= 0) body = body.substring(0, pt).trim();
+    return parseFields(body.split("\n"), data);
+  }
+  
+  @Override
+  public Field getField(String name) {
+    if (name.equals("DATE")) return new DateField("\\d\\d/\\d\\d/\\d{4}", true);
+    if (name.equals("TIME")) return new TimeField("\\d\\d:\\d\\d:\\d\\d", true);
+    return super.getField(name);
+  }
+
+}
