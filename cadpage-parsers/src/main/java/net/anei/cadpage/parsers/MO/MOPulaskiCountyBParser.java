07b9eae729bf473d09b280f93b595fa29768327e
Ken Corbin <ken@cadpage.org>
2018-10-09 14:36:11 -0700

    Fixed parsing problem with Pulaski County, MO (B)

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/MO/MOPulaskiCountyBParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/MO/MOPulaskiCountyBParser.java
@@ -1,5 +1,8 @@
 package net.anei.cadpage.parsers.MO;
 
+import java.util.regex.Pattern;
+
+import net.anei.cadpage.parsers.MsgInfo.Data;
 import net.anei.cadpage.parsers.dispatch.DispatchA19Parser;
 
 
@@ -14,6 +17,27 @@ public class MOPulaskiCountyBParser extends DispatchA19Parser {
   
   @Override
   public String getFilter() {
-    return "DISPATCH";
+    return "DISPATCH,messaging@iamresponding.com";
   }
+  
+  private static final Pattern MISSING_CITY_PTN = Pattern.compile("\nAddress: (.*?) , (.*)\n");
+
+  @Override
+  protected boolean parseMsg(String subject, String body, Data data) {
+    
+    // Fix IAR edits
+    if (subject.equals("Waynesville Fire")) {
+      if (!body.contains("LONG TERM CAD#")) {
+        body = body.replace("\nREPORTED:", "\nLONG TERM CAD#   X   ACTIVE CALL# X\nPRIORITY:          REPORTED:");
+      }
+      if (!body.contains(" Zone:")) {
+        body = MISSING_CITY_PTN.matcher(body).replaceFirst("\nAddress: $1 Zone: City:$2\n");
+      }
+    }
+    
+    if (!super.parseMsg(subject, body, data)) return false;
+    data.strCallId = stripFieldEnd(data.strCallId, "/X/X");
+    return true;
+  }
+  
 }
