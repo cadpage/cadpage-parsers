c7839c8438a4b5141ab63e183e3c5298af3880bc
Ken Corbin <ken@cadpage.org>
2018-10-12 14:08:09 -0700

    Fixed mapping problem with Morrow COunty, OH (A)

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/OH/OHMorrowCountyAParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/OH/OHMorrowCountyAParser.java
@@ -1,5 +1,8 @@
 package net.anei.cadpage.parsers.OH;
 
+import java.util.Properties;
+import java.util.regex.Pattern;
+
 import net.anei.cadpage.parsers.dispatch.DispatchA1Parser;
 
 /**
@@ -9,10 +12,17 @@ public class OHMorrowCountyAParser extends DispatchA1Parser {
 
   public OHMorrowCountyAParser() {
     super("MORROW COUNTY", "OH"); 
+    setupGpsLookupTable(GPS_LOOKUP_TABLE);
   }
   
   @Override
   public String getFilter() {
     return "mc911@rrohio.com";
   }
+  
+  private static final Properties GPS_LOOKUP_TABLE = buildCodeTable(new String[]{
+      "1996 RD 146",                          "+40.435715,-83.010948",
+      "77 RD 158",                            "+40.440529,-83.020379",
+      "US RT 42 & RD 25",                     "+40.469301,-82.943640"
+  });
 }
