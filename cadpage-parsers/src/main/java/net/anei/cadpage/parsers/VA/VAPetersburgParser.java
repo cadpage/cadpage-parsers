6d0e382aa6e8038b13661456a225b38a6ec5ca8d
Ken Corbin <ken@cadpage.org>
2018-10-12 16:33:48 -0700

    Fixed parsing problem with Petersburg, VA

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/VA/VAPetersburgParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/VA/VAPetersburgParser.java
@@ -1,5 +1,6 @@
 package net.anei.cadpage.parsers.VA;
 
+import net.anei.cadpage.parsers.MsgInfo.Data;
 import net.anei.cadpage.parsers.dispatch.DispatchOSSIParser;
 
 
@@ -7,11 +8,24 @@ public class VAPetersburgParser extends DispatchOSSIParser {
   
   public VAPetersburgParser() {
     super("PETERSBURG", "VA",
-          "( CANCEL ADDR SKIP! | FYI CALL ADDR! X X ) INFO");
+          "( CANCEL ADDR SKIP! | FYI CALL PRI? ADDR! X X ) INFO");
   }
   
   @Override
   public String getFilter() {
     return "CAD@petersburg-police.com";
+  }
+  
+  @Override
+  protected boolean parseMsg(String body, Data data) {
+    int pt = body.indexOf('\n');
+    if (pt >= 0) body = body.substring(0,pt).trim();
+    return super.parseMsg(body, data);
+  }
+
+  @Override
+  public Field getField(String name) {
+    if (name.equals("PRI")) return new PriorityField("\\d", true);
+    return super.getField(name);
   }
 }
