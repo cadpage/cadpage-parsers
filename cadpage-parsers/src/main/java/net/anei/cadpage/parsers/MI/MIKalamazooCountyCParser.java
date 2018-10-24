743cd0d72ea498174f7446898141fba4809c3c37
Ken Corbin <ken@cadpage.org>
2018-10-23 19:12:53 -0700

    Fixed parsing problem with Kalamazoo County, MI (C new)

---
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/MI/MIKalamazooCountyCParser.java
@@ -0,0 +1,53 @@
+package net.anei.cadpage.parsers.MI;
+
+import net.anei.cadpage.parsers.MsgInfo.Data;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import net.anei.cadpage.parsers.MsgParser;
+
+public class MIKalamazooCountyCParser extends MsgParser {
+  
+  public MIKalamazooCountyCParser() {
+    super("KALAMAZOO COUNTY", "MI");
+    setFieldList("CALL ADDR APT CITY PLACE INFO");
+  }
+  
+  @Override
+  public String getFilter() {
+    return "paging@kccda911.org";
+  }
+  
+  private static final Pattern MASTER = Pattern.compile("([^@]+)@([^,]+),([^/]*)/(.*)");
+  
+  @Override
+  protected boolean parseMsg(String subject, String body, Data data) {
+    
+    if (!subject.equals("!DISP")) return false;
+    
+    body = stripFieldEnd(body, "//");
+    
+    String extra = null;
+    int pt = body.indexOf('\n');
+    if (pt >= 0) {
+      extra = body.substring(pt+1).trim();
+      body = body.substring(0, pt).trim();
+    }
+    
+    Matcher match = MASTER.matcher(body);
+    if (!match.matches()) return false;
+    data.strCall = match.group(1).trim();
+    parseAddress(match.group(2).trim(), data);
+    data.strCity = match.group(3).trim();
+    data.strPlace = match.group(4).trim();
+    
+    if (extra != null) {
+      for (String line : extra.split("\n")) {
+        line = line.trim();
+        data.strSupp = append(data.strSupp, "\n", line);
+      }
+    }
+    return true;
+  }
+}
--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/MI/MIKalamazooCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/MI/MIKalamazooCountyParser.java
@@ -1,45 +1,12 @@
 package net.anei.cadpage.parsers.MI;
 
-import java.util.Properties;
-
-import net.anei.cadpage.parsers.dispatch.DispatchA50Parser;
+import net.anei.cadpage.parsers.GroupBestParser;
 
 
-public class MIKalamazooCountyParser extends DispatchA50Parser {
+public class MIKalamazooCountyParser extends GroupBestParser {
   
   public MIKalamazooCountyParser() {
-    super(CITY_CODES, "KALAMAZOO COUNTY", "MI");
-    }
-  
-  @Override
-  public String getFilter() {
-    return "kdpshelpdesk@kalamazoocity.org";
+    super(new MIKalamazooCountyAParser(), new MIKalamazooCountyCParser());
   }
   
-  private static final Properties CITY_CODES = buildCodeTable(new String[]{
-      "ALAM",  "ALAMO TWP",
-      "AUGU",  "AUGUSTA",
-      "BRAD",  "BRADY TWP",
-      "CHAR",  "CHARLESTON TWP",
-      "CLXT",  "CLIMAX TWP",
-      "CLXV",  "CLIMAX",
-      "COMS",  "COMSTOCK TWP",
-      "COOP",  "COOPER TWP",
-      "GALE",  "GALESBURG",
-      "KALC",  "KALAMAZOO",
-      "KALT",  "KALAMAZOO TWP",
-      "OSHT",  "OSHTEMO TWP",
-      "PARC",  "PARCHMENT",
-      "PAVI",  "PAVILION TWP",
-      "PORT",  "PORTAGE",
-      "PRRO",  "PRAIRIE RONDE TWP",
-      "RICT",  "RICHLAND TWP",
-      "RICV",  "RICHLAND",
-      "ROSS",  "ROSS TWP",
-      "SCHT",  "SCHOOLCRAFT TWP",
-      "SCHV",  "SCHOOLCRAFT",
-      "TEXA",  "TEXAS TWP",
-      "VICK",  "VICKSBURG",
-      "WAKE",  "WAKESHMA TWP"
-  });
 }
