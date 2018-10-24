a35de5d62ec69749956ae9d16540f11a4dbf8b25
Ken Corbin <ken@cadpage.org>
2018-10-23 22:16:17 -0700

    Fixed parsing problem with Marion County, IN

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/IN/INMarionCountyBParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/IN/INMarionCountyBParser.java
@@ -1,5 +1,8 @@
 package net.anei.cadpage.parsers.IN;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import net.anei.cadpage.parsers.MsgInfo.Data;
 import net.anei.cadpage.parsers.dispatch.DispatchA52Parser;
 
@@ -11,13 +14,16 @@ public class INMarionCountyBParser extends DispatchA52Parser {
   
   @Override
   public String getFilter() {
-    return "MotorolaCAD@page.indy.gov";
+    return "MotorolaCAD@page.indy.gov,CAD@page.indy.gov";
   }
+  
+  private static final Pattern MARKER = Pattern.compile("(?:Motorola )?CAD: *");
   
   @Override
   protected boolean parseMsg(String body, Data data) {
-    if (!body.startsWith("Motorola CAD:")) return false;
-    body = body.substring(13).trim();
+    Matcher match = MARKER.matcher(body);
+    if (!match.lookingAt()) return false;
+    body = body.substring(match.end());
     return super.parseMsg(body, data);
   }
 
