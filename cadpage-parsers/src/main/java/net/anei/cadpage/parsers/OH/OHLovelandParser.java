f614992fbf3ebfe2a36c7d88215314bc5395fec5
Ken Corbin <ken@cadpage.org>
2018-10-27 18:59:11 -0700

    Fixed parasing problem with Clermont County, OH

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/OH/OHLovelandParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/OH/OHLovelandParser.java
@@ -3,6 +3,7 @@ package net.anei.cadpage.parsers.OH;
 import java.util.Properties;
 import java.util.regex.Pattern;
 
+import net.anei.cadpage.parsers.MsgInfo.Data;
 import net.anei.cadpage.parsers.dispatch.DispatchA1Parser;
 
 public class OHLovelandParser extends DispatchA1Parser {
@@ -23,9 +24,23 @@ public class OHLovelandParser extends DispatchA1Parser {
   
   @Override
   public String getFilter() {
-    return "dispatcher@safety-center.org,utcc@union-township.oh.us,dispatcher@lsfd.org";
+    return "dispatcher@safety-center.org,utcc@union-township.oh.us,dispatcher@lsfd.org,BrownCommCtr@BrownCountyOhio.gov";
   }
   
+  @Override
+  protected boolean parseMsg(String subject, String body, Data data) {
+    if (subject.length() == 0)
+      if (body.startsWith("Alert:")) {
+        int pt = body.indexOf('\n');
+        if (pt < 0) return false;
+        subject = body.substring(0, pt).trim();
+        body = body.substring(pt+1).trim();
+      } else if (body.startsWith("LOC:")) {
+        subject = "Alert:";
+    }
+    return super.parseMsg(subject, body, data);
+  }
+
   @Override
   public String adjustMapAddress(String addr) {
     return TR_PTN.matcher(addr).replaceAll("TERRACE");
