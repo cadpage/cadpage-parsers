6791efca476bc93ef71d9b651ea3827502578246
Ken Corbin <ken@cadpage.org>
2018-10-29 12:38:44 -0700

    Fixed parsing problem with parser County, CA (C)

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/CA/CAPlacerCountyCParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/CA/CAPlacerCountyCParser.java
@@ -8,32 +8,18 @@ import net.anei.cadpage.parsers.MsgInfo.Data;
 public class CAPlacerCountyCParser extends FieldProgramParser {
   
   public CAPlacerCountyCParser() {
-    super(CITY_LIST, "PLACER COUNTY", "CA", 
-          "( SELECT/1 CALL! Address:ADDR! Common_Name:PLACE! XStreets:X! Additional_Location_Info:INFO! Assigned_Units:UNIT! Quadrant:MAP! District:MAP/S! Beat:MAP/S! Incident:ID! Narrative:INFO! " + 
-          "| CALL ADDR CODE UNIT UNIT/CS INFO+? ID! )");
+    super(CITY_LIST, "PLACER COUNTY", "CA",
+          "Call:CALL! Place:PLACE! ADDR:ADDRCITY! City:CITY! District:MAP! Map:MAP/S! Units:UNIT! Narrative:INFO! INFO/N+ Incidents:ID! CFS:SKIP! Primary:SKIP!");
   }
   
   @Override
-  public String getFilter() {
-    return "Dispatch@roseville.ca.us";
-  }
-  
-  @Override
-  protected boolean parseMsg(String subject, String body, Data data) {
-    if (body.indexOf(" Address:") >= 0) {
-      setSelectValue("1");
-      return super.parseMsg(body, data);
-    }  else {
-      setSelectValue("2");
-      return parseFields(body.split("\n"), data);
-    }
+  protected boolean parseMsg(String body, Data data) {
+    return parseFields(body.split("\n"), data);
   }
   
   @Override
   public Field getField(String name) {
     if (name.equals("CALL")) return new MyCallField();
-    if (name.equals("ADDR")) return new MyAddressField();
-    if (name.equals("ID")) return new IdField("\\d{4}-\\d{8}\\b.*", true);
     return super.getField(name);
   }
   
@@ -52,31 +38,6 @@ public class CAPlacerCountyCParser extends FieldProgramParser {
     @Override
     public String getFieldNames() {
       return "CODE CALL";
-    }
-  }
-  
-  private class MyAddressField extends AddressField {
-    @Override
-    public void parse(String field, Data data) {
-      int pt = field.indexOf(',');
-      if (pt >= 0) {
-        String addr = field.substring(0,pt).replace('@',  '&');
-        String city =field.substring(pt+1).trim();
-        parseAddress(StartType.START_ADDR, FLAG_NO_CITY | FLAG_RECHECK_APT | FLAG_ANCHOR_END, 
-                     addr, data);
-        parseAddress(StartType.START_ADDR, FLAG_ONLY_CITY, city, data);
-        if (data.strCity.length() == 0) abort();
-        data.strPlace = getLeft();
-      } else {
-        field = field.replace('@', '&');
-        parseAddress(StartType.START_ADDR, FLAG_RECHECK_APT, field, data);
-        data.strPlace = getLeft();
-      }
-    }
-    
-    @Override
-    public String getFieldNames() {
-      return "ADDR APT CITY PLACE";
     }
   }
   
