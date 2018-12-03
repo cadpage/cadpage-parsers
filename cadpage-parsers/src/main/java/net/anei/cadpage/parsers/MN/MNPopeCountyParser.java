1cbdee864373602abd24427b1f2c6c8dc54b7231
Ken Corbin <ken@cadpage.org>
2018-12-02 18:04:52 -0800

    Added GPS lookup table for Pope County, MN

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/MN/MNPopeCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/MN/MNPopeCountyParser.java
@@ -1,9 +1,16 @@
 package net.anei.cadpage.parsers.MN;
 
+import java.util.Properties;
+
 import net.anei.cadpage.parsers.dispatch.DispatchA43Parser;
 
 public class MNPopeCountyParser extends DispatchA43Parser {
   public MNPopeCountyParser() {
     super("POPE COUNTY", "MN");
+    setupGpsLookupTable(GPS_LOOKUP_TABLE);
   }
+  
+  private static final Properties GPS_LOOKUP_TABLE = buildCodeTable(new String[]{
+      "11550 200TH ST",           "45.607180,-95.171412"
+  });
 }
