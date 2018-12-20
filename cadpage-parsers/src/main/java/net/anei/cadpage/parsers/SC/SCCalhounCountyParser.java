d4e334c5d5bf8afad357661e016bd9425c3a9f88
Ken Corbin <ken@cadpage.org>
2018-12-19 21:11:05 -0800

    Fixed parsing problem with Racine County, WI (A)

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/SC/SCCalhounCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/SC/SCCalhounCountyParser.java
@@ -42,6 +42,7 @@ public class SCCalhounCountyParser extends DispatchA48Parser {
       "GAS LEAK/PROPANE",
       "GUNSHOT WOUND",
       "PAIN",
+      "RESPIRATORY DISTRESS",
       "ROAD BLOCKAGE",
       "STRUCTURE FIRE",
       "SUSPICIOUS ACTIVITY",
