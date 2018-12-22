a837369e9f2d6c75e11564e68baa2cff76f22961
Ken Corbin <ken@cadpage.org>
2018-12-21 21:33:04 -0800

    Fixed parsing problem with Washtenaw County, MI (B)

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/MI/MIWashtenawCountyBParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/MI/MIWashtenawCountyBParser.java
@@ -5,12 +5,13 @@ import java.util.regex.Pattern;
 
 import net.anei.cadpage.parsers.MsgInfo.Data;
 import net.anei.cadpage.parsers.MsgInfo.MsgType;
-import net.anei.cadpage.parsers.MsgParser;
+import net.anei.cadpage.parsers.FieldProgramParser;
 
-public class MIWashtenawCountyBParser extends MsgParser {
+public class MIWashtenawCountyBParser extends FieldProgramParser {
   
   public MIWashtenawCountyBParser() {
-    super("WASHTENAW COUNTY", "MI");
+    super("WASHTENAW COUNTY", "MI", 
+          "CALL:CALL! PLACE:PLACE! ADDR:ADDRCITY! ID:ID! PRI:PRI! DATE:DATETIME! INFO:INFO!");
     setFieldList("ID PRI CALL ADDR APT CITY");
   }
   
@@ -20,7 +21,7 @@ public class MIWashtenawCountyBParser extends MsgParser {
   }
   
   private static final Pattern SUBJECT_PTN = Pattern.compile("(New Incident|Update to Incident|Incident Completed) - (\\d+)");
-  private static final Pattern MASTER = Pattern.compile("New Incident:\n(\\S+) - (.*?) at (.*?)(?:, (.*?))?(?: (\\d{5}))?");
+  private static final Pattern MASTER = Pattern.compile("New Incident:\n(.*?) - (.*?) at(?: (.*?)(?:, (.*?))?(?: (\\d{5}))?)?");
   
   @Override
   protected boolean parseMsg(String subject, String body, Data data) {
@@ -29,11 +30,16 @@ public class MIWashtenawCountyBParser extends MsgParser {
     String type = match.group(1);
     data.strCallId = match.group(2);
     
+    if (body.startsWith("CALL:")) {
+      body = body.replace("ID:", " ID:");
+      return super.parseMsg(body, data);
+    }
+    
     match = MASTER.matcher(body);
     if (!match.matches()) return false;
     data.strPriority = match.group(1);
     data.strCall = match.group(2);
-    parseAddress(match.group(3).trim(), data);
+    parseAddress(getOptGroup(match.group(3)), data);
     data.strCity = getOptGroup(match.group(4));
     if (data.strCity.length() == 0) data.strCity = getOptGroup(match.group(5));
     
@@ -44,5 +50,37 @@ public class MIWashtenawCountyBParser extends MsgParser {
     }
     return true;
   }
-
+  
+  @Override
+  public Field getField(String name) {
+    if (name.equals("ADDRCITY")) return new MyAddressCityField();
+    if (name.equals("DATETIME")) return new MyDateTimeField();
+    return super.getField(name);
+  }
+  
+  private static final Pattern ADDR_ZIP_PTN = Pattern.compile("(.*?) +(\\d{5})");
+  private class MyAddressCityField extends AddressCityField {
+    @Override
+    public void parse(String field, Data data) {
+      String zip = null;
+      Matcher match = ADDR_ZIP_PTN.matcher(field);
+      if (match.matches()) {
+        field = match.group(1);
+        zip = match.group(2);
+      }
+      super.parse(field, data);
+      if (data.strCity.length() == 0 && zip != null) data.strCity = zip;
+    }
+  }
+  
+  private static final Pattern DATE_TIME_PTN = Pattern.compile("(\\d\\d?-\\d\\d?-\\d{4}) (\\d\\d?:\\d\\d)");
+  private class MyDateTimeField extends DateTimeField {
+    @Override
+    public void parse(String field, Data data) {
+      Matcher match = DATE_TIME_PTN.matcher(field);
+      if (!match.matches()) abort();
+      data.strDate = match.group(1).replace('-', '/');
+      data.strTime = match.group(2);
+    }
+  }
 }
