8b616fa2e84bcd454d4a1b8ad1318c647da38982
Ken Corbin <ken@cadpage.org>
2019-01-08 14:52:07 -0800

    Fixed parsing problem with Elk County, PA

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/PA/PAElkCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/PA/PAElkCountyParser.java
@@ -20,7 +20,7 @@ public class PAElkCountyParser extends FieldProgramParser {
   
   PAElkCountyParser(String defCity, String defState) {
     super(CITY_TABLE, defCity, defState,
-          "( Inc_Code:CALL! Address:ADDRCITY! ( Common_Name:PLACE! Units:UNIT! Cross_Streets:X% " + 
+          "( Inc_Code:CALL! Address:ADDRCITY! ( Common_Name:PLACE! ( Units:UNIT! Cross_Streets:X% | Cross_Streets:X! Units:UNIT% ) " + 
                                              "| City:CITY! Cross_Streets:X? Apt:APT? Agency:SRC% INFO+? DATETIME " + 
                                              ") " + 
           "| CALL! Address:ADDRCITY! Common_Name:PLACE! Units:UNIT! Cross_St:X! " + 
