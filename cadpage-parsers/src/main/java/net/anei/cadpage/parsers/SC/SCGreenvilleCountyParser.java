21e5feb5d224bdb1a880ade7c471421fbb881558
Ken Corbin <ken@cadpage.org>
2019-01-18 15:08:49 -0800

    Fixed another parseing problem

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/SC/SCGreenvilleCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/SC/SCGreenvilleCountyParser.java
@@ -1,13 +1,15 @@
 package net.anei.cadpage.parsers.SC;
 
 import net.anei.cadpage.parsers.GroupBestParser;
+import net.anei.cadpage.parsers.GroupBlockParser;
 
 
 public class SCGreenvilleCountyParser extends GroupBestParser {
   
   public SCGreenvilleCountyParser() {
-    super(new SCGreenvilleCountyAParser(), 
-          new SCGreenvilleCountyBParser(),
-          new SCGreenvilleCountyDParser());
+    super(new SCGreenvilleCountyDParser(),
+          new GroupBlockParser(),
+          new SCGreenvilleCountyAParser(), 
+          new SCGreenvilleCountyBParser());
   }
 }
