8560cabba226f669887f98293213e3804737546e
Ken Corbin <ken@cadpage.org>
2019-01-25 14:01:29 -0800

    Fixed mapping problem with Lac Qui Parle County, MN

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/MN/MNLacQuiParleCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/MN/MNLacQuiParleCountyParser.java
@@ -1,10 +1,22 @@
 package net.anei.cadpage.parsers.MN;
 
+import java.util.Properties;
+
 import net.anei.cadpage.parsers.dispatch.DispatchA43Parser;
 
 public class MNLacQuiParleCountyParser extends DispatchA43Parser {
   
   public MNLacQuiParleCountyParser() {
     super("LAC QUI PARLE COUNTY", "MN");
+    setupGpsLookupTable(GPS_LOOKUP_TABLE);
   }
+  
+  @Override
+  protected String adjustGpsLookupAddress(String address) {
+    return address.toUpperCase();
+  }
+
+  private static final Properties GPS_LOOKUP_TABLE = buildCodeTable(new String[]{
+      "1372 310TH ST",                        "+45.111060,-96.379520"
+  });
 }
