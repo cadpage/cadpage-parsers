ed6a0980b237a3038de6024ef720c455353f0201
Ken Corbin <ken@cadpage.org>
2019-03-03 16:36:42 -0800

    Fixed parsing problem with Warren County, PA

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/PA/PAWarrenCountyBParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/PA/PAWarrenCountyBParser.java
@@ -1,5 +1,8 @@
 package net.anei.cadpage.parsers.PA;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import net.anei.cadpage.parsers.FieldProgramParser;
 import net.anei.cadpage.parsers.MsgInfo.Data;
 
@@ -7,7 +10,7 @@ public class PAWarrenCountyBParser extends FieldProgramParser {
   
   public PAWarrenCountyBParser() {
     super("WARREN COUNTY", "PA", 
-          "Inc_Code:CALL! Address:ADDRCITY! Common_Name:PLACE! Units:UNIT! Cross_Streets:X! END");
+          "Inc_Code:CALL! Address:ADDRCITY! Common_Name:PLACE! Units:UNIT! Cross_Streets:X END");
   }
   
   @Override
@@ -20,5 +23,28 @@ public class PAWarrenCountyBParser extends FieldProgramParser {
     if (!subject.equals("Warren County Alert")) return false;
     return parseFields(body.split("\n"), data);
   }
-
+  
+  @Override
+  public Field getField(String name) {
+    if (name.equals("PLACE")) return new MyPlaceField();
+    return super.getField(name);
+  }
+  
+  private static final Pattern PLACE_PTN = Pattern.compile("\\d\\d +(.*)");
+  private class MyPlaceField extends PlaceField {
+    @Override
+    public void parse(String field, Data data) {
+      Matcher match = PLACE_PTN.matcher(field);
+      if (match.matches()) field = match.group(1);
+      super.parse(field, data);
+    }
+  }
+  
+  @Override
+  public String adjustMapCity(String city) {
+    if (city.endsWith(" TWP")) {
+      city = city.substring(0,city.length()-3) + "TOWNSHIP";
+    }
+    return city;
+  }
 }
