cca877e1737e9daa93889bec59f56edc5560073d
Ken Corbin <ken@cadpage.org>
2019-03-03 18:02:33 -0800

    Fixed parsing problem with Clay COunty, MO (C)

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/MO/MOClayCountyCParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/MO/MOClayCountyCParser.java
@@ -1,5 +1,6 @@
 package net.anei.cadpage.parsers.MO;
 
+import net.anei.cadpage.parsers.MsgInfo.Data;
 import net.anei.cadpage.parsers.dispatch.DispatchA33Parser;
 
 
@@ -14,4 +15,23 @@ public class MOClayCountyCParser extends DispatchA33Parser {
   public String getFilter() {
     return "ITI@NKC.ORG";
   }
+
+  @Override
+  protected boolean parseMsg(String body, Data data) {
+    if (!super.parseMsg(body, data)) return false;
+    String cross = data.strCross;
+    if (cross.endsWith(", MO")) {
+      data.strCity = append(data.strCity, " ", cross.substring(0, cross.length()-4).trim());
+      data.strState = "MO";
+      data.strCross = "";
+    } else if (cross.endsWith("CITY")) {
+      data.strCity = append(data.strCity, " ", cross);
+      data.strCross = "";
+    } else if (cross.equals("MO")) {
+      data.strState = "MO";
+      data.strCross = "";
+    }
+    return true;
+  }
+  
 }
