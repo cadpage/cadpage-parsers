ebf46eb8543f0a218edbc1db4da30cfdcf8f8384
Ken Corbin <ken@cadpage.org>
2019-03-13 12:25:59 -0700

    Fixed parsing problem with South Lake Tahoe, CA

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/CA/CASouthLakeTahoeParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/CA/CASouthLakeTahoeParser.java
@@ -5,12 +5,12 @@ import net.anei.cadpage.parsers.dispatch.DispatchA58Parser;
 public class CASouthLakeTahoeParser extends DispatchA58Parser {
 
   public CASouthLakeTahoeParser() {
-    super("Alliance Message", CITY_LIST, "SOUTH LAKE TAHOE", "CA");
+    super("Cyrun Auto Paging", CITY_LIST, "SOUTH LAKE TAHOE", "CA");
   }
 
   @Override
   public String getFilter() {
-    return "cyrunpaging@gmail.com";
+    return "cyrunpaging@cyrun.com";
   }
   
   private static String[] CITY_LIST = new String[] {
