ffc83e5fd9721101795dfd2d2b5bfd09bc019fcd
Ken Corbin <ken@cadpage.org>
2019-03-22 10:32:17 -0700

    Fixed parsing problem with St Louis County, MO (C)

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/MO/MOStLouisCountyJParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/MO/MOStLouisCountyJParser.java
@@ -10,39 +10,44 @@ public class MOStLouisCountyJParser extends DispatchProQAParser {
   
   public MOStLouisCountyJParser() {
     super(MOStLouisCountyParser.CITY_LIST, "ST LOUIS COUNTY", "MO",
-          "PRI! CALL! PLACE! EMPTY UNIT EMPTY TIME INFO/SLS+? ID! ADDR! INFO2+", true);
-  }
-  
-  private String infoLine;
-  
-  @Override
-  protected boolean parseMsg(String body, Data data) {
-    infoLine = "";
-    if (!super.parseMsg(body, data)) return false;
-    data.strSupp = append(data.strSupp, "\n", infoLine);
-    return true;
-  }
-  @Override
-  public Field getField(String name) {
-    if (name.equals("INFO2")) return new MyInfo2Field();
-    return super.getField(name);
+          "PRI! CALL! PLACE! PLACE+? TIME INFO/SLS+? ID! ADDR! INFO2+", true);
   }
 
-  private static final Pattern INFO_APT_PTN = Pattern.compile("(?:APT|RM|LOT)[- ]+(.*)");
+  @Override
+  public Field getField(String name) {
+    if (name.equals("PLACE")) return new MyPlaceField();
+    if (name.equals("INFO2")) return new MyInfo2Field();
+    if (name.equals("TIME")) return new TimeField("\\d\\d:\\d\\d", true);
+    return super.getField(name);
+  }
+  
+  private class MyPlaceField extends PlaceField {
+    @Override
+    public void parse(String field, Data data) {
+      data.strPlace = append(data.strPlace, "/", field);
+    }
+  }
+
+  private static final Pattern INFO_APT_PTN = Pattern.compile("(?:APT|RM|LOT)[- ]+(.*)|ED[- ]+.*", Pattern.CASE_INSENSITIVE);
   private class MyInfo2Field extends Field {
     @Override
     public void parse(String field, Data data) {
-      Matcher match = INFO_APT_PTN.matcher(field);
-      if (match.matches()) {
-        data.strApt = append(data.strApt, "-", match.group(1));
-        return;
+      if (data.strApt.length() == 0) {
+        Matcher match = INFO_APT_PTN.matcher(field);
+        if (match.matches()) {
+          String apt = match.group(1);
+          if (apt == null) apt = field;
+          data.strApt = append(data.strApt, "-", apt);
+          return;
+        }
       }
       
       if (data.strCity.length() == 0 && isCity(field)) {
         data.strCity = field;
         return;
       }
-      infoLine = append(infoLine, " / ", data.strSupp);
+      
+      data.strSupp = append(data.strSupp, " / ", field);
     }
 
     @Override
