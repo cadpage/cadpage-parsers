e305d1d6f68a484688279c7c0d8e920f2f4db18c
Ken Corbin <ken@cadpage.org>
2019-03-23 21:27:46 -0700

    Fixed parsing problem with Rockky Mount, NC

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/NC/NCRockyMountParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/NC/NCRockyMountParser.java
@@ -1,5 +1,7 @@
 package net.anei.cadpage.parsers.NC;
 
+import java.util.Properties;
+
 import net.anei.cadpage.parsers.FieldProgramParser;
 import net.anei.cadpage.parsers.MsgInfo.Data;
 
@@ -10,33 +12,51 @@ public class NCRockyMountParser extends FieldProgramParser {
   
   public NCRockyMountParser() {
     super("ROCKY MOUNT", "NC",
-          "ADDR CALL X! INFO+");
+          "ADDR CALL CALL/CS+? X DATETIME! END");
+    setupGpsLookupTable(GPS_LOOKUP_TABLE);
   }
 
   @Override
   public String getFilter() {
-    return "send.bot@rockymountnc.gov";
+    return "@rockymountnc.gov";
   }
   
   @Override
   protected boolean parseMsg(String subject, String body, Data data) {
     if (!subject.equals("Rocky Mount 911 Incident")) return false;
-    return parseFields(body.split(", "), data);
+    return parseFields(body.split(","), data);
   }
   
   @Override
   public Field getField(String name) {
     if (name.equals("X")) return new MyCrossField();
+    if (name.equals("DATETIME")) return new DateTimeField("\\d\\d?/\\d\\d?/\\d{4} \\d\\d?:\\d\\d:\\d\\d", true);
     return super.getField(name);
   }
   
   private class MyCrossField extends CrossField {
     @Override
-    public void parse(String field, Data data) {
-      if (!field.contains("/")) abort();
+    public boolean canFail() {
+      return true;
+    }
+    
+    @Override
+    public boolean checkParse(String field, Data data) {
+      if (!field.contains("/")) return false;
       field = stripFieldStart(field, "/");
       field = stripFieldEnd(field, "/");
       super.parse(field, data);
+      return true;
+    }
+    
+    @Override
+    public void parse(String field, Data data) {
+      if (!checkParse(field, data)) abort();
     }
   }
+  
+  private static final Properties GPS_LOOKUP_TABLE = buildCodeTable(new String[]{
+      "396 PARK AVE",                         "+35.945500,-77.786200"
+
+  });
 }
