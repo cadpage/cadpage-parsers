a4e4254cce4b73a23ed872dcec0ad0a830aea511
Ken Corbin <ken@cadpage.org>
2019-03-29 18:19:41 -0700

    Fixed parsing problem with Brown County, MN

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/MN/MNBrownCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/MN/MNBrownCountyParser.java
@@ -1,5 +1,6 @@
 package net.anei.cadpage.parsers.MN;
 
+import net.anei.cadpage.parsers.MsgInfo.Data;
 import net.anei.cadpage.parsers.dispatch.DispatchA27Parser;
 
 public class MNBrownCountyParser extends DispatchA27Parser {
@@ -12,4 +13,13 @@ public class MNBrownCountyParser extends DispatchA27Parser {
   public String getFilter() {
     return "noreply@cisusa.org";
   }
+
+  @Override
+  public boolean parseMsg(String subject, String body, Data data) {
+    int pt = body.indexOf("\nNotification from CIS Active 911:");
+    if (pt >= 0) body = body.substring(pt+1);
+    return super.parseMsg(subject, body, data);
+  }
+  
+  
 }
