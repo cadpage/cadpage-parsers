6dfc4f0be60f316542bc329e7a97e87f920e6a0d
Ken Corbin <ken@cadpage.org>
2019-04-03 19:20:07 -0700

    Fixed parsing problem with Monroeville, PA

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/PA/PAMonroevilleParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/PA/PAMonroevilleParser.java
@@ -18,6 +18,12 @@ public class PAMonroevilleParser extends DispatchA1Parser {
 
   @Override
   protected boolean parseMsg(String subject, String body, Data data) {
+    if (subject.equals("AUTOMATED MESSAGE DO NOT REPLY")) {
+      int pt = body.indexOf(", RUN CARD:");
+      if (pt < 0) return false;
+      subject = body.substring(0, pt).trim();
+      body = body.substring(pt+2);
+    }
     body = body.replace("\n ", " ");
     return super.parseMsg(subject, body, data);
   }
