90c43ae8e976f93a501c79d831fb161274d99a4b
Ken Corbin <ken@cadpage.org>
2019-04-04 20:09:09 -0700

    Fixed parsing problem with Lycoming County, PA

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/PA/PALycomingCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/PA/PALycomingCountyParser.java
@@ -13,8 +13,7 @@ public class PALycomingCountyParser extends FieldProgramParser {
 
   public PALycomingCountyParser() {
     super(CITY_CODES, "LYCOMING COUNTY", "PA", 
-          "( DISPATCH_REPORT! Ver:SKIP! CFS_Number:ID! DATETIME1! RECOMMENDED! UNIT1/ZS+ Note:INFO/N+ CALL:CALL! INC_DESC:CALL_DESC! LOCATION:ADDRCITY! GPS! BUSINESS:PLACE! RECEIVED:SKIP! Cross_Streets:X? Boundaries:INFO/N+ " +
-          "| DATETIME_CODE! CALL! ADDR! CITY! Apt:APT! Cross_Streets:X? EMPTY? GOOGMAP_GPS! )");
+          "DATETIME_CODE CALL! ADDR! CITY! Apt:APT! Cross_Streets:X? EMPTY? GOOGMAP_GPS!");
   }
   
   @Override
@@ -43,47 +42,24 @@ public class PALycomingCountyParser extends FieldProgramParser {
     return saddress;
   }
   
-  private static Pattern SUBJ_ID = Pattern.compile("CAD Page for CFS (\\d+-\\d+)");
+  private static Pattern SUBJ_ID = Pattern.compile("CAD Page for CFS (B?\\d+-\\d+)");
   @Override
   public boolean parseMsg(String subject, String body, Data data) {
-    //format A
-    if (subject.equals("DISPATCH REPORT")) {
-      //insert a tag before CALL so SAP can parse it easily
-      int ci = body.indexOf("RECOMMENDED:");
-      if (ci == -1) return false;
-      ci = body.indexOf("\n \n", ci) + 3;
-      if (ci == 2) return false; //-1+3=2
-      body = body.substring(0, ci) + "CALL:" + body.substring(ci);
-      
-      return parseFields(body.split("\n\\s*"), data);
-    }
-    
-    //format B
     Matcher mat = SUBJ_ID.matcher(subject);
     if (!mat.matches()) return false;
     data.strCallId = mat.group(1);
     return parseFields(body.split("\n"), data);
   }
   
-  private static final DateFormat DATE_FMT1 = new SimpleDateFormat("EEE MMM dd yyyy HH:mm:ss");
   @Override
   public Field getField(String name) {
-    //both formats
-    if (name.equals("CALL")) return new CallField("\\s*|(?i)[A-Z](?: [A-Z0-9 /]+)?", true); //might be blank
-    //format A
-    if (name.equals("DISPATCH_REPORT")) return new SkipField("DISPATCH REPORT", true);
-    if (name.equals("DATETIME1")) return new DateTimeField(DATE_FMT1);
-    if (name.equals("RECOMMENDED")) return new SkipField("RECOMMENDED:", true);
-    if (name.equals("UNIT1")) return new UnitField("([^\\s]+).+"); //first word only
-    if (name.equals("CALL_DESC")) return new CallField();
-    //format B
+    if (name.equals("CALL")) return new CallField("\\s*|(?i)[A-Z](?: [-*A-Z0-9 /]+)?", true); //might be blank
     if (name.equals("DATETIME_CODE")) return new MyDateTimeCodeField();
     if (name.equals("X")) return new MyCrossField();
     if (name.equals("GOOGMAP_GPS")) return new MyGMapGPSField();
     return super.getField(name);
   }
   
-  //////////   Format B fields   ///////////////////////////////////////////////////////////////////
   private static Pattern DT_CODE = Pattern.compile("(.+) (.+)");
   private static final DateFormat DATE_FMT2 = new SimpleDateFormat("EEE MMM dd yyyy HH:mm");
   private class MyDateTimeCodeField extends DateTimeField {
@@ -133,34 +109,44 @@ public class PALycomingCountyParser extends FieldProgramParser {
   
   //////////   Other stuff   ///////////////////////////////////////////////////////////////////
   private static final Properties CITY_CODES = buildCodeTable(new String[]{
-      "ANT",        "ANTHONY",
-      "BAS",        "BASTRESS",
-      "CHO",        "COGAN HOUSE",  //??
-      "CRA",        "CRAWFORD",
-      "CUM",        "CUMMINGS",
+      "ANT",        "ANTHONY TWP",
+      "BAS",        "BASTRESS TWP",
+      "BRO",        "BROWN TWP",
+      "CHO",        "COGAN HOUSE TWP",
+      "CLI",        "CLINTON TWP",
+      "CRA",        "CRAWFORD TWP",
+      "CUM",        "CUMMINGS TWP",
       "DUB",        "DUBOISTOWN",
-      "ELD",        "ELDRED",
-      "FAI",        "FAIRFIELD",
-      "GAM",        "GAMBLE",
-      "HEP",        "HEPBURN",
+      "ELD",        "ELDRED TWP",
+      "FAI",        "FAIRFIELD TWP",
+      "FRA",        "FRANKLIN TWP",
+      "GAM",        "GAMBLE TWP",
+      "GRN",        "GREENE TWP",
+      "HEP",        "HEPBURN TWP",
+      "JOR",        "JORDAN TWP",
       "JSH",        "JERSEY SHORE",
-      "LEW",        "LEWIS",
-      "LIM",        "LIMESTONE",
-      "LOY",        "LOYALSOCK",
-      "LYC",        "LYCOMING",
-      "MCH",        "MCHENRY",
-      "MCI",        "MCINTYRE",
+      "LEW",        "LEWIS TWP",
+      "LIM",        "LIMESTONE TWP",
+      "LOY",        "LOYALSOCK TWP",
+      "LYC",        "LYCOMING TWP",
+      "MCH",        "MCHENRY TWP",
+      "MCI",        "MCINTYRE TWP",
+      "MOR",        "MORELAND TWP",
+      "MTG",        "MONTGOMERY",
       "MTV",        "MONTOURSVILLE",
-      "MUT",        "MUNCY",
-      "NIP",        "NIPENOSE",
-      "OLY",        "OLD LYCOMING",
-      "PIA",        "PIATT",
-      "SUS",        "SUSQUEHANNA",
+      "MUN",        "MUNCY",
+      "MUT",        "MUNCY TWP",
+      "NIP",        "NIPENOSE TWP",
+      "OLY",        "OLD LYCOMING TWP",
+      "PCT",        "PINE CREEK TWP",
+      "PEN",        "PENN TWP",
+      "PIA",        "PIATT TWP",
+      "SUS",        "SUSQUEHANNA TWP",
       "SWI",        "SOUTH WILLIAMSPORT",
-      "UFA",        "UPPER FAIRFIELD",
-      "WAS",        "WASHINGTON",
-      "WAT",        "WATSON",
-      "WAY",        "WAYNE",
+      "UFA",        "UPPER FAIRFIELD TWP",
+      "WAS",        "WASHINGTON TWP",
+      "WAT",        "WATSON TWP",
+      "WAY",        "WAYNE TWP",
       "WIL",        "WILLIAMSPORT"
   });
   
