fb8b716120a85a9e52eba5e88f900ebd68e37147
Ken Corbin <ken@cadpage.org>
2019-04-11 16:37:27 -0700

    Fixed mapping problem with Ellis County, KS

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/KS/KSEllisCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/KS/KSEllisCountyParser.java
@@ -1,5 +1,7 @@
 package net.anei.cadpage.parsers.KS;
 
+import java.util.regex.Pattern;
+
 import net.anei.cadpage.parsers.dispatch.DispatchA25Parser;
 
 
@@ -11,6 +13,14 @@ public class KSEllisCountyParser extends DispatchA25Parser {
   
   @Override
   public String getFilter() {
-    return "1-HPDDispatch@haysusa.com";
+    return "1-HPDDispatch@haysusa.com,cmccollum@haysusa.com";
+  }
+  
+  private static final Pattern US_183_ALT_PTN = Pattern.compile("\\b183 +ALT(?: +HWY)?\\b", Pattern.CASE_INSENSITIVE);
+  
+  @Override
+  public String adjustMapAddress(String addr) {
+    addr = US_183_ALT_PTN.matcher(addr).replaceAll("183 BYPASS");
+    return addr;
   }
 }
