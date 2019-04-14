a89461cbfdeb1c7faafe77d6c0713ecf2a4b0bf3
Ken Corbin <ken@cadpage.org>
2019-04-13 18:29:11 -0700

    Fixed parsing problem with Trigg County, KY

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/KY/KYTriggCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/KY/KYTriggCountyParser.java
@@ -1,18 +1,35 @@
 package net.anei.cadpage.parsers.KY;
 
-import net.anei.cadpage.parsers.dispatch.DispatchGeoconxParser;
+import net.anei.cadpage.parsers.dispatch.DispatchA65Parser;
 
 /**
  * Trigg County, KY
  */
-public class KYTriggCountyParser extends DispatchGeoconxParser {
+public class KYTriggCountyParser extends DispatchA65Parser {
   
   public KYTriggCountyParser() {
-    super("TRIGG COUNTY", "KY");
+    super(CITY_LIST, "TRIGG COUNTY", "KY");
   }
   
   @Override
   public String getFilter() {
-    return "dispatch@911comm2.info,dispatch@911email.org";
+    return "dispatch@911comm2.info";
   }
+  
+  private static final String[] CITY_LIST = new String[]{
+      
+      // City
+      "CADIZ",
+
+      // Census-designated place
+      "CERULEAN",
+
+      // Other unincorporated places
+      "CALEDONIA",
+      "CANTON",
+      "LINTON",
+      "ROARING SPRING",
+      "ROCKCASTLE",
+      "WALLONIA"
+  };
 }
