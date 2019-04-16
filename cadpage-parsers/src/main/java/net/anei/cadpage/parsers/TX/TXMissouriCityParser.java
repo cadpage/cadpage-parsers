c0356d3adaf02f250dfc6ef971426256dc413358
Ken Corbin <ken@cadpage.org>
2019-04-15 18:22:52 -0700

    Fixed parsing problem with Missouri City, TX

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/TX/TXMissouriCityParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/TX/TXMissouriCityParser.java
@@ -10,13 +10,19 @@ import net.anei.cadpage.parsers.dispatch.DispatchOSSIParser;
 public class TXMissouriCityParser extends DispatchOSSIParser {
   public TXMissouriCityParser() {
     super("MISSOURI CITY", "TX",
-        "FYI? CALL ADDR INFO! INFO+");
+        "FYI? CALL ADDR UNIT? INFO! INFO/N+");
   }
   
   
   @Override
   public SplitMsgOptions getActive911SplitMsgOptions() {
     return new SplitMsgOptionsCustom();
+  }
+  
+  @Override
+  public Field getField(String name) {
+    if (name.equals("UNIT")) return new UnitField("[A-Z]+\\d+|\\S+,\\S+", true);
+    return super.getField(name);
   }
 
 
