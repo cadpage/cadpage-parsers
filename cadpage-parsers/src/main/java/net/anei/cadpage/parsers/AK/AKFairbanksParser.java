d90727d321294bbdef5c181aa3ad67f5eb778172
Ken Corbin <ken@cadpage.org>
2019-05-06 16:13:46 -0700

    Fixed parsing problem with Fairbanks, AK

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/AK/AKFairbanksParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/AK/AKFairbanksParser.java
@@ -14,20 +14,29 @@ public class AKFairbanksParser extends DispatchOSSIParser {
   
   public AKFairbanksParser() {
     super("FAIRBANKS", "AK",
-           "FYI? MUTAID? ADDR CALL/SDS! CH? BOX INFO+");
+          "( CANCEL ADDR CITYN! INFO/N " +
+          "| FYI? MUTAID? ADDR CALL/SDS! CH? BOX? UNIT END )");
   }
   
   @Override
   public String getFilter() {
-    return "CAD@ci.fairbanks.ak.us";
+    return "CAD@ci.fairbanks.ak.us,CAD@fairbanksalaska.us";
   }
   
   @Override
+  protected boolean parseMsg(String body, Data data) {
+    int pt = body.indexOf("\nCAUTION:");
+    if (pt >= 0) body = body.substring(0, pt).trim();
+    return super.parseMsg(body, data);
+  }
+
+  @Override
   public Field getField(String name) {
+    if (name.equals("CITYN")) return new SkipField("[A-Z]{4}", true);
     if (name.equals("MUTAID")) return new CallField(".*\\b(?:MUTUAL AID|AUTO AID)\\b.*", true);
     if (name.equals("CALL")) return new MyCallField();
     if (name.equals("CH")) return new ChannelField("TAC\\d+", true);
-    if (name.equals("BOX")) return new UnitField("\\d{3}", true);
+    if (name.equals("BOX")) return new BoxField("\\d{2,3}", true);
     return super.getField(name);
   }
   
