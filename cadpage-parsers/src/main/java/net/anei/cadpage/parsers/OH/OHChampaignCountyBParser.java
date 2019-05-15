b601bd0ce9a4f4b5064470ebb99dc8dd5ac4fa84
Ken Corbin <ken@cadpage.org>
2019-05-15 16:00:34 -0700

    Fixed parsing problem with Perry County, OH

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/OH/OHChampaignCountyBParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/OH/OHChampaignCountyBParser.java
@@ -1,38 +1,16 @@
 package net.anei.cadpage.parsers.OH;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
-import net.anei.cadpage.parsers.FieldProgramParser;
-import net.anei.cadpage.parsers.MsgInfo.Data;
+import net.anei.cadpage.parsers.dispatch.DispatchA75Parser;
 
 
-public class OHChampaignCountyBParser extends FieldProgramParser {
+public class OHChampaignCountyBParser extends DispatchA75Parser {
 
-  private static final Pattern SUBJECT_PTN = Pattern.compile("CFS: *\\d\\d-\\d{6}");
   public OHChampaignCountyBParser() {
-    super("CHAMPAIGN COUNTY", "OH",
-           "CFS:ID! RUN#:SKIP! CODE:CODE! PLACE:PLACE! LAT:GPS1! LONG:GPS2! ADDR:ADDR! CITY:CITY! CROSS:X! PRI:PRI! DATE:DATE! TIME:TIME! SECT:MAP! DIST:SRC! UNIT:UNIT! DESC:INFO/N+");
+    super("CHAMPAIGN COUNTY", "OH");
   }
   
   @Override
   public String getFilter() {
     return "lcso911@co.logan.oh.us,champaign911@co.champaign.oh.us";
-  }
-  
-  @Override
-  public boolean parseMsg(String subject, String body, Data data) {
-    Matcher match = SUBJECT_PTN.matcher(subject);
-    if (!match.matches()) return false;
-    int pt = body.indexOf("\n\n\n");
-    if (pt >= 0) body = body.substring(0,pt).trim();
-    return parseFields(body.split("\n"), data);
-  }
-  
-  @Override
-  public Field getField(String name) {
-    if (name.equals("DATE")) return new DateTimeField("\\d\\d?/\\d\\d?/\\d{4}", true);
-    if (name.equals("TIME")) return new DateTimeField("\\d\\d:\\d\\d:\\d\\d", true);
-    return super.getField(name);
   }
 }
--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/OH/OHPerryCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/OH/OHPerryCountyParser.java
@@ -2,25 +2,17 @@ package net.anei.cadpage.parsers.OH;
 
 import java.util.regex.Pattern;
 
-import net.anei.cadpage.parsers.SplitMsgOptions;
-import net.anei.cadpage.parsers.SplitMsgOptionsCustom;
-import net.anei.cadpage.parsers.dispatch.DispatchEmergitechParser;
+import net.anei.cadpage.parsers.dispatch.DispatchA75Parser;
 
-public class OHPerryCountyParser extends DispatchEmergitechParser {
+public class OHPerryCountyParser extends DispatchA75Parser {
 
   public OHPerryCountyParser() {
-    super("Perry911:", CITY_LIST, "PERRY COUNTY", "OH");
+    super("PERRY COUNTY", "OH");
   }
   
   @Override
   public String getFilter() {
-    return "@perrycountyohio.net";
-  }
-  
-  
-  @Override
-  public SplitMsgOptions getActive911SplitMsgOptions() {
-    return new SplitMsgOptionsCustom();
+    return "PerryCountyDispatch@PerryCountyOhio.Net";
   }
 
   @Override
@@ -29,115 +21,4 @@ public class OHPerryCountyParser extends DispatchEmergitechParser {
     return super.adjustMapAddress(addr);
   }
   private static final Pattern TWP_RD_PTN = Pattern.compile("\\b(?:MONDAY CREEK|SALT LICK|[A-Z]+) TWP RD\\b");
-  
-  private static final String[] CITY_LIST = new String[] { 
-    
-   //villages
-    "CORNING",
-    "CROOKSVILLE",
-    "GLENFORD",
-    "HEMLOCK",
-    "JUNCTION CITY",
-    "NEW LEXINGTON",
-    "NEW STRAITSVILLE",
-    "RENDVILLE",
-    "ROSEVILLE",
-    "SHAWNEE",
-    "SOMERSET",
-    "THORNVILLE",
-
-    //townships
-    "BEARFIELD TWP",
-    "CLAYTON TWP",
-    "COAL TWP",
-    "HARRISON TWP",
-    "HOPEWELL TWP",
-    "JACKSON TWP",
-    "MADISON TWP",
-    "MONDAY CREEK TWP",
-    "MONDAY CREEK",
-    "MONROE TWP",
-    "PIKE TWP",
-    "PLEASANT TWP",
-    "READING TWP",
-    "SALT LICK TWP",
-    "THORN TWP",
-   
-    //other communities
-    "BRISTOL",
-    "GLASS ROCK",
-    "MCLUNEY",
-    "MILLIGAN",
-    "MOUNT PERRY",
-    "MOXAHALA",
-    "PORTERSVILLE",
-    "REHOBOTH",
-    "WHIPSTOWN",
-    
-    //ghost town
-    "SAN TOY",
-    
-    // Athens Counthy
-    "AMES TWP",
-    "CHAUNCEY TWP",
-    "TRIMBLE TWP",
-    "YORK TWP",
-    "AMESVILLE",
-    "DOVER",
-    "JACKSONVILLE",
-    "GLOUSTER",
-    "NELSONVILLE",
-    "TRIMBLE",
-    
-    // Fairfield County
-    "PLEASANT TWP",
-    "RICHLAND TWP",
-    "RUSH CREEK TWP",
-    "WALNUT TWP",
-    "BUCKEYE LAKE",
-    "BREMEN",
-    "LANCASTER",
-    "MILLERSPORT",
-    "PLEASANTVILLE",
-    "RUSHVILLE",
-    "THURSTON",
-    "WEST RUSHVILLE",
-    
-    // Hocking County
-    "FALLS TWP",
-    "GREEN TWP",
-    "MARION TWP",
-    "WARD TWP",
-    "BUCHTEL",
-    "FALLS",
-    "LOGAN",
-    "MURRAY CITY",
-    
-    // Licking County
-    "BOWLING GREEN",
-    "FRANKLIN TWP",
-    "HOPEWELL TWP",
-    "LICKING TWP",
-    "UNION TWP",
-    "GRATIOT",
-    "HARBOR HILLS",
-    "HEBRON",
-    
-    // Muskingum County
-    "BRUSH CREEK TWP",
-    "CLAY TWP",
-    "FALLS TWP",
-    "HOPEWELL TWP",
-    "NEWTON TWP",
-    "SPRINGFIELD TWP",
-    "SOUTH ZANESVILLE",
-    "ROSEVILLE",
-    "ZANESVILLE",
-    
-    // Morgan County
-    "DEERFIELD TWP",
-    "HOMER TWP",
-    "UNION TWP",
-    "YORK TWP"
-  };
 }
