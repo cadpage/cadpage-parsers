46f8130dedf9f2ae8129194dd1d3bca2b1593323
Ken Corbin <ken@cadpage.org>
2019-05-19 10:01:32 -0700

    Fixed mapping problem with Callaway County, MO

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/MO/MOCallawayCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/MO/MOCallawayCountyParser.java
@@ -1,8 +1,8 @@
 package net.anei.cadpage.parsers.MO;
 
+import java.util.regex.Pattern;
+
 import net.anei.cadpage.parsers.dispatch.DispatchA25Parser;
-
-
 
 public class MOCallawayCountyParser extends DispatchA25Parser {
  
@@ -14,4 +14,11 @@ public class MOCallawayCountyParser extends DispatchA25Parser {
   public String getFilter() {
     return "EnterpolAlerts@cceoc.org";
   }
+  
+  @Override
+  public String adjustMapAddress(String addr) {
+    addr = BUS_54_PTN.matcher(addr).replaceAll("US 54 BUS");
+    return addr;
+  }
+  private static final Pattern BUS_54_PTN = Pattern.compile("\\bBUS 54\\b", Pattern.CASE_INSENSITIVE);
 }
