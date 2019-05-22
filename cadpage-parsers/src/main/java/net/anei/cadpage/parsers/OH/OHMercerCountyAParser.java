f4fffb6642645d50d09adb8ab0d4d4744dc0e3b3
Ken Corbin <ken@cadpage.org>
2019-05-21 20:46:11 -0700

    Fixed parsing problem with Mercer County, OH (A)

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/OH/OHMercerCountyAParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/OH/OHMercerCountyAParser.java
@@ -1,119 +1,77 @@
 package net.anei.cadpage.parsers.OH;
 
+import java.util.Properties;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import net.anei.cadpage.parsers.FieldProgramParser;
 import net.anei.cadpage.parsers.MsgInfo.Data;
-import net.anei.cadpage.parsers.SplitMsgOptions;
-import net.anei.cadpage.parsers.SplitMsgOptionsCustom;
-import net.anei.cadpage.parsers.dispatch.DispatchEmergitechParser;
 
-
-
-public class OHMercerCountyAParser extends DispatchEmergitechParser {
+public class OHMercerCountyAParser extends FieldProgramParser {
   
   public OHMercerCountyAParser() {
-    super(true, CITY_LIST, "MERCER COUNTY", "OH");
-    setupSpecialStreets("ERASTUS DURBIN");
-  }
-  
-  @Override
-  public SplitMsgOptions getActive911SplitMsgOptions() {
-    return new SplitMsgOptionsCustom();
+    super("MERCER COUNTY", "OH", 
+          "SRC CALL STATUS ADDRCITY UNIT BOX! INFO/N+");
   }
 
-  private static final Pattern DIR_OF_PTN = Pattern.compile("\\b([NSEW])/OF\\b");
   @Override
   protected boolean parseMsg(String body, Data data) {
-    body = body.replace(" BTWN ", " BETWEEN ");
-    body = DIR_OF_PTN.matcher(body).replaceAll("$1O");
-    if (!super.parseMsg(body, data)) return false;
-    data.strCode = data.strCall;
-    data.strCall = convertCodes(data.strCode, OHMercerCountyParser.CALL_CODES);
-    data.strCross = data.strCross.replace(" BETWEEN: ", " BTWN ");
-    data.strSupp = data.strSupp.replace(" BETWEEN: ", " BTWN ");
-    return true;
+    return parseFields(body.split("\n"), data);
   }
   
   @Override
-  public String getProgram() {
-    return super.getProgram().replace("CALL", "CODE CALL");
-  }
-
-  @Override
-  protected Result parseAddress(StartType sType, int flags, String address) {
-    if (sType == StartType.START_PLACE) sType = StartType.START_ADDR;
-    return super.parseAddress(sType, flags, address);
-  }
-
-  @Override
-  public String adjustMapAddress(String addr) {
-    return OHMercerCountyParser.adjustMapAddr(addr);
+  public Field getField(String name) {
+    if (name.equals("SRC")) return new SourceField("[A-Z]{1,4}", true);
+    if (name.equals("STATUS")) return new SkipField("DIS|ENR");
+    if (name.equals("ADDRCITY")) return new MyAddressCityField();
+    if (name.equals("UNIT")) return new UnitField("[A-Z]{3,4}\\d?|SQ\\d{1,2}", true);
+    if (name.equals("BOX")) return new BoxField("\\d{4}");
+    return super.getField(name);
   }
   
-  private static final String[] CITY_LIST = new String[]{
-
-    // Cities
-    "CELINA",
-
-    // Villages
-    "BURKETTSVILLE",
-    "CHICKASAW",
-    "COLDWATER",
-    "FORT RECOVERY",
-    "MENDON",
-    "MONTEZUMA",
-    "ROCKFORD",
-    "ST HENRY",
-
-    // Townships
-    "BLACK CREEK",
-    "BUTLER",
-    "CENTER",
-    "DUBLIN",
-    "FRANKLIN",
-    "GIBSON",
-    "GRANVILLE",
-    "HOPEWELL",
-    "JEFFERSON",
-    "LIBERTY",
-    "MARION",
-    "RECOVERY",
-    "UNION",
-    "WASHINGTON",
-
-    // Unincorporated communities
-    "CARTHAGENA",
-    "CASSELLA",
-    "CHATTANOOGA",
-    "CRANBERRY PRAIRIE",
-    "DURBIN",
-    "ERASTUS",
-    "HINTON",
-    "MACEDON",
-    "MARIA STEIN",
-    "MERCER",
-    "NEPTUNE",
-    "PADUA",
-    "PHILOTHEA",
-    "SEBASTIAN",
-    "SHARPSBURG",
-    "SHIVELY",
-    "SKEELS CROSSING",
-    "SKEELS CROSSROADS",
-    "ST JOSEPH",
-    "ST PETER",
-    "ST ROSE",
-    "TAMA",
-    "WABASH",
-    "WENDELIN",
+  private static final Pattern APT_PTN = Pattern.compile("APT +(.*)|\\d{1,4}[A-Z]?|[A-Z]");
+  private class MyAddressCityField extends AddressCityField {
+    @Override
+    public void parse(String field, Data data) {
+      int pt = field.lastIndexOf(',');
+      if (pt >= 0) {
+        data.strCity = convertCodes(field.substring(pt+1).trim(), CITY_CODES);
+        field = field.substring(0, pt).trim();
+      }
+      for (String part : field.split(";")) {
+        part = part.trim();
+        if (part.length() == 0) continue;
+        if (data.strAddress.length() == 0) {
+          parseAddress(field, data);
+        } else {
+          Matcher match = APT_PTN.matcher(part);
+          if (match.matches()) {
+            String apt = match.group(1);
+            if (apt == null) apt = part;
+            if (!data.strApt.contains(apt)) {
+              data.strApt = append(data.strApt, "-", apt);
+            }
+          } else {
+            data.strPlace = append(data.strPlace, " - ", part);
+          }
+        }
+      }
+    }
     
-    // Alaglaize County
-    "ST MARYS",
-    
-    // Miami County
-    "TROY",
-    
-    // Wert County
-    "VENEDOCIA"
-  };
+    @Override
+    public String getFieldNames() {
+      return "ADDR APT PLACE CITY";
+    }
+  }
+  
+  private static final Properties CITY_CODES = buildCodeTable(new String[]{
+      "CEL",  "CELINA",
+      "CW",   "CHICKASAW",
+      "FTR",  "FORT RECOVERY",
+      "MAR",  "MARION TWP",
+      "MEN",  "MENDON",
+      "ROC",  "ROCKFORD",
+      "SPE",  "SPENCERVILLE",
+      "STH",  "ST HENRY"
+  });
 }
