cc707a52c462b528ad04461c500b27a2aee2c21a
Ken Corbin <ken@cadpage.org>
2019-05-25 15:11:30 -0700

    Fixed parsing problem with Prince George, BC

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/ZCABC/ZCABCPrinceGeorgeParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/ZCABC/ZCABCPrinceGeorgeParser.java
@@ -9,12 +9,17 @@ import net.anei.cadpage.parsers.MsgInfo.Data;
 public class ZCABCPrinceGeorgeParser extends FieldProgramParser { 
   public ZCABCPrinceGeorgeParser() {
     super(CITY_LIST,  "PRINCE GEORGE", "BC",
-          "Date:DATETIME! Type:CALL! Address:ADDRCITY/S! Latitude:LAT Longitude:LONG Units_Responding:UNIT");
+          "Date:DATETIME! Dept:SRC Type:CALL! Address:ADDRCITY/S! Unit:APT Suite:APT 1st_Cross_Street:X 2nd_Cross_Street:X Building:PLACE Common_Place_Name:PLACE INFO/N+ Latitude:GPS1 Longitude:GPS2 Google_Maps_Link:SKIP Units_Responding:UNIT");
   }
   
   @Override
   public String getFilter() {
     return "donotreply@princegeorge.ca,donotreply@city.pg.bc.ca";
+  }
+  
+  @Override
+  public int getMapFlags() {
+    return MAP_FLG_PREFER_GPS;
   }
  
   @Override 
@@ -26,10 +31,30 @@ public class ZCABCPrinceGeorgeParser extends FieldProgramParser {
   @Override
   public Field getField(String name) {
     if (name.equals("DATETIME")) return new MyDateTime();
+    if (name.equals("CALL")) return new MyCallField();
     if (name.equals("ADDRCITY")) return new MyAddressCityField();
-    if (name.equals("LAT")) return new MyLatitudeField();
-    if (name.equals("LONG")) return new MyLongitudeField();
+    if (name.equals("APT")) return new MyAptField();
+    if (name.equals("X")) return new MyCrossField();
+    if (name.equals("INFO")) return new MyInfoField();
     return super.getField(name);
+  }
+  
+  private static final Pattern CODE_CALL_PTN = Pattern.compile("([A-Z0-9]+) +- +(.*)");
+  private class MyCallField extends Field {
+    @Override
+    public void parse(String field, Data data) {
+      Matcher m = CODE_CALL_PTN.matcher(field);
+      if (m.matches()) {
+        data.strCode = m.group(1);
+        field = m.group(2);
+      }
+      data.strCall = field;
+    }
+
+    @Override
+    public String getFieldNames() {
+      return "CODE CALL";
+    }
   }
   
   private static final Pattern DATE_TIME_PTN = Pattern.compile("(\\d{4})-(\\d\\d)-(\\d\\d) (\\d\\d:\\d\\d:\\d\\d)");
@@ -44,21 +69,15 @@ public class ZCABCPrinceGeorgeParser extends FieldProgramParser {
   }
   
   private static final Pattern INITIAL_APT_PATTERN
-    = Pattern.compile("#(\\d+) *- *(.*)");
-  private static final Pattern DRAGON_LAKE_PATTERN
-    = Pattern.compile("(.*?), *RED +BLUFF *\\/ *DRAGON +LAKE(?:, *BC)?");
+    = Pattern.compile("# *(\\S+?) *- *(.*)");
   private class MyAddressCityField extends AddressCityField {
     @Override
     public void parse(String field, Data data) {
       Matcher m = INITIAL_APT_PATTERN.matcher(field);
       if (m.matches()) {
-        data.strApt = m.group(1);
+        String apt = m.group(1);
+        if (!apt.equals("0")) data.strApt = m.group(1);
         field = m.group(2);
-      }
-      m = DRAGON_LAKE_PATTERN.matcher(field);
-      if (m.matches()) {
-        data.strPlace = append(data.strPlace, " - ", "RED BLUFF / DRAGON LAKE");
-        field = m.group(1)+", QUESNEL";
       }
       super.parse(field.replace(", BC", ""), data);
     }
@@ -69,18 +88,39 @@ public class ZCABCPrinceGeorgeParser extends FieldProgramParser {
     }
   }
   
-  private class MyLatitudeField extends GPSField {
+  private class MyAptField extends AptField {
     @Override
     public void parse(String field, Data data) {
-      data.strGPSLoc = field;
+      if (field.equals("0")) return;
+      super.parse(field, data);
     }
   }
   
-  private class MyLongitudeField extends GPSField {
+  private class MyCrossField extends CrossField {
     @Override
     public void parse(String field, Data data) {
-      super.parse(append(data.strGPSLoc, ",", field), data);
+      int pt = field.indexOf(',');
+      if (pt >= 0) {
+        if (data.strCity.length() == 0) data.strCity = field.substring(pt+1).trim();
+        field = field.substring(0, pt).trim();
+      }
+      super.parse(field, data);
     }
+  }
+  
+  private class MyInfoField extends InfoField {
+    @Override
+    public void parse(String field, Data data) {
+      if (field.equals("Pre-Incident Plan:")) return;
+      super.parse(field, data);
+    }
+  }
+  
+  @Override
+  public String adjustMapCity(String city) {
+    int pt = city.indexOf('/');
+    if (pt >= 0) city = city.substring(0, pt).trim();
+    return city;
   }
   
   private static final String[] CITY_LIST = {
