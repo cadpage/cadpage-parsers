d31e6a01c6d57b18984cd6bb36677d7d9465e9f2
Ken Corbin <ken@cadpage.org>
2019-05-25 13:11:37 -0700

    Fixed parsing problem with ANdrew County, MO (Add B)

---
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/MO/MOAndrewCountyAParser.java
@@ -0,0 +1,15 @@
+package net.anei.cadpage.parsers.MO;
+
+import net.anei.cadpage.parsers.dispatch.DispatchA33Parser;
+
+public class MOAndrewCountyAParser extends DispatchA33Parser {
+  
+  public MOAndrewCountyAParser() {
+    super("ANDREW COUNTY", "MO");
+  }
+  
+  @Override
+  public String getFilter() {
+    return "DISPATCH@ANDREWCOUNTY.COM";
+  }
+}
---
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/MO/MOAndrewCountyBParser.java
@@ -0,0 +1,15 @@
+package net.anei.cadpage.parsers.MO;
+
+import net.anei.cadpage.parsers.dispatch.DispatchBCParser;
+
+public class MOAndrewCountyBParser extends DispatchBCParser {
+  
+  public MOAndrewCountyBParser() {
+    super("ANDREW COUNTY", "MO");
+  }
+  
+  @Override
+  public String getFilter() {
+    return "DISPATCH@ANDREWCOUNTY.COM";
+  }
+}
