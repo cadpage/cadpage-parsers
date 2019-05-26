d7ef51db901f635433c14ba0b94adea49fcba8dd
Ken Corbin <ken@cadpage.org>
2019-05-26 10:16:42 -0700

    new location: Lunenburg County, VA (B)

---
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/VA/VALunenburgCountyBParser.java
@@ -0,0 +1,68 @@
+package net.anei.cadpage.parsers.VA;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import net.anei.cadpage.parsers.FieldProgramParser;
+import net.anei.cadpage.parsers.MsgInfo.Data;
+
+public class VALunenburgCountyBParser extends FieldProgramParser {
+  
+  public VALunenburgCountyBParser() {
+    super("LUNENBURG COUNTY", "VA", 
+          "UNIT CALL ADDRCITY ID DATETIME! END");
+  }
+  
+  @Override
+  public String getFilter() {
+    return "LunenburgCAD@lunenburg911.com";
+  }
+  
+  @Override
+  protected boolean parseMsg(String body, Data data) {
+    return parseFields(body.split(" \\| "), data);
+  }
+  
+  @Override
+  public Field getField(String name) {
+    if (name.equals("UNIT")) return new UnitField("[A-Z0-9]+", true);
+    if (name.equals("ADDRCITY"))  return new MyAddressCityField();
+    if (name.equals("DATETIME")) return new MyDateTimeField();
+    return super.getField(name);
+  }
+  
+  private static final Pattern ADDR_CITY_ST_PTN = Pattern.compile("(.*), ([A-Z]{2})(?: +(\\d{5}))?");
+  private class MyAddressCityField extends AddressCityField {
+    @Override
+    public void parse(String field, Data data) {
+      String zip = null;
+      Matcher match = ADDR_CITY_ST_PTN.matcher(field);
+      if (match.matches()) {
+        field = match.group(1);
+        data.strState = match.group(2);
+        zip = match.group(3);
+      }
+      super.parse(field, data);
+      if (zip != null && data.strCity.length() == 0) data.strCity = zip;
+    }
+    
+    @Override
+    public String getFieldNames() {
+      return "ADDR APT CITY ST";
+    }
+  }
+
+  private static final Pattern DATE_TIME_PTN = Pattern.compile("(\\d\\d?/\\d\\d?/\\d{4}) +(\\d\\d?:\\d\\d:\\d\\d [AP]M)");
+  private static final DateFormat TIME_FMT = new SimpleDateFormat("hh:mm:ss aa");
+  private class MyDateTimeField extends DateTimeField {
+    @Override
+    public void parse(String field, Data data) {
+      Matcher match = DATE_TIME_PTN.matcher(field);
+      if (!match.matches()) abort();
+      data.strDate = match.group(1);
+      setTime(TIME_FMT, match.group(2), data);
+    }
+  }
+}
--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/VA/VALunenburgCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/VA/VALunenburgCountyParser.java
@@ -1,74 +1,13 @@
 package net.anei.cadpage.parsers.VA;
 
-import java.util.regex.Pattern;
+import net.anei.cadpage.parsers.GroupBestParser;
 
-import net.anei.cadpage.parsers.CodeSet;
-import net.anei.cadpage.parsers.MsgInfo.Data;
-import net.anei.cadpage.parsers.dispatch.DispatchDAPROParser;
-
-
-
-public class VALunenburgCountyParser extends DispatchDAPROParser {
+/**
+ * Lunenburg County, VA
+ */
+public class VALunenburgCountyParser extends GroupBestParser {
   
   public VALunenburgCountyParser() {
-    super("LUNENBURG COUNTY", "VA");
-    setupCallList(CALL_SET);
+    super(new VALunenburgCountyAParser(), new VALunenburgCountyBParser());
   }
-  
-  @Override
-  public String getFilter() {
-    return "MAILBOX@rushpost.com";
-  }
-  
-  @Override
-  public boolean parseMsg(String subject, String body, Data data) {
-    if (!super.parseMsg(subject, body, data)) return false;
-    data.strAddress = TRAILER_PK_PTN.matcher(data.strAddress).replaceAll("TRAILER PARK RD");
-    if (data.strApt.startsWith("APTS")) {
-      data.strAddress = append(data.strAddress, " ", "APTS");
-      data.strApt = data.strApt.substring(4).trim();
-    }
-    return true;
-  }
-  private static final Pattern TRAILER_PK_PTN = Pattern.compile("\\bTRAILER PK(?: RD)?\\b", Pattern.CASE_INSENSITIVE);
-
-  private static final CodeSet CALL_SET = new CodeSet(
-      "ABDOMINAL PAIN",
-      "ACCIDENT",
-      "ALARM",
-      "ALERGIC REACT",
-      "ALLERGIC REACT",
-      "ARGUEMENT",
-      "ARGUMENT",
-      "ASSIST",
-      "ASTHMA",
-      "BACK PAIN",
-      "BLEEDING",
-      "BRUSH FIRE",
-      "CHEST PAINS",
-      "DIABETIC",
-      "DIVERSION CMHER - ICU DIVERSION",
-      "DIFF BREATHING",
-      "DOG BITE",
-      "DUMPSTER FIRE",
-      "DOA",
-      "FALLEN",
-      "FIRE",
-      "FIRE ALARM",
-      "HIGH BLOOD PRESS",
-      "MUTUAL AID",
-      "OTHER",
-      "OTHER TRAINING",
-      "PASSED OUT",
-      "PREGNANCY",
-      "PUBLIC SERVICE",
-      "RESCUE",
-      "SEIZURE",
-      "SICK",
-      "SMOKE IN BUILD",
-      "STAND BY",
-      "STROKE",
-      "TREE IN ROAD",
-      "UNRESPONSIVE"
-  );
 }
