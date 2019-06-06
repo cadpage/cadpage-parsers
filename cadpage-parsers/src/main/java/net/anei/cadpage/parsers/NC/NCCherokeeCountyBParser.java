db75a22e3ed04733961e6a14fc65436f181fd313
Ken Corbin <ken@cadpage.org>
2019-06-06 06:50:10 -0700

    Fixed parsing problem with Cherokee County, NC (B)

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/NC/NCCherokeeCountyBParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/NC/NCCherokeeCountyBParser.java
@@ -10,7 +10,12 @@ public class NCCherokeeCountyBParser extends FieldProgramParser {
   
   public NCCherokeeCountyBParser() {
     super("CHEROKEE COUNTY", "NC", 
-          "CALL:CALL! PLACE:PLACE ADDR:ADDR! CITY:CITY! XY:GPS ID:ID! PRI:PRI! DATE:DATE! TIME:TIME! UNIT:UNIT INFO:X_INFO END");
+          "CALL:CALL! PLACE:PLACE ADDR:ADDR! CITY:CITY! XY:GPS ID:ID! PRI:PRI! DATE:DATE! TIME:TIME! UNIT:UNIT X:X INFO:X_INFO/N+");
+  }
+  
+  @Override
+  public String getFilter() {
+    return "Do-Not-Reply-Dispatch@cherokeecounty-nc.gov";
   }
   
   @Override
@@ -23,6 +28,7 @@ public class NCCherokeeCountyBParser extends FieldProgramParser {
     if (name.equals("ADDR")) return new MyAddressField();
     if (name.equals("DATE")) return new DateField("\\d\\d/\\d\\d/\\d{4}", true);
     if (name.equals("TIME")) return new TimeField("\\d\\d:\\d\\d:\\d\\d", true);
+    if (name.equals("X")) return new MyCrossField();
     if (name.equals("X_INFO")) return new MyCrossInfoField();
     return super.getField(name);
   }
@@ -35,10 +41,18 @@ public class NCCherokeeCountyBParser extends FieldProgramParser {
     }
   }
   
+  private class MyCrossField extends CrossField {
+    @Override
+    public void parse(String field, Data data) {
+      field = field.replace('@', '/');
+      super.parse(field, data);
+    }
+  }
+  
   private static Pattern CROSS_INFO_PTN1 = Pattern.compile("([A-Z0-9 ]*@ +(?:(?:I|ST|NC) \\d+|[A-Z0-9]+(?: [A-Z ]+?)? (?:AV|CT|DR|LN|PL|ST|TER))) *(.*)");
   private static Pattern CROSS_INFO_PTN2 = Pattern.compile("([A-Z0-9 ]*@ +(?:(?:I|ST|NC) \\d+|[A-Z0-9]+)) *(.*)");
   
-  private class MyCrossInfoField extends Field {
+  private class MyCrossInfoField extends InfoField {
 
     @Override
     public void parse(String field, Data data) {
@@ -46,17 +60,20 @@ public class NCCherokeeCountyBParser extends FieldProgramParser {
         field = field.substring(1).trim();
         field = field.replace(" (Verify) ", " ");
       }
-      Matcher match = CROSS_INFO_PTN1.matcher(field);
-      if (!match.matches()) {
-        match = CROSS_INFO_PTN2.matcher(field);
+      if (data.strCross.length() == 0) {
+        Matcher match = CROSS_INFO_PTN1.matcher(field);
         if (!match.matches()) {
-          data.strSupp = field;
-          return;
+          match = CROSS_INFO_PTN2.matcher(field);
+          if (!match.matches()) {
+            data.strSupp = field;
+            return;
+          }
         }
+        String cross = match.group(1).replace('@',  '/');
+        data.strCross = stripFieldStart(cross, "/");
+        field = match.group(2);
       }
-      String cross = match.group(1).replace('@',  '/');
-      data.strCross = stripFieldStart(cross, "/");
-      data.strSupp = match.group(2);
+      super.parse(field, data);
     }
 
     @Override
