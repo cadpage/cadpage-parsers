c6044fc39e1a4f31b58f61c3876199e0661f1417
Ken Corbin <ken@cadpage.org>
2019-06-17 17:07:24 -0700

    Fixed parsing problem with Caroline County, MD (Add B)

---
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/MD/MDCarolineCountyBParser.java
@@ -0,0 +1,65 @@
+package net.anei.cadpage.parsers.MD;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import net.anei.cadpage.parsers.FieldProgramParser;
+import net.anei.cadpage.parsers.MsgInfo.Data;
+
+public class MDCarolineCountyBParser extends FieldProgramParser {
+  
+  public MDCarolineCountyBParser() {
+    super("CAROLINE COUNTY", "MD", 
+          "Call_Type:CALL! Call_Date/Time:DATETIME! Call_Location:ADDRCITY! Cross_Streets:X! Common_Name:PLACE! Box_Area:BOX! Radio_Channel:CH! Map:MAP! https:SKIP! Incident_Number:ID! Units:UNIT! END");
+  }
+  
+  @Override
+  public String getFilter() {
+    return "carolinemfp@carolinemd.org";
+  }
+  
+  @Override
+  protected boolean parseMsg(String body, Data data) {
+    return parseFields(body.split("\n"), data);
+  }
+  
+  @Override
+  public Field getField(String name) {
+    if (name.equals("DATETIME")) return new DateTimeField("\\d\\d?/\\d\\d?/\\d{4} +\\d\\d:\\d\\d:\\d\\d", true);
+    if (name.equals("ADDRCITY")) return new MyAddressCityField();
+    if (name.equals("X")) return new MyCrossField();
+    return super.getField(name);
+  }
+  
+  private static final Pattern ADDR_APT_PTN = Pattern.compile("(.*?) +(\\d{1,4}[A-Z]?|[A-Z])");
+  private class MyAddressCityField extends AddressCityField {
+    @Override
+    public void parse(String field, Data data) {
+      if (field.contains(",")) {
+        String apt = "";
+        Matcher match = ADDR_APT_PTN.matcher(field);
+        if (match.matches()) {
+          field = match.group(1);
+          apt = match.group(2);
+        }
+        super.parse(field, data);
+        data.strApt = append(data.strApt, "-", apt);
+      } else {
+        parseAddress(StartType.START_ADDR, FLAG_RECHECK_APT | FLAG_ANCHOR_END, field, data);
+      }
+    }
+    
+    @Override
+    public String getFieldNames() {
+      return "ADDR CITY APT";
+    }
+  }
+  
+  private class MyCrossField extends CrossField {
+    @Override
+    public void parse(String field, Data data) {
+      if (field.equals("No Cross Streets Found")) return;
+      super.parse(field, data);
+    }
+  }
+}
--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/MD/MDCarolineCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/MD/MDCarolineCountyParser.java
@@ -1,51 +1,13 @@
 package net.anei.cadpage.parsers.MD;
 
-import net.anei.cadpage.parsers.dispatch.DispatchChiefPagingParser;
+import net.anei.cadpage.parsers.GroupBestParser;
 
-public class MDCarolineCountyParser extends DispatchChiefPagingParser {
+
+public class MDCarolineCountyParser extends GroupBestParser {
   
   public MDCarolineCountyParser() {
-    super(CITY_LIST, "CAROLINE COUNTY", "MD");
+    super(new MDCarolineCountyAParser(),
+           new MDCarolineCountyBParser());
   }
   
-  private static final String[] CITY_LIST = new String[]{
-
-    // Cities
-    "DENTON",
-    "FEDERALSBURG",
-    "GOLDSBORO",
-    "GREENSBORO",
-    "HENDERSON",
-    "HILLSBORO",
-    "MARYDEL",
-    "PRESTON",
-    "RIDGELY",
-    "TEMPLEVILLE",
-
-    // Towns
-    "CHOPTANK",
-    "WEST DENTON",
-    "WILLISTON",
-
-    // Unincorporated Communities
-    "AMERICAN CORNER",
-    "ANDERSONTOWN",
-    "BALTIMORE CORNER",
-    "BETHLEHEM",
-    "BURRSVILLE",
-    "GILPIN POINT",
-    "GROVE",
-    "HARMONY",
-    "HICKMAN",
-    "HOBBS",
-    "JUMPTOWN",
-    "LINCHESTER",
-    "OAKLAND",
-    "OIL CITY",
-    "TANYARD",
-    "TWO JOHNS",
-    "RELIANCE",
-    "WHITELEYSBURG"
-    
-  };
 }
