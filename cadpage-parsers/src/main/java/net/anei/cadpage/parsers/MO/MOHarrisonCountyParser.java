f437d29c5e608de1f5ef152698ead53f486a0cbf
Ken Corbin <ken@cadpage.org>
2019-06-23 19:03:48 -0700

    Added GPS lookup table for Harrison County,  MO

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/MO/MOHarrisonCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/MO/MOHarrisonCountyParser.java
@@ -1,15 +1,23 @@
 package net.anei.cadpage.parsers.MO;
 
+import java.util.Properties;
+
 import net.anei.cadpage.parsers.dispatch.DispatchBCParser;
 
 
 public class MOHarrisonCountyParser extends DispatchBCParser {
   public MOHarrisonCountyParser() {
     super("HARRISON COUNTY", "MO");
+    setupGpsLookupTable(GPS_LOOKUP_TABLE);
   }
    
   @Override
   public String getFilter() {
     return "HARCO911@GRM.NET,HARRISON911@PUBLICSAFETYSOFTWARE.NET";
   }
+  
+  private static final Properties GPS_LOOKUP_TABLE = buildCodeTable(new String[]{
+      "24475 E 225TH AVE",                    "+40.360419,-93.996812",
+      "1011 MAIN ST",                         "+40.383097,-93.936801"
+  });
 }
