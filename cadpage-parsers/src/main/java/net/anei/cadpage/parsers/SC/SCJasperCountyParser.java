c49a0f32681aac2e35cb76de1af71a7e4f35429b
Ken Corbin <ken@cadpage.org>
2019-07-15 20:16:29 -0700

    Fixed parsing problem with Jasper County, SC

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/SC/SCJasperCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/SC/SCJasperCountyParser.java
@@ -1,19 +1,25 @@
 package net.anei.cadpage.parsers.SC;
 
-import net.anei.cadpage.parsers.dispatch.DispatchA9Parser;
-
+import net.anei.cadpage.parsers.dispatch.DispatchH05Parser;
 
 /**
  * Jasper County, SC
  */
-public class SCJasperCountyParser extends DispatchA9Parser {
+public class SCJasperCountyParser extends DispatchH05Parser {
       
   public SCJasperCountyParser() {
-    super(null,"JASPER COUNTY", "SC");
+    super("JASPER COUNTY", "SC",
+          "Respond_for:CALL! Radio_Channel_Assigned:CH! Respond_to:ADDRCITY! Lot/Appt/Room_Number:APT! Common_Name:PLACE! Cross_Streets:X! Additional_Location_Information:INFO! MAP! Caller_Name:NAME! Caller_Phone:PHONE! Call_Narrative:EMPTY! INFO_BLK/Z+? Incident_Number(s):ID! Units_Assigned:UNIT!");
   }
   
   @Override
   public String getFilter() {
-    return "ispatch@jaspercountysc.gov,dispatch@jaspercountysc.gov,";
+    return "@jasperscountysc.gov";
+  }
+  
+  @Override
+  public Field getField(String name) {
+    if (name.equals("MAP")) return new MapField("Map Page *(.*)", true);
+    return super.getField(name);
   }
 }
