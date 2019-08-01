162bae5dfb34909d7e1c07f32810ba23102fc6c5
Ken Corbin <ken@cadpage.org>
2019-07-31 21:06:53 -0700

    Fixed parsing problem with Bucombe County, NC

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/NC/NCBuncombeCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/NC/NCBuncombeCountyParser.java
@@ -12,16 +12,15 @@ import net.anei.cadpage.parsers.dispatch.DispatchOSSIParser;
 public class NCBuncombeCountyParser extends DispatchOSSIParser {
   
   public NCBuncombeCountyParser() {
-    super("BUNCOMBE COUNTY", "NC",
-           "FYI? CHANGE? ASSREQ/SDS? PREFIX/SDS? CALL/SDS? ADDR! SRC? UNIT? SPEC+");
+    super(CITY_LIST, "BUNCOMBE COUNTY", "NC",
+          "FYI? ( CANCEL ADDR UNIT PHONE END " +
+               "| CALL? ADDR! UNIT1? UNIT2? SPEC/N+ )");
   }
   
   @Override
   public String getFilter() {
     return "CAD@buncombecounty.org";
   }
-  
-  private boolean callFound;
   
   @Override
   public boolean parseMsg(String body, Data data) {
@@ -33,64 +32,24 @@ public class NCBuncombeCountyParser extends DispatchOSSIParser {
     // Sanity check - must have at least one semicolons
     if (!body.contains(";")) return false;
     
-    callFound = false;
+    if (!body.startsWith("CAD:")) body = "CAD:" + body;
+    
     if (! super.parseMsg(body, data)) return false;
     if (data.strCall.length() == 0) data.strCall = "Unknown";
+    if (data.strCity.endsWith(" EOC")) {
+      data.strCity = data.strCity.substring(0, data.strCity.length()-4) + " COUNTY";
+    }
     return true;
   }
   
   @Override
   protected Field getField(String name) {
-    if (name.equals("CHANGE")) return new MyChangeField();
-    if (name.equals("ASSREQ")) return new AssReqField();
     if (name.equals("PREFIX")) return new CallField("UNDER CONTROL|Working fire", true);
     if (name.equals("CALL")) return new MyCallField();
-    if (name.equals("SRC")) return new SourceField("FS\\d+");
-    if (name.equals("UNIT")) return new UnitField("[A-Z]{1,5}[0-9]{1,3}[A-Z]?(?:,.*)?", true);
+    if (name.equals("UNIT1")) return new UnitField("[A-Z]{1,5}\\d+|", true);
+    if (name.equals("UNIT2")) return new MyUnitField("[A-Z]{1,5}[0-9]{1,4}[A-Z]?(?:,.*)?|", true);
     if (name.equals("SPEC")) return new SpecField();
     return super.getField(name);
-  }
-  
-  private static final Pattern CHANGED_PTN = Pattern.compile("Changed ([A-Za-z]+) from .*? to (.*)");
-  private class MyChangeField extends CallField {
-    
-    @Override
-    public boolean canFail() {
-      return true;
-    }
-    
-    @Override
-    public boolean checkParse(String field, Data data) {
-      Matcher match = CHANGED_PTN.matcher(field);
-      if (!match.matches()) return false;
-      String type = match.group(1);
-      String newValue = match.group(2).trim();
-      if (type.equals("Nature")) data.strCall = newValue;
-      return true;
-    }
-
-    @Override
-    public void parse(String field, Data data) {
-    }
-  }
-
-  /**
-   * Call field that only trips if it contains the words "ASSISTANCE REQUESTED"
-   */
-  private class AssReqField extends CallField {
-    
-    @Override
-    public boolean canFail() {
-      return true;
-    }
-
-    @Override
-    public boolean checkParse(String field, Data data) {
-      
-      if (!field.contains("ASSISTANCE REQUESTED")) return false;
-      parse(field, data);
-      return true;
-    }
   }
   
   private class MyCallField extends CallField {
@@ -110,33 +69,35 @@ public class NCBuncombeCountyParser extends DispatchOSSIParser {
     
     @Override
     public void parse(String field, Data data) {
-      callFound = true;
       super.parse(field, data);
+    }
+  }
+  
+  private class MyUnitField extends UnitField {
+    
+    public MyUnitField(String pattern, boolean hard) {
+      super(pattern, hard);
+    }
+    
+    @Override
+    public void parse(String field, Data data) {
+      if (field.length() == 0) return;
+      if (data.strUnit.length() > 0) {
+        if (Pattern.compile("\\b" + data.strUnit + "\\b").matcher(field).find()) data.strUnit = "";
+      }
+      data.strUnit = append(data.strUnit, ",", field);
     }
   }
   
   /**
    * Covers fields entered in random order between the Unit and cross street
    */
-  
-  // Field will be treated as a call description if it contains
-  // A something in parenthesis
-  // Any of a set of words generally found in call descriptions
+
+  private static final Pattern CALL_PFX_PTN = Pattern.compile("\\((?:FR|Z)\\) *(.*)");
   private static final Pattern CALL_PATTERN = 
-    Pattern.compile("^\\(.+\\)|^\\{.+\\}|\\b(?:ASSAULT|ASSIST|CARDIAC|CHOKING|CONVULSIONS|DAMAGE|EMS|FALL|FALLS|FIRE|HEART|HEMORRHAGE|INJURY|INJURIES|PAIN|PAINS|PROBLEM|PROBLEMS|SEIZURES|SICK|SMOKE|STROKE|TRANSPORT|UNCONSCIOUS|WORKING)\\b", Pattern.CASE_INSENSITIVE);
-  private class SpecField extends Field {
-    
-    private Field callField = new CallField();
-    private Field nameField = new NameField();
-    private Field phoneField = new PhoneField();
-    private Field idField = new IdField();
-    private Field crossField = new CrossField();
-    private Field channelField = new ChannelField();
-    private Field infoField = new InfoField();
-    
-    public SpecField() {
-      callField.setQual("SDS");
-    }
+    Pattern.compile("\\b(?:ALARM|ALLERGIES|ANIMAL BITES|ASSAULT|ASSIST|ASST|ASSISTANCE|BLOCKING|BURNS?|CARDIAC|CHECK|CHECKED P/T|CHOKING|CONVULSIONS|DAMAGE|DETECTOR|DRONE|EMS|EXPOSURE|FALL|FALLS|FILL CASCADE|FIRE(?! DEPT| DEPARTMENT)|GAS|HEART|HELP|HEMORRHAGE|HIT|INJURY|INJURIES|LINES? DOWN|LOCKED IN|LOCKED OUT|MAN DOWN|MULTIPLE|MUTUAL AID|MVA|NO TONES|ODER|ODOR|OVERDOSE|OUT OF CONTROL|PAIN|PAINS|POISONING|POWER LINES?|PROBLEM|PROBLEMS|PUBLIC SERVICE|PULL STATION|REMOVAL|RESCUE|ROADWAY|SEARCH|SEIZURES|SHOCK|SICK|SMELL|SMOKE|SMOKING|STAND ?BY|STRIKE|STROKE|STUCK|SMOKING|SUICIDE|TEST CALL|TRANSFER|TRANSPORT|TREE DOWN|UNCONSCIOUS|WATER LINE|WORKING|WRECK)\\b", Pattern.CASE_INSENSITIVE);
+  private static final Pattern PHONE_DIST_PTN = Pattern.compile("(\\d{10}) DIST.*");
+  private class SpecField extends InfoField {
 
     @Override
     public void parse(String field, Data data) {
@@ -149,7 +110,6 @@ public class NCBuncombeCountyParser extends DispatchOSSIParser {
       if (upField.startsWith("SPRINT")) return;
       if (upField.startsWith("US CELLULAR")) return;
       if (upField.startsWith("AT&T MOBILITY")) return;
-      if (upField.contains("DIST:")) return;
       
       String nextFld = getRelativeField(+1);
       if (nextFld.startsWith("Radio Channel:")) {
@@ -157,43 +117,163 @@ public class NCBuncombeCountyParser extends DispatchOSSIParser {
         if (field.equals(nextFld)) return;
       }
       
-      Field fieldProc;
+      Matcher match;
       if (field.startsWith("Radio Channel:")) {
-        field = field.substring(14).trim();
-        fieldProc = channelField;
+        data.strChannel = field.substring(14).trim();
+      }
+      
+      else if (data.strCity.length() == 0 && isCity(field)) {
+        data.strCity = field;
       }
       
       else if (data.strName.length() == 0 && field.length() <= 35 && field.contains(",")) {
-        fieldProc = nameField;
+        data.strName = field;
       }
       
-      else if (data.strPhone.length() == 0 && field.length() == 10 && NUMERIC.matcher(field).matches()) {
-        fieldProc = phoneField;
+      else if (data.strPhone.length() == 0 && field.length() == 10 && NUMERIC.matcher(field).matches() && 
+              !field.startsWith("1") && ! field.contains("00")) {
+        data.strPhone = field;
       }
       
-      else if (data.strCallId.length() == 0 && field.length() == 8 && NUMERIC.matcher(field).matches()) {
-        fieldProc = idField;
+      else if (data.strCallId.length() == 0 && field.length() >= 8 && NUMERIC.matcher(field).matches()) { 
+        data.strCallId = field;
+      }
+      
+      else if ((match = PHONE_DIST_PTN.matcher(field)).matches()) {
+        data.strPhone = match.group(1);
       }
       
       else if (!field.startsWith("FM") && checkAddress(field) == STATUS_STREET_NAME) {
-        fieldProc = crossField;
+        data.strCross = append(data.strCross, " & ", field);
       }
       
-      else if (!callFound && CALL_PATTERN.matcher(field).find()) {
-        callFound = true;
-        fieldProc = callField;
+      else if ((match = CALL_PFX_PTN.matcher(field)).matches()) {
+        data.strCall = append(match.group(1), " - ", data.strCall);
       }
       
-      else {
-        fieldProc = infoField;
+      else if (data.strCall.length() == 0 && CALL_PATTERN.matcher(field).find()) {
+        data.strCall = field;
       }
       
-      fieldProc.parse(field, data);
+      else if (!upField.contains("DIST:")) {
+        super.parse(field, data);
+      }
     }
     
     @Override
     public String getFieldNames() {
-      return "CALL NAME PHONE INFO X ID CH";
+      return "CITY CALL NAME PHONE INFO X ID CH";
     }
   }
+  
+  private static final String[] CITY_LIST = new String[]{
+      
+      // Cities
+      "ASHEVILLE",
+
+      // Towns
+      "BILTMORE FOREST",
+      "BLACK MOUNTAIN",
+      "MONTREAT",
+      "WEAVERVILLE",
+      "WOODFIN",
+
+      // Townships
+      "ASHEVILLE",
+      "AVERY CREEK",
+      "BLACK MOUNTAIN",
+      "BROAD RIVER",
+      "FAIRVIEW",
+      "FLAT CREEK",
+      "FRENCH BROAD",
+      "HAZEL [18]",
+      "IVY",
+      "LEICESTER",
+      "LIMESTONE",
+      "LOWER HOMINY",
+      "REEMS CREEK",
+      "SANDY MUSH",
+      "SWANNANOA",
+      "WOODFIN",
+      "UPPER HOMINY",
+
+      // Census-designated places
+      "AVERY CREEK",
+      "BENT CREEK",
+      "FAIRVIEW",
+      "ROYAL PINES",
+      "SWANNANOA",
+
+      // Unincorporated communities
+      "ALEXANDER",
+      "ARDEN",
+      "BARNARDSVILLE",
+      "CANDLER",
+      "ENKA",
+      "FLAT CREEK",
+      "FORKS OF IVY",
+      "JUPITER",
+      "LEICESTER",
+      "RIDGECREST",
+      "SKYLAND",
+      "STOCKSVILLE",
+      
+      // Haywood County
+      "HAYWOOD COUNTY",
+      "HAYWOOD CO",
+      "HAYWOOD",
+      "BEAVERDAM",
+      "CANTON",
+      "EAST FORK",
+      "PIGEON",
+      
+      // Henderson County
+      "HENDERSON COUNTY",
+      "HENDERSON CO",
+      "HENDERSON",
+      "BALFOUR",
+      "CLEAR CREEK",
+      "EDNEYVILLE",
+      "FLETCHER",
+      "GERTON",
+      "HENDERSONVILLE",
+      "HOOPERS CREEK",
+      "MILLS RIVER",
+      "MOUNTAIN HOME",
+      
+      // Madison County
+      "MADISON COUNTY",
+      "MADISON CO",
+      "MADISON EOC",
+      "MADISON",
+      "BEECH GLENN",
+      "MARS HILL",
+      "MARSHALL",
+      "NORTH MARSHALL",
+      "SANDY MUSH",
+      "SOUTH MARSHALL",
+      "SPRING CREEK",
+      
+      // McDowell County
+      "MCDOWELL COUNTY",
+      "MCDOWELL CO",
+      "MCDOWELL",
+      "COLD FORT",
+      "CROOKED CREEK",
+      "OLD FORT",
+      
+      // Rutherford County
+      "RUTHERFORD COUNTY",
+      "RUTHERFORD CO",
+      "RUTHERFORD",
+      "CHIMNEY ROCK",
+      "LAKE LURE",
+      
+      // Yancy Couty
+      "YANCY COUNTY",
+      "YANCY CO",
+      "YANCY",
+      "PENSACOLA",
+      "SOUTH TOE"
+  };
 }
