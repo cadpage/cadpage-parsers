d46734f3b23ffe7ad38949c6ef853e33af498471
Ken Corbin <ken@cadpage.org>
2019-08-02 20:09:05 -0700

    Fixed parsing problem with Hancock County, KY

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/KY/KYHancockCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/KY/KYHancockCountyParser.java
@@ -1,13 +1,53 @@
 package net.anei.cadpage.parsers.KY;
 
-import net.anei.cadpage.parsers.dispatch.DispatchGeoconxParser;
+import net.anei.cadpage.parsers.SplitMsgOptions;
+import net.anei.cadpage.parsers.SplitMsgOptionsCustom;
+import net.anei.cadpage.parsers.dispatch.DispatchA65Parser;
 
 /**
  * Hancock County, KY
  */
-public class KYHancockCountyParser extends DispatchGeoconxParser {
+public class KYHancockCountyParser extends DispatchA65Parser {
   
   public KYHancockCountyParser() {
-    super("HANCOCK COUNTY", "KY");
+    super(CITY_LIST, "HANCOCK COUNTY", "KY");
   }
+  
+  @Override
+  public String getFilter() {
+    return "dispatch@hancockky.us";
+  }
+  
+  @Override
+  public SplitMsgOptions getActive911SplitMsgOptions() {
+    return new SplitMsgOptionsCustom(){
+      @Override public boolean splitBreakIns() { return true; }
+    };
+  }
+
+  private static final String[] CITY_LIST = new String[]{
+      
+      // Incorporated
+      "HAWESVILLE",
+      "LEWISPORT",
+
+      // Unincorporated
+      "ADAIR",
+      "CABOT",
+      "CHAMBERS",
+      "DUKES",
+      "EASTON",
+      "FLORAL",
+      "GOERING",
+      "PATESVILLE",
+      "PELLVILLE",
+      "PETRI",
+      "ROSEVILLE",
+      "SANDERS",
+      "SKILLMAN",
+      "UTILITY",
+      "WAITMAN",
+      "WEBERSTOWN",
+      "BOLING CHAPEL"
+  };
 }
