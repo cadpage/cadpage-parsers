900f6062df684379d0924378f8ac8e47fd4c3624
Ken Corbin <ken@cadpage.org>
2019-08-14 09:19:00 -0700

    Fixed parsing problem with Albemarle County, VA (C)

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/VA/VAAlbemarleCountyCParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/VA/VAAlbemarleCountyCParser.java
@@ -32,17 +32,43 @@ public class VAAlbemarleCountyCParser extends FieldProgramParser {
     return parseFields(body.split("\n"), data);
   }
   
-  private static DateFormat DATE_TIME_FMT = new SimpleDateFormat("MM/dd/yyyy hh:mm:ss aa");
-  
   @Override
   public Field getField(String name) {
-    if (name.equals("DATETIME")) return new DateTimeField(DATE_TIME_FMT, true);
+    if (name.equals("DATETIME")) return new MyDateTimeField();
     if (name.equals("PRI_CALL")) return new MyPriorityCallField();
     if (name.equals("ADDRCITY")) return new MyAddressCityField();
     if (name.equals("X"))  return new MyCrossField();
     if (name.equals("INFO")) return new MyInfoField();
     if (name.equals("UNIT")) return new UnitField("(?:\\b(?:[A-Z]+\\d{1,3}(?:A|ALS|CHF|DUTY)?|COUNTY(?:BC\\d+|DUTY|FM)?|VOL)\\b[, ]*)+", true);
     return super.getField(name);
+  }
+  
+  private static final Pattern DATE_TIME_PTN = Pattern.compile("(\\d\\d?/\\d\\d?/\\d{4}) (\\d\\d?:\\d\\d:\\d\\d(?: [AP]M)?)"); 
+  private static final DateFormat TIME_FMT = new SimpleDateFormat("hh:mm:ss aa");
+  private class MyDateTimeField extends DateTimeField {
+    @Override
+    public boolean canFail() {
+      return true;
+    }
+    
+    @Override
+    public boolean checkParse(String field, Data data) {
+      Matcher match = DATE_TIME_PTN.matcher(field);
+      if (!match.matches()) return false;
+      data.strDate =  match.group(1);
+      String time = match.group(2);
+      if (time.endsWith("M")) {
+        setTime(TIME_FMT, time, data);
+      } else {
+        data.strTime = time;
+      }
+      return true;
+    }
+    
+    @Override
+    public void parse(String field, Data data) {
+      if (!checkParse(field, data)) abort();
+    }
   }
   
   private static final Pattern PRIORITY_CALL_PTN = Pattern.compile("([A-Z0-9]+) +(.*)"); 
