b113f3f1f8a276d5cf0d41f2fc584dbe1c422171
Ken Corbin <ken@cadpage.org>
2019-08-21 13:22:03 -0700

    Fixed parsing problem with Pulaski County, AR (C)

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/AR/ARPulaskiCountyCParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/AR/ARPulaskiCountyCParser.java
@@ -11,7 +11,7 @@ public class ARPulaskiCountyCParser extends SmartAddressParser {
   
   public ARPulaskiCountyCParser() {
     super("PULASKI COUNTY", "AR");
-    setFieldList("ID CALL ADDR PLACE APT CITY ST");
+    setFieldList("ID CALL ADDR PLACE APT CITY ST CH");
     setupCallList(CALL_LIST);
     setupMultiWordStreets(ARPulaskiCountyAParser.MWORD_STREET_LIST);
   }
@@ -22,13 +22,19 @@ public class ARPulaskiCountyCParser extends SmartAddressParser {
   }
   
   private static final Pattern ID_PTN = Pattern.compile("CFS\\d{4}-\\d{5}");
-  private static final Pattern MASTER = Pattern.compile("([^,]+)(?:, *([A-Z ]+))??(?:, *([A-Z]{2})(?: (\\d{5}))?)? Please respond immediately\\.");
+  private static final Pattern MASTER = Pattern.compile("([^,]+)(?:, *([A-Z ]+))??(?:, *([A-Z]{2})(?: (\\d{5}))?)?(?: ([ A-Z0-9]*?))?");
   
   @Override
   protected boolean parseMsg(String subject, String body, Data data) {
     
     if (!ID_PTN.matcher(subject).matches()) return false;
     data.strCallId = subject;
+    
+    int pt = body.indexOf(" Fire Channel");
+    if (pt >= 0) {
+      data.strChannel = body.substring(pt+13).trim();
+      body = stripFieldEnd(body.substring(0,pt), "Respond");
+    }
     
     Matcher match = MASTER.matcher(body);
     if (!match.matches()) return false;
@@ -37,15 +43,24 @@ public class ARPulaskiCountyCParser extends SmartAddressParser {
     data.strState = getOptGroup(match.group(3));
     String zip = match.group(4);
     if (data.strCity.length() == 0 && zip != null) data.strCity = zip;
+    data.strChannel = append(data.strChannel, "/", getOptGroup(match.group(5)));
     return true;
   }
   
   private static final CodeSet CALL_LIST = new CodeSet(
       "ACCIDENT INJURY",
+      "CHEMICAL SPILL",
       "FALL",
+      "FIRE GRASS/WOODS",
+      "FIRE OTHER STRUCTURE",
+      "FIRE RESIDENTIAL",
+      "FIRE VEHICLE",
+      "ILLEGAL BURN",
       "MEDICAL EMERGENCY",
       "NATURAL DISASTER",
+      "SHOOTING",
       "SUICIDE ATTEMPT",
+      "Suicide Threats",
       "SUSPICIOUS PERSON",
       "WELFARE CONCERN"
   );
