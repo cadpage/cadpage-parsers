aaa8d0d7e5bbf85a7bed2619c5a484357f8d57cf
Ken Corbin <ken@cadpage.org>
2019-08-22 15:40:50 -0700

    Fixed parsing problem with Lyon County, MN

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/MN/MNLyonCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/MN/MNLyonCountyParser.java
@@ -1,5 +1,6 @@
 package net.anei.cadpage.parsers.MN;
 
+import net.anei.cadpage.parsers.MsgInfo.Data;
 import net.anei.cadpage.parsers.dispatch.DispatchA27Parser;
 
 /**
@@ -16,4 +17,15 @@ public class MNLyonCountyParser extends DispatchA27Parser {
   public String getFilter() {
     return "noreply@cisusa.org";
   }
+
+  @Override
+  public boolean parseMsg(String subject, String body, Data data) {
+    if (!body.startsWith("Notification ")) {
+      int pt = body.indexOf("\n\nNotification ");
+      if (pt < 0) return false;
+      body = body.substring(pt+2);
+    }
+    return super.parseMsg(subject, body, data);
+  }
+  
 }
