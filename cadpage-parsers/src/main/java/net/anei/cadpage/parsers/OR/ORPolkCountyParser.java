6a591a200cfd904af90577295e61ee2bd457e4c7
Ken Corbin <ken@cadpage.org>
2019-09-19 10:12:53 -0700

    Fixed parsing problem with Polk County, OR

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/OR/ORPolkCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/OR/ORPolkCountyParser.java
@@ -1,5 +1,8 @@
 package net.anei.cadpage.parsers.OR;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import net.anei.cadpage.parsers.FieldProgramParser;
 import net.anei.cadpage.parsers.MsgInfo.Data;
 
@@ -8,7 +11,7 @@ public class ORPolkCountyParser extends FieldProgramParser {
   
   public ORPolkCountyParser() {
     super("POLK COUNTY", "OR",
-          "CALL SRC DATETIME ADDR/SP UNIT ID!");
+          "CALL SRC DATETIME ADDR/SP UNIT ID! INFO/N+");
   }
   
   @Override
@@ -26,6 +29,25 @@ public class ORPolkCountyParser extends FieldProgramParser {
     if (name.equals("SRC")) return new SourceField("[A-Z0-9]{3}", true);
     if (name.equals("DATETIME")) return new DateTimeField("\\d\\d/\\d\\d/\\d\\d \\d\\d:\\d\\d:\\d\\d", true);
     if (name.equals("ID")) return new IdField("[A-Z]{3}\\d{12}", true);
+    if (name.equals("INFO")) return new MyInfoField();
     return super.getField(name);
+  }
+  
+  private static final Pattern INFO_PHONE_PTN = Pattern.compile("\\[(\\d{3}-\\d{3}-\\d{4})\\] *(.*)");
+  private class MyInfoField extends InfoField {
+    @Override
+    public void parse(String field, Data data) {
+      Matcher match = INFO_PHONE_PTN.matcher(field);
+      if (match.matches()) {
+        data.strPhone = match.group(1);
+        field = match.group(2);
+      }
+      super.parse(field, data);
+    }
+    
+    @Override
+    public String getFieldNames() {
+      return "PHONE " + super.getFieldNames();
+    }
   }
 }
