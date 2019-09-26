c0d8dd2fe27131969eab2fa1914324e1b4f9e46e
Ken Corbin <ken@cadpage.org>
2019-09-26 15:04:42 -0700

    Fixed parsing problem with Brusnwick County, NC

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/NC/NCBrunswickCountyBParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/NC/NCBrunswickCountyBParser.java
@@ -1,5 +1,6 @@
 package net.anei.cadpage.parsers.NC;
 
+import net.anei.cadpage.parsers.MsgInfo.Data;
 import net.anei.cadpage.parsers.dispatch.DispatchA71Parser;
 
 public class NCBrunswickCountyBParser extends DispatchA71Parser {
@@ -11,5 +12,21 @@ public class NCBrunswickCountyBParser extends DispatchA71Parser {
   @Override
   public int getMapFlags() {
     return MAP_FLG_PREFER_GPS;
+  }
+
+  @Override
+  protected boolean parseMsg(String body, Data data) {
+    if (!super.parseMsg(body, data)) return false;
+    int pt = data.strPriority.indexOf(' ');
+    if (pt >= 0) {
+      data.strCode = data.strPriority.substring(pt+1).trim();
+      data.strPriority = data.strPriority.substring(0,pt);
+    }
+    return true;
+  }
+  
+  @Override
+  public String getProgram() {
+    return super.getProgram().replace("PRI", "PRI CODE");
   }
 }
