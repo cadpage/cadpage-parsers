216eea94e4f709bdc8077140c44164df7b57e54c
Ken Corbin <ken@cadpage.org>
2019-09-29 18:58:56 -0700

    Fixed mapping problem with Burnet County, TX

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/TX/TXBurnetCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/TX/TXBurnetCountyParser.java
@@ -1,5 +1,7 @@
 package net.anei.cadpage.parsers.TX;
 
+import java.util.regex.Pattern;
+
 import net.anei.cadpage.parsers.dispatch.DispatchA72Parser;
 
 public class TXBurnetCountyParser extends DispatchA72Parser {
@@ -15,5 +17,13 @@ public class TXBurnetCountyParser extends DispatchA72Parser {
   @Override
   public String getFilter() {
     return "alert@burnetsheriff.com";
+  }
+  
+  private static final Pattern RR_PTN = Pattern.compile("\\bRR\\b");
+  
+  @Override
+  public String adjustMapAddress(String addr) {
+    addr = RR_PTN.matcher(addr).replaceAll("RANCH ROAD");
+    return super.adjustMapAddress(addr);
   }
 }
