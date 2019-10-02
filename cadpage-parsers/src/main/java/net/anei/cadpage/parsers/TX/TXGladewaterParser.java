1e6d16a02c3c01acc7ec2483ae9113a2b7df4811
Ken Corbin <ken@cadpage.org>
2019-10-02 16:50:06 -0700

    Fixed parsing problem with Gladewater, TX

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/TX/TXGladewaterParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/TX/TXGladewaterParser.java
@@ -1,8 +1,8 @@
 package net.anei.cadpage.parsers.TX;
 
-import net.anei.cadpage.parsers.dispatch.DispatchA19Parser;
+import net.anei.cadpage.parsers.dispatch.DispatchA53Parser;
 
-public class TXGladewaterParser extends DispatchA19Parser {
+public class TXGladewaterParser extends DispatchA53Parser {
   
   public TXGladewaterParser() {
     super("GLADEWATER", "TX");
@@ -10,7 +10,7 @@ public class TXGladewaterParser extends DispatchA19Parser {
   
   @Override
   public String getFilter() {
-    return "noreply@responsemaster.net";
+    return "@cityofgladewater.com";
   }
 
 }
