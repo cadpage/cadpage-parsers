ad0329c232517cb4ac505bbe76bb313ab8819af4
Ken Corbin <ken@cadpage.org>
2019-10-06 14:39:54 -0700

    Fixed parsing problem with Moniteau County, MO

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/MO/MOMoniteauCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/MO/MOMoniteauCountyParser.java
@@ -1,16 +1,23 @@
 package net.anei.cadpage.parsers.MO;
 
-import net.anei.cadpage.parsers.dispatch.DispatchA25Parser;
+import net.anei.cadpage.parsers.FieldProgramParser;
+import net.anei.cadpage.parsers.MsgInfo.Data;
 
 
-public class MOMoniteauCountyParser extends DispatchA25Parser {
+public class MOMoniteauCountyParser extends FieldProgramParser {
   
   public MOMoniteauCountyParser() {
-    super("MONITEAU COUNTY", "MO");
+    super("MONITEAU COUNTY", "MO",
+          "Call:CALL! Sub:CALL/S! Place:PLACE! Address:ADDR! City:CITY! Apt:APT! Cross_Streets:X! Event#:ID! Reporting_Person:NAME! Phone#:PHONE! Unit:UNIT! Initiated:SKIP! Notes:INFO! INFO/N+");
   }
   
   @Override
   public String getFilter() {
-    return "cad@moniteau911.com";
+    return "CAD@MONITEAU911.COM";
+  }
+  
+  @Override
+  protected boolean parseMsg(String body, Data data) {
+    return parseFields(body.split("\n"), data);
   }
 }
