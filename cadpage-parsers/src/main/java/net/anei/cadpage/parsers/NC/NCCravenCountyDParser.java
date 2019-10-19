dd68859cdfe3fcb5844cd3818e3d2f61bf6e5da7
Ken Corbin <ken@cadpage.org>
2019-10-18 19:01:40 -0700

    Fixed parsing problem with Craven County, NC

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/NC/NCCravenCountyDParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/NC/NCCravenCountyDParser.java
@@ -3,24 +3,24 @@ package net.anei.cadpage.parsers.NC;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import net.anei.cadpage.parsers.SmartAddressParser;
+import net.anei.cadpage.parsers.FieldProgramParser;
 import net.anei.cadpage.parsers.MsgInfo.Data;
 
 /**
  * Craven County, NC
  */
-public class NCCravenCountyDParser extends SmartAddressParser {
+public class NCCravenCountyDParser extends FieldProgramParser {
   
   public NCCravenCountyDParser() {
-    super("CRAVEN COUNTY", "NC");
-    setFieldList("SRC PLACE ADDR APT GPS CITY CALL ID INFO");
+    super("CRAVEN COUNTY", "NC", 
+          "CALL ADDRCITY/S X GPS PLACE MAP MAP/L UNIT! Narrataive:INFO/N+");
     setupMultiWordStreets(MWORD_STREET_LIST);
     removeWords("COUNTY");
   }
   
   @Override
   public String getFilter() {
-    return "dispatch@cravencountync.gov,dispatch@newbernnc.gov";
+    return "dispatch@newbernnc.gov";
   }
   
   @Override
@@ -28,45 +28,44 @@ public class NCCravenCountyDParser extends SmartAddressParser {
     return MAP_FLG_PREFER_GPS;
   }
   
-  private static final Pattern MASTER = Pattern.compile("(.+?) Location: (.*?) (\\d{2}\\.\\d{6,} -\\d{2}\\.\\d{6,}) (New Bern) (.+?) (?:(\\d{4}-\\d{8}) )?Narrative:\\s*(?s)(.*)");
-  private static final Pattern BREAK_PTN = Pattern.compile(" +\n");
-  
   @Override
   public boolean parseMsg(String subject, String body, Data data) {
     if (!subject.equals("[DISP]")) return false;
-    Matcher match = MASTER.matcher(body);
-    if (!match.matches()) return false;
-    data.strSource = match.group(1).trim();
-    parseOurAddress(match.group(2).trim(), data);
-    setGPSLoc(match.group(3).trim(), data);
-    data.strCity = match.group(4);
-    data.strCall = match.group(5);
-    data.strCallId = getOptGroup(match.group(6));
-    data.strSupp = BREAK_PTN.matcher(match.group(7).trim()).replaceAll("\n");
-    return true;
+    return parseFields(body.split("\n"), data);
   }
   
-  private static final Pattern DIR_SECTOR_PTN = Pattern.compile(" *\\b[NSEW] SECTOR\\b *");
-  private static final Pattern STREET_APT_PTN = Pattern.compile("(.*\\b\\d+ )(\\d{1,3}[A-Z]?|[A-K]\\d*) (.*)");
+  @Override
+  public Field getField(String  name) {
+    if (name.equals("ADDRCITY")) return new MyAddressCityField();
+    if (name.equals("X")) return new MyCrossField();
+    if (name.equals("GPS")) return new MyGPSField();
+    return super.getField(name);
+  }
   
-  private void parseOurAddress(String addr, Data data) {
-    
-    addr =  DIR_SECTOR_PTN.matcher(addr).replaceAll(" ").trim();
-    
-    String apt = "";
-    Matcher match = STREET_APT_PTN.matcher(addr);
-    if (match.matches()) {
-      addr = match.group(1) + match.group(3);
-      apt = match.group(2);
+  private class MyAddressCityField extends AddressCityField {
+    @Override
+    public void parse(String field, Data data) {
+      field = field.replace('@',  '&');
+      super.parse(field, data);
     }
-    parseAddress(StartType.START_PLACE, FLAG_IMPLIED_INTERSECT | FLAG_ANCHOR_END, addr, data);
-    data.strApt = append(apt, "-", data.strApt);
-    
-    // A numeric place name was probably a street number followed by an apt number that was mistaken
-    // for the street number
-    if (NUMERIC.matcher(data.strPlace).matches()) {
-      data.strAddress = append(data.strPlace, " ", data.strAddress);
-      data.strPlace = "";
+  }
+  
+  private class MyCrossField extends CrossField {
+    @Override
+    public void parse(String field, Data data) {
+      if (field.startsWith("Failed ")) return;
+      super.parse(field, data);
+    }
+  }
+  
+  private static final Pattern GPS_PTN = Pattern.compile("(-\\d{2}\\.\\d{4,})(\\d{2}\\.\\d{4,})");
+  private class MyGPSField extends GPSField {
+    @Override
+    public void parse(String field, Data data) {
+      if (field.length() == 0) return;
+      Matcher match = GPS_PTN.matcher(field);
+      if (!match.matches()) abort();
+      super.parse(match.group(1)+','+match.group(2), data);
     }
   }
   
