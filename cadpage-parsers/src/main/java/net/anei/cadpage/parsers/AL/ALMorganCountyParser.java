21b1727023221617001dba47fb48a84a9c848c2a
Ken Corbin <ken@cadpage.org>
2019-10-19 10:20:32 -0700

    Fixed parsing problem with Morgan County, AL

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/AL/ALMorganCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/AL/ALMorganCountyParser.java
@@ -4,6 +4,7 @@ package net.anei.cadpage.parsers.AL;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import net.anei.cadpage.parsers.MsgInfo.Data;
@@ -30,14 +31,19 @@ public class ALMorganCountyParser extends DispatchOSSIParser {
 
   public ALMorganCountyParser() {
     super("MORGAN COUNTY", "AL",
-        // The simple program string we want to use is
+        
+        // Odd variants
+        "FYI? ( BUS_FIRE PLACE? ADDR/Z ID2 END " +
+    
+        // For the rest, the simple program string we want to use is
         // PLACE? ADDR CALL! X? X? ID? UNIT CH
         // But of course that will never work, so we have to do something
         // complicated
         
         // If we can identify the call field, things are pretty simple
-        "FYI? ( PLACE ADDR/Z CALL X/Z+? ( ID UNIT | UNIT ) " +
-             "| ADDR/Z CALL X/Z+? ( ID UNIT | UNIT ) " +
+             "| ADDR/Z X/Z PLACE CALL X/Z+? ( INFO | ID UNIT? | UNIT ) " +
+             "| PLACE ADDR/Z CALL X/Z+? ( INFO | ID UNIT? | UNIT ) " +
+             "| ADDR/Z CALL X/Z+? ( INFO | ID UNIT? | UNIT ) " +
                
              // If the call field is unknown, things get complicated
              // next see if the unit is in the 7th place, which means all
@@ -46,10 +52,10 @@ public class ALMorganCountyParser extends DispatchOSSIParser {
              
              // Still no luck.  All we can do is hope the address
              // is recognizable
-             "| PLACE? ADDR/s CALL! X/Z+? ( ID UNIT | UNIT ) " +
+             "| PLACE? ADDR/s CALL! X/Z+? ( INFO | ID UNIT? | UNIT ) " +
             
              // And finally an optional channel at the end
-             ") CH");
+             ") CH? INFO/N+");
   }
   
   @Override
@@ -65,11 +71,14 @@ public class ALMorganCountyParser extends DispatchOSSIParser {
 
   @Override
   public Field getField(String name) {
+    if (name.equals("BUS_FIRE")) return new CallField("BUSINESS FIRE", true);
     if (name.equals("CALL")) return new MyCallField();
     if (name.equals("X")) return new MyCrossField();
     if (name.equals("ID")) return new IdField("\\d{8}", true);
-    if (name.equals("UNIT")) return new UnitField("(?!TAC)[A-Z]{3}[A-Z0-9]", true);
-    if (name.equals("CH")) return new ChannelField("TAC? ?\\d{0,2}", true);
+    if (name.equals("ID2")) return new IdField("20\\d{9}");
+    if (name.equals("UNIT")) return new UnitField("(?!TAC|ATC)[A-Z]{3}[A-Z0-9]", true);
+    if (name.equals("CH")) return new ChannelField("(?:TAC?|ATC) ?\\d{0,2}", true);
+    if (name.equals("INFO")) return new MyInfoField();
     return super.getField(name);
   }
   
@@ -97,6 +106,36 @@ public class ALMorganCountyParser extends DispatchOSSIParser {
     }
   }
   
+  private static final Pattern INFO_PTN = Pattern.compile(".*[a-z].*");
+  private class MyInfoField extends InfoField {
+    @Override
+    public boolean canFail() {
+      return true;
+    }
+    
+    @Override
+    public boolean checkParse(String field, Data data) {
+      if (field.length() == 0) return true;
+      if (!INFO_PTN.matcher(field).matches()) return false;
+      parse(field, data);
+      return true;
+    }
+    
+    @Override
+    public void parse(String field, Data data) {
+      if (field.startsWith("Radio Channel:")) {
+        data.strChannel = field.substring(14).trim();
+      } else {
+        super.parse(field, data);
+      }
+    }
+    
+    @Override
+    public String getFieldNames() {
+      return "CH " + super.getFieldNames();
+    }
+  }
+  
   @Override
   public String adjustMapAddress(String addr) {
     addr = addr.replace("MARK SELBY PVT", "SELBY PVT");
@@ -105,49 +144,89 @@ public class ALMorganCountyParser extends DispatchOSSIParser {
   
   @Override
   public boolean checkCall(String field) {
-    return (CALL_CAT_PTN.matcher(field).matches() ||
-            CALL_LIST.contains(field));
+    if (CALL_CAT_PTN.matcher(field).matches()) return true;
+    Matcher match = CALL_SFX_PTN.matcher(field);
+    if (match.matches()) field = match.group(1);
+    return CALL_LIST.contains(field);
     
   }
   
   private static final Pattern CALL_CAT_PTN = Pattern.compile(".* CAT ?\\d");
+  private static final Pattern CALL_SFX_PTN = Pattern.compile("(.*?) *- [A-EO]");
   private Set<String> CALL_LIST = new HashSet<String>(Arrays.asList(
+      "ABDOMINAL PAIN / PROBLEMS",
+      "ADVANCED SEND",
+      "ALLERGIES / ENVENOMATIONS",
       "APPLIANCE FIRE",
+      "ANIMAL BITES / ATTACKS",
+      "ASSAULT/SEXUAL ASLT/STUNGUN",
       "ASSIST LAW ENFORCEMENT",
+      "BACK PAIN",
+      "BURNS / EXPLOSION",
       "BUSINESS FIRE",
       "BUSINESS FIRE ALARM",
+      "BREATHING PROBLEMS",
       "BRUSH FIRE",
-      "CARBON MONOXIDE ALARM",
       "CAR FIRE",
+      "CARBON MONOXIDE ALARM",
+      "CARDIAC/RESPIRATORY/DEATH",
+      "CAVE RESCUE",
+      "CHEST PAIN / NON-TRAUMATIC",
+      "CHOKING",
       "COMMERCIAL VEHICLE FIRE",
       "CONTROLLED BURN",
+      "CONVULSIONS / SEIZURES",
+      "DIABETIC PROBLEMS",
+      "ELECTROCUTION / LIGHTNING",
       "EMERGENCY TRANSPORT",
+      "EMS PR EVENT",
       "EXPLOSION",
+      "EYE PROBLEMS / INJURIES",
+      "FALLS",
       "FIRE DEPARTMENT CHECK",
+      "FIRE DRILL (MOCK INCIDENT)",
       "FIRE OTHER",
       "FIRE PR EVENT",
       "GAS LEAK",
+      "GAS SPILL",
       "GRASS FIRE",
+      "HEADACHE",
+      "HEART PROBLEMS / AICD",
+      "HEAT / COLD EXPOSURE",
+      "HEMORRHAGE / LACERATIONS",
       "HIT AND RUN NO INJURY",
       "HIT AND RUN WITH INJURY",
       "ILLEGAL BURN",
       "LIFT ASSIST",
+      "MEDICAL CALL",
       "MEDICAL TRANSPORT",
+      "OVERDOSE / POISONING",
       "PASSENGER VEHICLE FIRE",
+      "PREGNANCY / CHILDBIRTH",
+      "PSYCHIATRIC / SUICIDE ATT",
       "RESIDENTIAL FIRE",
       "RESIDENTIAL FIRE ALARM",
       "SEARCH AND RESCUE OPERATIONS",
       "SHOOTING",
+      "SICK PERSON",
       "SMOKE INVESTIGATION",
       "STABBING",
+      "STROKE (CVA) / TIA",
       "STRUCTURE FIRE",
       "SUICIDE",
       "SUICIDE ATTEMPTED",
       "SUICIDE THREATENED",
+      "TRAFFIC INCIDENT ENTRAP",
+      "TRAFFIC/TRANSPORT INCIDENT",
+      "TRANSFER / INTERFACILITY",
       "TRASH FIRE",
+      "TRAUMATIC INJURIES",
+      "UNCONSCIOUS / FAINTING",
+      "UNKNOWN / PERSON DOWN",
       "URGENT TRANSPORT",
       "WATER RESCUE WITH INJURY",
       "WEATHER RELATED",
+      "WRECK",
       "WRECK NO INJURY",
       "WRECK WITH ENTRAPMENT",
       "WRECK WITH INJURY",
