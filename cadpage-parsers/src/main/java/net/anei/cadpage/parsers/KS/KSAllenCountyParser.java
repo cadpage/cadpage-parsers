f9e916b1bcb2ccfe9f98c0a6eb633b613b52e178
Ken Corbin <ken@cadpage.org>
2019-11-05 09:16:26 -0800

    Fixed mapping problem with Allen County, KS

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/KS/KSAllenCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/KS/KSAllenCountyParser.java
@@ -1,5 +1,9 @@
 package net.anei.cadpage.parsers.KS;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import net.anei.cadpage.parsers.MsgInfo.Data;
 import net.anei.cadpage.parsers.dispatch.DispatchA25Parser;
 
 
@@ -13,4 +17,30 @@ public class KSAllenCountyParser extends DispatchA25Parser {
   public String getFilter() {
     return "reports@allencounty911.org";
   }
+
+  @Override
+  protected boolean parseMsg(String subject, String body, Data data) {
+    if (!super.parseMsg(subject, body, data)) return false;
+    data.strAddress = fixAddress(data.strAddress);
+    return true;
+  }
+  
+  private static final Pattern NUMBER_ST_AV_PTN = Pattern.compile("(\\d+) (AVE?|ST)\\b", Pattern.CASE_INSENSITIVE);
+  private String fixAddress(String addr) {
+    Matcher match = NUMBER_ST_AV_PTN.matcher(addr);
+    if (match.find()) {
+      StringBuffer sb = new StringBuffer();
+      do {
+        String street = match.group(1);
+        char lastDigit = street.charAt(street.length()-1);
+        String suffix = lastDigit == '1' ? "st" : lastDigit == '2' ? "nd" : lastDigit == '3' ? "rd" : "th";
+        match.appendReplacement(sb, street+suffix+' '+match.group(2));
+      } while (match.find());
+      match.appendTail(sb);
+      addr = sb.toString();
+    }
+    return addr;
+  }
+  
+  
 }
