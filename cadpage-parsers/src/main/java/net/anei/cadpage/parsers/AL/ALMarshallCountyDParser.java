4c2354b8c097ca0d37e93e84df2b8c3cdb81209b
Ken Corbin <ken@cadpage.org>
2019-11-08 21:07:28 -0800

    Fixed parsing problem with Marshall County, AL (D)

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/AL/ALMarshallCountyDParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/AL/ALMarshallCountyDParser.java
@@ -1,5 +1,6 @@
 package net.anei.cadpage.parsers.AL;
 
+import net.anei.cadpage.parsers.MsgInfo.Data;
 import net.anei.cadpage.parsers.dispatch.DispatchA74Parser;
 
 public class ALMarshallCountyDParser extends DispatchA74Parser {
@@ -12,4 +13,11 @@ public class ALMarshallCountyDParser extends DispatchA74Parser {
   public int getMapFlags() {
     return MAP_FLG_PREFER_GPS;
   }
+
+  @Override
+  public boolean parseMsg(String subject, String body, Data data) {
+    body = body.replace('|', '\n');
+    return super.parseMsg(subject, body, data);
+  }
+  
 }
