2ee76118b16531bef9c37bdd1c3a5c0e08a376e0
Ken Corbin <ken@cadpage.org>
2019-11-13 20:29:32 -0800

    Fixed parsing problem with Smith County, TX

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/TX/TXSmithCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/TX/TXSmithCountyParser.java
@@ -1,5 +1,9 @@
 package net.anei.cadpage.parsers.TX;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import net.anei.cadpage.parsers.MsgInfo.Data;
 import net.anei.cadpage.parsers.dispatch.DispatchA19Parser;
 /**
  * Smith County, TX
@@ -9,4 +13,24 @@ public class TXSmithCountyParser extends DispatchA19Parser {
   public TXSmithCountyParser() {
     super("SMITH COUNTY", "TX");
   }
+  
+  @Override
+  public String getFilter() {
+    return "Dgeneric@smith-county.com";
+  }
+
+  private static final Pattern SUBJ_PTN = Pattern.compile("(DISPATCH) +(?=Incident #)");
+  private static final Pattern FIELD_BRK_PTN = Pattern.compile(" +(?=CAD Call ID #:|Type:|Date/Time:)");
+  
+  @Override
+  protected boolean parseMsg(String subject, String body, Data data) {
+    Matcher match = SUBJ_PTN.matcher(subject);
+    if (match.lookingAt()) {
+      body = FIELD_BRK_PTN.matcher(subject.substring(match.end())).replaceAll("\n") + '\n' + body;
+      subject = match.group(1);
+    }
+    // TODO Auto-generated method stub
+    return super.parseMsg(subject, body, data);
+  }
+  
  }
