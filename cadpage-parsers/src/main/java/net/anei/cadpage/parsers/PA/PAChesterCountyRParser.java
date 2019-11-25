2558d99320da35d1a3f38a0fc8b69ce3451f6994
Ken Corbin <ken@cadpage.org>
2019-11-24 20:42:29 -0800

    Fixed parsing problem with Chester County, PA (R)

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/PA/PAChesterCountyRParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/PA/PAChesterCountyRParser.java
@@ -1,11 +1,14 @@
 package net.anei.cadpage.parsers.PA;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import net.anei.cadpage.parsers.MsgInfo.Data;
 
 public class PAChesterCountyRParser extends PAChesterCountyBaseParser {
   
   public PAChesterCountyRParser() {
-    super("( DATE TIME DISPATCH CALL ADDRCITY APT CITY! X NAME PHONE " +
+    super("( DATE TIME DISPATCH CALL ADDRCITY APT CITY! X NAME PHONE UNIT INFO/N+? ID END" +
           "| CALL ADDRCITY! END " + 
           ")");
   }
@@ -28,6 +31,8 @@ public class PAChesterCountyRParser extends PAChesterCountyBaseParser {
     if (name.equals("TIME")) return new TimeField("\\d\\d?:\\d\\d:\\d\\d", true);
     if (name.equals("DISPATCH")) return new SkipField("DISPATCH", true);
     if (name.equals("CITY")) return new MyCityField();
+    if (name.equals("INFO")) return new MyInfoField();
+    if (name.equals("ID")) return new IdField("F\\d+", true);
     return super.getField(name);
   }
   
@@ -37,5 +42,20 @@ public class PAChesterCountyRParser extends PAChesterCountyBaseParser {
       if (field.length() == 0) return;
       super.parse(field, data);
     }
+  }
+  
+  private static final Pattern INFO_HEADER_PTN = Pattern.compile("\\d\\d?:\\d\\d:\\d\\d \\S+ +");
+  private static final Pattern INFO_JUNK_PTN = Pattern.compile("[0-9:]+(?: \\S+)?");
+  private class MyInfoField extends InfoField {
+    @Override
+    public void parse(String field, Data data) {
+      if (field.length() == 0) return;
+      Matcher match = INFO_HEADER_PTN.matcher(field);
+      if (match.lookingAt()) {
+        field = field.substring(match.end());
+      } else if (INFO_JUNK_PTN.matcher(field).matches()) return;
+      super.parse(field, data);
+    }
+    
   }
 }
