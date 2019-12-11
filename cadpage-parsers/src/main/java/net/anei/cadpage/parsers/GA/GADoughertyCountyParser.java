cd0fdadd47089f418786e6d6a1e66be5106153b7
Ken Corbin <ken@cadpage.org>
2019-12-11 12:05:15 -0800

    Fixed parsing problem with Dohgherty County, GA

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/GA/GADoughertyCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/GA/GADoughertyCountyParser.java
@@ -13,12 +13,23 @@ public class GADoughertyCountyParser extends DispatchOSSIParser {
   
   @Override
   public String getFilter() {
-    return "cad@dougherty.ga.us";
+    return "cad@dougherty.ga.us,777";
   }
   
   @Override
   protected boolean parseMsg(String subject, String body, Data data) {
-    if (!subject.equals("Text Message")) return false;
+    do {
+      if (subject.equals("Text Message")) break;
+      
+      body = stripFieldStart(body, "City of Albany:");
+      if (body.startsWith("(Text Message)")) {
+        body = body.substring(14).trim();
+        break;
+      }
+      
+      return false;
+    } while (false);
+    
     int pt = body.indexOf("\n");
     if (pt >= 0) body = body.substring(0,pt).trim();
     return super.parseMsg(body, data);
