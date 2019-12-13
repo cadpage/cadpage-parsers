1ca4ac46a6d807b5a44502fd54152f67f6de20cd
Ken Corbin <ken@cadpage.org>
2019-12-13 12:20:57 -0800

    Fixed parsing problem with Orangeburg County, SC (C)

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/SC/SCOrangeburgCountyCParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/SC/SCOrangeburgCountyCParser.java
@@ -2,6 +2,8 @@ package net.anei.cadpage.parsers.SC;
 
 import java.util.Properties;
 
+import net.anei.cadpage.parsers.SplitMsgOptions;
+import net.anei.cadpage.parsers.SplitMsgOptionsCustom;
 import net.anei.cadpage.parsers.dispatch.DispatchOSSIParser;
 
 public class SCOrangeburgCountyCParser extends DispatchOSSIParser {
@@ -9,12 +11,17 @@ public class SCOrangeburgCountyCParser extends DispatchOSSIParser {
   public SCOrangeburgCountyCParser() {
     super(CITY_CODES, "ORANGEBURG COUNTY", "SC", 
           "( CANCEL ADDR CITY!" +
-          "| CALL ADDR X X CITY ID! ) INFO/N+");
+          "| FYI? CALL ADDR X X CITY ID! ) INFO/N+");
+  }
+
+  @Override
+  public SplitMsgOptions getActive911SplitMsgOptions() {
+    return new SplitMsgOptionsCustom();
   }
 
   @Override
   public Field getField(String name) {
-    if (name.equals("ID")) return new IdField("\\d{7}", true);
+    if (name.equals("ID")) return new IdField("\\d{7,9}|", true);
     return super.getField(name);
   }
   
@@ -23,13 +30,16 @@ public class SCOrangeburgCountyCParser extends DispatchOSSIParser {
       "BWMN", "BOWMAN",
       "COPE", "COPE",
       "CRDV", "CORDOVA",
+      "ELLR", "ELLOREE",
       "EUTW", "EUTAWVILLE",
       "HHLL", "HOLLY HILL",
       "NEES", "NEESES",
+      "NORT", "NORTH",
       "NRTH", "NORTH",
       "ORBG", "ORANGEBURG",
       "RWSV", "ROWESVILLE",
       "SNTE", "SANTEE",
+      "SPRI", "SPRINGFIELD",
       "VANC", "VANCE"
   });
 }
