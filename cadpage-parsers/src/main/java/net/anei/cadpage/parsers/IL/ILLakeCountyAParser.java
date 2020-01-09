dfb1ccf86147974133b1b2b18eac7719f8c660ad
Ken Corbin <ken@cadpage.org>
2020-01-08 16:49:05 -0800

    Update call table for Harrisson County, WV

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/IL/ILLakeCountyAParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/IL/ILLakeCountyAParser.java
@@ -1,5 +1,8 @@
 package net.anei.cadpage.parsers.IL;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import net.anei.cadpage.parsers.FieldProgramParser;
 import net.anei.cadpage.parsers.MsgInfo.Data;
 
@@ -23,13 +26,22 @@ public class ILLakeCountyAParser extends FieldProgramParser {
     return super.getField(name);
   }
   
+  private static final Pattern ADDR_CITY_APT_PTN = Pattern.compile("(.*?)-(?:UNINC. )?([A-Z][A-Z ]+)(?:[- ]+(\\d.*))?");
+  
   private class MyAddressCityField extends AddressCityField {
     @Override
     public void parse(String field, Data data) {
-      int pt = field.lastIndexOf('-');
-      if (pt < 0) abort();
-      parseAddress(field.substring(0, pt).trim(), data);
-      data.strCity = field.substring(pt+1).trim();
+      field = stripFieldEnd(field, "-");
+      if (field.length() == 0) return;
+      String apt = "";
+      Matcher match = ADDR_CITY_APT_PTN.matcher(field);
+      if (match.matches()) {
+        field = match.group(1).trim();
+        data.strCity = match.group(2);
+        apt = getOptGroup(match.group(3));
+      }
+      parseAddress(field, data);
+      data.strApt = append(data.strApt, "-", apt);
     }
   }
 }
