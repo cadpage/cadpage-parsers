d6406779b622bb8f05100b5d659d3db540959c0f
Ken Corbin <ken@cadpage.org>
2020-02-07 20:46:03 -0800

    Fixed parsing problem with Marion County, SC (Add B)

---
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/SC/SCMarionCountyBParser.java
@@ -0,0 +1,24 @@
+package net.anei.cadpage.parsers.SC;
+
+import net.anei.cadpage.parsers.MsgInfo.Data;
+import net.anei.cadpage.parsers.dispatch.DispatchSPKParser;
+
+public class SCMarionCountyBParser extends DispatchSPKParser {
+  
+  public SCMarionCountyBParser() {
+    super("MARION COUNTY", "SC");
+  }
+  
+  @Override
+  public String getFilter() {
+    return "marionsc911@gmail.com";
+  }
+
+  @Override
+  protected boolean parseHtmlMsg(String subject, String body, Data data) {
+    if (!super.parseHtmlMsg(subject, body, data)) return false;
+    if (data.strPlace.equals("SC - -")) data.strPlace = "";
+    return true;
+  }
+
+}
