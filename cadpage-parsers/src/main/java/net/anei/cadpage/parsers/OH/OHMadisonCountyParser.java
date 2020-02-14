8b9bc8ac15538a702a7915cc47e2153ad328f142
Ken Corbin <ken@cadpage.org>
2020-02-14 14:35:05 -0800

    Fixed parsing problem with Madison County, OH

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/OH/OHMadisonCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/OH/OHMadisonCountyParser.java
@@ -1,5 +1,6 @@
 package net.anei.cadpage.parsers.OH;
 
+import net.anei.cadpage.parsers.MsgInfo.Data;
 import net.anei.cadpage.parsers.dispatch.DispatchA1Parser;
 
 /**
@@ -11,6 +12,17 @@ public class OHMadisonCountyParser extends DispatchA1Parser {
     super("MADISON COUNTY", "OH"); 
   }
   
+  @Override
+  protected boolean parseMsg(String subject, String body, Data data) {
+    if (subject.length() == 0 && body.startsWith("Alert:")) {
+      int pt = body.indexOf('\n');
+      if (pt < 0) return false;
+      subject = body.substring(0,pt).trim();
+      body = body.substring(pt+1).trim();
+    }
+    return super.parseMsg(subject, body, data);
+  }
+
   @Override
   public String getFilter() {
     return "mcsocad@madisonsheriff.org,777";
