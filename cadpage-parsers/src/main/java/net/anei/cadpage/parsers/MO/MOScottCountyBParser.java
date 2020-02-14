1c202a9d5e9e804e19723260e6047019b4544a5d
Ken Corbin <ken@cadpage.org>
2020-02-14 15:45:41 -0800

    Fixed parsing problem with Scott County, MO (B)

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/MO/MOScottCountyBParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/MO/MOScottCountyBParser.java
@@ -1,5 +1,10 @@
 package net.anei.cadpage.parsers.MO;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import net.anei.cadpage.parsers.FieldProgramParser;
 import net.anei.cadpage.parsers.MsgInfo.Data;
 
@@ -7,7 +12,12 @@ public class MOScottCountyBParser extends FieldProgramParser {
   
   public MOScottCountyBParser() {
     super("SCOTT COUNTY", "MO", 
-          "CALL:CALL! ADDR:ADDR! CITY:CITY! ID:ID! DATE:DATE! TIME:TIME! UNIT:UNIT END");
+          "ID DATETIME! Situation:CALL! Address:ADDRCITY! CALL/SDS Notes:INFO/N+");
+  }
+  
+  @Override
+  public String getFilter() {
+    return "SIKESTONDPS@OMNIGO.COM";
   }
   
   @Override
@@ -17,9 +27,31 @@ public class MOScottCountyBParser extends FieldProgramParser {
   
   @Override
   public Field getField(String name) {
-    if (name.equals("DATE")) return new DateField("\\d\\d?/\\d\\d?/\\d{4}", true);
-    if (name.equals("TIME")) return new TimeField("\\d\\d?:\\d\\d:\\d\\d", true);
+    if (name.equals("ID")) return new IdField("\\d\\d-\\d{5}", true);
+    if (name.equals("DATETIME")) return new MyDateTimeField();
+    if (name.equals("ADDRCITY")) return new MyAddressCityField();
     return super.getField(name);
   }
-
+  
+  private static final Pattern DATE_TIME_PTN = Pattern.compile("(\\d\\d?/\\d\\d?/\\d{4}) +(\\d\\d?:\\d\\d:\\d\\d [AP]M)");
+  private static final DateFormat TIME_FMT = new SimpleDateFormat("hh:mm:ss aa");
+  private class MyDateTimeField extends DateTimeField {
+    @Override
+    public void parse(String field, Data data) {
+      Matcher match = DATE_TIME_PTN.matcher(field);
+      if (!match.matches()) abort();
+      data.strDate = match.group(1);
+      setTime(TIME_FMT, match.group(2), data);
+    }
+  }
+  
+  private class MyAddressCityField extends AddressCityField {
+    @Override
+    public void parse(String field, Data data) {
+      int pt = field.lastIndexOf("   ");
+      if (pt < 0) abort();
+      parseAddress(field.substring(0,pt).trim(), data);
+      data.strCity = field.substring(pt+3);
+    }
+  }
 }
