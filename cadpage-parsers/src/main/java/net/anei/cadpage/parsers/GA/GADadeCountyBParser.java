a24ddf1be181d2b9c5fdb5101ee36239987ffdb6
Ken Corbin <ken@cadpage.org>
2020-03-12 08:59:05 -0700

    Fixed parsing probelm with Dade County, GA

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/GA/GADadeCountyBParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/GA/GADadeCountyBParser.java
@@ -6,8 +6,9 @@ import net.anei.cadpage.parsers.dispatch.DispatchH05Parser;
 public class GADadeCountyBParser extends DispatchH05Parser {
   
   public  GADadeCountyBParser() {
-    super("DADE COUNTY", "GA", 
-          "DATETIME ID ADDRCITY FIRE_CALL_TYPE EMS_CALL_TYPE! UNIT TABLE! TIMES+? TABLE< INFO_BLK+", "table");
+    super("DADE COUNTY", "GA",
+          "( SELECT/1 Date:DATETIME! Fire_Call:CALL_TYPE! EMS_Call:CALL_TYPE! Addr:ADDRCITY/S6! Common_Name:PLACE! Cross_St:X! Nature_of_Call:CALL/SDS! Unit:UNIT! Primary_Incident:ID! Info:INFO! INFO/N+ " + 
+          "| DATETIME ID2 ADDRCITY2/S6 FIRE_CALL_TYPE EMS_CALL_TYPE! UNIT TABLE! TIMES+? TABLE< INFO_BLK+ )", "table");
   }
   
   @Override
@@ -16,26 +17,42 @@ public class GADadeCountyBParser extends DispatchH05Parser {
   }
   
   @Override
+  protected boolean parseHtmlMsg(String subject, String body, Data data) {
+    if (subject.equals("!")) {
+      setSelectValue("1");
+      body = body.replace(" Common Name:", "\nCommon Name:");
+      return parseFields(body.split("\n"), data);
+    } else {
+      setSelectValue("2");
+      return super.parseHtmlMsg(subject, body, data);
+    }
+  }
+
+  @Override
   public Field getField(String name) {
-    if (name.equals("ID")) return new IdField("Incident # *(.*)", true);
-    if (name.equals("ADDRCITY")) return new AddressCityField("Location +(.*)", true);
-    if (name.equals("FIRE_CALL_TYPE")) return new MyCallField("Fire Call Type");
-    if (name.equals("EMS_CALL_TYPE")) return new MyCallField("EMS Call Type");
+    if (name.equals("DATETIME")) return new DateTimeField("\\d\\d?/\\d\\d?/\\d{4} \\d\\d?:\\d\\d:\\d\\d", true);
+    if (name.equals("ID2")) return new IdField("Incident # *(.*)", true);
+    if (name.equals("ADDRCITY2")) return new AddressCityField("Location +(.*)", true);
+    if (name.equals("CALL_TYPE")) return new MyCallTypeField(null);
+    if (name.equals("FIRE_CALL_TYPE")) return new MyCallTypeField("Fire Call Type");
+    if (name.equals("EMS_CALL_TYPE")) return new MyCallTypeField("EMS Call Type");
     if (name.equals("TABLE")) return new SkipField("<table>");
     return super.getField(name);
   }
   
-  private class MyCallField extends CallField {
+  private class MyCallTypeField extends CallField {
     private String label;
     
-    public MyCallField(String label) {
+    public MyCallTypeField(String label) {
       this.label = label;
     }
     
     @Override
     public void parse(String field, Data data) {
-      if (!field.startsWith(label)) abort();
-      field = field.substring(label.length()).trim();
+      if (label != null) {
+        if (!field.startsWith(label)) abort();
+        field = field.substring(label.length()).trim();
+      }
       if (!field.equals(data.strCall)) data.strCall = append(data.strCall, "/", field);
     }
   }
