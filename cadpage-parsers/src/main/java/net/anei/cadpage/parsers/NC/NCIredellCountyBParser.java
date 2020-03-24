5ce5dc0a126df12972aaaa6c0bc06ddcce1a1aec
Ken Corbin <ken@cadpage.org>
2020-03-24 13:19:23 -0700

    Fixed parsing problem with Iredell County, NC (B)

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/NC/NCIredellCountyBParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/NC/NCIredellCountyBParser.java
@@ -11,9 +11,9 @@ public class NCIredellCountyBParser extends DispatchOSSIParser {
   
   public NCIredellCountyBParser() {
     super(CITY_CODES, "IREDELL COUNTY", "NC", 
-          "( UNIT/Z ENROUTE ADDR CITY CALL SRC END " + 
-          "| CALL ADDR/Z CITY/Y! INFO/N+? ( UNIT2 SRC | SRC ) END " +
-          "| CALL PRI ADDR! X X INFO/N+? ( UNIT2 SRC | SRC ) END )");
+          "FYI? ( UNIT/Z ENROUTE ADDR CITY CALL SRC END " + 
+               "| CALL ADDR/Z CITY/Y! INFO/N+? ( UNIT2 SRC | SRC ) END " +
+               "| UNIT1? CALL PRI ADDR! X X INFO/N+? ( UNIT2 SRC | SRC ) END )");
   }
   
   @Override
@@ -44,12 +44,19 @@ public class NCIredellCountyBParser extends DispatchOSSIParser {
   public Field getField(String name) {
     if (name.equals("ENROUTE")) return new SkipField("Enroute", true);
     if (name.equals("PRI")) return new PriorityField("[P1-9]?", true);
+    if (name.equals("UNIT1"))  return new MyUnit1Field();
     if (name.equals("UNIT2")) return new MyUnit2Field();
-    if (name.equals("SRC")) return new SourceField("[a-z]+\\d*", true);
+    if (name.equals("SRC")) return new MySourceField();
     return super.getField(name);
   }
   
-  private static final Pattern UNIT_PTN2 = Pattern.compile("[A-Z]+,[A-Z][A-Z0-9,]*");
+  private static final Pattern UNIT_PTN2 = Pattern.compile("[A-Z]{3,4}|[A-Z]+[,0-9][A-Z0-9,]*");
+  private class MyUnit1Field extends UnitField {
+    public MyUnit1Field() {
+      setPattern(UNIT_PTN2, true);
+    }
+  }
+  
   private class MyUnit2Field extends UnitField {
     @Override
     public boolean canFail() {
@@ -70,8 +77,31 @@ public class NCIredellCountyBParser extends DispatchOSSIParser {
     }
   }
   
+  private static Pattern SRC_PTN = Pattern.compile("[a-z]+\\d*");
+  private class MySourceField extends SourceField {
+    @Override
+    public boolean canFail() {
+      return true;
+    }
+    
+    @Override
+    public boolean checkParse(String field, Data data) {
+      if (!isLastField()) return false;
+      if (!SRC_PTN.matcher(field).matches()) return false;
+      super.parse(field, data);
+      return true;
+    }
+    
+    @Override
+    public void parse(String field, Data data) {
+      if (!checkParse(field, data)) abort();
+    }
+    
+  }
+  
   private static final Properties CITY_CODES = buildCodeTable(new String[]{
       "CLE", "CLEVELANE",
+      "HAM", "HAMPTONVILLE",
       "HAR", "HARMONY",
       "MOC", "MOCKSVILLE",
       "MOR", "MOORESVILLE",
@@ -79,7 +109,9 @@ public class NCIredellCountyBParser extends DispatchOSSIParser {
       "STA", "STATESVILLE",
       "STO", "STONEY POINT",
       "TRO", "TROUTMAN",
-      "OLI", "OLIN TWP"
+      "OLI", "OLIN TWP",
+      "UNG", "UNION GROVE TWP",
+      "XRO", "MOUNT ULLA TWP",
   });
 
 }
