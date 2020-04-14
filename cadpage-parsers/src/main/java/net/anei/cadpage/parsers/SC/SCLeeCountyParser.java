38901cd48552a142d939f222204854cde9968985
Ken Corbin <ken@cadpage.org>
2020-04-13 17:40:27 -0700

    Fixed parsing problem with Lee County, SC

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/SC/SCLeeCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/SC/SCLeeCountyParser.java
@@ -1,41 +1,12 @@
 
 package net.anei.cadpage.parsers.SC;
 
-import net.anei.cadpage.parsers.MsgInfo.Data;
-import net.anei.cadpage.parsers.dispatch.DispatchSouthernParser;
+import net.anei.cadpage.parsers.dispatch.DispatchA71Parser;
 
 
-public class SCLeeCountyParser extends DispatchSouthernParser {
+public class SCLeeCountyParser extends DispatchA71Parser {
 
   public SCLeeCountyParser() {
-    super(CITY_LIST, "LEE COUNTY", "SC", DSFLAG_ID_OPTIONAL | DSFLAG_FOLLOW_CROSS);
+    super("LEE COUNTY", "SC");
   }
-  
-  @Override
-  public String getFilter() {
-    return "dispatch@leecountysc.org";
-  }
-
-  @Override
-  protected boolean parseMsg(String body, Data data) {
-    if (!super.parseMsg(body, data)) return false;
-    data.strCross = stripFieldEnd(data.strCross, " INTERSECTION");
-    return true;
-  }
-
-
-  private static final String[] CITY_LIST = new String[]{
-      "BISHOPVILLE",
-      "DALZELL",
-      "LYNCHBURG",
-      
-      // Darlington County
-      "HARTSVILLE",
-      
-      // Kershaw County
-      "CAMDEN",
-      
-      // Sumpter County
-      "REMBERT"
-  };
 }
