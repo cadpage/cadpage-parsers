ccca44a622ce47985fc98b54a87c9ccaf3c1ce1a
Ken Corbin <ken@cadpage.org>
2020-05-05 19:17:14 -0700

    Added GPS lookup for Redwood County, MN

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/MN/MNRedwoodCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/MN/MNRedwoodCountyParser.java
@@ -1,10 +1,22 @@
 package net.anei.cadpage.parsers.MN;
 
+import java.util.Properties;
+
 import net.anei.cadpage.parsers.dispatch.DispatchA43Parser;
 
 public class MNRedwoodCountyParser extends DispatchA43Parser {
   
   public MNRedwoodCountyParser() {
     super("REDWOOD COUNTY", "MN");
+    setupGpsLookupTable(GPS_LOOKUP_TABLE);
   }
+  
+  @Override
+  protected String adjustGpsLookupAddress(String address) {
+    return address.toUpperCase();
+  }
+
+  private static final Properties GPS_LOOKUP_TABLE = buildCodeTable(new String[]{
+      "16626 KENWOOD AVE",                    "+44.291477,-95.189718"
+  });
 }
