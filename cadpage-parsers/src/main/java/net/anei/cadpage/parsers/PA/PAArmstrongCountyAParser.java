5132316da8a7d46bed3408a02195e03667325106
Ken Corbin <ken@cadpage.org>
2020-05-09 14:31:25 -0700

    Fixed parsing problem with Armstrong County, PA (A)

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/PA/PAArmstrongCountyAParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/PA/PAArmstrongCountyAParser.java
@@ -1,5 +1,6 @@
 package net.anei.cadpage.parsers.PA;
 
+import java.util.Properties;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -13,16 +14,20 @@ public class PAArmstrongCountyAParser extends FieldProgramParser {
 
   // Marker is time and run number at end of message
   private static final Pattern MARKER_PATTERN1 = Pattern.compile(" +(\\d+) +(\\d\\d:\\d\\d)$");
-  private static final Pattern MARKER_PATTERN2 = Pattern.compile(" +(\\d{1,6}) (\\d\\d:\\d\\d)(?: (\\d{10}))?(?: +([A-Z0-9]+))?$");
   
   public PAArmstrongCountyAParser() {
-    super("ARMSTRONG COUNTY", "PA",
-          "CALL PLACE? ADDR/Z ( X CITY END | CITY/Z X/Z! END )");
+    super(CITY_CODES, "ARMSTRONG COUNTY", "PA",
+          "CALL PLACE ( ADDR2/ZiS GPS! | ADDR1 CITY! ) X+");
   }
   
   @Override
   public String getFilter() {
     return "911Dispatch@co.armstrong.pa.us,2183500107";
+  }
+  
+  @Override
+  public int getMapFlags() {
+    return MAP_FLG_PREFER_GPS;
   }
 
   @Override
@@ -38,79 +43,19 @@ public class PAArmstrongCountyAParser extends FieldProgramParser {
     
     // There are two different formats we process.
     // First look for the current one
-    if (subject.equals("Dispatch")) {
-      Matcher match = MARKER_PATTERN1.matcher(body);
-      if (match.find()) {
-        data.strCallId = match.group(1);
-        data.strTime = match.group(2);
-        body = body.substring(0,match.start());
-        
-        // There are two versions of this format.
-        // Newer one uses // as field delimiters
-        String[] flds = body.split("//");
-        if (flds.length >= 4) {
-          return parseFields(flds, data);
-        }
-        
-        // Older one has same fields parsed by hand
-        setFieldList("CALL PLACE ADDR X");
-        
-        parseAddress(StartType.START_CALL, FLAG_START_FLD_REQ | FLAG_CROSS_FOLLOWS | FLAG_NO_IMPLIED_APT, body, data);
-        data.strCross = getLeft();
-        
-        pt = data.strCall.indexOf("  ");
-        if (pt >= 0) {
-          data.strPlace = data.strCall.substring(pt+2).trim();
-          data.strCall = data.strCall.substring(0,pt);
-        }
-        return true;
-      }
+    if (!subject.equals("Dispatch")) return false;
+    Matcher match = MARKER_PATTERN1.matcher(body);
+    if (!match.find()) return false;
+    data.strCallId = match.group(1);
+    data.strTime = match.group(2);
+    body = body.substring(0,match.start());
+    
+    if (!parseFields(body.split("//"), data)) return false;
+    if (data.strAddress.length() == 0) {
+      data.strAddress = data.strGPSLoc;
+      data.strGPSLoc = "";
     }
-    
-    
-    // No go, see if this matches an older format
-    Matcher match = MARKER_PATTERN2.matcher(body);
-    if (match.find()) {
-      setFieldList("ADDR X PLACE ID TIME PHONE CALL");
-      
-      data.strCallId = match.group(1);
-      data.strTime = match.group(2);
-      data.strPhone = getOptGroup(match.group(3));
-      data.strCall = getOptGroup(match.group(4));
-      body = body.substring(0,match.start());
-      
-      if (data.strCall.length() == 0) {
-        Parser p = new Parser(body);
-        data.strCall = p.getLast(' ');
-        body = p.get();
-      }
-      
-      
-      // We need to call the smart parser twice, once for the real address
-      // and a second time to get the cross streets (which look like intersections)
-      
-      body = body.replace(",", "/");
-      parseAddress(StartType.START_ADDR, body, data);
-      body = getLeft();
-      do {
-        if (body.startsWith("/")) body = body.substring(1).trim();
-        Result result2 = parseAddress(StartType.START_ADDR, FLAG_ONLY_CROSS, body);
-        if (result2.isValid()) {
-          String oldCross = data.strCross;
-          data.strCross = "";
-          result2.getData(data);
-          data.strCross = append(oldCross, " / ", data.strCross);
-          body = result2.getLeft();
-        }
-      } while (body.startsWith("/"));
-      
-      data.strPlace = body;
-  
-      return true;
-    }
-    
-    // Not luck on either format
-    return false;
+    return true;
   }
   
   @Override
@@ -121,31 +66,55 @@ public class PAArmstrongCountyAParser extends FieldProgramParser {
   @Override
   public Field getField(String name) {
     if (name.equals("CALL")) return new MyCallField();
+    if (name.equals("ADDR1")) return new AddressField("(.+?) *(?:&|(?i)AND)", false);
+    if (name.equals("ADDR2")) return new MyAddress2Field();
     if (name.equals("CITY")) return new MyCityField();
-    if (name.equals("X")) return new CrossField(".*(?:/| and ).*");
+    if (name.equals("GPS")) return new GPSField("-361 -361|(\\d+\\.\\d+ +-\\d+\\.\\d+)", true);
+    if (name.equals("X")) return new MyCrossField();
     return super.getField(name);
   }
   
-  private static final Pattern CALL_CH_PLACE_PTN = Pattern.compile("(.*?) *\\b((?:EMS )?TAC ?\\d|EMS ?\\d|ET ?\\d|CW ?\\d|FIRE FT\\d|FRE \\d)\\b *(.*?)");
+  private static final Pattern CALL_BRK_PTN = Pattern.compile(" *\n *");
+  private static final Pattern CALL_CH_PLACE_PTN = Pattern.compile("(.*?) *\\b((?:EMS )?TAC ?\\d{1,2}|FIRE TAC ?\\d{1,2}(?: EMS TAC ?\\d{1,2})?|EMS ?\\d{1,2}|ET ?\\d{1,2}|CW ?\\d{1,2}|FIRE FT\\d{1,2}|FI?RE \\d{1,2}(?: EMS ?\\d{1,2})?)\\b *(.*?)");
   private class MyCallField extends CallField {
     @Override
     public void parse(String field, Data data) {
+      field = CALL_BRK_PTN.matcher(field).replaceAll(" ");
       Matcher match = CALL_CH_PLACE_PTN.matcher(field);
       if (match.matches()) {
-        field = match.group(1);
+        field = append(match.group(1), " - ", match.group(3));
         data.strChannel = match.group(2);
-        data.strPlace = match.group(3);
       }
       super.parse(field, data);
     }
     
     @Override
     public String getFieldNames() {
-      return "CALL CH PLACE";
+      return "CALL CH INFO";
     }
   }
   
   private static final Pattern CITY_BORO_PTN = Pattern.compile("(.*?) +BORO", Pattern.CASE_INSENSITIVE);
+  private class MyAddress2Field extends AddressField {
+    @Override
+    public void parse(String field, Data data) {
+      int pt = field.lastIndexOf('&');
+      if (pt >= 0) {
+        String city = field.substring(pt+1).trim();
+        field = field.substring(0,pt).trim();
+        Matcher match = CITY_BORO_PTN.matcher(city);
+        if (match.matches())city = match.group(1);
+        data.strCity = convertCodes(city, CITY_CODES);
+      }
+      super.parse(field, data);
+    }
+    
+    @Override
+    public String getFieldNames() {
+      return super.getFieldNames() + " CITY";
+    }
+  }
+  
   private class MyCityField extends CityField {
     @Override
     public void parse(String field, Data data) {
@@ -154,4 +123,17 @@ public class PAArmstrongCountyAParser extends FieldProgramParser {
       super.parse(field, data);
     }
   }
+  
+  private class MyCrossField extends CrossField {
+    @Override
+    public void parse(String field, Data data) {
+      if (field.equals("No Cross Streets Found")) return;
+      super.parse(field, data);
+    }
+  }
+  
+  private static final Properties CITY_CODES = buildCodeTable(new String[]{
+      "KITTG",     "KITTANNING",
+      "KITTG TWP", "KITTANNING TWP"
+  });
 }
