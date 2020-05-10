4b23a9c5aed242bd4259b3ded767aabb620e393e
Ken Corbin <ken@cadpage.org>
2020-05-09 17:13:02 -0700

    Fixed parsing problem with Yukon, OK

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/OK/OKYukonParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/OK/OKYukonParser.java
@@ -8,7 +8,8 @@ import net.anei.cadpage.parsers.HtmlProgramParser;
 
 public class OKYukonParser extends HtmlProgramParser {
   public OKYukonParser() {
-    super("YUKON", "OK", "CALL:CALL! PLACE:PLACE! ADDR:ADDRCITY! CROSS_ST:X! ID:ID! PRI:PRI! LAT/LON:GPS! INFO:INFO/N+");
+    super("YUKON", "OK", 
+          "CALL:CALL! PLACE:PLACE! ( ADDRESS:ADDRCITY | ADDR:ADDRCITY! ) CROSS_ST:X! ( INCIDENT_#:ID! | INCIDENT_NUMBER:ID! | ID:ID! ) PRI:PRI? LAT/LON:GPS! INFO:INFO/N+");
   }
 
   @Override
@@ -18,8 +19,17 @@ public class OKYukonParser extends HtmlProgramParser {
   
   @Override
   public Field getField(String name) {
+    if (name.equals("ID")) return new MyIdField();
     if (name.equals("INFO")) return new MyInfoField();
     return super.getField(name);
+  }
+  
+  private class MyIdField extends IdField {
+    @Override
+    public void parse(String field, Data data) {
+      if (field.startsWith("[Incident")) return;
+      super.parse(field, data);
+    }
   }
   
   private static final Pattern DATE_TIME_PTN = Pattern.compile("\\*{3}\\d\\d?/\\d\\d?/\\d{4}\\*{3}|\\d\\d?:\\d\\d:\\d\\d");
