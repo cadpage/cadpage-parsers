87c0bb67f481f4fa5fcc2f07c5a5f32de3a53ae2
Ken Corbin <ken@cadpage.org>
2020-05-09 17:16:55 -0700

    Update GLPS lookup table for Dodge County, MN

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/MN/MNDodgeCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/MN/MNDodgeCountyParser.java
@@ -38,10 +38,12 @@ public class MNDodgeCountyParser extends DispatchA27Parser {
   }
 
   private static final Properties GPS_LOOKUP_TABLE = buildCodeTable(new String[]{
+      "416 9TH ST W",                         "+44.071160,-92.761986",
       "24304 555TH ST",                       "+44.145333,-92.750888",
       "1000 BLANCH CT",                       "+44.072252,-92.754519",
       "1003 BLANCH CT",                       "+44.072405,-92.754666",
       "141 COUNTY RD 12",                     "+44.061981,-92.757681"
+
   });
   
 }
