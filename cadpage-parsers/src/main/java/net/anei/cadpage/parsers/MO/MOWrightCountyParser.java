3253ddfc618dd2cd99e0baf504a18c09311799d2
Ken Corbin <ken@cadpage.org>
2020-05-15 20:41:04 -0700

    Added GPS field for Wright County, MO

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/MO/MOWrightCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/MO/MOWrightCountyParser.java
@@ -7,12 +7,17 @@ public class MOWrightCountyParser extends FieldProgramParser {
   
   public MOWrightCountyParser() {
     super("WRIGHT COUNTY", "MO", 
-          "CALL:CALL! PLACE:PLACE! ADDR:ADDRCITY! CITY:CITY? X_STREET:X! NOTE:INFO/N+");
+          "CALL:CALL! PLACE:PLACE! ADDR:ADDRCITY! CITY:CITY? X_STREET:X! LAT/LON:GPS? NOTE:INFO/N+");
   }
   
   @Override
   public String getFilter() {
     return "dispatch@wrightcounty911.org";
+  }
+  
+  @Override
+  public int getMapFlags() {
+    return MAP_FLG_PREFER_GPS;
   }
   
   @Override
@@ -25,6 +30,7 @@ public class MOWrightCountyParser extends FieldProgramParser {
   public Field getField(String name) {
     if (name.equals("CITY")) return new MyCityField();
     if (name.equals("X")) return new MyCrossField();
+    if (name.equals("GPS")) return new MyGPSField();
     return super.getField(name);
   }
   
@@ -41,6 +47,14 @@ public class MOWrightCountyParser extends FieldProgramParser {
     @Override
     public void parse(String field, Data data) {
       if (field.equals("No Cross Streets Found")) return;
+      super.parse(field, data);
+    }
+  }
+  
+  private class MyGPSField extends GPSField {
+    @Override
+    public void parse(String field, Data data) {
+      field = field.replace("-", ",-");
       super.parse(field, data);
     }
   }
