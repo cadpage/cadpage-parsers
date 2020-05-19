1b15637c8eab04a1ca7f56ce662b340a7f0e4a5b
Ken Corbin <ken@cadpage.org>
2020-05-18 17:14:50 -0700

    Fixed minor issue with Wright County, MO

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/MO/MOWrightCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/MO/MOWrightCountyParser.java
@@ -7,7 +7,7 @@ public class MOWrightCountyParser extends FieldProgramParser {
   
   public MOWrightCountyParser() {
     super("WRIGHT COUNTY", "MO", 
-          "CALL:CALL! PLACE:PLACE! ADDR:ADDRCITY! CITY:CITY? X_STREET:X! LAT/LON:GPS? NOTE:INFO/N+");
+          "CALL:CALL! PLACE:PLACE! ADDR:ADDRCITY/S6! CITY:CITY? X_STREET:X! LAT/LON:GPS? NOTE:INFO/N+");
   }
   
   @Override
@@ -23,15 +23,31 @@ public class MOWrightCountyParser extends FieldProgramParser {
   @Override
   protected boolean parseMsg(String subject, String body, Data data) {
     if (!subject.equals("Dispatch")) return false;
-    return parseFields(body.split("\n"), data);
+    if (!parseFields(body.split("\n"), data)) return false;
+    if (data.strApt.equals("AVE")) {
+      data.strApt = "";
+    } else if (data.strApt.startsWith("BOX")) {
+      data.strAddress = append(data.strAddress, " ", data.strApt);
+      data.strApt = "";
+    }
+    return true;
   }
   
   @Override
   public Field getField(String name) {
+    if (name.equals("ADDRCITY")) return new MyAddressCityField();
     if (name.equals("CITY")) return new MyCityField();
     if (name.equals("X")) return new MyCrossField();
     if (name.equals("GPS")) return new MyGPSField();
     return super.getField(name);
+  }
+  
+  private class MyAddressCityField extends AddressCityField {
+    @Override
+    public void parse(String field, Data data) {
+      field = field.replace('@', '&');
+      super.parse(field, data);
+    }
   }
   
   private class MyCityField extends CityField {
