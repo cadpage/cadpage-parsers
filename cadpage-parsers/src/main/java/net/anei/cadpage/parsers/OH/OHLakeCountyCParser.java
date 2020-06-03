21654287ced192c479007782c149c043eb482417
Ken Corbin <ken@cadpage.org>
2020-06-02 21:41:34 -0700

    Fixed parsing problem with Lake County, OH

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/OH/OHLakeCountyCParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/OH/OHLakeCountyCParser.java
@@ -9,22 +9,55 @@ import net.anei.cadpage.parsers.MsgInfo.Data;
 public class OHLakeCountyCParser extends FieldProgramParser {
   
   public OHLakeCountyCParser() {
-    super("LAKE COUNTY", "OH", 
-          "CALL:CALL! PLACE:PLACE! ADDR:ADDR/S6! CITY:CITY! ID:ID! UNIT:UNIT! INFO:INFO/N+");
+    super(CITY_LIST, "LAKE COUNTY", "OH", 
+          "CALL ADDR ( CITY | PLACE CITY | ) INFO/CS+");
   }
   
   @Override
   public String getFilter() {
     return "info@sundance-sys.com";
   }
-  
-  private static final Pattern TRAIL_ID_PTN = Pattern.compile("\\s*FireNo *\\d*$");
+
   
   @Override
   protected boolean parseMsg(String subject, String body, Data data) {
-    if (!subject.equals("From: WillowickCAD")) return false;
-    Matcher match = TRAIL_ID_PTN.matcher(body);
-    if (match.find()) body = body.substring(0,match.start());
-    return parseFields(body.split("\n"), data);
+    if (!subject.equals("From: WillowickPager")) return false;
+    return parseFields(body.split(",", -1), 5, data);
   }
+  
+  @Override
+  public Field getField(String name) {
+    if (name.equals("CALL")) return new MyCallField();
+    if (name.equals("INFO")) return new MyInfoField();
+    return super.getField(name);
+  }
+  
+  private static final Pattern CODE_CALL_PTN = Pattern.compile("(\\w+)-(.*)");
+  private class MyCallField extends CallField {
+    @Override
+    public void parse(String field, Data data) {
+      Matcher match = CODE_CALL_PTN.matcher(field);
+      if (!match.matches()) abort();
+      data.strCode = match.group(1);
+      data.strCall = match.group(2).trim();
+    }
+    
+    @Override
+    public String getFieldNames() {
+      return "CODE CALL";
+    }
+  }
+  
+  private class MyInfoField extends InfoField {
+    @Override
+    public void parse(String field, Data data) {
+      for (String part : field.split(";")) {
+        data.strSupp = append(data.strSupp, "\n", part);
+      }
+    }
+  }
+  
+  private static final String[] CITY_LIST = new String[]{
+    "WILLOWICK"
+  };
 }
