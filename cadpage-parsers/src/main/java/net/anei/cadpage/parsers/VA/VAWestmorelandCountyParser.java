9ec4f0e7cd5afe042141f7a537b2320982d9f60a
Ken Corbin <ken@cadpage.org>
2020-06-15 20:00:53 -0700

    Fixed parsing problem with Westmoreland County, VA

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/VA/VAWestmorelandCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/VA/VAWestmorelandCountyParser.java
@@ -1,49 +1,10 @@
 package net.anei.cadpage.parsers.VA;
 
-import net.anei.cadpage.parsers.FieldProgramParser;
-import net.anei.cadpage.parsers.MsgInfo.Data;
+import net.anei.cadpage.parsers.dispatch.DispatchA71Parser;
 
-public class VAWestmorelandCountyParser extends FieldProgramParser {
+public class VAWestmorelandCountyParser extends DispatchA71Parser {
   
   public VAWestmorelandCountyParser() {
-    super("WESTMORELAND COUNTY", "VA", 
-          "CALL:CALL! ADDR:ADDR! CITY:CITY! ID:ID! PRI:PRI! DATE:DATE! TIME:TIME! NAME:NAME? PHONE:PHONE? X:X? UNIT:UNIT? INFO:INFO/N+");
-  }
-  
-  @Override
-  public boolean parseMsg(String body, Data data) {
-    return parseFields(body.split("\n"), data);
-  }
-  
-  @Override
-  public Field getField(String name) {
-    if (name.equals("ADDR")) return new MyAddressField();
-    if (name.equals("X")) return new MyCrossField();
-    if (name.equals("UNIT")) return new MyUnitField();
-    return super.getField(name);
-  }
-  
-  private class MyAddressField extends AddressField {
-    @Override
-    public void parse(String field, Data data) {
-      field = stripFieldEnd(field, ",");
-      super.parse(field, data);
-    }
-  }
-  
-  private class MyCrossField extends CrossField {
-    @Override
-    public void parse(String field, Data data) {
-      field = field.replace('@', '/');
-      super.parse(field, data);
-    }
-  }
-  
-  private class MyUnitField extends UnitField {
-    @Override
-    public void parse(String field, Data data) {
-      field = stripFieldEnd(field, ",");
-      super.parse(field, data);
-    }
+    super("WESTMORELAND COUNTY", "VA");
   }
 }
