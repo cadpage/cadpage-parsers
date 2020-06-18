ee461f3205044f80334817e670a1eade75c3f9e7
Ken Corbin <ken@cadpage.org>
2020-06-18 13:40:52 -0700

    Fixed parsing problem with Lake County, OH (C)

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/OH/OHLakeCountyCParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/OH/OHLakeCountyCParser.java
@@ -3,14 +3,13 @@ package net.anei.cadpage.parsers.OH;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import net.anei.cadpage.parsers.FieldProgramParser;
 import net.anei.cadpage.parsers.MsgInfo.Data;
+import net.anei.cadpage.parsers.dispatch.DispatchA24Parser;
 
-public class OHLakeCountyCParser extends FieldProgramParser {
+public class OHLakeCountyCParser extends DispatchA24Parser {
   
   public OHLakeCountyCParser() {
-    super(CITY_LIST, "LAKE COUNTY", "OH", 
-          "CALL ADDR ( CITY | PLACE CITY | ) INFO/CS+");
+    super("LAKE COUNTY", "OH");
   }
   
   @Override
@@ -18,46 +17,23 @@ public class OHLakeCountyCParser extends FieldProgramParser {
     return "info@sundance-sys.com";
   }
 
-  
+  private static final Pattern FIRENO_PTN = Pattern.compile("(.*) FireNo (\\d+)", Pattern.DOTALL);
   @Override
-  protected boolean parseMsg(String subject, String body, Data data) {
+  public boolean parseMsg(String subject, String body, Data data) {
     if (!subject.equals("From: WillowickPager")) return false;
-    return parseFields(body.split(",", -1), 5, data);
+    String fireno = "";
+    Matcher match = FIRENO_PTN.matcher(body);
+    if (match.matches()) {
+      body = match.group(1).trim();
+      fireno = match.group(2);
+    }
+    if (!super.parseMsg(body, data)) return false;
+    data.strCallId = append(data.strCallId, "/", fireno);
+    return true;
   }
   
   @Override
-  public Field getField(String name) {
-    if (name.equals("CALL")) return new MyCallField();
-    if (name.equals("INFO")) return new MyInfoField();
-    return super.getField(name);
+  public String getProgram() {
+    return super.getProgram() + " ID";
   }
-  
-  private static final Pattern CODE_CALL_PTN = Pattern.compile("(\\w+)-(.*)");
-  private class MyCallField extends CallField {
-    @Override
-    public void parse(String field, Data data) {
-      Matcher match = CODE_CALL_PTN.matcher(field);
-      if (!match.matches()) abort();
-      data.strCode = match.group(1);
-      data.strCall = match.group(2).trim();
-    }
-    
-    @Override
-    public String getFieldNames() {
-      return "CODE CALL";
-    }
-  }
-  
-  private class MyInfoField extends InfoField {
-    @Override
-    public void parse(String field, Data data) {
-      for (String part : field.split(";")) {
-        data.strSupp = append(data.strSupp, "\n", part);
-      }
-    }
-  }
-  
-  private static final String[] CITY_LIST = new String[]{
-    "WILLOWICK"
-  };
 }
