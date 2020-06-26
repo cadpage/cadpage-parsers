dda74049baf0885eeac0930cfd4e5bf7e53cf61a
Kenneth Corbin <ken@cadpage.org>
2020-06-26 16:39:47 -0700

    Added GPS lookkup table for Coffee COunty, TN

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/TN/TNCoffeeCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/TN/TNCoffeeCountyParser.java
@@ -1,5 +1,7 @@
 package net.anei.cadpage.parsers.TN;
 
+import java.util.Properties;
+
 import net.anei.cadpage.parsers.dispatch.DispatchA65Parser;
 
 
@@ -7,12 +9,17 @@ public class TNCoffeeCountyParser extends DispatchA65Parser {
   
   public TNCoffeeCountyParser() {
     super(CITY_LIST, "COFFEE COUNTY", "TN");
+    setupGpsLookupTable(GPS_LOOKUP_TABLE);
   }
   
   @Override
   public String getFilter() {
     return "coffeecotn@911email.net,geoconex@nlamerica.com,dispatch@911comm2.info,@coffeetne911.info";
   } 
+  
+  private static final Properties GPS_LOOKUP_TABLE = buildCodeTable(new String[] {
+      "21 POWELL RD",                         "+35.407222,-86.173306"
+  });
   
   private static final String[] CITY_LIST = new String[]{
 
