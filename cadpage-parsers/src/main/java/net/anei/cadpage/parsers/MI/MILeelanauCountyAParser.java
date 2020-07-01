edcd8e017123a8cac7ee0eae9013484a4de83072
Kenneth Corbin <ken@cadpage.org>
2020-07-01 11:50:20 -0700

    Added call code table for Leelanau County, MI (A&B)

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/MI/MILeelanauCountyAParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/MI/MILeelanauCountyAParser.java
@@ -4,22 +4,24 @@ import net.anei.cadpage.parsers.MsgInfo.Data;
 import net.anei.cadpage.parsers.dispatch.DispatchA19Parser;
 
 public class MILeelanauCountyAParser extends DispatchA19Parser {
-  
+
   public MILeelanauCountyAParser() {
     super(MILeelanauCountyParser.CITY_CODES, "LEELANAU COUNTY", "MI");
   }
-  
+
   @Override
   protected boolean parseMsg(String subject, String body, Data data) {
     if (!subject.equals("Rapid Notifications")) return false;
-    return super.parseMsg("", body, data);
+    if (!super.parseMsg("", body, data)) return false;
+    data.strCall = convertCodes(data.strCall, MILeelanauCountyParser.CALL_CODES);
+    return true;
   }
 
   @Override
   public String getFilter() {
     return "outgoing@co.leelanau.mi.us";
   }
-  
+
   @Override
   public int getMapFlags() {
     return MAP_FLG_PREFER_GPS;
--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/MI/MILeelanauCountyBParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/MI/MILeelanauCountyBParser.java
@@ -1,20 +1,28 @@
 package net.anei.cadpage.parsers.MI;
 
+import net.anei.cadpage.parsers.MsgInfo.Data;
 import net.anei.cadpage.parsers.dispatch.DispatchA77Parser;
 
 public class MILeelanauCountyBParser extends DispatchA77Parser {
-  
+
   public MILeelanauCountyBParser() {
     super("Rapid Notifications", MILeelanauCountyParser.CITY_CODES, "LEELANAU COUNTY", "MI");
   }
-  
+
   @Override
   public String getFilter() {
     return "outgoing@co.leelanau.mi.us";
   }
-  
+
   @Override
   public int getMapFlags() {
     return MAP_FLG_PREFER_GPS;
+  }
+
+  @Override
+  protected boolean parseMsg(String subject, String body, Data data) {
+    if (!super.parseMsg(subject, body, data)) return false;
+    data.strCall = convertCodes(data.strCall, MILeelanauCountyParser.CALL_CODES);
+    return true;
   }
 }
