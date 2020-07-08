9b7fd58f4e54b009ec3d5506c2c75268c0f0228e
Kenneth Corbin <ken@cadpage.org>
2020-07-07 21:30:48 -0700

    Ditto

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/Active911ParserTable.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/Active911ParserTable.java
@@ -353,6 +353,7 @@ public class Active911ParserTable {
       "US/GA/MeriweatherCounty",          "GAMeriwetherCounty",
       "US/GA/MFRE911",                    "GASchleyCounty",
       "US/GA/MurrayCounty",               "GAMurrayCounty",
+      "US/GA/OconeeCounty",               "GAOconeeCounty",
       "US/GA/PauldingCounty",             "GAPauldingCounty",
       "US/GA/PickensCo911",               "GAPickensCounty",
       "US/GA/PikeCounty",                 "GAPikeCounty",
