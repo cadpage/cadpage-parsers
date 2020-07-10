d5f4155e69568709c5ceef3a062e6c78dcc70c04
Kenneth Corbin <ken@cadpage.org>
2020-07-10 11:55:07 -0700

    Fixed parsing problem with Mason County, WV (Add B)

---
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/WV/WVMasonCountyBParser.java
@@ -0,0 +1,76 @@
+package net.anei.cadpage.parsers.WV;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import net.anei.cadpage.parsers.FieldProgramParser;
+import net.anei.cadpage.parsers.MsgInfo.Data;
+
+public class WVMasonCountyBParser extends FieldProgramParser {
+
+  public WVMasonCountyBParser() {
+    super("MASON COUNTY", "WV",
+          "CFS_#:ID! CALL_TYPE:CODE! LOC:ADDRCITY! LAT/LONG:GPS! RESP:UNIT! END");
+  }
+
+  @Override
+  public String getFilter() {
+    return "no-reply@zuercherportal.com";
+  }
+
+  @Override
+  public int getMapFlags() {
+    return MAP_FLG_PREFER_GPS;
+  }
+
+  @Override
+  protected boolean parseMsg(String subject, String body, Data data) {
+    if (subject.length() == 0) return false;
+    data.strCall = subject;
+    return super.parseMsg(body, data);
+  }
+
+  @Override
+  public String getProgram() {
+    return "CALL " + super.getProgram();
+  }
+
+  @Override
+  public Field getField(String name) {
+    if (name.equals("ADDRCITY")) return new MyAddressCityField();
+    if (name.equals("GPS")) return new MyGPSField();
+    return super.getField(name);
+  }
+
+  private static final Pattern ST_ZIP_PTN = Pattern.compile("([A-Z]{2})(?: +(\\d{5}))?");
+  private class MyAddressCityField extends AddressCityField {
+    @Override
+    public void parse(String field, Data data) {
+      Parser p = new Parser(field);
+      String zip =  null;
+      String city = p.getLastOptional(',');
+      Matcher match = ST_ZIP_PTN.matcher(city);
+      if (match.matches()) {
+        data.strState = match.group(1);
+        zip = match.group(2);
+        city = p.getLastOptional(',');
+      }
+      if (city.length() == 0 && zip != null) city = zip;
+      data.strCity = city;
+      parseAddress(p.get(), data);
+    }
+
+    @Override
+    public String getFieldNames() {
+      return "ADDR APT CITY ST";
+    }
+  }
+
+  private class MyGPSField extends GPSField {
+    @Override
+    public void parse(String field, Data data) {
+      field = field.replace("-", ",-");
+      super.parse(field, data);
+    }
+  }
+}
