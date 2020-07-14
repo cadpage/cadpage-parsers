35d378b228e1567c216d03e342214fe9434125b6
Kenneth Corbin <ken@cadpage.org>
2020-07-14 09:18:12 -0700

    Update sender filter for Bourbon County, KS

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/KS/KSBourbonCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/KS/KSBourbonCountyParser.java
@@ -3,14 +3,14 @@ package net.anei.cadpage.parsers.KS;
 import net.anei.cadpage.parsers.dispatch.DispatchBCParser;
 
 public class KSBourbonCountyParser extends DispatchBCParser {
-  
+
   public KSBourbonCountyParser() {
     super("BOURBON COUNTY", "KS");
   }
-  
+
   @Override
   public String getFilter() {
-    return "ACTIVE911@FSCITY.ORG";
+    return "ACTIVE911@FSCITY.ORG,ACTIVE911@OMNIGO.COM";
   }
 
 }
