fb48c21f0f5097018a984855fd5257aa3790b541
Kenneth Corbin <ken@cadpage.org>
2020-07-16 19:04:52 -0700

    Updated sender filter for Wayne County,  MO

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/MO/MOWayneCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/MO/MOWayneCountyParser.java
@@ -4,14 +4,14 @@ import net.anei.cadpage.parsers.dispatch.DispatchA33Parser;
 
 
 public class MOWayneCountyParser extends DispatchA33Parser {
-  
-  
+
+
   public MOWayneCountyParser() {
     super("WAYNE COUNTY", "MO");
   }
-  
+
   @Override
   public String getFilter() {
-    return "dispatch@wccd911.org";
+    return "dispatch@wccd911.org,WAYNECODISPATCH@itiusa.com";
   }
 }
--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/WV/WVHarrisonCountyAParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/WV/WVHarrisonCountyAParser.java
@@ -7,22 +7,22 @@ import net.anei.cadpage.parsers.MsgInfo.Data;
  * Harrison County, WV
  */
 public class WVHarrisonCountyAParser extends FieldProgramParser {
-  
+
   public WVHarrisonCountyAParser() {
     super("HARRISON COUNTY", "WV",
            "ID ADDR EMPTY EMPTY CITY X1 X! Map:MAP! EMPTY CALL EMPTY EMPTY EMPTY EMPTY ESN:BOX!");
   }
-  
+
   @Override
   public String getFilter() {
     return "911@harrco911.org,harrison@911page.net";
   }
-  
+
   @Override
   protected boolean parseMsg(String body, Data data) {
     return parseFields(body.split("\n"), data);
   }
-  
+
   @Override
   public Field getField(String name) {
     if (name.equals("X1")) return new CrossField("XStr- *(.*)",   true);
