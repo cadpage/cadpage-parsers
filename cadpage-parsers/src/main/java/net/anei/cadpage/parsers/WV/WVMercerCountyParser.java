59a90495c64ef46723b229c5cf9f64f1bcd06b8c
Kenneth Corbin <ken@cadpage.org>
2020-07-23 09:51:51 -0700

    Updated city code table for Mercer County, WV

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/WV/WVMercerCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/WV/WVMercerCountyParser.java
@@ -5,21 +5,48 @@ import java.util.Properties;
 import net.anei.cadpage.parsers.dispatch.DispatchA19Parser;
 
 public class WVMercerCountyParser extends DispatchA19Parser {
-  
+
   public WVMercerCountyParser() {
     super(CITY_CODES, "MERCER COUNTY", "WV");
   }
-  
+
   @Override
   public String getFilter() {
     return "dispatch@mercer911.com";
   }
-  
+
   private static final Properties CITY_CODES = buildCodeTable(new String[]{
+//      "BLU", "BLUEWELL",
+
+      "ATH", "ATHENS",
+      "BEE", "BEESON",
       "BLF", "BLUEFIELD",
-      "BLU", "BLUEWELL",
-      "COU", "COU",
-      "PRN", "PRINCETON"
+      "BLU", "BLUEFIELD",
+      "BRM", "BRAMWELL",
+      "CAM", "CAMP CREEK",
+      "COU", "MERCER COUNTY",
+      "FLT", "FLAT TOP",
+      "GAR", "GARDNER",
+      "GRV", "GREEN VALLEY",
+      "HER", "HERNDON",
+      "HIA", "HIAWATHA",
+      "HWA", "HIAWATHA",
+      "JUM", "JUMPING BRANCH",
+      "KEG", "KEGLEY",
+      "LER", "LERONA",
+      "LSH", "LASHMEET",
+      "MAY", "MAYBEURY",
+      "MNT", "MONTCALM",
+      "MTK", "MATOAKA",
+      "OAK", "OAKVALE",
+      "ODD", "ODD",
+      "PIP", "PIPESTEM",
+      "PRI", "PRINCETON",
+      "PRN", "PRINCETON",
+      "RCK", "ROCK",
+      "SPN", "SPANISHBURG",
+      "WLF", "WOLFE"
+
   });
 
 }
