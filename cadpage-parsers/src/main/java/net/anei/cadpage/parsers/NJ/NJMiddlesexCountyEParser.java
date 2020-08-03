db83cf092da4f4922055fab7896c77bac8deff92
Kenneth Corbin <ken@cadpage.org>
2020-08-03 10:13:50 -0700

    Fixed parsing problem with Middlesex County, NJ (E)

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/NJ/NJMiddlesexCountyEParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/NJ/NJMiddlesexCountyEParser.java
@@ -1,9 +1,10 @@
 package net.anei.cadpage.parsers.NJ;
 
+import net.anei.cadpage.parsers.MsgInfo.Data;
 import net.anei.cadpage.parsers.dispatch.DispatchA48Parser;
 
 public class NJMiddlesexCountyEParser extends DispatchA48Parser {
-  
+
   public NJMiddlesexCountyEParser() {
     super(null, "MIDDLESEX COUNTY", "NJ", FieldType.X);
   }
@@ -12,4 +13,17 @@ public class NJMiddlesexCountyEParser extends DispatchA48Parser {
   public String getFilter() {
     return "@Rutgers.edu";
   }
+
+  @Override
+  protected boolean parseMsg(String subject, String body, Data data) {
+    boolean badDate = false;
+    if (subject.startsWith("RipAndRun_NBFD_")) {
+      badDate = true;
+      subject = "As of 99/99/99 99:99:99";
+    }
+    if (!super.parseMsg(subject, body, data)) return false;
+    if (badDate) data.strDate = data.strTime = "";
+    return true;
+  }
+
 }
