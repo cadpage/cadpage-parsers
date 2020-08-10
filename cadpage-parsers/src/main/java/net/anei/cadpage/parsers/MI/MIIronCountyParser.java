0d07b47cf709d120e47979b6bfbc4f3c5a1fdbff
Kenneth Corbin <ken@cadpage.org>
2020-08-10 11:46:06 -0700

    Fixed parsing problem with Iron County and related locations in MI

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/MI/MIIronCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/MI/MIIronCountyParser.java
@@ -17,9 +17,10 @@ public class MIIronCountyParser extends DispatchOSSIParser {
 
   public MIIronCountyParser(String defCity, String defState) {
     super(defCity, defState,
-          "FYI? CALL MASH+? UNIT CITY INFO+");
+          "( CANCEL ADDR! INFO+ " +
+          "| FYI? CALL MASH+? UNIT! CITY? INFO+ )");
   }
-  
+
   @Override
   public String getAliasCode() {
     return "MIIronCounty";
@@ -29,12 +30,15 @@ public class MIIronCountyParser extends DispatchOSSIParser {
   public String getFilter() {
     return "CAD@up-911.com";
   }
-  
+
   private static final Pattern DATE_TIME_PTN = Pattern.compile("(.*)\n(\\d{4}/\\d{2}/\\d{2} \\d{2}:\\d{2}:\\d{2}[AP]M)");
   private static final DateFormat DATE_TIME_FMT = new SimpleDateFormat("yyyy/MM/dd hh:mm:ssaa");
 
   @Override
   protected boolean parseMsg(String body, Data data) {
+
+    if (!body.startsWith("CAD:")) body = "CAD:" + body;
+
     Matcher match = DATE_TIME_PTN.matcher(body);
     if (match.matches()) {
       body = match.group(1).trim();
@@ -43,30 +47,31 @@ public class MIIronCountyParser extends DispatchOSSIParser {
     if (!super.parseMsg(body, data)) return false;
     return data.strAddress.length() > 0;
   }
-  
+
   @Override
   public Field getField(String name) {
     if (name.equals("MASH")) return new MyMashField();
-    if (name.equals("UNIT")) return new UnitField("(?!FS\\d+)(?:[A-Z]?\\d+[A-Z]+|[A-Z]+\\d+)", true);
+    if (name.equals("CITY")) return new CityField("[ A-Z]+", true);
+    if (name.equals("UNIT")) return new UnitField("(?!FS\\d+)(?:[A-Z]?\\d+[A-Z]+|[A-Z]+\\d+|\\d{4}|[A-Z]{2}FD)", true);
     return super.getField(name);
   }
 
   // General place/address/cross/apt field processor
   private static final Pattern APT_PTN = Pattern.compile("(?:#|APT|LOT|RM|ROOM) *(.*)|\\d{1,4}[A-Z]?");
   private class MyMashField extends Field {
-    
+
     // Keep internal state so we know where we are
     int state = 0;
-    
-    @Override 
+
+    @Override
     public void parse(String field, Data data) {
-      
+
       // Initialize state for the first field
       if (data.strPlace.length() == 0 && data.strAddress.length() == 0) state = 0;
-      
+
       // And switch on current state
       switch (state) {
-      
+
       // Assume first field is address until we have a second to compare it to
       // We do have to save it in the address on the off chance that there is no
       // second field
@@ -74,22 +79,22 @@ public class MIIronCountyParser extends DispatchOSSIParser {
         parseAddress(field, data);
         state++;
         break;
-        
+
       // For the second field, we need to do some work to figure out whether the
-      // first field is a place name or address.  
+      // first field is a place name or address.
       case 1:
-        
-        // Either way, we will want to increment the state 
+
+        // Either way, we will want to increment the state
         state++;
-        
+
         // Compare the address status of the previous field and this field
-        // If this field status is a simple street name, assume it is a 
+        // If this field status is a simple street name, assume it is a
         // following cross street and eliminate it from consideration
         String prevField = getRelativeField(-1);
         int stat1 = checkAddress(prevField);
         int stat2 = checkAddress(field);
         if (stat2 == STATUS_STREET_NAME) stat2 = -1;
-        
+
         // This field does look like a better address
         // Save the first field in the place name and parse this one
         // as an address
@@ -99,12 +104,12 @@ public class MIIronCountyParser extends DispatchOSSIParser {
           parseAddress(field, data);
           break;
         }
-        
+
         // Otherwise, address is already parsed so we will fall through
         // to the next case to process this field
-        
+
       case 2:
-        
+
         // Anything else is either an apt or a cross street
         Matcher match = APT_PTN.matcher(field);
         if (match.matches()) {
@@ -112,7 +117,7 @@ public class MIIronCountyParser extends DispatchOSSIParser {
           if (apt == null) apt = field;
           data.strApt = append(data.strApt, "-", apt);
         }
-        
+
         else {
           data.strCross = append(data.strCross, " / ", field);
         }
