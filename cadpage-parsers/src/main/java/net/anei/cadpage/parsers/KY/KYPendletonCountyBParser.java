73cb7f9f507562c5f43e1c2061d7af53177e6436
Kenneth Corbin <ken@cadpage.org>
2020-08-13 21:46:38 -0700

    Updated GPS lookup table for Pendleton County, KY (B)

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/KY/KYPendletonCountyBParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/KY/KYPendletonCountyBParser.java
@@ -1,14 +1,22 @@
 package net.anei.cadpage.parsers.KY;
 
+import java.util.Properties;
+
 import net.anei.cadpage.parsers.dispatch.DispatchSPKParser;
 
 public class KYPendletonCountyBParser extends DispatchSPKParser {
   public KYPendletonCountyBParser() {
     super("PENDLETON COUNTY", "KY");
+    setupGpsLookupTable(GPS_LOOKUP_TABLE);
   }
 
   @Override
   public String getFilter() {
     return "noreply@interact911.com,noreply@public-safety-cloud.com,Ksp.NGCAD@ky.gov";
   }
+
+  private static final Properties GPS_LOOKUP_TABLE = buildCodeTable(new String[]{
+      "20 HI & DRY LN",                       "+38.631508,-84.315271"
+
+  });
 }
