2f07cf97f4e763365b4914268fe64808dabcc14d
Kenneth Corbin <ken@cadpage.org>
2020-08-14 08:00:10 -0700

    Fixed parsing problem with Atrim County, MI

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/MI/MIAntrimCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/MI/MIAntrimCountyParser.java
@@ -9,33 +9,34 @@ import net.anei.cadpage.parsers.dispatch.DispatchOSSIParser;
 
 
 public class MIAntrimCountyParser extends DispatchOSSIParser {
-  
+
   public MIAntrimCountyParser() {
     this("ANTRIM COUNTY", "MI");
   }
-  
+
   MIAntrimCountyParser(String defCity, String defState) {
     super(CITY_CODES, defCity, defState,
-          "FYI? CALL ADDR! ( CITY | X/Z CITY | X/Z X/Z CITY | X+? ) INFO+");
+          "FYI? CALL PRI? ADDR! ( CITY | X/Z CITY | X/Z X/Z CITY | X+? ) INFO+");
   }
-  
+
   @Override
   public String getFilter() {
     return "CAD@antrimcounty.org";
   }
-  
+
   @Override
   public Field getField(String name) {
+    if (name.equals("PRI")) return new PriorityField("[1-9P]");
     if (name.equals("INFO")) return new MyInfoField();
     return super.getField(name);
   }
-  
+
   private static final Pattern DATE_TIME_PTN = Pattern.compile("(\\d\\d?/\\d\\d?/\\d{4}) (\\d\\d:\\d\\d:\\d\\d)");
   private static final String DATE_TIME_STR = "NN/NN/NNNN NN:NN:NN";
   private class MyInfoField extends InfoField {
     @Override
     public void parse(String field, Data data) {
-      
+
       // See if this is a date time field
       Matcher match = DATE_TIME_PTN.matcher(field);
       if (match.matches()) {
@@ -45,7 +46,7 @@ public class MIAntrimCountyParser extends DispatchOSSIParser {
         }
         return;
       }
-      
+
       // Or if it is a truncated field at end of message
       else if (getRelativeField(+1).length() == 0) {
         if (DATE_TIME_STR.startsWith(field.replaceAll("\\d", "N"))) return;
@@ -53,8 +54,8 @@ public class MIAntrimCountyParser extends DispatchOSSIParser {
       super.parse(field, data);
     }
   }
-  
-  
+
+
   private static final Properties CITY_CODES = buildCodeTable(new String[]{
       "BANK",  "BANKS TWP",
       "CL",    "CENTRAL LAKE",
