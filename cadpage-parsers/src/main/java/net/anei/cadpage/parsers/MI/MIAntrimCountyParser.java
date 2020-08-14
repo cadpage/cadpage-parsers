b7c930d7bdceeff70dc42380bc0ecb7a388c9e7d
Kenneth Corbin <ken@cadpage.org>
2020-08-14 13:23:49 -0700

    Fixed parsing problem with Antrim County, MI

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/MI/MIAntrimCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/MI/MIAntrimCountyParser.java
@@ -16,7 +16,7 @@ public class MIAntrimCountyParser extends DispatchOSSIParser {
 
   MIAntrimCountyParser(String defCity, String defState) {
     super(CITY_CODES, defCity, defState,
-          "FYI? CALL PRI? ADDR! ( CITY | X/Z CITY | X/Z X/Z CITY | X+? ) INFO+");
+          "FYI? CALL PRI? ADDR! ( GPS1 GPS2 CITY! X+? | CITY! X+? | X/Z CITY! | X/Z X/Z CITY! | X+? ) INFO/N+? ( GPS1 GPS2 PRI |  PRI ) END");
   }
 
   @Override
@@ -25,8 +25,15 @@ public class MIAntrimCountyParser extends DispatchOSSIParser {
   }
 
   @Override
+  public int getMapFlags() {
+    return MAP_FLG_PREFER_GPS;
+  }
+
+  @Override
   public Field getField(String name) {
     if (name.equals("PRI")) return new PriorityField("[1-9P]");
+    if (name.equals("GPS1")) return new GPSField(1, "[-+]?\\d{2}\\.\\d{6,}", true);
+    if (name.equals("GPS2")) return new GPSField(2, "[-+]?[0-9.]*", true);
     if (name.equals("INFO")) return new MyInfoField();
     return super.getField(name);
   }
@@ -36,6 +43,21 @@ public class MIAntrimCountyParser extends DispatchOSSIParser {
   private class MyInfoField extends InfoField {
     @Override
     public void parse(String field, Data data) {
+
+      // Look for wayward city codes
+      if (data.strCity.length() == 0) {
+        String city = CITY_CODES.getProperty(field);
+        if (city != null) {
+          data.strCity = city;
+          return;
+        }
+      }
+
+      // See if this is a radio channel
+      if (field.startsWith("Radio Channel:")) {
+        data.strChannel = field.substring(14).trim();
+        return;
+      }
 
       // See if this is a date time field
       Matcher match = DATE_TIME_PTN.matcher(field);
@@ -53,26 +75,49 @@ public class MIAntrimCountyParser extends DispatchOSSIParser {
       }
       super.parse(field, data);
     }
+
+    @Override
+    public String getFieldNames() {
+      return "CH DATE TIME " + super.getFieldNames() + " CITY";
+    }
   }
 
 
   private static final Properties CITY_CODES = buildCodeTable(new String[]{
+      "BEV",   "BELLAIRE",
+      "BKT",   "BANKS TWP",
+      "CLT",   "CENTRAL LAKE TWP",
+      "CLV",   "CENTRAL LAKE",
+      "CHT",   "CHESTONIA TWP",
+      "CUT",   "CUSTER TWP",
+      "ECT",   "ECHO TWP",
+      "ELL",   "ELLSWORTH",
+      "ERT",   "ELK RAPIDS TWP",
+      "ERV",   "ELK RAPIDS",
+      "FHT",   "FOREST HOME TWP",
+      "HLT",   "HELENA TWP",
+      "JDT",   "JORDAN TWP",      // Guess to fill in missing entry
+      "KRT",   "KEARNEY TWP",
+      "MIT",   "MILTON TWP",
+      "MNT",   "MANCELONA TWP",
+      "MNV",   "MANCELONA",
+      "STT",   "STAR TWP",
+      "TLT",   "TORCH LAKE TWP",
+      "WRT",   "WARNER TWP",
+
+      // old codes
       "BANK",  "BANKS TWP",
       "CL",    "CENTRAL LAKE",
-      "CLT",   "CENTRAL LAKE TWP",
-      "CHT",   "CHESTONIA TWP",
       "CUST",  "CUSTER TWP",
       "ECHO",  "ECHO TWP",
       "ELLV",  "ELLSORTH",
-      "ERT",   "ELK RAPIDS TWP",
-      "FHT",   "FOREST HOME TWP",
+      "HELE",  "HELENA TWP",
       "JORD",  "JORDAN TWP",
       "KERN",  "KEARNEY TWP",
       "MAN",   "MANCELONA TWP",
       "MANV",  "MANCEOLNA",
       "MILT",  "MILTON TWP",
       "STAR",  "STAR TWP",
-      "TLT",   "TORCH LAKE TWP",
       "VBL",   "BELLAIRE",
       "VER",   "ELK RAPIDS",
       "WARN",  "WARNER TWP"
