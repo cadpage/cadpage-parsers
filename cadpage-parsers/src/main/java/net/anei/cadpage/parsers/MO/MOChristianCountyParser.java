ef9ad040b2610f581cfeeef432fbfc2bb063ba4e
Kenneth Corbin <ken@cadpage.org>
2020-08-20 20:38:15 -0700

    Fixed parsing problem with Christian County, MO

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/MO/MOChristianCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/MO/MOChristianCountyParser.java
@@ -1,22 +1,18 @@
 package net.anei.cadpage.parsers.MO;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
+import net.anei.cadpage.parsers.FieldProgramParser;
 import net.anei.cadpage.parsers.MsgInfo.Data;
-import net.anei.cadpage.parsers.dispatch.DispatchPrintrakParser;
 
 /**
  * Christian County, MO
  */
-public class MOChristianCountyParser extends DispatchPrintrakParser {
-  
-  private static final Pattern TRAIL_DATE_TIME_PTN = Pattern.compile(" +(\\d\\d/\\d\\d/\\d\\d) +(\\d\\d:\\d\\d)$");
-  
+public class MOChristianCountyParser extends FieldProgramParser {
+
   public MOChristianCountyParser() {
-    super(CITY_LIST, "CHRISTIAN COUNTY", "MO", FLG_VERSION_2);
+    super("CHRISTIAN COUNTY", "MO", 
+          "CALL:CALL! PLACE:PLACE! ADDR:ADDRCITY! ID:ID! XST:X! DATE:DATETIME! UNIT:UNIT! INFO:INFO! INFO/N+");
   }
-  
+
   @Override
   public String getFilter() {
     return "ccespage@cces911.org";
@@ -24,83 +20,13 @@ public class MOChristianCountyParser extends DispatchPrintrakParser {
   
   @Override
   protected boolean parseMsg(String subject, String body, Data data) {
-    if (!subject.equals("INC:")) return false;
-    Matcher match = TRAIL_DATE_TIME_PTN.matcher(body);
-    if (match.find()) {
-      data.strDate = match.group(1);
-      data.strTime = match.group(2);
-      body = body.substring(0,match.start());
-    }
-    if (!super.parseMsg(body, data)) return false;
-    
-    // It seems that intersections are going in the place field
-    if (data.strPlace.contains(" / ")) {
-      data.strAddress = "";
-      parseAddress(data.strPlace, data);
-      data.strPlace = "";
-    }
-    
-    return true;
+    if (!subject.equals("!")) return false;
+    return parseFields(body.split("\n"), data);
   }
   
   @Override
-  public String adjustMapAddress(String sAddr) {
-    String[] parts =   sAddr.split("&");
-    if (parts.length > 1) {
-      String result = "";
-      for (String part : parts) {
-        part = BOUND_PTN.matcher(part).replaceAll("").trim();
-        if (NUMERIC.matcher(part).matches()) part = "RT " + part;
-        result = append(result, " & ", part);
-      }
-      sAddr = result;
-    }
-    return sAddr;
+  public Field getField(String name) {
+    if (name.equals("DATETIME")) return new DateTimeField("\\d\\d?/\\d\\d?/\\d{4} \\d\\d:\\d\\d:\\d\\d", true);
+    return super.getField(name);
   }
-  private static final Pattern BOUND_PTN = Pattern.compile("(?<=\\b|\\d)[NSEW]B(?=\\b|\\d)");
-  
-  
-  @Override
-  public String getProgram() {
-    return super.getProgram() + " DATE TIME";
-  }
-  
-  private static final String[] CITY_LIST = new String[]{
-    "BILLINGS",
-    "BOAZ",
-    "BATTLEFIELD",
-    "BRADLEYVILLE",
-    "BROOKLINE",
-    "BRUNER",
-    "CHADWICK",
-    "CHESTNUTRIDGE",
-    "CLEVER",
-    "CRANE",
-    "ELKHEAD",
-    "FREMONT HILLS",
-    "FORDLAND",
-    "FORSYTH",
-    "GALENA",
-    "GARRISON",
-    "HIGHLANDVILLE",
-    "HURLEY",
-    "KELTNER",
-    "LINDEN",
-    "MARIONVILLE",
-    "MT VERNON",
-    "NIXA",
-    "OLDFIELD",
-    "OZARK",
-    "PONCE DE LEON",
-    "REEDS SPRING",
-    "REPUBLIC",
-    "ROGERSVILLE",
-    "SADDLEBROOKE",
-    "SPARTA",
-    "SPOKANE",
-    "SPRINGFIELD",
-    "TANEYVILLE",
-    
-    "LAWRENCE COUNTY",
-  };
 }
