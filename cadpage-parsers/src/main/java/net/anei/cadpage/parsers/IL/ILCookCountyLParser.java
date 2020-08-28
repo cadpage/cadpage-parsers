fdb54a6e77e06b731cd64014c4d2a1371ca8d942
Kenneth Corbin <ken@cadpage.org>
2020-08-27 17:11:12 -0700

    Update sender filter for Cook County,IL (L)

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/IL/ILCookCountyLParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/IL/ILCookCountyLParser.java
@@ -3,14 +3,14 @@ package net.anei.cadpage.parsers.IL;
 import net.anei.cadpage.parsers.dispatch.DispatchA19Parser;
 
 public class ILCookCountyLParser extends DispatchA19Parser {
-  
+
   public ILCookCountyLParser() {
     super("COOK COUNTY", "IL");
   }
-  
+
   @Override
   public String getFilter() {
-    return "MABAS22@CALCOMM911.org";
+    return "MABAS22@CALCOMM911.org,MABAS22@calumetparkvillage.org";
   }
 
 }
