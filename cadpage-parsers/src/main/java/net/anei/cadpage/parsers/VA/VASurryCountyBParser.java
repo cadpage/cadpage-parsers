b054bc32b1338e78d8f591c51dbd0142f56e7209
Ken Corbin <ken@cadpage.org>
2020-09-03 16:38:54 -0700

    Fixed parsing problem with Surry County, VA (Add B)

---
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/VA/VASurryCountyBParser.java
@@ -0,0 +1,10 @@
+package net.anei.cadpage.parsers.VA;
+
+import net.anei.cadpage.parsers.dispatch.DispatchA71Parser;
+
+public class VASurryCountyBParser extends DispatchA71Parser {
+  
+  public VASurryCountyBParser() {
+    super("SURRY COUNTY", "VA");
+  }
+}
--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/VA/VASurryCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/VA/VASurryCountyParser.java
@@ -1,52 +1,13 @@
 package net.anei.cadpage.parsers.VA;
 
-import java.util.Properties;
-
-import net.anei.cadpage.parsers.CodeSet;
-import net.anei.cadpage.parsers.MsgInfo.Data;
-import net.anei.cadpage.parsers.dispatch.DispatchDAPROParser;
+import net.anei.cadpage.parsers.GroupBestParser;
 
 /**
  * Surry County, VA
  */
-public class VASurryCountyParser extends DispatchDAPROParser {
+public class VASurryCountyParser extends GroupBestParser {
   
   public VASurryCountyParser() {
-    super(CITY_CODE_TABLE, "SURRY COUNTY", "VA");
-    setupCallList(CALL_SET);
+    super(new VASurryCountyAParser(), new VASurryCountyBParser());
   }
-  
-  @Override
-  public String getFilter() {
-    return "mailbox@sheriffsoffice.local";
-  }
-  
-  private static final CodeSet CALL_SET = new CodeSet(
-      "ABDOMINAL PAIN",
-      "ALTERED MENTAL STATUS",
-      "CHEST PAIN/HEART ATTACK",
-      "CITIZEN ASSIST",
-      "DIFFICULTY BREATHING",
-      "FALL",
-      "MVA- MOTOR VEHICLE ACCIDENT",
-      "MVA W/ INJURIES",
-      "ODOR OF SMOKE IN STRUCTURE",
-      "OTHER/UNKNOWN PROBLEM",
-      "PAIN",
-      "SICK",
-      "STRUCTURE FIRE",
-      "TEST CALL",
-      "UNCONSCIOUS/UNRESPONSIVE"
-  );
-  
-  private static final Properties CITY_CODE_TABLE =
-    buildCodeTable(new String[]{
-        "CLA", "CLAREMONT",
-        "DEN", "DENDRON",
-        "ELB", "ELBERON",
-        "SMI", "SMITHFIELD",
-        "SPR", "SPRING GROVE",
-        "SUR", "SURRY",
-        "WAV", "WAVERLY"
-    });
 }
