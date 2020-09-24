8302e80be8f9b65cf93829eb6bf95b42d4cfa6df
Ken Corbin <ken@cadpage.org>
2020-09-24 10:08:39 -0700

    Fixed parsing problem with Catawba County, NC

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/NC/NCCatawbaCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/NC/NCCatawbaCountyParser.java
@@ -1,5 +1,8 @@
 package net.anei.cadpage.parsers.NC;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.regex.Pattern;
 
 import net.anei.cadpage.parsers.MsgInfo.Data;
@@ -11,7 +14,8 @@ public class NCCatawbaCountyParser extends DispatchA3Parser {
   private static final Pattern C_AND_B = Pattern.compile("\\bC AND B\\b|\\bC *& *B\\b", Pattern.CASE_INSENSITIVE);
   
   public NCCatawbaCountyParser() {
-    super(-2, "CATAWBA COUNTY", "NC");
+    super("CATAWBA COUNTY", "NC",
+          "ID? ADDR APT CH CITY ( X2 CALL UNIT! | X/Z CALL UNIT! | X X MAP INFO1 SKIP CALL! PLACENAME PHONE UNIT ) INFO+");
   }
 
   @Override
@@ -20,11 +24,46 @@ public class NCCatawbaCountyParser extends DispatchA3Parser {
     if (body.length() == 0) return false;
     body = body.replaceAll("//+", "/");
     body = C_AND_B.matcher(body).replaceAll("C%B");
-    if (!super.parseMsg(body, data)) return false;
+    if (!super.parseFields(body.split("\\*"), data)) return false;
     data.strAddress = data.strAddress.replace("C%B", "C AND B");
     data.strCross = data.strCross.replace("C%B", "C AND B");
     
     return true;
+  }
+  
+  @Override
+  public Field getField(String name) {
+    if (name.equals("X2")) return new MyCross2Field();
+    if (name.equals("CALL")) return new MyCallField();
+    return super.getField(name);
+  }
+  
+  private class MyCross2Field extends CrossField {
+    @Override
+    public boolean canFail() {
+      return true;
+    }
+    
+    @Override
+    public boolean checkParse(String field, Data data) {
+      if (!field.contains("/")) return false;
+      parse(field, data);
+      return true;
+    }
+  }
+  
+  private class MyCallField extends CallField {
+    @Override
+    public boolean canFail() {
+      return true;
+    }
+    
+    @Override
+    public boolean checkParse(String field, Data data) {
+      if (!checkCall(field)) return false;
+      parse(field, data);
+      return true;
+    }
   }
   
   @Override
@@ -36,6 +75,52 @@ public class NCCatawbaCountyParser extends DispatchA3Parser {
   public String postAdjustMapAddress(String sAddress) {
     return sAddress.replace("C%B", "C AND B");
   }
-  
+   
+  @Override
+  public boolean checkCall(String call) {
+    return CALL_LIST.contains(call);
+  }
+
+  private static final Set<String> CALL_LIST = new HashSet<>(Arrays.asList(
+      "ALLERGIES",
+      "BREATHING",
+      "CAD TESTING COMPLAINT TYPE",
+      "CHEST PAIN",
+      "CONVULSIONS / SEIZURES",
+      "DUMPSTER FIRE",
+      "FALLS",
+      "FIRE ALARM COMMERCIAL",
+      "FIRE ALARM RESIDENCE",
+      "GAS LEAK/SMELL",
+      "HEADACHE",
+      "HEART PROBLEMS",
+      "HEMORRHAGE/LACERATION",
+      "MUTUAL AIDE ASSIST",
+      "NEONATAL TRANSFER",
+      "PREGNANCY/CHILDBIRTH",
+      "PSYCHIATRIC/SUICIDE",
+      "PUBLIC ASSIST",
+      "SERVICE CALL",
+      "SICK PERSON",
+      "SMOKE REPORT/SCARE",
+      "STANDBY/MOVE UP",
+      "STROKE",
+      "STRUCTURE FIRE COMMERCIAL",
+      "TESTING ALARM",
+      "TRAFFIC ACC",
+      "TRAFFIC ACC (PIN/ENT/OT)",
+      "TRANSFER/EMERGENCY",
+      "TRANSFER/ROUTINE",
+      "UNCONSCIOUS/FAINTING",
+      "UNKNOWN PROBLEM",
+      "yCHEST PAIN DELTA",
+      "yFALLS ALPHA",
+      "yFALLS BRAVO",
+      "ySICK PERSON ALPHA",
+      "ySTROKE CHARLIE",
+      "yUNCONSCIOUS/FAINTING DELTA",
+      "yUNKNOWN PROBLEM BRAVO",
+      "ZCAD TESTING"
+  ));
   
 }
