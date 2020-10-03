45d0563e4e8be1b393a3fe13715ea6cc33d7ee09
Ken Corbin <ken@cadpage.org>
2020-10-02 19:53:18 -0700

    Fixed parsing problem with Parke County, IN

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/IN/INParkeCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/IN/INParkeCountyParser.java
@@ -2,6 +2,9 @@ package net.anei.cadpage.parsers.IN;
 
 
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import net.anei.cadpage.parsers.MsgInfo.Data;
 import net.anei.cadpage.parsers.dispatch.DispatchA56Parser;
 
@@ -18,11 +21,28 @@ public class INParkeCountyParser extends DispatchA56Parser {
   public String getFilter() {
     return "DISPATCH@bloomingdaletel.com,DISPATCH@parkecounty-in.gov,parkecountydispatch911@gmail.com,DISPATCHtext@parkecounty-in.gov,ParkeCountyDispatch@outlook.com";
   }
+  
+  private static final Pattern SHORT_FORM_PTN = Pattern.compile("([^:\n]+?)//(?:[-/&A-Z0-9 ]+//)?([A-Z]{3,4}:[A-Z]{3,6})[,/]");
 
   @Override
   public boolean parseMsg(String body, Data data) {
+    
+    // See if this is the short form which eliminates everything up to the first \n\n
+    // If it is, add dummy information to make this parseable
+    boolean shortForm = false;
+    Matcher match = SHORT_FORM_PTN.matcher(body);
+    if (match.lookingAt()) {
+      String addr = match.group(1);
+      if (!addr.contains("//") && !addr.contains(" - ")) {
+        shortForm = true;
+        body = match.group(2) + " - 99/99 99:99 - XXXX:99-999999 9999 XXXXX\n\n" + body;
+      }
+    }
+    
     if (!body.startsWith("DISPATCH:")) body = "DISPATCH:" + body;
-    return super.parseMsg(body, data);
+    if (!super.parseMsg(body, data)) return false;
+    if (shortForm) data.strDate = data.strTime = data.strSource = data.strCallId = data.strCode = data.strCall = "";
+    return true;
   }
    
 }
