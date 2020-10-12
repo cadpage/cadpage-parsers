6167ec94a46faec4ed72cd8b5740e7c668828ad2
Ken Corbin <ken@cadpage.org>
2020-10-12 10:23:15 -0700

    Fixed parsing problem with Canadian County, OK (A)

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/OK/OKCanadianCountyAParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/OK/OKCanadianCountyAParser.java
@@ -1,15 +1,27 @@
 package net.anei.cadpage.parsers.OK;
 
-import net.anei.cadpage.parsers.dispatch.DispatchBCParser;
+import net.anei.cadpage.parsers.FieldProgramParser;
+import net.anei.cadpage.parsers.MsgInfo.Data;
 
-public class OKCanadianCountyAParser extends DispatchBCParser {
+public class OKCanadianCountyAParser extends FieldProgramParser {
   
   public OKCanadianCountyAParser() {
-    super("CANADIAN COUNTY", "OK");
+    super("CANADIAN COUNTY", "OK",
+          "ID:ID! CALL:CALL! INFO:INFO! ADDR:ADDR! CITY:CITY! PLACE:PLACE! LATITUDE:GPS1! LONGITUDE:GPS2! UNIT:UNIT! END");
   }
   
   @Override
   public String getFilter() {
-    return "@cityofelreno.com";
+    return "support@geosafe.com";
+  }
+  
+  @Override
+  public int getMapFlags() {
+    return MAP_FLG_PREFER_GPS;
+  }
+  
+  @Override
+  protected boolean parseMsg(String body, Data data) {
+    return parseFields(body.split("\n"), data);
   }
 }
