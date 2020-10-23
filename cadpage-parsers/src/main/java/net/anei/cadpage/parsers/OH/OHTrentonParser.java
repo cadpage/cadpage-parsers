5f6e8f0fcc978b261d52dd77750048683c15fff5
Ken Corbin <ken@cadpage.org>
2020-10-22 20:25:54 -0700

    Fixed parsing problem with Trenton, OH

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/OH/OHTrentonParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/OH/OHTrentonParser.java
@@ -5,98 +5,95 @@ import java.text.SimpleDateFormat;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import net.anei.cadpage.parsers.FieldProgramParser;
 import net.anei.cadpage.parsers.MsgInfo.Data;
-import net.anei.cadpage.parsers.dispatch.DispatchEmergitechParser;
 
 
-public class OHTrentonParser extends DispatchEmergitechParser {
-  
-  private static final Pattern DATE_TIME_PTN = Pattern.compile("(.*?) +(?:-(?:\\d+|TRAIN))?\\((\\d\\d?/\\d\\d?/\\d{4}) (\\d\\d?:\\d\\d(:\\d\\d)? [AP]M)\\)");
-  private static final DateFormat TIME_FMT1 = new SimpleDateFormat("hh:mm:ss aa"); 
-  private static final DateFormat TIME_FMT2 = new SimpleDateFormat("hh:mm aa"); 
+public class OHTrentonParser extends FieldProgramParser {
   
   public OHTrentonParser() {
-    super("", CITY_LIST, "TRENTON", "OH");
+    super("TRENTON", "OH", 
+          "SRC_CALL! CAD_#:ID! Address:ADDRCITY! Cross_Streets:X! Call_Details:INFO! INFO/N+ Page_Time:DATETIME");
   }
+  
+  
+  @Override
+  public boolean parseMsg(String body, Data data) {
+    body = stripFieldStart(body, "Trenton Paging System");
+    body = stripFieldStart(body, "-");
+    return parseFields(body.split(" - "), data);
+  }
+  
+  @Override
+  public Field getField(String name) {
+    if (name.equals("SRC_CALL"))  return new MySourceCallField();
+    if (name.equals("ADDRCITY")) return new MyAddressCityField();
+    if (name.equals("INFO")) return new MyInfoField();
+    if (name.equals("DATETIME")) return new MyDateTimeField();
+    return super.getField(name);
+  }
+  
+  private static final Pattern SRC_CALL_PTN = Pattern.compile("([A-Z]{3,5})\\b *(.*)");
+  private class MySourceCallField extends Field {
 
-  @Override
-  public String getFilter() {
-    return "paging@ci.trenton.oh.us,@cityoftrenton.com";
-  }
-  
-  
-  @Override
-  public boolean parseMsg(String subject, String body, Data data) {
-    
-    Matcher match = DATE_TIME_PTN.matcher(body);
-    if (match.matches()) {
-      body = match.group(1).trim();
-      data.strDate = match.group(2);
-      setTime(match.group(4) != null ? TIME_FMT1 :  TIME_FMT2, match.group(3), data);
+    @Override
+    public void parse(String field, Data data) {
+      Matcher match = SRC_CALL_PTN.matcher(field);
+      if (!match.matches()) abort();
+      data.strSource = match.group(1);
+      data.strCall = match.group(2);
     }
-    return super.parseMsg(subject, body, data);
+
+    @Override
+    public String getFieldNames() {
+      return "SRC CALL";
+    }
   }
   
-  @Override
-  public String getProgram() {
-    return super.getProgram() + " DATE TIME";
+  private static final Pattern ST_ZIP_PTN = Pattern.compile("([A-Z]{2})(?: +(\\d{5}))?");
+  private class MyAddressCityField extends AddressField {
+    @Override
+    public void parse(String field, Data data) {
+      Parser p = new Parser(field);
+      String city;
+      String zip = null;
+      while (true) {
+        city = p.getLastOptional(',');
+        Matcher match = ST_ZIP_PTN.matcher(city);
+        if (!match.matches()) break;
+        data.strState = match.group(1);
+        String t = match.group(2);
+        if (t != null) zip = t;
+      }
+      if (city.isEmpty() && zip != null) city = zip;
+      data.strCity = city;
+      super.parse(p.get(), data);
+    }
+    
+    @Override
+    public String getFieldNames() {
+      return "ADDR APT CITY ST";
+    }
   }
-
-  private static final String[] CITY_LIST = new String[]{
-      "ALERT",
-      "AMANDA",
-      "BECKETT RIDGE",
-      "BETHANY",
-      "BLUE BALL",
-      "COLLEGE CORNER",
-      "COLLINSVILLE",
-      "DARRTOWN",
-      "ENGLE'S CORNER",
-      "EXCELLO",
-      "FAIRFIELD",
-      "FOUR BRIDGES",
-      "HAMILTON",
-      "HANOVER",
-      "HERITAGE",
-      "INDIAN SPRINGS",
-      "JACKSONBURG",
-      "LEMON",
-      "LESOURDESVILLE",
-      "LIBERTY",
-      "MADISON",
-      "MAUD",
-      "MCGONIGLE",
-      "MIDDLETOWN",
-      "MILFORD",
-      "MILLVILLE",
-      "MILTONVILLE",
-      "MONROE",
-      "MORGAN",
-      "NEW MIAMI",
-      "OKEANA",
-      "OLDE WEST CHESTER",
-      "ONEIDA",
-      "OVERPECK",
-      "OXFORD",
-      "PISGAH",
-      "POASTTOWN",
-      "PORT UNION",
-      "PRINCETON",
-      "REILY",
-      "ROSS",
-      "SCIPIO",
-      "SEVEN MILE",
-      "SHANDON",
-      "SHARONVILLE",
-      "SOMERVILLE",
-      "ST CLAIR",
-      "TRENTON",
-      "TYLERSVILLE",
-      "WAYNE",
-      "WEST CHESTER",
-      "WEST MIDDLETOWN",
-      "WETHERINGTON",
-      "WILLIAMSDALE",
-      "WOODSDALE"
-  };
+  
+  private static final Pattern INFO_DATE_TIME_PTN = Pattern.compile("[; ]*\\b\\d\\d?/\\d\\d?/\\d\\d \\d\\d:\\d\\d:\\d\\d$");
+  private class MyInfoField extends InfoField {
+    @Override
+    public void parse(String field, Data data) {
+      field = INFO_DATE_TIME_PTN.matcher(field).replaceAll("");
+      data.strSupp = append(data.strSupp, "\n", field);
+    }
+  }
+  
+  private static final Pattern DATE_TIME_PTN = Pattern.compile("\\((\\d\\d?/\\d\\d?/\\d\\d(?:\\d\\d)?) +(\\d\\d?:\\d\\d [AP]M)\\)");
+  private static final DateFormat TIME_FMT = new SimpleDateFormat("hh:mm aa");
+  private class MyDateTimeField extends DateTimeField {
+    @Override
+    public void parse(String field, Data data) {
+      Matcher match = DATE_TIME_PTN.matcher(field);
+      if (!match.matches()) abort();
+      data.strDate = match.group(1);
+      setTime(TIME_FMT, match.group(2), data);
+    }
+  }
 }
