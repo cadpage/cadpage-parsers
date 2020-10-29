0837a03b3f495d0beecafad464c307b9f98ae71f
Ken Corbin <ken@cadpage.org>
2020-10-29 16:22:24 -0700

    Fixed problem with Hoke County,NC (B)

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/NC/NCHokeCountyBParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/NC/NCHokeCountyBParser.java
@@ -7,18 +7,27 @@ public class NCHokeCountyBParser extends FieldProgramParser {
   
   public NCHokeCountyBParser() {
     super("HOKE COUNTY", "NC", 
-          "CALL ADDRCITYST X PLACE EMPTY! END");
+          "CALL ADDRCITYST X PLACE INFO! END");
   }
   
   @Override
   public String getFilter() {
-    return "74121";
+    return "no-reply@zuercherportal.com,74121";
   }
   
   @Override
-  protected boolean parseMsg(String body, Data data) {
-    if (!body.startsWith("HOKE COUNTY 911:")) return false;
-    body = body.substring(16).trim();
+  protected boolean parseMsg(String subject, String body, Data data) {
+    do {
+      if (body.startsWith("HOKE COUNTY 911:")) {
+        body = body.substring(16).trim();
+        break;
+      }
+      
+      if (subject.equals("HOKE COUNTY 911")) break;
+      
+      return false;
+    } while (false);
+    
     return parseFields(body.split("\\*\\*", -1), data);
   }
 }
