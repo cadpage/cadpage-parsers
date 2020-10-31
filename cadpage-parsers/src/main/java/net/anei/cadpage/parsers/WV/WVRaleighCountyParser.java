499d52e63ae9674f353c2c4c8768f598ea49c59f
Ken Corbin <ken@cadpage.org>
2020-10-31 11:18:42 -0700

    Fixed parsing problem with Raleigh County, WV

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/WV/WVRaleighCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/WV/WVRaleighCountyParser.java
@@ -3,6 +3,7 @@ package net.anei.cadpage.parsers.WV;
 
 import java.util.Properties;
 
+import net.anei.cadpage.parsers.MsgInfo.Data;
 import net.anei.cadpage.parsers.dispatch.DispatchA19Parser;
 
 
@@ -15,6 +16,14 @@ public class WVRaleighCountyParser extends DispatchA19Parser {
   @Override
   public String getFilter() {
     return "Raleigh911@SuddenLinkMail.com";
+  }
+
+  
+  @Override
+  protected boolean parseMsg(String subject, String body, Data data) {
+    if (!super.parseMsg(subject, body, data)) return false;
+    if (data.strCity.equals("OUT OF COUNTY")) data.defCity = "";
+    return true;
   }
 
   @Override
@@ -34,6 +43,7 @@ public class WVRaleighCountyParser extends DispatchA19Parser {
       "EUNICE",             "WHITESVILLE",
       "FARLEY HILL",        "PRINCEWICK",
       "FITZPATRICK",        "BECKLEY",
+      "GLEN MORGAN",        "BEAVER",
       "GLEN VIEW",          "CRAB ORCHARD",
       "GRANDVIEW",          "BEAVER",
       "HARPER",             "BECKLEY",
@@ -47,11 +57,13 @@ public class WVRaleighCountyParser extends DispatchA19Parser {
       "METALTON",           "BECKLEY",
       "MOUNT TABOR",        "BECKLEY",
       "OAK GROVE",          "BECKLEY",
-      "OOC",                "",
+      "OUT OF COUNTY",      "",
       "PACKSVILLE",         "",
       "PLEASANT HILLS",     "BECKLEY",
       "PLUTO",              "SHADY SPRING",
       "SANDLICK",           "BECKLEY",
+      "SKELETON",           "BECKLEY",
+      "SOAK CREEK",         "SOPHIA",
       "SPRAGUE",            "BECKLEY",
       "STANAFORD",          "BECKLEY",
       "STOVER",             "GLEN DANIEL",
@@ -60,56 +72,146 @@ public class WVRaleighCountyParser extends DispatchA19Parser {
   });
   
   private static final Properties CITY_CODES = buildCodeTable(new String[]{
+      "ABH", "ABRAHAM",
+      "AME", "AMEAGLE",
       "AMG", "AMIGO",
+      "ARN", "ARNETT",
+      "ART", "ARTIE",
       "BDY", "BRADLEY",
       "BEA", "BEAVER",
       "BEC", "BECKLEY",
+      "BES", "BESOCO",
+      "BJN", "BECKLEY JCT",
       "BJY", "BLUE JAY",        // -> BEAVER
+      "BLT", "BOLT",
+      "BLV", "BLUEMONT",
       "BRG", "BRAGG",
-      "CBO", "CRAB ORCHARD",
+      "BRN", "BARN",
+      "BS",  "BERKELEY SPRING",
+      "BVV", "BERRYVILLE",
       "CBH", "CABELL HEIGHTS",  // -> BECKLEY
+      "CBO", "CRAB ORCHARD",
+      "CBY", "CRANBERRY",
       "CCY", "COAL CITY",
+      "CLC", "CLEAR CREEK",
+      "CLH", "CALLOWAY HGTS",
       "CLR", "COOL RIDGE",
       "CRD", "COLCORD",
       "CRW", "CROW",
+      "CT",  "CHARLES TOWN",
+      "CTR", "CHARLES TOWN",
+      "CTU", "CHARLES TOWN",
+      "CVC", "COVE CREEK",
+      "CVL", "CIRTSVILLE",
+      "DAM", "DAMERON",
       "DAN", "DANIELS",
+      "DCK", "DRY CREEK",
       "DHL", "DRY HILL",        // -> BECKLEY
+      "DRT", "DOROTHY",
+      "DUN", "DUNNS",
+      "ECC", "ECCLES",
+      "EDW", "EDWIGHT",
+      "EGF", "EAST GULF",
       "EGR", "EGERIA",          // -> ODD
+      "EUN", "EUNICE",          // -> WHITESVILLE
       "FAY", "FAYETTE COUNTY",
+      "FCO", "FIRECO",
       "FDL", "FAIRDALE",
+      "FRH", "FARLEY HILL",
       "FTP", "FLAT TOP",
       "FZP", "FITZPATRICK",
       "GDL", "GLEN DANIEL",
       "GHT", "GHENT",
-      "GMG", "BEAVER",          // ????
+      "GMG", "GLEN MORGAN",     // -> BEAVER
       "GRV", "GRANDVIEW",       // -> BEAVER
+      "GVW", "GLEN VIEW",
       "GWT", "GLEN WHITE",
+      "HAR", "HARPER",
+      "HF",  "HARPERS FERRY",
+      "HFB", "HARPERS FERRY",
+      "HFU", "HARPERS FERRY",
       "HHT", "HARPER HEIGHTS",  // -> BECKLEY
+      "HIN", "HINTON",
+      "HKS", "HOTCHKISS",
+      "HLN", "HELEN",
       "HPK", "HARPER PARK",     // -> BECKLEY
+      "IMT", "IRISH MOUNTAIN",
+      "JBN", "JONBEN",
+      "JOS", "JOSEPHINE",
+      "KAY", "KAYFORD",
+      "KIL", "KILLARNEY",
+      "KVR", "KEARNEYSVILLE",
+      "KVU", "KEARNEYSVILLE",
       "LBK", "LILLYBROOK",      // -> COAL CITY
+      "LGO", "LEGO",
+      "LRK", "LANARK",
       "LTR", "LESTER",
+      "LVL", "LEEVALE",        // -> WHITESVILLE
       "MAB", "MABSCOTT",
       "MAC", "MACARTHUR",
+      "MCC", "MCCREERY",
+      "MCL", "MONTCOAL",
+      "MED", "MEAD",
+      "MET", "METALTON",
       "MFK", "MAPLE FORK",      // -> BRADLEY
+      "MLC", "MILL CREEK",
+      "MNR", "MAYNOR",
+      "MPN", "MCALPIN",
       "MTB", "MOUNT TABOR",     // -> BECKLEY
+      "MVU", "MILLVILLE",
+      "MWY", "MIDWAY",
+      "NEW", "NEW",
       "NMA", "NAOMA",
+      "ODD", "ODD",
+      "OGV", "OAK GROVE",
+      "OOC", "OUT OF COUNTY",
+      "PBM", "PETTRY BOTTOM",
       "PEM", "PEMBERTON",
-      "PWK", "PRINCEWICK",
+      "PHL", "PRICE HILL",
       "PRP", "PROSPERITY",
+      "PSN", "PICKSHIN",
+      "PTH", "PLEASANT HILLS",
       "PTO", "PLUTO",           // -> SHADY SPRING
+      "PTU", "PETTUS",
+      "PVL", "PACKSVILLE",
+      "PVV", "PURCELLVILLE",
       "PVW", "PINEY VIEW",
+      "PWK", "PRINCEWICK",
       "RAL", "RALEIGH COUNTY",
+      "RCK", "ROCK CREEK",
       "RHO", "RHODELL",
+      "RN",  "RANSON",
+      "RNC", "RANSON",
+      "RNU", "RANSON",
       "SBY", "STOTESBURY",
-      "SCK", "SOPHIA",          // ???
-      "SKE", "BECKLEY",         // ???
+      "SCK", "SOAK CREEK",      // -> SOPHIA
+      "SDL", "SANDLICK",
+      "SFK", "SLAB FORK",
+      "SJU", "SHENANDOAH JCT",
+      "SKE", "SKELTON",         // -> BECKLEY
       "SPH", "SOPHIA",
       "SPR", "SPRAGUE",         // -> BECKLEY
+      "SPU", "SUMMIT POINT",
       "SRV", "SURVEYOR",
       "SSG", "SHADY SPRINGS",
-      "SWB", "SWEENEYSBURG",    // -> BECKLEY
+      "ST",  "SHEPHERDSTOWN",
+      "STK", "STICKNEY",
       "STN", "STANAFORD",       // -> BECKLEY
+      "STU", "SHEPHERDSTOWN",
+      "STV", "STOVER",          // -> GLEN DANIEL
+      "SUL", "SULLIVAN",
+      "SUM", "SUMMERS CTY",
+      "SUN", "SUNDIAL",
+      "SWB", "SWEENEYSBURG",    // -> BECKLEY
+      "TAM", "TAMS",
+      "TRY", "TERRY",
+      "TTN", "TOLLEYTOWN",
       "URY", "URY",
-      "WTK", "WHITE OAK"
+      "WBT", "WILLIBET",
+      "WBY", "WHITBY",
+      "WHM", "WICKHAM",
+      "WTK", "WHITE OAK",
+      "WVL", "WHITESVILLE",
+      "WVW", "WESTVIEW"
   });
 }
