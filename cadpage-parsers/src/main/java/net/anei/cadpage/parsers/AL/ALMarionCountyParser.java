9e65280096540031be6ffc233be6199abc48fe03
Ken Corbin <ken@cadpage.org>
2020-10-31 09:05:52 -0700

    Fixed parsing problem with Marion County, AL

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/AL/ALMarionCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/AL/ALMarionCountyParser.java
@@ -1,6 +1,7 @@
 
 package net.anei.cadpage.parsers.AL;
 
+import java.util.Properties;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -16,7 +17,7 @@ public class ALMarionCountyParser extends FieldProgramParser {
 
   public ALMarionCountyParser() {
     super(CITY_LIST, "MARION COUNTY", "AL",
-          "Unit:UNIT? Event:ID! Location:ADDR/S! Event_Type:CALL! Event_SubType:CALL/SDS! Complainant_Name:NAME! Complainant_Phone:PHONE INFO+");
+          "Unit:UNIT? Event:ID! Location:ADDR/S! LAT/LONG:GPS? Event_Type:CALL! Event_SubType:CALL/SDS! Complainant_Name:NAME! Complainant_Phone:PHONE INFO+");
   }
     
   @Override
@@ -44,24 +45,92 @@ public class ALMarionCountyParser extends FieldProgramParser {
     return super.getField(name);
   }
   
+  private static final Pattern STATE_PTN = Pattern.compile("[A-Z]{2}");
+  private static final Pattern MSPACE_PTN = Pattern.compile(" {2,}");
+  private static final Pattern APT_PTN1 = Pattern.compile("(?:APT|LOT|ROOM|RM|STE)[# ]*(.*)");
+  private static final Pattern APT_PTN2 = Pattern.compile("\\d{1,4}[A-Z]?|[A-Z]");
+      
   private class MyAddressField extends AddressField {
     @Override
     public void parse(String field, Data data) {
-      field = stripFieldStart(field, "Nearest:");
+      
+      String city = null;
       int pt = field.lastIndexOf(',');
       if (pt >= 0) {
-        data.strCity = field.substring(pt+1).trim();
-        parseAddress(field.substring(0,pt).trim(), data);
-      } else {
-        parseAddress(StartType.START_ADDR, FLAG_ANCHOR_END, field, data);
+        city = field.substring(pt+1).trim();
+        if (STATE_PTN.matcher(city).matches()) {
+          field = field.substring(0, pt).trim();
+          data.strState = city;
+          city = null;
+        }
       }
       
-      // Check for duplicated city
-      if (data.strCity.length() > 0) {
-        data.strAddress = stripFieldEnd(data.strAddress, " " + data.strCity);
+      if (city != null) {
+        if (city.length() > 0 && Character.isAlphabetic(city.charAt(0))) {
+          field = field.substring(0, pt).trim();
+          pt = city.indexOf("  ");
+          if (pt >= 0) {
+            data.strCity = city.substring(0,pt); 
+          } else if (city.contains(" ")) {
+            parseAddress(StartType.START_ADDR, FLAG_ONLY_CITY, city, data);
+            if (data.strCity.isEmpty()) data.strCity = city;
+          } else {
+            data.strCity = city;
+          }
+        }
+      } else {
+        parseAddress(StartType.START_OTHER, FLAG_ONLY_CITY | FLAG_ANCHOR_END, field, data);
+        field = getStart();
+        field = stripFieldEnd(field, " " + data.strCity);
+      }
+      
+
+      field = MSPACE_PTN.matcher(field).replaceAll(" ");
+      for (String part : field.split("[:@]")) {
+        part = part.trim();
+        if (part.isEmpty() || part.equals("@") || part.equals("Nearest")) continue;
+        
+        if (data.strAddress.isEmpty()) {
+          parseAddress(part, data);
+          continue;
+        }
+        
+        if (GPS_PATTERN.matcher(part).matches()) {
+          setGPSLoc(part, data);
+          continue;
+        }
+        
+        Matcher match = APT_PTN1.matcher(part);
+        if (match.matches()) {
+          data.strApt = append(data.strApt, "-", match.group(1));
+          continue;
+        }
+        
+        if (APT_PTN2.matcher(part).matches()) {
+          data.strApt = append(data.strApt, "-", part);
+          continue;
+        }
+        
+        data.strPlace = append(data.strPlace, " - ", part);
       }
     }
+    
+    @Override
+    public String getFieldNames() {
+      return "ADDR PLACE APT GPS? CITY ST";
+    }
   }
+  
+  @Override
+  public String adjustMapCity(String city) {
+    return convertCodes(city, MAP_CITY_TABLE);
+  }
+  
+  private static final Properties MAP_CITY_TABLE = buildCodeTable(new String[]{
+      "BYRD",           "HAMILTON",
+      "PEA RIDGE",      "BRILLIANT",
+      "SHILOH",         "HAMILTON",
+  });
   
   private static final String[] CITY_LIST = new String[]{
 
@@ -94,6 +163,9 @@ public class ALMarionCountyParser extends FieldProgramParser {
     "SHOTTSVILLE",
     "SHILOH",
     "BYRD", 
-    "PEA RIDGE"
+    "PEA RIDGE",
+    
+    // Lamar County
+    "SULLIGENT"
   };
 }
