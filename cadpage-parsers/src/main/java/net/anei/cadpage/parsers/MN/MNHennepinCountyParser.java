44d56db5dc8d3286a75fc00d0eeab0590f40cd40
Kenneth Corbin <ken@cadpage.org>
2020-11-05 09:34:40 -0800

    Update sender filter for Hennepin County, MN

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/MN/MNHennepinCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/MN/MNHennepinCountyParser.java
@@ -17,44 +17,44 @@ import net.anei.cadpage.parsers.MsgInfo.Data;
  * Hennepin county, MN
  */
 public class MNHennepinCountyParser extends FieldProgramParser {
-  
+
   public MNHennepinCountyParser() {
     super(CITY_LIST, "HENNEPIN COUNTY", "MN",
-          "( NAME:NAME! ( LOC:ADDR! CITY:CITY? EVTYPE:CALL! " + 
+          "( NAME:NAME! ( LOC:ADDR! CITY:CITY? EVTYPE:CALL! " +
                        "| PH#:PHONE! ADDRSS:ADDR! APT#:APT! CITY:CITY! X_ST:X! EVTYPE:CALL! INC#:ID! " +
-                       "| ADDRESS:ADDR! APT#:APT! CITY:CITY! XSTREET:X! EVTYPE:CALL! INC#:ID! " + 
+                       "| ADDRESS:ADDR! APT#:APT! CITY:CITY! XSTREET:X! EVTYPE:CALL! INC#:ID! " +
                        ") COMMENTS:INFO/N+ " +
           "| INC#:ID! ADDRESS:ADDR! EVTYPE:CALL! INFO/RN+ " +
           ")");
     setBreakChar('-');
   }
-  
+
   @Override
   public String getFilter() {
-    return "sheriff.cadpaging@co.hennepin.mn.us,SHERIFF.CADPAGING@HENNEPIN.US,osseofiredepartment@googlegroups.com";
+    return "sheriff.cadpaging@co.hennepin.mn.us,SHERIFF.CADPAGING@HENNEPIN.US,osseofiredepartment@googlegroups.com,firepaging@mapleplain.com";
   }
 
   @Override
   protected boolean parseMsg(String body, Data data) {
-    
+
     int pt = body.indexOf("\n\n\n");
     if (pt < 0) pt = body.indexOf("\n\n--");
     if (pt >= 0) body = body.substring(0, pt).trim();
-    
+
     body = body.replace(" X STREET-", " XSTREET-");
     if (!super.parseFields(splitFields(body), data)) return false;
-    
+
     // If we did not find a city, check the name to see if it looks like
     // a neighboring county dispatch center
     if (data.strCity.length() == 0) {
       Matcher match = COUNTY_NAME_PTN.matcher(data.strName);
       if (match.find()) data.strCity = match.group().toUpperCase() + " COUNTY";
     }
-    
+
     return true;
   }
   private static final Pattern COUNTY_NAME_PTN = Pattern.compile("\\b(ANOKA|RAMSEY|DAKOTA|SCOTT|CARVER|WRIGHT)\\b", Pattern.CASE_INSENSITIVE);
-  
+
   /**
    * Break message body into list of fields
    * @param body message body
@@ -77,7 +77,7 @@ public class MNHennepinCountyParser extends FieldProgramParser {
     fields.add(body.substring(last).trim());
     return fields.toArray(new String[0]);
   }
-  
+
   @Override
   public Field getField(String name) {
     if (name.equals("ADDR")) return new MyAddressField();
@@ -86,12 +86,12 @@ public class MNHennepinCountyParser extends FieldProgramParser {
     if (name.equals("INFO")) return new MyInfoField();
     return super.getField(name);
   }
-  
+
   private static final Pattern ADDR_CITY_PTN = Pattern.compile("(.*)(?:,| -) *(.*)");
   private class MyAddressField extends AddressField {
     @Override
     public void parse(String field, Data data) {
-      
+
       // Break off things from end separated by [
       String apt = "";
       String extra = "";
@@ -99,38 +99,38 @@ public class MNHennepinCountyParser extends FieldProgramParser {
       while (true) {
         String part = p.getLastOptional('[');
         if (part.length() == 0) break;
-        
+
         // For each chunk try to split off a city name
         part = splitCity(part, data);
-        
+
         // Now we get to sort out what each part is.
         // Apartment?
         if (part.startsWith("#")) {
           apt = append(part.substring(1).trim(), "-", apt);
         }
-        
+
         // Place name
         else if (part.startsWith("@")) {
           data.strPlace = append(part.substring(1).trim(), " - ", data.strPlace);
         }
-        
+
         // City name or code
         else if (part.startsWith("=")) {
           data.strCity = convertCodes(part.substring(1).trim(), CITY_CODES);
         }
-        
+
         // City name
         else if (isCity(part)) {
           data.strCity = part;
         }
-        
+
         // City code
         else {
           String city = CITY_CODES.getProperty(part);
           if (city != null) {
             data.strCity = city;
           }
-          
+
           // Anything else turns into extra address information
           else {
             part = part.replace("(", "").replace(")", "").trim();
@@ -139,18 +139,18 @@ public class MNHennepinCountyParser extends FieldProgramParser {
           }
         }
       }
-      
+
       field = p.get();
       field = splitCity(field, data);
       parseAddress(StartType.START_ADDR, FLAG_ANCHOR_END, field, data);
-      
+
       data.strApt = append(data.strApt, "-", apt);
-      
+
       if (extra.length() > 0) {
         data.strAddress = append(data.strAddress, " ", '(' + extra + ')');
       }
     }
-    
+
     private String splitCity(String part, Data data) {
       Matcher match = ADDR_CITY_PTN.matcher(part);
       if (match.matches()) {
@@ -167,13 +167,13 @@ public class MNHennepinCountyParser extends FieldProgramParser {
       }
       return part;
     }
-    
+
     @Override
     public String getFieldNames() {
       return "ADDR APT PLACE CITY";
     }
   }
-  
+
   private class MyCityField extends CityField {
     @Override
     public void parse(String field, Data data) {
@@ -181,7 +181,7 @@ public class MNHennepinCountyParser extends FieldProgramParser {
       data.strCity = convertCodes(field, CITY_CODES);
     }
   }
-  
+
   private class MyCallField extends CallField {
     @Override
     public void parse(String field, Data data) {
@@ -189,7 +189,7 @@ public class MNHennepinCountyParser extends FieldProgramParser {
       super.parse(field, data);
     }
   }
-  
+
   private static final Pattern INFO_PFX_PTN = Pattern.compile("\\[\\d+\\] *(.*)");
   private static final Pattern INFO_CROSS_PTN = Pattern.compile(".*: ?cross streets ?: *(.*)");
   private static final Pattern INFO_UNIT_PTN = Pattern.compile("Resources required for this event are: *(.*?);?");
@@ -222,13 +222,13 @@ public class MNHennepinCountyParser extends FieldProgramParser {
         }
       }
     }
-    
+
     @Override
     public String getFieldNames() {
       return "X INFO UNIT";
     }
   }
-  
+
   private static final Properties CITY_CODES = buildCodeTable(new String[]{
       "ANC", "ANOKA",
       "ANO", "ANOKA COUNTY",
@@ -286,15 +286,15 @@ public class MNHennepinCountyParser extends FieldProgramParser {
       "WRI", "WRIGHT COUNTY"
 
   });
-  
+
   private static final String[] CITY_LIST = new String[]{
-    
+
     // Anakoka County
     "COLUMBIA HEIGHTS",
     "FRIDLEY",
-    
+
     // Carver County
     "WACONIA",
     "WATERTOWN"
-  }; 
+  };
 }
