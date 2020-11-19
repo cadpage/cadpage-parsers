1d0a185c567ef1d071bc8c3ddf0281627f7a64e6
Kenneth Corbin <ken@cadpage.org>
2020-11-19 08:05:27 -0800

    Fixed parsing proble mwith Northampton County, NC

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/NC/NCNorthamptonCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/NC/NCNorthamptonCountyParser.java
@@ -1,16 +1,25 @@
 package net.anei.cadpage.parsers.NC;
 
+import net.anei.cadpage.parsers.MsgInfo.Data;
 import net.anei.cadpage.parsers.dispatch.DispatchA3Parser;
 
 
 public class NCNorthamptonCountyParser extends DispatchA3Parser {
-  
+
+  private static final String PREFIX = "Northampton911:*";
+
   public NCNorthamptonCountyParser() {
-    super(1, "Northampton911:*", "NORTHAMPTON COUNTY", "NC");
+    super(1, PREFIX, "NORTHAMPTON COUNTY", "NC");
   }
-  
+
   @Override
   public String getFilter() {
-    return "Northampton911@nhcnc.net";
+    return "Northampton911@nhcnc.net,dispatcher@nhcnc.net";
+  }
+
+  @Override
+  protected boolean parseMsg(String subject, String body, Data data) {
+    if (subject.equals("INTERNAL") && ! body.startsWith(PREFIX)) body = PREFIX + body;
+    return super.parseMsg(body, data);
   }
 }
