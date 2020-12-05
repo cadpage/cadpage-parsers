fcfc39abf6b1fb2cfca784d3a95fbd2cd66a4cd2
Kenneth Corbin <ken@cadpage.org>
2020-12-05 12:59:08 -0800

    Fixed parsing problem with Franklin County, TX

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/TX/TXFranklinCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/TX/TXFranklinCountyParser.java
@@ -1,28 +1,20 @@
 package net.anei.cadpage.parsers.TX;
 
-import net.anei.cadpage.parsers.MsgInfo.Data;
-import net.anei.cadpage.parsers.dispatch.DispatchA72Parser;
+import net.anei.cadpage.parsers.dispatch.DispatchA71Parser;
 
-public class TXFranklinCountyParser extends DispatchA72Parser {
-  
+public class TXFranklinCountyParser extends DispatchA71Parser {
+
   public TXFranklinCountyParser() {
     super("FRANKLIN COUNTY", "TX");
   }
-  
+
   @Override
   public String getFilter() {
     return "franklin@co.franklin.tx.us";
   }
-  
+
   @Override
   public int getMapFlags() {
     return MAP_FLG_PREFER_GPS | MAP_FLG_SUPPR_LA;
-  }
-
-  @Override
-  protected boolean parseMsg(String subject, String body, Data data) {
-    int pt = body.indexOf("\n\n\n");
-    if (pt >= 0) body = body.substring(0, pt).trim();
-    return super.parseMsg(subject, body, data);
   }
 }
