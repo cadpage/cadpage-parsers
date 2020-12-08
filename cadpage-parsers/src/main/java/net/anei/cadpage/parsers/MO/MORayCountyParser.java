17b660e76c20e3fac252e25cc748a19bd873c15d
Kenneth Corbin <ken@cadpage.org>
2020-12-07 17:47:46 -0800

    Fixed parsing problem with Ray County, MO

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/MO/MORayCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/MO/MORayCountyParser.java
@@ -9,29 +9,31 @@ import net.anei.cadpage.parsers.FieldProgramParser;
 
 
 public class MORayCountyParser extends FieldProgramParser {
-  
+
   public MORayCountyParser() {
-    super("RAY COUNTY", "MO", 
-          "CALL ADDR CALL/SDS X DATETIME EMPTY END");
+    super("RAY COUNTY", "MO",
+          "CALL ADDR CALL/SDS X DATETIME! INFO/N+ END");
   }
-  
+
   @Override
   public String getFilter() {
     return "donotreply@raycounty911.com";
   }
-  
+
   @Override
   public boolean parseMsg(String subject, String body, Data data) {
     if (!subject.equals("CAD Call")) return false;
     if (body.endsWith(":")) body += ' ';
     return parseFields(body.split(": ", -1), data);
   }
-  
+
   @Override
   public Field getField(String name) {
     if (name.contentEquals("ADDR")) return new MyAddressField();
     if (name.contentEquals("CALL"))  return new MyCallField();
+    if (name.equals("X")) return new MyCrossField();
     if (name.contentEquals("DATETIME")) return new DateTimeField("\\d\\d?/\\d\\d?/\\d\\d +\\d\\d?:\\d\\d", true);
+    if (name.equals("INFO")) return new MyInfoField();
     return super.getField(name);
   }
 
@@ -51,17 +53,33 @@ public class MORayCountyParser extends FieldProgramParser {
       if (city.length() == 0 && zip != null) data.strCity = zip;
       super.parse(p.get(), data);
     }
-    
+
     @Override
     public String getFieldNames() {
       return "ADDR APT CITY ST";
     }
   }
-  
+
   private class MyCallField extends CallField {
     @Override
     public void parse(String field, Data data) {
       if (field.contentEquals("None")) return;
+      super.parse(field, data);
+    }
+  }
+
+  private class MyCrossField extends CrossField {
+    @Override
+    public void parse(String field, Data data) {
+      if (field.contentEquals("None")) return;
+      super.parse(field, data);
+    }
+  }
+
+  private class MyInfoField extends InfoField {
+    @Override
+    public void parse(String field, Data data) {
+      if (field.equals("None")) return;
       super.parse(field, data);
     }
   }
