8b3be2c240022b6d9a582cf1f338d6dc6a221e9e
Kenneth Corbin <ken@cadpage.org>
2020-12-10 18:39:07 -0800

    Update msg doc

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/PA/PABradfordCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/PA/PABradfordCountyParser.java
@@ -13,7 +13,7 @@ import net.anei.cadpage.parsers.SplitMsgOptionsCustom;
 import net.anei.cadpage.parsers.dispatch.DispatchA65Parser;
 
 public class PABradfordCountyParser extends DispatchA65Parser {
-  
+
   public PABradfordCountyParser() {
     super(CITY_LIST, "BRADFORD COUNTY", "PA");
   }
@@ -29,30 +29,30 @@ public class PABradfordCountyParser extends DispatchA65Parser {
       @Override public boolean splitKeepLeadBreak() { return true; }
     };
   }
-  
+
   private static final Pattern MISSING_BRK_PTN = Pattern.compile("(.*[^\n])\n([^\n]+)", Pattern.DOTALL);
-  
+
   @Override
   protected boolean parseMsg(String subject, String body, Data data) {
-    
+
     // Fix extraneous subject problem
     int pt = subject.lastIndexOf('|');
     if (pt >= 0) subject = subject.substring(pt+1).trim();
-    
+
     // A double line break gets lost when call is split into two messages, so we
     // need to restore it
     Matcher match = MISSING_BRK_PTN.matcher(body);
     if (match.matches()) body = match.group(1) + "\n\n" + match.group(2);
-    
+
     if (!super.parseMsg(subject, body, data)) return false;
     data.strCity = convertCodes(data.strCity.toUpperCase(), MISSPELLED_CITIES);
-    
+
     if (data.strState.length() == 0 && NY_CITY_TABLE.contains(data.strCity)) data.strState = "NY";
     return true;
   }
 
   private static final String[] CITY_LIST = new String[]{
-    
+
     // Boroughs
     "ALBA",
     "ATHENS",
@@ -116,21 +116,21 @@ public class PABradfordCountyParser extends DispatchA65Parser {
     // Unincorporated communities
     "BERRYTOWN",
     "BROWNTOWN",
-    
+
     // Chemung County, NY
     "CHEMUNG",
     "WELLSBURG",
-    
+
     // Tioga County, NY
     "BARTON",
     "LOCKWOOD",
     "WAVERLY"
   };
-  
+
   private static final Properties MISSPELLED_CITIES = buildCodeTable(new String[]{
       "NTOWANDA", "TOWANDA"
   });
-  
+
   private static final Set<String> NY_CITY_TABLE = new HashSet<String>(Arrays.asList(new String[]{
       "BARTON",
       "CHEMUNG",
