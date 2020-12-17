45117f93af52a124c3a936e5c81fc0bf0b1db746
Kenneth Corbin <ken@cadpage.org>
2020-12-17 11:15:52 -0800

    Update sender filter for Prince Georges County, MD (G)

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/MD/MDPrinceGeorgesCountyGParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/MD/MDPrinceGeorgesCountyGParser.java
@@ -11,55 +11,55 @@ import net.anei.cadpage.parsers.MsgInfo.Data;
  * Prince Georges County, MD (variant E)
  */
 public class MDPrinceGeorgesCountyGParser extends MDPrinceGeorgesCountyBaseParser {
-  
+
   public MDPrinceGeorgesCountyGParser() {
     super("CALL ADDR! APT? PLACE/CS+? X! CH? BOX Units:UNIT% UNIT+ Remarks:INFO");
   }
-  
+
   @Override
   public String getFilter() {
-    return "@alert.co.pg.md.us,@c-msg.net,14100,12101,@everbridge.net,89361,87844";
+    return "@alert.co.pg.md.us,@c-msg.net,14100,12101,@everbridge.net,89361,87844,88911";
   }
-  
+
   private static final Pattern HTML_FILTER_PTN = Pattern.compile("\n {3,}<p> (.*?)</p>\n {3,}", Pattern.DOTALL);
-  
+
   @Override
   protected boolean parseHtmlMsg(String subject, String body, Data data) {
     if (body.startsWith("<!doctype html>\n")) {
       Matcher match = HTML_FILTER_PTN.matcher(body);
       if (!match.find()) return false;
-      body = match.group(1).trim();  
+      body = match.group(1).trim();
       body = body.replace("\n<br>", " ").replace("\n", " ").replace("<br>", " ");
     }
 
     return super.parseHtmlMsg(subject, body, data);
   }
-  
+
   private static final Pattern ID_PTN = Pattern.compile("(PF\\d{14}): *");
 
   @Override
   public boolean parseMsg(String body, Data data) {
-    
+
     body = stripFieldStart(body, "Dispatch\n");
-    
+
     Matcher match = ID_PTN.matcher(body);
     if (!match.lookingAt()) return false;
     data.strCallId = match.group(1);
     body = body.substring(match.end()).trim();
-    
+
     body = body.replace(" Unit:", " Units:");
     body = body.replace(". Remarks:", ", Remarks:");
     if (!parseFields(body.split(","), data)) return false;
-    
+
     fixCity(data);
     return true;
   }
-  
+
   @Override
   public String getProgram() {
     return "ID " + super.getProgram().replace("BOX", "BOX CITY ST");
   }
-  
+
   @Override
   public Field getField(String name) {
     if (name.equals("CALL")) return new MyCallField();
@@ -71,7 +71,7 @@ public class MDPrinceGeorgesCountyGParser extends MDPrinceGeorgesCountyBaseParse
     if (name.equals("UNIT")) return new MyUnitField();
     return super.getField(name);
   }
-  
+
   private class MyCallField extends Field {
     @Override
     public void parse(String field, Data data) {
@@ -83,13 +83,13 @@ public class MDPrinceGeorgesCountyGParser extends MDPrinceGeorgesCountyBaseParse
         data.strCall = field;
       }
     }
-    
+
     @Override
     public String getFieldNames() {
       return "CODE CALL";
     }
   }
-  
+
   // Cross field only exist if it has the correct keyword
   private class MyCrossField extends CrossField {
     @Override
@@ -103,13 +103,13 @@ public class MDPrinceGeorgesCountyGParser extends MDPrinceGeorgesCountyBaseParse
       super.parse(field, data);
       return true;
     }
-    
+
     @Override
     public void parse(String field, Data data) {
       if (!checkParse(field, data)) abort();
     }
   }
-  
+
   // Info field drops ProQA comments
   private class MyInfoField extends InfoField {
     @Override
@@ -119,7 +119,7 @@ public class MDPrinceGeorgesCountyGParser extends MDPrinceGeorgesCountyBaseParse
       super.parse(field, data);
     }
   }
-  
+
   // Unit fields join together with comma separators
   private class MyUnitField extends UnitField {
     @Override
@@ -131,7 +131,7 @@ public class MDPrinceGeorgesCountyGParser extends MDPrinceGeorgesCountyBaseParse
       }
     }
   }
-  
+
   static void fixCity(Data data) {
     if (data.strCity.length() > 0) return;
     if (data.strBox.startsWith("MA")) {
@@ -146,12 +146,12 @@ public class MDPrinceGeorgesCountyGParser extends MDPrinceGeorgesCountyBaseParse
       }
     }
   }
-  
+
   @Override
   public String adjustMapAddress(String addr) {
     return MDPrinceGeorgesCountyParser.adjustMapAddressCommon(addr);
   }
-  
+
   private static final Properties CALL_CODES = buildCodeTable(new String[]{
       "2ALARM",               "2ND ALARM",
       "ACC W INJ",            "PERSONAL INJURY ACCIDENT",
@@ -379,7 +379,7 @@ public class MDPrinceGeorgesCountyGParser extends MDPrinceGeorgesCountyBaseParse
       "WTASKH",               "WATER SUPPLY TASK FORCE",
       "WTASKN",               "WATER SUPPLY TASK FORCE-NON HYDRANT"
   });
-  
+
   private static final Properties MA_CITY_TABLE = buildCodeTable(new String[]{
       "MAAA", "ANNE ARUNDEL COUNTY",
       "MACA", "CALVERT COUNTY",
