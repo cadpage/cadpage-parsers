583a2db3803b708fb623c10ae7c85c8cc154e208
Kenneth Corbin <ken@cadpage.org>
2020-12-17 18:46:20 -0800

    Fixed parsing probem with Moniteau and Morgan Counties, MO

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/MO/MOMoniteauCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/MO/MOMoniteauCountyParser.java
@@ -1,10 +1,10 @@
 package net.anei.cadpage.parsers.MO;
 
-import net.anei.cadpage.parsers.FieldProgramParser;
+import net.anei.cadpage.parsers.HtmlProgramParser;
 import net.anei.cadpage.parsers.MsgInfo.Data;
 
 
-public class MOMoniteauCountyParser extends FieldProgramParser {
+public class MOMoniteauCountyParser extends HtmlProgramParser {
 
   public MOMoniteauCountyParser() {
     this("MONITEAU COUNTY", "MO");
@@ -12,7 +12,9 @@ public class MOMoniteauCountyParser extends FieldProgramParser {
 
   public MOMoniteauCountyParser(String defCity, String defState) {
     super(defCity, defState,
-          "Call:CALL! Sub:CALL/S! Place:PLACE! Address:ADDR! City:CITY! Apt:APT! Cross_Streets:X! Event#:ID! Reporting_Person:NAME! Phone#:PHONE! Unit:UNIT! Initiated:SKIP! Notes:INFO! INFO/N+");
+          "( SELECT/HTML Call:EMPTY! CALL! Sub:EMPTY! CALL/S Place:EMPTY! PLACE Address:EMPTY! ADDR City:EMPTY! CITY Apt:EMPTY! APT Cross_Streets:EMPTY! X Event#:EMPTY! ID! Reporting_Person:EMPTY! NAME Phone#:EMPTY! PHONE Initiated:EMPTY! SKIP! INFO/N+ Units%EMPTY! UNIT/C+?  COPYRIGHT " +
+          "| Call:CALL! Sub:CALL/S! Place:PLACE! Address:ADDR! City:CITY! Apt:APT! Cross_Streets:X! Event#:ID! Reporting_Person:NAME! Phone#:PHONE! ( Unit:UNIT! Initiated:SKIP! Notes:INFO! INFO/N+ | Initiated:SKIP! INFO/N+ Units%EMPTY! UNIT/C+ ) " +
+          "| ADDR! Apt:APT! CITY! Cross_Streets:X! Event#:ID! Reporting_Person:NAME! Phone#:PHONE! INFO/N+ Units%EMPTY! UNIT/C+ )");
   }
 
   @Override
@@ -26,7 +28,35 @@ public class MOMoniteauCountyParser extends FieldProgramParser {
   }
 
   @Override
+  protected boolean parseHtmlMsg(String subject, String body, Data data) {
+    if (body.startsWith("<html>")) {
+      setSelectValue("HTML");
+      int pt = body.indexOf("\n©");
+      if (pt >= 0) body  = body.substring(0,pt).trim();
+      return super.parseHtmlMsg(subject, body, data);
+    } else {
+      setSelectValue("");
+      return parseMsg(body, data);
+    }
+  }
+
+  @Override
   protected boolean parseMsg(String body, Data data) {
     return parseFields(body.split("\n"), data);
+  }
+
+  @Override
+  public Field getField(String name) {
+    if (name.equals("INFO")) return new MyInfoField();
+    if (name.equals("COPYRIGHT")) return new SkipField("©.*", true);
+    return super.getField(name);
+  }
+
+  private class MyInfoField extends InfoField {
+    @Override
+    public void parse(String field, Data data) {
+      if (field.startsWith("Call Received on ")) return;
+      super.parse(field, data);
+    }
   }
 }
