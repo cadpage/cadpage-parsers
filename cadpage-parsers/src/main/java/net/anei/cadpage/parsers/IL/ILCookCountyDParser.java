5fa450a13a694a43e11d4614079c6a3b1d9d5d34
Kenneth Corbin <ken@cadpage.org>
2020-12-18 12:35:48 -0800

    Fixed parsing problem with Cook County, IL

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/IL/ILCookCountyDParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/IL/ILCookCountyDParser.java
@@ -9,24 +9,24 @@ import net.anei.cadpage.parsers.MsgParser;
 
 
 public class ILCookCountyDParser extends MsgParser {
-  
+
   public ILCookCountyDParser() {
     super("COOK COUNTY", "IL");
     setFieldList("CALL TIME UNIT ADDR APT CITY PLACE INFO ID GPS MAP");
   }
-  
+
   @Override
   public String getFilter() {
     return "911@nwcds.org";
   }
-  
-  private static final Pattern BAD_PREFIX = Pattern.compile("(\\d\\d:\\d\\d:\\d\\d)\\] *");
+
+  private static final Pattern BAD_PREFIX = Pattern.compile("(?:\\}\\[)?(\\d\\d:\\d\\d:\\d\\d)\\] *");
   private static final Pattern SUBJECT_PTN = Pattern.compile("([-/ A-Z0-9]+)\\|(\\d\\d:\\d\\d:\\d\\d)");
-  private static final Pattern MASTER = Pattern.compile("(?:([A-Z0-9]+) )?at ([^,]*?)(?:, ([ A-Z]+))? - ([^,]*), (.*?)\\. ?\\$([A-Z]{3}\\d{8})\\. (\\d{8})\\.(?: ?([-+]?\\d{2}\\.\\d{6,})\\. ?([-+]?\\d{2}\\.\\d{6,})\\. ?\\. ?([/A-Z0-9]*)\\.)?", Pattern.DOTALL);
-  
+  private static final Pattern MASTER = Pattern.compile("(?:([A-Z0-9]*),* )?at ([^,]*?)(?:, ([ A-Z]+))? - ([^,]*), (.*?)\\. ?\\$([A-Z]{3}\\d{8})\\. (\\d{8})\\.(?: ?([-+]?\\d{2}\\.\\d{6,})\\. ?([-+]?\\d{2}\\.\\d{6,})\\. ?\\. ?([-/A-Z0-9]*)\\.)?[\\. ]*", Pattern.DOTALL);
+
   @Override
   public boolean parseMsg(String subject, String body, Data data) {
-    
+
     Matcher match = SUBJECT_PTN.matcher(subject);
     if (match.matches()) {
       data.strCall = match.group(1).trim();
@@ -37,9 +37,9 @@ public class ILCookCountyDParser extends MsgParser {
       data.strCall = subject;
       data.strTime =  match.group(1);
       body = body.substring(match.end());
-      
+
     }
-    
+
     match = MASTER.matcher(body);
     if (!match.matches()) return false;
     data.strUnit = getOptGroup(match.group(1));
@@ -52,7 +52,7 @@ public class ILCookCountyDParser extends MsgParser {
     data.strMap = getOptGroup(match.group(10));
     return true;
   }
-  
+
   private static final Properties CITY_CODES = buildCodeTable(new String[]{
       "AH",   "ARLINGTON HEIGHTS",
       "BA",   "BARRINGTON",
