52040c25ffd8e0268d3298539ae5bdc43bb6d2af
Kenneth Corbin <ken@cadpage.org>
2020-12-21 17:33:04 -0800

    Update sender filter for Lancaster County, PA

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/PA/PALancasterCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/PA/PALancasterCountyParser.java
@@ -20,7 +20,7 @@ public class PALancasterCountyParser extends FieldProgramParser {
 
   @Override
   public String getFilter() {
-    return "911@lcwc911.us,messaging@iamresponding.com,@everbridge.net,@den.everbridge.net,@den2.everbridge.net,@smtpic-ne.prd1.everbridge.net,141000,89361";
+    return "911@lcwc911.us,messaging@iamresponding.com,@everbridge.net,@den.everbridge.net,@den2.everbridge.net,@smtpic-ne.prd1.everbridge.net,141000,89361,88911";
   }
 
   private static final Pattern XML_COMMENT_PTN = Pattern.compile("<!--.*?-->");
