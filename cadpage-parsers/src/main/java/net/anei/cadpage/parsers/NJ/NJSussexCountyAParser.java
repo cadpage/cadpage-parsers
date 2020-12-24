77940132426500cc8eb99d05f3b5f74b856d17f9
Kenneth Corbin <ken@cadpage.org>
2020-12-23 18:55:31 -0800

    Update Genome.log

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/NJ/NJSussexCountyAParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/NJ/NJSussexCountyAParser.java
@@ -10,31 +10,31 @@ import net.anei.cadpage.parsers.MsgInfo.Data;
 
 
 public class NJSussexCountyAParser extends SmartAddressParser {
-  
+
   public NJSussexCountyAParser() {
     this("SUSSEX COUNTY", "NJ");
   }
-  
+
   public NJSussexCountyAParser(String defCity, String defState) {
     super(defCity, defState);
     setFieldList("ID CODE CALL BOX PLACE ADDR X APT CITY ST INFO UNIT");
   }
-  
+
   @Override
   public String getAliasCode() {
     return "NJSussexCountyA";
   }
-  
+
   @Override
   public String getFilter() {
     return "@nwbcd.org,@middle,@englewoodpd.org,@enforsys.com,@atpdtext.org,alerts@atpd.org,wcpdicm@enforsys.com,@Enfwebmail.onmicrosoft.com,ems911page@twpoceannj.gov,donotreply@Enfwebmail.onmicrosoft.com,westcaldwellpd@enfwebmail.onmicrosoft.com";
   }
-  
-  
+
+
   private static final Pattern SUBJECT_PTN = Pattern.compile("[A-Z]{1,5}-?[A-Z]?\\d{4}-?\\d{5,6}(?: .*)?");
   private static final Pattern TRAIL_MARK_PTN = Pattern.compile("[-.\\s]*\n\\*{1,3}This\\semail");
   private static final Pattern UNIT_JUNK_PTN = Pattern.compile(" - (?:CAR|RESCUE|ENGINE|TOWER|TRUCK|TENDER) \\d+");
-  private static final Pattern MASTER_PTN = 
+  private static final Pattern MASTER_PTN =
     Pattern.compile("([-/.,;&A-Za-z0-9 ]+) @ (?:BOX (\\S+) - )?(?:([^,]*) - )?([^,]+?) *(?:, ([^-\\.]*)(?:\\. -| -|\\.|$)| |(?<! )-(?= )|$)(?: (.*?)[-\\.]*)?", Pattern.DOTALL);
   private static final Pattern CITY_ST_ZIP_PTN = Pattern.compile("(.*?)(?: ([A-Z]{2}))?(?: \\d{5})?");
   private static final Pattern END_STAR_PTN = Pattern.compile("([A-Z0-9])\\*");
@@ -49,7 +49,7 @@ public class NJSussexCountyAParser extends SmartAddressParser {
       if (! SUBJECT_PTN.matcher(subject).matches()) return false;
       data.strCallId = subject;
     }
-    
+
     Matcher match = TRAIL_MARK_PTN.matcher(body);
     if (match.find()) body = body.substring(0,match.start()).trim();
 
@@ -58,7 +58,7 @@ public class NJSussexCountyAParser extends SmartAddressParser {
     if (pt >= 0) {
       String unit = body.substring(pt+14).trim();
       body = body.substring(0, pt).trim();
-      
+
       unit = UNIT_JUNK_PTN.matcher(unit).replaceAll("");
       pt = unit.indexOf(" - ");
       if (pt >= 0) {
@@ -69,8 +69,8 @@ public class NJSussexCountyAParser extends SmartAddressParser {
       }
       data.strUnit = unit.replace(' ', '_');
     }
-    
-    
+
+
     body = body.replace('\n', ' ');
     match = MASTER_PTN.matcher(body);
     if (!match.matches()) return false;
@@ -80,28 +80,28 @@ public class NJSussexCountyAParser extends SmartAddressParser {
       data.strCode = data.strCall;
       data.strCall = call;
     }
-    
+
     data.strBox = getOptGroup(match.group(2));
     data.strPlace = getOptGroup(match.group(3));
     parseAddress(StartType.START_ADDR, FLAG_NO_CITY | FLAG_ANCHOR_END | FLAG_RECHECK_APT, match.group(4).trim(), data);
     String city = getOptGroup(match.group(5));
     String sInfo = getOptGroup(match.group(6));
-    
+
     pt = city.lastIndexOf(',');
     if (pt >= 0) {
       data.strApt = append(data.strApt, ", ", city.substring(0,pt).trim());
       city = city.substring(pt+1).trim();
     }
-    
+
     match = CITY_ST_ZIP_PTN.matcher(city);
     if (match.matches()) {
       city = match.group(1).trim();
       data.strState = getOptGroup(match.group(2));
     }
-    
+
     if (city.equals("CMCH")) city = "CAPE MAY COURT HOUSE";
     data.strCity = city;
-    
+
     if (data.strCity.equals("OUT OF TOWN")) {
       data.strCity = data.defCity = data.defState = "";
     }
@@ -146,13 +146,13 @@ public class NJSussexCountyAParser extends SmartAddressParser {
     data.strSupp = append(data.strSupp, "\n", trailInfo);
     return true;
   }
-  
+
   @Override
   protected boolean isNotExtraApt(String apt) {
     if (apt.startsWith("(")) return true;
     return super.isNotExtraApt(apt);
   }
-  
+
   private static final Properties CALL_CODES = buildCodeTable(new String[]{
       // "1045F",    "1045F",
       // "ASSIST-F", "ASSIST-F",
@@ -197,7 +197,7 @@ public class NJSussexCountyAParser extends SmartAddressParser {
       "FSTRUCT",  "FIRE STRUCTURE",
       "FTRIB",    "TRI-BORO ALARM",
       "FTRIB-O",  "TRI-BORO RESPONSE",
-      "FUEL",     "FUEL SPILL",   
+      "FUEL",     "FUEL SPILL",
       "FVEHF",    "VEHICLE FIRE",
       "FVEHICLE", "FIRE VEHICLE",
       "FWORK",    "WORKING FIRE (CONFIRMED)",
