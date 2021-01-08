e4e74d8d74653a2f4040564d898654c1aaad449c
Kenneth Corbin <ken@cadpage.org>
2021-01-07 21:29:43 -0800

    Fixed parsing problems with Portage County, OH

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/OH/OHPortageCountyFParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/OH/OHPortageCountyFParser.java
@@ -3,11 +3,11 @@ package net.anei.cadpage.parsers.OH;
 import net.anei.cadpage.parsers.dispatch.DispatchA9Parser;
 
 public class OHPortageCountyFParser extends DispatchA9Parser {
-  
+
   public OHPortageCountyFParser() {
     super("PORTAGE COUNTY", "OH");
   }
-  
+
   @Override
   public String getFilter() {
     return "dispatch@kent.edu";
--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/OH/OHPortageCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/OH/OHPortageCountyParser.java
@@ -1,5 +1,6 @@
 package net.anei.cadpage.parsers.OH;
 
+import java.util.Properties;
 import java.util.regex.Pattern;
 
 import net.anei.cadpage.parsers.GroupBestParser;
@@ -10,26 +11,33 @@ import net.anei.cadpage.parsers.GroupBlockParser;
  */
 
 public class OHPortageCountyParser extends GroupBestParser {
-  
+
   public OHPortageCountyParser() {
     super(new OHPortageCountyAParser(), new OHPortageCountyBParser(), new OHPortageCountyCParser(),
-        new OHPortageCountyDParser(), new OHPortageCountyFParser(), 
+        new OHPortageCountyDParser(), new OHPortageCountyFParser(),
         new GroupBlockParser(), new OHPortageCountyCencommParser());
   }
-  
+
   static String fixMapAddress(String addr) {
     return STATE225_PTN.matcher(addr).replaceAll("STATE ROUTE 225");
   }
   private static final Pattern STATE225_PTN = Pattern.compile("\\b(STATE|ST|OH) +225\\b", Pattern.CASE_INSENSITIVE);
 
-  
+
   static String fixCity(String city) {
-    if (city.equalsIgnoreCase("EDINBERG") || city.equalsIgnoreCase("EDIN")) city = "EDINBURG";
-    return city;
+    String fixCity = FIX_CITY_TABLE.getProperty(city.toUpperCase());
+    return fixCity != null ? fixCity : city;
   }
 
+  private static final Properties FIX_CITY_TABLE = buildCodeTable(new String[]{
+      "EDINBERT",     "EDINBURG",
+      "EDIN",         "EDINBURG",
+      "GARRETSVILE",  "GARRETTSVILLE",
+      "ROOTSOWN",     "ROOTSTOWN",
+  });
+
   static final String[] CITY_LIST = new String[]{
-    
+
     // Townships
     "ATWATER TOWNSHIP",
     "BRIMFIELD TOWNSHIP",
@@ -68,7 +76,7 @@ public class OHPortageCountyParser extends GroupBestParser {
     "SHALERSVILLE TWP",
     "SUFFIELD TWP",
     "WINDHAM TWP",
-    
+
     // Townships without suffix
     "ATWATER",
     "BRIMFIELD",
@@ -92,6 +100,7 @@ public class OHPortageCountyParser extends GroupBestParser {
     "AURORA",
     "BRADY LAKE",
     "GARRETTSVILLE",
+    "GARRETSVILE",  // Misspelled
     "HIRAM",
     "KENT",
     "MANTUA",
@@ -108,11 +117,18 @@ public class OHPortageCountyParser extends GroupBestParser {
     // Census-designated places
     "ATWATER",
     "BRIMFIELD",
-    
+
+    // Geauga County
+    "AUBURN",
+    "AUBURN TWP",
+    "BURTON",
+    "TROY",
+    "TROY TWP",
+
     // Mahoning County
     "BERLIN",
     "BERLIN CENTER",
-    
+
     // Trumbull County
     "NEWTON FALLS",
     "SOUTHINGTON TWP"
