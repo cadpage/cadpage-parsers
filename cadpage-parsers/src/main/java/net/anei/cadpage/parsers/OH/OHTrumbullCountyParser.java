a15723ba5f37acc0e1448231f1e9b1366d990e3a
Kenneth Corbin <ken@cadpage.org>
2021-01-08 16:59:38 -0800

    Fixed parsing problem with Trumbull County, OH

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/OH/OHTrumbullCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/OH/OHTrumbullCountyParser.java
@@ -1,29 +1,41 @@
 package net.anei.cadpage.parsers.OH;
 
+import java.util.regex.Pattern;
+
 import net.anei.cadpage.parsers.FieldProgramParser;
 import net.anei.cadpage.parsers.MsgInfo.Data;
 
 /**
- * Trumble County, OH 
+ * Trumble County, OH
  */
 public class OHTrumbullCountyParser extends FieldProgramParser {
-  
+
   public OHTrumbullCountyParser() {
-    super("TRUMBULL COUNTY", "OH", 
-           "ADDR APT EMPTY+? CALL! EMPTY+? SRC EMPTY+? X EMPTY+? INFO+");
+    super("TRUMBULL COUNTY", "OH",
+           "ADDR PLACE? CALL! UNIT X! INFO+");
   }
-  
+
   @Override
   public String getFilter() {
     return "911no@co.trumbull.oh.us";
   }
-  
+
+  private static final Pattern DELIM = Pattern.compile("\\*{2,}");
+
   @Override
   public boolean parseMsg(String body, Data data) {
     if (!body.startsWith("*")) return false;
-    return parseFields(body.substring(1).trim().split("\\*"), data);
+    body = body.substring(1).trim();
+    return parseFields(DELIM.split(body), data);
   }
-  
+
+  @Override
+  protected Field getField(String name) {
+    if (name.equals("ADDR")) return new MyAddressField();
+    if (name.equals("X")) return new MyCrossField();
+    return super.getField(name);
+  }
+
   private class MyAddressField extends AddressField {
     @Override
     public void parse(String field, Data data) {
@@ -33,26 +45,32 @@ public class OHTrumbullCountyParser extends FieldProgramParser {
       data.strCity = city;
       super.parse(p.get(), data);
     }
-    
+
     @Override
     public String getFieldNames() {
       return super.getFieldNames() + " CITY";
     }
   }
-  
+
   private class MyCrossField extends CrossField {
     @Override
-    public void parse(String field, Data data) {
-      field = field.replace("//", "/");
-      if (field.endsWith("/")) field = field.substring(0,field.length()-1).trim();
-      super.parse(field, data);
+    public boolean canFail() {
+      return true;
     }
-  }
 
-  @Override
-  protected Field getField(String name) {
-    if (name.equals("ADDR")) return new MyAddressField();
-    if (name.equals("X")) return new MyCrossField();
-    return super.getField(name);
+    @Override
+    public boolean checkParse(String field, Data data) {
+      if (!field.contains("/")) return false;
+
+      field = field.replace("//", "/");
+      field = stripFieldEnd(field, "/");
+      super.parse(field, data);
+      return true;
+    }
+
+    @Override
+    public void parse(String field, Data data) {
+      if (!checkParse(field, data)) abort();
+    }
   }
 }
