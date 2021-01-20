a82e2de4093aa1840c878d605ad41151f89c531c
Kenneth Corbin <ken@cadpage.org>
2021-01-20 11:05:53 -0800

    Fixed parsing problem with Petersburg, VA

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/VA/VAPetersburgParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/VA/VAPetersburgParser.java
@@ -1,39 +1,29 @@
 package net.anei.cadpage.parsers.VA;
 
-import java.util.Properties;
-
+import net.anei.cadpage.parsers.FieldProgramParser;
 import net.anei.cadpage.parsers.MsgInfo.Data;
-import net.anei.cadpage.parsers.dispatch.DispatchOSSIParser;
 
 
-public class VAPetersburgParser extends DispatchOSSIParser {
+public class VAPetersburgParser extends FieldProgramParser {
 
   public VAPetersburgParser() {
-    super(CITY_CODES, "PETERSBURG", "VA",
-          "( CANCEL ADDR CITY! | FYI CALL PRI? ADDR! CITY? ( X X? | ) ) INFO/N+");
-    setupSpecialStreets("BOULEVARD");
-    removeWords("NEW");
+    super("PETERSBURG", "VA",
+          "ID UNIT CALL ADDRCITYST! XST1:X! XST2:X! NOTES:INFO! END");
   }
 
   @Override
   public String getFilter() {
-    return "CAD@petersburg-police.com";
+    return "CAD@petersburg-police.com,cad@petersburg-va.org";
   }
 
   @Override
   protected boolean parseMsg(String body, Data data) {
-    int pt = body.indexOf("\nThis e-mail");
-    if (pt >= 0) body = body.substring(0,pt).trim();
-    return super.parseMsg(body, data);
+    return parseFields(body.split(" \\| "), data);
   }
 
   @Override
   public Field getField(String name) {
-    if (name.equals("PRI")) return new PriorityField("\\d", true);
+    if (name.equals("ID")) return new IdField("\\$([A-Z]{1,5}\\d{2}-\\d{6})", true);
     return super.getField(name);
   }
-
-  private static final Properties CITY_CODES = buildCodeTable(new String[]{
-    "PTBG", "PETERSBURG"
-  });
 }
