dfc4c238d9edd55d659089ae4ddff8f402e91f07
Kenneth Corbin <ken@cadpage.org>
2021-01-20 16:48:29 -0800

    Updated city code table for Sawyer County, WI

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/WI/WISawyerCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/WI/WISawyerCountyParser.java
@@ -2,6 +2,7 @@ package net.anei.cadpage.parsers.WI;
 
 import java.util.Properties;
 
+import net.anei.cadpage.parsers.MsgInfo.Data;
 import net.anei.cadpage.parsers.dispatch.DispatchA19Parser;
 
 public class WISawyerCountyParser extends DispatchA19Parser {
@@ -20,19 +21,59 @@ public class WISawyerCountyParser extends DispatchA19Parser {
     return MAP_FLG_PREFER_GPS;
   }
 
+  @Override
+  protected boolean parseMsg(String subject, String body, Data data) {
+    if (!super.parseMsg(subject, body, data)) return false;
+    if (data.strCity.equals("OUT OF COUNTY")) data.defCity = "";
+    return true;
+  }
+
+  @Override
+  public String adjustMapCity(String city) {
+    if (city.equals("OUT OF COUNTY")) return "";
+    return city;
+  }
+
+
   private static final Properties CITY_CODES = buildCodeTable(new String[] {
       "BAS", "BASS LAKE",
+      "C",   "COUDERAY",
       "CHA", "HAYWARD",
+      "CUD", "COUDERAY",
+      "DRA", "DRAPER",
+      "E",   "EDGEWATER",
+      "EDG", "EDGEWATER",
+      "EXE", "EXELAND",
       "HAY", "HAYWARD",
+      "HUN", "HUNTER",
       "LBA", "BASS LAKE",
       "LEN", "LENROOT",
+      "LCU", "COUDERAY",
       "LHA", "HAYWARD",
       "LHU", "HUNTER",
+      "LRA", "RADISSON",
       "LSA", "SAND LAKE",
+      "MEA", "MEADOWBROOK",
+      "MET", "METEOR",
+      "O",   "OJIBWA",
       "OJI", "OJIBWA",
+      "OUT", "OUT OF COUNTY",
+      "R",   "RADISSON",
       "RAD", "RADISSON",
+      "ROU", "ROUND LAKE",
+      "S",   "STONE LAKE",
+      "SAN", "SAND LAKE",
       "SPI", "SPIDER LAKE",
-      "VEX", "EXELAND"
+      "THA", "HAYWARD",
+      "VCN", "CONVERSION",
+      "VEX", "EXELAND",
+      "VCO", "COUDERAY",
+      "VEX", "EXELAND",
+      "VRA", "RADISSON",
+      "VWI", "WINTER",
+      "W",   "WINTER",
+      "WEI", "WEIRGOR",
+      "WIN", "WINTER"
   });
 
 }
