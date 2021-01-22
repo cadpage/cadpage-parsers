4fccfe8c0e6602535083dbf6105b0499f8c99118
Kenneth Corbin <ken@cadpage.org>
2021-01-21 19:11:27 -0800

    Fixed problem with DispatchA49Parser subclasses

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/dispatch/DispatchA49Parser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/dispatch/DispatchA49Parser.java
@@ -13,33 +13,33 @@ Lafayette Parish, LA
 */
 
 public class DispatchA49Parser extends FieldProgramParser {
-  
+
   private boolean checkCity;
-  
+
   public  DispatchA49Parser(String defCity, String defState) {
     this(null, defCity, defState);
   }
 
   public DispatchA49Parser(Properties cityCodes, String defCity, String defState) {
-    super(cityCodes, defCity, defState, 
+    super(cityCodes, defCity, defState,
           "( Call_Number:ID! Date/Time:DATETIME! Address:ADDR! " +
           "| CAD_Num:SKIP! Addr:ADDR/S! Times:EMPTY! INFO/R! INFO/N+ Rpt#:ID END " +
           "| ( Rpt#:ID! Addr:ADDR/S! Inc_Type:CODE! " +
-            "| DATE_TIME_SRC! Addr:ADDR/S! City:CITY? Cross:X? Inc_Type:CODE? Juris:SKIP? Report_#:ID? " + 
+            "| DATE_TIME_SRC! Addr:ADDR/S! City:CITY? Cross:X? Inc_Type:CODE? Juris:SKIP? Report_#:ID? " +
             "| Date:DATE! Time:TIME! Inc#:ID! INFO/RN+ " +
             ") REMARKS? EXTRA+ )");
     checkCity = cityCodes != null;
   }
-  
+
   private static final Pattern REMARKS_PTN = Pattern.compile("(\nRemarks)[: ]+");
-  
+
   @Override
   protected boolean parseMsg(String body, Data data) {
     body = REMARKS_PTN.matcher(body).replaceFirst("$1:\n");
     if (body.startsWith("Call Number:")) data.msgType = MsgType.RUN_REPORT;
     return parseFields(body.split("\n+"), data);
   }
-  
+
   @Override
   public Field getField(String name) {
     if (name.equals("DATETIME")) return new DateTimeField("\\d\\d/\\d\\d/\\d{4} \\d\\d:\\d\\d:\\d\\d", true);
@@ -68,7 +68,7 @@ public class DispatchA49Parser extends FieldProgramParser {
       return "DATE TIME SRC ID";
     }
   }
-  
+
   private class BaseAddressField extends AddressField {
     @Override
     public void parse(String field, Data data) {
@@ -80,7 +80,7 @@ public class DispatchA49Parser extends FieldProgramParser {
       }
     }
   }
-  
+
   private class BaseCityField extends CityField {
     @Override
     public void parse(String field, Data data) {
@@ -89,8 +89,9 @@ public class DispatchA49Parser extends FieldProgramParser {
     }
   }
 
+  private static final Pattern EXTRA_JUNK_PTN = Pattern.compile("(.*?) +\\d{6}");
   private static final Pattern EXTRA_ID_PTN  = Pattern.compile(">(?:RPT#|AC)< *([-\\d]+)");
-  private static final Pattern EXTRA_CALL_PTN = Pattern.compile("F>>?IC< *(?:F\\.)? *(.*?)(?: \\d{6})?");
+  private static final Pattern EXTRA_CALL_PTN = Pattern.compile("F>>?IC< *(?:F\\.)? *(.*?)");
   private static final Pattern EXTRA_GPS_PTN = Pattern.compile("\\bLat=([-+]\\d+\\.\\d{4,}) Long=([-+]\\d+\\.\\d{4,})\\b");
   private static final Pattern EXTRA_TIME_OP_PTN = Pattern.compile("(.*) \\d{4},\\d{3}");
   private static final Pattern EXTRA_PREFIX_PTN = Pattern.compile("(?:[A-Z]>)?(?:>(?:AC|E9|IC)<)? *(?:[A-Z]\\.)? *(.*)");
@@ -100,73 +101,76 @@ public class DispatchA49Parser extends FieldProgramParser {
   private class BaseExtraField extends Field {
     @Override
     public void parse(String field, Data data) {
-      
-      Matcher match = EXTRA_ID_PTN.matcher(field);
+
+      Matcher match = EXTRA_JUNK_PTN.matcher(field);
+      if (match.matches()) field = match.group(1);
+
+      match = EXTRA_ID_PTN.matcher(field);
       if (match.matches()) {
         data.strCallId = match.group(1);
         return;
       }
-      
+
       match = EXTRA_CALL_PTN.matcher(field);
       if (match.matches()) {
         data.strCall = append(data.strCall, " / ", match.group(1));
         return;
       }
-      
+
       match = EXTRA_GPS_PTN.matcher(field);
       if (match.find()) {
         setGPSLoc(match.group(1) + ',' + match.group(2), data);
         return;
       }
-      
+
       match = EXTRA_TIME_OP_PTN.matcher(field);
       if (match.matches()) field = match.group(1).trim();
-      
+
       match = EXTRA_PREFIX_PTN.matcher(field);
       if (match.matches()) field = match.group(1);
-     
+
       // Old data changes are not worth keeping
       if (field.startsWith(">UG<")) return;
-      
+
       // Place name is worth extracting
       if (field.startsWith(">CP<")) {
         field = field.substring(4).trim();
         if (field.toUpperCase().startsWith("ALARM =")) {
           field = field.substring(7).trim();
           int pt = field.indexOf(',');
-          if (pt >= 0) field = field.substring(pt+1).trim(); 
+          if (pt >= 0) field = field.substring(pt+1).trim();
         }
         data.strPlace = append(data.strPlace, " - ", field);
         return;
       }
-      
+
       if (EXTRA_PHONE_PTN1.matcher(field).matches()) {
         data.strPhone = field;
         return;
       }
-      
+
       // Drop useless cell tower address
       if (field.startsWith("ADDR =")) return;
-      
+
       // and coordinate comment
       if (field.startsWith("WPH2 COORDS.")) return;
-      
+
       // Cell phone number
       match = EXTRA_PHONE_PTN2.matcher(field);
       if (match.matches()) {
         data.strPhone = match.group(1);
         return;
       }
-      
+
       // Cell phone name
       if (field.startsWith("NAME =")) {
         data.strName = cleanWirelessCarrier(field.substring(6).trim());
         return;
       }
-      
+
       match = EXTRA_LEAD_GT_PTN.matcher(field);
       if (match.lookingAt()) field = field.substring(match.end()).trim();
-      
+
       data.strSupp = append(data.strSupp, "\n", field);
     }
 
