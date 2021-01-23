3156abd9db9eca5db5a1d51adc351994e2d2e20a
Kenneth Corbin <ken@cadpage.org>
2021-01-22 18:59:40 -0800

    Fixed parsing problem with Pearl River County,  MS

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/MS/MSPearlRiverCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/MS/MSPearlRiverCountyParser.java
@@ -1,5 +1,6 @@
 package net.anei.cadpage.parsers.MS;
 
+import java.util.Properties;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -9,8 +10,9 @@ import net.anei.cadpage.parsers.MsgInfo.Data;
 public class MSPearlRiverCountyParser extends FieldProgramParser {
 
   public MSPearlRiverCountyParser() {
-    super("PEARL RIVER COUNTY", "MS",
+    super(CITY_LIST, "PEARL RIVER COUNTY", "MS",
           "CALL ADDRCITY! INFO/N+");
+    setupCities(MAP_CITY_TABLE);
   }
 
   @Override
@@ -45,17 +47,86 @@ public class MSPearlRiverCountyParser extends FieldProgramParser {
   private class MyAddressCityField extends AddressCityField {
     @Override
     public void parse(String field, Data data) {
+      field = stripFieldStart(field, "INTERSECTION:");
       Matcher match = ADDRESS_PTN.matcher(field);
       if (!match.matches()) abort(); //  Can't happen
-      super.parse(match.group(1), data);
+      field = match.group(1);
       data.strBox = getOptGroup(match.group(2));
       String gps = match.group(3);
       if (gps != null) setGPSLoc(gps.trim(), data);
+
+      String[] parts = field.split(" *, *");
+      switch (parts.length) {
+      case 1:
+        field = stripFieldEnd(field, " MS");
+        parseAddress(StartType.START_ADDR, FLAG_ANCHOR_END, field, data);
+        break;
+
+      case 2:
+        if (!isCity(parts[1]) && isValidAddress(parts[1])) {
+          data.strPlace = parts[0];
+          parseAddress(parts[1], data);
+        } else {
+          parseAddress(parts[0], data);
+          data.strCity = parts[1];
+        }
+        break;
+
+      case 3:
+        data.strPlace = parts[0];
+        parseAddress(parts[1], data);
+        data.strCity = parts[2];
+        break;
+
+      default:
+        abort();
+      }
     }
 
     @Override
     public String getFieldNames() {
-      return "ADDR APT CITY BOX GPS";
+      return "PLACE ADDR APT CITY BOX GPS";
     }
   }
+
+  @Override
+  public String adjustMapCity(String city) {
+    return convertCodes(city, MAP_CITY_TABLE);
+  }
+
+  private static final Properties MAP_CITY_TABLE = buildCodeTable(new String[]{
+      "DERBY",          "POPLARVILLE",
+      "HILLSDALE",      "LUMBERTON",
+      "LEETOWN",        "LUMBERTON",
+      "MILL CREEK",     "CARRIERE",
+      "NICHOLSON",      "PICAYUNE",
+      "PROGRESS",       "POPLARVILLE",
+      "RESTERTOWN",     "POPLARVILLE",
+      "SALEM",          "PICAYUNE",
+      "SONES CHAPEL",   "POPLARVILLE",
+      "SYCAMORE",       "CARRIERE",
+  });
+
+  private static final String[] CITY_LIST = new String[]{
+
+      // Cities
+      "LUMBERTON",
+      "PICAYUNE",
+      "POPLARVILLE",
+
+      // Census-designated places
+      "HIDE-A-WAY LAKE",
+      "NICHOLSON",
+
+      // Other unincorporated communities
+      "CAESAR",
+      "CARRIERE",
+      "CROSSROADS",
+      "HENLEYFIELD",
+      "MCNEILL",
+      "OZONA"
+
+
+
+  };
 }
