7a16c6bd21761d262b58af1869d56212f4020b72
Kenneth Corbin <ken@cadpage.org>
2021-01-23 18:51:04 -0800

    Fixed parsing problem with St Tammay Parish, LA

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/LA/LAStTammanyParishAParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/LA/LAStTammanyParishAParser.java
@@ -9,66 +9,66 @@ import net.anei.cadpage.parsers.MsgInfo.Data;
 
 /**
  * St Tammany Parish, LA
- * NOTES: There is an Apartment field that is not always present.  This means that 
+ * NOTES: There is an Apartment field that is not always present.  This means that
  * the SRC_X field must have check logic in order for the FieldProgramParser to know
  * whether the apartment field exits or not.
  */
 public class LAStTammanyParishAParser extends FieldProgramParser {
-  
+
   private static final Pattern DELIM = Pattern.compile("\\|");
-  
+
   public LAStTammanyParishAParser() {
     super("ST TAMMANY PARISH", "LA",
         "CALL! PLACE ADDRCITY APT? SRC_X TIME! INFO");
   }
-  
+
   @Override
   public String getFilter() {
-    return "@dapage.net,@stfd.dapage.net,@stfpd1.dapage.net";
+    return "@dapage.net,@stfd.dapage.net,@stfpd1.dapage.net,stfpd1@stfpd1.dapage.net";
   }
-  
+
   @Override
   public boolean parseMsg(String subject, String body, Data data) {
-    
+
     String[] fields = DELIM.split(body);
-    
-    // Check to see subject and first field match, this is just so we 
+
+    // Check to see subject and first field match, this is just so we
     // don't have a greedy parser
     if(!subject.equals(fields[0])) return false;
-    
+
     if(!super.parseFields(fields, data)) return false;
-    
+
     if (data.strAddress.length() == 0) {
       data.strAddress = "";
       parseAddress(data.strCross, data);
       data.strCross = "";
     }
-    
+
     // Replace certain small towns or subdivisions with Google parameter
     String cityAlt = CITY_TABLE.getProperty(data.strCity);
     if(cityAlt != null && cityAlt.length() > 0) {
       data.strPlace = data.strCity;
       data.strCity = cityAlt;
     }
-    
+
     return true;
   }
-  
+
   @Override
   public Field getField(String name) {
     if (name.equals("SRC_X")) return new MySourceCrossField();
     if (name.equals("TIME")) return new TimeField("\\d\\d:\\d\\d", true);
     return super.getField(name);
   }
-  
+
   /***
-   * MySourceCrossField - Extracts from the field that contains the source the 
+   * MySourceCrossField - Extracts from the field that contains the source the
    * cross streee and source.  It also checks for an optional GRID field.  Grid is
    * then stored in MAP.
    */
-  private static final Pattern SOURCE_X_PTN = Pattern.compile("(STA [^ ]+) ?(?:GRID (\\d+))? XS");
+  private static final Pattern SOURCE_X_PTN = Pattern.compile("((?:STA )?[^ ]+) ?(?:GRID (\\d+))? XS");
   private class MySourceCrossField extends SourceField {
-    
+
     @Override
     public boolean checkParse(String field, Data data) {
       Matcher sourceMatch = SOURCE_X_PTN.matcher(field);
@@ -78,8 +78,8 @@ public class LAStTammanyParishAParser extends FieldProgramParser {
       }
       else return false;
     }
-    
-    @Override 
+
+    @Override
     public void parse(String field, Data data) {
       Matcher sourceMatch = SOURCE_X_PTN.matcher(field);
       if(sourceMatch.lookingAt()) {
@@ -88,17 +88,17 @@ public class LAStTammanyParishAParser extends FieldProgramParser {
         data.strCross = field.substring(sourceMatch.end()).trim();
       }
     }
-    
-    @Override 
+
+    @Override
     public String getFieldNames() {
       return super.getFieldNames() + " X MAP";
     }
-    
-    @Override 
+
+    @Override
     public boolean canFail() {
       return true;
     }
-    
+
   }
 
   // Lookup table for Google city designations
