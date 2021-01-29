aebcdf68ba75a49d8c65e7df6e36d415e263ce70
Kenneth Corbin <ken@cadpage.org>
2021-01-29 07:18:57 -0800

    Update call code table for St LOuis County, MO (G)

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/MO/MOStLouisCountyGParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/MO/MOStLouisCountyGParser.java
@@ -10,22 +10,22 @@ import net.anei.cadpage.parsers.dispatch.DispatchGlobalDispatchParser;
 
 
 public class MOStLouisCountyGParser extends DispatchGlobalDispatchParser {
-  
-  
+
+
   public MOStLouisCountyGParser() {
     super(MOStLouisCountyParser.CITY_LIST, "ST LOUIS COUNTY", "MO", LEAD_SRC_UNIT_ADDR,
           Pattern.compile("FD|STILL"), Pattern.compile("[A-Z]{0,2}\\d{4}|\\d{2}[A-Z]{0,3}|DUTY"));
     setupCallList(CALL_LIST);
 
   }
-  
+
   @Override
   public String getFilter() {
     return "Kirkwood@Kirkwoodmo.org";
   }
-  
+
   private static final Pattern TAC_PTN = Pattern.compile(" *\\b((?:DUTY )?TAC \\S+)\\b *");
-  
+
   @Override
   public boolean parseMsg(String subject, String body, Data data) {
     if (!subject.equals("Active 911 Paging")) return false;
@@ -38,7 +38,7 @@ public class MOStLouisCountyGParser extends DispatchGlobalDispatchParser {
     if (data.strApt.equals("-")) data.strApt = "";
     return true;
   }
-  
+
   @Override
   public String getProgram() {
     return "CH " + super.getProgram();
@@ -57,7 +57,7 @@ public class MOStLouisCountyGParser extends DispatchGlobalDispatchParser {
       super.parse(field, data);
     }
   }
-  
+
   private static final CodeSet CALL_LIST = new CodeSet(
       "1ST ALARM/ FIRE COMMERCIAL",
       "1ST ALARM/ FIRE RESIDENTIAL",
@@ -68,6 +68,7 @@ public class MOStLouisCountyGParser extends DispatchGlobalDispatchParser {
       "BRUSH WITH EXPOSURES",
       "CAR FIRE WITH EXPOSURES",
       "CHECK THE AREA",
+      "CHECK THE BUILDING STILL",
       "CO DETECTOR",
       "ELEVATOR RESCUE",
       "EXTERIOR GAS LEAK",
@@ -90,10 +91,10 @@ public class MOStLouisCountyGParser extends DispatchGlobalDispatchParser {
       "SMELL OF SMOKE RESIDENTIAL",
       "STILL EXTERIOR GAS LEAK",
       "TRANSFORMER FIRE",
+      "VEHICLE FIRE NON COMMERCIAL STILL",
       "VEHICLE FIRE WITH EXPOSURES",
       "VEHICLE LOCKOUT",
       "WASH DOWN",
       "WIRES DOWN"
-
   );
 }
