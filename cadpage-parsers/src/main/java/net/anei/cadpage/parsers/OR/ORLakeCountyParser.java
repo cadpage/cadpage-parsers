c1f2305bcffe8c774614ff642cea5dc7bd6cbdc9
Kenneth Corbin <ken@cadpage.org>
2021-02-05 19:01:43 -0800

    Added city code conversion to Lake County, OR

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/OR/ORLakeCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/OR/ORLakeCountyParser.java
@@ -1,18 +1,33 @@
 package net.anei.cadpage.parsers.OR;
 
 
+import java.util.Properties;
+import java.util.regex.Pattern;
+
 import net.anei.cadpage.parsers.dispatch.DispatchA22Parser;
 
 
 
 public class ORLakeCountyParser extends DispatchA22Parser {
-  
+
   public ORLakeCountyParser() {
-    super("LAKE COUNTY", "OR");
+    super(CITY_CODES, "LAKE COUNTY", "OR");
   }
-  
+
   @Override
   public String getFilter() {
     return "Dispatch@LCETS.net,Dispatch@psnet.us,donotreply@townoflakeview.org";
   }
+
+  private static final Pattern CV_PTN = Pattern.compile("\\bCV\\b");
+
+  @Override
+  public String adjustMapAddress(String addr) {
+    addr = CV_PTN.matcher(addr).replaceAll("CHRISTMAS VALLEY");
+    return addr;
+  }
+
+  private static final Properties CITY_CODES = buildCodeTable(new String[] {
+      "CV",       "CHRISTMAS VALLEY"
+  });
 }
