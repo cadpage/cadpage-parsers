74af293c7f08a00332091ff8b555090a0d1b2b78
Kenneth Corbin <ken@cadpage.org>
2021-02-08 08:13:13 -0800

    Fixed parsing problem with Fulg County, FL

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/FL/FLGulfCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/FL/FLGulfCountyParser.java
@@ -2,25 +2,35 @@ package net.anei.cadpage.parsers.FL;
 
 import net.anei.cadpage.parsers.FieldProgramParser;
 import net.anei.cadpage.parsers.MsgInfo.Data;
+import net.anei.cadpage.parsers.MsgInfo.MsgType;
 
 public class FLGulfCountyParser extends FieldProgramParser {
-  
+
   public FLGulfCountyParser() {
-    super("GULF COUNTY", "FL", 
-          "CALL! ADDRESS:ADDR! PLACE! RX:DATETIME! CAD#:ID! UNIT:UNIT! END");
+    super("GULF COUNTY", "FL",
+          "( SELECT/RR CAD#:ID! EMPTY CALL ADDR RX:DATETIME! INFO! INFO/N+ UNIT:UNIT! END " +
+          "| CALL! ADDRESS:ADDR! PLACE RX:DATETIME! CAD#:ID! UNIT:UNIT END )");
   }
-  
+
   @Override
   public String getFilter() {
     return "smartcop@gcso.fl.gov";
   }
-  
+
   @Override
   protected boolean parseMsg(String subject, String body, Data data) {
-    if (!subject.equals("CALL FOR SERVICE")) return false;
+    if (subject.equals("RUN REPORT")) {
+      setSelectValue("RR");
+      data.msgType = MsgType.RUN_REPORT;
+    }
+    else if (subject.equals("CALL FOR SERVICE") || subject.equals("SQL Server Message")) {
+      setSelectValue("");
+    }
+    else return false;
+
     return parseFields(body.split("\n"), data);
   }
-  
+
   @Override
   public Field getField(String name) {
     if (name.equals("DATETIME")) return new DateTimeField("\\d\\d/\\d\\d/\\d{4} \\d\\d:\\d\\d:\\d\\d", true);
