fc5b2f2b9f1efa187a361351f4c813bd865f3700
Kenneth Corbin <ken@cadpage.org>
2021-02-16 14:29:56 -0800

    Update sender filter for Summit County, OH (H)

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/OH/OHSummitCountyHParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/OH/OHSummitCountyHParser.java
@@ -6,29 +6,29 @@ import net.anei.cadpage.parsers.FieldProgramParser;
 import net.anei.cadpage.parsers.MsgInfo.Data;
 
 public class OHSummitCountyHParser extends FieldProgramParser {
-  
+
   public OHSummitCountyHParser() {
-    super("SUMMIT COUNTY", "OH", 
+    super("SUMMIT COUNTY", "OH",
           "CALL:CALL! PLACE:PLACE! ADDR:ADDR/S6! CITY:CITY! ID:ID! UNIT:UNIT! INFO:INFO/N+");
     setupGpsLookupTable(GPS_LOOKUP_TABLE);
   }
-  
+
   @Override
   public String getFilter() {
-    return "Dispatch1,info@sundance-sys.com";
+    return "Dispatch1,info@sundance-sys.com,BHDispMap";
   }
-  
+
   @Override
   protected boolean parseMsg(String body, Data data) {
     return parseFields(body.split("\n"), data);
   }
-  
+
   @Override
   public Field getField(String name) {
     if (name.equals("UNIT")) return new MyUnitField();
     return super.getField(name);
   }
-  
+
   private class MyUnitField extends UnitField {
     @Override
     public void parse(String field, Data data) {
@@ -36,7 +36,7 @@ public class OHSummitCountyHParser extends FieldProgramParser {
       super.parse(field, data);
     }
   }
-  
+
   private static final Properties GPS_LOOKUP_TABLE = buildCodeTable(new String[]{
       "7854 MAIN STREET",                     "+40.926976,-81.629593"
   });
