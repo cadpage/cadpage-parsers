a491666053d7436000e354060e924fe242f88218
Kenneth Corbin <ken@cadpage.org>
2021-02-16 09:25:53 -0800

    Fixed parsing problem with Middletown, CT

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/CT/CTMiddletownParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/CT/CTMiddletownParser.java
@@ -13,8 +13,9 @@ import net.anei.cadpage.parsers.MsgInfo.Data;
 public class CTMiddletownParser extends FieldProgramParser {
 
   public CTMiddletownParser() {
-    super("MIDDLETOWN","CT",
-          "ID CALL ADDR SKIP APT CITY ZIP X UNIT DATETIME/d! INFO/N+");
+    super(CITY_LIST, "MIDDLETOWN","CT",
+          "( ID CALL PLACE ADDR APT CITY ZIP X UNIT DATETIME/d! INFO/N+ " +
+          "| UNIT CALL PLACE ADDR APT CITY? ZIP ID INFO/N+ )");
   }
 
   @Override
@@ -31,9 +32,29 @@ public class CTMiddletownParser extends FieldProgramParser {
   @Override
   public Field getField(String name) {
     if (name.equals("ID")) return new IdField("\\d{10}", true);
+    if (name.equals("ADDR")) return new MyAddressField();
     if (name.equals("APT")) return new MyAptField();
+    if (name.equals("UNIT")) return new MyUnitField();
     if (name.equals("DATETIME")) return new DateTimeField("\\d\\d-\\d\\d-\\d{4} +\\d\\d:\\d\\d:\\d\\d", true);
     return super.getField(name);
+  }
+
+  private static final Pattern MSPACE_PTN = Pattern.compile(" {2,}");
+  private static final Pattern LEAD_ZERO_PTN = Pattern.compile("^0+");
+
+  private class MyAddressField extends AddressField {
+    @Override
+    public void parse(String field, Data data) {
+      field = squeeze(field);
+      if (field.equals(squeeze(data.strPlace))) data.strPlace = "";
+      super.parse(field, data);
+    }
+
+    private String squeeze(String field) {
+      field = MSPACE_PTN.matcher(field).replaceAll(" ");
+      field = LEAD_ZERO_PTN.matcher(field).replaceFirst("");
+      return field;
+    }
   }
 
   private static final Pattern APT_PTN = Pattern.compile("(?:Apt|Unit|Room|Lot)[ #]*(.*)");
@@ -45,4 +66,23 @@ public class CTMiddletownParser extends FieldProgramParser {
       super.parse(field, data);
     }
   }
+
+  private class MyUnitField extends UnitField {
+    @Override
+    public void parse(String field, Data data) {
+      field = stripFieldEnd(field, ",");
+      super.parse(field, data);
+    }
+  }
+
+  private static final String[] CITY_LIST = new String[] {
+      "CROMWELL",
+      "DURHAM",
+      "EAST HAMPTON",
+      "HADDAM",
+      "HIGGANUM",
+      "MIDDLEFIELD",
+      "MIDDLETOWN",
+      "PORTLAND"
+  };
 }
