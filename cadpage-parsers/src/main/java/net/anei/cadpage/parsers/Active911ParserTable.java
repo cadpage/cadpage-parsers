4270a18e33b2be688851fc7c24f03393c1bb5cd3
Kenneth Corbin <ken@cadpage.org>
2021-02-18 13:57:37 -0800

    Update A911 parser table

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/Active911ParserTable.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/Active911ParserTable.java
@@ -1032,6 +1032,7 @@ public class Active911ParserTable {
       "US/MO/WrightCounty",               "MOWrightCounty",
       "US/MS/Biloxi",                     "MSBiloxi",
       "US/MS/CityofHernando",             "MSHernando",
+      "US/MS/CityofHornLake",             "MSDesotoCountyA",
       "US/MS/CityofLouisville",           "MSWinstonCounty",
       "US/MS/DesotoCounty",               "MSDeSotoCountyB,MSTateCounty",
       "US/MS/Gulfport",                   "MSHarrisonCountyB",
