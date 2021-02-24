75b9cb9113783b924b5c8f58abf8b475cb282089
Kenneth Corbin <ken@cadpage.org>
2021-02-24 13:34:47 -0800

    Update sender filter for Humboldt County, NV

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/NV/NVHumboldtCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/NV/NVHumboldtCountyParser.java
@@ -6,16 +6,16 @@ import net.anei.cadpage.parsers.dispatch.DispatchA20Parser;
  * Humbolt County, NV
  */
 public class NVHumboldtCountyParser extends DispatchA20Parser {
-  
+
   public NVHumboldtCountyParser() {
     super("HUMBOLDT COUNTY", "NV");
   }
-  
+
   @Override
   public String getFilter() {
-    return "hcsodispatch795@gmail.com,hcsodispatch@hcsonv.com";
+    return "hcsodispatch795@gmail.com,hcsodispatch@hcsonv.com,no-reply-rico@humboldtcountynv.gov";
   }
-  
+
   @Override
   public int getMapFlags() {
     return MAP_FLG_SUPPR_LA;
