fbc0abe61af5637862a24cc642e1c79a7e77b349
Kenneth Corbin <ken@cadpage.org>
2021-02-26 19:27:11 -0800

    Update msg doc

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/CO/COMontezumaCountyAParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/CO/COMontezumaCountyAParser.java
@@ -3,11 +3,11 @@ package net.anei.cadpage.parsers.CO;
 import net.anei.cadpage.parsers.dispatch.DispatchA55Parser;
 
 public class COMontezumaCountyAParser extends DispatchA55Parser {
-  
+
   public  COMontezumaCountyAParser() {
     super("MONTEZUMA COUNTY", "CO");
   }
-  
+
   @Override
   public String getFilter() {
     return "cadalerts@messaging.eforcesoftware.net";
