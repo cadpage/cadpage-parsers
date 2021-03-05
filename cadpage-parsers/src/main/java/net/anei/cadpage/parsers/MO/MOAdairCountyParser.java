9d770818920adb591528e497f447364c7872a73c
Kenneth Corbin <ken@cadpage.org>
2021-03-05 12:22:44 -0800

    Update sender filter for Adair County,  MO

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/MO/MOAdairCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/MO/MOAdairCountyParser.java
@@ -3,13 +3,13 @@ package net.anei.cadpage.parsers.MO;
 import net.anei.cadpage.parsers.dispatch.DispatchBCParser;
 
 public class MOAdairCountyParser extends DispatchBCParser {
-  
+
   public MOAdairCountyParser() {
     super("ADAIR COUNTY", "MO");
   }
-  
+
   @Override
   public String getFilter() {
-    return "ADAIR911@PUBLICSAFETYSOFTWARE.NET";
+    return "ADAIR911@PUBLICSAFETYSOFTWARE.NET,adair911@itiusa.com,adair911@omnigo.com";
   }
 }
