6862ba49ebe646b805f55f05ce21df1f21bc7a9f
Kenneth Corbin <ken@cadpage.org>
2021-03-05 13:36:08 -0800

    Added level field for Hamilton County, IN (C)

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/IN/INHamiltonCountyCParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/IN/INHamiltonCountyCParser.java
@@ -6,29 +6,29 @@ import net.anei.cadpage.parsers.FieldProgramParser;
 import net.anei.cadpage.parsers.MsgInfo.Data;
 
 public class INHamiltonCountyCParser extends FieldProgramParser {
-  
+
   public INHamiltonCountyCParser() {
     super("HAMILTON COUNTY", "IN",
-          "NOC:CALL! Status:SKIP! Call_Type:CALL/SDS! Address:ADDRCITY! ( City:CITY! Common_Name:PLACE! Closest_Intersections:X! Quad:MAP! Units:UNIT! | Common_Name:PLACE! City:CITY! Units:UNIT! Quad:MAP! ) Narrative:INFO! CC_Text:CALL/SDS Caller_Statement:INFO/N Dispatch_Code:CODE INFO/N+ Coord:GPS Inc_#:ID END");
+          "NOC:CALL! Status:SKIP! Call_Type:CALL/SDS! Alarm_Level:PRI? Address:ADDRCITY! ( City:CITY! Common_Name:PLACE! Closest_Intersections:X! Quad:MAP! Units:UNIT! | Common_Name:PLACE! City:CITY! Units:UNIT! Quad:MAP! ) Narrative:INFO! CC_Text:CALL/SDS Caller_Statement:INFO/N Dispatch_Code:CODE INFO/N+ Coord:GPS Inc_#:ID END");
   }
-  
+
   @Override
   public String getFilter() {
     return "dispatch@hamiltoncounty.in.gov";
   }
-  
+
   @Override
   public int getMapFlags() {
     return MAP_FLG_PREFER_GPS;
   }
-  
+
   private static final Pattern DELIM = Pattern.compile("\n+|(?=Common Name:|CC Text:)");
-  
+
   @Override
   protected boolean parseMsg(String body, Data data) {
     return parseFields(DELIM.split(body), data);
   }
-  
+
   @Override
   public Field getField(String name) {
     if (name.equals("ADDRCITY")) return new MyAddressCityField();
@@ -39,7 +39,7 @@ public class INHamiltonCountyCParser extends FieldProgramParser {
     if (name.equals("ID")) return new MyIdField();
     return super.getField(name);
   }
-  
+
   private class MyAddressCityField extends AddressCityField {
     @Override
     public void parse(String field, Data data) {
@@ -47,7 +47,7 @@ public class INHamiltonCountyCParser extends FieldProgramParser {
       super.parse(field, data);
     }
   }
-  
+
   private class MyCityField extends CityField {
     @Override
     public void parse(String field, Data data) {
@@ -55,7 +55,7 @@ public class INHamiltonCountyCParser extends FieldProgramParser {
       super.parse(field, data);
     }
   }
-  
+
   private class MyCrossField extends CrossField {
     @Override
     public void parse(String field, Data data) {
@@ -63,7 +63,7 @@ public class INHamiltonCountyCParser extends FieldProgramParser {
       super.parse(field, data);
     }
   }
-  
+
   private class MyCodeField extends CodeField {
     @Override
     public void parse(String field, Data data) {
@@ -72,7 +72,7 @@ public class INHamiltonCountyCParser extends FieldProgramParser {
       super.parse(field, data);
     }
   }
-  
+
   private class MyGPSField extends GPSField {
     @Override
     public void parse(String field, Data data) {
@@ -80,7 +80,7 @@ public class INHamiltonCountyCParser extends FieldProgramParser {
       super.parse(field, data);
     }
   }
-  
+
   private class MyIdField extends IdField {
     @Override
     public void parse(String field, Data data) {
