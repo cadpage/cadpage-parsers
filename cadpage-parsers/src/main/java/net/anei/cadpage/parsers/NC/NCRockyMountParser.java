5adc482fc35201098f29ec59c2334dafc82f23c5
Kenneth Corbin <ken@cadpage.org>
2021-03-12 13:26:13 -0800

    Fixed parsing problem with Rocky Mount, NC

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/NC/NCRockyMountParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/NC/NCRockyMountParser.java
@@ -2,17 +2,18 @@ package net.anei.cadpage.parsers.NC;
 
 import java.util.Properties;
 
-import net.anei.cadpage.parsers.FieldProgramParser;
 import net.anei.cadpage.parsers.MsgInfo.Data;
+import net.anei.cadpage.parsers.dispatch.DispatchOSSIParser;
 
 /**
  * Rocky Mount, NC
  */
-public class NCRockyMountParser extends FieldProgramParser {
-  
+public class NCRockyMountParser extends DispatchOSSIParser {
+
   public NCRockyMountParser() {
-    super("ROCKY MOUNT", "NC",
-          "ADDR CALL CALL/CS+? X DATETIME! END");
+    super(CITY_CODES, "ROCKY MOUNT", "NC",
+          "( CANCEL ADDR CITY! " +
+          "| FYI ID? ADDR CALL! X+? ) INFO/N+");
     setupGpsLookupTable(GPS_LOOKUP_TABLE);
   }
 
@@ -20,41 +21,23 @@ public class NCRockyMountParser extends FieldProgramParser {
   public String getFilter() {
     return "@rockymountnc.gov";
   }
-  
+
   @Override
   protected boolean parseMsg(String subject, String body, Data data) {
-    if (!subject.equals("Rocky Mount 911 Incident")) return false;
-    return parseFields(body.split(","), data);
+    if (!body.startsWith("CAD:")) body = "CAD:" + body;
+    return super.parseMsg(body, data);
   }
-  
+
   @Override
   public Field getField(String name) {
-    if (name.equals("X")) return new MyCrossField();
-    if (name.equals("DATETIME")) return new DateTimeField("\\d\\d?/\\d\\d?/\\d{4} \\d\\d?:\\d\\d:\\d\\d", true);
+    if (name.equals("ID")) return new IdField("\\d{10}", true);
     return super.getField(name);
   }
-  
-  private class MyCrossField extends CrossField {
-    @Override
-    public boolean canFail() {
-      return true;
-    }
-    
-    @Override
-    public boolean checkParse(String field, Data data) {
-      if (!field.contains("/")) return false;
-      field = stripFieldStart(field, "/");
-      field = stripFieldEnd(field, "/");
-      super.parse(field, data);
-      return true;
-    }
-    
-    @Override
-    public void parse(String field, Data data) {
-      if (!checkParse(field, data)) abort();
-    }
-  }
-  
+
+  private static final Properties CITY_CODES = buildCodeTable(new String[] {
+      "RM",  "ROCKY MOUNT"
+  });
+
   private static final Properties GPS_LOOKUP_TABLE = buildCodeTable(new String[]{
       "396 PARK AVE",                         "+35.945500,-77.786200"
 
