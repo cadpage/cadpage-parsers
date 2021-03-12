34a6fcfa8c674a56d49385dcd061dd3307c3191b
Kenneth Corbin <ken@cadpage.org>
2021-03-12 13:50:49 -0800

    Fixed parsing problem with Carroll County, KY (B)

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/KY/KYCarrollCountyBParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/KY/KYCarrollCountyBParser.java
@@ -1,59 +1,22 @@
 package net.anei.cadpage.parsers.KY;
 
-import net.anei.cadpage.parsers.CodeSet;
-import net.anei.cadpage.parsers.MsgInfo.Data;
-import net.anei.cadpage.parsers.dispatch.DispatchB2Parser;
+import net.anei.cadpage.parsers.dispatch.DispatchA27Parser;
 
 
 
-public class KYCarrollCountyBParser extends DispatchB2Parser {
-  
+public class KYCarrollCountyBParser extends DispatchA27Parser {
+
   public KYCarrollCountyBParser() {
-    super("CARROLLCOUNTY911:", CITY_LIST, "CARROLL COUNTY", "KY");
-    setupCallList(CALL_LIST);
-    setupMultiWordStreets(MWORD_STREET_LIST);
-  }
-  
-  @Override
-  protected boolean parseAddrField(String field, Data data) {
-    field = field.replace('@', '&');
-    return super.parseAddrField(field, data);
+    super("CARROLL COUNTY", "KY");
   }
 
-  private static final CodeSet CALL_LIST = new CodeSet(
-      "911 HANGUP CALLS",
-      "BURGLAR ALARM",
-      "CALL AT SCHOOL",
-      "CRIMINAL INVESTIGATION",
-      "DOG COMPLAINT",
-      "FIGHT IN PROGRESS",
-      "INFORMATIONAL CALL",
-      "MOTORIST ASSIST",
-      "PROPERTY DAMAGE ACCIDENT",
-      "RECKLESS DRIVING",
-      "REQUEST AMBULANCE",
-      "ROAD HAZARD",
-      "TEST CALL",
-      "THEFT REPORT",
-      "TROUBLE CALL",
-      "VISITOR PRESENT"
-  ); 
-  
-  private static final String[] MWORD_STREET_LIST = new String[]{
-    "COUNTY PARK",
-    "HIGH SCHOOL",
-    "REDWOOD HOMES",
-    "SCHOOL 1706 HIGHLAND",
-    "WHITES RUN"
-   
-  };
-  
-  private static final String[] CITY_LIST = new String[]{
-      "CARROLLTON",
-      "ENGLISH",
-      "GHENT",
-      "PRESTONVILLE",
-      "SANDERS",
-      "WORTHVILLE"
-  };
+  @Override
+  public String getFilter() {
+    return "noreply@cisusa.org";
+  }
+
+  @Override
+  public int getMapFlags() {
+    return MAP_FLG_PREFER_GPS;
+  }
 }
