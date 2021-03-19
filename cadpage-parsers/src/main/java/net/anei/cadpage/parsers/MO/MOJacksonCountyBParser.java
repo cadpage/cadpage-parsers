11af7fecdff5baea31c949e1f44ded761c4a4670
Kenneth Corbin <ken@cadpage.org>
2021-03-18 17:20:07 -0700

    Update sender filter for Jackson County, MO (B)

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/MO/MOJacksonCountyBParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/MO/MOJacksonCountyBParser.java
@@ -10,22 +10,22 @@ import net.anei.cadpage.parsers.MsgInfo.MsgType;
 public class MOJacksonCountyBParser extends FieldProgramParser {
 
   public MOJacksonCountyBParser() {
-    super("JACKSON COUNTY", "MO", 
+    super("JACKSON COUNTY", "MO",
           "( SELECT/RR ID ID/C ADDR CALLCODE TIMES END " +
           "| ID3 ADDR CITY CALL CH MAP! Assigned:UNIT UNIT/C+ Apt/Lot:APT! Location:PLACE END " +
           "| ADDR! CALLCODE! CH! CASE! ( ID2/C! Assigned:UNIT! | UNIT! ) UNIT/C+ )");
   }
-  
+
   @Override
   public String getFilter() {
     return "totalaccess@mobilfonetotalaccess.com,KCFD.Notify@kcmo.org";
   }
-  
+
   private static Pattern BODY_DATETIME = Pattern.compile("(.*?)(\\d{2}/\\d{2}/\\d{2})? (\\d{2}:\\d{2}:\\d{2})"); //no space between BODY and DATE
   private static final Pattern RR_DELIM = Pattern.compile(" {3,}");
   private static final Pattern DELIM = Pattern.compile(" *, *| +/// *| *(?=Apt/Lot:|Location:)");
   public boolean parseMsg(String body, Data data) {
-    
+
     //parse trailing DATE? TIME
     Matcher mat = BODY_DATETIME.matcher(body);
     if (mat.matches()) {
@@ -33,28 +33,28 @@ public class MOJacksonCountyBParser extends FieldProgramParser {
       data.strDate = getOptGroup(mat.group(2));
       data.strTime = mat.group(3);
     }
-    
+
     if (body.contains("First Arrive,")) {
       data.msgType = MsgType.RUN_REPORT;
       setSelectValue("RR");
       if (!parseFields(RR_DELIM.split(body), data)) return false;
     }
-    
+
     //try parsing normally
     else {
       setSelectValue("");
       if (!parseFields(DELIM.split(body), data)) return false;
     }
-    
+
     if (data.strCity.equals("North Kansas Ci")) data.strCity += "ty";
     return true;
   }
-  
+
   @Override
   public String getProgram() {
     return super.getProgram() + " DATE TIME";
   }
-  
+
   @Override
   public Field getField(String name) {
     if (name.equals("ID3")) return new IdField("\\d\\d-\\d{6}", true);
@@ -66,7 +66,7 @@ public class MOJacksonCountyBParser extends FieldProgramParser {
     if (name.equals("TIMES")) return new MyTimesField();
     return super.getField(name);
   }
-  
+
   private static final Pattern ADDR_CITY_PTN = Pattern.compile("(.*?) {3,}(.*)");
   private class MyAddressField extends AddressField {
     @Override
@@ -78,13 +78,13 @@ public class MOJacksonCountyBParser extends FieldProgramParser {
       }
       super.parse(field, data);
     }
-    
+
     @Override
     public String getFieldNames() {
       return super.getFieldNames() + " CITY";
     }
   }
-  
+
   //CALL( / CODE)?
   private static Pattern CALL_CODE = Pattern.compile("(.*?)(?:(?:-| / )(.*))?");
   public class MyCallCodeField extends Field {
@@ -96,13 +96,13 @@ public class MOJacksonCountyBParser extends FieldProgramParser {
           data.strCode = getOptGroup(mat.group(2));
         } else data.strCall = field;
     }
-  
+
     @Override
     public String getFieldNames() {
       return "CALL CODE";
-    } 
+    }
   }
-  
+
   private static final Pattern TIME_BRK_PTN = Pattern.compile("(?<=\\d\\d:\\d\\d:\\d\\d)(?=[A-Z])");
   private class MyTimesField extends InfoField {
     @Override
