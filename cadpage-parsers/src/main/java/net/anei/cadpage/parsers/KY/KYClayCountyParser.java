4ca672ea693390522bac76e8aef53c31d09520cb
Kenneth Corbin <ken@cadpage.org>
2021-03-19 18:23:49 -0700

    Fixed parsing problem with Clay County, KY

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/KY/KYClayCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/KY/KYClayCountyParser.java
@@ -1,15 +1,35 @@
 package net.anei.cadpage.parsers.KY;
 
-import net.anei.cadpage.parsers.dispatch.DispatchSPKParser;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-public class KYClayCountyParser extends DispatchSPKParser {
-  
+import net.anei.cadpage.parsers.MsgInfo.Data;
+import net.anei.cadpage.parsers.dispatch.DispatchA74Parser;
+
+public class KYClayCountyParser extends DispatchA74Parser {
+
   public KYClayCountyParser() {
     super("CLAY COUNTY", "KY");
   }
-  
+
   @Override
   public String getFilter() {
-    return "clay.co.ky911@gmail.com";
+    return "Dispatch@clayky911.com";
   }
+
+  private static final Pattern APT_ADDR_PTN = Pattern.compile("(\\d+[A-Z]?) - (\\d+.*)");
+
+  @Override
+  public boolean parseMsg(String subject, String body, Data data) {
+    if (!super.parseMsg(subject, body, data)) return false;
+    if (data.strApt.isEmpty()) {
+      Matcher match = APT_ADDR_PTN.matcher(data.strAddress);
+      if (match.matches()) {
+        data.strApt = match.group(1);
+        data.strAddress = match.group(2);
+      }
+    }
+    return true;
+  }
+
 }
