509eadf0538c1823125ff6823679c5bd22fa1cd2
Kenneth Corbin <ken@cadpage.org>
2021-03-20 13:03:39 -0700

    Added GPS lookup table for Wayne County, OH (E)

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/OH/OHWayneCountyEParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/OH/OHWayneCountyEParser.java
@@ -11,23 +11,24 @@ import net.anei.cadpage.parsers.FieldProgramParser;
 public class OHWayneCountyEParser extends FieldProgramParser {
 
   public OHWayneCountyEParser() {
-    super("WAYNE COUNTY", "OH", 
+    super("WAYNE COUNTY", "OH",
           "ADDR CALL INFO! X:X! ID! DATETIME! END");
+    setupGpsLookupTable(GPS_LOOKUP_TABLE);
   }
-  
+
   @Override
   public String getFilter() {
     return "noreply@zuercherportal.com";
   }
-  
+
   private static final Pattern DELIM = Pattern.compile(" /(?= )");
-  
+
   @Override
   protected boolean parseMsg(String body, Data data) {
     if (body.startsWith("/")) body = " / " +  body;
     return parseFields(DELIM.split(body), data);
   }
-  
+
   public Field getField(String name) {
     if (name.equals("ADDR")) return new MyAddressField();
     if (name.equals("INFO")) return new MyInfoField();
@@ -36,14 +37,14 @@ public class OHWayneCountyEParser extends FieldProgramParser {
     if (name.equals("DATETIME")) return new DateTimeField("\\d\\d/\\d\\d/\\d\\d \\d\\d:\\d\\d", true);
     return super.getField(name);
   }
-  
+
   private static final Pattern ST_ZIP_PTN = Pattern.compile("([A-Z]{2})(?: +(\\d{5}))?");
   private class MyAddressField extends AddressCityField {
     @Override
     public void parse(String field, Data data) {
       Parser p = new Parser(field);
       String city = p.getLastOptional(',');
-      
+
       String zip = null;
       Matcher match = ST_ZIP_PTN.matcher(city);
       if (match.matches()) {
@@ -53,16 +54,16 @@ public class OHWayneCountyEParser extends FieldProgramParser {
       }
       if (city.length() == 0 && zip !=  null) city = convertCodes(zip, ZIP_CODE_TABLE);
       data.strCity = city;
-      
+
       parseAddress(p.get(), data);
     }
-    
+
     @Override
     public String getFieldNames() {
       return "ADDR APT CITY ST";
     }
   }
-  
+
   private static final Pattern INFO_DATE_TIME_PTN = Pattern.compile(";? *\\b(?:(\\d\\d/\\d\\d/\\d\\d) (\\d\\d:\\d\\d:\\d\\d) - +)");
   private class MyInfoField extends InfoField {
     @Override
@@ -73,7 +74,7 @@ public class OHWayneCountyEParser extends FieldProgramParser {
       }
     }
   }
-  
+
   private class MyCrossField extends CrossField {
     @Override
     public void parse(String field, Data data) {
@@ -81,7 +82,7 @@ public class OHWayneCountyEParser extends FieldProgramParser {
       super.parse(field, data);
     }
   }
-  
+
   private static final Properties ZIP_CODE_TABLE = buildCodeTable(new String[]{
       "44230", "DOYLESTOWN",
       "44270", "RITTMAN",
@@ -103,5 +104,10 @@ public class OHWayneCountyEParser extends FieldProgramParser {
       "44676", "SHREVE",
       "44677", "SMITHVILLE",
       "44689", "WILMOT"
-  }); 
+  });
+
+  private static final Properties GPS_LOOKUP_TABLE = buildCodeTable(new String[] {
+      "580 E SUMMIT ST",                      "+40.977530,-81.694290",
+      "48 EDWARDS DR",                        "+40.977530,-81.694290"
+  });
 }
