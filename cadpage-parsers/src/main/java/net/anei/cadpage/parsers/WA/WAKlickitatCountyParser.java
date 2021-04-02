24be61700939800b74296fb72dce41c302169044
Kenneth Corbin <ken@cadpage.org>
2021-04-01 20:21:07 -0700

    Added city code table to Klickitat County, WA

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/WA/WAKlickitatCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/WA/WAKlickitatCountyParser.java
@@ -1,17 +1,41 @@
 package net.anei.cadpage.parsers.WA;
 
+import java.util.Properties;
+
 import net.anei.cadpage.parsers.dispatch.DispatchA19Parser;
 
 
 
 public class WAKlickitatCountyParser extends DispatchA19Parser {
-  
+
   public WAKlickitatCountyParser() {
-    super("KLICKITAT COUNTY", "WA");
+    super(CITY_CODES, "KLICKITAT COUNTY", "WA");
    }
-  
+
   @Override
   public String getFilter() {
     return "@alert.active911.com,noreply@klickitatcounty.org";
   }
+
+  private static final Properties CITY_CODES =  buildCodeTable(new String[] {
+      "APP", "APPLETON",
+      "BIN", "BINGEN",
+      "BZ",  "BZ CORNERS",
+      "CEN", "CENTERVILLE",
+      "DAL", "DALLESPORT",
+      "GLD", "GOLDENDALE",
+      "GLN", "GLENWOOD",
+      "HPR", "HIGH PRARIE",
+      "KLI", "KLICKITAT",
+      "LYL", "LYLE",
+      "MAR", "MARRYHILL",
+      "RSV", "ROOSEVELT",
+      "SNO", "SNOWDEN",
+      "TIM", "TIMBER VALLEY",
+      "TL",  "TROUT LAKE",
+      "WHK", "WAHKIACUS",
+      "WS",  "WHITE SALMON",
+      "WSH", "WISHRAM"
+
+  });
 }
