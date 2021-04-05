7897f61ef5e9bd45c51f9ae987ef7c14a19375ce
Kenneth Corbin <ken@cadpage.org>
2021-04-05 12:19:59 -0700

    Fixed parsing problem with Hoke County, CO

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/NC/NCHokeCountyBParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/NC/NCHokeCountyBParser.java
@@ -1,33 +1,39 @@
 package net.anei.cadpage.parsers.NC;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import net.anei.cadpage.parsers.FieldProgramParser;
 import net.anei.cadpage.parsers.MsgInfo.Data;
 
 public class NCHokeCountyBParser extends FieldProgramParser {
-  
+
   public NCHokeCountyBParser() {
-    super("HOKE COUNTY", "NC", 
+    super("HOKE COUNTY", "NC",
           "CALL ADDRCITYST X PLACE INFO! END");
   }
-  
+
   @Override
   public String getFilter() {
     return "no-reply@zuercherportal.com,74121";
   }
-  
+
+  private static final Pattern MARKER = Pattern.compile("HOKE CO(?:UNTY)? 911:");
+
   @Override
   protected boolean parseMsg(String subject, String body, Data data) {
     do {
-      if (body.startsWith("HOKE COUNTY 911:")) {
-        body = body.substring(16).trim();
+      Matcher match = MARKER.matcher(body);
+      if (match.lookingAt()) {
+        body = body.substring(match.end()).trim();
         break;
       }
-      
-      if (subject.equals("HOKE COUNTY 911")) break;
-      
+
+      if (subject.equals("HOKE COUNTY 911") || subject.equals("HOKE CO 911")) break;
+
       return false;
     } while (false);
-    
+
     return parseFields(body.split("\\*\\*", -1), data);
   }
 }
