5ec64474208deab71d1eeb010414e9e64b0ed623
Kenneth Corbin <ken@cadpage.org>
2021-04-09 18:13:09 -0700

    Fixed parsing problem with Blaine County, ID

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/ID/IDBlaineCountyBParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/ID/IDBlaineCountyBParser.java
@@ -10,7 +10,8 @@ public class IDBlaineCountyBParser extends FieldProgramParser {
 
   public IDBlaineCountyBParser() {
     super("BLAINE COUNTY", "ID",
-          "CALL ADDRCITYST! X INFO/N+");
+          "( SELECT/1 CALL ADDRCITYST! X INFO/N+ " +
+          "| CODE CALL ADDRCITYST! X PLACE? INFO/N+? GPS1 GPS2 )");
   }
 
   @Override
@@ -18,19 +19,62 @@ public class IDBlaineCountyBParser extends FieldProgramParser {
     return "zdispatch@co.blaine.id.us";
   }
 
-  private static final Pattern MASTER = Pattern.compile(".*?[\n ]*\nBCEC: ([A-Z]{2}\\d{6}-\\d{3}) -- ");
+  private static final Pattern MASTER1 = Pattern.compile(".*?[\n ]*\nBCEC: ([A-Z]{2}\\d{6}-\\d{3}) -- ");
+  private static final Pattern MASTER2 = Pattern.compile(".*?\n(?:([A-Z]{2}\\d{6}-\\d{3})| )\n");
   @Override
   protected boolean parseMsg(String body , Data data) {
 
-    Matcher match = MASTER.matcher(body);
-    if (!match.lookingAt()) return false;
-    data.strCallId =  match.group(1);
-    body = body.substring(match.end()).trim();
-    return parseFields(body.split("\n"), data);
+    Matcher match = MASTER1.matcher(body);
+    if (match.lookingAt()) {
+      setSelectValue("1");
+      data.strCallId =  match.group(1);
+      body = body.substring(match.end()).trim();
+      return parseFields(body.split("\n"), data);
+    }
+
+    match = MASTER2.matcher(body);
+    if (match.lookingAt()) {
+      setSelectValue("2");
+      data.strCallId = getOptGroup(match.group(1));
+      body = body.substring(match.end()).trim();
+      return parseFields(body.split("\n"), data);
+    }
+    return false;
   }
 
   @Override
   public String getProgram() {
     return "ID " + super.getProgram();
+  }
+
+  @Override
+  public Field getField(String name) {
+    if (name.equals("X")) return new MyCrossField();
+    if (name.equals("PLACE")) return new PlaceField("[- A-Z]*", true);
+    if (name.equals("GPS1")) return new MyGPSField(1);
+    if (name.equals("GPS2")) return new MyGPSField(2);
+    return super.getField(name);
+  }
+
+  private class MyCrossField extends CrossField {
+    @Override
+    public void parse(String field, Data data) {
+      if (field.equals("None")) return;
+      super.parse(field, data);
+    }
+  }
+
+  private static final Pattern GPS_PTN = Pattern.compile("[-+]?\\d{2,3}\\.\\d{6,}|None");
+  private class MyGPSField extends GPSField {
+    public MyGPSField(int type) {
+      super(type);
+      setPattern(GPS_PTN, true);
+    }
+
+    @Override
+    public boolean checkParse(String field, Data data) {
+      if (!isLastField(2)) return false;
+      return super.checkParse(field, data);
+    }
   }
 }
