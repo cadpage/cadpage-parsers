f054033b3cdf67b9c74bba1dc504b8d4eaba5164
Kenneth Corbin <ken@cadpage.org>
2021-04-17 12:30:19 -0700

    Added city code table to Butts COunty, GA

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/GA/GAButtsCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/GA/GAButtsCountyParser.java
@@ -1,17 +1,25 @@
 package net.anei.cadpage.parsers.GA;
 
+import java.util.Properties;
+
 import net.anei.cadpage.parsers.dispatch.DispatchA19Parser;
 /**
  * Butts County, GA
  */
 public class GAButtsCountyParser extends DispatchA19Parser {
-  
+
   public GAButtsCountyParser() {
-    super("BUTTS COUNTY", "GA");
+    super(CITY_CODES, "BUTTS COUNTY", "GA");
   }
-  
+
   @Override
   public String getFilter() {
     return "active911@buttscounty.org";
   }
+
+  private static final Properties CITY_CODES = buildCodeTable(new String[] {
+      "FLO", "Flovilla",
+      "JAK", "Jackson",
+      "JEN", "Jenkinsburg"
+  });
 }
