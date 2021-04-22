f1b5b0a1a7d440cab086fb5277c18c4c570bdf10
Kenneth Corbin <ken@cadpage.org>
2021-04-21 17:26:42 -0700

    Fixed parsing problem with Pocahontas County, WV

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/WV/WVPocahontasCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/WV/WVPocahontasCountyParser.java
@@ -1,16 +1,58 @@
 package net.anei.cadpage.parsers.WV;
 
+import java.util.Properties;
+
 import net.anei.cadpage.parsers.dispatch.DispatchA19Parser;
 
 
 public class WVPocahontasCountyParser extends DispatchA19Parser {
-  
+
   public WVPocahontasCountyParser() {
-    super("POCAHONTAS COUNTY", "WV");
+    super(CITY_CODES, "POCAHONTAS COUNTY", "WV");
   }
 
   @Override
   public String getFilter() {
     return "@pocahontasemergency.com,@alert.active911.com";
   }
+
+  private static Properties CITY_CODES = buildCodeTable(new String[] {
+      "ARB", "ARBOVALE",
+      "BAR", "BARTOW",
+      "BH",  "BEARD HEIGHTS",
+      "BOY", "BOYER",
+      "BRO", "BROWNSBURG",
+      "BS",  "BIG SPRINGS",
+      "BUC", "BUCKEYE",
+      "CAM", "CAMPBELLTOWN",
+      "CAS", "CASS",
+      "CL",  "CLOVER LICK",
+      "COU", "",
+      "DEN", "DENMAR",
+      "DM",  "DROOP MOUNTAIN",
+      "DUN", "DUNMORE",
+      "DUR", "DURBIN",
+      "EDR", "EDRAY",
+      "FRA", "FRANK",
+      "FRO", "FROST",
+      "GB",  "GREEN BANK",
+      "HIL", "HILLSBORO",
+      "HUN", "HUNTERSVILLE",
+      "JAC", "JACOX",
+      "LIN", "LINWOOD",
+      "LOB", "LOBELIA",
+      "MAC", "MACE",
+      "MAR", "MARLINTON",
+      "MP",  "MILL POINT",
+      "MS",  "MINNEHAHASPRING",
+      "RIM", "RIMEL",
+      "SB",  "STONY BOTTOM",
+      "SEE", "SEEBERT",
+      "SLA", "SLATYFORK",
+      "SNO", "SNOWSHOE",
+      "THO", "THORNWOOD",
+      "WOO", "WOODROW",
+      "WR",  "WILLIAMS RIVER"
+
+  });
 }
