449cffd427e1d194dbc776c55efe382ce6b21c2c
Kenneth Corbin <ken@cadpage.org>
2021-04-29 16:23:32 -0700

    Fixed parsing problem with Pasquotank County, NC

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/NC/NCPasquotankCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/NC/NCPasquotankCountyParser.java
@@ -10,7 +10,7 @@ import net.anei.cadpage.parsers.dispatch.DispatchOSSIParser;
  * fAlamance county, NC
  */
 public class NCPasquotankCountyParser extends DispatchOSSIParser {
-  
+
   public NCPasquotankCountyParser() {
     super(CITY_CODES, "PASQUOTANK COUNTY", "NC",
           "( CANCEL ADDR! CITY? INFO/N+ " +
@@ -22,9 +22,15 @@ public class NCPasquotankCountyParser extends DispatchOSSIParser {
   public String getFilter() {
     return "CAD@co.pasquotank.nc.us";
   }
-  
+
   @Override
   protected boolean parseMsg(String subject, String body, Data data) {
+
+    if (body.startsWith("/ Text Message / ")) {
+      int pt = body.indexOf(':');
+      body = "CAD:" + body.substring(pt+1).trim();
+    }
+
     if (subject.contains(";")) {
       int pt = body.indexOf(":CAD:");
       if (pt < 0) return false;
@@ -33,29 +39,29 @@ public class NCPasquotankCountyParser extends DispatchOSSIParser {
     }
     return super.parseMsg(body, data);
   }
-  
+
   @Override
   public Field getField(String name) {
     if (name.equals("ADDR")) return new MyAddressField();
     return super.getField(name);
   }
-  
+
   private class MyAddressField extends AddressField {
     @Override
     public boolean canFail() {
       return true;
     }
-    
+
     @Override
     public boolean checkParse(String field, Data data) {
-      
+
       // Treat first field as address unless next field is a better address
       if (checkAddress(getRelativeField(+1)) > checkAddress(field)) return false;
       parse(field, data);
       return true;
     }
   }
-  
+
   private static final Properties CITY_CODES = buildCodeTable(new String[]{
       "ELIZ", "ELIZABETH CITY"
   });
