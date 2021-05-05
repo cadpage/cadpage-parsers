3e5b4f8b35cf14a943904f4363863225b7e4e25e
Kenneth Corbin <ken@cadpage.org>
2021-05-05 14:39:43 -0700

    Added GPS lookup table for Cerro Gordo County, IA

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/IA/IACerroGordoCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/IA/IACerroGordoCountyParser.java
@@ -1,5 +1,6 @@
 package net.anei.cadpage.parsers.IA;
 
+import java.util.Properties;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -7,17 +8,18 @@ import net.anei.cadpage.parsers.FieldProgramParser;
 import net.anei.cadpage.parsers.MsgInfo.Data;
 
 public class IACerroGordoCountyParser extends FieldProgramParser {
-  
+
   public IACerroGordoCountyParser() {
     super("CERRO GORDO COUNTY", "IA",
           "Message:INFO! Time:DATE_TIME_PLACE! Address:ADDRCITY! Nearest_intersection:X!");
+    setupGpsLookupTable(GPS_LOOKUP_TABLE);
   }
-  
+
   @Override
   public String getFilter() {
     return "no-reply@zuercherportal.com,zuercher@cgcounty.org";
   }
-  
+
   @Override
   protected boolean parseMsg(String subject, String body, Data data) {
     if (!subject.startsWith("Call Type:")) return false;
@@ -27,12 +29,12 @@ public class IACerroGordoCountyParser extends FieldProgramParser {
     if (pt >= 0) body = body.substring(pt+2).trim();
     return super.parseMsg(body, data);
   }
-  
+
   @Override
   public String getProgram() {
     return "CALL " + super.getProgram();
   }
-  
+
   @Override
   public Field  getField(String name) {
     if (name.equals("INFO")) return new MyInfoField();
@@ -41,7 +43,7 @@ public class IACerroGordoCountyParser extends FieldProgramParser {
     if (name.equals("X")) return new MyCrossField();
     return super.getField(name);
   }
-  
+
   private class MyInfoField extends InfoField {
     @Override
     public void parse(String field, Data data) {
@@ -49,7 +51,7 @@ public class IACerroGordoCountyParser extends FieldProgramParser {
       super.parse(field, data);
     }
   }
-  
+
   private static final Pattern DATE_TIME_PLACE_PTN = Pattern.compile("(\\d\\d/\\d\\d/\\d\\d) (\\d\\d:\\d\\d)\\b *(.*)");
   private class MyDateTimePlaceField extends Field {
     @Override
@@ -60,13 +62,13 @@ public class IACerroGordoCountyParser extends FieldProgramParser {
       data.strTime = match.group(2);
       data.strPlace = match.group(3);
     }
-    
+
     @Override
     public String getFieldNames() {
       return "DATE TIME PLACE";
     }
   }
-  
+
   private static final Pattern ADDR_ST_ZIP = Pattern.compile("(.*), *([A-Z]{2})(?: (\\d{5}))?");
   private class MyAddressCityField extends AddressCityField {
     @Override
@@ -81,13 +83,13 @@ public class IACerroGordoCountyParser extends FieldProgramParser {
       super.parse(field, data);
       if (data.strCity.length() == 0 && zip != null) data.strCity = zip;
     }
-    
+
     @Override
     public String getFieldNames() {
       return super.getFieldNames() + " ST";
     }
   }
-  
+
   private class MyCrossField extends CrossField {
     @Override
     public void parse(String field, Data data) {
@@ -95,4 +97,30 @@ public class IACerroGordoCountyParser extends FieldProgramParser {
       super.parse(field, data);
     }
   }
+
+  private static final Pattern SLASH_PTN = Pattern.compile(" */ *");
+  private static final Pattern TRAIL_DIR_PTN = Pattern.compile(" *[NSEW]B?$");
+
+  @Override
+  protected String adjustGpsLookupAddress(String address) {
+    if (address.startsWith("MM ")) {
+      address = SLASH_PTN.matcher(address).replaceAll(" ");
+      address = TRAIL_DIR_PTN.matcher(address).replaceFirst("");
+    }
+    return address;
+  }
+
+  private static final Properties GPS_LOOKUP_TABLE = buildCodeTable(new String[] {
+      "MM 180 I35",                           "+42.943993,-93.350784",
+      "MM 182 I35",                           "+42.980130,-93.344672",
+      "MM 183 HWY 18",                        "+43.105356,-93.260737",
+      "MM 186 HWY 18",                        "+43.104299,-93.201360",
+      "MM 188 I35",                           "+43.067302,-93.342899",
+      "MM 190 HWY 18",                        "+43.112076,-93.122528",
+      "MM 190 I35",                           "+43.099276,-93.345547",
+      "MM 193 I35",                           "+43.133711,-93.356062",
+      "MM 194 I35",                           "+43.146258,-93.355832",
+      "MM 195 HWY 18",                        "+43.116339,-93.023539",
+      "MM 197 I35",                           "+43.198764,-93.349298"
+  });
 }
