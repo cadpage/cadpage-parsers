6e340ad1debb167ebca456dd1edcb923164d0b4c
Kenneth Corbin <ken@cadpage.org>
2021-05-12 16:21:39 -0700

    Updated city table for Stokes County, NC

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/NC/NCStokesCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/NC/NCStokesCountyParser.java
@@ -13,11 +13,11 @@ import net.anei.cadpage.parsers.MsgInfo.Data;
 public class NCStokesCountyParser extends FieldProgramParser {
 
   public NCStokesCountyParser() {
-    super(CITY_CODES, "STOKES COUNTY", "NC", 
+    super(CITY_CODES, "STOKES COUNTY", "NC",
           "SRC ( STA ID TIME/RN+ | SRC CALL ADDRCITY UNIT! INFO/N+ )");
     setupGpsLookupTable(GPS_LOOKUP_TABLE);
   }
-  
+
   @Override
   public String getFilter() {
     return "informationsystems@co.stokes.nc.us";
@@ -56,7 +56,7 @@ public class NCStokesCountyParser extends FieldProgramParser {
   private class MyAddressCityField extends AddressCityField {
     @Override
     public void parse(String field, Data data) {
-      
+
       Matcher mat = ADDR_SEMICOLON_PLACE.matcher(field);
       if (mat.matches()) {
         data.strPlace = mat.group(2);
@@ -71,7 +71,7 @@ public class NCStokesCountyParser extends FieldProgramParser {
       return "ADDR PLACE CITY";
     }
   }
-  
+
   private class TimeField extends InfoField {
     @Override
     public void parse(String field, Data data) {
@@ -88,46 +88,70 @@ public class NCStokesCountyParser extends FieldProgramParser {
   private class MyInfoField extends InfoField {
     @Override
     public void parse(String field, Data data) {
-      
+
       if (ID_PTN.matcher(field).matches()) {
         data.strCallId = field;
         return;
       }
-      
+
       Matcher match = DATE_TIME_PTN.matcher(field);
       if (match.matches()) {
         data.strDate = match.group(1);
         data.strTime = match.group(2);
         return;
       }
-      
+
       if (isLastField(+1)) {
         if (ID_TRUNC_PTN.matcher(field).matches()) return;
         if (DATE_TIME_TRUNC_PTN.matcher(field).matches()) return;
       }
       super.parse(field, data);
     }
-    
+
     @Override
     public String getFieldNames() {
       return "INFO ID DATE TIME";
     }
   }
-  
+
   private static final Properties GPS_LOOKUP_TABLE = buildCodeTable(new String[]{
       "100 STOKES DR",                        "+36.265710,-80.384445"
   });
-  
+
   private static final Properties CITY_CODES = buildCodeTable(new String[]{
-      "DAN", "Danbury",
-      "GER", "Germanton",
-      "KIN", "King",
-      "LAW", "Lawsonville",
-      "MAD", "Madison",
-      "PIN", "Pinnacle",
-      "SAN", "Sandy Ridge",
-      "WAL", "Walnut Cove",
-      "WC",  "Walnut Cove",
-      "WES", "Westfield"
+      "BELE", "Belews Creek",
+      "BETH", "Bethania",
+      "CLEM", "Clemmons",
+      "COLF", "Colfax",
+      "DAN",  "Danbury",
+      "DC",   "DavidsonCounty",
+      "EDEN", "Eden",
+      "FC",   "ForsythCounty",
+      "GC",   "GuilfordCounty",
+      "GER",  "Germanton",
+      "GERM", "Germanton",
+      "HP",   "HighPoint",
+      "KER",  "Kernersville",
+      "KIN",  "King",
+      "KING", "King",
+      "LAW",  "Lawsonville",
+      "LEW",  "Lewisville",
+      "MAD",  "Madison",
+      "PFAF", "Pfafftown",
+      "PIN",  "Pinnacle",
+      "RH",   "RuralHall",
+      "SAN",  "Sandy Ridge",
+      "SC",   "StokesCounty",
+      "STA",  "Stanleyville",
+      "STOK", "Stokesdale",
+      "SU",   "Surry",
+      "TOB",  "Tobaccoville",
+      "WAL",  "Walnut Cove",
+      "WALK", "Walkertown",
+      "WALN", "Walnut Cove",
+      "WC",   "Walnut Cove",
+      "WES",  "Westfield",
+      "WS",   "Winston-Salem"
+
   });
 }
