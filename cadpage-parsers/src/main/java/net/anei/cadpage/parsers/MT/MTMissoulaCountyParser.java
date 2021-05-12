3e1b78b274e0390d45b5c297d06e9ad170bcd07c
Kenneth Corbin <ken@cadpage.org>
2021-05-12 16:06:54 -0700

    Fixed parsing probem with Missoula County, MT

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/MT/MTMissoulaCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/MT/MTMissoulaCountyParser.java
@@ -13,28 +13,31 @@ public class MTMissoulaCountyParser extends FieldProgramParser {
     super("MISSOULA COUNTY", "MT",
           "Call_Type:CALL! Address:ADDR/S6 Common_Name:PLACE! Assigned_Units:UNIT! Narrative:INFO");
   }
-  
+
   @Override
   public String getFilter() {
     return "911@missoulaonthealert.com,911Alerts@missoulacounty.us";
   }
-  
+
   private static final Pattern MARKER = Pattern.compile("(?:(\\d{4}), )?Call # (\\d+) +");
-  
+
   @Override
   protected boolean parseMsg(String subject, String body, Data data) {
-    
+
     if (!subject.equals("911 Page")) return false;
-    
+
     Matcher match = MARKER.matcher(body);
     if (!match.lookingAt()) return false;
     data.strSource = getOptGroup(match.group(1));
     data.strCallId = match.group(2);
     body = body.substring(match.end());
-    
+
+    int pt = body.indexOf("\nMessages and attachments");
+    if (pt >= 0) body = body.substring(0,pt).trim();
+
     return super.parseMsg(body, data);
   }
-  
+
   @Override
   public String getProgram() {
     return "SRC ID " + super.getProgram();
@@ -45,7 +48,7 @@ public class MTMissoulaCountyParser extends FieldProgramParser {
     if (name.equals("ADDR")) return new MyAddressField();
     return super.getField(name);
   }
-  
+
   private static final Pattern PRESERVE_NUMBER_PTN = Pattern.compile(".* MM HWY");
   private class MyAddressField extends AddressField {
     @Override
@@ -58,9 +61,9 @@ public class MTMissoulaCountyParser extends FieldProgramParser {
         data.strAddress = data.strAddress + ' ' + data.strApt;
         data.strApt = "";
       }
-      
+
     }
-    
+
     @Override
     public String getFieldNames() {
       return super.getFieldNames() + " CITY";
