fa57b9a891702469454c5e3d4a4601d5b1f47d41
Kenneth Corbin <ken@cadpage.org>
2021-05-13 15:25:58 -0700

    Fixed parsing problem with St Louis County, MO (C)

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/MO/MOStLouisCountyCParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/MO/MOStLouisCountyCParser.java
@@ -10,21 +10,21 @@ import net.anei.cadpage.parsers.MsgInfo.Data;
 public class MOStLouisCountyCParser extends FieldProgramParser {
 
   public MOStLouisCountyCParser() {
-    super("ST LOUIS COUNTY", "MO", 
+    super("ST LOUIS COUNTY", "MO",
         "( SELECT/2 UNIT_RESP On:CALL! AT:ADDR! APT:APT? BUS:PLACE? CHL:CH! FD:SRC " +
-        "| CALL! AT:ADDR! APT:APT? BUS:PLACE? XST:X BUS:PLACE? )");
+        "| CALL! AT:ADDR! APT:APT? BUS:PLACE? XST:X BUS:PLACE )");
   }
-  
+
   @Override
   public String getFilter() {
     return "dispatch@cce911.org,paging@cce911.org,93001";
   }
-  
+
   @Override
   public int getMapFlags() {
     return MAP_FLG_PREFER_GPS;
   }
-  
+
   private static final Pattern BAD_MSG_PTN = Pattern.compile(".* AT .* BUS:.*Units:");
   private static final Pattern RESPONDING_PTN = Pattern.compile("(.*?) (?:Is|Are) Responding (.*?) (?=On:)");
   private static final Pattern CHANNEL_PTN = Pattern.compile("(?:TAC: *)?((?:CC911 )?(?:SO|NO) - (?:S MAIN F|N MAIN F|FTAC \\d{2}))");
@@ -32,16 +32,16 @@ public class MOStLouisCountyCParser extends FieldProgramParser {
   private static final Pattern ID_PTN = Pattern.compile(" *(?:Incident:)?(\\d{2}-\\d+)$");
   private static final Pattern TIME_PTN = Pattern.compile(" +(\\d\\d:\\d\\d)$");
   private static final Pattern LAT_LONG_PTN = Pattern.compile("(?:(38)(\\d{6}) +(\\d{2})(\\d{6})| 0 0)$");
-  private static final Pattern SRC_UNIT_PTN = Pattern.compile(" *(?:FD: *)?(\\d\\d\\b|(?:Affton|Brentwood|City of St Loui|Crestwood FD|Eureka|Fenton|Fenton FPD|Franklin|Jefferson|Kirkwood|Ladue|Lemay|Mehlville|Olivette|Shrewsbury|St Charles|St Louis City|St Louis|Webster Groves)(?: FPD)?)(?: *(?:\\[ ?|Units: )? *((?:(?:(?:STL )?[A-Za-z0-9]+|AB \\d+|GTWY \\d+|\\d+ DUTY|METRO AIR|NEED (?:A|AMB|EMS|MED)(?: \\d+)?|Rescue Dut)\\b,?)+))?$");
+  private static final Pattern SRC_UNIT_PTN = Pattern.compile(" *(?:FD: *)?(\\d\\d\\b|(?:Affton|Brentwood|City of St Loui|Crestwood FD|Eureka|Fenton|Fenton FPD|Franklin|Jefferson|Kirkwood|Ladue|Lemay|Mehlville|Olivette|Shrewsbury|St Charles|St Louis City|St Louis|Webster Groves)(?: FPD)?)(?: *(?:\\[ ?|Units:)? *((?:(?:(?:STL )?[A-Za-z0-9]+|AB \\d+|GTWY \\d+|\\d+ DUTY|METRO AIR|NEED (?:A|AMB|EMS|MED)(?: \\d+)?|Rescue Dut)\\b,?)+))?$");
   private static final Pattern BAD_AT_PTN = Pattern.compile("(.*?[a-z ])AT(.*)");
-  
+
   @Override
   protected boolean parseMsg(String body, Data data) {
-    
+
     if (BAD_MSG_PTN.matcher(body).matches()) return false;
-    
+
     body = stripFieldEnd(body, "AT:");
-    
+
     Matcher match = RESPONDING_PTN.matcher(body);
     if (match.lookingAt()) {
       setSelectValue("2");
@@ -50,21 +50,21 @@ public class MOStLouisCountyCParser extends FieldProgramParser {
       body = body.substring(match.end());
       return super.parseMsg(body, data);
     }
-    
+
     setSelectValue("1");
-    
+
     // Occasionally calls are duplicated and we need to cut out the duplicated content :(
     match = DUP_ALERT_PTN.matcher(body);
     if (match.matches())  body = match.group(1) + match.group(2);
-    
+
     // Channels tend to show up in the middle of things
     // So we will start by identifying and getting rid of them
     match = CHANNEL_PTN.matcher(body);
     if (match.find()) {
       data.strChannel = match.group(1);
-      body = append(body.substring(0,match.start()).trim(), " ", body.substring(match.end()).trim()); 
+      body = append(body.substring(0,match.start()).trim(), " ", body.substring(match.end()).trim());
     }
-    
+
     match =  ID_PTN.matcher(body);
     if (match.find()) {
       data.strCallId = match.group(1);
@@ -76,7 +76,7 @@ public class MOStLouisCountyCParser extends FieldProgramParser {
       data.strTime = match.group(1);
       body = body.substring(0,match.start());
     }
-    
+
     match = LAT_LONG_PTN.matcher(body);
     if (match.find()) {
       if (match.group(1) != null) {
@@ -84,13 +84,13 @@ public class MOStLouisCountyCParser extends FieldProgramParser {
       }
       body = body.substring(0,match.start()).trim();
     }
-    
+
     match = SRC_UNIT_PTN.matcher(body);
     if (!match.find()) return false;
     data.strSource = match.group(1);
     data.strUnit = getOptGroup(match.group(2)).replace(' ', '_');
     body = body.substring(0,match.start()).trim();
-    
+
     // Make sure there is a blank in front of the AT keyword
     // And a colon behind it
     int pt = body.indexOf("AT:");
@@ -106,36 +106,36 @@ public class MOStLouisCountyCParser extends FieldProgramParser {
       if (!match.matches()) return false;
       body = match.group(1) + " AT:" + match.group(2) + body.substring(pt);
     }
-    
+
     body = body.replace("TAC:", " TAC:");
 
     return super.parseMsg(body, data);
   }
-  
+
   @Override
   public String getProgram() {
     return super.getProgram() + " CH MAP SRC UNIT GPS TIME ID";
   }
-  
+
   @Override
   public Field getField(String name) {
     if (name.equals("CALL")) return new MyCallField();
     if (name.equals("UNIT_RESP"))  return new MyUnitResponseField();
     return super.getField(name);
   }
-  
+
   private static final Pattern COMMENT_PTN = Pattern.compile("Comment: *(.*), *(.*)");
   private static final Pattern CALL_CODE_PTN = Pattern.compile("(\\d{2}(?:[A-Z]\\d)?) +(.*)");
   private class MyCallField extends CallField {
     @Override
     public void parse(String field, Data data) {
-      
+
       Matcher match = COMMENT_PTN.matcher(field);
       if (match.matches()) {
         data.strSupp = match.group(1).trim();
         field = match.group(2).trim();
       }
-      
+
       match = CALL_CODE_PTN.matcher(field);
       if (match.matches()) {
         data.strCode = match.group(1);
@@ -144,21 +144,21 @@ public class MOStLouisCountyCParser extends FieldProgramParser {
       field = stripFieldEnd(field, " QD");
       super.parse(field, data);
     }
-    
+
     @Override
     public String getFieldNames() {
       return "INFO CODE CALL";
     }
   }
-  
+
   private class MyUnitResponseField extends SkipField {
-    
+
     @Override
     public String getFieldNames() {
       return "UNIT INFO";
     }
   }
-  
+
   @Override
   public String adjustMapAddress(String addr) {
     return DE_MERVILLE_PTN.matcher(addr).replaceAll("$1$2");
