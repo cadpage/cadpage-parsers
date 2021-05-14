ba964b1d82dbc9c8b288ea7c6bafe0ee0eda351b
Kenneth Corbin <ken@cadpage.org>
2021-05-14 16:06:03 -0700

    Fixed parsing problem with Anderson County, SC (Add D)

---
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/SC/SCAndersonCountyDParser.java
@@ -0,0 +1,54 @@
+package net.anei.cadpage.parsers.SC;
+
+import java.util.regex.Pattern;
+
+import net.anei.cadpage.parsers.FieldProgramParser;
+import net.anei.cadpage.parsers.MsgInfo.Data;
+
+public class SCAndersonCountyDParser extends FieldProgramParser {
+
+  public SCAndersonCountyDParser() {
+    super("ANDERSON COUNTY", "SC",
+          "DATETIME CODE CALL ADDRCITYST INFO UNIT ID! END");
+  }
+
+  @Override
+  public String getFilter() {
+    return "centralsquare@andersonsheriff.com";
+  }
+
+  @Override
+  protected boolean parseMsg(String subject, String body, Data data) {
+    if (subject.isEmpty()) return false;
+    return parseFields(body.split("\\|"), data);
+  }
+
+  @Override
+  public Field getField(String name) {
+    if (name.equals("DATETIME")) return new DateTimeField("\\d\\d/\\d\\d/\\d\\d \\d\\d:\\d\\d", true);
+    if (name.equals("INFO")) return new MyInfoField();
+    if (name.equals("UNIT")) return new MyUnitField();
+    if (name.equals("ID")) return new IdField("CFS\\d{4}-\\d+", true);
+    return super.getField(name);
+  }
+
+  private static final Pattern INFO_BRK_PTN = Pattern.compile("(?:^|; *)\\d\\d/\\d\\d/\\d\\d \\d\\d:\\d\\d:\\d\\d - *");
+  private class MyInfoField extends InfoField {
+    @Override
+    public void parse(String field, Data data) {
+      if (field.equals("None")) return;
+      for (String line : INFO_BRK_PTN.split(field)) {
+        data.strSupp = append(data.strSupp, "\n", line.trim());
+      }
+    }
+  }
+
+  private static final Pattern UNIT_BRK_PTN = Pattern.compile("; *");
+  private class MyUnitField extends UnitField {
+    @Override
+    public void parse(String field, Data data) {
+      field = UNIT_BRK_PTN.matcher(field).replaceAll(",");
+      super.parse(field, data);
+    }
+  }
+}
--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/SC/SCAndersonCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/SC/SCAndersonCountyParser.java
@@ -4,38 +4,8 @@ import net.anei.cadpage.parsers.GroupBestParser;
 
 
 public class SCAndersonCountyParser extends GroupBestParser {
-  
+
   public SCAndersonCountyParser() {
-    super(new SCAndersonCountyAParser(), new SCAndersonCountyBParser(), new SCAndersonCountyCParser());
+    super(new SCAndersonCountyCParser(), new SCAndersonCountyDParser());
   }
-
-  static final String[] CITY_LIST = new String[]{
-    "ANDERSON", 
-    "BELTON", 
-    "CLEMSON", 
-    "EASLEY",
-    "HONEA PATH",
-    "IVA",
-    "PELZER", 
-    "STARR",
-    "WEST PELZER",
-    "WILLIAMSTON",
-    "CENTERVILLE",
-    "HOMELAND PARK",
-    "LA FRANC",
-    "NORTHLAKE",
-    "PIEDMONT", 
-    "POWDERSVILLE",
-    "SANDY SPRINGS",
-    "TOWNVILLE",
-    
-    "PENDLETON",
-    
-    // Greenville County
-    "GREENVILLE",
-    
-    // Oconee County
-    "FAIR PLAY"
-  };
-
 }
