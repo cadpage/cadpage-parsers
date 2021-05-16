a5ce985d43192dd96225737a4226060432903bec
Kenneth Corbin <ken@cadpage.org>
2021-05-15 20:20:40 -0700

    Update sender filter for Kent County, MD (C)

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/MD/MDKentCountyCParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/MD/MDKentCountyCParser.java
@@ -16,7 +16,7 @@ public class MDKentCountyCParser extends DispatchA19Parser {
 
   @Override
   public String getFilter() {
-    return "FlexRapidNotification@dccnotify.com,911@Kentgov.org,@alert.active911.com";
+    return "FlexRapidNotification@dccnotify.com,911@Kentgov.org,@alert.active911.com,cadflex@kentgov.org";
   }
 
   @Override
