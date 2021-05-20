339c0390c3603424a819ba1ea724bf134967c58e
Kenneth Corbin <ken@cadpage.org>
2021-05-20 14:13:24 -0700

    Update sender filter for Clay County, NC

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/NC/NCClayCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/NC/NCClayCountyParser.java
@@ -8,23 +8,23 @@ import net.anei.cadpage.parsers.dispatch.DispatchA65Parser;
  * Clay County, NC
  */
 public class NCClayCountyParser extends DispatchA65Parser {
-  
+
   public NCClayCountyParser() {
     super(CITY_LIST, "CLAY COUNTY", "NC");
   }
-  
+
   @Override
   public String getFilter() {
-    return "dispatch@911email.net,clayconc@911email.net";
+    return "dispatch@911email.net,clayconc@911email.net,@claync911.net";
   }
-  
+
   @Override
   public SplitMsgOptions getActive911SplitMsgOptions() {
     return new SplitMsgOptionsCustom();
   }
 
   private static final String[] CITY_LIST = new String[]{
-      
+
       // Town
       "HAYESVILLE",
 
