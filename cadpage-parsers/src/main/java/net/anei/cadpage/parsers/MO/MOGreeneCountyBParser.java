4da08e887ed036ed804a7ee0943e902bfc7caf08
Kenneth Corbin <ken@cadpage.org>
2021-05-20 18:59:46 -0700

    Updated call code table for Greene County, MO (B)

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/MO/MOGreeneCountyBParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/MO/MOGreeneCountyBParser.java
@@ -11,18 +11,18 @@ public class MOGreeneCountyBParser extends DispatchA52Parser {
   public MOGreeneCountyBParser() {
     super(CALL_CODES, "GREENE COUNTY", "MO");
   }
-  
+
   @Override
   public String getFilter() {
     return "GRN_Alert@springfieldmo.gov";
   }
-  
+
   @Override
   protected boolean parseMsg(String subject, String body, Data data) {
-    
+
     // Reject MOChristiainCounty alerts
     if (subject.equals("INC:")) return false;
-    
+
     body = stripFieldStart(body, "GRN_Alert@springfieldmo.gov:");
     body = stripFieldStart(body, "GRN_Alert:");
     if (body.startsWith(":")) body = "LOC" + body;
@@ -30,7 +30,7 @@ public class MOGreeneCountyBParser extends DispatchA52Parser {
     if (!parseMsg(body, data)) return false;
     return data.strAddress.length() > 0;
   }
-  
+
   @Override
   public int getMapFlags() {
     return MAP_FLG_KEEP_STATE_HIGHWAY;
@@ -42,7 +42,7 @@ public class MOGreeneCountyBParser extends DispatchA52Parser {
     sAddress = FARM_ROAD.matcher(sAddress).replaceAll("$1 FARM ROAD $2");
     sAddress = STATE_HWY.matcher(sAddress).replaceAll("$1 STATE HIGHWAY $2");
     sAddress = STATE_HWY_HWY.matcher(sAddress).replaceAll("$1");
-    
+
     // Cross streets include a house number that is not useful for this purpose
     if (cross) {
       sAddress = HOUSE_NUM_PTN.matcher(sAddress).replaceFirst("");
@@ -53,7 +53,7 @@ public class MOGreeneCountyBParser extends DispatchA52Parser {
   private static Pattern FARM_ROAD = Pattern.compile("\\b([NESW]) FR(\\d+)\\b");
   private static Pattern STATE_HWY = Pattern.compile("\\b([NESW]) SH(([A-Z])(?:\\3)?)\\b");
   private static Pattern STATE_HWY_HWY = Pattern.compile("\\b([NESW] STATE HIGHWAY [^ ]+) HWY\\b");
-  
+
   private static final Properties CALL_CODES = buildCodeTable(new String[]{
       "FALARM",   "FIRE ALARM",
       "FAMBA",    "AMBULANCE ASSIST",
@@ -63,6 +63,7 @@ public class MOGreeneCountyBParser extends DispatchA52Parser {
       "FBOMBT",   "BOMB THREAT",
       "FELEV",    "ELEVATOR EMERGENCY",
       "FEMS",     "MEDICAL EMERGENCY",
+      "FEMS2",    "MEDICAL EMERGENCY",
       "FGBLDG",   "GAS ODOR IN A STRUCTURE",
       "FGOTS",    "GAS ODOR OUTSIDE",
       "FHOUSE",   "HOUSE FIRE",
