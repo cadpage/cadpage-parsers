35aa320582f6611f1ab107508657326b1e89fc5b
Kenneth Corbin <ken@cadpage.org>
2021-05-21 12:05:06 -0700

    fixed parsing issue with Harris County ESD1, TX (B)

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/TX/TXHarrisCountyESD1BParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/TX/TXHarrisCountyESD1BParser.java
@@ -15,28 +15,28 @@ public class TXHarrisCountyESD1BParser extends FieldProgramParser {
 
   public TXHarrisCountyESD1BParser() {
     super("HARRIS COUNTY", "TX",
-           "( CODE_CALL ADDR1? ADDR UNIT! KM:MAP! Xst's:X " + 
+           "( CODE_CALL ADDR1? ADDR UNIT1! KM:MAP! Xst's:X " +
            "| ID/Z ADDRCH! ADDR+? Cross_Streets:X ( Map:MAP | Key_Map:MAP ) Zip:ZIP Lat:GPS1/d Lon:GPS2/d" +
-           "| ID/Z ( Unit:UNIT! Type:CODE! | Type:CODE! ) Problem:CALL! Loc:ADDR! Apt:APT! Bldg:PLACE! EMPTY? ( Map:MAP! | Key_Map:MAP! ) Cross_Streets:X! Box_#:BOX! Zip:ZIP! Lat:GPS1/d! Lon:GPS2/d! Notes:INFO " +
-           "| ( ID UNIT2? | UNIT ) CODE CALL! PREALERT? ADDR1? ADDR! Apt:APT! Bldg:PLACE Key_Map:MAP% Cross_Streets:X Box_#:BOX )");
+           "| ID/Z ( Unit:UNIT! Type:CODE! | Type:CODE! ) Problem:CALL! Loc:ADDR! Apt:APT! Bldg:PLACE! EMPTY? ( Map:MAP! | Key_Map:MAP! ) Cross_Streets:X! Box_#:BOX! Zip:ZIP! Lat:GPS1/d! Lon:GPS2/d! Notes:INFO Units:UNIT " +
+           "| ( ID UNIT2? | UNIT1 ) CODE CALL! PREALERT? ADDR1? ADDR! Apt:APT! Bldg:PLACE Key_Map:MAP% Cross_Streets:X Box_#:BOX )");
   }
-  
+
   @Override
   public String getFilter() {
     return "cadnoreply@proxy.hcec.com,93001,agaleucia@avfd.com,CadNoReply@HC911.Com";
   }
-  
+
   @Override
   public int getMapFlags() {
     return MAP_FLG_PREFER_GPS | MAP_FLG_SUPPR_LA;
   }
-  
+
   private static final Pattern REPORT_PTN = Pattern.compile("(?:\\*\\*(Call Completed|Call Cancelled|Times at Hospital) - ?)?ID#:([^ ]+) *- *UNIT:([^ ]+) *- *(.*)");
   private static final Pattern TIME_DELIM_PTN = Pattern.compile(" +- *");
-  
+
   @Override
   public boolean parseMsg(String body, Data data) {
-    
+
     Matcher match = REPORT_PTN.matcher(body);
     if (match.matches()) {
       setFieldList("CALL ID UNIT INFO");
@@ -51,11 +51,11 @@ public class TXHarrisCountyESD1BParser extends FieldProgramParser {
     body = stripFieldStart(body, "**Address Update:  ");
     return parseFields(splitFields(body), data);
   }
-  
+
   private static final Pattern ID_PTN = Pattern.compile("(ID#:?\\d\\d-\\d\\d-\\d+) *-");
   private static final Pattern ZIP_PTN = Pattern.compile("[- ]+(Zip:)");
-  private static final Pattern KEY_SPLIT_PTN = Pattern.compile(" +(?=(?:Lat|Lon|Notes):)");
-  
+  private static final Pattern KEY_SPLIT_PTN = Pattern.compile(" +(?=(?:Lat|Lon|Notes|Units):)");
+
   private String[] splitFields(String body) {
     List<String> flds = new ArrayList<String>();
     Matcher match = ID_PTN.matcher(body);
@@ -80,13 +80,13 @@ public class TXHarrisCountyESD1BParser extends FieldProgramParser {
   public String getProgram() {
     return super.getProgram() + " PLACE";
   }
-  
+
   @Override
   public Field getField(String name) {
     if (name.equals("CODE_CALL")) return new CodeCallField();
     if (name.equals("ID")) return new MyIdField();
     if (name.equals("ADDRCH"))  return new AddressChangeField();
-    if (name.equals("UNIT")) return new MyUnitField();
+    if (name.equals("UNIT1")) return new MyUnit1Field();
     if (name.equals("UNIT2")) return new MyUnit2Field();
     if (name.equals("CODE")) return new MyCodeField();
     if (name.equals("CALL")) return new MyCallField();
@@ -97,14 +97,14 @@ public class TXHarrisCountyESD1BParser extends FieldProgramParser {
     if (name.equals("ZIP")) return new MyZipField();
     return super.getField(name);
   }
-  
+
   private static final Pattern CALL_CODE_PTN = Pattern.compile("(\\d{1,2}[a-z]\\d{1,2}[a-z]?) +([^ ].*)");
   private class CodeCallField extends Field {
     @Override
     public boolean canFail() {
       return true;
     }
-    
+
     @Override
     public boolean checkParse(String field, Data data) {
       Matcher match = CALL_CODE_PTN.matcher(field);
@@ -113,24 +113,24 @@ public class TXHarrisCountyESD1BParser extends FieldProgramParser {
       data.strCall = match.group(2);
       return true;
     }
-    
+
     @Override
     public void parse(String field, Data data) {
       if (!checkParse(field, data)) abort();
     }
-    
+
     @Override
     public String getFieldNames() {
       return "CODE CALL";
     }
   }
-  
+
   private class MyIdField extends IdField {
     @Override
     public boolean canFail() {
       return true;
     }
-    
+
     @Override
     public boolean checkParse(String field, Data data) {
       if (!field.startsWith("ID#")) return false;
@@ -138,14 +138,14 @@ public class TXHarrisCountyESD1BParser extends FieldProgramParser {
       super.parse(field, data);
       return true;
     }
-    
+
     @Override
     public void parse(String field, Data data) {
       if (!checkParse(field, data)) abort();
     }
   }
-  
-  // Address change field 
+
+  // Address change field
   private class AddressChangeField extends AddressField {
     @Override
     public boolean checkParse(String field, Data data) {
@@ -155,21 +155,21 @@ public class TXHarrisCountyESD1BParser extends FieldProgramParser {
       super.parse(field, data);
       return true;
     }
-    
+
     @Override
     public String getFieldNames() {
       return "CALL " + super.getFieldNames();
     }
   }
-  
+
   // Unit field only exists if it is tagged (or contains something that looks like a unit
-  private class MyUnit2Field extends MyUnitField {
-    
+  private class MyUnit2Field extends MyUnit1Field {
+
     @Override
     public boolean canFail() {
       return true;
     }
-    
+
     @Override
     public boolean checkParse(String field, Data data) {
       if (field.startsWith("Units:")) {
@@ -179,9 +179,9 @@ public class TXHarrisCountyESD1BParser extends FieldProgramParser {
       return true;
     }
   }
-  
-  private static final Pattern UNIT_PTN = Pattern.compile("[A-Z]+[0-9]+|\\d{4}|BOX|NWREH|[A-Z]{4}HOME|[A-Z]{2}FD");
-  private class MyUnitField extends UnitField {
+
+  private static final Pattern UNIT_PTN = Pattern.compile("(?:\\b(?:[A-Z]+[0-9]+|\\d{1,4}|[A-Z]|BAT|BOX|Fresno-E|HCM[CD]?|IC|NONEMER|NWR|NWREH|REH|[A-Z]{4}HOME|[A-Z]{2}FD)\\b,?)+");
+  private class MyUnit1Field extends UnitField {
     @Override
     public void parse(String field, Data data) {
       if (field.length() == 0) return;
@@ -189,7 +189,7 @@ public class TXHarrisCountyESD1BParser extends FieldProgramParser {
       super.parse(field, data);
     }
   }
-  
+
   // Code field has to fit specific pattern
   private static final Pattern CODE_PTN = Pattern.compile("\\d\\d?[A-Z]\\d\\d[A-Za-z]?|");
   private class MyCodeField extends CodeField {
@@ -197,7 +197,7 @@ public class TXHarrisCountyESD1BParser extends FieldProgramParser {
       setPattern(CODE_PTN, true);
     }
   }
-  
+
   private class MyCallField extends CallField {
     @Override
     public void parse(String field, Data data) {
@@ -212,7 +212,7 @@ public class TXHarrisCountyESD1BParser extends FieldProgramParser {
       super.parse(field, data);
     }
   }
-  
+
   private class MyAddressField extends AddressField {
     @Override
     public void parse(String field, Data data) {
@@ -221,20 +221,20 @@ public class TXHarrisCountyESD1BParser extends FieldProgramParser {
       super.parse(field, data);
     }
   }
-  
+
   // PreAlert is a special call extender field
   private class PreAlertField extends CallField {
-    
+
     public PreAlertField() {
       setPattern(Pattern.compile("PreAlert"));
     }
-    
+
     @Override
     public void parse(String field, Data data) {
       data.strCall = append(data.strCall, " - ", field);
     }
   }
-  
+
   // There are two fields, APT and BLD mapped to the apartment field.  But they
   // occur in reverse order, if both are specified, we want to to put bldg first.
   private class MyAptField extends AptField {
@@ -243,7 +243,7 @@ public class TXHarrisCountyESD1BParser extends FieldProgramParser {
       data.strApt = append(field, "-", data.strApt);
     }
   }
-  
+
   private class MyZipField extends CityField {
     @Override
     public void parse(String field, Data data) {
