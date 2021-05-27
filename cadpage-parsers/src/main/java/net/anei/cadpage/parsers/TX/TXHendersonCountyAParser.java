855b8cb357f6fdfa61e735b7c184b120213a0cfe
Kenneth Corbin <ken@cadpage.org>
2021-05-27 11:44:30 -0700

    Fixed parsing problem with Henderson County, TX (A)

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/TX/TXHendersonCountyAParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/TX/TXHendersonCountyAParser.java
@@ -1,13 +1,31 @@
 package net.anei.cadpage.parsers.TX;
 
+import net.anei.cadpage.parsers.MsgInfo.Data;
 import net.anei.cadpage.parsers.dispatch.DispatchA64Parser;
 
 public class TXHendersonCountyAParser extends DispatchA64Parser {
-  
+
   public TXHendersonCountyAParser() {
     super("HENDERSON COUNTY", "TX");
   }
-  
+
+  @Override
+  protected boolean parseMsg(String subject, String body, Data data) {
+    if (body.startsWith("dkim-signature:")) {
+      if (!body.contains("\nSubject:   Dispatch Alert")) return false;
+      body = body.replace("=\n", "");
+      int pt1 = body.indexOf("\n  Call Type:");
+      if (pt1 < 0) return false;
+      int pt2 = body.indexOf('\n', pt1+13);
+      if (pt2 < 0) pt2 = body.length();
+      subject = "Dispatch Alert";
+      body = body.substring(pt1+1, pt2).trim();
+      body = stripFieldEnd(body, "=20");
+    }
+
+    return super.parseMsg(subject, body, data);
+  }
+
   @Override
   public String getFilter() {
     return "cadalerts@eforcesoftware.com";
