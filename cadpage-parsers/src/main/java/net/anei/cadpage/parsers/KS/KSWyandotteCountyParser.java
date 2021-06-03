033da0f217353a3d8fd7c34de00d99616911c292
Kenneth Corbin <ken@cadpage.org>
2021-06-03 10:26:45 -0700

    Fixed parsing problem with Wyandotte County, KS

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/KS/KSWyandotteCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/KS/KSWyandotteCountyParser.java
@@ -5,35 +5,35 @@ import java.text.SimpleDateFormat;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import net.anei.cadpage.parsers.FieldProgramParser;
 import net.anei.cadpage.parsers.MsgInfo.Data;
+import net.anei.cadpage.parsers.dispatch.DispatchH05Parser;
 
-public class KSWyandotteCountyParser extends FieldProgramParser {
-  
+public class KSWyandotteCountyParser extends DispatchH05Parser {
+
   public KSWyandotteCountyParser() {
-    super("WYANDOTTE COUNTY", "KS", 
-          "CALL:CALL! PLACE:PLACE! ADDR:ADDRCITY! PRI:PRI! DATE:DATETIME! UNIT:UNIT! INFO:INFO/N+");
+    super("WYANDOTTE COUNTY", "KS",
+          "CALL:CALL! INCIDENT:ID? PLACE:PLACE! ADDR:ADDRCITY! PRI:PRI! DATE:DATETIME! UNIT:UNIT! INFO:INFO_BLK+ Call_Date:SKIP");
   }
-  
+
   @Override
   public String getFilter() {
     return "@kckfd.org";
   }
-  
+
   @Override
   public boolean parseMsg(String subject, String body, Data data) {
     if (!subject.startsWith("Automatic R&R Notification:")) return false;
     return parseFields(body.split("\n"), data);
   }
-  
-  
+
+
   @Override
   public Field getField(String name) {
     if (name.equals("ADDRCITY")) return new MyAddressCityField();
     if (name.equals("DATETIME")) return new MyDateTimeField();
     return super.getField(name);
   }
-  
+
   private class MyAddressCityField extends AddressCityField {
     @Override
     public void parse(String field, Data data) {
@@ -41,7 +41,7 @@ public class KSWyandotteCountyParser extends FieldProgramParser {
       super.parse(field, data);
     }
   }
-  
+
   private static final Pattern DATE_TIME_PTN = Pattern.compile("(\\d\\d?/\\d\\d?/\\d{4}) +(\\d\\d?:\\d\\d:\\d\\d(?: [AP]M)?)");
   private static final DateFormat DATE_TIME_FMT = new SimpleDateFormat("hh:mm:ss aa");
   private class MyDateTimeField extends DateTimeField {
