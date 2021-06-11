cd2c9ae450722234f42c10a1aaf94079566b9c9f
Kenneth Corbin <ken@cadpage.org>
2021-06-11 11:29:41 -0700

    Update msg docs

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/CA/CAShastaCountyCParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/CA/CAShastaCountyCParser.java
@@ -3,13 +3,13 @@ package net.anei.cadpage.parsers.CA;
 import net.anei.cadpage.parsers.dispatch.DispatchA19Parser;
 
 public class CAShastaCountyCParser extends DispatchA19Parser {
-  
+
   public CAShastaCountyCParser() {
     super("SHASTA COUNTY", "CA");
   }
-  
+
   @Override
   public String getFilter() {
-    return "SHASCOM";
+    return "SHASCOM@ipsshasta.com";
   }
 }
