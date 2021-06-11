2ad407ed56e0b253dc3d4b27c11b9370f447f5b8
Kenneth Corbin <ken@cadpage.org>
2021-06-11 16:02:25 -0700

    Fixed parsing problem with LaRue County, KY

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/KY/KYStatePoliceBParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/KY/KYStatePoliceBParser.java
@@ -3,15 +3,15 @@ package net.anei.cadpage.parsers.KY;
 import net.anei.cadpage.parsers.dispatch.DispatchSPKParser;
 
 public class KYStatePoliceBParser extends DispatchSPKParser {
-  
+
   public KYStatePoliceBParser() {
     this("");
   }
-  
+
   KYStatePoliceBParser(String defCity) {
     super(defCity, "KY");
   }
-  
+
   @Override
   public String getAliasCode() {
     return "KYStatePoliceB";
