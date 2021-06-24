ea2598e9ae8092775249bcf1236f88ae57b26daf
Kenneth Corbin <ken@cadpage.org>
2021-06-24 12:26:45 -0700

    Update sender filter for Talbot County, MD

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/MD/MDTalbotCountyCParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/MD/MDTalbotCountyCParser.java
@@ -3,13 +3,13 @@ package net.anei.cadpage.parsers.MD;
 import net.anei.cadpage.parsers.dispatch.DispatchSPKParser;
 
 public class MDTalbotCountyCParser extends DispatchSPKParser {
-  
+
   public MDTalbotCountyCParser() {
     super("TALBOT COUNTY", "MD");
   }
-  
+
   @Override
   public String getFilter() {
-    return "@talbotdes.org";
+    return "@talbotdes.org,@server.com";
   }
 }
