f87aded8088d0aea4bb86535728b5236f1e34b14
Ken Corbin <ken@cadpage.org>
2021-06-23 20:23:15 -0700

    Update sender filter for Jasper County, IN

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/IN/INJasperCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/IN/INJasperCountyParser.java
@@ -18,7 +18,7 @@ public class INJasperCountyParser extends DispatchA19Parser {
 
   @Override
   public String getFilter() {
-    return "noreply@jaspercountypolice.com";
+    return "noreply@jaspercountypolice.com,@alert.active911.com";
   }
 
   private static final Pattern DIR_PTN = Pattern.compile("[NSEW]B");
