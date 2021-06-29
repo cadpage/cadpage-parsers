4788c469cb70cae8e819af255e1395f6a92ab89a
Kenneth Corbin <ken@cadpage.org>
2021-06-28 17:35:50 -0700

    Added city field to Wilson County, NC

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/NC/NCWilsonCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/NC/NCWilsonCountyParser.java
@@ -1,6 +1,7 @@
 package net.anei.cadpage.parsers.NC;
 
 
+import java.util.Properties;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -10,27 +11,27 @@ import net.anei.cadpage.parsers.dispatch.DispatchOSSIParser;
 
 
 public class NCWilsonCountyParser extends DispatchOSSIParser {
-  
+
   public NCWilsonCountyParser() {
-    super("WILSON COUNTY", "NC",
-           "ID? CODE? CALL ADDR! ( PLACE SRC | SRC? )  X+? INFO+");
+    super(CITY_CODES, "WILSON COUNTY", "NC",
+          "ID? CODE? CALL ADDR! CITY? ( PLACE SRC | SRC? )  X+? INFO+");
   }
 
   @Override
   public String getFilter() {
     return "@Wilson-co.com,930010";
   }
-  
+
   @Override
   public boolean parseMsg(String subject, String body, Data data) {
-    
+
     body = stripFieldStart(body, "/ Text Message / ");
-    
+
     if (subject.equals("Sanoca")) {
       if (body.startsWith(";")) body = "CAD:" + body.substring(1).trim();
     }
     else if (body.startsWith("CAD:") && subject.contains(";")) {
-      body = "CAD:" + subject + ' ' + body.substring(4); 
+      body = "CAD:" + subject + ' ' + body.substring(4);
     }
     return super.parseMsg(body,  data);
   }
@@ -43,7 +44,7 @@ public class NCWilsonCountyParser extends DispatchOSSIParser {
     if (name.equals("X")) return new MyCrossField();
     return super.getField(name);
   }
-  
+
   private class MyCrossField extends CrossField {
     @Override
     public boolean checkParse(String field, Data data) {
@@ -54,8 +55,8 @@ public class NCWilsonCountyParser extends DispatchOSSIParser {
       return super.checkParse(field, data);
     }
   }
-  
-  
+
+
   @Override
   public String adjustMapAddress(String address) {
     Matcher match = US_264_DIR.matcher(address);
@@ -71,4 +72,87 @@ public class NCWilsonCountyParser extends DispatchOSSIParser {
     return address;
   }
   private static final Pattern US_264_DIR = Pattern.compile("\\bUS +264(A?)(?: +[NESW])?\\b", Pattern.CASE_INSENSITIVE);
+
+  private static final Properties CITY_CODES = buildCodeTable(new String[] {
+      "ANGI", "ANGIER",
+      "APEX", "APEX",
+      "AYDE", "AYDEN",
+      "BALY", "BAILEY",
+      "BC",   "BLACK CREEK",
+      "BENS", "BENSON",
+      "BTLB", "BATTLEBORO",
+      "CARY", "CARY",
+      "CAST", "CASTALIA",
+      "CH",   "CHARLOTTE",
+      "CLAY", "CLAYTON",
+      "CNTY", "COUNTY",
+      "COL",  "SC COLUMBIA",
+      "CREE", "CREEDMOOR",
+      "CRT",  "CARTERET COUNTY",
+      "DUDL", "DUDLEY",
+      "DUR",  "DURHAM",
+      "EC",   "ELM CITY",
+      "EDGE", "EDGECOMBE COUNTY",
+      "EUR",  "EUREKA",
+      "FARM", "FARMVILLE",
+      "FOUN", "FOUNTAIN",
+      "FOUR", "FOUR OAKS",
+      "FRMT", "FREMONT",
+      "FUQ",  "FUQUAY VARINA",
+      "GAR",  "GARNER",
+      "GOL",  "GOLDSBORO",
+      "GREE", "GREENE COUNTY",
+      "GRIF", "GRIFTON",
+      "GVIL", "GREENVILLE",
+      "HALI", "HALIFAX COUNTY",
+      "HOLL", "HOLLESTER",
+      "HOOK", "HOOKERTON",
+      "HS",   "HOLLY SPRINGS",
+      "JOHN", "JOHNSTON COUNTY",
+      "KINS", "KINSTON",
+      "KNIG", "KNIGHTDALE",
+      "KNLY", "KENLY",
+      "LAGR", "LAGRANGE",
+      "LOUI", "LOUISBURG",
+      "LUC",  "LUCAMA",
+      "MAC",  "MACCLESFIELD",
+      "MCRO", "MICRO",
+      "MORR", "MORRISVILLE",
+      "MSEX", "MIDDLESEX",
+      "MTOL", "MOUNT OLIVE",
+      "NASH", "NASH COUNTY",
+      "NB",   "NEW BERN",
+      "NEWH", "NEW HILL",
+      "NHC",  "NORTHAMPTON COUNTY",
+      "NVLE", "NASHVILLE",
+      "OTHE", "OTHER COUNTY",
+      "PERQ", "PERQUIMANS",
+      "PIKE", "PIKEVILLE",
+      "PINE", "PINETOPS",
+      "PITT", "PITT COUNTY",
+      "PRIN", "PRINCETON",
+      "RAL",  "RALEIGH",
+      "RMT",  "ROCKY MOUNT",
+      "ROLE", "ROLESVILLE",
+      "SAR",  "SARATOGA",
+      "SEVE", "SEVENSPRINGS",
+      "SH",   "SPRING HOPE",
+      "SHAR", "SHARPSBURG",
+      "SIMS", "SIMS",
+      "SNO",  "SNOW HILL",
+      "STBG", "STANTONSBURG",
+      "TARB", "TARBORO",
+      "WAKE", "WAKE COUNTY",
+      "WALS", "WALSTONBURG",
+      "WAYN", "WAYNE COUNTY",
+      "WEND", "WENDELL",
+      "WF",   "WAKE FOREST",
+      "WHIT", "WHITAKERS",
+      "WIN",  "WINTERVILLE",
+      "WLSN", "WILSON",
+      "WS",   "WILLOW SPRING",
+      "YVLE", "YOUNGSVILLE",
+      "ZEB",  "ZEBULON"
+
+  });
 }
