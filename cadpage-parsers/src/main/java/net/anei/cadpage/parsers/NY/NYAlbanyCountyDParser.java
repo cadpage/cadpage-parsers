2d8c5107dcfe5f19136324418c3612d9564dc3eb
Kenneth Corbin <ken@cadpage.org>
2021-07-05 09:29:45 -0700

    Update sender filter for Albany County, NY

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/NY/NYAlbanyCountyDParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/NY/NYAlbanyCountyDParser.java
@@ -10,13 +10,13 @@ import net.anei.cadpage.parsers.MsgInfo.MsgType;
 public class NYAlbanyCountyDParser extends FieldProgramParser {
 
   public NYAlbanyCountyDParser() {
-    super("ALBANY COUNTY", "NY", 
+    super("ALBANY COUNTY", "NY",
           "CALL EMPTY UNIT EMPTY EMPTY? ADDR EMPTY! Cross_streets:X! Routing_Info:SKIP! Business:PLACE! RP:NAME! PHONE:PHONE! INFO/N+");
   }
-  
+
   @Override
   public String getFilter() {
-    return "root@capsnet.albany-ny.org";
+    return "root@capsnet.albany-ny.org,@colonie.org>";
   }
 
   @Override
@@ -24,14 +24,14 @@ public class NYAlbanyCountyDParser extends FieldProgramParser {
     body = body.replace(" PHONE:", "\nPHONE:");
     return super.parseFields(body.split("\n"), data);
   }
-  
+
   @Override
   public Field getField(String name) {
     if (name.equals("NAME")) return new MyNameField();
     if (name.equals("INFO")) return new MyInfoField();
     return super.getField(name);
   }
-  
+
   private class MyNameField extends NameField {
     @Override
     public void parse(String field, Data data) {
@@ -40,25 +40,25 @@ public class NYAlbanyCountyDParser extends FieldProgramParser {
       data.strName = field;
     }
   }
-  
+
   private static final Pattern INFO_JUNK_PTN = Pattern.compile("-*");
   private static final Pattern INFO_ID_TIME_PTN = Pattern.compile("Seq: (\\d+) TO: ([^ ]*?) ST: ([^ ]*) ID: *(.*)");
   private static final Pattern INFO_RR_TIME_PTN = Pattern.compile("\\d\\d/\\d\\d/\\d\\d \\d\\d:\\d\\d:\\d\\d [A-Z0-9]+ +.*");
   private class MyInfoField extends InfoField {
     @Override
     public void parse(String field, Data data) {
-      
+
       if (INFO_JUNK_PTN.matcher(field).matches()) return;
-      
+
       Matcher match = INFO_ID_TIME_PTN.matcher(field);
-      
+
       if (match.matches()) {
         data.strCallId = append(match.group(1), "/", match.group(4));
         String time = match.group(2);
         if (!time.equals("EVNT")) data.strTime = time;
         field = "ST: " + match.group(3);
       }
-      
+
       if (INFO_RR_TIME_PTN.matcher(field).matches()) {
         if (data.msgType == MsgType.PAGE){
           data.msgType = MsgType.RUN_REPORT;
@@ -66,11 +66,11 @@ public class NYAlbanyCountyDParser extends FieldProgramParser {
         } else {
           data.strSupp = append(data.strSupp, "\n", field);
         }
-      } else if (data.msgType == MsgType.PAGE) { 
+      } else if (data.msgType == MsgType.PAGE) {
         super.parse(field, data);
       }
     }
-    
+
     @Override
     public String getFieldNames() {
       return "ID TIME " + super.getFieldNames();
