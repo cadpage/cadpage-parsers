1e759f789a94657cd5989688445b8e5732691d68
Kenneth Corbin <ken@cadpage.org>
2021-07-08 20:47:19 -0700

    Update sender filter for Booen County, WV

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/WV/WVBooneCountyBParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/WV/WVBooneCountyBParser.java
@@ -8,35 +8,35 @@ import net.anei.cadpage.parsers.MsgInfo.Data;
 public class WVBooneCountyBParser extends FieldProgramParser {
 
   public WVBooneCountyBParser() {
-    super("BOONE COUNTY", "WV", 
+    super("BOONE COUNTY", "WV",
           "Location:ADDRCITYST! Cross_Street:X! CFS_#:ID! 911_Lat:GPS1! 911_Long:GPS2! Details:INFO! END");
   }
-  
+
   @Override
   public String getFilter() {
-    return "no-reply@zuercherportal.com";
+    return "no-reply@zuercherportal.com,boonezuercher@gmail.com";
   }
-  
+
   @Override
   protected boolean parseMsg(String subject, String body, Data data) {
-    
+
     if (!subject.startsWith("Type:")) return false;
     data.strCall = subject.substring(5).trim();
-    
+
     return super.parseMsg(body, data);
   }
-  
+
   @Override
   public String getProgram() {
     return "CALL " + super.getProgram();
   }
-  
+
   @Override
   public Field getField(String name) {
     if (name.equals("INFO")) return new MyInfoField();
     return super.getField(name);
   }
-  
+
   private static final Pattern INFO_DATE_TIME_PTN = Pattern.compile("[; ]*\\b\\d\\d?/\\d\\d?/\\d\\d \\d\\d?:\\d\\d:\\d\\d - *");
   private class MyInfoField extends InfoField {
     @Override
