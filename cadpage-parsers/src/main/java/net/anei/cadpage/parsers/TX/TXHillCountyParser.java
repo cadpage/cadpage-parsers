28cff41da7557288dfd156382515cf7aceff2ba1
Kenneth Corbin <ken@cadpage.org>
2021-07-13 20:31:20 -0700

    Added call translation table for Hill County, TX

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/TX/TXHillCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/TX/TXHillCountyParser.java
@@ -1,5 +1,8 @@
 package net.anei.cadpage.parsers.TX;
 
+import java.util.Properties;
+
+import net.anei.cadpage.parsers.MsgInfo.Data;
 import net.anei.cadpage.parsers.dispatch.DispatchA19Parser;
 
 public class TXHillCountyParser extends DispatchA19Parser {
@@ -17,5 +20,20 @@ public class TXHillCountyParser extends DispatchA19Parser {
   public int getMapFlags() {
     return MAP_FLG_SUPPR_LA | MAP_FLG_PREFER_GPS;
   }
+
+  @Override
+  protected boolean parseMsg(String subject, String body, Data data) {
+    if (!super.parseMsg(subject, body, data)) return false;
+    data.strCall = convertCodes(data.strCall, CALL_CODES);
+    return true;
+  }
+
+  private static final Properties CALL_CODES = buildCodeTable(new String[] {
+      "AccidentMajor",   "Major Accident",
+      "AccidentMinor",   "Minor Accident",
+      "Ambulance Emerg", "Medical Emergency",
+      "FIREGRASS",       "Grass Fire",
+      "Traffic Stop",    "Assist PD"
+  });
 
 }
