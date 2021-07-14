8b01ecf8ba26cd3c248588121392b5ecac3c4108
Kenneth Corbin <ken@cadpage.org>
2021-07-14 07:32:03 -0700

    Updated sender filter for Etowah County, AL (C)

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/AL/ALEtowahCountyCParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/AL/ALEtowahCountyCParser.java
@@ -8,27 +8,27 @@ import net.anei.cadpage.parsers.FieldProgramParser;
 import net.anei.cadpage.parsers.MsgInfo.Data;
 
 public class ALEtowahCountyCParser extends FieldProgramParser {
-  
+
   public ALEtowahCountyCParser() {
-    super("ETOWAH COUNTY", "AL", 
+    super("ETOWAH COUNTY", "AL",
           "CALL:CALL! PLACE:PLACE! ADDR:ADDR! CITY:CITY! ID:ID! DATE:DATE! TIME:TIME! INFO:INFO! Description:INFO! INFO/N+");
   }
-  
+
   @Override
   public String getFilter() {
-    return "southsidedispatch@southsideal.city";
+    return "southsidedispatch@southsideal.city,Active911Dispatch@calcoso.org";
   }
-  
+
   private static final Pattern DELIM = Pattern.compile("\n|/{2,}");
-  
+
   @Override
   protected boolean parseMsg(String subject, String body, Data data) {
     if (!subject.equals("SSFD Alert")) return false;
     return parseFields(DELIM.split(body), data);
   }
-  
+
   private static final DateFormat TIME_FMT = new SimpleDateFormat("hh:mm aa");
-  
+
   @Override
   public Field getField(String name) {
     if (name.equals("ADDR")) return new MyAddressField();
@@ -36,7 +36,7 @@ public class ALEtowahCountyCParser extends FieldProgramParser {
     if (name.equals("TIME")) return new TimeField(TIME_FMT, true);
     return super.getField(name);
   }
-  
+
   private class MyAddressField extends AddressField {
     @Override
     public void parse(String field, Data data) {
