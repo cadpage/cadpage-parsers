fd035f4d2261ab506554e0aadf775f772fdf820d
Kenneth Corbin <ken@cadpage.org>
2021-07-23 20:31:07 -0700

    Fixed parsing probem with Cocke County, TN

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/TN/TNCockeCountyBParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/TN/TNCockeCountyBParser.java
@@ -1,46 +1,15 @@
 package net.anei.cadpage.parsers.TN;
 
-import net.anei.cadpage.parsers.SplitMsgOptions;
-import net.anei.cadpage.parsers.SplitMsgOptionsCustom;
-import net.anei.cadpage.parsers.dispatch.DispatchA65Parser;
+import net.anei.cadpage.parsers.dispatch.DispatchA74Parser;
 
-public class TNCockeCountyBParser extends DispatchA65Parser {
+public class TNCockeCountyBParser extends DispatchA74Parser {
 
   public TNCockeCountyBParser() {
-    super(CITY_LIST, "COCKE COUNTY", "TN");
+    super("COCKE COUNTY", "TN");
   }
 
   @Override
   public String getFilter() {
     return "dispatch@911comm2.info";
   }
-
-  @Override
-  public SplitMsgOptions getActive911SplitMsgOptions() {
-    return new SplitMsgOptionsCustom();
-  }
-
-  private static final String[] CITY_LIST = new String[] {
-
-      // City
-      "NEWPORT",
-
-      // Towns
-      "PARROTTSVILLE",
-
-      // Unincorporated communities
-      "ALLEN GROVE",
-      "BALTIMORE",
-      "BOOMER",
-      "BRIAR THICKET",
-      "BRIDGEPORT",
-      "BYBEE",
-      "COSBY",
-      "DEL RIO",
-      "HARTFORD",
-      "MIDWAY",
-      "REIDTOWN",
-      "TOM TOWN",
-      "WASP"
-  };
 }
