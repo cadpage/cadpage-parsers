eb074cf302152de84d72b3d02fa9504f88c50ab3
Kenneth Corbin <ken@cadpage.org>
2021-08-07 12:23:20 -0700

    Fixed mapping problem with Benton County, MO

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/MO/MOBentonCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/MO/MOBentonCountyParser.java
@@ -1,5 +1,8 @@
 package net.anei.cadpage.parsers.MO;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import net.anei.cadpage.parsers.dispatch.DispatchBCParser;
 
 
@@ -7,9 +10,30 @@ public class MOBentonCountyParser extends DispatchBCParser {
   public MOBentonCountyParser() {
     super("BENTON COUNTY", "MO");
   }
-   
+
   @Override
   public String getFilter() {
     return "911.ADMIN@BENTONCOMO.COM";
+  }
+
+  private static final Pattern HIGHWAY_XX_PTN = Pattern.compile("\\b(?:HIGHWAY|HWY) ([A-Z]{1,2})\\b", Pattern.CASE_INSENSITIVE);
+
+  @Override
+  public String postAdjustMapAddress(String address) {
+    Matcher match = HIGHWAY_XX_PTN.matcher(address);
+    if (match.find()) {
+      StringBuffer sb = new StringBuffer();
+      do {
+        String hwy = match.group(1);
+        if (hwy.length() == 2 && hwy.charAt(0) != hwy.charAt(1)) {
+          hwy = match.group();
+        } else {
+          hwy = "STATE HWY " + hwy;
+        }
+        match.appendReplacement(sb, hwy);
+      } while (match.find());
+      address = sb.toString();
+    }
+    return address;
   }
 }
