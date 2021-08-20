025b77a766ef1e98f6eec841ccfa22bc66d7afa7
Kenneth Corbin <ken@cadpage.org>
2021-08-20 16:09:37 -0700

    Update sender filter for New London County, CT (A)

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/CT/CTNewLondonCountyAParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/CT/CTNewLondonCountyAParser.java
@@ -6,16 +6,16 @@ import net.anei.cadpage.parsers.dispatch.DispatchA32Parser;
  * New London County, CT
  */
 public class CTNewLondonCountyAParser extends DispatchA32Parser {
-  
+
   public CTNewLondonCountyAParser() {
     super(CITY_LIST, "NEW LONDON COUNTY", "CT");
   }
 
   @Override
   public String getFilter() {
-    return "ledyard911@ct.org,LEDYARD-911@LEDYARDCT.ORG";
+    return "ledyard911@ct.org,LEDYARD-911@LEDYARDCT.ORG,ledyard911@gmail.com";
   }
- 
+
   private static final String[] CITY_LIST= new String[]{
     "EAST LYME",
     "GALES FERRY",
