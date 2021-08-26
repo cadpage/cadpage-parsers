898fd4c6bb9d45bd3c6c5738468dcc33ff1e8d45
Kenneth Corbin <ken@cadpage.org>
2021-08-25 18:07:23 -0700

    Fixed parsing problem with Centre County, PA (B)

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/PA/PACentreCountyBParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/PA/PACentreCountyBParser.java
@@ -7,31 +7,29 @@ import net.anei.cadpage.parsers.FieldProgramParser;
 import net.anei.cadpage.parsers.MsgInfo.Data;
 
 public class PACentreCountyBParser extends FieldProgramParser {
-  
+
   public PACentreCountyBParser() {
-    super("CENTRE COUNTY", "PA", 
-          "Box:BOX_CALL! CALL? ADDRCITY! PLACE APT NAME Name:NAME Due:UNIT END");
+    super("CENTRE COUNTY", "PA",
+          "Box:BOX_CALL! CALL? ADDRCITY! PLACE APT NAME Name:NAME ( Number:ID! | Due:UNIT ) END");
   }
-  
+
   @Override
   public String getFilter() {
     return "alerts@centre.ealert911.com,dispatch@centrecountypa.gov,Centre County Alerts";
   }
-  
+
   private static final Pattern HTTP_PTN = Pattern.compile("[ \n]http:");
   private static final Pattern PREFIX_PTN = Pattern.compile("(?:FD|EMS) (?=Box:)");
   private static final Pattern FIX_CITY_PTN = Pattern.compile("(?:BL(?!AIR)|CT|CF|HT|UN(?!ION)) *(.*)");
-  
+
   @Override
   protected boolean parseMsg(String subject, String body, Data data) {
-    if (!subject.equals("Centre County Alerts") && !subject.equals("| !!")) return false;
-    
     Matcher match = HTTP_PTN.matcher(body);
     if (match.find()) body = body.substring(0,match.start()).trim();
-    
+
     match = PREFIX_PTN.matcher(body);
     if (match.lookingAt()) body = body.substring(match.end());
-    
+
     body = stripFieldEnd(body, ".");
     body = body.replace(" Due:", "\nDue:");
     if (!parseFields(body.split("\n"), data)) return false;
@@ -40,7 +38,7 @@ public class PACentreCountyBParser extends FieldProgramParser {
     if (match.matches()) data.strCity = match.group(1);
     return true;
   }
-  
+
   @Override
   public Field getField(String name) {
     if (name.equals("BOX_CALL")) return new MyBoxCallField();
@@ -49,7 +47,7 @@ public class PACentreCountyBParser extends FieldProgramParser {
     if (name.equals("NAME")) return new MyNameField();
     return super.getField(name);
   }
-  
+
   private static final Pattern BOX_CALL_PTN = Pattern.compile("(\\d{2,})(?: +(.*))?");
   private class MyBoxCallField extends Field {
 
@@ -66,28 +64,28 @@ public class PACentreCountyBParser extends FieldProgramParser {
     public String getFieldNames() {
       return "BOX CALL";
     }
-    
+
   }
-  
+
   private class MyCallField extends CallField {
     @Override
     public boolean canFail() {
       return true;
     }
-    
+
     @Override
     public boolean checkParse(String field, Data data) {
       if (data.strCall.length() > 0) return false;
       super.parse(field, data);
       return true;
     }
-    
+
     @Override
     public void parse(String field, Data data) {
       if (!checkParse(field, data)) abort();
     }
   }
-  
+
   private class MyNameField extends NameField {
     @Override
     public void parse(String field, Data data) {
