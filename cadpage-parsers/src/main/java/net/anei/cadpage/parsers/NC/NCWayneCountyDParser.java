154b6352d6aaf816cd242ae7acb1c4d973726c83
Kenneth Corbin <ken@cadpage.org>
2021-09-04 12:14:25 -0700

    Update sender filter for Wayne County, NC (D)

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/NC/NCWayneCountyDParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/NC/NCWayneCountyDParser.java
@@ -3,13 +3,13 @@ package net.anei.cadpage.parsers.NC;
 import net.anei.cadpage.parsers.dispatch.DispatchA19Parser;
 
 public class NCWayneCountyDParser extends DispatchA19Parser {
-  
+
   public NCWayneCountyDParser() {
     super("WAYNE COUNTY", "NC");
   }
-  
+
   @Override
   public String getFilter() {
-    return "@alert.active911.com";
+    return "@alert.active911.com,WayneCounty911@waynegov.com";
   }
 }
