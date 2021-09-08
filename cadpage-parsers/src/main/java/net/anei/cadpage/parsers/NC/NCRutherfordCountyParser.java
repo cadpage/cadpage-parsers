d9c1c00a0389b26f0c7caec0ca33e930ffe13f7a
Kenneth Corbin <ken@cadpage.org>
2021-09-08 15:27:38 -0700

    Fixed parsing problem with Rutherford County,NC

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/NC/NCRutherfordCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/NC/NCRutherfordCountyParser.java
@@ -1,61 +1,112 @@
 package net.anei.cadpage.parsers.NC;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import net.anei.cadpage.parsers.FieldProgramParser;
 import net.anei.cadpage.parsers.MsgInfo.Data;
+import net.anei.cadpage.parsers.MsgInfo.MsgType;
 
 
 public class NCRutherfordCountyParser extends FieldProgramParser {
-  
+
   public NCRutherfordCountyParser() {
     super("RUTHERFORD COUNTY", "NC",
-          "( Location:ADDR! APT/ROOM:APT? City:CITY! Call_Type:CALL! Line11:INFO? Units:UNIT! " + 
-          "| City:CITY! Call_Type:CALL! Units:UNIT! END )");
+          "( ( UNIT Prob:CALL! | Prob:CALL! ) /Addr:ADDR! ( /City:CITY! CrossSt:X! | CrossSt:X! /City:CITY ) " +
+          "| UNIT Run_Number:ID! Address:ADDR! Dispatch:TIMES! Unit:UNIT! " +
+          "| Location:ADDR! APT/ROOM:APT? City:CITY! Call_Type:CALL! Line11:INFO? Units:UNIT! DATETIME1 " +
+          "| City:CITY! Call_Type:CALL! Units:UNIT! " +
+          ") END");
   }
-  
+
   @Override
   public String getFilter() {
     return "paging@rutherfordcountync.gov,8284295922";
   }
-  
+
+  private static final Pattern MISSING_BLANK_PTN = Pattern.compile("(?<=\\S)(?=Prob:|Run Number:|Unit:|/Addr:)");
+  private static final Pattern MISSING_COLON_PTN = Pattern.compile("(<=/City)(?!:)");
   private static final Pattern PREFIX_PTN = Pattern.compile("To - (\\w+)\\s+");
   private static final Pattern KEYWORD_DELIM = Pattern.compile("(?<=Location|APT/ROOM|City|Call Type|Line11|Units)[^*]");
-  
+
   @Override
   public boolean parseMsg(String subject, String body, Data data) {
-    
+
+    // Parse new page format
+    if (subject.equals("911 Paging")) {
+      int pt = body.indexOf("\n\n____");
+      if (pt >= 0) body = body.substring(0,pt).trim();
+      body = stripFieldStart(body, "Units:");
+      body = MISSING_BLANK_PTN.matcher(body).replaceAll(" ");
+      body = MISSING_COLON_PTN.matcher(body).replaceAll(":");
+      body = body.replace("Cross St:", "CrossSt:");
+      return parseMsg(body,  data);
+    }
+
+    // page old page format
     String[] parts = subject.split("\\|");
     subject = parts[parts.length-1];
     do {
       if (subject.equals("Paging")) break;
-      
+
       if (body.startsWith("Paging:")) {
         body = body.substring(7).trim();
         break;
       }
       if (subject.endsWith("PageGate")) break;
-      
+
       Matcher match = PREFIX_PTN.matcher(body);
       if (match.lookingAt()) {
         data.strSource = match.group(1);
         body = body.substring(match.end()).replace("\n", "");
         break;
       }
-      
+
       if (body.startsWith("City=")) break;
-      
+
       return false;
     } while (false);
-    
+
     body = body.replaceAll("\n", "");
     body = KEYWORD_DELIM.matcher(body).replaceAll(":");
     return super.parseFields(body.split("\\*"), data);
   }
-  
+
   @Override
   public String getProgram() {
     return "SRC " + super.getProgram();
+  }
+
+  private static final DateFormat DATE_TIME_FMT = new SimpleDateFormat("MMMdd,hh:mmaa");
+
+  @Override
+  public Field getField(String name) {
+    if (name.equals("X")) return new MyCrossField();
+    if (name.equals("TIMES")) return new MyTimesField();
+    if (name.equals("DATETIME1")) return new DateTimeField(DATE_TIME_FMT, false);
+    return super.getField(name);
+  }
+
+  private class MyCrossField extends CrossField {
+    @Override
+    public void parse(String field, Data data) {
+      field = field.replace("Not Found", "");
+      field = stripFieldStart(field, "/");
+      field = stripFieldEnd(field, "/");
+      super.parse(field, data);
+    }
+  }
+
+  private static final Pattern TIMES_BRK_PTN = Pattern.compile("(?<=\\d\\d:\\d\\d:\\d\\d)\\s*|\\s+");
+  private class MyTimesField extends InfoField {
+    @Override
+    public void parse(String field, Data data) {
+      data.msgType = MsgType.RUN_REPORT;
+      field = getRelativeField(0);
+      field = TIMES_BRK_PTN.matcher(field).replaceAll("\n");
+      super.parse(field, data);
+    }
   }
 }
