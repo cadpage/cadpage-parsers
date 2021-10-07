8d866c76dbb330054d125c607b1eace0b38a588e
Kenneth Corbin <ken@cadpage.org>
2021-10-07 16:41:20 -0700

    Update sender filter for Claiborne County, TN

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/TN/TNClaiborneCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/TN/TNClaiborneCountyParser.java
@@ -3,14 +3,14 @@ package net.anei.cadpage.parsers.TN;
 import net.anei.cadpage.parsers.dispatch.DispatchA74Parser;
 
 public class TNClaiborneCountyParser extends DispatchA74Parser {
-  
+
   public TNClaiborneCountyParser() {
     super("CLAIBORNE COUNTY", "TN");
   }
-  
+
   @Override
   public String getFilter() {
-    return "dispatch@911comm1.info";
+    return "dispatch@911comm1.info,Dispatch@ClaiborneTNE911.net";
   }
 
   @Override
