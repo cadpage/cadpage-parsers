457b663c12f93ddf3a7cdc0044cc433f12e6c912
Kenneth Corbin <ken@cadpage.org>
2021-10-15 20:23:42 -0700

    Fixed Parsing problem with Lake County, OH

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/OH/OHLakeCountyAParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/OH/OHLakeCountyAParser.java
@@ -8,17 +8,17 @@ import net.anei.cadpage.parsers.MsgInfo.Data;
 
 
 public class OHLakeCountyAParser extends FieldProgramParser {
-  
+
   public OHLakeCountyAParser() {
     super(CITY_CODES, "LAKE COUNTY", "OH",
-           "Location:ADDR/S? APT:APT? Cross_Streets:X! TYPE_CODE:CALL! Sub_Type:CALL! TIME:TIME! Comments:INFO");
+           "Location:ADDR/S? APT:APT? Cross_Streets:X! ( Type_Code:CALL! | TYPE_CODE:CALL! ) Sub_Type:CALL! ( Time:TIME! | TIME:TIME! ) Comments:INFO");
   }
-  
+
   @Override
   public String getFilter() {
     return "ingr.com@archwireless.net,ingr.com@usamobility.net";
   }
-  
+
   @Override
   protected boolean parseMsg(String body, Data data) {
     if (!super.parseMsg(body, data)) return false;
@@ -32,34 +32,33 @@ public class OHLakeCountyAParser extends FieldProgramParser {
     }
     return true;
   }
-  
+
   @Override
   public String getProgram() {
     return "PLACE " + super.getProgram();
   }
-  
-  private class MyCallField extends CallField {
-    @Override
-    public void parse(String field, Data data) {
-      data.strCall = append(data.strCall, " - ", field);
-    }
-  }
-  
-  private class MyCrossField extends CrossField {
-    @Override
-    public void parse(String field, Data data) {
-      parseAddress(StartType.START_ADDR, FLAG_ONLY_CROSS | FLAG_IMPLIED_INTERSECT | FLAG_ANCHOR_END, field, data);
-    }
-  }
-  
+
   @Override
   public Field getField(String name) {
     if (name.equals("CALL")) return new MyCallField();
     if (name.equals("X")) return new MyCrossField();
     return super.getField(name);
   }
-  
-  
+
+  private class MyCallField extends CallField {
+    @Override
+    public void parse(String field, Data data) {
+      data.strCall = append(data.strCall, " - ", field);
+    }
+  }
+
+  private class MyCrossField extends CrossField {
+    @Override
+    public void parse(String field, Data data) {
+      parseAddress(StartType.START_ADDR, FLAG_ONLY_CROSS | FLAG_IMPLIED_INTERSECT | FLAG_ANCHOR_END, field, data);
+    }
+  }
+
   private static final Properties CITY_CODES = buildCodeTable(new String[]{
       "CO", "CONCORD TWP",
       "FH", "FAIRPORT HARBOR",
