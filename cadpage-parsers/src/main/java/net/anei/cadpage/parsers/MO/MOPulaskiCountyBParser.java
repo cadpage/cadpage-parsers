ccf1ee741a17a8355c4f51ec13db2d3cc3a85c9f
Kenneth Corbin <ken@cadpage.org>
2021-10-27 20:23:20 -0700

    Update sender filter for Pulaski County, MO (B)

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/MO/MOPulaskiCountyBParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/MO/MOPulaskiCountyBParser.java
@@ -10,21 +10,21 @@ import net.anei.cadpage.parsers.dispatch.DispatchA19Parser;
  * Pulaski County, MO
  */
 public class MOPulaskiCountyBParser extends DispatchA19Parser {
-  
+
   public MOPulaskiCountyBParser() {
     super("PULASKI COUNTY", "MO");
   }
-  
+
   @Override
   public String getFilter() {
-    return "DISPATCH,messaging@iamresponding.com";
+    return "DISPATCH,messaging@iamresponding.com,911dispatch@embargmail.com";
   }
-  
+
   private static final Pattern MISSING_CITY_PTN = Pattern.compile("\nAddress: (.*?) , (.*)\n");
 
   @Override
   protected boolean parseMsg(String subject, String body, Data data) {
-    
+
     // Fix IAR edits
     if (subject.equals("Waynesville Fire")) {
       if (!body.contains("LONG TERM CAD#")) {
@@ -34,10 +34,10 @@ public class MOPulaskiCountyBParser extends DispatchA19Parser {
         body = MISSING_CITY_PTN.matcher(body).replaceFirst("\nAddress: $1 Zone: City:$2\n");
       }
     }
-    
+
     if (!super.parseMsg(subject, body, data)) return false;
     data.strCallId = stripFieldEnd(data.strCallId, "/X/X");
     return true;
   }
-  
+
 }
