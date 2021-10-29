df95eb3e8e4899221cbd3bbb142e578c16acf3cb
Kenneth Corbin <ken@cadpage.org>
2021-10-29 15:19:54 -0700

    Update msg doc

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/VA/VAGoochlandCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/VA/VAGoochlandCountyParser.java
@@ -6,12 +6,12 @@ import net.anei.cadpage.parsers.dispatch.DispatchOSSIParser;
 
 
 public class VAGoochlandCountyParser extends DispatchOSSIParser {
-  
+
   public VAGoochlandCountyParser() {
     super("GOOCHLAND COUNTY", "VA",
            "( CANCEL ADDR | FYI? CALL ADDR! X X ) INFO+");
   }
-  
+
   @Override
   public String adjustMapAddress(String addr) {
     addr = COTTONTAIL_CHSE_PTN.matcher(addr).replaceAll("COTTONTAIL CHASE RD");
