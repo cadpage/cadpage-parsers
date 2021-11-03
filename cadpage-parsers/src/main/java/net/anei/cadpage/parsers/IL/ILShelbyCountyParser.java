33234d04a67dd006d82e75f0219aab784479165a
Kenneth Corbin <ken@cadpage.org>
2021-11-03 08:09:49 -0700

    Fixed parsing problem with Shelby County, IL

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/IL/ILShelbyCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/IL/ILShelbyCountyParser.java
@@ -1,115 +1,59 @@
 package net.anei.cadpage.parsers.IL;
 
-import net.anei.cadpage.parsers.MsgInfo.Data;
-import net.anei.cadpage.parsers.dispatch.DispatchEmergitechParser;
+import java.util.Properties;
+
+import net.anei.cadpage.parsers.dispatch.DispatchA19Parser;
 
 /**
  * Christian County, IL
  */
-public class ILShelbyCountyParser extends DispatchEmergitechParser {
-  
+public class ILShelbyCountyParser extends DispatchA19Parser {
+
   public ILShelbyCountyParser() {
-    super("ChristianCounty911:", true, CITY_LIST, "SHELBY COUNTY", "IL", TrailAddrType.PLACE);
+    super(CITY_CODES, "SHELBY COUNTY", "IL");
   }
-  
+
   @Override
-  public Field getField(String name) {
-    if (name.equals("ADDR2")) return new MyAddressField();
-    return super.getField(name);
-  }
-  
-  private class MyAddressField extends BaseAddressField {
-    @Override
-    public void parse(String field, Data data) {
-      field = field.replace('@', '&');
-      super.parse(field, data);
-    }
+  public String getFilter() {
+    return "flex@christiancountysheriff.com";
   }
 
-  private static final String[] CITY_LIST = new String[]{
-    
-    // Cities
-    "SHELBYVILLE",
-    "WINDSOR",
+  private static final Properties CITY_CODES = buildCodeTable(new String[] {
+      "ASU",  "ASSUMPTION",
+//      "BC",   "",
+      "BEE",  "BEECHER CITY",
+      "BET",  "BETHANY",
+      "BLU",  "BLUE MOUND",
+      "COW",  "COWDEN",
+      "FIN",  "FINDLAY",
+      "HER",  "HERRICK",
+      "LAK",  "LAKEWOOD",
+      "MOD",  "MODE",
+      "MOW",  "MOWEAQUA",
+      "OCO",  "OCONEE TWP",
+      "PAN",  "PANA",
+      "SHE",  "SHELBYVILLE",
+      "STE",  "STEWARDSON",
+      "STO",  "STONINGTON TWP",
+      "SUL",  "SULLIVAN",
+      "TAY",  "TAYLORVILLE",
+      "WIN",  "WINDSOR TWP"
+  });
 
-    // Town
-    "SIGEL",
+  /**
+1596 E 50 N
 
-    // Villages
-    "COWDEN",
-    "FINDLAY",
-    "HERRICK",
-    "MOWEAQUA",
-    "OCONEE",
-    "STEWARDSON",
-    "STRASBURG",
-    "TOWER HILL",
+CERRO GORDO
+BLANDINSVILLE
+HERRICK, SHELBY COUNTY
+EFFINGHAM, EFFINGHAM COUNTY
 
-    // Census-designated place
-    "WESTERVELT",
+1650 E 50TH RD
 
-    // Other unincorporated communities
-    "CLARKSBURG",
-    "DOLLVILLE",
-    "DUVALL",
-    "FANCHER",
-    "HENTON",
-    "HERBORN",
-    "HINTON",
-    "KINGMAN",
-    "LAKEWOOD",
-    "MIDDLESWORTH",
-    "MODE",
-    "RENNERVILLE",
-    "YANTISVILLE",
-
-    // Townships
-    "ASH GROVE",
-    "BIG SPRING",
-    "CLARKSBURG",
-    "COLD SPRING",
-    "DRY POINT",
-    "FLAT BRANCH",
-    "HERRICK",
-    "HOLLAND",
-    "LAKEWOOD",
-    "MOWEAQUA",
-    "OCONEE",
-    "OKAW",
-    "PENN",
-    "PICKAWAY",
-    "PRAIRIE",
-    "RICHLAND",
-    "RIDGE",
-    "ROSE",
-    "RURAL",
-    "SHELBYVILLE",
-    "SIGEL",
-    "TODDS POINT",
-    "TOWER HILL",
-    "WINDSOR",
-    
-    // Christian County
-    "ASSUMPTION",
-    "MOWEAQUA",
-    "PANA",
-    "PRAIRETON",
-    
-    // Coles County
-    "MATTOON",
-    "PARADISE",
-    
-    // Macon County
-    "MACON",
-    "SOUTH MACON",
-    "MILAM",
-    
-    // Moultrie County
-    "BETHANY",
-    "GAYS",
-    "LAKE SHELBYVILLE",
-    "MARROWBONE",
-    "SULLIVAN",
-    "WHITLEY"
-  };
+MENDOTA
+ARTHUR, DOUGLAS COUNTY
+SEYMOUR
+FARINA, FAYETTE/MARION COUNTY
+CONGERVILLE
+   */
 }
