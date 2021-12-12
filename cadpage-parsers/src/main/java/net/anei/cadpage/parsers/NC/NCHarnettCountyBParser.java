895d50e8372cbed9a33fb5b6bb95df5cc68a6ee2
Kenneth Corbin <ken@cadpage.org>
2021-12-12 12:08:18 -0800

    Fixed test problems

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/NC/NCHarnettCountyBParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/NC/NCHarnettCountyBParser.java
@@ -9,11 +9,11 @@ import net.anei.cadpage.parsers.dispatch.DispatchSouthernParser;
 
 
 public class NCHarnettCountyBParser extends DispatchSouthernParser {
-  
+
   private static final Pattern RUN_REPORT_PTN = Pattern.compile("\\b\\d{1,2}:\\d\\d:\\d\\d \\d\\d/\\d\\d/\\d\\d\\b|\\bGINO\\(EN\\)");
-  
+
   public NCHarnettCountyBParser() {
-    super(NCHarnettCountyParser.CITY_LIST, "HARNETT COUNTY", "NC", 
+    super(NCHarnettCountyParser.CITY_LIST, "HARNETT COUNTY", "NC",
            DSFLAG_OPT_DISPATCH_ID | DSFLAG_LEAD_PLACE | DSFLAG_CROSS_NAME_PHONE | DSFLAG_ID_OPTIONAL);
   }
 
@@ -21,14 +21,18 @@ public class NCHarnettCountyBParser extends DispatchSouthernParser {
   public String getFilter() {
     return "@dunn-nc.org";
   }
-  
+
   @Override
   public int getMapFlags() {
     return MAP_FLG_PREFER_GPS;
   }
-  
+
   @Override
   protected boolean parseMsg(String body, Data data) {
+
+    // Eliminate NCHarnettCountyD alerts
+    if (body.startsWith("AD:") || body.startsWith("PN:")) return false;
+
     if (RUN_REPORT_PTN.matcher(body).find()) {
       data.strCall = "RUN REPORT";
       data.strPlace = body;
@@ -36,7 +40,7 @@ public class NCHarnettCountyBParser extends DispatchSouthernParser {
     }
     return super.parseMsg(body, data);
   }
-  
+
   @Override
   public String adjustMapAddress(String address) {
     address = address.replace("THREE BRIDGE RD", "3 BRIDGE RD");
