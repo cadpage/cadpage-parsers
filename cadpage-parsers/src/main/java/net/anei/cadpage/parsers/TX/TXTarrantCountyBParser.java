2bdde1a21fa6664e3bda5f71648ead869bbc44f1
Kenneth Corbin <ken@cadpage.org>
2021-12-21 17:49:37 -0800

    Update sender filter for Tarrrant County, TX

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/TX/TXTarrantCountyBParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/TX/TXTarrantCountyBParser.java
@@ -5,16 +5,16 @@ import net.anei.cadpage.parsers.dispatch.DispatchA19Parser;
  * TARRANT County, TX (B)
  */
 public class TXTarrantCountyBParser extends DispatchA19Parser {
-  
+
   public TXTarrantCountyBParser() {
     super("TARRANT COUNTY", "TX");
   }
-  
+
   @Override
   public String getFilter() {
-    return "dispatch@nrhfd.com,DISPATCH";
+    return "dispatch@nrhfd.com,DISPATCH,flexripnrun@nrhtx.com";
   }
-  
+
   @Override
   public int getMapFlags() {
     return MAP_FLG_SUPPR_LA | MAP_FLG_PREFER_GPS;
