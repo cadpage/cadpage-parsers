bf7fbd78d5fd216aeb03ca1d42d293497ed1edb8
Kenneth Corbin <ken@cadpage.org>
2021-12-30 12:58:51 -0800

    Fixed parsing problem with King County, WA (A)

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/WA/WAKingCountyAParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/WA/WAKingCountyAParser.java
@@ -3,28 +3,40 @@ package net.anei.cadpage.parsers.WA;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import net.anei.cadpage.parsers.CodeSet;
 import net.anei.cadpage.parsers.MsgInfo.Data;
-import net.anei.cadpage.parsers.MsgParser;
+import net.anei.cadpage.parsers.SmartAddressParser;
+import net.anei.cadpage.parsers.SplitMsgOptions;
+import net.anei.cadpage.parsers.SplitMsgOptionsCustom;
 
 
 
-public class WAKingCountyAParser extends MsgParser {
+public class WAKingCountyAParser extends SmartAddressParser {
 
-  private static final Pattern GPS_PTN = Pattern.compile("#LAT:(\\d{2})(\\d{6}) +#LON:(\\d{3})(\\d{6})(?: +(\\d+))?$");
+  private static final Pattern GPS_PTN = Pattern.compile("#LAT:?(?:(\\d{2})(\\d{6})|0) +#LON:?(?:(\\d{3})(\\d{6})|0)(?: +(\\d+))?$");
 
   public WAKingCountyAParser() {
-    super("KING COUNTY", "WA");
-    setFieldList("ADDR APT CITY CALL CH UNIT GPS");
+    super(CITY_LIST, "KING COUNTY", "WA");
+    setFieldList("INFO ADDR APT CITY CALL CH UNIT GPS");
   }
 
   @Override
   public String getFilter() {
-    return "CAD@bellevuewa.gov,VisiCAD@norcom.org";
+    return "CAD@bellevuewa.gov,VisiCAD@norcom.org,NoReply@snosafe.org";
   }
 
   @Override
   public int getMapFlags() {
     return MAP_FLG_PREFER_GPS;
+  }
+
+  @Override
+  public SplitMsgOptions getActive911SplitMsgOptions() {
+    return new SplitMsgOptionsCustom() {
+      @Override public boolean splitBlankIns() { return false; }
+      @Override public int splitBreakLength() { return 255; }
+      @Override public int splitBreakPad() { return 4; }
+    };
   }
 
   @Override
@@ -35,7 +47,9 @@ public class WAKingCountyAParser extends MsgParser {
     String gpsInfo = null;
     Matcher match = GPS_PTN.matcher(body);
     if (match.find()) {
-      gpsInfo = '+' + match.group(1) + '.' + match.group(2) + ",-" + match.group(3) + '.' + match.group(4);
+      if (match.group(1) != null && match.group(3)!=null) {
+        gpsInfo = '+' + match.group(1) + '.' + match.group(2) + ",-" + match.group(3) + '.' + match.group(4);
+      }
       body = body.substring(0,match.start()).trim();
       shortForm = match.group(5) != null;
     }
@@ -96,15 +110,212 @@ public class WAKingCountyAParser extends MsgParser {
     return true;
   }
 
-  private static final Pattern COMP_MASTER = Pattern.compile("([^#]+) # (.*?) (FT[A-Z0-9]+) ([A-Z0-9,]+)");
+  private static final Pattern COMP_MASTER = Pattern.compile("([^#]+) #(\\S*) (.*)");
+  private static final Pattern TRAIL_MASTER = Pattern.compile("(.*?) (?:#?((?:FT|SP)[A-Z0-9]+)|#) ([A-Z0-9, ]+)");
+
   private boolean parseCompressedMsg(String body, Data data) {
     if (body.startsWith("CAD||")) return false;
+
+    body = body.replace("CALLBK=", " ");
+
     Matcher match = COMP_MASTER.matcher(body);
     if (!match.matches()) return false;
-    parseAddress(match.group(1).trim(), data);
-    data.strCall = match.group(2).trim();
-    data.strChannel = match.group(3);
-    data.strUnit = match.group(4).trim();
-    return true;
+    String addr = match.group(1).trim();
+    if (addr.startsWith("Comment:")) {
+      int pt = addr.lastIndexOf(',');
+      if (pt < 0) return false;
+      data.strSupp = addr.substring(8, pt).trim();
+      addr = addr.substring(pt+1).trim();
+    } else {
+      addr = stripFieldStart(addr, "ADDRESS CHANGE");
+    }
+    parseAddress(addr, data);
+    data.strApt = append(data.strApt, "-", match.group(2));
+    String tail = match.group(3);
+    parseAddress(StartType.START_ADDR, FLAG_ONLY_CITY, tail, data);
+    tail = getLeft();
+
+    match = TRAIL_MASTER.matcher(tail);
+    if (match.matches()) {
+      data.strCall = match.group(1).trim();
+      data.strChannel = getOptGroup(match.group(2));
+      data.strUnit = match.group(3).trim().replace(' ', '_');
+      return true;
+    }
+
+    String call = CALL_LIST.getCode(tail);
+    if (call != null) {
+      data.strCall = call;
+      data.strUnit = tail.substring(call.length()).trim();
+      return true;
+    }
+
+    return false;
   }
+
+  @Override
+  public CodeSet getCallList() {
+    return CALL_LIST;
+  }
+
+  private static final CodeSet CALL_LIST = new CodeSet(
+      "AFA - Commercial",
+      "AFA - Multi-Family",
+      "AFA - Reset",
+      "AFA - Residential",
+      "AFA - Trouble",
+      "AFA Water Flow - Commercial",
+      "AFA Water Flow - Multi-Family",
+      "Aid - Emergency",
+      "Aid - Emergency CVA Protocol",
+      "Aid - Emergency - DOA",
+      "Aid - Emergency Weapons",
+      "Aid - Non-Emergency",
+      "Appliance - Fire",
+      "Cardiac Arrest",
+      "Electrical - Odor",
+      "Flooding - Minor",
+      "Haz",
+      "Medic",
+      "Medic - CVA Protocol",
+      "Medic - Weapons",
+      "Mutual Aid Request",
+      "MVA - Aid Emergency",
+      "MVA - Medic",
+      "MVA - Rescue",
+      "Natural Gas - Major",
+      "Natural Gas - Odor",
+      "Rescue - Surface Water",
+      "Service Call - Fire",
+      "Smoke - Haze In The Area",
+      "Smoke - Residence",
+      "Smoke - Smell",
+      "Still Alarm",
+      "Structure Fire - Commercial",
+      "Structure Fire - Outbuilding",
+      "Structure Fire - Residential",
+      "Structure Fire - Unconfirmed",
+      "Test Call",
+      "UPGRADE",
+      "Vehicle - Fire",
+      "Wires Air - Flames Seen",
+      "Wires Ground - Fire/Arc/Spark",
+      "Working Fire - Multi/Fam",
+      "Working Fire - Residential"
+  );
+
+  private static final String[] CITY_LIST = new String[] {
+
+      // Counties
+      "KING COUNTY",
+      "SNOHOMISH COUNTY",
+
+      // Cities
+      "ALGONA",
+      "AUBURN",
+      "BELLEVUE",
+      "BLACK DIAMOND",
+      "BOTHELL",
+      "BURIEN",
+      "CARNATION",
+      "CLYDE HILL",
+      "COVINGTON",
+      "DES MOINES",
+      "DUVALL",
+      "ENUMCLAW",
+      "FEDERAL WAY",
+      "ISSAQUAH",
+      "KENMORE",
+      "KENT",
+      "KIRKLAND",
+      "LAKE FOREST PARK",
+      "MAPLE VALLEY",
+      "MEDINA",
+      "MERCER ISLAND",
+      "MILTON",
+      "NEWCASTLE",
+      "NORMANDY PARK",
+      "NORTH BEND",
+      "PACIFIC",
+      "REDMOND",
+      "RENTON",
+      "SAMMAMISH",
+      "SEATAC",
+      "SEATTLE",
+      "SHORELINE",
+      "SNOQUALMIE",
+      "TUKWILA",
+      "WOODINVILLE",
+      "TOWNS",
+      "BEAUX ARTS VILLAGE",
+      "HUNTS POINT",
+      "SKYKOMISH",
+      "YARROW POINT",
+
+      // Census-designated places
+      "AMES LAKE",
+      "BARING",
+      "BOULEVARD PARK",
+      "BRYN MAWR-SKYWAY",
+      "COTTAGE LAKE",
+      "EAST RENTON HIGHLANDS",
+      "FAIRWOOD",
+      "FALL CITY",
+      "HOBART",
+      "INGLEWOOD-FINN HILL",
+      "KLAHANIE",
+      "LAKE HOLM",
+      "LAKE MARCEL-STILLWATER",
+      "LAKE MORTON-BERRYDALE",
+      "LAKELAND NORTH",
+      "LAKELAND SOUTH",
+      "MAPLE HEIGHTS-LAKE DESIRE",
+      "MIRRORMONT",
+      "RAVENSDALE",
+      "RIVERBEND",
+      "RIVERTON",
+      "SHADOW LAKE",
+      "TANNER",
+      "UNION HILL-NOVELTY HILL",
+      "VASHON",
+      "WESTWOOD",
+      "WHITE CENTER",
+      "WILDERNESS RIM",
+
+      // Other unincorporated communities
+      "CEDAR FALLS",
+      "CUMBERLAND",
+      "DENNY CREEK",
+      "ERNIES GROVE",
+      "GROTTO",
+      "KANASKAT",
+      "KANGLEY",
+      "LAKE JOY",
+      "NACO",
+      "NOVELTY",
+      "PALMER",
+      "PRESTON",
+      "SELLECK",
+      "SPRING GLEN",
+      "WABASH",
+
+      // Former cities and towns
+      "EAST REDMOND",
+      "FOSTER",
+      "HOUGHTON",
+      "GHOST TOWNS",
+      "BAYNE",
+      "CEDAR FALLS",
+      "EDGEWICK",
+      "FRANKLIN",
+      "HOT SPRINGS",
+      "KRAIN",
+      "LESTER",
+      "MONOHON",
+      "NAGROM",
+      "OSCEOLA",
+      "TAYLOR",
+      "WELLINGTON",
+      "WESTON"
+  };
 }
