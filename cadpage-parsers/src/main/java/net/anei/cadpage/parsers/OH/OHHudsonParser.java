4314e4838dff2a51876ddde135034c865a808b05
Kenneth Corbin <ken@cadpage.org>
2022-01-15 15:16:00 -0800

    Fixed parsing problem with Hudson, OH

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/OH/OHHudsonParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/OH/OHHudsonParser.java
@@ -6,122 +6,28 @@ import java.util.regex.Pattern;
 import net.anei.cadpage.parsers.CodeSet;
 import net.anei.cadpage.parsers.FieldProgramParser;
 import net.anei.cadpage.parsers.MsgInfo.Data;
+import net.anei.cadpage.parsers.dispatch.DispatchA71Parser;
 
 
 
 public class OHHudsonParser extends FieldProgramParser {
-  
+
   public OHHudsonParser() {
     super("HUDSON", "OH",
-           "NUM ADDR PLACE SRC! INFO/CS+");
+          "CALL CALL:CALL/SDS PLACE:PLACE! ADDR:ADDR! CITY:CITY! ID:ID UNIT:UNIT INFO:INFO XSTREET:X WS:MAP END");
   }
-  
+
+  private static final Pattern DELIM = Pattern.compile("(?!\n|$)(?=CALL:)|\n");
+
   @Override
   public String getFilter() {
-    return "@hudson.oh.us";
-  }
-  
-  private static final Pattern MARKER = Pattern.compile("HudsonCAD: ?CAD System +");
-  
-  @Override
-  public boolean parseMsg(String body, Data data) {
-    Matcher match = MARKER.matcher(body);
-    if (!match.lookingAt()) return false;
-    body = body.substring(match.end());
-    return parseFields(body.split(","), data);
+    return "sunsrv@sundance-sys.com,info@sundance-sys.com";
   }
 
   @Override
-  protected Field getField(String name) {
-    if (name.equals("NUM")) return new NumberField();
-    if (name.equals("ADDR")) return new MyAddressField();
-    if (name.equals("PLACE")) return new MyPlaceField();
-    if (name.equals("SRC")) return new SourceField("(?:HUDSON|Hudson|BOSTON HEIGHTS)? ?(?:EMS|FIRE|F?\\d+[A-Z]?)|HUDSON[A-Z]*", true);
-    if (name.equals("INFO")) return new MyInfoField();
-    return super.getField(name);
+  public boolean parseMsg(String subject, String body, Data data) {
+    if (!subject.equals("From: BHDispMap")) return false;
+    if (! parseFields(DELIM.split(body), data)) return false;
+    return !data.strCall.isEmpty();
   }
-  
-  private class NumberField extends Field {
-    @Override
-    public void parse(String field, Data data) {
-      if (!field.equals("0")) data.strAddress = field;
-    }
-    
-    @Override
-    public String getFieldNames() {
-      return "ADDR";
-    }
-  }
-  
-  private class MyAddressField extends AddressField {
-    @Override
-    public void parse(String field, Data data) {
-      field = append(data.strAddress, " ", field);
-      data.strAddress = "";
-      field = field.replace(".", "");
-      parseAddress(field, data);
-    }
-  }
-  
-  private class MyPlaceField extends PlaceField {
-    @Override
-    public void parse(String field, Data data) {
-      if (field.equals(getRelativeField(-1))) return;
-      super.parse(field, data);
-    }
-  }
-  
-  private static Pattern MADDR_PTN  = Pattern.compile("\\d+ +.*");
-  private static Pattern MUT_AID_PTN = Pattern.compile(" TO (.*) FOR ");
-  private class MyInfoField extends InfoField {
-    
-    @Override
-    public void parse(String field, Data data) {
-      if (data.strCall.length() == 0) {
-        if (field.startsWith("F-")) {
-          data.strCall = field.substring(2).trim();
-          return;
-        } else {
-          String call = CALL_LIST.getCode(field);
-          if (call != null) {
-            data.strCall = call;
-            field = field.substring(call.length()).trim();
-            field = stripFieldStart(field, ":");
-          }
-          else {
-            data.strCall = field;
-            return;
-          }
-        }
-      }
-      
-      if (data.strAddress.equals("ANYWHERE")) {
-        if (MADDR_PTN.matcher(field).matches() || isValidAddress(field)) {
-          data.strAddress = "";
-          parseAddress(field, data);
-          return;
-        }
-      }
-
-      Matcher match = MUT_AID_PTN.matcher(field);
-      if (match.find()) data.strCity = match.group(1);
-
-      super.parse(field, data);
-    }
-    
-    @Override
-    public String getFieldNames() {
-      return "CALL INFO ADDR CITY";
-    }
-  }
-  
-  private static final CodeSet CALL_LIST = new CodeSet(
-      "FIRE",
-      "EMS",
-      "Crash",
-      "Fire Alarm",
-      "Fire Alarm: Residential",
-      "Lockout: Motor Vehicle",
-      "Natural Disaster / Tornado / Earthquake"
-   );
 }
