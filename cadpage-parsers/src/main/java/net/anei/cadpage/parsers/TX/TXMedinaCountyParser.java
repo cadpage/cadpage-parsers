dc1859909a67cb28a6d41a8557c02f67f17eebde
Kenneth Corbin <ken@cadpage.org>
2022-01-24 10:24:49 -0800

    Update sender filter for Medina County, TX

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/TX/TXMedinaCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/TX/TXMedinaCountyParser.java
@@ -3,14 +3,13 @@ package net.anei.cadpage.parsers.TX;
 import net.anei.cadpage.parsers.dispatch.DispatchA72Parser;
 
 public class TXMedinaCountyParser extends DispatchA72Parser {
-  
+
   public TXMedinaCountyParser() {
     super("MEDINA COUNTY", "TX");
   }
-  
+
   @Override
   public String getFilter() {
-    return "active911@medinacountytexas.org";
+    return "active911@medinacountytexas.org,medinacodispatch@gmail.com";
   }
-
 }
