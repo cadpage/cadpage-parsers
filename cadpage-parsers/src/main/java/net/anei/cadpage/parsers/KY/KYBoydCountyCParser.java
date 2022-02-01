1c1671fb742a2e48c29bd79a071d2e759e7abbc9
Kenneth Corbin <ken@cadpage.org>
2022-01-31 18:53:03 -0800

    Update sender filter for Boyd County, KY (C)

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/KY/KYBoydCountyCParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/KY/KYBoydCountyCParser.java
@@ -10,7 +10,7 @@ public class KYBoydCountyCParser extends DispatchA27Parser {
 
   @Override
   public String getFilter() {
-    return "noreply@cisusa.org,cadexport@boydcounty911.com,cadexport@windstream.net";
+    return "noreply@cisusa.org,cadexport@boydcounty911.com,cadexport@windstream.net,support@cis.com";
   }
 
   @Override
