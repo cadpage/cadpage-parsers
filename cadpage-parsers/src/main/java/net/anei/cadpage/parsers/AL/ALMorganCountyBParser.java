3e9667042e62d045255aa650ffc88005ad74d200
Kenneth Corbin <ken@cadpage.org>
2022-02-02 21:02:57 -0800

    Fixed parsing problem with Morgan County, AL (B)

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/AL/ALMorganCountyBParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/AL/ALMorganCountyBParser.java
@@ -12,7 +12,7 @@ public class ALMorganCountyBParser extends FieldProgramParser {
 
   public ALMorganCountyBParser() {
     super(CITY_LIST, "MORGAN COUNTY", "AL",
-          "Location:ADDR/S! Loc_Name:PLACE! Tac:SRC_CH! SRC_CH+? Assigned:UNIT! detail:INFO! INFO/N+");
+          "( SELECT/1 CALL! Location:ADDRCITYST! | Location:ADDR/S! ) Desc:DESC? Loc_Name:PLACE! TAC:SRC_CH! SRC_CH+? Assigned:DATETIME! Detail:INFO! INFO/N+");
   }
 
   @Override
@@ -20,8 +20,8 @@ public class ALMorganCountyBParser extends FieldProgramParser {
     return "cad-no-reply@morgan911.org";
   }
 
-  private static final Pattern MASTER = Pattern.compile("([^;\n]+?) (?:([A-Z][a-z][ A-Za-z]*(?: V?FD)?|MCSO) - (TAC\\d+)|None)\\b(.*)");
-  private static final Pattern INFO_BRK_PTN = Pattern.compile("(?:^|; *)\\d\\d/\\d\\d/\\d\\d \\d\\d:\\d\\d:\\d\\d - +");
+  private static final Pattern SUBJECT_PTN = Pattern.compile("CFS - Unit Assigned - #(.*)");
+  private static final Pattern DELIM_PTN1 = Pattern.compile(";?\n");
 
   private Set<String> channelSet = new HashSet<>();
 
@@ -29,6 +29,15 @@ public class ALMorganCountyBParser extends FieldProgramParser {
   protected boolean parseMsg(String subject, String body, Data data) {
 
     if (subject.isEmpty()) return false;
+
+    channelSet.clear();
+
+    Matcher match = SUBJECT_PTN.matcher(subject);
+    if (match.matches()) {
+      data.strCallId = match.group(1).trim();
+      setSelectValue("1");
+      return parseFields(DELIM_PTN1.split(body), data);
+    }
     int pt = subject.indexOf(';');
     if (pt >= 0) subject = subject.substring(0,pt).trim();
     subject = stripFieldEnd(subject, " None");
@@ -36,36 +45,62 @@ public class ALMorganCountyBParser extends FieldProgramParser {
 
     if (body.startsWith("Location:")) {
       body = stripFieldEnd(body, ";");
-      channelSet.clear();
+      setSelectValue("2");
+      body = body.replace("Tac:", "TAC:").replace("detail:", "Detail:");
       return parseFields(body.split("; "), data);
     }
 
-    else {
-      Matcher match = MASTER.matcher(body);
-      if (!match.matches()) return false;
-
-      setFieldList("ADDR APT CITY SRC CH INFO");
-      parseAddress(StartType.START_ADDR, FLAG_ANCHOR_END, match.group(1).trim(), data);
-      data.strSource = getOptGroup(match.group(2));
-      data.strChannel = getOptGroup(match.group(3));
-      String info = match.group(4).trim();
-      if (!info.equals("None")) {
-        data.strSupp = INFO_BRK_PTN.matcher(info).replaceAll("\n").trim();
-      }
-      return true;
-    }
+    return false;
   }
 
   @Override
   public String getProgram() {
-    return "CALL " + super.getProgram();
+    return "ID CALL " + super.getProgram();
   }
 
   @Override
   public Field getField(String name) {
+    if (name.equals("CALL")) return new MyCallField();
+    if (name.equals("DESC")) return new MyDescField();
     if (name.equals("SRC_CH")) return new MySourceChannelField();
+    if (name.equals("DATETIME")) return new DateTimeField("(\\d\\d/\\d\\d/\\d\\d +\\d\\d:\\d\\d:\\d\\d)\\b[ *]*", true);
     if (name.equals("INFO")) return new MyInfoField();
     return super.getField(name);
+  }
+
+  private class MyCallField extends CallField {
+    @Override
+    public void parse(String field, Data data) {
+      int pt = field.indexOf(';');
+      if (pt >= 0) field = field.substring(0,pt).trim();
+      super.parse(field, data);
+    }
+  }
+
+  private static final Pattern APT_DESC_PTN = Pattern.compile("(?:APT|LOT|RM|ROOM)[. ]*(\\S+) *(.*)", Pattern.CASE_INSENSITIVE);
+  private static final Pattern APT_PTN = Pattern.compile("\\d+[A-Z]?|[A-Z]", Pattern.CASE_INSENSITIVE);
+  private class MyDescField extends Field {
+
+    @Override
+    public void parse(String field, Data data) {
+      if (field.equals("None")) return;
+
+      Matcher match = APT_DESC_PTN.matcher(field);
+      if (match.matches()) {
+        data.strApt = append(data.strApt, "-", match.group(1));
+        data.strPlace = match.group(2);
+      }
+      else if (APT_PTN.matcher(field).matches()) {
+        data.strApt = append(data.strApt, "-", field);
+      } else {
+        data.strPlace = field;
+      }
+    }
+
+    @Override
+    public String getFieldNames() {
+      return "APT PLACE";
+    }
   }
 
   private static final Pattern SRC_CHANNEL_PTN = Pattern.compile("(.*) - (TAC\\d+)");
@@ -78,6 +113,7 @@ public class ALMorganCountyBParser extends FieldProgramParser {
 
     @Override
     public boolean checkParse(String field, Data data) {
+      if (field.equals("None")) return true;
       Matcher match = SRC_CHANNEL_PTN.matcher(field);
       if (!match.matches()) return false;
       data.strSource = append(data.strSource, ",", match.group(1).trim());
@@ -97,14 +133,15 @@ public class ALMorganCountyBParser extends FieldProgramParser {
     }
   }
 
-  private static final Pattern INFO_PFX_PTN = Pattern.compile("(?:\\d\\d/\\d\\d/\\d\\d \\d\\d:\\d\\d:\\d\\d [-\\*] *)+");
+  private static final Pattern INFO_PFX_PTN = Pattern.compile("[; ]*\\b(?:\\d\\d/\\d\\d/\\d\\d \\d\\d:\\d\\d:\\d\\d [-\\*] *)+");
   private class MyInfoField extends InfoField {
     @Override
     public void parse(String field, Data data) {
-      Matcher match = INFO_PFX_PTN.matcher(field);
-      if (match.lookingAt()) field = field.substring(match.end());
       if (field.equals("None")) return;
-      super.parse(field, data);
+      for (String line : INFO_PFX_PTN.split(field)) {
+        line = line.trim();
+        data.strSupp = append(data.strSupp, "\n", line);
+      }
     }
   }
 
