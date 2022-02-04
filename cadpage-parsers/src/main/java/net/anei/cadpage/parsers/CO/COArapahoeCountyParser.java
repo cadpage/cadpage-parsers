7698f563eb558f5d132f163b9bb9f3bec646dd5a
Kenneth Corbin <ken@cadpage.org>
2022-02-03 19:48:14 -0800

    Fixed parsing problem with Arapahoe County, CO

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/CO/COArapahoeCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/CO/COArapahoeCountyParser.java
@@ -9,196 +9,167 @@ import net.anei.cadpage.parsers.MsgInfo.Data;
 import net.anei.cadpage.parsers.ReverseCodeSet;
 
 public class COArapahoeCountyParser extends FieldProgramParser {
-  
+
   public COArapahoeCountyParser() {
     super("ARAPAHOE COUNTY", "CO",
-           "HEAD? ID ( MAP ( GPS1 | ADDR/Z GPS1 ) | ADDR/Z? MAP GPS1 ) GPS2 DUP_ADDR? APT BLDG PLACE CALL UNIT! INFO+");
+          "( Resp.Info:MAP! ADDR ( GPS/d | GPS1/d GPS2/d ) APT EMPTY PLACE CALL ID! " +
+          "| Address_Changed:MAP! GPS/d ADDR2 PLACE CALL ID! " +
+          "| Incident_Location_Changed_to:EMPTY! ID3 MAP ADDR GPS1 GPS2 EMPTY EMPTY PLACE CALL UNIT! " +
+          "| Inc_Address_Update:ADDR! ID3 MAP GPS1 GPS2 EMPTY EMPTY PLACE " +
+          ") EMPTY? END");
     setupParseAddressFlags(FLAG_ALLOW_DUAL_DIRECTIONS);
     setupSpecialStreets("BROADWAY", "BROADWAY CIR");
   }
-  
+
   @Override
   public String getFilter() {
-    return "metcomdispatch@metcom911.org,SMTPRELAY@SMFRA.onmicrosoft.com";
+    return "smfrrelay@smfra.com";
   }
-  
+
   @Override
   public int getMapFlags() {
     return MAP_FLG_PREFER_GPS;
   }
-  
-  private static final Pattern MASTER = Pattern.compile("(?:(?:Comment: (.*) )?Info Only: )?(?:([A-Z]{1,2}-\\d{2}-[A-Z](?:-[A-Z])?) )?(.*?) *([A-Z][,A-Z0-9]+)");
-  
+
+  private static final Pattern PREFIX = Pattern.compile("(UNIVERSAL PRECAUTIONS(?:, SOB)?) *");
+  private static final Pattern MASTER1 = Pattern.compile("RI:([A-Z]-\\d{2}-[A-Z](?:-[A-Z])?) (.*?) +([A-Z][,A-Z0-9]+)");
+  private static final Pattern MASTER2 = Pattern.compile("([ A-Z]+) (?:-|RESPOND)(.*?)([A-Z]-\\d{2}-[A-Z](?:-[A-Z])?) (.*?)(?: Cmnd Chnl:(.*))?");
+  private static final Pattern MASTER3 = Pattern.compile("ADDRESS CHANGE *([A-Z]-\\d{2}-[A-Z](?:-[A-Z])?) (.*)");
+
   @Override
-  public boolean parseMsg(String body, Data data) {
-    
+  public boolean parseMsg(String subject, String body, Data data) {
+
+    if (!subject.equals("SMFR Dispatch Info:")) return false;
+
+    Matcher match = PREFIX.matcher(body);
+    String prefix = null;
+    if (match.lookingAt()) {
+      prefix = match.group(1);
+      body = body.substring(match.end());
+    }
+
     // We have two different page formats
     // Check for the pipe delimited field format
-    String[] flds = body.split("\\|");
-    if (flds.length >= 10) return parseFields(flds, data);
-    
-    // No go. Check for the undelimited field format
-    Matcher match = MASTER.matcher(body);
-    if (!match.matches()) return false;
-    
-    setFieldList("INFO MAP ADDR APT PLACE CALL UNIT");
-    data.strSupp = getOptGroup(match.group(1));
-    data.strMap = getOptGroup(match.group(2));
-    body = match.group(3).trim();
-    data.strUnit = match.group(4);
-    
-    String call = CALL_LIST.getCode(body, true);
-    if (call != null) body = body.substring(0,body.length()-call.length()).trim();
-    parseAddress(StartType.START_ADDR, FLAG_ALLOW_DUAL_DIRECTIONS, body, data);
-    if (call != null) {
-      data.strCall = call;
-      data.strPlace = getLeft();
-    } else {
-      data.strCall = getLeft();
-      if (data.strCall.length() == 0) return false;
+    String[] flds = body.split("\\|", -1);
+    if (flds.length >= 6) {
+      if (!parseFields(flds, data)) return false;
     }
+
+    // No go. Check for the undelimited field format
+    else if ((match = MASTER1.matcher(body)).matches()) {
+
+      setFieldList("MAP ADDR APT PLACE CALL UNIT");
+      data.strMap = match.group(1);
+      body = match.group(2).trim();
+      data.strUnit = match.group(3);
+
+      String call = CALL_LIST.getCode(body, true);
+      if (call != null) body = body.substring(0,body.length()-call.length()).trim();
+      parseAddress(StartType.START_ADDR, FLAG_ALLOW_DUAL_DIRECTIONS, body, data);
+      if (call != null) {
+        data.strCall = call;
+        data.strPlace = getLeft();
+      } else {
+        data.strCall = getLeft();
+        if (data.strCall.length() == 0) return false;
+      }
+    }
+
+    else if ((match = MASTER2.matcher(body)).matches()) {
+      setFieldList("UNIT CALL MAP ADDR APT CH");
+      data.strUnit = match.group(1).trim();
+      data.strCall = match.group(2).trim();
+      data.strMap = match.group(3);
+      parseAddress(match.group(4), data);
+      data.strChannel = getOptGroup(match.group(5));
+    }
+
+    else if ((match = MASTER3.matcher(body)).matches()) {
+      setFieldList("MAP ADDR APT CALL");
+      data.strMap = match.group(1);
+      body = match.group(2).trim();
+      String call = CALL_LIST.getCode(body);
+      if (call != null) {
+        data.strCall = call;
+        body = body.substring(0, body.length()-call.length()).trim();
+        parseAddress(body, data);
+      } else {
+        parseAddress(StartType.START_ADDR, body, data);
+        data.strCall = getLeft();
+        if (data.strCall.isEmpty()) return false;
+      }
+    }
+
+    else return false;
+
+    if (prefix != null) data.strCall = append(prefix, " - ", data.strCall);
     return true;
   }
-  
+
   @Override
   public Field getField(String name) {
-    if (name.equals("HEAD")) return new SkipField("Incident Location Changed to:?|Response Comments:");
-    if (name.equals("ID")) return new IdField("\\d{4}-\\d{7}", true);
-    if (name.equals("MAP")) return new MapField("[A-Z]{1,2}-\\d{2}-[A-Z](?:-[A-Z])?", true);
-    if (name.equals("GPS1")) return new MyGPSField(1);
-    if (name.equals("GPS2")) return new MyGPSField(2);
-    if (name.equals("DUP_ADDR")) return new MyDupAddrField();
-    if (name.equals("BLDG")) return new MyBuildingField();
-    if (name.equals("INFO")) return new MyInfoField();
+    if (name.equals("MAP")) return new MapField("[A-Z]-\\d{2}-[A-Z](?:-[A-Z])?", true);
+    if (name.equals("GPS")) return new GPSField("\\d{8,9} \\d{8,9}", true);
+    if (name.equals("ID")) return new IdField("(?:Case#)?(\\d\\d-[A-Z]{2}-\\d{6}|\\d\\d-\\d{7})", true);
+    if (name.equals("ID3")) return new IdField("\\d{4}-\\d{7}", true);
+    if (name.equals("ADDR2")) return new AddressField(".* TO: +(.*)");
     return super.getField(name);
   }
-  
-  private class MyGPSField extends GPSField {
-    
-    public MyGPSField(int type) {
-      super(type);
-    }
-    
-    @Override
-    public boolean canFail() {
-      return true;
-    }
-    
-    @Override
-    public boolean checkParse(String field, Data data) {
-      if (!NUMERIC.matcher(field).matches()) return false;
-      int pt = field.length() - 6;
-      if (pt < 2) return false;
-      field = field.substring(0,pt) + '.' + field.substring(pt);
-      super.parse(field, data);
-      return true;
-    }
-    
-    @Override
-    public void parse(String field, Data data) {
-      if (!checkParse(field, data)) abort();
-    }
-  }
-  
-  private class MyDupAddrField extends SkipField {
-    @Override
-    public boolean canFail() {
-      return true;
-    }
-    
-    @Override
-    public boolean checkParse(String field, Data data) {
-      if (data.strAddress.length() > 0) {
-        if (field.equals(data.strAddress)) return true;
-        if (field.equals(getRelativeField(-2))) return true;
-      }
-      return false;
-    }
-  }
-  
-  private class MyBuildingField extends AptField {
-    @Override
-    public void parse(String field, Data data) {
-      data.strApt = append(field, "-", data.strApt);
-    }
-  }
-  
-  private static final Pattern INFO_CASE_PTN = Pattern.compile(".*\\bAutomatic Case Number\\(s\\) ?issued for ([ \\w]+?): ([-A-Z0-9]+).");
-  private class MyInfoField extends InfoField {
-    @Override
-    public void parse(String field, Data data) {
-      String delim = "\n";
-      for (String part : field.split(",")) {
-        part = part.trim();
-        if (part.length() == 0) continue;
-        Matcher match = INFO_CASE_PTN.matcher(part);
-        if (match.matches()) {
-          String source = match.group(1).trim();
-          if (!data.strSource.contains(source)) {
-            data.strSource = append(data.strSource, ",", source);
-          }
-          String id = match.group(2);
-          if (!data.strCallId.contains(id)) {
-            data.strCallId = append(data.strCallId, "/", id);
-          }
-          continue;
-        }
-        data.strSupp = append(data.strSupp, delim, part);
-        delim = ", ";
-      }
-    }
-    
-    @Override
-    public String getFieldNames() {
-      return "INFO SRC ID";
-    }
-  }
-  
+
   @Override
   public CodeSet getCallList() {
     return CALL_LIST;
   }
 
   private static final ReverseCodeSet CALL_LIST = new ReverseCodeSet(
-      "Abdominal Pain/Problem",
+      "Abdominal Pain/Problems",
       "Air Alert 2 Inflight Emergency",
       "Alarm-CO No Sick Parties",
       "Alarm-Fire Alarm Commercial",
       "Alarm-Fire Alarm Residential",
       "Alarm-Medical Alarm",
       "Alcohol Evaluation",
+      "Allergies/Envenomation",
+      "Animal Bites/Attacks",
       "Assault/Sexual Assault",
-      "Assist - Lift Assist",
-      "Assist - Lock In (Child/Pet)",
-      "Assist - Lock Out",
-      "Assist - Other Agency Assist",
-      "Assist - Public Assist",
+      "Assist-Blood Draw",
+      "Assist-Lift Assist",
+      "Assist-Lock In (Child/Pet)",
+      "Assist-Lock Out",
+      "Assist-Other Agency Assist",
+      "Assist-Police Assist",
+      "Assist-Public Assist",
+      "Assist-Water Problem/Shut Off",
       "Back Pain (Non-Traumatic)",
       "Breathing Problems",
       "Cardiac or Respiratory Arrest",
       "Chest Pain (Non-Traumatic)",
       "Convulsions/Seizures",
+      "Diabetic Problems",
       "Driveway Eye Problems/ Injuries",
       "Electrical Hazard",
       "Falls",
-      "Fire - Appliance Fire",
-      "Fire - BBQ Grill Fire",
-      "Fire - Brush Fire Large",
-      "Fire - Brush Fire Small",
-      "Fire - Commercial Carrier Fire",
-      "Fire - Outside Fire",
+      "Fire-Appliance Fire",
+      "Fire-BBQ Grill Fire",
+      "Fire-Brush Fire Large",
+      "Fire-Brush Fire Small",
+      "Fire-Commercial Carrier Fire",
+      "Fire-Illegal Burn",
+      "Fire-Outside Fire",
+      "Fire-Vehicle Fire",
       "Fuel Spill Large",
       "Fuel Spill Small",
-      "Gas - Commercial Leak/Odor",
-      "Gas - Residential Leak/Odor",
+      "Gas-Commercial Leak",
+      "Gas-Residential Leak",
       "HazMat",
       "Heart Problems/ A.I.C.D",
       "Hemorrhage/Lacerations",
-      "Invest - Lighting Strike",
-      "Invest - Odor Commercial",
-      "Invest - Odor Outside",
-      "Invest - Smoke Inside",
-      "Invest - Smoke Outside",
+      "Invest-Lighting Strike",
+      "Invest-Odor Commercial",
+      "Invest-Odor Outside",
+      "Invest-Odor Residential",
+      "Invest-Smoke Inside",
+      "Invest-Smoke Outside",
       "Line Down / Transformer",
       "Medical Assist",
       "MVA Extrication",
@@ -209,18 +180,19 @@ public class COArapahoeCountyParser extends FieldProgramParser {
       "MVA Unknown Injuries",
       "MVA Vehicle Into Building",
       "Overdose/Poisoning (Ingestion)",
+      "Psych Problems",
       "Psych/Abn Behavior/Suicide Att",
-      "Resc - Confined Space Rescue",
-      "SF - Comm Str Fire Reported",
-      "SF - Multi-Fam Str Fire Report",
-      "SF - Outbuilding Fire",
-      "SF - Res Str Fire Reported",
-      "SF1C - Commercial Str Fire",
-      "Sick Person (Spec. Diagnosis)",
+      "Resc-Confined Space Rescue",
+      "SF-Comm Str Fire Reported",
+      "SF-Multi-Fam Str Fire Report",
+      "SF-Outbuilding Fire",
+      "SF-Res Structure Fire Reported",
+      "SF1C-Commercial Str Fire",
+      "Sick Person (Specific Diag)",
       "Stab/Gunshot/Penetrating Traum",
       "Standby In The Area",
       "Stroke(CVA)",
-      "Test Call (Do Not Dispatch )",
+      "Test Call (Do Not Dispatch)",
       "Traffic Pedestrian Acciden",
       "Traumatic Injuries (Specific)",
       "Unconscious/Fainting (Near)",
