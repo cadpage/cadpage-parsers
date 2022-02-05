31cf8bd613c8421bfd03ce31450ba9aad6622858
Kenneth Corbin <ken@cadpage.org>
2022-02-04 19:13:19 -0800

    Update A911 parser table

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/Active911ParserTable.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/Active911ParserTable.java
@@ -522,6 +522,7 @@ public class Active911ParserTable {
       "US/IL/NWCDS",                      "ILCookCountyD",
       "US/IL/OFallon",                    "ILOFallon",
       "US/IL/OrlandFire",                 "ILCookCountyG",
+      "US/IL/PekinFD",                    "ILTazewellCounty",
       "US/IL/PleasantviewFPD",            "ILCookCountyI",
       "US/IL/PrairieDuPont",              "ILStClairCountyA,ILStClairCountyC",
       "US/IL/Quadcom911",                 "ILKaneCountyD,ILKaneCountyE,ILKaneCountyF",
