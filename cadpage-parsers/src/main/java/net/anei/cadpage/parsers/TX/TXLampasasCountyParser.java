8cf110fd7c70ca9707e05f92b4c6eaf9e459e560
Kenneth Corbin <ken@cadpage.org>
2022-02-07 18:55:57 -0800

    Updated sender filter for Lampasas County, TX

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/TX/TXLampasasCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/TX/TXLampasasCountyParser.java
@@ -5,8 +5,13 @@ import net.anei.cadpage.parsers.dispatch.DispatchA19Parser;
  * Lampasas County, TX
  */
 public class TXLampasasCountyParser extends DispatchA19Parser {
-  
+
   public TXLampasasCountyParser() {
     super("LAMPASAS COUNTY", "TX");
+  }
+
+  @Override
+  public String getFilter() {
+    return "ripnrun@co.lampasas.tx.us";
   }
 }
