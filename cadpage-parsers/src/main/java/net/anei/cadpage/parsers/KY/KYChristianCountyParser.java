e8147557bef1fe8f17c885350e1d163bdffcca99
Kenneth Corbin <ken@cadpage.org>
2022-02-18 18:16:43 -0800

    Fixed parsing problem with Christian County, KY Fixed parsing problem with Mercer Couty, WV

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/KY/KYChristianCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/KY/KYChristianCountyParser.java
@@ -1,18 +1,43 @@
 package net.anei.cadpage.parsers.KY;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import net.anei.cadpage.parsers.MsgInfo.Data;
 import net.anei.cadpage.parsers.dispatch.DispatchA27Parser;
 
 /**
  * Christian County, KY
  */
 public class KYChristianCountyParser extends DispatchA27Parser {
-  
+
   public KYChristianCountyParser() {
     super("CHRISTIAN COUNTY", "KY", "[A-Z0-9]+|Jsmc|Fhfd");
   }
-  
+
   @Override
   public String getFilter() {
     return "noreply@cisusa.org,noreply@hopkinsvilleky.us,noreply@cis.com";
   }
+
+  @Override
+  public Field getField(String name) {
+    if (name.equals("ADDRCITY")) return new MyAddressField();
+    return super.getField(name);
+  }
+
+  private static final Pattern POUND_DIR_PTN = Pattern.compile("  *#([NSEW]B)\\b", Pattern.CASE_INSENSITIVE);
+
+  private class MyAddressField extends BaseAddressField {
+    @Override
+    public void parse(String field, Data data) {
+      Matcher match = POUND_DIR_PTN.matcher(field);
+      if (match.find()) {
+        String dir = ' ' + match.group(1);
+        field = stripFieldEnd(field.substring(0,match.start()), dir) + dir + field.substring(match.end());
+      }
+      super.parse(field, data);
+    }
+  }
+
 }
