cb811abb01d942936e3a74d2e831b0d68e0ff761
Kenneth Corbin <ken@cadpage.org>
2022-02-21 19:30:00 -0800

    Fixed parsing problem with Benton County, AR (B)

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/AR/ARBentonCountyBParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/AR/ARBentonCountyBParser.java
@@ -1,6 +1,5 @@
 package net.anei.cadpage.parsers.AR;
 
-import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import net.anei.cadpage.parsers.FieldProgramParser;
@@ -11,7 +10,12 @@ public class ARBentonCountyBParser extends FieldProgramParser {
 
   public ARBentonCountyBParser() {
     super("BENTON COUNTY", "AR",
-          "INC_TYPE:CALL! COMPANIES:UNIT! ADDRESS:ADDR!");
+          "INC_TYPE:CALL! COMPANIES:UNIT! ADDRESS:ADDRCITY/S6! Map:MAP! END");
+  }
+
+  @Override
+  public String getFilter() {
+    return "Dispatch@rogersar.gov";
   }
 
   @Override
@@ -19,51 +23,63 @@ public class ARBentonCountyBParser extends FieldProgramParser {
     return MsgInfo.MAP_FLG_SUPPR_AND_ADJ;
   }
 
+  private static Pattern INFO_BRK_PTN = Pattern.compile("\\s*\\n\\s*");
+
   @Override
   protected boolean parseMsg(String subject, String body, Data data) {
-    if (!subject.equals("CAD Dispatch")) return false;
+    if (!subject.equals("Dispatch")) return false;
     if (!body.startsWith("***")) return false;
     body = body.substring(3).trim();
-    if (body.endsWith("***EOT***")) {
-      body = body.substring(0,body.length()-9).trim();
+    if (body.endsWith("\n ***END OF TRANSMISSION***")) {
+      body = body.substring(0,body.length()-27).trim();
     } else {
       data.expectMore = true;
     }
-    return super.parseMsg(body, data);
+
+    int pt = body.indexOf("\n\n :");
+    if (pt < 0) return false;
+    String info = body.substring(pt+4).trim();
+    body = body.substring(0,pt).trim();
+    if (!super.parseMsg(body, data)) return false;
+    data.strSupp = INFO_BRK_PTN.matcher(info).replaceAll("\n");
+    return true;
+  }
+
+  @Override
+  public String getProgram() {
+    return super.getProgram() + " INFO";
   }
 
   @Override
   public Field getField(String name) {
-    if (name.equals("ADDR")) return new MyAddressField();
+    if (name.equals("UNIT")) return new MyUnitField();
+    if (name.equals("ADDRCITY")) return new MyAddressCityField();
     return super.getField(name);
   }
 
-  private static final Pattern ADDR_PTN = Pattern.compile("([^,]*) , ([ A-Z]*) , ([A-Z]{2})(?: ([^ ].*?))?  (.*?)(?:\\bMap (\\d{4}))?");
-  private class MyAddressField extends AddressField {
+  private class MyUnitField extends UnitField {
+    @Override
+    public void parse(String field, Data data) {
+      field = field.replace(' ', '_');
+      super.parse(field, data);
+    }
+  }
+
+  private class MyAddressCityField extends AddressCityField {
 
     @Override
     public void parse(String field, Data data) {
-      if (field.startsWith(",")) field = ' ' + field;
-      Matcher match = ADDR_PTN.matcher(field);
-      if (!match.matches()) abort();
-      String addr = match.group(1).trim();
-      data.strCity = match.group(2).trim();
-      String state = match.group(3);
-      if (!state.equals("AR")) data.strState = state;
-      data.strPlace = getOptGroup(match.group(4));
-      data.strSupp = match.group(5).trim();
-      data.strMap = getOptGroup(match.group(6));
-
-      if (addr.length() == 0) {
-        addr = data.strPlace;
-        data.strPlace = "";
+      int pt = field.indexOf("    ");
+      if (pt >= 0) {
+        data.strPlace = field.substring(0,pt);
+        field = field.substring(pt+4).trim();
       }
-      parseAddress(StartType.START_ADDR, FLAG_IMPLIED_INTERSECT | FLAG_ANCHOR_END, addr, data);
+      super.parse(field, data);;
     }
 
     @Override
     public String getFieldNames() {
-      return "ADDR APT CITY ST PLACE INFO MAP";
+      return "PLACE " + super.getFieldNames();
     }
   }
 }
