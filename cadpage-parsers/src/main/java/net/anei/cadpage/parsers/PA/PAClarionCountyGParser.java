82e7cb0dded11d7ede5db80bedeb5ad970814f45
Kenneth Corbin <ken@cadpage.org>
2022-02-28 19:49:38 -0800

    Fixed parsing problem with Clarioin County, PA (G)

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/PA/PAClarionCountyGParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/PA/PAClarionCountyGParser.java
@@ -7,28 +7,28 @@ import net.anei.cadpage.parsers.FieldProgramParser;
 import net.anei.cadpage.parsers.MsgInfo.Data;
 
 public class PAClarionCountyGParser extends FieldProgramParser {
-  
+
   public PAClarionCountyGParser() {
-    super("CLARION COUNTY", "PA", 
-          "Alert_Code:SRC! Address:ADDRCITY! Type:CALL! Xstreets:X Narrative:INFO/N+ Common_Name:PLACE! Loc_Info:APT2! Caller_Name:NAME! Caller_Phone:PHONE! Units:UNIT! GPS:GPS! END");
+    super("CLARION COUNTY", "PA",
+          "Alert_Code:SRC! Address:ADDRCITY! Type:CALL! Channel:CH? Xstreets:X Narrative:INFO/N+ Common_Name:PLACE! Loc_Info:APT2! Caller_Name:NAME! Caller_Phone:PHONE! Units:UNIT! GPS:GPS! END");
   }
 
   @Override
   public String getFilter() {
     return "noreply@ntr911sa.com";
   }
-  
+
   @Override
   public int getMapFlags() {
     return MAP_FLG_PREFER_GPS;
   }
-  
+
   @Override
   protected boolean parseMsg(String subject, String body, Data data) {
     if (!subject.equals("!NTR!")) return false;
     return parseFields(body.split("\n"), data);
   }
-  
+
   @Override
   public Field getField(String name) {
     if (name.equals("ADDRCITY")) return new MyAddressCityField();
@@ -36,7 +36,7 @@ public class PAClarionCountyGParser extends FieldProgramParser {
     if (name.equals("INFO")) return new MyInfoField();
     return super.getField(name);
   }
-  
+
   private class MyAddressCityField extends AddressCityField {
     @Override
     public void parse(String field, Data data) {
@@ -44,7 +44,7 @@ public class PAClarionCountyGParser extends FieldProgramParser {
       PAClarionCountyParser.fixCity(data);
     }
   }
-  
+
   private static final Pattern PLACE_PTN = Pattern.compile("\\d+ +(.*)");
   private class MyPlaceField extends PlaceField {
     @Override
@@ -54,7 +54,7 @@ public class PAClarionCountyGParser extends FieldProgramParser {
       super.parse(field, data);
     }
   }
-  
+
   private static final Pattern INFO_CODE_PTN = Pattern.compile("Dispatch Code: *([A-Z0-9]*)\\b.*");
   private class MyInfoField extends InfoField {
     @Override
@@ -66,7 +66,7 @@ public class PAClarionCountyGParser extends FieldProgramParser {
         super.parse(field, data);
       }
     }
-    
+
     @Override
     public String getFieldNames() {
       return "CODE " + super.getFieldNames();
