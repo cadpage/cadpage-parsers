f28daf293b1fa63a07a500394783b40acfccad50
Kenneth Corbin <ken@cadpage.org>
2022-03-03 17:20:03 -0800

    Update sender filter for Monroe County, KY

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/KY/KYMonroeCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/KY/KYMonroeCountyParser.java
@@ -5,11 +5,11 @@ import net.anei.cadpage.parsers.SplitMsgOptionsCustom;
 import net.anei.cadpage.parsers.dispatch.DispatchA65Parser;
 
 public class KYMonroeCountyParser extends DispatchA65Parser {
-  
+
   public KYMonroeCountyParser() {
     super(CITY_LIST, "MONROE COUNTY", "KY");
   }
-  
+
   @Override
   public SplitMsgOptions getActive911SplitMsgOptions() {
     return new SplitMsgOptionsCustom(){
@@ -20,18 +20,18 @@ public class KYMonroeCountyParser extends DispatchA65Parser {
 
   @Override
   public String getFilter() {
-    return "dispatch@911comm1.info,geoconex@nlamerica.com";
+    return "dispatch@911comm1.info,geoconex@nlamerica.com,dispatch@MonroeCoE911.com";
   }
-  
+
   private static final String[] CITY_LIST = new String[]{
 
       "FOUNTAIN RUN",
       "GAMALIEL",
       "TOMPKINSVILLE",
-      
+
       "BUGTUSSLE",
       "MOUNT HERMON",
-      
+
       "HESTAND"
 
   };
