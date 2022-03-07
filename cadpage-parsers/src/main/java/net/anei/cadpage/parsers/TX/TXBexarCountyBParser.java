5bf3a72a8e92c9444b6f9ac77d465adcb2aba1a9
Kenneth Corbin <ken@cadpage.org>
2022-03-07 10:38:39 -0800

    New Location: Bexar County, TX (B)

---
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/TX/TXBexarCountyBParser.java
@@ -0,0 +1,21 @@
+package net.anei.cadpage.parsers.TX;
+
+import net.anei.cadpage.parsers.dispatch.DispatchA19Parser;
+
+public class TXBexarCountyBParser extends DispatchA19Parser {
+
+  public TXBexarCountyBParser() {
+    super("BEXAR COUNTY", "TX");
+  }
+
+  @Override
+  public String getFilter() {
+    return "FlexRapidNotification@dccnotify.com";
+  }
+
+  @Override
+  public int getMapFlags() {
+    return MAP_FLG_PREFER_GPS;
+  }
+
+}
