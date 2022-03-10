dfe2177734faf98eaa073e7d0248cc452cff2a3d
Kenneth Corbin <ken@cadpage.org>
2022-03-10 15:36:43 -0800

    Update sender filter for Garrard County, KY

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/KY/KYGarrardCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/KY/KYGarrardCountyParser.java
@@ -3,14 +3,14 @@ package net.anei.cadpage.parsers.KY;
 import net.anei.cadpage.parsers.dispatch.DispatchSPKParser;
 
 public class KYGarrardCountyParser extends DispatchSPKParser {
-  
+
   public KYGarrardCountyParser() {
     super("GARRARD COUNTY", "KY");
   }
-  
+
   @Override
   public String getFilter() {
-    return "noreply@interact911.com,noreply@public-safety-cloud.com";
+    return "noreply@interact911.com,noreply@public-safety-cloud.com,bluegrasskyreports@gmail.com";
   }
-  
+
 }
