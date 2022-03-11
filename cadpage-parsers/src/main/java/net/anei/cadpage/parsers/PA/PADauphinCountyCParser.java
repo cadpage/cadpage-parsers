e3e5045130294ad0997f186fa292cd0b7594f473
Kenneth Corbin <ken@cadpage.org>
2022-03-11 12:18:36 -0800

    Fixed parsing problem with Dauphin County, PA (C)

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/PA/PADauphinCountyCParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/PA/PADauphinCountyCParser.java
@@ -4,17 +4,22 @@ import net.anei.cadpage.parsers.FieldProgramParser;
 import net.anei.cadpage.parsers.MsgInfo.Data;
 
 public class PADauphinCountyCParser extends FieldProgramParser {
-  
+
   public PADauphinCountyCParser() {
-    super("DAUPHIN COUNTY", "PA", 
-          "CALL:CALL! PLACE:PLACE? ADDR:ADDR! CITY:CITY! ID:ID! DATE:DATE! TIME:TIME! UNIT:UNIT! NOTES:INFO/N+");
+    super("DAUPHIN COUNTY", "PA",
+          "CALL:CALL! PLACE:PLACE? ADDR:ADDR! ( LAT:GPS1 LNG:GPS2! | ) CITY:CITY! ID:ID! DATE:DATE! TIME:TIME! UNIT:UNIT! NOTES:INFO/N+");
   }
-  
+
+  @Override
+  public int getMapFlags() {
+    return MAP_FLG_PREFER_GPS;
+  }
+
   @Override
   protected boolean parseMsg(String body, Data data) {
     return parseFields(body.split("\n"), data);
   }
-  
+
   @Override
   public Field getField(String name) {
     if (name.equals("ADDR")) return new MyAddressField();
@@ -22,7 +27,7 @@ public class PADauphinCountyCParser extends FieldProgramParser {
     if (name.equals("TIME")) return new TimeField("\\d\\d:\\d\\d:\\d\\d", true);
     return super.getField(name);
   }
-  
+
   private class MyAddressField extends AddressField {
     @Override
     public void parse(String field, Data data) {
