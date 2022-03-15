bd5aa9a39b1da9f37ff1ac27114023a2a223e01b
Kenneth Corbin <ken@cadpage.org>
2022-03-14 20:24:14 -0700

    Fixed parsing problem with Ellis County, KS

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/KS/KSEllisCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/KS/KSEllisCountyParser.java
@@ -2,22 +2,39 @@ package net.anei.cadpage.parsers.KS;
 
 import java.util.regex.Pattern;
 
-import net.anei.cadpage.parsers.dispatch.DispatchA25Parser;
+import net.anei.cadpage.parsers.MsgInfo.Data;
+import net.anei.cadpage.parsers.dispatch.DispatchH05Parser;
 
 
-public class KSEllisCountyParser extends DispatchA25Parser {
-  
+public class KSEllisCountyParser extends DispatchH05Parser {
+
   public KSEllisCountyParser() {
-    super("ELLIS COUNTY", "KS");
+    super("ELLIS COUNTY", "KS",
+        "CALL:CALL! PLACE:PLACE! ADDR:ADDRCITY! ID:ID! DATE:DATETIME! INFO:EMPTY! INFO_BLK+ TIMES:EMPTY! TIMES+");
   }
-  
+
   @Override
   public String getFilter() {
-    return "1-HPDDispatch@haysusa.com,cmccollum@haysusa.com";
+    return "alert@ellisco.net,tnw.ecems@gmail.com,alenser@haysusa.com";
   }
-  
+
+  @Override
+  public Field getField(String name) {
+    if (name.equals("ADDRCITY")) return new MyAddressCityField();
+    if (name.equals("DATETIME")) return new DateTimeField("\\d\\d?/\\d\\d?/\\d{4} +\\d\\d:\\d\\d:\\d\\d", true);
+    return super.getField(name);
+  }
+
+  private class MyAddressCityField extends AddressCityField {
+    @Override
+    public void parse(String field, Data data) {
+      field = stripFieldEnd(field, ",");
+      super.parse(field, data);
+    }
+  }
+
   private static final Pattern US_183_ALT_PTN = Pattern.compile("\\b183 +ALT(?: +HWY)?\\b", Pattern.CASE_INSENSITIVE);
-  
+
   @Override
   public String adjustMapAddress(String addr) {
     addr = US_183_ALT_PTN.matcher(addr).replaceAll("183 BYPASS");
