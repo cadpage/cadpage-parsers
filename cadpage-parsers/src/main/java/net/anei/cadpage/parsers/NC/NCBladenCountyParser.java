2fcc4b147838dea011b31c01388736b0c3a7846d
Kenneth Corbin <ken@cadpage.org>
2022-03-21 14:52:55 -0700

    Fixed parsing problem with Baden County, NC

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/NC/NCBladenCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/NC/NCBladenCountyParser.java
@@ -1,93 +1,21 @@
 package net.anei.cadpage.parsers.NC;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import net.anei.cadpage.parsers.dispatch.DispatchSouthernParser;
 
-import net.anei.cadpage.parsers.FieldProgramParser;
-import net.anei.cadpage.parsers.MsgInfo.Data;
+public class NCBladenCountyParser extends DispatchSouthernParser {
 
-public class NCBladenCountyParser extends FieldProgramParser {
-  
   public NCBladenCountyParser() {
-    super("BLADEN COUNTY", "NC", 
-          "ID CALL ADDR DUPADDR? DUPCITY? UNIT! END");
+    super(CITY_LIST, "BLADEN COUNTY", "NC",
+          DSFLG_ADDR | DSFLG_ADDR_TRAIL_PLACE | DSFLG_OPT_CODE | DSFLG_TIME);
   }
-  
+
   @Override
   public String getFilter() {
-    return "PageGate@bladenco.org";
+    return "@bladenco.org";
   }
-  
-  private static final Pattern SUBJECT_PTN = Pattern.compile("As of (\\d\\d?/\\d\\d?/\\d\\d) (\\d\\d:\\d\\d:\\d\\d)");
-  
-  @Override
-  protected boolean parseMsg(String subject, String body, Data data) {
-    
-    Matcher match = SUBJECT_PTN.matcher(subject);
-    if (!match.matches()) return false;
-    data.strDate = match.group(1);
-    data.strTime = match.group(2);
-    
-    return parseFields(body.split("\n"), 4, data);
-  }
-  
-  @Override
-  public String getProgram() {
-    return "DATE TIME " + super.getProgram();
-  }
-  
-  @Override
-  public Field getField(String name) {
-    if (name.equals("ID")) return new IdField("\\d{4}-\\d{8}", true);
-    if (name.equals("ADDR")) return new MyAddressField();
-    if (name.equals("DUPADDR")) return new MyDupAddressField();
-    if (name.equals("DUPCITY")) return new MyDupCityField();
-    return super.getField(name);
-  }
-  
-  private class MyAddressField extends AddressField {
-    @Override
-    public void parse(String field, Data data) {
-      int pt =  field.lastIndexOf(',');
-      if (pt >= 0) {
-        data.strCity = field.substring(pt+1).trim();
-        field = field.substring(0,pt);
-      }
-      super.parse(field, data);
-    }
-    
-    @Override
-    public String getFieldNames() {
-      return super.getFieldNames() + " CITY";
-    }
-  }
-  
-  private class MyDupAddressField extends SkipField {
-    @Override
-    public boolean canFail() {
-      return true;
-    }
-    
-    @Override
-    public boolean checkParse(String field, Data data) {
-      return field.equals(getRelativeField(-1));
-    }
-  }
-  
-  private class MyDupCityField extends SkipField {
-    @Override
-    public boolean canFail() {
-      return true;
-    }
-    
-    @Override
-    public boolean checkParse(String field, Data data) {
-      return field.equals(data.strCity);
-    }
-  }
-  
+
   private static final String[] CITY_LIST = new String[]{
-    
+
     // Towns
     "BLADENBORO",
     "CLARKTON",
