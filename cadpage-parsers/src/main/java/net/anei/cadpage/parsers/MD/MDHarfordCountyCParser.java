0ea3ad1709bd6cd11e3b4f1f1969899ca2aa38e3
Kenneth Corbin <ken@cadpage.org>
2022-03-21 12:51:37 -0700

    Fixed parsing problem with Harford County, MD (C)

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/MD/MDHarfordCountyCParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/MD/MDHarfordCountyCParser.java
@@ -1,16 +1,27 @@
 package net.anei.cadpage.parsers.MD;
 
+import net.anei.cadpage.parsers.MsgInfo.Data;
 import net.anei.cadpage.parsers.dispatch.DispatchA48Parser;
 
 public class MDHarfordCountyCParser extends DispatchA48Parser {
 
   public MDHarfordCountyCParser() {
-    super(MDHarfordCountyParser.CITY_LIST, "HARFORD COUNTY", "MD", FieldType.X, A48_NO_CODE);
+    super(MDHarfordCountyParser.CITY_LIST, "HARFORD COUNTY", "MD", FieldType.PLACE, A48_NO_CODE);
   }
 
   @Override
   public String getFilter() {
-    return "EMS-YORKSOUTHCOUNTY@harfordcountymd.gov";
+    return "@harfordcountymd.gov";
+  }
+
+  @Override
+  protected boolean parseMsg(String subject, String body, Data data) {
+    if (!super.parseMsg(subject, body, data)) return false;
+    if (data.strPlace.contains("/ -")) {
+      data.strCross = data.strPlace.replace("/ -", "/");
+      data.strPlace = "";
+    }
+    return true;
   }
 
 }
