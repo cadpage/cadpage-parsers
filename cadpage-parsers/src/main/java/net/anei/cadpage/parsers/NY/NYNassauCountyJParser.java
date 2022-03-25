76389ded3f1dbab4429767611a0d20d8f9f4eb18
Kenneth Corbin <ken@cadpage.org>
2022-03-25 08:23:53 -0700

    Update sender filter for Nassau County, NY (J)

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/NY/NYNassauCountyJParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/NY/NYNassauCountyJParser.java
@@ -4,13 +4,13 @@ import net.anei.cadpage.parsers.dispatch.DispatchA14Parser;
 
 
 public class NYNassauCountyJParser extends DispatchA14Parser {
-  
+
   public NYNassauCountyJParser() {
     super("NASSAU COUNTY", "NY", true);
   }
-  
+
   @Override
   public String getFilter() {
-    return "paging1@firerescuesystems.xohost.com";
+    return "paging1@firerescuesystems.xohost.com,8559495933";
   }
 }
