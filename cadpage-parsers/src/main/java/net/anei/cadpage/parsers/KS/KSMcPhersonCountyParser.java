0903b68ffe2c64784496e3c475fd498bd78b265a
Kenneth Corbin <ken@cadpage.org>
2022-04-01 15:56:54 -0700

    Flagged McPherson County, KS to trust GPS coordinates'

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/KS/KSMcPhersonCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/KS/KSMcPhersonCountyParser.java
@@ -7,14 +7,19 @@ import net.anei.cadpage.parsers.dispatch.DispatchSPKParser;
  */
 
 public class KSMcPhersonCountyParser extends DispatchSPKParser {
-  
+
   public KSMcPhersonCountyParser() {
     super("MCPHERSON COUNTY", "KS");
   }
-  
+
   @Override
   public String getFilter() {
     return "mcpherson911@mcphersoncountyks.us";
+  }
+
+  @Override
+  public int getMapFlags() {
+    return MAP_FLG_PREFER_GPS;
   }
 
 }
