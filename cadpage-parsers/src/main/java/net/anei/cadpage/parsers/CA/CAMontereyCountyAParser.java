9c15ae80c4aa29e8785ed5875f10a875b2b8cd92
Kenneth Corbin <ken@cadpage.org>
2022-04-19 13:53:51 -0700

    Fixed parsing problem with Monterey County, CA

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/CA/CAMontereyCountyAParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/CA/CAMontereyCountyAParser.java
@@ -4,24 +4,25 @@ import java.util.Properties;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import net.anei.cadpage.parsers.MsgParser;
+import net.anei.cadpage.parsers.FieldProgramParser;
 import net.anei.cadpage.parsers.MsgInfo.Data;
 
 
 /**
  * Monterey County, CA
  */
-public class CAMontereyCountyAParser extends MsgParser {
-  
+public class CAMontereyCountyAParser extends FieldProgramParser {
+
   public CAMontereyCountyAParser() {
-    super("MONTEREY COUNTY", "CA");
+    super("MONTEREY COUNTY", "CA",
+          "SRC UNIT CALL ADDR! CROSS_STREETS%EMPTY! X EMPTY! END");
   }
-  
+
   @Override
   public String getFilter() {
     return "donotreply@co.monterey.ca.us,donotreply@co.monterey,MONTEREY911@CO.MONTEREY.CA.US";
   }
-  
+
   @Override
   public int getMapFlags() {
     return MAP_FLG_SUPPR_LA;
@@ -29,14 +30,18 @@ public class CAMontereyCountyAParser extends MsgParser {
 
   @Override
   protected boolean parseMsg(String subject, String body, Data data) {
-    
+
+    String[] flds = body.split(";;", -1);
+    if (flds.length >= 6) {
+      return parseFields(flds, data);
+    }
     if (parseMsg4(body, data)) return true;
     if (parseMsg3(body, data)) return true;
     if (parseMsg1(subject, body, data)) return true;
     if (parseMsg2(body, data)) return true;
     return false;
   }
-  
+
   private boolean parseMsg4(String body, Data data) {
     FParser fp = new FParser(body);
     String src = fp.get(8);
@@ -48,7 +53,7 @@ public class CAMontereyCountyAParser extends MsgParser {
     String addr = fp.get(100);
     if (!fp.check(" ")) return false;
     String cross = fp.get();
-    
+
     setFieldList("SRC UNIT CALL ADDR APT X");
     data.strSource = src;
     data.strUnit = unit;
@@ -57,9 +62,9 @@ public class CAMontereyCountyAParser extends MsgParser {
     data.strCross = cross;
     return true;
   }
-  
+
   private static final Pattern CODE_CALL_PTN = Pattern.compile("(\\d\\d?[A-Z]\\d\\d?[A-Z]?) +(.*)", Pattern.CASE_INSENSITIVE);
-  
+
   private boolean parseMsg3(String body, Data data) {
     FParser fp = new FParser(body);
     String unit = fp.get(10);
@@ -74,7 +79,7 @@ public class CAMontereyCountyAParser extends MsgParser {
     if (!fp.check(" ")) return false;
     if (fp.check(" ")) return false;
     String call = fp.get();
-    
+
     setFieldList("UNIT ADDR APT X CITY CODE CALL");
     data.strUnit = unit;
     parseAddress(addr, data);
@@ -89,32 +94,32 @@ public class CAMontereyCountyAParser extends MsgParser {
     data.strCall = call;
     return true;
   }
-  
+
   private static final Pattern MARKER = Pattern.compile("([A-Z ]+) - +");
-  
+
   private boolean parseMsg2(String body, Data data) {
-    
+
     Matcher match = MARKER.matcher(body);
     if (!match.lookingAt()) return false;
     setFieldList("SRC UNIT CALL ADDR APT X MAP");
-    
+
     String source = match.group(1);
     body = body.substring(match.end());
-    
+
     FParser fp = new FParser(body);
     int pt = fp.checkAhead("CROSS STREETS", 68, 92, 93, 94, 142);
     if (pt >= 0) {
-      
+
       if (pt == 68) {
         String unit = fp.get(6);
-        
+
         if (fp.check(" ")) return false;
         String call = fp.get(10);
-        
+
         if (!fp.check(" ")) return false;
         if (fp.check(" ")) return false;
         String addr = fp.get(50);
-        
+
         if (!fp.check(" CROSS STREETS")) return false;
         String cross = fp.get();
 
@@ -125,7 +130,7 @@ public class CAMontereyCountyAParser extends MsgParser {
         data.strCross = cross;
         return true;
       }
-      
+
       if (pt == 142) {
         setFieldList("SRC CALL UNIT ADDR APT X");
         String call = fp.get(10);
@@ -135,7 +140,7 @@ public class CAMontereyCountyAParser extends MsgParser {
         String addr = fp.get(100);
         if (!fp.check(" CROSS STREETS")) return false;
         String cross = fp.get(50);
-        
+
         data.strSource = source;
         data.strCall = call;
         data.strUnit = unit;
@@ -147,16 +152,16 @@ public class CAMontereyCountyAParser extends MsgParser {
       int unitLen = pt - 63;
       String unit = fp.get(unitLen);
       if (!fp.check(" ")) return false;
-      
+
       if (fp.check(" ")) return false;
       String call = fp.get(10);
-      
+
       if (!fp.check(" ") || fp.check(" ")) return false;
       String addr = fp.get(50);
-      
+
       if (!fp.check(" CROSS STREETS ")) return false;
       String cross = fp.get();
-      
+
       data.strSource = source;
       data.strUnit = unit;
       data.strCall = call;
@@ -164,26 +169,26 @@ public class CAMontereyCountyAParser extends MsgParser {
       data.strCross = cross;
       return true;
     }
-    
+
     pt = fp.checkAhead("X STREETS", 92, 93);
     if (pt >= 0) {
 
       String unit = fp.get(29);
       if (!fp.check(" ")) return false;
-      
+
       if (fp.check(" ")) return false;
       String call = fp.get(10);
-      
+
       if (!fp.check(" ") || fp.check(" ")) return false;
       String addr = fp.get(50);
-      
+
       if (!fp.check(" X STREETS ")) return false;
       String cross = fp.get(76);
-      
+
       fp.setOptional();
       if (!fp.check("MAP PAGE")) return false;
       String map = fp.get();
-      
+
       data.strSource = source;
       data.strUnit = unit;
       data.strCall = call;
@@ -191,16 +196,16 @@ public class CAMontereyCountyAParser extends MsgParser {
       data.strCross = cross;
       data.strMap = map;
       return true;
-      
+
     }
-    
+
     if (fp.checkAhead(243, "MAP PAGE")) {
       String unit = fp.get(20);
       if (!fp.check(" ") || fp.check(" ")) return false;
       String call = fp.get(9);
       if (fp.check(" ")) return false;
       String addr = fp.get(50);
-      
+
       data.strUnit = unit;
       data.strCall = call;
       parseAddress(addr, data);
@@ -222,7 +227,7 @@ public class CAMontereyCountyAParser extends MsgParser {
     String addr = fp.get(40);
     if (!fp.checkBlanks(361)) return false;
     String cross = fp.get();
-    
+
     data.strSource = source;
     data.strUnit = unit;
     data.strCall = call;
@@ -230,25 +235,25 @@ public class CAMontereyCountyAParser extends MsgParser {
     data.strCross = cross;
     return true;
   }
-    
+
   private static final Pattern MASTER = Pattern.compile("(?:(.*?) - )?([A-Z0-9]{2,6}):(.*?) - (.*?)(?: - ([A-Z]{3}))? *(?:Units?:(.*?))?");
-   
+
   private boolean parseMsg1(String subject, String body, Data data) {
     // Old page format
-    
+
     if (!subject.equals("CAD Page")) return false;
     setFieldList("MAP CODE CALL ADDR PLACE CITY UNIT INFO");
-    
+
     int pt = body.indexOf('\n');
     String extra = null;
     if (pt >= 0) {
       extra = body.substring(pt+1).trim();
       body = body.substring(0,pt).trim();
     }
-    
+
     Matcher match = MASTER.matcher(body);
     if (!match.matches()) return false;
-    
+
     data.strMap = getOptGroup(match.group(1));
     data.strCode = match.group(2);
     data.strCall = match.group(3).trim();
@@ -256,21 +261,21 @@ public class CAMontereyCountyAParser extends MsgParser {
     String city = match.group(5);
     if (city != null) data.strCity = convertCodes(city, CITY_CODES);
     data.strUnit = getOptGroup(match.group(6));
-    
+
     pt = data.strCity.indexOf('/');
     if (pt >= 0) {
       data.strPlace = data.strCity.substring(0,pt);
       data.strCity = data.strCity.substring(pt+1);
     }
-    
+
     if (extra != null) {
       if (extra.startsWith("Message:")) extra = extra.substring(8).trim();
       data.strSupp = extra;
     }
-    
+
     return true;
   }
-  
+
   private static final Properties CITY_CODES = buildCodeTable(new String[]{
       "ARO",   "AROMAS",
       "ARS",   "ARROYO SECO/GREENFIELD",
@@ -303,7 +308,7 @@ public class CAMontereyCountyAParser extends MsgParser {
       "IND",   "INDIAN SPRINGS/SALINAS",
       "JOL",   "JOLON",
       "KCY",   "KING CITY",
-      "LAM",   "LAMESA VILLAGE/MONTEREY", 
+      "LAM",   "LAMESA VILLAGE/MONTEREY",
       "LLG",   "LOS LAURELES GRADE/SALINAS",
       "LLS",   "LAS LOMAS",
       "LOC",   "LOCKWOOD/MONTEREY",
