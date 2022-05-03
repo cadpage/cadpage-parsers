a9d99ee1da93f3d184bead612126965d115b2b95
Kenneth Corbin <ken@cadpage.org>
2022-05-02 19:08:17 -0700

    Fixed parsing problem with Queen Annes County, MD (C)

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/MD/MDQueenAnnesCountyCParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/MD/MDQueenAnnesCountyCParser.java
@@ -9,12 +9,17 @@ public class MDQueenAnnesCountyCParser extends FieldProgramParser {
 
   public MDQueenAnnesCountyCParser() {
     super("QUEEN ANNES COUNTY", "MD",
-          "CALL ADDRCITY PLACE X UNIT CH ( MAP GPS1 GPS2 | ) INFO! DATETIME END");
+          "CALL ADDRCITY PLACE X UNIT ( Radio_Channel:CH! | CH! ) ( Box_Area:MAP GPS1 GPS2! | ) INFO! DATETIME END");
   }
 
   @Override
   public String getFilter() {
     return "Dispatch@qac.gov,@c-msg.net";
+  }
+
+  @Override
+  public int getMapFlags() {
+    return MAP_FLG_PREFER_GPS;
   }
 
   @Override
@@ -31,7 +36,7 @@ public class MDQueenAnnesCountyCParser extends FieldProgramParser {
     return super.getField(name);
   }
 
-  private static final Pattern GPS_PTN = Pattern.compile("[-+]?\\d{2,3}\\.\\d{6,}");
+  private static final Pattern GPS_PTN = Pattern.compile("[-+]?\\d{2,3}\\.\\d{6,}|-361|");
   private class MyGPSField extends GPSField {
     public MyGPSField(int type) {
       super(type);
