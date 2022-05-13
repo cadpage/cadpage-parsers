f722f59bb219b35c396b007fe83de78fda83cad6
Ken Corbin <ken@cadpage.org>
2022-05-13 10:31:19 -0700

    Fixed parsing problem with Howard COunty, MD (Add B)

---
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/MD/MDHowardCountyBParser.java
@@ -0,0 +1,25 @@
+package net.anei.cadpage.parsers.MD;
+
+import net.anei.cadpage.parsers.FieldProgramParser;
+import net.anei.cadpage.parsers.MsgInfo.Data;
+
+public class MDHowardCountyBParser extends FieldProgramParser {
+  
+  public MDHowardCountyBParser() {
+    super("HOWARD COUNTY", "MD", 
+          "ID CALL PLACE ADDR CITY INFO UNIT TIME! END");
+  }
+  
+  @Override
+  protected boolean parseMsg(String body, Data data) {
+    return parseFields(body.split(" //"), data);
+  }
+  
+  @Override
+  public Field getField(String name) {
+    if (name.equals("ID")) return new IdField("[EF]\\d{8}", true);
+    if (name.equals("TIME")) return new TimeField("\\d\\d:\\d\\d:\\d\\d", true);
+    return super.getField(name);
+  }
+
+}
