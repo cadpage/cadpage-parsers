ca0f46594dd9223f57ac73d73b5da857ea093aaa
Ken Corbin <ken@cadpage.org>
2022-05-23 14:40:25 -0700

    Update A911 parser table

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/VA/VABuckinghamCountyBParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/VA/VABuckinghamCountyBParser.java
@@ -12,4 +12,9 @@ public class VABuckinghamCountyBParser extends DispatchA71Parser {
   public String getFilter() {
     return "notify@somahub.io";
   }
+  
+  @Override
+  public int getMapFlags() {
+    return MAP_FLG_PREFER_GPS;
+  }
 }
