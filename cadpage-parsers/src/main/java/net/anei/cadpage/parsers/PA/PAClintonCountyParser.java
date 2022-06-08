d12dc4943e3dab4076a02a30ee2c013f1a8e6e3c
Kenneth Corbin <ken@cadpage.org>
2022-06-08 07:22:56 -0700

    Update sender filter for Clinton County, PA

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/PA/PAClintonCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/PA/PAClintonCountyParser.java
@@ -9,22 +9,22 @@ import net.anei.cadpage.parsers.dispatch.DispatchH05Parser;
  Clinton County, PA
  */
 public class PAClintonCountyParser extends DispatchH05Parser {
-  
+
   public PAClintonCountyParser() {
-    super("CLINTON COUNTY", "PA", 
-          "( SELECT/1 CALL:CALL! PLACE:PLACE! ADDR:ADDRCITY! CROSS_ST:X! ID:SKIP! PRI:PRI! " + 
-          "| EMPTY Final%EMPTY? DATETIME CALL PLACE ADDRCITY! BOX CROSS_ST:X! " + 
+    super("CLINTON COUNTY", "PA",
+          "( SELECT/1 CALL:CALL! PLACE:PLACE! ADDR:ADDRCITY! CROSS_ST:X! ID:SKIP! PRI:PRI! " +
+          "| EMPTY Final%EMPTY? DATETIME CALL PLACE ADDRCITY! BOX CROSS_ST:X! " +
           ") DATE:DATETIME! MAP:SKIP! UNIT:UNIT! Nature_of_Call:INFO? INFO:EMPTY! INFO_BLK/Z+? ID! TIMES+");
   }
-  
+
   @Override
   public String getFilter() {
-    return "@ClintonCountyPA.com";
+    return "@ClintonCountyPA.com,@ClintonCountyPA.gov";
   }
-  
+
   private static final Pattern INFO_TIME_PTN = Pattern.compile("\n(\\d\\d:\\d\\d:\\d\\d) {8,}");
   private static final Pattern DELIM = Pattern.compile("\\.*\n\\.*(?:(?<!\n)\\*)?");
-  
+
   @Override
   protected boolean parseHtmlMsg(String subject, String body, Data data) {
     if (body.startsWith("<")) {
@@ -43,14 +43,14 @@ public class PAClintonCountyParser extends DispatchH05Parser {
     if (name.equals("ID")) return new MyIdField();
     return super.getField(name);
   }
-  
-  private static final Pattern ID_NOT_ASSIGNED = Pattern.compile("(?:, *)?\\[Incident not yet created \\d+\\]"); 
+
+  private static final Pattern ID_NOT_ASSIGNED = Pattern.compile("(?:, *)?\\[Incident not yet created \\d+\\]");
   private class MyIdField extends IdField {
     @Override
     public boolean canFail() {
       return true;
     }
-    
+
     @Override
     public boolean checkParse(String field, Data data) {
       if (field.length() == 0) return false;
@@ -59,7 +59,7 @@ public class PAClintonCountyParser extends DispatchH05Parser {
       data.strCallId = field;
       return true;
     }
-    
+
     @Override
     public void parse(String field, Data data) {
       if (!checkParse(field, data)) abort();
