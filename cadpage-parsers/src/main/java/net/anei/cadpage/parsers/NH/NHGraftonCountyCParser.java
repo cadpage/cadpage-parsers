58330a49795d3450cf6fbe15740885a4c8f012ce
Ken Corbin <ken@cadpage.org>
2022-06-27 19:16:35 -0700

    Fixed parsing problem with Grafton County, NH (C)

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/NH/NHGraftonCountyCParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/NH/NHGraftonCountyCParser.java
@@ -21,7 +21,7 @@ public class NHGraftonCountyCParser extends DispatchA27Parser {
     return "notification@nhpd.cloud,Dispatch@lebanonnh.gov";
   }
   
-  private static final Pattern LEAD_ID_PTN = Pattern.compile("(\\d{4}-\\d{6})\n");
+  private static final Pattern LEAD_ID_PTN = Pattern.compile("(\\d{4}-\\d{6})(?: [\\[\\(]P#:\\d\\d-\\d{6}[\\)\\]])?\n");
   private static final Pattern GPS_PTN = Pattern.compile("(.*?)\\(((?:[-+]?\\d+\\.\\d{4,}|0), *(?:[-+]?\\d+\\.\\d{4,}|0))\\)");
 
   @Override
@@ -29,7 +29,22 @@ public class NHGraftonCountyCParser extends DispatchA27Parser {
     
     int pt = subject.lastIndexOf('|');
     if (pt >= 0)subject = subject.substring(pt+1).trim();
-    if (subject.startsWith("CFS Notification - ")) {
+    
+    if (body.startsWith("<h4 ")) {
+      pt = body.indexOf('>');
+      body = body.substring(pt+1).trim().replace("</h4>", "");
+      if (body.startsWith("[")) {
+        pt = body.indexOf(']');
+        if (pt < 0) return false;
+        subject = body.substring(1,pt).trim();
+        body = body.substring(pt+1).trim();
+      }
+      body = stripFieldEnd(body, "\n\nCSI TECHNOLOGY GROUP");
+    }
+    String src = null;
+    if (subject.startsWith("CFS Notification - ")) src = subject.substring(19).trim();
+    else if (subject.endsWith(" - CFS Notification")) src = subject.substring(0,subject.length()-19).trim();
+    if (src != null) {
       String id = null;
       Matcher match = LEAD_ID_PTN.matcher(body);
       if (match.lookingAt()) {
@@ -46,7 +61,7 @@ public class NHGraftonCountyCParser extends DispatchA27Parser {
       if (id != null) head = head + ' ' + id;
       body = head + tail;
       
-      body = "Notification from " + subject.substring(19) + ":\n" + body;
+      body = "Notification from " + src + ":\n" + body;
       body = body.replace("\nUnit(s) responed:", "\nUnit(s) responded:");
     }
     
