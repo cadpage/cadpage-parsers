51d750b2a9b8c45b3b21fcc2c2194d26e1c08e45
Ken Corbin <ken@cadpage.org>
2022-07-07 14:18:13 -0700

    Fixed parsing problem with Kent County, MI (Add B)

---
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/MI/MIKentCountyAParser.java
@@ -0,0 +1,52 @@
+package net.anei.cadpage.parsers.MI;
+
+import java.util.Properties;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import net.anei.cadpage.parsers.MsgInfo.Data;
+import net.anei.cadpage.parsers.MsgInfo.MsgType;
+import net.anei.cadpage.parsers.dispatch.DispatchH03Parser;
+
+public class MIKentCountyAParser extends DispatchH03Parser {
+
+  public MIKentCountyAParser() {
+    super("KENT COUNTY", "MI");
+    setupGpsLookupTable(GPS_LOOKUP_TABLE);
+  }
+  
+  @Override
+  public String getFilter() {
+    return "@Kent911.org,ssiler@caledoniatownship.org";
+  }
+  
+  private static final Pattern SUBJECT_PTN = Pattern.compile("(.*) (?:Med|Fire) Alert");
+
+  @Override
+  protected boolean parseHtmlMsg(String subject, String body, Data data) {
+    
+    if (subject.equalsIgnoreCase("BURN PERMIT")) {
+      setFieldList("CALL INFO");
+      data.msgType = MsgType.GEN_ALERT;
+      data.strCall = subject;
+      data.strSupp = decodeHtmlSequence(body).trim();
+      return true;
+    }
+    
+    Matcher match = SUBJECT_PTN.matcher(subject);
+    if (!match.matches()) return false;
+    data.strSource = match.group(1).trim();
+
+    return super.parseHtmlMsg(subject, body, data);
+  }
+
+  @Override
+  public String getProgram() {
+    return "SRC " + super.getProgram();
+  }
+  
+  private static final Properties GPS_LOOKUP_TABLE = buildCodeTable(new String[]{
+      "I-96 EB WO DEAN LAKE-GT",      "+43.01519,-85.61512",
+      "I-96 EB WO FULTON",            "+42.96480,-85.58169"
+  });
+}
