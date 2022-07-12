b4907b6bd6dfe5d9b9de2138bf71057e31549d5f
Ken Corbin <ken@cadpage.org>
2022-07-11 21:27:56 -0700

    Fixed parsing problem with Bladen County, NC

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/NC/NCBladenCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/NC/NCBladenCountyParser.java
@@ -1,5 +1,9 @@
 package net.anei.cadpage.parsers.NC;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import net.anei.cadpage.parsers.MsgInfo.Data;
 import net.anei.cadpage.parsers.dispatch.DispatchSouthernParser;
 
 public class NCBladenCountyParser extends DispatchSouthernParser {
@@ -12,6 +16,30 @@ public class NCBladenCountyParser extends DispatchSouthernParser {
   @Override
   public String getFilter() {
     return "@bladenco.org";
+  }
+  
+  private static final Pattern OCA_MASTER = Pattern.compile("BEMS OCA: (\\d{4}-\\d{5}) assigned to CFS: (\\d{4}-\\d{6}) (.*)");
+
+  @Override
+  protected boolean parseMsg(String body, Data data) {
+    Matcher match = OCA_MASTER.matcher(body);
+    if (match.matches()) {
+      setFieldList("ID ADDR APT CITY CALL");
+      data.strCallId = match.group(1) + '/' + match.group(2);
+      String addr = match.group(3);
+      int pt = addr.indexOf(',');
+      if (pt >= 0) {
+        parseAddress(addr.substring(0,pt).trim(), data);
+        parseAddress(StartType.START_ADDR, FLAG_ONLY_CITY, addr.substring(pt+1).trim(), data);
+      } else {
+        parseAddress(StartType.START_ADDR, addr, data);
+      }
+      data.strCall = getLeft();
+      return true;
+    } 
+    else {
+      return super.parseMsg(body, data);
+    }
   }
 
   private static final String[] CITY_LIST = new String[]{
@@ -57,6 +85,19 @@ public class NCBladenCountyParser extends DispatchSouthernParser {
     "TURNBULL",
     "WHITE OAK",
     "DUBLIN",
-    "WHITES CREEK"
+    "WHITES CREEK",
+    
+    // Columbus County
+    "RIEGELWOOD",
+    
+    // Cumberland County
+    "FAYETTEVILLE",
+    
+    // Robeson County
+    "ST PAULS",
+    
+    // Sampson County
+    "HARRELLS"
+    
   };
 }
