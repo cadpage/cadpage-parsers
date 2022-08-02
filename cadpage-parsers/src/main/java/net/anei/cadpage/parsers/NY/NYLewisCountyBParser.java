b92e74bf3ecb39d498ca7706ad438fd79000103d
Kenneth Corbin <ken@cadpage.org>
2022-08-01 22:11:38 -0700

    New Location: Lewis County, NY (B)

---
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/NY/NYLewisCountyBParser.java
@@ -0,0 +1,16 @@
+package net.anei.cadpage.parsers.NY;
+
+import net.anei.cadpage.parsers.dispatch.DispatchA19Parser;
+
+public class NYLewisCountyBParser extends DispatchA19Parser {
+
+  public NYLewisCountyBParser() {
+    super("LEWIS COUNTY", "NY");
+  }
+
+  @Override
+  public String getFilter() {
+    return "Lewis911@cnymail.com";
+  }
+
+}
