e227eb610ccc686fe8804e2654fec26a809429c6
Kenneth Corbin <ken@cadpage.org>
2022-08-02 14:02:37 -0700

    Added correction for misspelled city in Richmand County, NC

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/NC/NCRichmondCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/NC/NCRichmondCountyParser.java
@@ -1,35 +1,38 @@
 package net.anei.cadpage.parsers.NC;
 
+import java.util.Properties;
+
 import net.anei.cadpage.parsers.dispatch.DispatchSouthernParser;
 
 public class NCRichmondCountyParser extends DispatchSouthernParser {
-  
+
   public NCRichmondCountyParser() {
-    super(CITY_LIST, "RICHMOND COUNTY", "NC", 
+    super(CITY_LIST, "RICHMOND COUNTY", "NC",
           DSFLG_ADDR | DSFLG_OPT_X | DSFLG_OPT_CODE | DSFLG_ID | DSFLG_TIME | DSFLG_NO_INFO);
+    setupCities(MISSPELLED_CITIES);
   }
 
   private static final String[] CITY_LIST = new String[]{
-    
+
     // Cities
     "HAMLET",
     "ROCKINGHAM",
-    
+
     // Towns
     "DOBBINS HEIGHTS",
     "ELLERBE",
     "HOFFMAN",
     "NORMAN",
-    
+
     //Census-designated places
     "CORDOVA",
     "EAST ROCKINGHAM",
     "ROBERDEL",
-    
+
     // Other unincorporated communities
     "FRUITLAND",
     "MARSTON",
-    
+
     // Townships
     "STEELES",
     "MINERAL SPRINGS",
@@ -39,4 +42,8 @@ public class NCRichmondCountyParser extends DispatchSouthernParser {
     "ROCKINGHAM",
     "BLACKJACK"
   };
+
+  private static final Properties MISSPELLED_CITIES = buildCodeTable(new String[] {
+      "DOBBIN HEIGHTS",     "DOBBINS HEIGHTS"
+  });
 }
