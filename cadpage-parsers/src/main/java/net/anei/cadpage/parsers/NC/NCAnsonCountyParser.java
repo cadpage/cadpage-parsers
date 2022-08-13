1622ceabb26ea6dab32554ac35bbbfa821aefa32
Ken Corbin <ken@cadpage.org>
2022-08-13 12:31:52 -0700

    Fixed parsing problem with Anson County, NC

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/NC/NCAnsonCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/NC/NCAnsonCountyParser.java
@@ -13,21 +13,21 @@ public class NCAnsonCountyParser extends DispatchSouthernParser {
 
   public NCAnsonCountyParser() {
     super(CITY_LIST, "ANSON COUNTY", "NC", 
-          DSFLG_ADDR | DSFLG_ID | DSFLG_TIME);
+          DSFLG_ADDR | DSFLG_OPT_CODE | DSFLG_ID | DSFLG_TIME);
+    setCallPtn("[-A-Z]+");
   }
   
   private static final Pattern MARKER = Pattern.compile("notifyuser:|911:");
   
   @Override
   public String getFilter() {
-    return "chyatt@ansonvillefire.com,notifyuser@co.anson.nc.us,911@co.anson.nc.us";
+    return "chyatt@ansonvillefire.com,notifyuser@co.anson.nc.us,911@co.anson.nc.us,9911@ansoncountync.gov";
   }
   
   @Override
   public boolean parseMsg(String body, Data data) {
     Matcher match = MARKER.matcher(body);
-    if (!match.lookingAt()) return false;
-    body = body.substring(match.end()).trim();
+    if (match.lookingAt()) body = body.substring(match.end()).trim();
     if (! super.parseMsg(body, data)) return false;
     if (data.strAddress.startsWith("0 ")) data.strAddress = data.strAddress.substring(2).trim();
     return true;
@@ -40,17 +40,6 @@ public class NCAnsonCountyParser extends DispatchSouthernParser {
     sAddr = AT_PTN.matcher(sAddr).replaceAll("@");
     super.parseMain(sAddr, data);
   }
-
-  @Override
-  protected void parseExtra(String sExtra, Data data) {
-    Matcher match = EXTRA_PTN.matcher(sExtra);
-    if (match.find()) {
-      data.strSupp = sExtra.substring(match.end()).trim();
-      sExtra = sExtra.substring(0,match.start()).trim();
-    }
-    data.strCall = sExtra;
-  }
-  private static final Pattern EXTRA_PTN = Pattern.compile(" (?=MA ?\\d:)");
 
   private static final String[] CITY_LIST = new String[]{
     "ANSONVILLE",
