6eaa49a025cab2fc8eeda5e974c81b6dc20245d0
Ken Corbin <ken@cadpage.org>
2022-08-18 15:12:47 -0700

    Fixed parsing problem with Dyer County, TN

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/TN/TNDyerCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/TN/TNDyerCountyParser.java
@@ -3,36 +3,39 @@ package net.anei.cadpage.parsers.TN;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import net.anei.cadpage.parsers.FieldProgramParser;
 import net.anei.cadpage.parsers.MsgInfo.Data;
-import net.anei.cadpage.parsers.dispatch.DispatchA49Parser;
 
-public class TNDyerCountyParser extends DispatchA49Parser {
+public class TNDyerCountyParser extends FieldProgramParser {
 
 
   public TNDyerCountyParser() {
-    super("DYER COUNTY","TN");
+    super("DYER COUNTY","TN", 
+          "DATE:DATE! TIME:TIME! EVENT_NUM:ID! ADDRESS:ADDR! EVENT_TYPE:CODE! REMARKS:INFO! INFO/N+ Incident_Type:CALL! Incident_Type:SKIP END");
   }
 
   @Override
   public String getFilter() {
-    return "cadalerts@dyersburgtn.gov";
+    return "alerts@tailorbuilt.app";
   }
+  
+  private static final Pattern DELIM = Pattern.compile("\n| (?=(?:TIME|EVENT NUM|Incident Type):)");
+  private static final Pattern CALL_GUIDE_PTN =  Pattern.compile(".*?\\*+CALL GUIDE\\*+ *(.*)");
 
   @Override
-  protected boolean parseMsg(String body, Data data) {
-    if (!super.parseMsg(body, data)) return false;
+  protected boolean parseMsg(String subject, String body, Data data) {
+    if (!subject.equals("System Alert")) return false;
+    if (!parseFields(DELIM.split(body), data)) return false;
     Matcher match = CALL_GUIDE_PTN.matcher(data.strCall);
     if (match.matches()) data.strCall = match.group(1);
     return true;
   }
-
-  private static final Pattern CALL_GUIDE_PTN =  Pattern.compile(".*?\\*+CALL GUIDE\\*+ *(.*)");
-
+  
   @Override
-  protected String fixCall(String call) {
-    Matcher match = CALL_GUIDE_PTN.matcher(call);
-    if (match.matches()) call = match.group(1);
-    return super.fixCall(call);
+  public Field getField(String name) {
+    if (name.equals("DATE")) return new DateField("\\d\\d/\\d\\d/\\d\\d", true);
+    if (name.equals("TIME")) return new TimeField("\\d\\d:\\d\\d:\\d\\d", true);
+    return super.getField(name);
   }
 
   private static final Pattern BY_PTN = Pattern.compile("\\bBY\\b", Pattern.CASE_INSENSITIVE);
