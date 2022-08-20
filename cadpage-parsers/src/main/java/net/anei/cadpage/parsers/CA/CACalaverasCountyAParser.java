f1967f70f623dbabcf6c71d1dfe120d6b9cf593c
Ken Corbin <ken@cadpage.org>
2022-08-19 20:59:06 -0700

    Fixed parsin gproblem with Calaveras COunty, CA (A)

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/CA/CACalaverasCountyAParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/CA/CACalaverasCountyAParser.java
@@ -4,24 +4,22 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import net.anei.cadpage.parsers.MsgInfo.Data;
-import net.anei.cadpage.parsers.MsgParser;
+import net.anei.cadpage.parsers.FieldProgramParser;
 
 
 /**
  * Calaveras County, CA
  */
-public class CACalaverasCountyAParser extends MsgParser {
+public class CACalaverasCountyAParser extends FieldProgramParser {
   
-  private static final Pattern MASTER = 
-      Pattern.compile("Inc# (\\d+):([^:]+):(?:([^@]+)@)?(.+?) *, *([A-Z_]+) *(?:\\(([^]]*?)\\) *)?:Map +([^:]*):(?: :)? LAT/LONG (X: [-+]?\\d+ \\d+\\.\\d+ +Y: [-+]?\\d+ \\d+\\.\\d+): ([^:]*)(?::([^:]*))?(?::.*)?");
   
   public CACalaverasCountyAParser() {
     this("CALAVERAS COUNTY");
   }
   
   protected CACalaverasCountyAParser(String defCity) {
-    super(defCity, "CA");
-    setFieldList("ID CALL PLACE ADDR APT CITY MAP GPS INFO UNIT");
+    super(defCity, "CA",
+          "ID CALL ADDRCITY CALL/SDS GPS! http:SKIP! Resources:UNIT! Command:CH! Tac:CH/L! Air:CH/L! Grd:CH/L! END");
   }
   
   @Override
@@ -38,12 +36,21 @@ public class CACalaverasCountyAParser extends MsgParser {
   public int getMapFlags() {
     return MAP_FLG_SUPPR_LA | MAP_FLG_PREFER_GPS;
   }
+  
+  private static final Pattern DELIM = Pattern.compile("\n+");
+  private static final Pattern MASTER = 
+      Pattern.compile("Inc# (\\d+):([^:]+):(?:([^@]+)@)?(.+?) *, *([A-Z_]+) *(?:\\(([^]]*?)\\) *)?:Map +([^:]*):(?: :)? LAT/LONG (X: [-+]?\\d+ \\d+\\.\\d+ +Y: [-+]?\\d+ \\d+\\.\\d+): ([^:]*)(?::([^:]*))?(?::.*)?");
 
   @Override
-  protected boolean parseMsg(String subject, String body, Data data) {
+  protected boolean parseMsg(String body, Data data) {
+    if (body.startsWith("Incident #")) {
+      return parseFields(DELIM.split(body), data);
+    }
     
     Matcher match = MASTER.matcher(body);
     if (!match.matches()) return false;
+    
+    setFieldList("ID CALL PLACE ADDR APT CITY MAP GPS INFO UNIT");
     data.strCallId = match.group(1);
     data.strCall = match.group(2);
     data.strPlace = getOptGroup(match.group(3));
@@ -56,5 +63,41 @@ public class CACalaverasCountyAParser extends MsgParser {
     data.strUnit = getOptGroup(match.group(10));
     
     return true;
+  }
+  
+  @Override
+  public Field getField(String name) {
+    if (name.equals("ID")) return new IdField("Incident #(\\d+)", true);
+    if (name.equals("ADDRCITY")) return new MyAddressCityField();
+    if (name.equals("GPS")) return new MyGPSField();
+    return super.getField(name);
+  }
+  
+  private class MyAddressCityField extends AddressCityField {
+    @Override
+    public void parse(String field, Data data) {
+      int pt = field.indexOf('@');
+      if (pt >= 0) {
+        data.strPlace = field.substring(0,pt).trim();
+        field = field.substring(pt+1).trim();
+      }
+      super.parse(field, data);
+      data.strCity = data.strCity.replace('_', ' ');
+    }
+    
+    @Override
+    public String getFieldNames() {
+      return "PLACE " + super.getFieldNames();
+    }
+  }
+  
+  private static final Pattern GPS_PTN = Pattern.compile("Lat: *(.*?); Long: *(.*)");
+  private class MyGPSField extends GPSField {
+    @Override
+    public void parse(String field, Data data) {
+      Matcher match = GPS_PTN.matcher(field);
+      if (!match.matches()) abort();
+      setGPSLoc(match.group(1)+','+match.group(2), data);
+    }
   }
 }
