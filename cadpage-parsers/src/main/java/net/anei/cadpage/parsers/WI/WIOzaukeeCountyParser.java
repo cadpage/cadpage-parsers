fd50ee40db8620e4010f9b7feaf72712b3f63d9c
Ken Corbin <ken@cadpage.org>
2022-08-29 15:21:07 -0700

    Fixed parsing problem witih Ozaukee County, WI

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/WI/WIOzaukeeCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/WI/WIOzaukeeCountyParser.java
@@ -8,7 +8,7 @@ import net.anei.cadpage.parsers.dispatch.DispatchA63Parser;
 
 public class WIOzaukeeCountyParser extends DispatchA63Parser {  
   public WIOzaukeeCountyParser() {
-    super(CITY_CODES, "OZAUKEE COUNTY", "WI");
+    super(CITY_CODES, CITY_LIST, "OZAUKEE COUNTY", "WI");
   }
   
   @Override
@@ -20,6 +20,51 @@ public class WIOzaukeeCountyParser extends DispatchA63Parser {
   public int getMapFlags() {
     return MAP_FLG_SUPPR_LA;
   }
+
+  private static final String[] CITY_LIST = new String[]{
+    // Cities
+    "Cedarburg",
+    "MEQUON",
+    "PORT WASHINGTON",
+    
+    // Villages
+    "BAYSIDE",
+    "BELGIUM",
+    "FREDONIA",
+    "GRAFTON",
+    "NEWBURG",
+    "SAUKVILLE",
+    "THIENSVILLE",
+    
+    // Towns
+    "BELGIUM",
+    "CEDARBURG",
+    "FREDONIA",
+    "GRAFTON",
+    "PORT WASHINGTON",
+    "SAUKVILLE",
+    
+    // Census-designated place
+    "WAUBEKA",
+    
+    // Unincorporated communities
+    "DACADA",
+    "DECKER",
+    "DECKER CORNER",
+    "DRUECKER",
+    "HAMILTON",
+    "HOLY CROSS",
+    "HORNS CORNERS",
+    "KNELLSVILLE",
+    "LAKE CHURCH",
+    "LAKEFIELD",
+    "LITTLE KOHLER",
+    "SAUK TRAIL BEACH",
+    "ULAO",
+    
+    // Ghost town
+    "STONEHAVEN"
+  };
   
   private static final Properties CITY_CODES = buildCodeTable (new String[]{
     "ME", "MEQUON"
