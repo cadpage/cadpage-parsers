2075f1835e06e66139d03a47b3286bb511cc331e
Kenneth Corbin <ken@cadpage.org>
2022-09-12 19:21:20 -0700

    Update sender filter for Park County, WY

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/WY/WYParkCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/WY/WYParkCountyParser.java
@@ -6,14 +6,14 @@ import net.anei.cadpage.parsers.dispatch.DispatchA20Parser;
  * Park County, WY
  */
 public class WYParkCountyParser extends DispatchA20Parser {
-  
+
   public WYParkCountyParser() {
     super("PARK COUNTY", "WY");
   }
-  
+
   @Override
   public String getFilter() {
-    return "@parkcounty.us,chief@pvfd.net";
+    return "@parkcounty.us,chief@pvfd.net,@parkcounty-wy.gov,@parkcountysheriff-wy.gov,bhcsheriffpaging@gmail.com";
   }
 
 }
