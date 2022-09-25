91cada77d06f0ffae26db702a367606109f1abf4
Kenneth Corbin <ken@cadpage.org>
2022-09-24 17:24:19 -0700

    Update msg doc

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/VA/VAGraysonCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/VA/VAGraysonCountyParser.java
@@ -9,45 +9,45 @@ import net.anei.cadpage.parsers.MsgInfo.Data;
 import net.anei.cadpage.parsers.dispatch.DispatchSouthernParser;
 
 public class VAGraysonCountyParser extends DispatchSouthernParser {
-  
+
   public VAGraysonCountyParser() {
     this("GRAYSON COUNTY", "VA");
   }
-  
+
   VAGraysonCountyParser(String defCity, String defState) {
-    super(CITY_LIST, defCity, defState, 
+    super(CITY_LIST, defCity, defState,
           DSFLG_PROC_EMPTY_FLDS|DSFLG_ADDR|DSFLG_BAD_PLACE|DSFLG_X|DSFLG_NAME|DSFLG_PHONE|DSFLG_CODE|DSFLG_UNIT1|DSFLG_ID |DSFLG_TIME);
   }
-  
+
   @Override
   public String getAliasCode() {
     return "VAGraysonCounty";
   }
-  
+
   private static final Pattern ZIP_PTN = Pattern.compile("\\d{5}");
 
   @Override
   protected boolean parseMsg(String body, Data data) {
-    
+
     if (!super.parseMsg(body, data)) return false;
-    
+
     if (data.strCity.length() == 0 && ZIP_PTN.matcher(data.strApt).matches()) {
       data.strCity = data.strApt;
       data.strApt = "";
     }
-    
+
     if (NC_CITY_SET.contains(data.strCity.toUpperCase())) data.strState = "NC";
-    
+
     return true;
   }
-  
+
   @Override
   public String getProgram() {
     return super.getProgram().replaceAll("CITY", "CITY ST");
   }
 
   private static final String[] CITY_LIST = new String[]{
-      
+
       // Grayson County
       // Towns
       "FRIES",
@@ -66,7 +66,7 @@ public class VAGraysonCountyParser extends DispatchSouthernParser {
       "RUGBY",
       "VOLNEY",
       "WHITETOP",
-      
+
       // Carroll County
       // Town
       "HILLSVILLE",
@@ -82,25 +82,25 @@ public class VAGraysonCountyParser extends DispatchSouthernParser {
       "LAMBSBURG",
       "LAUREL FORK",
       "SYLVATUS",
-      
+
       // Floyd County
       "INDIAN VALLEY",
-      
+
       // Indendent city
       "GALAX",
-      
+
       // ALleghany County, NC
       "SPARTA",
-      
+
       // Wythe County
       "AUSTINVILLE",
       "IVANHOE",
-      
+
       // North Carolina??
       "WINSTON",
       "27030"
   };
-  
+
   private static final Set<String> NC_CITY_SET = new HashSet<String>(Arrays.asList(
       "SPARTA",
       "WINSTON",
