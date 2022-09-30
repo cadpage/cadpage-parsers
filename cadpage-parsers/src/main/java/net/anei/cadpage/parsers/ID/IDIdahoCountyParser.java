8b55bb1fbe4456b7292e3bbd32f53c7cfdd44f93
Kenneth Corbin <ken@cadpage.org>
2022-09-30 13:34:22 -0700

    Added city code table for Idaho County, ID

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/ID/IDIdahoCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/ID/IDIdahoCountyParser.java
@@ -1,12 +1,14 @@
 package net.anei.cadpage.parsers.ID;
 
+import java.util.Properties;
+
 import net.anei.cadpage.parsers.MsgInfo.Data;
 import net.anei.cadpage.parsers.dispatch.DispatchA19Parser;
 
 public class IDIdahoCountyParser extends DispatchA19Parser {
 
   public IDIdahoCountyParser() {
-    super("IDAHO COUNTY", "ID");
+    super(CITY_CODES, "IDAHO COUNTY", "ID");
   }
 
   @Override
@@ -14,37 +16,39 @@ public class IDIdahoCountyParser extends DispatchA19Parser {
     return "spillmannotify@idahocounty.org";
   }
 
-  @Override
-  protected boolean parseMsg(String subject, String body, Data data) {
-    if (!super.parseMsg(subject, body, data)) return false;
-
-    // City code is just not reliable
-    data.strCity = "";
-    return true;
-  }
-
-//  "NEW" : "New Meadows",
-//  "KST" : "Stites",
-//  "KPD" : "Kooskia",
-//  "KOO" : "Kooskia",
-//  "RIG" : "Riggins",
-//  "ELC" : "Elk City",
-//  "KAM" : "Kamiah",
-//  "CRA" : "Craigmont",
-//  "GRA" : "Grangeville",
-//  "WHI" : "White Bird",
-//  "GPD" : "Grangeville",
-//  "MCC" : "McCall",
-//  "FER" : "Ferdinand",
-//  "CPD" : "Cottonwood",
-//  "KEU" : "Keuterville",
-//  "STI" : "Stites",
-//  "LUC" : "Lucile",
-//  "WIN" : "Winchester",
-//  "ELK" : "Elk City",
-//  "Rig City Limits" : "Riggins",
-//  "COT" : "Cottonwood",
-//  "POL" : "Pollock",
-//  "RPD" : "Riggins"
+private static final Properties CITY_CODES = buildCodeTable(new String[] {
+    "CLE", "Clearwater", //
+    "COT", "Cottonwood",
+    "CPD", "Cottonwood",
+    "CRA", "Craigmont",
+    "DIX", "Dixie", //
+    "ELC", "Elk City",
+    "ELK", "Elk City",
+    "ELW", "Elk City", //
+    "FEN", "Fenn", //
+    "FER", "Ferdinand",
+    "GPD", "Grangeville",
+    "GRA", "Grangeville",
+    "GRE", "Greencreek", //
+    "HAR", "Harpster", //
+    "KAM", "Kamiah",
+    "KEU", "Keuterville",
+    "KOO", "Kooskia",
+    "KPD", "Kooskia",
+    "KST", "Stites",
+    "LOW", "Lowell", //
+    "LUC", "Lucile",
+    "MCC", "McCall",
+    "NEW", "New Meadows",
+    "NEZ", "Nez Perce County",
+    "POL", "Pollock",
+    "POW", "Powell Junction", // ???
+    "Rig City Limits", "Riggins",
+    "RIG", "Riggins",
+    "RPD", "Riggins",
+    "STI", "Stites",
+    "WHI", "White Bird",
+    "WIN", "Winchester"
+});
 
 }
