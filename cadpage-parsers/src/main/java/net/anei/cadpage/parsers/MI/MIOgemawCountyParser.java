f888e0d218495fbe10874b3b0e1fa277cce195e4
Kenneth Corbin <ken@cadpage.org>
2022-09-29 22:15:49 -0700

    Fixed parsing problem with Ogemaw County, MI

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/MI/MIOgemawCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/MI/MIOgemawCountyParser.java
@@ -1,36 +1,20 @@
 package net.anei.cadpage.parsers.MI;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import net.anei.cadpage.parsers.dispatch.DispatchA19Parser;
 
-import net.anei.cadpage.parsers.MsgInfo.Data;
-import net.anei.cadpage.parsers.dispatch.DispatchOSSIParser;
+public class MIOgemawCountyParser extends DispatchA19Parser {
 
-public class MIOgemawCountyParser extends DispatchOSSIParser {
-  
   public MIOgemawCountyParser() {
-    super("OGEMAW COUNTY", "MI", 
-          "( CANCEL ADDR! SKIP " +
-          "| FYI? CALL ADDR! X+? " + 
-          ") INFO/N+");
-  }
-  
-  private static final Pattern MARKER = Pattern.compile("([A-Z0-9]+)\nCAD\n|ACTIVE 911 CAD ");
-  
-  @Override
-  protected boolean parseMsg(String body, Data data) {
-    Matcher match = MARKER.matcher(body);
-    if (match.lookingAt()) {
-      data.strSource = getOptGroup(match.group(1));
-      body = body.substring(match.end());
-    }
-    body = "CAD:" + body;
-    return super.parseMsg(body, data);
-  }
-  
-  @Override
-  public String getProgram() {
-    return "SRC " + super.getProgram();
+    super("OGEMAW COUNTY", "MI");
   }
 
+  @Override
+  public String getFilter() {
+    return "noreply@iosco911.com";
+  }
+
+  @Override
+  public int getMapFlags() {
+    return MAP_FLG_PREFER_GPS;
+  }
 }
