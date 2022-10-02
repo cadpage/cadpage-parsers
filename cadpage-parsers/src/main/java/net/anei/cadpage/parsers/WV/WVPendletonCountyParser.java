65386690bfb50629b6bf53cc324a362a406bbab8
Kenneth Corbin <ken@cadpage.org>
2022-10-01 19:57:18 -0700

    Update sender filter for  Pendleton County, WV

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/WV/WVPendletonCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/WV/WVPendletonCountyParser.java
@@ -3,13 +3,13 @@ package net.anei.cadpage.parsers.WV;
 import net.anei.cadpage.parsers.dispatch.DispatchSPKParser;
 
 public class WVPendletonCountyParser extends DispatchSPKParser {
-  
+
   public WVPendletonCountyParser() {
     super("PENDLETON COUNTY", "WV");
   }
-  
+
   @Override
   public String getFilter() {
-    return "pendleton@shentel.net";
+    return "pendleton@shentel.net,@pendletoncountywv.gov";
   }
 }
