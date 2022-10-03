7e85fad956bb0499d86bb19bd1fa24c1cebfb897
Kenneth Corbin <ken@cadpage.org>
2022-10-03 15:03:21 -0700

    Fixed parsing problem with St Louis County, MO (E)

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/MO/MOStLouisCountyEParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/MO/MOStLouisCountyEParser.java
@@ -1,93 +1,19 @@
 package net.anei.cadpage.parsers.MO;
 
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
 import java.util.Properties;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
-import net.anei.cadpage.parsers.FieldProgramParser;
-import net.anei.cadpage.parsers.MsgInfo.Data;
+import net.anei.cadpage.parsers.dispatch.DispatchH03Parser;
 
-public class MOStLouisCountyEParser extends FieldProgramParser {
+public class MOStLouisCountyEParser extends DispatchH03Parser {
 
   public MOStLouisCountyEParser() {
-    super(MOStLouisCountyParser.CITY_LIST, "ST LOUIS COUNTY", "MO", "Call_Received_Time:DATE_TIME_CALL! ADDR/S! UNIT_PLACE! Description:INFO+");
+    super(CITY_CODES, "ST LOUIS COUNTY", "MO");
     setupGpsLookupTable(GPS_LOOKUP_TABLE);
   }
 
   @Override
   public String getFilter() {
-    return "ECDC@rejis.org,Admin@east-central.org";
-  }
-
-  @Override
-  protected boolean parseMsg(String subject, String body, Data data) {
-    if (!subject.equals("_Dispatch") && !subject.equals("Dispatch")) return false;
-
-    body = body.replace("Description:", "\nDescription:");
-
-    return parseFields(body.split("\n"), data);
-  }
-
-  @Override
-  public Field getField(String name) {
-    if (name.equals("DATE_TIME_CALL")) return new MyDateTimeCallField();
-    if (name.equals("UNIT_PLACE")) return new MyUnitPlaceField();
-    if (name.equals("INFO")) return new MyInfoField();
-    return super.getField(name);
-  }
-
-  private static final Pattern DATE_TIME_CALL_PATTERN = Pattern.compile(" *(\\d{1,2}/\\d{1,2}/\\d{4}) *(\\d{1,2}:\\d{2}:\\d{2}(?: [AP]M)?) *(.*?)");
-  private static final DateFormat TIME_FMT = new SimpleDateFormat("hh:mm:ss aa");
-
-  private class MyDateTimeCallField extends Field {
-    @Override
-    public void parse(String field, Data data) {
-      Matcher mat = DATE_TIME_CALL_PATTERN.matcher(field);
-      if (!mat.matches()) abort();
-      data.strDate = mat.group(1);
-      String time = mat.group(2);
-      if (time.endsWith("M")) {
-        setTime(TIME_FMT, time, data);
-      } else {
-        data.strTime = time;
-      }
-      data.strCall = mat.group(3);
-    }
-
-    @Override
-    public String getFieldNames() {
-      return "DATE TIME CALL";
-    }
-  }
-
-  private static final Pattern UNIT_PLACE_PATTERN = Pattern.compile("((?:[A-Z\\-]{0,10}\\d{1,10}[ \\-]?)+) ?(?:\\*{2}CHECK BLDG NAME\\*{4})? *(.*?)?");
-
-  private class MyUnitPlaceField extends Field {
-    @Override
-    public void parse(String field, Data data) {
-      Matcher mat = UNIT_PLACE_PATTERN.matcher(field);
-      if (mat.matches()) {
-        data.strUnit = mat.group(1).trim();
-        data.strPlace = getOptGroup(mat.group(2));
-      } else data.strUnit = field;
-    }
-
-    @Override
-    public String getFieldNames() {
-      return "UNIT PLACE";
-    }
-  }
-
-  private static final Pattern INFO_BRK_PTN = Pattern.compile(" *\\[\\d{1,2}/\\d{1,2}/\\d{4} \\d{2}:\\d{2}:\\d{2} [A-Z]+\\] *");
-  private class MyInfoField extends InfoField {
-    @Override
-    public void parse(String field, Data data) {
-      for (String line : INFO_BRK_PTN.split(field)) {
-        super.parse(line, data);
-      }
-    }
+    return "ECDC@rejis.org";
   }
 
   private static final Properties GPS_LOOKUP_TABLE = buildCodeTable(new String[]{
@@ -205,5 +131,22 @@ public class MOStLouisCountyEParser extends FieldProgramParser {
       "156 YELLOW",                           "+38.649409,-90.308369",
       "251 YELLOW",                           "+38.646908,-90.305996",
       "257 YELLOW",                           "+38.646830,-90.304728"
+  });
+
+  private static final Properties CITY_CODES = buildCodeTable(new String[] {
+      "BRW",  "BRENTWOOD",
+      "CLY",  "CLAYTON",
+      "GLN",  "GLENDALE",
+      "LAD",  "LADUE",
+      "MPL",  "MAPLEWOOD",
+      "NOR",  "NORMANDY",
+      "OLV",  "OLIVETTE",
+      "RMH",  "RICHMOND HEIGHTS",
+      "RKH",  "ROCK HILL",
+      "SHR",  "SHREWSBURY",
+      "UCT",  "UNIVERSITY CITY",
+      "UNI",  "UNIVERSITY CITY",
+      "WEB",  "WEBSTER GROVES",
+      "WEL",  "WELSTON"
   });
 }
