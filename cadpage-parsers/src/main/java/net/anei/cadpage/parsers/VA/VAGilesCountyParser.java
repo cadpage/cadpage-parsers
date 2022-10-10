ec84d52f9b3b5d9f0c5c681e116ec48178785f53
Kenneth Corbin <ken@cadpage.org>
2022-10-10 16:37:04 -0700

    Fixed more prblems with Giles County, VA

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/VA/VAGilesCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/VA/VAGilesCountyParser.java
@@ -1,5 +1,9 @@
 package net.anei.cadpage.parsers.VA;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Properties;
+import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -10,7 +14,9 @@ public class VAGilesCountyParser extends DispatchSouthernParser {
 
   public VAGilesCountyParser() {
     super(CITY_LIST, "GILES COUNTY", "VA",
-          DSFLG_PROC_EMPTY_FLDS | DSFLG_ADDR | DSFLG_ADDR_TRAIL_PLACE | DSFLG_NAME | DSFLG_PHONE | DSFLG_CODE | DSFLG_UNIT1 | DSFLG_ID | DSFLG_TIME);
+          DSFLG_PROC_EMPTY_FLDS | DSFLG_ADDR | DSFLG_ADDR_TRAIL_PLACE | DSFLG_BAD_PLACE | DSFLG_PHONE | DSFLG_CODE | DSFLG_UNIT1 | DSFLG_ID | DSFLG_TIME);
+    setupCities(MAP_CITY_TABLE);
+    setupCities(WV_CITY_LIST);
   }
 
   private static final Pattern LEAD_PRI_PTN = Pattern.compile("(\\d);");
@@ -23,21 +29,24 @@ public class VAGilesCountyParser extends DispatchSouthernParser {
     body = body.substring(match.end()).trim();
     if (!super.parseMsg(body, data)) return false;
 
-    data.strCode = stripFieldEnd(data.strCall, "-");
-    data.strCall = data.strSupp;
-    data.strSupp = "";
-    int pt = data.strCall.indexOf('\n');
-    if (pt >= 0) {
-      data.strSupp = data.strCall.substring(pt+1).trim();
-      data.strCall = data.strCall.substring(0,pt).trim();
-    }
+    data.strAddress = data.strAddress.replace("KATHERINE", "CATHERINE");
+    if (WV_CITY_SET.contains(data.strCity)) data.strState = "WV";
     return true;
   }
 
   @Override
   public String getProgram() {
-    return "PRI " + super.getProgram();
+    return "PRI " + super.getProgram().replace("CITY", "CITY ST");
   }
+
+  @Override
+  public String adjustMapCity(String city) {
+    return convertCodes(city, MAP_CITY_TABLE);
+  }
+
+  private static final Properties MAP_CITY_TABLE = buildCodeTable(new String[] {
+      "POPLAR HILL",    "PEARISBURG"
+  });
 
   private static final String[] CITY_LIST = new String[]{
 
@@ -59,7 +68,37 @@ public class VAGilesCountyParser extends DispatchSouthernParser {
       "RIPPLEMEAD",
       "STAFFORDSVILLE",
       "TRIGG",
-      "WHITE GATE"
+      "WHITE GATE",
+
+      // Independent Cities
+      "ROANOKE",
+
+      // Bland County
+      "BLAND",
+
+      // Craig County
+      "CRAIG",
+      "CRAIG COUNTY",
+      "NEW CASTLE",
+      "NEWCASTLE",
+
+      // Pulaski County
+      "DUBLIN",
+
+      // Smyth county
+      "MARION"
   };
 
+  private static final String[] WV_CITY_LIST = new String[] {
+
+      // Monroe County, WV
+      "BALLARD",
+      "LINDSIDE",
+      "PETERSTOWN",
+
+      // Mercer County, WV
+      "SPANISHBURG"
+  };
+
+  private static final Set<String> WV_CITY_SET = new HashSet<>(Arrays.asList(WV_CITY_LIST));
 }
