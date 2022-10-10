3d19b921a11a3f74ab2d740cf0acbe32afa0376e
Kenneth Corbin <ken@cadpage.org>
2022-10-10 14:16:47 -0700

    Fixed parsing problem with Giles County, VA

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/VA/VAGilesCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/VA/VAGilesCountyParser.java
@@ -7,14 +7,14 @@ import net.anei.cadpage.parsers.MsgInfo.Data;
 import net.anei.cadpage.parsers.dispatch.DispatchSouthernParser;
 
 public class VAGilesCountyParser extends DispatchSouthernParser {
-  
+
   public VAGilesCountyParser() {
-    super(CITY_LIST, "GILES COUNTY", "VA", 
-          DSFLG_ADDR | DSFLG_ADDR_TRAIL_PLACE | DSFLG_OPT_PHONE  | DSFLG_UNIT1 | DSFLG_ID | DSFLG_TIME);
+    super(CITY_LIST, "GILES COUNTY", "VA",
+          DSFLG_PROC_EMPTY_FLDS | DSFLG_ADDR | DSFLG_ADDR_TRAIL_PLACE | DSFLG_NAME | DSFLG_PHONE | DSFLG_CODE | DSFLG_UNIT1 | DSFLG_ID | DSFLG_TIME);
   }
-  
+
   private static final Pattern LEAD_PRI_PTN = Pattern.compile("(\\d);");
-  
+
   @Override
   protected boolean parseMsg(String body, Data data) {
     Matcher match = LEAD_PRI_PTN.matcher(body);
@@ -22,7 +22,7 @@ public class VAGilesCountyParser extends DispatchSouthernParser {
     data.strPriority = match.group(1);
     body = body.substring(match.end()).trim();
     if (!super.parseMsg(body, data)) return false;
-    
+
     data.strCode = stripFieldEnd(data.strCall, "-");
     data.strCall = data.strSupp;
     data.strSupp = "";
@@ -33,21 +33,21 @@ public class VAGilesCountyParser extends DispatchSouthernParser {
     }
     return true;
   }
-  
+
   @Override
   public String getProgram() {
     return "PRI " + super.getProgram();
   }
 
   private static final String[] CITY_LIST = new String[]{
-      
+
       // Towns
       "GLEN LYN",
       "NARROWS",
       "PEARISBURG",
       "PEMBROKE",
       "RICH CREEK",
-      
+
       // Unincorporated communities
       "EGGLESTON",
       "GOLDBOND",
