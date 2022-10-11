06598bd2c06c3b85fb4ab54979ebfc421c9868e5
Kenneth Corbin <ken@cadpage.org>
2022-10-10 21:24:46 -0700

    Fixed parsing problem with Schuyler County, MO

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/MO/MOSchuylerCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/MO/MOSchuylerCountyParser.java
@@ -1,9 +1,15 @@
 package net.anei.cadpage.parsers.MO;
 
-public class MOSchuylerCountyParser extends MOMaconCountyParser {
+import net.anei.cadpage.parsers.dispatch.DispatchBCParser;
 
+public class MOSchuylerCountyParser extends DispatchBCParser {
   public MOSchuylerCountyParser() {
     super("SCHUYLER COUNTY", "MO");
+  }
+
+  @Override
+  public String getFilter() {
+    return "noreply@omnigo.com";
   }
 
 }
