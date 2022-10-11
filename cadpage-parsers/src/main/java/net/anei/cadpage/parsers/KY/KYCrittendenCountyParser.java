c0e21f2e21932b902c814b692426aa82a8f318bd
Kenneth Corbin <ken@cadpage.org>
2022-10-11 16:47:10 -0700

    Fixed parsing problem with Crittenden County, KY

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/KY/KYCrittendenCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/KY/KYCrittendenCountyParser.java
@@ -1,37 +1,20 @@
 package net.anei.cadpage.parsers.KY;
 
-import net.anei.cadpage.parsers.MsgInfo.Data;
-import net.anei.cadpage.parsers.SmartAddressParser;
+import net.anei.cadpage.parsers.dispatch.DispatchA27Parser;
 
-public class KYCrittendenCountyParser extends SmartAddressParser {
+public class KYCrittendenCountyParser extends DispatchA27Parser {
 
   public KYCrittendenCountyParser() {
     super("CRITTENDEN COUNTY", "KY");
-    setFieldList("CALL PLACE ADDR APT");
   }
 
   @Override
   public String getFilter() {
-    return "page@relay2.thefirehorn.com";
+    return "marcritcoe911@cissystem.com";
   }
 
   @Override
-  protected boolean parseMsg(String subject, String body, Data data) {
-    if (!subject.equals("Page|TheFireHorn/NextDispatch")) return false;
-    if (body.startsWith("-")) body = ' ' + body;
-    int pt = body.indexOf(" - ");
-    if (pt < 0) return false;
-
-    data.strCall = body.substring(0,pt).trim();
-    String addr = body.substring(pt+3).trim();
-
-    parseAddress(StartType.START_PLACE, FLAG_RECHECK_APT | FLAG_ANCHOR_END, addr, data);
-    if (data.strAddress.isEmpty()) {
-      parseAddress(data.strPlace, data);
-      data.strPlace = "";
-    }
-    data.strApt = stripFieldStart(data.strApt, "-");
-    return true;
+  public int getMapFlags() {
+    return MAP_FLG_PREFER_GPS;
   }
-
 }
