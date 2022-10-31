7969a750fa4dba804f01f1df9390d5c51bea1593
Kenneth Corbin <ken@cadpage.org>
2022-10-31 15:09:13 -0700

    Fixed parsing problelm with Dallas County, TX (F)

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/TX/TXDallasCountyFParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/TX/TXDallasCountyFParser.java
@@ -1,12 +1,16 @@
 package net.anei.cadpage.parsers.TX;
 
-import net.anei.cadpage.parsers.dispatch.DispatchSouthernParser;
+import net.anei.cadpage.parsers.dispatch.DispatchA71Parser;
 
-public class TXDallasCountyFParser extends DispatchSouthernParser {
-  
+public class TXDallasCountyFParser extends DispatchA71Parser {
+
   public TXDallasCountyFParser() {
-    super(TXDallasCountyParser.CITY_LIST, "DALLAS COUNTY", "TX",
-          DSFLG_PROC_EMPTY_FLDS | DSFLG_ADDR | DSFLG_ADDR_TRAIL_PLACE | DSFLG_OPT_X | DSFLG_OPT_CODE | DSFLG_ID |  DSFLG_TIME);
+    super("DALLAS COUNTY", "TX");
+  }
+
+  @Override
+  public int getMapFlags() {
+    return MAP_FLG_PREFER_GPS;
   }
 
 }
