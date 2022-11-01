f2ee62273197b8805a628be9b492963a48a001e1
Kenneth Corbin <ken@cadpage.org>
2022-10-31 17:13:30 -0700

    Fixed parsing problem with Dallas County, TX (C)

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/TX/TXDallasCountyCParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/TX/TXDallasCountyCParser.java
@@ -1,9 +1,6 @@
 package net.anei.cadpage.parsers.TX;
 
-import java.util.Arrays;
-import java.util.HashSet;
 import java.util.Properties;
-import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -11,54 +8,40 @@ import net.anei.cadpage.parsers.MsgInfo.Data;
 import net.anei.cadpage.parsers.dispatch.DispatchOSSIParser;
 
 public class TXDallasCountyCParser extends DispatchOSSIParser {
-  
+
   public TXDallasCountyCParser() {
     super(CITY_CODES, "DALLAS COUNTY", "TX",
-          "( CANCEL ADDR CITY " +
-          "| FYI ( CODE1 SRC! " +
-                "| SRC UNIT? PLACE? CODE CALL X2+? ( ZERO | P ONE! ) " +
-                "| ADDR PLACE1? ( CITY CITY2? | ) ( CODE1 SRC! " +
-                                                 "| SRC MAP? UNIT? PLACE? CODE CALL X2+? ( ZERO | P ONE! ) " +
-                                                 ") " +
-                ") INFO+? MARK X2+? P ONE CITY? CALL BOX ID? " +
-          ") INFO+");
+          "( CANCEL ADDR CITY? " +
+          "| UNIT/Z ENROUTE ADDR CITY CALL/SDS END " +
+          "| FYI? ID MAP UNIT CALL ADDR X X CITY PLACE CH! "+
+          ") INFO/N+");
   }
-  
-  @Override
-  public String getFilter() {
-    return "CAD@ntecc.org";
-  }
-  
+
   @Override
   public int getMapFlags() {
     return MAP_FLG_SUPPR_LA;
   }
- 
-  
-  private static final Pattern TRAIL_MARK_PTN = Pattern.compile("(.*\\])(.*;[P12](?:$|;1.*))");
+
+
   private static final Pattern STREET_APT_ADDR_PTN = Pattern.compile("(\\d+)-(\\d+) +(.*)");
-  
+
   @Override
-  protected boolean parseMsg(String subject, String body, Data data) {
-    if (subject.startsWith("FYI:") || subject.startsWith("Update:")) {
-      body = subject + ';' + body;
-    }
+  protected boolean parseMsg(String body, Data data) {
     if (!body.startsWith("CAD:")) body = "CAD:" + body;
-    
-    body = body.replace("\n", "");
-    Matcher match = TRAIL_MARK_PTN.matcher(body);
-    if (match.matches()) body = match.group(1)+"<<%MARK%>>;" + match.group(2);
-    
-    if (!super.parseMsg(body, data)) return false;
-    if (data.strCall.length() == 0) {
-      data.strCall = data.strCode;
-      data.strCode = "";
+
+//    body = body.replace("\n", "");
+
+    if (body.contains(",Enroute,")) {
+      if (!super.parseFields(body.split(","), data)) return false;
     }
-    
+    else {
+      if (!super.parseMsg(body, data)) return false;
+    }
+
     // A leading nnn-nnn address might be a regular address range
     // but more often that not it will be address-apt combination.
     // See if we can figure them out
-    match = STREET_APT_ADDR_PTN.matcher(data.strAddress);
+    Matcher match = STREET_APT_ADDR_PTN.matcher(data.strAddress);
     if (match.matches()) {
       String stno1 = match.group(1);
       String stno2 = match.group(2);
@@ -71,108 +54,22 @@ public class TXDallasCountyCParser extends DispatchOSSIParser {
     }
     return true;
   }
-  
-  @Override
-  public String getProgram() {
-    return super.getProgram().replace("CODE", "CODE CALL?");
-  }
-  
+
   @Override
   public Field getField(String name) {
-    if (name.equals("PLACE1")) return new MyPlace1Field();
-    if (name.equals("CITY2")) return new MyCity2Field();
-    if (name.equals("CODE")) return new CodeField("[A-Z]{2,6}", true);
-    if (name.equals("SRC")) return new SourceField("[A-Z]{3,4}", true);
-    if (name.equals("MAP")) return new MapField("\\d{4}", true);
-    if (name.equals("UNIT")) return new MyUnitField();
-    if (name.equals("X2")) return new MyCross2Field();
-    if (name.equals("ZERO")) return new SkipField("0", true);
-    if (name.equals("P")) return new SkipField("[P12]", true);
-    if (name.equals("ONE")) return new SkipField("1", true);
-    if (name.equals("INFO")) return new MyInfoField();
-
-    // Supporting obsolete format
-    if (name.equals("CODE1")) return new MyCode1Field();
-    if (name.equals("MARK")) return new SkipField("<<%MARK%>>");
-    if (name.equals("BOX")) return new BoxField("\\d+", true);
+    if (name.equals("ENROUTE")) return new CallField("Enroute", true);
     if (name.equals("ID")) return new IdField("\\d{10}", true);
+    if (name.equals("MAP")) return new MapField("\\d{4}|", true);
+    if (name.equals("CH")) return new ChannelField("TAC *\\d+|", true);
+    if (name.equals("INFO")) return new MyInfoField();
     return super.getField(name);
   }
-  
-  private static final Pattern PLACE_PTN = Pattern.compile("\\(S\\) *(.*?) *\\(N\\)");
-  private static final Pattern NOT_PLACE_PTN = Pattern.compile("[A-Z]{3,4}");
-  private class MyPlace1Field extends PlaceField {
-    
-    @Override
-    public boolean canFail() {
-      return true;
-    }
-    
-    @Override
-    public boolean checkParse(String field, Data data) {
-      Matcher match = PLACE_PTN.matcher(field);
-      if (match.matches()) {
-        data.strPlace = match.group(1);
-        return true;
-      }
-      
-      if (NOT_PLACE_PTN.matcher(field).matches()) return false;
-      data.strPlace = field;
-      return true;
-    }
-    
-    @Override
-    public void parse(String field, Data data) {
-      if (!checkParse(field, data)) abort();
-    }
-  }
-  
-  private class MyCity2Field extends SkipField {
-    @Override
-    public boolean canFail() {
-      return true;
-    }
-    
-    @Override
-    public boolean checkParse(String field, Data data) {
-      return field.equals(getRelativeField(-1));
-    }
-  }
-  
-  private static final Pattern UNIT_PTN = Pattern.compile("[A-Z]+\\d+|.*,.*");
-  private class MyUnitField extends UnitField {
-    public MyUnitField() {
-      setPattern(UNIT_PTN, true);
-    }
-  }
 
-
-  private static final Pattern CROSS_TAC_PTN = Pattern.compile("TAC *\\S*");
-  private class MyCross2Field extends Field {
-    @Override
-    public void parse(String field, Data data) {
-      if (CROSS_TAC_PTN.matcher(field).matches()) {
-        data.strChannel = field;
-        return;
-      }
-      
-      if (UNIT_PTN.matcher(field).matches()) {
-        data.strUnit = append(data.strUnit, ",", field);
-        return;
-      }
-      data.strCross = append(data.strCross, "/", field);
-    }
-
-    @Override
-    public String getFieldNames() {
-      return "X CH UNIT";
-    }
-  }
-  
+  private static final Pattern TAC_PTN = Pattern.compile("TAC *\\S*");
   private class MyInfoField extends InfoField {
     @Override
     public void parse(String field, Data data) {
-      if (CROSS_TAC_PTN.matcher(field).matches()) {
+      if (TAC_PTN.matcher(field).matches()) {
         data.strChannel = field;
       } else if (field.startsWith("Radio Channel:")) {
         data.strChannel = field.substring(14).trim();
@@ -180,57 +77,19 @@ public class TXDallasCountyCParser extends DispatchOSSIParser {
         super.parse(field, data);
       }
     }
-    
+
     @Override
     public String getFieldNames() {
       return "CH? INFO";
     }
   }
-  
-  private class MyCode1Field extends CodeField {
-    @Override
-    public boolean canFail() {
-      return true;
-    }
-    
-    @Override
-    public boolean checkParse(String field, Data data) {
-      if (!CODE_SET.contains(field)) return false;
-      data.strCode = field;
-      return true;
-    }
-    
-    @Override
-    public void parse(String field, Data data) {
-      if (!checkParse(field, data)) abort();
-    }
-  }
-  
+
   @Override
   public String adjustMapCity(String city) {
     if (city.equals("DFW AIRPORT")) city = "DALLAS/FORT WORTH AIRPORT";
     return super.adjustMapCity(city);
   }
-  
-  // This is only needed for historical calls, so we don't have to worry about it
-  // being incomplete
-  private static final Set<String> CODE_SET = new HashSet<String>(Arrays.asList(
-      "CFA",
-      "FFA",
-      "GF",
-      "HAZCON",
-      "JAILME",
-      "MAJOR",
-      "MA",
-      "ME",
-      "MUTUAL",
-      "PUBLIC",
-      "RFA",
-      "SF",
-      "UNLK",
-      "UNLKEM"
-  ));
-  
+
   private static final Properties CITY_CODES = buildCodeTable(new String[]{
       "ADDI", "ADDISON",
       "CARR", "CARROLLTON",
--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/dispatch/DispatchOSSIParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/dispatch/DispatchOSSIParser.java
@@ -15,56 +15,56 @@ import net.anei.cadpage.parsers.MsgInfo.Data;
  * Format always starts with "CAD:" and generally consists of a series of
  * semicolon delimited fields which will be processed according to a program
  * passed from the subclass.
- * 
+ *
  * There are, of course, some exception
  * The text may contain a leading numeric call ID and a colon before the CAD:
  * marker.  Subclasses will indicate this by inserting an ID: term as the
  * first field term in the program
- * 
+ *
  * The text may contain some text in square brackets.  This comes in two flavors
- * 
+ *
  * [mm/dd/yy hh:mm:ss xxxxxx] appears to be an subsequent update marker, it will
  * be treated as field delimiter
- * 
+ *
  * [Medical Priority Info] or [Fire Priority Info]
  * The field that follows this may one of the two following formats
  * RESPONSE: xxxxxx RESPONDER SCRIPT: xxxxxx
  * PROBLEM: xxxxx # PATS: n AGE:  SEX:  xxxxxxx
- * 
+ *
  * In this case we will skip all text up to and including the keyword
  * "RESPONDER SCRIPT: or "PROBLEM:"
- * 
+ *
  * In addition, the last field in the string may be a date/time indicator
  * or a truncated piece of a date/time indicator.  If this is the case
  * drop it from further consideration
  **/
 
 public class DispatchOSSIParser extends FieldProgramParser {
-  
+
   private static final Pattern LEAD_ID_PTN = Pattern.compile("^(\\d+):");
   private static final Pattern DATE_TIME_PTN = Pattern.compile("(\\d\\d/\\d\\d/\\d{2,4}) (\\d\\d:\\d\\d:\\d\\d)\\b");
-  
+
   private boolean leadID = false;
   private boolean optLeadId = false;
   private boolean dateTime = false;
   private boolean dateTimeReq = false;
-  
+
   // Pattern searching for a leading square bracket or semicolon
   private Pattern delimPattern = Pattern.compile("\\[|;");
-  
+
   // Pattern searching for "PROBLEM: or "RESPONDER SCRIPT:"
   private static final Pattern KEYWORD = Pattern.compile("\\b(?:PROBLEM:|RESPONDER SCRIPT:)");
-  
+
   public DispatchOSSIParser(String defCity, String defState, String program) {
     super(defCity, defState, "SKIP");
     setup(program);
   }
-  
+
   public DispatchOSSIParser(Properties cityCodes, String defCity, String defState, String program) {
     super(cityCodes, defCity, defState, "SKIP");
     setup(program);
   }
-  
+
   public DispatchOSSIParser(String[] cityList, String defCity, String defState, String program) {
     super(cityList, defCity, defState, "SKIP");
     setup(program);
@@ -90,17 +90,17 @@ public class DispatchOSSIParser extends FieldProgramParser {
     }
     setProgram(program, 0);
   }
-  
+
   protected void setDelimiter(char delim) {
     delimPattern = Pattern.compile("\\[|" + delim);
   }
 
   @Override
   protected boolean parseMsg(String body, Data data) {
-    
+
     // Strip off leading /
     if (body.startsWith("/")) body = body.substring(1).trim();
-    
+
     // If format has a leading ID, strip that off
     if (leadID) {
       Matcher match = LEAD_ID_PTN.matcher(body);
@@ -109,13 +109,13 @@ public class DispatchOSSIParser extends FieldProgramParser {
         body = body.substring(match.end()).trim();
       } else if (!optLeadId) return false;
     }
-    
+
     // Body must start with 'CAD:'
     if (!body.startsWith("CAD:")) return false;
-    
+
     // Break down string into generally semicolon delimited fields
     // with some complications involving text in square brackets
-    
+
     List<String> fields = new ArrayList<String>();
     Matcher match = delimPattern.matcher(body);
     int st = 4;
@@ -123,7 +123,7 @@ public class DispatchOSSIParser extends FieldProgramParser {
 
     boolean priInfo = false;
     while (st < body.length()) {
-      
+
       // search for the next delimiter (either ; or [
       char delim;
       int pt;
@@ -140,7 +140,7 @@ public class DispatchOSSIParser extends FieldProgramParser {
       // If the delimiter in front of this term was a square bracket term
       // Search for and delete any text up to including the keywords
       // "PROBLEM:" or "RESPONDER SCRIPT:"
-      
+
       String field = body.substring(st,pt).trim();
       if (priInfo) {
         Matcher match2 = KEYWORD.matcher(field);
@@ -148,7 +148,7 @@ public class DispatchOSSIParser extends FieldProgramParser {
         else field = field.substring(match2.end()).trim();
       }
       if (field != null) fields.add(field);
-      
+
       // Find the start of the next field
       // if the delimiter was an open square bracket, the start of the
       // next field will follow the closing square bracket
@@ -172,7 +172,7 @@ public class DispatchOSSIParser extends FieldProgramParser {
 
     int ndx = fields.size()-1;
     if (ndx < 0) return false;
-    
+
     // Almost there.  Check to see if the last term looks like a date/time stamp
     // or the truncated remains of a date/time stamp.  If it does, remove it
     String field = fields.get(ndx);
@@ -190,22 +190,22 @@ public class DispatchOSSIParser extends FieldProgramParser {
           if (field.length() == 8) data.strTime = field;
           else if (field.length() >= 5) data.strTime = field.substring(0,5);
         }
-          
+
         fields.remove(ndx);
       }
       else if (dateTimeReq) return false;
     }
-      
+
     // We have a nice clean array of data fields, pass it to the programmer
     // field processor to parse
     return parseFields(fields.toArray(new String[fields.size()]), data);
   }
-  
+
   @Override
   public String getProgram() {
     return (leadID ? "ID " : "") + super.getProgram() + " DATE TIME";
   }
-  
+
   @Override
   protected Field getField(String name) {
     if (name.equals("FYI")) return new SkipField("FYI:|Update:", true);
@@ -213,27 +213,27 @@ public class DispatchOSSIParser extends FieldProgramParser {
     if (name.equals("DATETIME")) return new DateTimeField("\\d\\d/\\d\\d/\\d{4} +\\d\\d:\\d\\d:\\d\\d", true);
     return super.getField(name);
   }
-  
+
   private static final Pattern UNIT_PFX_PTN = Pattern.compile("(?:\\{([A-Z0-9,]+)\\} *)(.*)");
-  private static final Pattern CANCEL_PTN = Pattern.compile("(CANCEL\\b.*|.*\\bCOMMAND ESTABLISHED|CODE RED|CONFIRMED FIRE.*|CONFIRMED PIN IN|FIRE OUT|FIRE UNDER CONTROL|UNDER CONTROL|UNITS RESPOND CODE 1)");
+  private static final Pattern CANCEL_PTN = Pattern.compile("(CANCEL\\b.*|.*\\bCOMMAND ESTABLISHED|CODE RED|CONFIRMED FIRE.*|CONFIRMED PIN IN|FIRE OUT|FIRE UNDER CONTROL|UNDER CONTROL|UNITS RESPOND CODE 1|WORKING FIRE)");
   protected class BaseCancelField extends CallField {
-    
+
     private Pattern extraPattern;
-    
+
     public BaseCancelField() {
       this(null);
     }
-    
+
     public BaseCancelField(String pattern) {
       this.extraPattern = (pattern == null ? null : Pattern.compile(pattern));
     }
-    
-    
+
+
     @Override
     public boolean canFail() {
       return true;
     }
-    
+
     @Override
     public boolean checkParse(String field, Data data) {
       Matcher match = UNIT_PFX_PTN.matcher(field);
@@ -250,12 +250,12 @@ public class DispatchOSSIParser extends FieldProgramParser {
       data.strCall = field;
       return true;
     }
-    
+
     @Override
     public void parse(String field, Data data) {
       if (!checkParse(field, data)) abort();
     }
-    
+
     @Override
     public String getFieldNames() {
       return "UNIT? CALL";
