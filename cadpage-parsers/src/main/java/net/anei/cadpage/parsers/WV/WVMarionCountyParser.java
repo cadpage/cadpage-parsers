d48e85f0ce11b714acb7203d3e58e492e879c578
Kenneth Corbin <ken@cadpage.org>
2022-11-07 15:18:16 -0800

    Fixed parsing problem with Marion COunty, WV

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/WV/WVMarionCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/WV/WVMarionCountyParser.java
@@ -3,13 +3,13 @@ package net.anei.cadpage.parsers.WV;
 import net.anei.cadpage.parsers.dispatch.DispatchA81Parser;
 
 public class WVMarionCountyParser extends DispatchA81Parser {
-  
+
   public WVMarionCountyParser() {
     super("MARION COUNTY", "WV");
   }
-  
+
   @Override
   public String getFilter() {
-    return "marion911cad@gmail.com,marion911cad@marioncounty911.org,marion911@marioncountywv.com";
+    return "marion911cad@gmail.com";
   }
 }
--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/dispatch/DispatchA81Parser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/dispatch/DispatchA81Parser.java
@@ -4,34 +4,40 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import net.anei.cadpage.parsers.MsgInfo.Data;
-import net.anei.cadpage.parsers.MsgParser;
+import net.anei.cadpage.parsers.FieldProgramParser;
 
-public class DispatchA81Parser extends MsgParser {
-  
+public class DispatchA81Parser extends FieldProgramParser {
+
   public DispatchA81Parser(String defCity, String defState) {
-    super(defCity, defState);
-    setFieldList("CALL ID DATE TIME ADDR APT CITY ST INFO");
+    super(defCity, defState,
+          "ID DATETIME ADDRCITYST! END");
   }
-  
+
   private static final Pattern MASTER = Pattern.compile("([A-Z]{3}\\d{10}) (\\d\\d/\\d\\d/\\d\\d) (\\d\\d:\\d\\d)\\b *(.*)");
   private static final Pattern INFO_MARK_PTN = Pattern.compile(";? \\d\\d?/\\d\\d?/\\d\\d \\d\\d?:\\d\\d:\\d\\d - ");
   private static final Pattern TIMES_MARK_PTN = Pattern.compile(" [-/A-Z0-9]+ - (?:Assign|Enroute|On Scene|Available) \\d\\d?/\\d\\d?/\\d\\d \\d\\d?:\\d\\d:\\d\\d\\b");
   private static final Pattern STATE_ZIP_PTN = Pattern.compile("([A-Z]{2})(?: (\\d{5}))");
-  
+
   @Override
   protected boolean parseMsg(String subject, String body, Data data) {
-    
+
     data.strCall = subject.length() > 0 ? subject : "ALERT";
-    
+
+    String[] flds = body.split(" :");
+    if (flds.length >= 3) {
+      return parseFields(flds, data);
+    }
+
     Matcher match = MASTER.matcher(body);
     if (!match.matches()) return false;
+    setFieldList("ID DATE TIME ADDR APT CITY ST INFO");
     data.strCallId = match.group(1);
     data.strDate = match.group(2);
     data.strTime = match.group(3);
     body = match.group(4);
-    
+
     body = stripFieldEnd(body, "{incident_code_description");
-    
+
     match = INFO_MARK_PTN.matcher(body);
     if (match.find()) {
       int pt = match.start();
@@ -43,7 +49,7 @@ public class DispatchA81Parser extends MsgParser {
       data.strSupp = append(data.strSupp, "\n", body.substring(spt).trim());
       body = body.substring(0,pt).trim();
     }
-    
+
     match = TIMES_MARK_PTN.matcher(body);
     if (match.find()) {
       String times = body.substring(match.start()).trim().replace("; ", "\n");
@@ -62,5 +68,16 @@ public class DispatchA81Parser extends MsgParser {
     data.strCity = city;
     parseAddress(p.get(), data);
     return true;
+  }
+
+  @Override
+  public String getProgram() {
+    return "CALL " + super.getProgram();
+  }
+
+  @Override
+  public Field getField(String name) {
+    if (name.equals("DATETIME")) return new DateTimeField("\\d\\d/\\d\\d/\\d\\d \\d\\d:\\d\\d", true);
+    return super.getField(name);
   }
 }
