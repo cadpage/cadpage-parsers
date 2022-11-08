80ea5bb4efae753a3d1a8c9e82a03e2180013d31
Kenneth Corbin <ken@cadpage.org>
2022-11-07 19:45:17 -0800

    Update sender filter for St Louis Park, MN

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/MN/MNStLouisParkParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/MN/MNStLouisParkParser.java
@@ -10,19 +10,19 @@ import net.anei.cadpage.parsers.dispatch.DispatchA60Parser;
  * St Louis Park, MN
  */
 public class MNStLouisParkParser extends DispatchA60Parser {
-  
+
   public MNStLouisParkParser() {
     super("ST LOUIS PARK", "MN");
   }
-  
+
   @Override
   public String getFilter() {
-    return "zuercher@stlouispark.org";
+    return "zuercher@stlouispark.org,@stlouispark.gov";
   }
-  
+
   private static final Pattern SPEC_CALL_PTN = Pattern.compile("There is an? (All Call|Group 2)(?: for)? ");
   private static final Pattern TAIL_DATE_TIME_PTN = Pattern.compile(": \\d\\d/\\d\\d/\\d\\d \\d\\d:\\d\\d$");
-  
+
   @Override
   protected boolean parseMsg(String subject, String body, Data data) {
     if (subject.length() == 0) return false;
