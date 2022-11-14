7b87cd13aaaa3372630a004328ae6c70bc72e5db
Kenneth Corbin <ken@cadpage.org>
2022-11-14 12:15:05 -0800

    Fixed parsing problem with Logan County, OH (B)

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/OH/OHLoganCountyBParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/OH/OHLoganCountyBParser.java
@@ -1,47 +1,20 @@
 package net.anei.cadpage.parsers.OH;
 
-import net.anei.cadpage.parsers.FieldProgramParser;
-import net.anei.cadpage.parsers.MsgInfo.Data;
+import net.anei.cadpage.parsers.dispatch.DispatchA19Parser;
 
-public class OHLoganCountyBParser extends FieldProgramParser {
+public class OHLoganCountyBParser extends DispatchA19Parser {
 
   public OHLoganCountyBParser() {
-    super("LOGAN COUNTY", "OH",
-           "CFS:ID! RUN#:SKIP! CODE:CALL! PLACE:PLACE! LAT:GPS1! LONG:GPS2! ADDR:ADDR! CITY:CITY! CROSS:X! PRI:PRI! DATE:DATE! TIME:TIME! SECT:MAP! DIST:SRC! UNIT:UNIT! DESC:INFO/N+");
+    super("LOGAN COUNTY", "OH");
   }
 
   @Override
   public String getFilter() {
-    return "lcso911@co.logan.oh.us,lcso911@logancountyohio.gov";
+    return "FlexRapidNotification@dccnotify.com";
   }
 
   @Override
   public int getMapFlags() {
     return MAP_FLG_PREFER_GPS;
   }
-
-  @Override
-  protected boolean parseMsg(String subject, String body, Data data) {
-    if (!subject.startsWith("CFS:")) return false;
-    int pt = body.indexOf("\n\n\n");
-    if (pt >= 0) body = body.substring(0, pt).trim();
-    return parseFields(body.split("\n"), data);
-  }
-
-  @Override
-  public Field getField(String name) {
-    if (name.equals("X")) return new MyCrossField();
-    if (name.equals("DATE")) return new DateField("\\d\\d/\\d\\d/\\d{4}", true);
-    if (name.equals("TIME")) return new TimeField("\\d\\d:\\d\\d:\\d\\d", true);
-    return super.getField(name);
-  }
-
-  private class MyCrossField extends CrossField {
-    @Override
-    public void parse(String field, Data data) {
-      field = stripFieldEnd(field, "&");
-      super.parse(field, data);
-    }
-  }
-
 }
