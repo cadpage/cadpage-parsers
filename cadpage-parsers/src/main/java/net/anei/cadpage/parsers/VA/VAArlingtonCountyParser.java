efe2882f6f6f805f66e14a425109b1cf686323f9
Kenneth Corbin <ken@cadpage.org>
2022-11-16 11:24:59 -0800

    Fixed parsing problem with Arlington County, VA

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/VA/VAArlingtonCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/VA/VAArlingtonCountyParser.java
@@ -10,23 +10,23 @@ import net.anei.cadpage.parsers.MsgInfo.Data;
 public class VAArlingtonCountyParser extends FieldProgramParser {
 
   public VAArlingtonCountyParser() {
-    super(CITY_CODES, "ARLINGTON COUNTY", "VA", 
-          "CALL BOX? ADDR/S CITY? Units:UNIT/S+");
+    super(CITY_CODES, "ARLINGTON COUNTY", "VA",
+          "CALL BOX? ADDR/S CITY? INC:ID Units:UNIT/S+");
   }
-  
+
   @Override
   public String getFilter() {
-    return "rwaller@arlingtonva.us";
+    return "@arlingtonva.us";
   }
-  
+
   private static final DateFormat DATE_TIME_FMT = new SimpleDateFormat("EEE MMM dd HH:mm:ss yyyy");
-  
+
   @Override
   protected boolean parseMsg(String subject, String body, Data data) {
-    if (!subject.equals("Email Copy Message From Hiplink")) return false;
+    if (!subject.equalsIgnoreCase("Email Copy Message From Hiplink")) return false;
     if (!body.startsWith("DISPATCH:")) return false;
     body = body.substring(9).trim();
-    
+
     int pt = body.indexOf('\n');
     if (pt >= 0) {
       String tail = body.substring(pt+1).trim();
@@ -35,42 +35,42 @@ public class VAArlingtonCountyParser extends FieldProgramParser {
       if (pt >= 0) tail = tail.substring(pt+2).trim();
       setDateTime(DATE_TIME_FMT, tail, data);
     }
-    body = body.replace(" Unit:", " Units");
+    body = body.replace(" Unit:", " Units:");
     return parseFields(body.split(", "), data);
   }
-  
+
   @Override
   public String getProgram() {
     return super.getProgram() + " DATE TIME";
   }
-  
+
   @Override
   public Field getField(String name) {
     if (name.equals("BOX")) return new BoxField("\\d{4,5}");
     if (name.equals("CITY")) return new MyCityField();
     return super.getField(name);
   }
-  
+
   private class MyCityField extends CityField {
     @Override
     public boolean canFail() {
       return true;
     }
-    
+
     @Override
     public boolean checkParse(String field, Data data) {
       if (data.strCity.length() > 0) return false;
       return super.checkParse(field, data);
     }
   }
-  
+
   private static final Properties CITY_CODES = buildCodeTable(new String[]{
       "AC",   "ARLINGTON COUNTY",
       "AX",   "ALEXANDRIA",
       "FC",   "FALLS CHURCH",
       "FX",   "FAIRFAX",
-      "MW",   "RONALD REAGAN AIRPORT",  
-      
+      "MW",   "RONALD REAGAN AIRPORT",
+
       "ALEX", "ALEXANDRIA",
       "ANDL", "ANNANDALE",
       "DUNN", "DUNN LORING",
