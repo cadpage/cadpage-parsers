8ccfb2ceaac6b90501a2555b82b55209b6d7c5a0
Kenneth Corbin <ken@cadpage.org>
2022-11-22 16:09:14 -0800

    Fixed parsing problem with Van Buren County, MI

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/MI/MIVanBurenCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/MI/MIVanBurenCountyParser.java
@@ -1,15 +1,20 @@
 package net.anei.cadpage.parsers.MI;
 
-import net.anei.cadpage.parsers.dispatch.DispatchA1Parser;
+import net.anei.cadpage.parsers.dispatch.DispatchA19Parser;
 
-public class MIVanBurenCountyParser extends DispatchA1Parser {
-  
+public class MIVanBurenCountyParser extends DispatchA19Parser {
+
   public MIVanBurenCountyParser() {
     super("VAN BUREN COUNTY", "MI");
   }
-  
+
   @Override
   public String getFilter() {
     return "Dispatchers@vbco.org";
+  }
+
+  @Override
+  public int getMapFlags() {
+    return MAP_FLG_PREFER_GPS;
   }
 }
