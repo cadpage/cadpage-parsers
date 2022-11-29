20e5f929e5e6883a80bfc24712b430bbd34494d9
Kenneth Corbin <ken@cadpage.org>
2022-11-29 15:27:59 -0800

    Update sender filter for Oconee County, SC (C)

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/SC/SCOconeeCountyCParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/SC/SCOconeeCountyCParser.java
@@ -3,13 +3,13 @@ package net.anei.cadpage.parsers.SC;
 import net.anei.cadpage.parsers.dispatch.DispatchA72Parser;
 
 public class SCOconeeCountyCParser extends DispatchA72Parser {
-  
+
   public SCOconeeCountyCParser() {
     super("OCONEE COUNTY", "SC");
   }
 
   @Override
   public String getFilter() {
-    return "senecapolicedispatch@gmail.com";
+    return "senecapolicedispatch@gmail.com,itsupport@netmds.com";
   }
 }
