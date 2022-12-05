c87e4722c74e37a51f0747be90900213983fe33a
Kenneth Corbin <ken@cadpage.org>
2022-12-05 09:16:07 -0800

    Update sender filter for Rockland County, NY

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/NY/NYRocklandCountyCParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/NY/NYRocklandCountyCParser.java
@@ -10,19 +10,19 @@ import net.anei.cadpage.parsers.MsgInfo.Data;
 
 
 public class NYRocklandCountyCParser extends FieldProgramParser {
-  
+
   private static Pattern MARKER = Pattern.compile("(?:/ )?ACR# +(\\d+) / +");
-  
+
   public NYRocklandCountyCParser() {
     super("ROCKLAND COUNTY", "NY",
         "Addr:ADDR! X_St:X Name:NAME Phone:PHONE Comp:CALL? DATETIME? ");
   }
-  
+
   @Override
   public String getFilter() {
-    return "dispatch@hatzolohems.org,Dispatch";
+    return "dispatch@hatzolohems.org,Dispatch,cad@hatzolohems.org";
   }
-  
+
   @Override
   protected boolean parseMsg(String subject, String body, Data data) {
     do {
@@ -30,20 +30,20 @@ public class NYRocklandCountyCParser extends FieldProgramParser {
         data.strCallId = subject.substring(5).trim();
         break;
       }
-      
+
       Matcher match = MARKER.matcher(body);
       if (match.lookingAt()) {
         data.strCallId = match.group(1);
         body = body.substring(match.end());
         break;
       }
-      
+
     } while (false);
-    
+
     if (body.startsWith("Add:")) body = "Addr:" + body.substring(4);
     return parseFields(body.split("\n"), data);
   }
-  
+
   @Override
   public Field getField(String name) {
     if (name.equals("ADDR")) return new MyAddressField();
@@ -51,10 +51,10 @@ public class NYRocklandCountyCParser extends FieldProgramParser {
     if (name.equals("DATETIME")) return new MyDateTimeField();
     return super.getField(name);
   }
-  
+
   private static final Pattern ADDR_APT_PTN = Pattern.compile("(?:APT|RM|ROOM|LOT|#) *(.*)|[A-Z]?\\d+[A-Z]?|[A-Z]", Pattern.CASE_INSENSITIVE);
   private class MyAddressField extends AddressField {
-    
+
     @Override
     public void parse(String field, Data data) {
       Parser p = new Parser(field);
@@ -69,13 +69,13 @@ public class NYRocklandCountyCParser extends FieldProgramParser {
         data.strCity = city;
       }
     }
-    
+
     @Override
     public String getFieldNames() {
       return super.getFieldNames() + " CITY";
     }
   }
-  
+
   private class MyCrossField extends CrossField {
     @Override
     public void parse(String field, Data data) {
@@ -87,21 +87,21 @@ public class NYRocklandCountyCParser extends FieldProgramParser {
       super.parse(field, data);
     }
   }
-  
+
   @Override
   public String getProgram() {
     return "ID " + super.getProgram();
   }
-  
+
   private static final Pattern DATE_TIME_PTN =  Pattern.compile("(\\d\\d?/\\d\\d?/\\d{4}) - (\\d\\d?:\\d\\d [AP]M)");
   private static final DateFormat TIME_FMT = new SimpleDateFormat("hh:mm aa");
   private class MyDateTimeField extends DateTimeField {
-    
+
     @Override
     public boolean canFail() {
       return true;
     }
-    
+
     @Override
     public boolean checkParse(String field, Data data) {
       Matcher match = DATE_TIME_PTN.matcher(field);
@@ -110,7 +110,7 @@ public class NYRocklandCountyCParser extends FieldProgramParser {
       setTime(TIME_FMT, match.group(2), data);
       return true;
     }
-    
+
     @Override
     public void parse(String field, Data data) {
       if (!checkParse(field, data)) abort();
