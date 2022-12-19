3f990e1b8186e535a4a9fee6c805f11fa248c269
Kenneth Corbin <ken@cadpage.org>
2022-12-19 11:05:44 -0800

    Fixed parsing problem with Edina, MN

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/MN/MNEdinaParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/MN/MNEdinaParser.java
@@ -1,8 +1,8 @@
 package net.anei.cadpage.parsers.MN;
 
 import java.util.Properties;
+
 import net.anei.cadpage.parsers.dispatch.DispatchOSSIParser;
-import net.anei.cadpage.parsers.MsgInfo.Data;
 
 /**
  * Edina, MN
@@ -10,31 +10,23 @@ import net.anei.cadpage.parsers.MsgInfo.Data;
 public class MNEdinaParser extends DispatchOSSIParser {
   public MNEdinaParser() {
     super(CITY_CODES, "EDINA", "MN",
-        "( CANCEL ADDR CITY | ( PAGE | FYI? ) ( ADDRCITY | ADDR CITY? ) ( PLACE CALL | CALL! ) UNIT+? X X ) INFO+");
+        "( CANCEL ADDR CITY | FYI? CALL ADDR CITY CODE X/Z+? ID ID/L? ( UNIT/Z SRC! | SRC! | UNIT ) ) INFO/N+");
   }
-  
+
   public String getFilter() {
     return "CAD@ci.edina.mn.us";
   }
-  
+
   @Override
   public Field getField(String name) {
-    if (name.equals("PAGE")) return new CallField("RICH +FD +FULL +CALLBACK|EDINA +FIRE +(?:GENERAL +ALARM|CHIEFS +PAGE|GROUP +\\d +PAGED)", true);
-    if (name.equals("UNIT")) return new MyUnitField();
-    if (name.equals("CALL")) return new  CallField("GV-.*|.* RICHFIELD|ASSIST .*", false);
+    if (name.equals("CANCEL"))  return new BaseCancelField("[A-Z ]+ SELECT CALLBACK");
+    if (name.equals("CODE")) return new CodeField("[A-Z]+|911|AOAK9", true);
+    if (name.equals("ID")) return new IdField("\\d{7,10}");
+    if (name.equals("UNIT")) return new UnitField("[A-Z]*\\d+(?:,[A-Z]*\\d+)*|\\d{3}", false);
+    if (name.equals("SRC")) return new SourceField("[A-Z]+\\d+", true);
     return super.getField(name);
   }
-  
-  private class MyUnitField extends UnitField {
-    public MyUnitField() {
-      super("[A-Z]*\\d+(?:,[A-Z]*\\d+)*|ST *\\d+", true);
-    }
-    @Override
-    public void parse (String field, Data data) {
-      data.strUnit = append(data.strUnit, ",", field);
-    }
-  }
-  
+
   private static final Properties CITY_CODES = buildCodeTable(new String[]{
       "EDI",            "EDINA",
       "RCH",            "RICHFIELD",
@@ -44,5 +36,5 @@ public class MNEdinaParser extends DispatchOSSIParser {
       "BROOKLYN PARK",  "BROOKLYN PARK",
       "SLP",            "ST LOUIS PARK"
   });
-  
+
 }
