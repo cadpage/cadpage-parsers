33aa2a64ad0fe54c537d2f8eba44d8baae55ab15
Kenneth Corbin <ken@cadpage.org>
2022-12-22 14:19:36 -0800

    Update sender filter for Washignton County, IN

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/IN/INWashingtonCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/IN/INWashingtonCountyParser.java
@@ -3,13 +3,13 @@ package net.anei.cadpage.parsers.IN;
 import net.anei.cadpage.parsers.dispatch.DispatchSPKParser;
 
 public class INWashingtonCountyParser extends DispatchSPKParser {
-  
+
   public INWashingtonCountyParser() {
     super("WASHINGTON COUNTY", "IN");
   }
-  
+
   @Override
   public String getFilter() {
-    return "WashingtonCo@ipsc.in.gov";
+    return "WashingtonCo@ipsc.in.gov,noreply@public-safety-cloud.com";
   }
 }
