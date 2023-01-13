372314cc84556c98ec07fbfb58e3f4a87379f71c
Kenneth Corbin <ken@cadpage.org>
2023-01-12 17:15:02 -0800

    Update A911 parser table

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/Active911ParserTable.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/Active911ParserTable.java
@@ -1916,6 +1916,7 @@ public class Active911ParserTable {
       "US/PA/AvondaleFC",                 "PAChesterCountyO",
       "US/PA/BeaverCounty",               "PABeaverCounty",
       "US/PA/BedfordCounty",              "PABedfordCounty",
+      "US/PA/BenAvon",                    "PAAlleghenyCountyA",
       "US/PA/Bensalem",                   "PABucksCountyD",
       "US/PA/BerksCounty",                "PABerksCounty",
       "US/PA/BerwynFire",                 "PAChesterCountyD1",
