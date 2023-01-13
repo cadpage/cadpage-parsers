21620ebbe2feb18dd4774d111cb11bd81005df73
Kenneth Corbin <ken@cadpage.org>
2023-01-13 11:11:28 -0800

    Update sender filter for Jackson County, WV

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/WV/WVJacksonCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/WV/WVJacksonCountyParser.java
@@ -3,14 +3,14 @@ package net.anei.cadpage.parsers.WV;
 import net.anei.cadpage.parsers.dispatch.DispatchSPKParser;
 
 public class WVJacksonCountyParser extends DispatchSPKParser {
-  
+
   public WVJacksonCountyParser() {
     super("JACKSON COUNTY", "WV");
   }
-  
+
   @Override
   public String getFilter() {
-    return "jacksoncowv911@gmail.com";
+    return "jacksoncowv911@gmail.com,911Center@Jacksonwv911.com";
   }
 
 }
