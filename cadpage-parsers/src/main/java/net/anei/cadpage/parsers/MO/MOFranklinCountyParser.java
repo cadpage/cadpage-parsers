2d87684a515e5d5e808434a577cc43c7301af46c
Kenneth Corbin <ken@cadpage.org>
2023-01-16 20:10:52 -0800

    Fixed parsing problem with Franklin County, MO

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/MO/MOFranklinCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/MO/MOFranklinCountyParser.java
@@ -1,50 +1,104 @@
 package net.anei.cadpage.parsers.MO;
 
+import java.util.HashSet;
+import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import net.anei.cadpage.parsers.FieldProgramParser;
 import net.anei.cadpage.parsers.MsgInfo.Data;
-import net.anei.cadpage.parsers.dispatch.DispatchH01Parser;
+import net.anei.cadpage.parsers.MsgInfo.MsgType;
 
 
-public class MOFranklinCountyParser extends DispatchH01Parser {
+public class MOFranklinCountyParser extends FieldProgramParser {
   public MOFranklinCountyParser() {
     super("FRANKLIN COUNTY", "MO",
-          "( MARK1! Workstation:SKIP! Print_Time:SKIP! User:SKIP! Location:ADDR! Response_Type:CALL! Zone_Name:MAP! Status_Name:SKIP! Status_Time:DATETIME! Handling_Unit:UNIT! Agency:SRC! NOTES+ " +
-          "| MARK2/R! Number:ID? Location:ADDR! Response_Type:CALL! Agency:SRC! HandlingResource:UNIT! CallerName:NAME? PhoneNumber:PHONE? ZoneName:MAP! Response_State_Logs%EMPTY RR_NOTES/G+? END_TABLE Resource_Activities%EMPTY RR_NOTES+? END_TABLE " +
-          "| EMPTY CALL ADDR! END )");
+          "Location:ADDRCITYST! Intersection:X! Business_Name:PLACE! APT/SUITE:APT! " +
+            "( MapPageGrid:MAP! MapCoordinates:MAP/L! LocationDescription:INFO! Subdivision:LINFO/N! Latitude:GPS1! Longitude:GPS2! | ) " +
+            "CALL:DATE_TIME_ID! Response_Type:CALL! UNITS/REPORTS_ASSIGNED:EMPTY! TIMES/N+ CAD_INFORMATION:EMPTY INFO/N+");
   }
-  
+
   @Override
   public String getFilter() {
-    return "@franklinmo.net,@sbcglobal.net";
+    return "franklincodispatch@franklinmo.net";
   }
-  
-  private static final Pattern PREFIX_PTN = Pattern.compile("[-.,@A-Za-z0-9]+\n{2,}");
-  
+
+  private String times;
+  private Set<String> unitSet = new HashSet<>();
+
+  private static final Pattern TAG_PTN = Pattern.compile("\n<([ /A-Za-z]+: *)>");
+
   @Override
   protected boolean parseHtmlMsg(String subject, String body, Data data) {
-    
-    Matcher match = PREFIX_PTN.matcher(body);
-    if (match.lookingAt()) body = body.substring(match.end()).trim();
-    
-    int pt = body.indexOf("Scanned by YHTI SPAM firewall");
-    if (pt >= 0) body = body.substring(0,pt).trim();
-    
-    if (body.startsWith("<HTML><body>")) {
-      body = decodeHtmlSequence(body);
-      return parseFields(body.split("\n"), data);
-    }
-    
-    if (! super.parseHtmlMsg(subject, body, data)) return false;
-    if (data.strCity.equals("FRNKLN CNTY")) data.strCity = "FRANKLIN COUNTY";
+
+    times = "";
+    unitSet.clear();
+    body = TAG_PTN.matcher(body).replaceAll("\n$1");
+    if (!parseFields(body.split("\n"), data)) return false;
+    if (data.msgType == MsgType.RUN_REPORT) data.strSupp = append(times, "\n\n", data.strSupp);
+    times = null;
+    unitSet.clear();
     return true;
   }
 
   @Override
   public Field getField(String name) {
-    if (name.equals("MARK1")) return new SkipField("Franklin Co Response Email Report", true);
-    if (name.equals("MARK2")) return new SkipField("Franklin Co Completed Response Report", true);
+    if (name.equals("ADDRCITYST")) return new MyAddressCityStateField();
+    if (name.equals("DATE_TIME_ID")) return new MyDateTimeIdField();
+    if (name.equals("TIMES")) return new MyTimesField();
     return super.getField(name);
+  }
+
+  private static final Pattern MSPACE_PTN = Pattern.compile(" {2,}");
+  private static final Pattern CITY_STATE_ZIP_PTN = Pattern.compile("(.*?) *\\bMO(?: +\\d{5})?");
+  private class MyAddressCityStateField extends AddressCityField {
+    @Override
+    public void parse(String field, Data data) {
+      int pt = field.indexOf("Appt or Suite");
+      if (pt >= 0) field = field.substring(0,pt).trim();
+      field = MSPACE_PTN.matcher(field).replaceAll(" ");
+      super.parse(field, data);
+      Matcher match = CITY_STATE_ZIP_PTN.matcher(data.strCity);
+      if (match.matches()) data.strCity = match.group(1);
+    }
+  }
+
+  private static final Pattern DATE_TIME_ID_PTN = Pattern.compile("(\\d\\d/\\d\\d/\\d{4}) @ (\\d\\d:\\d\\d:\\d\\d) FC911 USE ONLY:(\\d{4}-\\d{8})");
+  private class MyDateTimeIdField extends Field {
+
+    @Override
+    public void parse(String field, Data data) {
+      Matcher match = DATE_TIME_ID_PTN.matcher(field);
+      if (!match.matches()) abort();
+      data.strDate = match.group(1);
+      data.strTime = match.group(2);
+      data.strCallId = match.group(3);
+    }
+
+    @Override
+    public String getFieldNames() {
+      return "DATE TIME ID";
+    }
+  }
+
+  private static final Pattern TIMES_PTN = Pattern.compile("--  (\\S*)  -- (.*?)<T>\\d\\d:\\d\\d:\\d\\d");
+  private class MyTimesField extends Field {
+    @Override
+    public void parse(String field, Data data) {
+      field = stripFieldEnd(field, ";");
+      Matcher match = TIMES_PTN.matcher(field);
+      if (match.matches()) {
+        String unit = match.group(1);
+        if (unitSet.add(unit)) data.strUnit = append(data.strUnit, ",", unit);
+        String status = match.group(2);
+        if (status.equals("IN SERVICE")) data.msgType = MsgType.RUN_REPORT;
+      }
+      times = append(times, "\n", field);
+    }
+
+    @Override
+    public String getFieldNames() {
+      return "UNIT INFO";
+    }
   }
 }
