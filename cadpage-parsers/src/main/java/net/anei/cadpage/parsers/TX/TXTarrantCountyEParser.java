d4fbfb10149c77586320f666bf017e1d1d162c3e
Kenneth Corbin <ken@cadpage.org>
2023-01-30 21:22:43 -0800

    Fixed parsing problem with Tarrant County, TX (E)

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/TX/TXTarrantCountyEParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/TX/TXTarrantCountyEParser.java
@@ -1,40 +1,68 @@
 package net.anei.cadpage.parsers.TX;
 
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import net.anei.cadpage.parsers.FieldProgramParser;
 import net.anei.cadpage.parsers.MsgInfo.Data;
 
 public class TXTarrantCountyEParser extends FieldProgramParser {
-  
+
   public TXTarrantCountyEParser() {
-    super("TARRANT COUNTY", "TX", 
-          "CALL ADDRCITYST X UNIT INFO ID!");
+    super("TARRANT COUNTY", "TX",
+          "CALL ADDRCITYST ( SELECT/1 X UNIT INFO ID/L DATETIME! SKIP " +
+                          "| SELECT/2 UNIT INFO DATETIME ID! ID/L " +
+                          "| INFO ) END");
   }
-  
+
   @Override
   public String getFilter() {
     return "no-reply@zuercherportal.com";
   }
-  
+
+  private static final Pattern SUBJECT_PTN = Pattern.compile("(?:CAD Call|Major Incident -) *(.*)");
+
   @Override
   protected boolean parseMsg(String subject, String body, Data data) {
-    if (!subject.equals("CAD Call")) return false;
+    Matcher match = SUBJECT_PTN.matcher(subject);
+    if (match.matches()) {
+      setSelectValue(subject.startsWith("Major") ? "3" : "1");
+      data.strCallId = match.group(1);
+    }
+    else {
+      if (subject.isEmpty()) return false;
+      setSelectValue("2");
+      body = subject + " | " + body;
+    }
     return parseFields(body.split("\\|"), data);
   }
-  
+
   @Override
   public Field getField(String name) {
     if (name.equals("INFO")) return new MyInfoField();
+    if (name.equals("ID")) return new MyIdField();
+    if (name.equals("DATETIME")) return new DateTimeField("\\d\\d/\\d\\d/\\d\\d \\d\\d:\\d\\d", true);
     return super.getField(name);
   }
-  
+
   private static final Pattern INFO_BRK_PTN = Pattern.compile("[ ;]*\\b\\d\\d/\\d\\d/\\d\\d \\d\\d:\\d\\d:\\d\\d - *");
   private class MyInfoField extends InfoField {
     @Override
     public void parse(String field, Data data) {
       if (field.equals("None")) return;
       field = INFO_BRK_PTN.matcher(field).replaceAll("\n").trim();
+      super.parse(field, data);
+    }
+  }
+
+  private class MyIdField extends IdField {
+    public MyIdField() {
+      super("(?:\\b(?:[A-Z]{2,3}\\d{8})\\b[; ]*)+", true);
+    }
+
+    @Override
+    public void parse(String field, Data data) {
+      field = field.replace("; ", "/");
       super.parse(field, data);
     }
   }
