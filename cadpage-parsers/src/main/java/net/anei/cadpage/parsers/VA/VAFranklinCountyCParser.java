0eb8a1290e8a38abca1e072b3e80e47906617ce3
Ken Corbin <ken@cadpage.org>
2023-02-15 12:45:43 -0800

    Fixed parsing problem with Frnakiln County, VA (C)

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/VA/VAFranklinCountyCParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/VA/VAFranklinCountyCParser.java
@@ -1,48 +1,24 @@
 package net.anei.cadpage.parsers.VA;
 
-import net.anei.cadpage.parsers.FieldProgramParser;
 import net.anei.cadpage.parsers.MsgInfo.Data;
+import net.anei.cadpage.parsers.dispatch.DispatchA71Parser;
 
-public class VAFranklinCountyCParser extends FieldProgramParser {
+public class VAFranklinCountyCParser extends DispatchA71Parser {
 
   public VAFranklinCountyCParser() {
-    super("FRANKLIN COUNTY", "VA", 
-          "CALL:CALL! PLACE:PLACE? ADDR:ADDR! CITY:CITY! XY:GPS? ID:ID! PRI:PRI! DATE:DATE! TIME:TIME! UNIT:UNIT! X:X? INFO:INFO/N+");
+    super("FRANKLIN COUNTY", "VA");
   }
   
   @Override
   public int getMapFlags() {
     return MAP_FLG_PREFER_GPS;
   }
-  
+
   @Override
-  protected boolean parseMsg(String body, Data data) {
-    return parseFields(body.split("\n"), data);
+  protected boolean parseMsg(String subject, String body, Data data) {
+    if (!super.parseMsg(subject, body, data)) return false;
+    data.strUnit = data.strUnit.replace(" ", "");
+    return true;
   }
   
-  @Override
-  public Field getField(String name) {
-    if (name.equals("DATE")) return new DateField("\\d\\d/\\d\\d.\\d{4}", true);
-    if (name.equals("TIME")) return new TimeField("\\d\\d:\\d\\d:\\d\\d", true);
-    if (name.equals("UNIT")) return new MyUnitField();
-    if (name.equals("X")) return new MyCrossField();
-    return super.getField(name);
-  }
-  
-  private class MyUnitField extends UnitField {
-    @Override
-    public void parse(String field, Data data) {
-      field = field.replace(" ", "");
-      field = stripFieldEnd(field, ",");
-      super.parse(field, data);
-    }
-  }
-  
-  private class MyCrossField extends CrossField {
-    @Override
-    public void parse(String field, Data data) {
-      field = field.replace('@', '/');
-      super.parse(field, data);
-    }
-  }
 }
