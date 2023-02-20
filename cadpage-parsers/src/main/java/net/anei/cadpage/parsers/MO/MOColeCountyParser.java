b5080c9ce9fb71c56ded88ff10c69da56764194b
Ken Corbin <ken@cadpage.org>
2023-02-20 10:02:25 -0800

    Fixed parsing problem with Cole COunty, MO

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/MO/MOColeCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/MO/MOColeCountyParser.java
@@ -10,14 +10,28 @@ public class MOColeCountyParser extends FieldProgramParser {
   
   public MOColeCountyParser() {
     super("COLE COUNTY", "MO",
-      "UNIT:UNIT! ZONE:MAP! PRI:PRI! PLACE:PLACE? ADDR:ADDR! CITY:CITY? CALL:CALL! INFO:INFO? INFO+? ID:ID! DATE:DATE! TIME:TIME! GPS:GPS");
+      "UNIT:UNIT! ADDR:ADDRCITY/S6 LOCATION_DETAILS:INFO! CROSS:X! CALL:CALL! P36:CALL/SDS! CFS_#:ID! RADIO_CH:CH! ZONE:MAP! SUMMARY:INFO/N! INFO/N+");
   }
   
   @Override
   public String getFilter() {
-    return "ccems";
+    return "paging@jeffcitymo.org";
   }
   protected boolean parseMsg(String body, Data data) {
     return super.parseFields(body.split("\n"), data);
+  }
+  
+  @Override
+  public Field getField(String name) {
+    if (name.equals("ADDRCITY")) return new MyAddressCityField();
+    return super.getField(name);
+  }
+  
+  private class MyAddressCityField extends AddressCityField {
+    @Override
+    public void parse(String field, Data data) {
+      field = field.replace("@", "&");
+      super.parse(field, data);
+    }
   }
 }
