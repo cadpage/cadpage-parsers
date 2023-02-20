972d0605f00ac246320dab1cbae18ab7457d750e
Ken Corbin <ken@cadpage.org>
2023-02-19 18:04:34 -0800

    Fixed parsing problem with Weld County, CO (B)

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/CO/COWeldCountyBParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/CO/COWeldCountyBParser.java
@@ -1,5 +1,6 @@
 package net.anei.cadpage.parsers.CO;
 
+import java.util.Properties;
 import java.util.regex.Pattern;
 
 import net.anei.cadpage.parsers.FieldProgramParser;
@@ -8,8 +9,9 @@ import net.anei.cadpage.parsers.MsgInfo.Data;
 public class COWeldCountyBParser extends FieldProgramParser {
 
   public COWeldCountyBParser() {
-    super("WELD COUNTY", "CO",
-          "( EMPTY ADDR CITY PLACE UNIT CALL? ID | ) INFO! GPS1/d GPS2/d EMPTY END");
+    super(CITY_LIST, "WELD COUNTY", "CO",
+          "( EMPTY ADDR ( APT APT CITY | CITY ) PLACE UNIT CALL? ID | ) INFO! GPS1/d GPS2/d SRC? EMPTY/Z END");
+    setupCities(MAP_CITY_TABLE.stringPropertyNames());
   }
 
   @Override
@@ -35,9 +37,28 @@ public class COWeldCountyBParser extends FieldProgramParser {
 
   @Override
   public Field getField(String name) {
+    if (name.equals("CITY")) return new MyCityField();
     if (name.equals("ID")) return new IdField("\\d\\d[A-Z]{2,3}\\d{6}|\\d+-\\d+", true);
     if (name.equals("INFO")) return new MyInfoField();
     return super.getField(name);
+  }
+  
+  private class MyCityField extends CityField {
+    @Override
+    public boolean checkParse(String field, Data data) {
+      return super.checkParse(cleanField(field), data);
+    }
+    
+    @Override
+    public void parse(String field, Data data) {
+      super.parse(cleanField(field), data);
+    }
+    
+    private String cleanField(String field) {
+      int pt = field.indexOf('(');
+      if (pt >= 0) field = field.substring(0,pt).trim();
+      return field;
+    }
   }
 
   private static final Pattern INFO_BRK_PTN = Pattern.compile("[ ,]*\\[\\d\\] *");
@@ -49,4 +70,99 @@ public class COWeldCountyBParser extends FieldProgramParser {
       }
     }
   }
+  
+  
+  
+  @Override
+  public String adjustMapCity(String city) {
+    String mapCity = MAP_CITY_TABLE.getProperty(city.toUpperCase());
+    if (mapCity != null) city = mapCity;
+    return city;
+  }
+  
+  private static final String[] CITY_LIST = new String[]{
+    
+    // County
+    "WELD",
+    "LARIMER COUNTY",
+    
+    // Cities
+    "BRIGHTON",
+    "DACONO",
+    "EVANS",
+    "FORT LUPTON",
+    "GREELEY",
+    "LONGMONT",
+    "NORTHGLENN",
+    "THORNTON",
+    
+    // Towns
+    "AULT",
+    "BERTHOUD",
+    "EATON",
+    "ERIE",
+    "FIRESTONE",
+    "FREDERICK",
+    "GARDEN CITY",
+    "GILCREST",
+    "GROVER",
+    "HUDSON",
+    "JOHNSTOWN",
+    "KEENESBURG",
+    "KERSEY",
+    "LASALLE",
+    "LA SALLE",
+    "LOCHBUIE",
+    "MEAD",
+    "MILLIKEN",
+    "NUNN",
+    "PIERCE",
+    "PLATTEVILLE",
+    "RAYMER",
+    "SEVERANCE",
+    "WINDSOR",
+    
+    // Census-designated places
+    "ARISTOCRAT RANCHETTES",
+    "BRIGGSDALE",
+    
+    // Unincorporated communities
+    "AUBURN",
+    "AVALO",
+    "CARR",
+    "DEARFIELD",
+    "GALETON",
+    "GILL",
+    "HEREFORD",
+    "HIGHLANDLAKE",
+    "IONE",
+    "KEOTA",
+    "LUCERNE",
+    "ROGGEN",
+    "STONEHAM",
+    "WATTENBURG",
+    
+    // Ghost towns
+    "ALDEN",
+    "DEARFIELD",
+    "ELWELL",
+    "FORT ST VRAIN",
+    "LATHAM",
+    "MASTERS",
+    "ROSEDALE",
+    "SERENE",
+    "SLIGO",
+    
+    // Latimer County
+    "FORT COLLINS",
+    
+    // Elsewhere
+    "AURORA"
+  };
+  
+  private static final Properties MAP_CITY_TABLE = buildCodeTable(new String[]{
+      "ARROWHEAD",    "GREELEY",
+      "CAMFIELD",     "EATON",
+      "HILL N PARK",  "GREELEY"
+  });
 }
