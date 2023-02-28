bec15d091a55f0303728c293a7587463e09d04e5
Ken Corbin <ken@cadpage.org>
2023-02-27 17:12:57 -0800

    Parsing problem with Camden County, GA (A)

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/GA/GACamdenCountyAParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/GA/GACamdenCountyAParser.java
@@ -1,5 +1,7 @@
 package net.anei.cadpage.parsers.GA;
 
+import java.util.regex.Pattern;
+
 import net.anei.cadpage.parsers.MsgInfo.Data;
 import net.anei.cadpage.parsers.dispatch.DispatchA57Parser;
 
@@ -18,9 +20,14 @@ public class GACamdenCountyAParser extends DispatchA57Parser {
     return "911paging@camdensheriff.org";
   }
   
+  private static final Pattern MISSING_COLON_PTN = Pattern.compile("((?:^|\n) *(?:Call Time|Call Type|Address|Common Name|Closest Intersection|Additional Location Info|Assigned Units|Quadrant|Primary Incident|Narrative)) ");
+  
   @Override
   protected boolean parseMsg(String subject, String body, Data data) {
     if (!subject.equals("Dispatch")) return false;
+    if (body.startsWith("Time ")) {
+      body = MISSING_COLON_PTN.matcher("Call " + body.replace("Closet Intersetion", "Closest Intersection")).replaceAll("$1:");
+    }
     return super.parseMsg(body, data);
   };
 }
