7c41bc81f6102238f2a9587e451116d244f83b19
Ken Corbin <ken@cadpage.org>
2023-03-08 08:44:28 -0800

    Fixed parsing problem with Lafayette Parish, LA

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/LA/LALafayetteParishParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/LA/LALafayetteParishParser.java
@@ -4,6 +4,7 @@ import java.util.Properties;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import net.anei.cadpage.parsers.FieldProgramParser;
 import net.anei.cadpage.parsers.MsgInfo.Data;
 import net.anei.cadpage.parsers.dispatch.DispatchA49Parser;
 
@@ -11,57 +12,71 @@ import net.anei.cadpage.parsers.dispatch.DispatchA49Parser;
 Lafayette Parish, LA
 */
 
-public class LALafayetteParishParser extends DispatchA49Parser {
-
-  private static final Pattern TRAIL_NA_PTN = Pattern.compile("(?: +OR)? +NA$");
+public class LALafayetteParishParser extends FieldProgramParser {
 
   public LALafayetteParishParser() {
-    super("LAFAYETTE PARISH","LA");
+    super("LAFAYETTE PARISH","LA", 
+          "Event_Date:DATETIME! Unit:UNIT! CAD:ID! Address:ADDR! Intersection:X! Event_Type:CODE_CALL! Report:ID! Remarks:INFO? INFO/N+");
   }
 
+  private static final Pattern TRAIL_HASH_PTN = Pattern.compile("(?<=Unit|CAD|Report)#");
+  private static final Pattern DELIM = Pattern.compile("\n|\\s+(?=(?:Unit|CAD|Intersection|):)");
+
   @Override
-  protected boolean parseMsg(String body, Data data) {
-    body = body.replace("  >", "\n>");
-    if (!super.parseMsg(body, data)) return false;
-    data.strAddress = TRAIL_NA_PTN.matcher(data.strAddress).replaceFirst("");
-    data.strCross = TRAIL_NA_PTN.matcher(data.strCross).replaceFirst("");
-    return true;
+  protected boolean parseMsg(String subject, String body, Data data) {
+    if (!subject.equals("System Alert")) return false;
+    body = TRAIL_HASH_PTN.matcher(body).replaceAll(":");
+    return parseFields(DELIM.split(body), data);
   }
 
 
   @Override
   public String getFilter() {
-    return "cadalert@lafayettela.gov,alerts@carencrofd.org";
-  }
-
-  @Override
-  protected String fixCall(String call) {
-    if (call.startsWith("CALL SENT TO ACADIAN IT")) return null;
-    if (call.equals("ACADIAN AMBULANCE INCIDENT TRANSFERED")) return null;
-    return super.fixCall(call);
+    return "alerts@lpcdops-lafla.gov";
   }
 
   @Override
   public Field getField(String name) {
+    if (name.equals("DATETIME")) return new DateTimeField("\\d\\d/\\d\\d/\\d{4} +\\d\\d:\\d\\d:\\d\\d", true);
     if (name.equals("ADDR")) return new MyAddressField();
+    if (name.equals("X")) return new MyCrossField();
+    if (name.equals("CODE_CALL")) return new MyCodeCallField();
     return super.getField(name);
   }
-
-  private static final Pattern ADDR_CITY_PTN = Pattern.compile("(.*)\\.([A-Z])");
+  
+  private static final Pattern MSPACE_PTN = Pattern.compile(" {2,}");
+  
   private class MyAddressField extends AddressField {
     @Override
     public void parse(String field, Data data) {
-      Matcher match = ADDR_CITY_PTN.matcher(field);
-      if (match.matches()) {
-        field = match.group(1).trim();
-        data.strCity = convertCodes(match.group(2), CITY_CODES);
-      }
+      field = MSPACE_PTN.matcher(field).replaceAll(" ");
       super.parse(field, data);
+    }
+  }
+  
+  private class MyCrossField extends CrossField {
+    @Override
+    public void parse(String field, Data data) {
+      field = MSPACE_PTN.matcher(field).replaceAll(" ");
+      super.parse(field, data);
+    }
+  }
+  
+  private static final Pattern CODE_CALL_PTN = Pattern.compile("\\((.*?)\\) *(.*)");
+  
+  private class MyCodeCallField extends Field {
+
+    @Override
+    public void parse(String field, Data data) {
+      Matcher match = CODE_CALL_PTN.matcher(field);
+      if (!match.matches()) abort();
+      data.strCode = match.group(1).trim();
+      data.strCall = match.group(2);
     }
 
     @Override
-    public String  getFieldNames() {
-      return super.getFieldNames() + " CITY";
+    public String getFieldNames() {
+      return "CODE CALL";
     }
   }
 
@@ -76,15 +91,4 @@ public class LALafayetteParishParser extends DispatchA49Parser {
   }
   private static final Pattern TW_PTN = Pattern.compile("\\bTW\\b", Pattern.CASE_INSENSITIVE);
   private static final Pattern CROSS_HOUSE_PTN = Pattern.compile("\\d+ +(.*)");
-
-  private static final Properties CITY_CODES = buildCodeTable(new String[]{
-      "B", "BROUSSARD",
-      "C", "CARENCRO",
-      "D", "DUSON",
-      "L", "LAFAYETTE",
-      "P", "LAFAYETTE PARISH",
-      "S", "SCOTT",
-      "Y", "YOUNGSVILLE"
-
-  });
 }
