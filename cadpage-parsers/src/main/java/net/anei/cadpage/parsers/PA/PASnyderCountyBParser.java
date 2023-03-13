2f2c22d039dd5a37d088cd6748638d6d4f239bbd
Ken Corbin <ken@cadpage.org>
2023-03-13 14:22:22 -0700

    Fixed parsing problem with Snyder County, PA

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/PA/PASnyderCountyBParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/PA/PASnyderCountyBParser.java
@@ -1,5 +1,7 @@
 package net.anei.cadpage.parsers.PA;
 
+import java.util.regex.Pattern;
+
 import net.anei.cadpage.parsers.FieldProgramParser;
 import net.anei.cadpage.parsers.MsgInfo.Data;
 
@@ -29,6 +31,35 @@ public class PASnyderCountyBParser extends FieldProgramParser {
     if (!parseFields(body.split(";"), data)) return false;
     data.strCity = stripFieldEnd(data.strCity,  " BORO");
     return true;
+  }
+  
+  @Override
+  public Field getField(String name) {
+    if (name.equals("ADDRCITY")) return new MyAddressCityField();
+    return super.getField(name);
+  }
+  
+  private static final Pattern COUNTY_PTN = Pattern.compile("[A-Z ]+ CO(?:UNTY)?", Pattern.CASE_INSENSITIVE);
+  
+  private class MyAddressCityField extends AddressCityField {
+    @Override
+    public void parse(String field, Data data) {
+      for (String part : field.split(",")) {
+        part = part.trim();
+        if (data.strAddress.isEmpty()) {
+          parseAddress(part, data);
+        } else if (data.strCity.isEmpty()) {
+          data.strCity = part;
+        } else if (!COUNTY_PTN.matcher(part).matches()) {
+          data.strPlace = append(data.strPlace, " - ", part);
+        }
+      }
+    }
+    
+    @Override
+    public String getFieldNames() {
+      return "ADDR APT CITY PLACE";
+    }
   }
 
 }
