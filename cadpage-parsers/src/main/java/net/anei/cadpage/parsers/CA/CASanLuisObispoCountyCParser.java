3d63a35d86216e4f99e09cba14e04c054954e0c8
Ken Corbin <ken@cadpage.org>
2023-03-14 17:34:33 -0700

    Fixed parsing proble mwith San Luis Obispo County, CA (C)

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/CA/CASanLuisObispoCountyCParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/CA/CASanLuisObispoCountyCParser.java
@@ -1,22 +1,65 @@
 package net.anei.cadpage.parsers.CA;
 
-import java.util.Properties;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-import net.anei.cadpage.parsers.dispatch.DispatchA22Parser;
+import net.anei.cadpage.parsers.FieldProgramParser;
+import net.anei.cadpage.parsers.MsgInfo.Data;
 
-public class CASanLuisObispoCountyCParser extends DispatchA22Parser {
+public class CASanLuisObispoCountyCParser extends FieldProgramParser {
 
   public CASanLuisObispoCountyCParser() {
-    super(CITY_CODES, "SAN LUIS OBISPO COUNTY", "CA");
+    super("SAN LUIS OBISPO COUNTY", "CA", 
+          "CALL:CALL! CALL_CODE:CODE! PLACE:PLACE! ADDR:ADDR! CITY:CITY! ID:ID! PRI:PRI! DATE:DATE! TIME:TIME! MAP:GPS! INFO:INFO/N+");
   }
 
   @Override
   public String getFilter() {
-    return "Disptach@atascadero.org";
+    return "disptach@atascadero.org";
   }
-
-  private static final Properties CITY_CODES = buildCodeTable(new String[] {
-      "AT", "ATASCADERO"
-  });
-
+  
+  @Override
+  public int getMapFlags() {
+    return MAP_FLG_SUPPR_LA | MAP_FLG_PREFER_GPS;
+  }
+  
+  private static final Pattern SUBJECT_PTN = Pattern.compile("(.*?) CAD Event: +\\d+");
+  
+  @Override
+  protected boolean parseMsg(String subject, String body, Data data) {
+    Matcher match = SUBJECT_PTN.matcher(subject);
+    if (!match.matches()) return false;
+    data.strSource = match.group(1);
+    return parseFields(body.split("\n"), data);
+  }
+  
+  @Override
+  public String getProgram() {
+    return "SRC " + super.getProgram();
+  }
+  
+  @Override
+  public Field getField(String name) {
+    if (name.equals("PLACE")) return new MyPlaceField();
+    if (name.equals("PRI")) return new MyPriorityField();
+    if (name.equals("DATE")) return new DateField("\\d\\d/\\d\\d/\\d{4}", true);
+    if (name.equals("TIME")) return new TimeField("\\d\\d:\\d\\d:\\d\\d", true);
+    return super.getField(name);
+  }
+  
+  private class MyPlaceField extends PlaceField {
+    @Override
+    public void parse(String field, Data data) {
+      if (field.equals("None")) return;
+      super.parse(field, data);
+    }
+  }
+  
+  private class MyPriorityField extends PriorityField {
+    @Override
+    public void parse(String field, Data data) {
+      field = stripFieldStart(field, "PRIORITY");
+      super.parse(field, data);
+    }
+  }
 }
