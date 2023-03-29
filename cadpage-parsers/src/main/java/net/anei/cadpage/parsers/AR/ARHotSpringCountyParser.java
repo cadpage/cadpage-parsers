4c4718a20d8209934e34f61545fc9d5b1095ba9d
Ken Corbin <ken@cadpage.org>
2023-03-29 13:38:40 -0700

    Fixed parsing problem with Hot Springs County, AR

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/AR/ARHotSpringCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/AR/ARHotSpringCountyParser.java
@@ -14,28 +14,28 @@ import net.anei.cadpage.parsers.MsgInfo.MsgType;
 import net.anei.cadpage.parsers.ReverseCodeSet;
 
 public class ARHotSpringCountyParser extends FieldProgramParser {
-  
+
   private HtmlDecoder decoder = new HtmlDecoder();
-  
+
   public ARHotSpringCountyParser() {
-    super("HOT SPRING COUNTY", "AR", 
+    super("HOT SPRING COUNTY", "AR",
           "( RUN_REPORT/R RUN_REPORT! CALL:CALL! PLACE:PLACE! ADDR:ADDR! CITY:CITY! ID:ID! Cross-Street:X! ( Notes:INFO! | INFO:INFO! ) " +
-          "| CALL:CALL! PLACE:PLACE? ADDR:ADDR! Cross-Streeet:X? CITY:CITY! XY:GPS? ID:ID? PRI:PRI? DATE:DATETIME? TIME:TIME? X:X? INFO:INFO! " + 
-          "| Category:CALL! Address:ADDR2! Intersection:X? Business_Name:PLACE! Event_#:ID! Date_/_Time:DATETIME! Notes:INFO " + 
+          "| CALL:CALL! PLACE:PLACE? ADDR:ADDR! Cross-Street:X? CITY:CITY! XY:GPS? ID:ID? PRI:PRI? DATE:DATETIME? TIME:TIME? X:X? INFO:INFO! " +
+          "| Category:CALL! Address:ADDR2! Intersection:X? Business_Name:PLACE! Event_#:ID! Date_/_Time:DATETIME! Notes:INFO " +
           "| LOCATION:ADDRCITYST! CALL_TYPE:CALL! CALLER:NAME! CALL_TIME:DATETIME3 CAD_NUMBER:ID! CALL_NOTES:INFO3 RUN_TIMES:TIMES3 END! " +
-          ") INFO/N+? CAD END");
+          ") INFO/N+? CAD INFO/N+");
   }
-  
+
   @Override
   public String getFilter() {
     return "DISPATCH@HOTSPRINGDEM.ORG";
   }
-  
+
   @Override
   public int getMapFlags() {
     return MAP_FLG_PREFER_GPS;
   }
-  
+
   @Override
   protected boolean parseHtmlMsg(String subject, String body, Data data) {
     if (body.startsWith("<html>")) {
@@ -67,36 +67,36 @@ public class ARHotSpringCountyParser extends FieldProgramParser {
       if (sb != null && sb.length() > 0) list.add(sb.toString());
       return parseFields(list.toArray(new String[list.size()]), data);
     }
-    
+
     return super.parseHtmlMsg(subject, body, data);
   }
-  
+
   private static final Pattern MARKER = Pattern.compile("(PLEASE RESPOND IMMEDIATELY|RUN REPORT) +(?=LOCATION:)");
 
   @Override
   protected boolean parseMsg(String body, Data data) {
     int pt = body.indexOf('�');
     if (pt >= 0) body = body.substring(0, pt).trim();
-    
+
     if (body.startsWith("CALL:") || body.startsWith("RUN REPORT\n")) {
       return super.parseFields(body.split("\n"), data);
     }
-    
+
     if (body.startsWith("Category:")) {
       body = body.replace('\n', ' ');
       return super.parseMsg(body, data);
     }
-    
+
     Matcher match = MARKER.matcher(body);
     if (match.lookingAt()) {
       if (match.group(1).equals("RUN REPORT")) data.msgType = MsgType.RUN_REPORT;
       body = body.substring(match.end());
       return super.parseMsg(body, data);
     }
-    
+
     return false;
   }
-  
+
   @Override
   public Field getField(String name) {
     if (name.equals("RUN_REPORT")) return new SkipField("RUN REPORT");
@@ -113,7 +113,7 @@ public class ARHotSpringCountyParser extends FieldProgramParser {
     if (name.equals("CAD")) return new IdField("CAD#[- ]+(.*)", true);
     return super.getField(name);
   }
-  
+
   private class MyCallField extends CallField {
     @Override
     public void parse(String field, Data data) {
@@ -121,7 +121,7 @@ public class ARHotSpringCountyParser extends FieldProgramParser {
       super.parse(field, data);
     }
   }
-  
+
   private class MyCityField extends CityField {
     @Override
     public void parse(String field, Data data) {
@@ -129,33 +129,33 @@ public class ARHotSpringCountyParser extends FieldProgramParser {
       super.parse(field, data);
     }
   }
-  
+
   private static final Pattern ADDR_PTN = Pattern.compile("(\\d+)([^ ].*)");
   private class MyAddress2Field extends AddressField {
     @Override
     public void parse(String field, Data data) {
-      
+
       // Extract city from end of field
       String city = CITY_LIST.getCode(field);
       if (city != null) {
         field = field.substring(0, field.length()-city.length()).trim();
         if (!city.equals("COUNTY")) data.strCity = city;
       }
-      
+
       // Replace blank between house number and street name
       Matcher match = ADDR_PTN.matcher(field);
       if (match.matches()) field =  match.group(1) + ' ' + match.group(2);
-      
+
       // parse address
       super.parse(field, data);
     }
-    
+
     @Override
     public String getFieldNames() {
       return super.getFieldNames() + " CITY";
     }
   }
-  
+
   private static final Pattern DATE_TIME_PTN = Pattern.compile("(\\d\\d?/\\d\\d?/\\d{4})(?: (\\d\\d?:\\d\\d:\\d\\d [AP]M))?");
   private static final DateFormat TIME_FMT = new SimpleDateFormat("hh:mm:ss aa");
   private class MyDateTimeField extends DateTimeField {
@@ -168,15 +168,16 @@ public class ARHotSpringCountyParser extends FieldProgramParser {
       if (time != null) setTime(TIME_FMT, time, data);
     }
   }
-  
+
   private class MyCrossField extends CrossField {
     @Override
     public void parse(String field, Data data) {
+      if (!data.strCross.isEmpty()) return;
       field = field.replace('@', '/');
       super.parse(field, data);
     }
   }
-  
+
   private static final Pattern ADDR_ST_ZIP_PTN = Pattern.compile("([A-Z]{2})(?: +(\\d{5}))?");
   private class MyAddressCityStateField extends Field {
     @Override
@@ -193,13 +194,13 @@ public class ARHotSpringCountyParser extends FieldProgramParser {
       data.strCity = city;
       parseAddress(p.get(), data);
     }
-    
+
     @Override
     public String getFieldNames() {
       return "ADDR APT CITY ST";
     }
   }
-  
+
   private static final Pattern INFO3_BRK_PTN = Pattern.compile("(?:^|; *)\\d\\d?/\\d\\d/\\d\\d \\d\\d?:\\d\\d:\\d\\d - +");
   private class MyInfo3Field extends InfoField {
     @Override
@@ -209,7 +210,7 @@ public class ARHotSpringCountyParser extends FieldProgramParser {
       }
     }
   }
-  
+
   private class MyTimes3Field extends InfoField {
     @Override
     public void parse(String field, Data data) {
@@ -219,9 +220,9 @@ public class ARHotSpringCountyParser extends FieldProgramParser {
       }
     }
   }
-  
+
   private static final ReverseCodeSet CITY_LIST = new ReverseCodeSet(
-      
+
       "COUNTY",
 
       // Cities
