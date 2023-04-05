5c1f575b15ffa7b5fe4df9f0629e605b5665c9ae
Ken Corbin <ken@cadpage.org>
2023-04-05 16:27:14 -0700

    Update msg doc

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/MO/MOHenryCountyAParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/MO/MOHenryCountyAParser.java
@@ -1,53 +1,12 @@
 package net.anei.cadpage.parsers.MO;
 
-import net.anei.cadpage.parsers.CodeSet;
 import net.anei.cadpage.parsers.dispatch.DispatchA48Parser;
 
 public class MOHenryCountyAParser extends DispatchA48Parser {
 
   public MOHenryCountyAParser() {
     super(CITY_LIST, "HENRY COUNTY", "MO", FieldType.PLACE, A48_ONE_WORD_CODE);
-    setupCallList(CALL_LIST);
   }
-
-  private static final CodeSet CALL_LIST = new  CodeSet(
-      "ABANVEH",
-      "ALARM",
-      "ALARM - ALARM",
-      "ANIMAL",
-      "ASSIST",
-      "ASSIST - ASSIST",
-      "BUILDCK",
-      "CI",
-      "CWB",
-      "DEATH",
-      "DIST",
-      "FIREGRASS - FIRE GRASS",
-      "FIRESTRU - FIRE STRUCTURE",
-      "FIREVEH - FIRE VEHICLE",
-      "FOLLUP",
-      "FOUNDPROP",
-      "INFORMATION",
-      "INVEST",
-      "INVEST - INVESTIGATION",
-      "MVA",
-      "MED",
-      "MED - MEDICAL",
-      "MISSPER",
-      "MVA - MOTOR VEHICLE ACCIDENT",
-      "PAPER",
-      "PS",
-      "RUNAWAY",
-      "STEAL",
-      "SUSP",
-      "TRANSPAT",
-      "TRANSPAT - TRANSFER PATIENT",
-      "TS",
-      "SUICIDE",
-      "VISIT",
-      "WARRANT"
-
-  );
 
   private static final String[] CITY_LIST = new String[]{
 
@@ -55,6 +14,7 @@ public class MOHenryCountyAParser extends DispatchA48Parser {
       "BROWNINGTON",
       "CALHOUN",
       "CLINTON",
+      "CREIGHTON",
       "DEEPWATER",
       "HARTWELL",
       "LA DUE",
