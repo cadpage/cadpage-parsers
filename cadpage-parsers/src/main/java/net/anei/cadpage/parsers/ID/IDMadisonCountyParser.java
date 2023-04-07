93bb6b8722bd3132655c6dcd4fd67f54ef8c5053
Ken Corbin <ken@cadpage.org>
2023-04-06 22:00:19 -0700

    Fixed parsing problem with Madison County, ID

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/ID/IDMadisonCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/ID/IDMadisonCountyParser.java
@@ -1,5 +1,8 @@
 package net.anei.cadpage.parsers.ID;
 
+import java.util.regex.Pattern;
+
+import net.anei.cadpage.parsers.MsgInfo.Data;
 import net.anei.cadpage.parsers.dispatch.DispatchA55Parser;
 
 public class IDMadisonCountyParser extends DispatchA55Parser {
@@ -7,9 +10,19 @@ public class IDMadisonCountyParser extends DispatchA55Parser {
   public IDMadisonCountyParser() {
     super("MADISON COUNTY", "ID");
   }
-  
+
   @Override
   public String getFilter() {
     return "ereports@eforcesoftware.com";
   }
+
+  private static final Pattern INFO_BRK_PTN = Pattern.compile(" *// *");
+
+  @Override
+  protected boolean parseMsg(String subject, String body, Data data) {
+    if (!super.parseMsg(subject, body, data)) return false;
+    data.strSupp = INFO_BRK_PTN.matcher(data.strSupp).replaceAll("\n");
+    return true;
+  }
+
 }
