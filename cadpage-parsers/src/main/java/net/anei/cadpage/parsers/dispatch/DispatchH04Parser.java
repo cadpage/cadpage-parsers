3afa4e82e409372095f1796e4096ba98a3a1de70
Ken Corbin <ken@cadpage.org>
2023-04-07 09:45:41 -0700

    Fixed parsaing problem with Flathead County, MT (C)

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/dispatch/DispatchH04Parser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/dispatch/DispatchH04Parser.java
@@ -6,24 +6,31 @@ import java.util.regex.Pattern;
 import net.anei.cadpage.parsers.MsgInfo.Data;
 
 public class DispatchH04Parser extends DispatchH05Parser {
-  
+
   public DispatchH04Parser(String defCity, String defState) {
-    super(defCity, defState, 
-          "SKIP+? ( ADDR:ADDRCITY! PLACE:PLACE! Fire_Call_Type:CALL! EMS_CALL_TYPE:CALL2! " + 
-                 "| ( CALL:CALL! | Fire_Call_Type:CALL! EMS_CALL_TYPE:CALL2 | FIRE_CALL_TYPE:CALL! EMS_CALL_TYPE:CALL2! | EMS_CALL_TYPE:CALL2 ) " + 
-                     "PLACE:PLACE! ADDR:ADDRCITY! " +
-                 ") " +
-             "Lat_/_Long:GPS? ( Cross_Streets:X | CROSS_STREETS:X | ) CALLER_NAME:NAME? " + 
-             "( CALLER_PHONE:PHONE | Caller_Phone:PHONE | ) Lat_/_Long:GPS? ID:ID? " + 
-             "( PRI:PRI! | FIRE_PRIORITY:PRI! EMS_PRIORITY:PRI2! | ) DATE:DATETIME! " + 
-             "( MAP:MAP! | FIRE_QUADRANT:MAP! EMS_District:MAP2! ) " + 
-             "( UNIT:UNIT! | ASSIGNED_UNIT(s):UNIT! ) " + 
-             "( INFO:INFO_BLK! | NARRATIVE:INFO_BLK! ) INFO_BLK/N+? TIMES:TIMES? TIMES+ " + 
+    super(defCity, defState,
+          "LEAD_JUNK+? ( ADDR:ADDRCITY! PLACE:PLACE! | ) " +
+          "( CALL:CALL! | Fire_Call_Type:CALL! EMS_CALL_TYPE:CALL2 | FIRE_CALL_TYPE:CALL! EMS_CALL_TYPE:CALL2! | EMS_CALL_TYPE:CALL2 ) " +
+          "( PLACE:PLACE! ADDR:ADDRCITY! | ) " +
+          "( ASSIGNED_UNIT(s):UNIT! NARRATIVE:EMPTY! INFO_BLK/N+? PLACE:PLACE! ADDR:ADDR! | ) " +
+          "Lat_/_Long:GPS? ( Cross_Streets:X | CROSS_STREETS:X | ) CALLER_NAME:NAME? " +
+             "( CALLER_PHONE:PHONE | Caller_Phone:PHONE | Caller_Number:PHONE | ) Lat_/_Long:GPS? ID:ID? " +
+             "( PRI:PRI! | FIRE_PRIORITY:PRI! EMS_PRIORITY:PRI2! | ) DATE:DATETIME! " +
+             "( MAP:MAP! | FIRE_QUADRANT:MAP! EMS_District:MAP2! ) " +
+             "( UNIT:UNIT! | ASSIGNED_UNIT(s):UNIT! | ) " +
+             "( INFO:INFO_BLK! | NARRATIVE:INFO_BLK! | ) INFO_BLK/N+? TIMES:TIMES? TIMES+ " +
              "CALLER_NAME:NAME FIRE_BOX:BOX INCIDENT_NUMBER:ID");
   }
 
   @Override
+  protected boolean parseHtmlMsg(String subject, String body, Data data) {
+    if (!super.parseHtmlMsg(subject, body, data)) return false;
+    return !data.strAddress.isEmpty();
+  }
+
+  @Override
   public Field getField(String name) {
+    if (name.equals("LEAD_JUNK")) return new SkipField("(?!(?:ADDR|CALL|Fire Call Type|FIRE CALL TYPE|EMS CALL TYPE):).*", true);
     if (name.equals("CALL2")) return new BaseCall2Field();
     if (name.equals("ADDRCITY")) return new BaseAddressCityField();
     if (name.equals("PRI2")) return new BasePriority2Field();
@@ -31,7 +38,7 @@ public class DispatchH04Parser extends DispatchH05Parser {
     if (name.equals("MAP2")) return new BaseMap2Field();
     return super.getField(name);
   }
-  
+
   private class BaseCall2Field extends CallField {
     @Override
     public void parse(String field, Data data) {
@@ -39,7 +46,7 @@ public class DispatchH04Parser extends DispatchH05Parser {
       data.strCall = append(data.strCall, " / ", field);
     }
   }
-  
+
   private static final Pattern ADDR_CITY_ST_PTN = Pattern.compile("(.*?,.*), *(.*)");
   private class BaseAddressCityField extends AddressCityField {
     @Override
@@ -52,13 +59,13 @@ public class DispatchH04Parser extends DispatchH05Parser {
       }
       super.parse(field, data);
     }
-    
+
     @Override
     public String getFieldNames() {
       return super.getFieldNames() + " ST";
     }
   }
-  
+
   private class BasePriority2Field extends CallField {
     @Override
     public void parse(String field, Data data) {
@@ -66,7 +73,7 @@ public class DispatchH04Parser extends DispatchH05Parser {
       data.strPriority = append(data.strPriority, " / ", field);
     }
   }
-  
+
   private class BaseMap2Field extends CallField {
     @Override
     public void parse(String field, Data data) {
