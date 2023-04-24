37ad5caed483641ff99e9a4414804171049a1e1d
Ken Corbin <ken@cadpage.org>
2023-04-24 12:28:59 -0700

    Fixed parsing problem with Montgomery County, MD (C)

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/MD/MDMontgomeryCountyCParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/MD/MDMontgomeryCountyCParser.java
@@ -11,7 +11,10 @@ public class MDMontgomeryCountyCParser extends FieldProgramParser {
 
   public MDMontgomeryCountyCParser() {
     super("MONTGOMERY COUNTY", "MD",
-          "ID CALL ADDR! CITY Apt/Suite:APT? LOC:PLACE ( Box_Area:BOX! | Area:MAP! Beat:MAP/L! ) X/Y:GPS? TG/CH:CH? Units:UNIT! END");
+          "( Trigger:CODE CAD_Incident_Number:ID! Incident_Type:CALL! Location_Name:PLACE! Location:ADDR! Units_Dispatched:UNIT! " +
+          "| CAD_Incident_Number:SKIP! Inc_#:ID! Incident_Type:CALL! Location:PLACE! Location_Name:ADDR! Units_Dispatched:UNIT! " +
+          "| ID CALL ADDR! CITY Apt/Suite:APT? LOC:PLACE ( Box_Area:BOX! | Area:MAP! Beat:MAP/L! ) X/Y:GPS? TG/CH:CH? Units:UNIT! " +
+          ") END");
   }
 
   @Override
@@ -28,15 +31,16 @@ public class MDMontgomeryCountyCParser extends FieldProgramParser {
         int pt2 = body.indexOf("</p>\n", pt1);
         if (pt2 < 0) return false;
         body = body.substring(pt1, pt2).trim();
+      } else if ((pt1 = body.indexOf("MESSAGE:")) >= 0) {
+        int pt2 = body.indexOf('\n', pt1);
+        if (pt2 < 0) return false;
+        body = body.substring(pt1, pt2).trim();
+      } else if ((pt1 = body.indexOf("Trigger:")) >= 0) {
+        int pt2 = body.indexOf("</p>", pt1+8);
+        if (pt2 < 0) return false;
+        body = body.substring(pt1, pt2).trim().replace("<br>", "\n");
       } else {
-        pt1 = body.indexOf("MESSAGE:");
-        if (pt1 >= 0) {
-          int pt2 = body.indexOf('\n', pt1);
-          if (pt2 < 0) return false;
-          body = body.substring(pt1, pt2).trim();
-        } else {
-          return false;
-        }
+        return false;
       }
       return parseMsg(body, data);
     }
@@ -50,6 +54,10 @@ public class MDMontgomeryCountyCParser extends FieldProgramParser {
     if (body.startsWith("CAD MSG:")) {
       body = body.substring(8).trim();
       return parseFields(body.split(" \\* "), data);
+    }
+
+    if (body.startsWith("Trigger:") || body.startsWith("CAD Incident Number:")) {
+      return parseFields(body.split("\n+"), data);
     }
 
     if (body.startsWith("MESSAGE:")) {
