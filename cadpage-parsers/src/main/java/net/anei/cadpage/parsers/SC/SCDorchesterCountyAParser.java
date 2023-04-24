b73eff4dec5d3b73705780a2a93174d18ac9df27
Ken Corbin <ken@cadpage.org>
2023-04-24 09:35:08 -0700

    Fixed parsing problem with Dorchester County, SC (A)

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/SC/SCDorchesterCountyAParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/SC/SCDorchesterCountyAParser.java
@@ -1,19 +1,17 @@
 package net.anei.cadpage.parsers.SC;
 
-import net.anei.cadpage.parsers.dispatch.DispatchA38Parser;
+import net.anei.cadpage.parsers.dispatch.DispatchA71Parser;
 
 
 
-public class SCDorchesterCountyAParser extends DispatchA38Parser {
-  
+public class SCDorchesterCountyAParser extends DispatchA71Parser {
+
   public SCDorchesterCountyAParser() {
     super("DORCHESTER COUNTY", "SC");
-    setupMultiWordStreets("OLD ST GEORGE");
-  }
-  
-  @Override
-  public String getFilter() {
-    return "dispatch@dorchestercounty.net";
   }
 
+  @Override
+  public int getMapFlags() {
+    return MAP_FLG_PREFER_GPS;
+  }
 }
