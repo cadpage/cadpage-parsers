b08efbe8b7528c7504ada62c4a1bdea72f150352
Ken Corbin <ken@cadpage.org>
2023-05-01 10:33:34 -0700

    Fixed parsing problem with Fremont County, CO

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/CO/COFremontCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/CO/COFremontCountyParser.java
@@ -1,16 +1,16 @@
 package net.anei.cadpage.parsers.CO;
 
-import net.anei.cadpage.parsers.dispatch.DispatchA33Parser;
+import net.anei.cadpage.parsers.dispatch.DispatchBCParser;
 
-public class COFremontCountyParser extends DispatchA33Parser {
+public class COFremontCountyParser extends DispatchBCParser {
 
 
   public COFremontCountyParser() {
     super("FREMONT COUNTY", "CO");
   }
-  
+
   @Override
   public String getFilter() {
-    return "FRECOM911@FREMONTSO.COM,FRECOM911@PUBLICSAFETYSOFTWARE.NET,FRECOM911@OMNIGO.COM,FRECOM911@ITIUSA.COM,noreply@omnigo.com";
+    return "noreply@omnigo.com";
   }
 }
