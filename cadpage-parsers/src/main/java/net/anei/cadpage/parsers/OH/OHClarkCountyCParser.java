0686a80d38ba2f6f4f55d95721e94572b74981bc
Ken Corbin <ken@cadpage.org>
2023-05-05 16:49:53 -0700

    Update sender filter for Clark County, OH (C)

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/OH/OHClarkCountyCParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/OH/OHClarkCountyCParser.java
@@ -3,13 +3,13 @@ package net.anei.cadpage.parsers.OH;
 import net.anei.cadpage.parsers.dispatch.DispatchA19Parser;
 
 public class OHClarkCountyCParser extends DispatchA19Parser {
-  
+
   public OHClarkCountyCParser() {
     super("CLARK COUNTY", "OH");
   }
-  
+
   @Override
   public String getFilter() {
-    return "@SPRINGFIELDOHIO.GOV,@alert.active911.com,FlexRapidNotification@dccnotify.com";
+    return "@SPRINGFIELDOHIO.GOV,@alert.active911.com,FlexRapidNotification@dccnotify.com,911Relay@ClarkCountyCommunicationsCe.onmicrosoft.com";
   }
 }
