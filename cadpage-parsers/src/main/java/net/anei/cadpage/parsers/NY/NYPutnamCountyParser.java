0f6991b58fa09cb7bb8220bf2bc2715ddcf9c55d
Ken Corbin <ken@cadpage.org>
2023-05-08 13:29:10 -0700

    Fixed parsing problem with Putnam County, NY

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/NY/NYPutnamCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/NY/NYPutnamCountyParser.java
@@ -8,7 +8,7 @@ import net.anei.cadpage.parsers.MsgInfo.Data;
 
 
 public class NYPutnamCountyParser extends MsgParser {
-  
+
   private static final Pattern STA_MARKER = Pattern.compile("[\\| ]STA ");
   private static final Pattern TSP_PATTERN = Pattern.compile("\\bTSP\\b");
 
@@ -17,7 +17,7 @@ public class NYPutnamCountyParser extends MsgParser {
     super("PUTNAM COUNTY", "NY");
     setFieldList("CALL PLACE ADDR CITY APT SRC X INFO");
   }
-  
+
 
 @Override
   public String getFilter() {
@@ -26,14 +26,18 @@ public class NYPutnamCountyParser extends MsgParser {
 
 @Override
   protected boolean parseMsg(String body, Data data) {
-  
-    if (body.indexOf('|') < 0) body = body.replace('\n', '|');
-  
+
+    if (body.indexOf('|') < 0) {
+      body = body.replace("=20\n", "\n");
+      body = body.replace('\n', '|');
+      body = stripFieldEnd(body, "=20");
+    }
+
     Matcher match = STA_MARKER.matcher(body);
     if (!match.find()) return false;
     String sPart1 = body.substring(0,match.start()).trim();
     String sPart2 = body.substring(match.end()).trim();
-    
+
     Parser p = new Parser(sPart1);
     data.strCall = p.get('|');
     data.strPlace = p.get('|');
@@ -48,7 +52,7 @@ public class NYPutnamCountyParser extends MsgParser {
     if (sCross.startsWith("XS ")) sCross = sCross.substring(3).trim();
     data.strSupp = p.get();
     if (data.strSupp.startsWith("NARR ")) data.strSupp = data.strSupp.substring(5).trim();
-    
+
     if (sAddr.length() == 0) {
       sAddr = sCross;
       sCross = "";
@@ -57,5 +61,5 @@ public class NYPutnamCountyParser extends MsgParser {
     data.strCross = sCross;
     return true;
   }
-  
+
 }
