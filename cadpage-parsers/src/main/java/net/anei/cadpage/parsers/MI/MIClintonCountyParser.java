f04b84aebc57fd30eb10ecaa4456465561b370fc
Ken Corbin <ken@cadpage.org>
2023-05-11 08:53:52 -0700

    Fixed parsing problem with Clington County, MI

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/MI/MIClintonCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/MI/MIClintonCountyParser.java
@@ -9,17 +9,17 @@ import net.anei.cadpage.parsers.dispatch.DispatchOSSIParser;
  * Clinton County, MI
  */
 public class MIClintonCountyParser extends DispatchOSSIParser {
-  
+
   public MIClintonCountyParser() {
     super("CLINTON COUNTY", "MI",
-          "( CANCEL ADDR! SKIP | ADDR X/Z+? CALL! PRI? ) INFO/N+");
+          "( CANCEL ADDR! SKIP | FYI? ADDR X/Z+? CALL! PRI? ) INFO/N+");
   }
-  
+
   @Override
   public String getFilter() {
     return "CAD@clinton-county.org,CAD@shiawassee.net";
   }
-  
+
   @Override
   protected boolean parseMsg(String body, Data data) {
     if (!body.startsWith("CAD:")) body = "CAD:" + body;
@@ -32,28 +32,28 @@ public class MIClintonCountyParser extends DispatchOSSIParser {
     if (name.equals("PRI")) return new PriorityField("\\d", true);
     return super.getField(name);
   }
-  
-  private static final Pattern CALL_PTN = Pattern.compile(".* [1-4]|FIRE .*|COMMUNITY POLICING|CARBON MONOXIDE.*|EMERGENCY SERVICES TEAM|HAZARDOUS MATERIALS|HIT AND RUN PI ACCIDENT|LIFT ASSIST.*|MEDICAL|MOUNTED DIVISION|MUTUAL AID.*|PERSONAL INJURY.*|POWER LINES DOWN|PSYCHIATRIC.*|ROAD CLOSED|STRUCTURE FIRE|SUICIDE.*|TRAINING.*|UNKNOWN ACCIDENT");
+
+  private static final Pattern CALL_PTN = Pattern.compile(".* [1-4]|FIRE .*|COMMUNITY POLICING|CARBON MONOXIDE.*|EMERGENCY SERVICES TEAM|HAZARDOUS MATERIALS|HIT AND RUN PI ACCIDENT|LIFT ASSIST.*|MEDICAL|MOUNTED DIVISION|MUTUAL AID.*|PERSONAL INJURY.*|POWER LINES DOWN|PSYCHIATRIC.*|ROAD CLOSED|SPECIAL OPERATIONS TEAM|STRUCTURE FIRE|SUICIDE.*|TRAINING.*|UNKNOWN ACCIDENT");
   private class MyCallField extends CallField {
     @Override
     public boolean canFail() {
       return true;
     }
-    
+
     public boolean checkParse(String field, Data data) {
       if (!getRelativeField(+1).equals("1") &&
           !CALL_PTN.matcher(field).matches()) return false;
       super.parse(field, data);
       return true;
     }
-    
+
     public void parse(String field, Data data) {
       if (!checkParse(field, data)) abort();
     }
   }
-  
+
   private static final Pattern BUS127_PTN = Pattern.compile("\\bBUS *127", Pattern.CASE_INSENSITIVE);
-  
+
   @Override
   public String adjustMapAddress(String addr) {
     addr = BUS127_PTN.matcher(addr).replaceAll("OLD US 27");
