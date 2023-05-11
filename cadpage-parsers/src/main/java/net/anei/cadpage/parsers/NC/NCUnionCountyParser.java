98c769b1a4e0487d97fb14117c2a77cb1fba4dcb
Ken Corbin <ken@cadpage.org>
2023-05-11 09:18:41 -0700

    Fixed parsing problem with Union County, NC

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/NC/NCUnionCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/NC/NCUnionCountyParser.java
@@ -12,25 +12,25 @@ import net.anei.cadpage.parsers.dispatch.DispatchOSSIParser;
 
 
 public class NCUnionCountyParser extends DispatchOSSIParser {
-  
+
   public NCUnionCountyParser() {
     super(CITY_LIST, "UNION COUNTY", "NC",
-           "( CANCEL ADDR CITY2 PLACE2 " + 
-           "| FYI? ID? ADDR ( CITY ID? | CITY/Z ID | ID? ) CALL! SRC? CH? INFO1/N+? DATETIME ID? UNIT Radio_Channel:CH INFO/N+ END " +
+           "( CANCEL ADDR CITY2 PLACE2 " +
+           "| FYI? ID? ADDR ( CITY ID? | CITY/Z ID | ID? ) CALL! SRC? CH? INFO1/N+? DATETIME ID? UNIT Radio_Channel:CH INFO/N+? GPS1 GPS2 " +
            ")");
     setupSaintNames("JOHNS", "SIMONS");
     setupProtectedNames("BURGESS AND HELMS", "SUGAR AND WINE");
     setupMultiWordStreets("INDIAN TRAIL FAIRVIEW");
   }
-  
+
   @Override
   public String getFilter() {
     return "cad@uc.co.union.nc.us,cad@webmail.co.union.nc.us,cad@co.union.nc.us,CAD@unioncountync.gov";
   }
-  
+
   @Override
   protected boolean parseMsg(String body, Data data) {
-    
+
     boolean good = body.startsWith("CAD:");
     if (!good) body = "CAD:" + body;
     if (!super.parseMsg(body, data)) return false;
@@ -49,22 +49,24 @@ public class NCUnionCountyParser extends DispatchOSSIParser {
     if (name.equals("INFO1")) return new MyInfo1Field();
     if (name.equals("DATETIME")) return new DateTimeField("\\d\\d/\\d\\d/\\d{4} \\d\\d:\\d\\d:\\d\\d");
     if (name.equals("ID")) return new IdField("\\d{5,}", true);
+    if (name.equals("GPS1")) return new MyGPSField(1);
+    if (name.equals("GPS2")) return new MyGPSField(2);
     return super.getField(name);
   }
-  
+
   private class MyCancelField extends BaseCancelField {
     public MyCancelField() {
       super("CPR IN PROGRESS|RETONE COMPLETE|STAGING RECOMMENDED");
     }
   }
-  
+
   private static final Pattern UNIT_CALL_PTN = Pattern.compile("\\{([A-Z0-9 ]+)\\} *(.*)");
   private class MyUnitCallField extends Field {
     @Override
     public boolean canFail() {
       return true;
     }
-    
+
     @Override
     public boolean checkParse(String field, Data data) {
       Matcher match = UNIT_CALL_PTN.matcher(field);
@@ -73,25 +75,25 @@ public class NCUnionCountyParser extends DispatchOSSIParser {
       data.strCall = match.group(2);
       return true;
     }
-    
+
     @Override
     public void parse(String field, Data data) {
       if (!checkParse(field, data)) abort();
     }
-    
+
     @Override
     public String getFieldNames() {
       return "UNIT CALL";
     }
   }
-  
+
   private class MyCity2Field extends CityField {
     @Override
     public void parse(String field, Data data) {
       data.strCity = convertCodes(field, CITY_CODES);
     }
   }
-  
+
   private Pattern PLACE_DIR_PTN = Pattern.compile(" *\\([NSEW]\\) *");
   private class MyPlace2Field extends PlaceField {
     @Override
@@ -100,20 +102,20 @@ public class NCUnionCountyParser extends DispatchOSSIParser {
       super.parse(field, data);
     }
   }
-  
+
   // Custom field is an optional 6 character field that
   // different departments use for different purposes.  We will look at the
   // previous source field to figure out which department this is and what
   // they want us to do with it.
-  
+
   private static final Pattern SRC_CHANNEL_PTN = Pattern.compile("S\\d\\d|CH *\\d");
   private class CustomField extends Field {
-    
+
     @Override
     public boolean canFail() {
       return true;
     }
-    
+
     @Override
     public boolean checkParse(String field, Data data) {
       if (field.length() > 6 || field.contains(" ")  && !field.startsWith("CH ")) return false;
@@ -129,27 +131,27 @@ public class NCUnionCountyParser extends DispatchOSSIParser {
         data.strUnit = field;
       }
     }
-    
+
     @Override
     public String getFieldNames() {
       return "CH UNIT?";
     }
   }
-  
+
   private class MyChannelField extends ChannelField {
-    
+
     MyChannelField() {
       super("[A-M][-A-Z0-9]{1,5}|.* OPS .*", false);
     }
-    
+
     @Override
     public void parse(String field, Data data) {
       if (data.strChannel.length() > 0) return;
       super.parse(field, data);
     }
-    
+
   }
-  
+
   private class MyInfo1Field extends InfoField {
     @Override
     public void parse(String field, Data data) {
@@ -161,13 +163,38 @@ public class NCUnionCountyParser extends DispatchOSSIParser {
         data.strSupp = append(data.strSupp, " / ", field);
       }
     }
-    
+
     @Override
     public String getFieldNames() {
       return "PLACE X INFO?";
     }
   }
-  
+
+  private static final Pattern GPS_PTN = Pattern.compile("[-+]?\\d{2}\\.\\d{6,}");
+  private class MyGPSField extends GPSField {
+    public MyGPSField(int type) {
+      super(type);
+    }
+
+    @Override
+    public boolean canFail() {
+      return true;
+    }
+
+    @Override
+    public boolean checkParse(String field, Data data) {
+      if (!isLastField(2)) return false;
+      if (!GPS_PTN.matcher(field).matches()) return false;
+      super.parse(field, data);
+      return true;
+    }
+
+    @Override
+    public void parse(String field, Data data) {
+      if (!checkParse(field, data)) abort();
+    }
+  }
+
   private static final String[] CITY_LIST = new String[]{
     "FAIRVIEW",
     "HEMBY BRIDGE",
@@ -183,14 +210,14 @@ public class NCUnionCountyParser extends DispatchOSSIParser {
     "WEDDINGTON",
     "WESLEY CHAPEL",
     "WINGATE",
-    
+
     // Anson County
     "PEACHLAND",
-    
+
     // Mecklenburg County
     "MATTHEWS"
   };
-  
+
   // CIty codes are only used for cance/under control/confirmed fire updates
   private static final Properties CITY_CODES = buildCodeTable(new String[]{
       "FAI", "FAIRVILLE",
