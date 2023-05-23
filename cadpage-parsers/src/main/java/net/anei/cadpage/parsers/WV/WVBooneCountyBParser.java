9f9fdc43dceaa664c54c22c01eec5476ab8bc785
Ken Corbin <ken@cadpage.org>
2023-05-23 15:00:06 -0700

    Fixed parsing problem with Booen County, WV (B)

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/WV/WVBooneCountyBParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/WV/WVBooneCountyBParser.java
@@ -1,5 +1,6 @@
 package net.anei.cadpage.parsers.WV;
 
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import net.anei.cadpage.parsers.FieldProgramParser;
@@ -9,12 +10,14 @@ public class WVBooneCountyBParser extends FieldProgramParser {
 
   public WVBooneCountyBParser() {
     super("BOONE COUNTY", "WV",
-          "Location:ADDRCITYST! Cross_Street:X! CFS_#:ID! 911_Lat:GPS1! 911_Long:GPS2! Details:INFO! END");
+          "Location:ADDRCITYST! ( CFS_#:ID! Responding_Agencies:SRC! Caller's_Number:PHONE! Nearest_Intersection:SKIP! Cross_Street:X! " +
+                               "| Cross_Street:X! CFS_#:ID! " +
+                               ") 911_Lat:GPS1! 911_Long:GPS2! Uncertainty:SKIP? Details:INFO! END");
   }
 
   @Override
   public String getFilter() {
-    return "no-reply@zuercherportal.com,boonezuercher@gmail.com";
+    return "zuercher@boonewv.com";
   }
 
   @Override
@@ -33,8 +36,36 @@ public class WVBooneCountyBParser extends FieldProgramParser {
 
   @Override
   public Field getField(String name) {
+    if (name.equals("ADDRCITYST")) return new MyAddressCityStateField();
+    if (name.equals("PHONE")) return new MyPhoneField();
     if (name.equals("INFO")) return new MyInfoField();
     return super.getField(name);
+  }
+
+  private static final Pattern ADDRESS_CITY_ST_PLACE_PTN = Pattern.compile("(.*, WV(?: +\\d{5})?) *(.*)");
+  private class MyAddressCityStateField extends AddressCityStateField {
+    @Override
+    public void parse(String field, Data data) {
+      Matcher match = ADDRESS_CITY_ST_PLACE_PTN.matcher(field);
+      if (match.matches()) {
+        field = match.group(1);
+        data.strPlace = match.group(2);
+      }
+      super.parse(field, data);
+    }
+
+    @Override
+    public String getFieldNames() {
+      return super.getFieldNames() + " PLACE";
+    }
+  }
+
+  private class MyPhoneField extends PhoneField {
+    @Override
+    public void parse(String field, Data data) {
+      if (field.equals("None")) return;
+      super.parse(field, data);
+    }
   }
 
   private static final Pattern INFO_DATE_TIME_PTN = Pattern.compile("[; ]*\\b\\d\\d?/\\d\\d?/\\d\\d \\d\\d?:\\d\\d:\\d\\d - *");
