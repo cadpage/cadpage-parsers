95f1385ee3a2f982748e1c135542cba0890b99de
Ken Corbin <ken@cadpage.org>
2023-06-04 22:10:28 -0700

    Fixed parsing problem with Blair County, PA (B)

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/PA/PABlairCountyBParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/PA/PABlairCountyBParser.java
@@ -9,27 +9,27 @@ import net.anei.cadpage.parsers.FieldProgramParser;
 import net.anei.cadpage.parsers.MsgInfo.Data;
 
 public class PABlairCountyBParser extends FieldProgramParser {
-  
+
   public PABlairCountyBParser() {
-    super("BLAIR COUNTY", "PA", 
+    super("BLAIR COUNTY", "PA",
           "Units:UNIT! Inc_Code:CALL! Address:ADDRCITY/S6! X-ST:X! CFS_Number:ID! Date/Time:DATETIME! Narrative:INFO/N+ Lat/Lon:GPS");
   }
-  
+
   @Override
   public String getFilter() {
     return "blair911@atlanticbbn.net";
   }
-  
+
   @Override
   public int getMapFlags() {
     return MAP_FLG_PREFER_GPS;
   }
-  
+
   @Override
   protected boolean parseMsg(String body, Data data) {
     return parseFields(body.split("\n"), data);
   }
-  
+
   @Override
   public Field getField(String name) {
     if (name.equals("ADDRCITY")) return new MyAddressCityField();
@@ -39,21 +39,23 @@ public class PABlairCountyBParser extends FieldProgramParser {
     if (name.equals("GPS")) return new MyGPSField();
     return super.getField(name);
   }
-  
+
   private Pattern ADDR_MAP_PTN = Pattern.compile("(.*) ((?:[NSEW]|[NS][EW]) SECTOR)");
   private class MyAddressCityField extends AddressField {
     @Override
     public void parse(String field, Data data) {
       field = field.replace('@', '&');
       int pt = field.lastIndexOf(',');
-      if (pt >= 0) {
-        String city = field.substring(pt+1).trim();
-        city = stripFieldEnd(city, " BORO");
-        field = field.substring(0,pt).trim();
-        
-        pt = city.indexOf('-');
-        if (pt >= 0) city = city.substring(pt+1).trim();
-        data.strCity = city;
+      if (!field.startsWith("LAT:")) {
+        if (pt >= 0) {
+          String city = field.substring(pt+1).trim();
+          city = stripFieldEnd(city, " BORO");
+          field = field.substring(0,pt).trim();
+
+          pt = city.indexOf('-');
+          if (pt >= 0) city = city.substring(pt+1).trim();
+          data.strCity = city;
+        }
       }
       Matcher match = ADDR_MAP_PTN.matcher(field);
       if (match.matches()) {
@@ -62,13 +64,13 @@ public class PABlairCountyBParser extends FieldProgramParser {
       }
       super.parse(field, data);
     }
-    
+
     @Override
     public String getFieldNames() {
       return "ADDR APT MAP CITY";
     }
   }
-  
+
   private class MyCrossField extends CrossField {
     @Override
     public void parse(String field, Data data) {
@@ -76,7 +78,7 @@ public class PABlairCountyBParser extends FieldProgramParser {
       super.parse(field, data);
     }
   }
-  
+
   private static final Pattern DATE_TIME_PTN = Pattern.compile("(\\d\\d?/\\d\\d?/\\d{4}) (\\d\\d?:\\d\\d:\\d\\d [AP]M)");
   private static final DateFormat TIME_FMT = new SimpleDateFormat("hh:mm:ss aa");
   private class MyDateTimeField extends DateTimeField {
@@ -88,7 +90,7 @@ public class PABlairCountyBParser extends FieldProgramParser {
       setTime(TIME_FMT, match.group(2), data);
     }
   }
-  
+
   private class MyInfoField extends InfoField {
     @Override
     public void parse(String field, Data data) {
@@ -96,7 +98,7 @@ public class PABlairCountyBParser extends FieldProgramParser {
       super.parse(field, data);
     }
   }
-  
+
   private class MyGPSField extends GPSField {
     @Override
     public void parse(String field, Data data) {
