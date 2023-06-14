01a8f351a7de2f94263cc6c3f6c99ae2af6b220d
Ken Corbin <ken@cadpage.org>
2023-06-13 17:41:20 -0700

    Fixed misc parser problems

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/MD/MDHowardCountyBParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/MD/MDHowardCountyBParser.java
@@ -23,9 +23,21 @@ public class MDHowardCountyBParser extends FieldProgramParser {
   @Override
   public Field getField(String name) {
     if (name.equals("ID")) return new IdField("[EF]\\d{8}|OOC\\d{7}", true);
+    if (name.equals("CITY")) return new MyCityField();
     if (name.equals("TIME")) return new TimeField("\\d\\d:\\d\\d:\\d\\d", true);
     if (name.equals("BOX")) return new MyBoxField();
     return super.getField(name);
+  }
+
+  private class MyCityField extends CityField {
+    @Override
+    public void parse(String field, Data data) {
+      if (field.endsWith(")")) {
+        int pt = field.indexOf('(');
+        if (pt >= 0) field = field.substring(0, pt).trim();
+      }
+      super.parse(field, data);
+    }
   }
 
   private class MyBoxField extends BoxField {
--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/MD/MDPrinceGeorgesCountyFParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/MD/MDPrinceGeorgesCountyFParser.java
@@ -4,33 +4,42 @@ import net.anei.cadpage.parsers.FieldProgramParser;
 import net.anei.cadpage.parsers.MsgInfo.Data;
 
 public class MDPrinceGeorgesCountyFParser extends FieldProgramParser {
-  
+
   public MDPrinceGeorgesCountyFParser() {
-    super("PRINCE GEORGES COUNTY", "MD", 
+    super("PRINCE GEORGES COUNTY", "MD",
           "CALL:CALL! ADDR:ADDR! CITY:CITY! UNIT:UNIT! INFO:INFO!");
   }
-  
+
   @Override
   public String getFilter() {
     return "firema8@ehub33.webhostinghub.com,messaging@firemapping.org";
   }
-  
+
   @Override
   protected boolean parseMsg(String body, Data data) {
     return parseFields(body.split("\n"), data);
   }
-  
+
   @Override
   public Field getField(String name) {
     if (name.equals("ADDR")) return new MyAddressField();
+    if (name.equals("CITY")) return new MyCityField();
     return super.getField(name);
   }
-  
+
   private class MyAddressField extends AddressField {
     @Override
     public void parse(String field, Data data) {
       int pt = field.indexOf(',');
       if (pt >= 0) field = field.substring(0,pt).trim();
+      super.parse(field, data);
+    }
+  }
+
+  private class MyCityField extends CityField {
+    @Override
+    public void parse(String field, Data data) {
+      field = field.replace(".", "");
       super.parse(field, data);
     }
   }
