0be3acb2ef4befba2df49c75ae150c334a6b3b4b
Ken Corbin <ken@cadpage.org>
2023-06-16 17:47:45 -0700

    Fixed parsing problem with Deschutes County, OR (B)

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/OR/ORDeschutesCountyBParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/OR/ORDeschutesCountyBParser.java
@@ -6,12 +6,12 @@ import net.anei.cadpage.parsers.HtmlProgramParser;
 import net.anei.cadpage.parsers.MsgInfo.Data;
 
 public class ORDeschutesCountyBParser extends HtmlProgramParser {
-  
+
   public ORDeschutesCountyBParser() {
-    super("DESCHUTES COUNTY", "OR", 
+    super("DESCHUTES COUNTY", "OR",
           "CALL:CALL! PLACE:PLACE! ADDR:ADDRCITY/S6! CROSS_ST:X! ID:ID% DATE:DATETIME% UNIT:UNIT% INFO:INFO% INFO/N+");
   }
-  
+
   @Override
   public String getFilter() {
     return "cad@dc911sd.org,@deschutes.org,7017710262";
@@ -19,16 +19,16 @@ public class ORDeschutesCountyBParser extends HtmlProgramParser {
 
   @Override
   protected boolean parseHtmlMsg(String subject, String body, Data data) {
-    
+
     if (!subject.equals("!") && !subject.startsWith("Automatic R&R Notification:")) return false;
-    
+
     if (body.startsWith("<")) {
       return super.parseHtmlMsg(subject, body, data);
     } else {
       return parseFields(body.split("\n"), data);
     }
   }
-  
+
   @Override
   public Field getField(String name) {
     if (name.equals("ADDRCITY")) return new MyAddressCityField();
@@ -36,21 +36,31 @@ public class ORDeschutesCountyBParser extends HtmlProgramParser {
     if (name.equals("INFO")) return new MyInfoField();
     return super.getField(name);
   }
-  
+
+  private static final Pattern CITY_PTN = Pattern.compile("[A-Z][a-z][A-Za-z ]+");
   private class MyAddressCityField extends AddressCityField {
     @Override
     public void parse(String field, Data data) {
-      field = stripFieldEnd(field, ",");
-      field = field.replace('@',  '/');
-      super.parse(field, data);
+      String apt = "";
+      Parser p = new Parser(field);
+      String city = p.getLastOptional(',');
+      if (!CITY_PTN.matcher(city).matches()) {
+        apt = city;
+        city = p.getLastOptional(',');
+      }
+      data.strCity = city;
+      field = p.get().replace('@',  '/');
+      if (!apt.isEmpty()) field = stripFieldEnd(field, ' '+apt);
+      parseAddress(StartType.START_ADDR, FLAG_RECHECK_APT | FLAG_ANCHOR_END, field, data);
+      if (!apt.isEmpty() && !apt.equalsIgnoreCase(data.strApt)) data.strApt = append(data.strApt, "-", apt);
     }
   }
-  
+
   private static final Pattern DATE_TIME_PTN = Pattern.compile("\\d\\d?/\\d\\d?/\\d{4} \\d\\d?:\\d\\d:\\d\\d");
   private class MyDateTimeField extends DateTimeField {
     @Override
     public void parse(String field, Data data) {
-      
+
       // Forgive a badly formatted date/time field if it is the last field and probably truncated
       if (DATE_TIME_PTN.matcher(field).matches()) {
         super.parse(field, data);
@@ -59,7 +69,7 @@ public class ORDeschutesCountyBParser extends HtmlProgramParser {
       }
     }
   }
-  
+
   private class MyInfoField extends InfoField {
     @Override
     public void parse(String field, Data data) {
@@ -69,7 +79,7 @@ public class ORDeschutesCountyBParser extends HtmlProgramParser {
         data.strSupp = append(data.strSupp, "\n", field);
       }
     }
-    
+
     @Override
     public String getFieldNames() {
       return "CH INFO";
