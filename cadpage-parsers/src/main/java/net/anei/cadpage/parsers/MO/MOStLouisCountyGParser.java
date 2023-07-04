e61337fd965bd1fcc7915f33d806020f96a6ccb2
Ken Corbin <ken@cadpage.org>
2023-07-04 15:06:30 -0700

    Fixed parsing problem with St Louis County, MO (G)

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/MO/MOStLouisCountyGParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/MO/MOStLouisCountyGParser.java
@@ -1,57 +1,15 @@
 package net.anei.cadpage.parsers.MO;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import net.anei.cadpage.parsers.dispatch.DispatchH03Parser;
 
-import net.anei.cadpage.parsers.MsgInfo.Data;
-import net.anei.cadpage.parsers.dispatch.DispatchGlobalDispatchParser;
-
-
-
-public class MOStLouisCountyGParser extends DispatchGlobalDispatchParser {
-
+public class MOStLouisCountyGParser extends DispatchH03Parser {
 
   public MOStLouisCountyGParser() {
-    super(MOStLouisCountyParser.CITY_LIST, "ST LOUIS COUNTY", "MO", LEAD_SRC_UNIT_ADDR,
-          Pattern.compile("FD|STILL"), Pattern.compile("[A-Z]{0,2}\\d{4}|\\d{2}[A-Z]{0,3}|DUTY"));
+    super("ST LOUIS COUNTY", "MO");
   }
 
   @Override
   public String getFilter() {
-    return "Kirkwood@Kirkwoodmo.org";
-  }
-
-  private static final Pattern TAC_PTN = Pattern.compile(" *\\b((?:DUTY )?TAC \\S+)\\b *");
-
-  @Override
-  public boolean parseMsg(String subject, String body, Data data) {
-    if (!subject.equals("Active 911 Paging")) return false;
-    Matcher match = TAC_PTN.matcher(body);
-    if (match.find()) {
-      data.strChannel = match.group(1);
-      body = append(body.substring(0,match.start()), " ", body.substring(match.end()));
-    }
-    if (!super.parseMsg(body, data)) return false;
-    if (data.strApt.equals("-")) data.strApt = "";
-    return true;
-  }
-
-  @Override
-  public String getProgram() {
-    return "CH " + super.getProgram();
-  }
-
-  @Override
-  public Field getField(String name) {
-    if (name.equals("ADDR")) return new MyAddressField();
-    return super.getField(name);
-  }
-
-  protected class MyAddressField extends BaseAddressField {
-    @Override
-    public void parse(String field, Data data) {
-      field = stripFieldEnd(field, " MO");
-      super.parse(field, data);
-    }
+    return "KRWC@rejis.org";
   }
 }
