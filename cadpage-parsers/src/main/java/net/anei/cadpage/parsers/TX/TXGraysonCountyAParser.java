e58e41a29884d741ee1a50abe8eb794f9182d532
Ken Corbin <ken@cadpage.org>
2023-07-03 18:25:48 -0700

    Fixed parsing problem with Grayson County, TX (A)

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/TX/TXGraysonCountyAParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/TX/TXGraysonCountyAParser.java
@@ -1,16 +1,50 @@
 package net.anei.cadpage.parsers.TX;
 
-import net.anei.cadpage.parsers.dispatch.DispatchA82Parser;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-public class TXGraysonCountyAParser extends DispatchA82Parser {
+import net.anei.cadpage.parsers.FieldProgramParser;
+import net.anei.cadpage.parsers.MsgInfo.Data;
+
+public class TXGraysonCountyAParser extends FieldProgramParser {
 
   public TXGraysonCountyAParser() {
-    super("GRAYSON COUNTY", "TX");
+    super("GRAYSON COUNTY", "TX",
+          "DATETIME! CFS:ID_CALL! ADDRCITY! INFO/N+ Narrative:INFO INFO/N+");
   }
 
   @Override
   public String getFilter() {
-    return "ICSAlerts@cityofdenison.com";
+    return "CADAlerts@cityofdenison.com";
   }
 
+  @Override
+  protected boolean parseMsg(String body, Data data) {
+    return parseFields(body.split("\n"), data);
+  }
+
+  @Override
+  public Field getField(String name) {
+    if (name.equals("ID_CALL")) return new MyIdCallField();
+    if (name.equals("DATETIME")) return new DateTimeField("\\d\\d?/\\d\\d?/\\d{4} \\d\\d?:\\d\\d:\\d\\d", true);
+    return super.getField(name);
+  }
+
+  private static final Pattern ID_CALL_PTN = Pattern.compile("(\\d+) *(?:Type: *)?(.*)");
+
+  private class MyIdCallField extends Field {
+
+    @Override
+    public void parse(String field, Data data) {
+      Matcher match = ID_CALL_PTN.matcher(field);
+      if (!match.matches()) abort();
+      data.strCallId = match.group(1);
+      data.strCall = match.group(2);
+    }
+
+    @Override
+    public String getFieldNames() {
+      return "ID CALL";
+    }
+  }
 }
