a70d83ea80c86241be582743ff2138ee8f733862
Ken Corbin <ken@cadpage.org>
2023-07-10 09:04:32 -0700

    Fixed misc parser problems

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/WV/WVJeffersonCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/WV/WVJeffersonCountyParser.java
@@ -1,17 +1,24 @@
 package net.anei.cadpage.parsers.WV;
 
+import java.util.Properties;
+
 import net.anei.cadpage.parsers.dispatch.DispatchA19Parser;
 
 
 
 public class WVJeffersonCountyParser extends DispatchA19Parser {
-  
+
   public WVJeffersonCountyParser() {
-    super("JEFFERSON COUNTY", "WV");
+    super(CITY_CODES, "JEFFERSON COUNTY", "WV");
   }
-  
+
   @Override
   public String getFilter() {
     return "CAD911@jeffersoncountywv.org";
   }
+
+  private static final Properties CITY_CODES = buildCodeTable(new String[] {
+      "HFU",  "Shannondale",
+      "RN",   "Ranson"
+  });
 }
