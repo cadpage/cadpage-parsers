c62fd08959575774f3bd25541ed17011acf7bdfe
Ken Corbin <ken@cadpage.org>
2023-07-31 19:01:44 -0700

    Fixed parsing problem with Chaffee County, CO

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/CO/COChaffeeCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/CO/COChaffeeCountyParser.java
@@ -1,8 +1,8 @@
 package net.anei.cadpage.parsers.CO;
 
-import net.anei.cadpage.parsers.dispatch.DispatchA64Parser;
+import net.anei.cadpage.parsers.dispatch.DispatchA55Parser;
 
-public class COChaffeeCountyParser extends DispatchA64Parser {
+public class COChaffeeCountyParser extends DispatchA55Parser {
 
 
   public COChaffeeCountyParser() {
