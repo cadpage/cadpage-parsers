c17addb223e6f667337d860604707805f5c59c67
Ken Corbin <ken@cadpage.org>
2023-08-09 16:04:47 -0700

    Added GPS lookup table for Johnson County, IA

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/IA/IAJohnsonCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/IA/IAJohnsonCountyParser.java
@@ -1,17 +1,29 @@
 package net.anei.cadpage.parsers.IA;
 
+import java.util.Properties;
+
 import net.anei.cadpage.parsers.dispatch.DispatchA38Parser;
 
 public class IAJohnsonCountyParser extends DispatchA38Parser {
-  
- 
+
+
   public IAJohnsonCountyParser() {
     super("JOHNSON COUNTY", "IA");
+    setupGpsLookupTable(GPS_LOOKUP_TABLE);
   }
-  
+
   @Override
   public String getFilter() {
     return "noreply@jecc-ema.org";
   }
-  
+
+  @Override
+  protected String adjustGpsLookupAddress(String address) {
+    return address.toUpperCase();
+  }
+
+  private static final Properties GPS_LOOKUP_TABLE =  buildCodeTable(new String[] {
+      "750 E FOSTER RD",                      "+41.684935,-91.527715",
+      "2640 N SCOTT BLVD",                    "+41.677864,-91.503297"
+  });
 }
