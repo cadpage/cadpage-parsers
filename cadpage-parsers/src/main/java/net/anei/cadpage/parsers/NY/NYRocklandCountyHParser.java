5a7208f2786b6c5d120cb927a2c2dc12ba67589b
Ken Corbin <ken@cadpage.org>
2023-08-15 10:16:15 -0700

    Fixed parsign problem with Rockland County, NY (H)

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/NY/NYRocklandCountyHParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/NY/NYRocklandCountyHParser.java
@@ -10,7 +10,7 @@ public class NYRocklandCountyHParser extends FieldProgramParser {
 
   public NYRocklandCountyHParser() {
     super("ROCKLAND COUNTY", "NY",
-          "SRC ADDRCITY! CALL_TYPE:CALL? CALLER:PHONE! CAD:ID! UNITS:UNIT? INFO/N+? GPS! END");
+          "SRC ADDRCITY! CROSS_STREET:X NAME:NAME FLOOR:FLR CALL_TYPE:CALL CALLBACK:PHONE CALLER:PHONE! CAD:ID! UNITS:UNIT? INFO/N+? GPS! END");
   }
 
   @Override
@@ -23,14 +23,18 @@ public class NYRocklandCountyHParser extends FieldProgramParser {
     return MAP_FLG_PREFER_GPS;
   }
 
+  private static final Pattern DELIM = Pattern.compile("\n|(?=(?:CROSS STREET|NAME|CALLER):)");
+
   @Override
   protected boolean parseMsg(String body, Data data) {
-    return parseFields(body.split("\n"), data);
+    return parseFields(DELIM.split(body), data);
   }
 
   @Override
   public Field getField(String name) {
     if (name.equals("ADDRCITY")) return new MyAddressCityField();
+    if (name.equals("FLR")) return new MyFloorField();
+    if (name.equals("PHONE")) return new MyPhoneField();
     if (name.equals("GPS")) return new GPSField("http://maps\\.google\\.com/maps\\?daddr=(.*)", true);
     return super.getField(name);
   }
@@ -47,6 +51,22 @@ public class NYRocklandCountyHParser extends FieldProgramParser {
       }
       super.parse(field, data);
       if (zip != null && data.strCity.isEmpty()) data.strCity = zip;
+    }
+  }
+
+  private class MyFloorField extends AptField {
+    @Override
+    public void parse(String field, Data data) {
+      data.strApt = append(data.strApt, " ", getRelativeField(0));
+    }
+  }
+
+  private class MyPhoneField extends PhoneField {
+    @Override
+    public void parse(String field, Data data) {
+      if (field.isEmpty() || field.equals("()-")) return;
+      if (field.equals(data.strPhone)) return;
+      data.strPhone = append(data.strPhone, " / ", field);
     }
   }
 }
