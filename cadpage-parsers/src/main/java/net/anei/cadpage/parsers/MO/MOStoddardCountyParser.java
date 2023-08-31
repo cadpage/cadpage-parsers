65c1ef660856baecdafa064e504cc841a0c579de
Ken Corbin <ken@cadpage.org>
2023-08-31 09:38:17 -0700

    Fixed parsing problem with Stoddard County,  MO

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/MO/MOStoddardCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/MO/MOStoddardCountyParser.java
@@ -4,59 +4,62 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import net.anei.cadpage.parsers.MsgInfo.Data;
-import net.anei.cadpage.parsers.SmartAddressParser;
+import net.anei.cadpage.parsers.MsgParser;
 
-public class MOStoddardCountyParser extends SmartAddressParser {
-  
+public class MOStoddardCountyParser extends MsgParser {
+
   public MOStoddardCountyParser() {
-    super(CITY_LIST, "STODDARD COUNTY", "MO");
-    setFieldList("CALL PRI PLACE ADDR APT CITY X SRC UNIT");
+    super("STODDARD COUNTY", "MO");
+    setFieldList("ADDR APT CITY ST INFO ID NAME PLACE UNIT MAP CALL DATE TIME");
   }
-  
-  private static final Pattern MASTER = Pattern.compile("(.*?) (?:PRIORITY (\\S+)|NON-PRIORITY) +(.*?) CrossStreets: *(.*)\nESN: *(\\S+) *\n(.*)");
-  
+
+  @Override
+  public String getFilter() {
+    return "noreply@stoddems.com";
+  }
+
+  private static final Pattern MASTER =
+      Pattern.compile("(?:([^;,]*(?:, -\\d+\\.\\d+ *)?(?:, APT [A-Z0-9]+ *)?)(?:, ([ A-Z]+))?(?:, ([A-Z]{2})(?: \\d{5})?)? )?([^;]*) ((?:[A-Z0-9]+; )*)((?!911)\\d{3}|[A-Z]FD) (?:(.*) )?Call Time: \\d\\d/\\d\\d/\\d\\d \\d\\d:\\d\\d Assigned (\\d\\d/\\d\\d/\\d\\d) (\\d\\d:\\d\\d:\\d\\d)\\b.*");
+
   @Override
   protected boolean parseMsg(String subject, String body, Data data) {
-    
-    if (!subject.equals("Dispatch Call")) return false;
-    
     Matcher match = MASTER.matcher(body);
     if (!match.matches()) return false;
-    data.strCall = match.group(1).trim();
-    data.strPriority = match.group(2);
-    if (data.strPriority == null) data.strPriority = "NONE";
-    parseAddress(StartType.START_PLACE, FLAG_FALLBACK_ADDR | FLAG_ANCHOR_END, match.group(3).trim(), data);
-    data.strCross = match.group(4).trim();
-    data.strSource = match.group(5);
-    data.strUnit = match.group(6).trim();
+    parseAddress(getOptGroup(getOptGroup(match.group(1))), data);
+    data.strCity = getOptGroup(match.group(2));
+    data.strState = getOptGroup(match.group(3));
+    String info = match.group(4).trim();
+    data.strUnit = match.group(5).replace(";", "").trim();
+    data.strMap = match.group(6);
+    data.strCall = getOptGroup(match.group(7));
+    data.strDate = match.group(8);
+    data.strTime = match.group(9);
+    if (subject.length() > data.strCall.length()) data.strCall = subject;
+
+    if (!info.isEmpty() && !info.equals("None")) {
+      if (info.endsWith(" --")) info += ' ';
+      String[] infoFlds = info.split(" -- ", -1);
+      if (infoFlds.length == 1) {
+         parseInfoFld(info.trim(), data);
+      } else {
+        data.strSupp = infoFlds[0].trim();
+        for (int ndx = 1; ndx < infoFlds.length; ndx++) {
+          parseInfoFld(infoFlds[ndx].trim(), data);
+        }
+      }
+    }
     return true;
   }
-  
-  private static final String[] CITY_LIST = new  String[]{
-    "ADVANCE",
-    "BAKER",
-    "BELL CITY",
-    "BERNIE",
-    "BLOOMFIELD",
-    "BROWNWOOD",
-    "CASTOR",
-    "DEXTER",
-    "DUCK CREEK",
-    "DUDLEY",
-    "ELK",
-    "ESSEX",
-    "GRAYRIDGE",
-    "GUAM",
-    "IDALIA",
-    "LEORA",
-    "LIBERTY",
-    "NEW LISBON",
-    "PAINTON",
-    "PENERMON",
-    "PIKE",
-    "POWE",
-    "PUXICO",
-    "RICHLAND",
-    "STODDARD COUNTY"
-  };
+
+  private void parseInfoFld(String info, Data data) {
+    if (info.isEmpty()) return;
+    if (info.startsWith("#")) {
+      data.strCallId = info.substring(1).trim();
+    } else if (info.contains("/")) {
+      data.strName = info;
+    } else {
+      data.strPlace = info;
+    }
+
+  }
 }
