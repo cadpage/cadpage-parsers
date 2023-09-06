975ddcbbd960fb0aee02cdffc15efb2c9dd7a7e5
Ken Corbin <ken@cadpage.org>
2023-09-05 20:29:04 -0700

    Fixed parsing problem with Fulton COunty, PA

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/PA/PAFultonCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/PA/PAFultonCountyParser.java
@@ -1,61 +1,78 @@
 package net.anei.cadpage.parsers.PA;
 
 import java.util.Properties;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
+import net.anei.cadpage.parsers.FieldProgramParser;
 import net.anei.cadpage.parsers.MsgInfo.Data;
-import net.anei.cadpage.parsers.dispatch.DispatchArchonixParser;
 
 
 
-public class PAFultonCountyParser extends DispatchArchonixParser {
+public class PAFultonCountyParser extends FieldProgramParser {
 
   public PAFultonCountyParser() {
-    super(CITY_CODES, MA_CITY_CODES, "FULTON COUNTY", "PA", 0);
+    super("FULTON COUNTY", "PA",
+          "Units:UNIT! UNIT? CALL! Location:EMPTY! ADDRCITY! Narrative:INFO! INFO/N+");
   }
 
   @Override
   public String getFilter() {
-    return "EP911@ccpa.net";
+    return "alerts@centre.ealert911.com";
+  }
+
+  @Override
+  public int getMapFlags() {
+    return MAP_FLG_PREFER_GPS;
   }
 
   @Override
   public boolean parseMsg(String subject, String body, Data data) {
-    if (!super.parseMsg(subject, body, data)) return false;
-    if (data.strCity.equals("WASHINGTON COUNTY")) data.strState = "PA";
-    return true;
+    if (!subject.equals(".")) return false;
+    return parseFields(body.split("\n"), data);
   }
 
   @Override
-  public String getProgram() {
-    return super.getProgram().replace("CITY", "CITY ST");
+  public Field getField(String name) {
+    if (name.equals("UNIT")) return new MyUnitField();
+    if (name.equals("ADDRCITY")) return new MyAddressCityField();
+    return super.getField(name);
   }
 
-  private static final Properties CITY_CODES = buildCodeTable(new String[]{
+  private class MyUnitField extends UnitField {
+    @Override
+    public boolean canFail() {
+      return true;
+    }
 
-      "AR", "AYR TWP",
-      "BC", "BRUSH CREEK TWP",
-      "BL", "BELFAST TWP",
-      "BT", "BETHEL TWP",
-      "DB", "DUBLIN TWP",
-      "LC", "LICKING CREEK TWP",
-      "MC", "MCCONNELLSBURG",
-      "TD", "TODD TWP",
-      "TH", "THOMPSON TWP",
-      "TY", "TAYLOR TWP",
-      "UN", "UNION TWP",
-      "VH", "VALLEY-HI",
-      "WL", "WELLS TWP",
+    @Override
+    public boolean checkParse(String field, Data data) {
+      if (!data.strUnit.isEmpty()) return false;
+      data.strUnit = field;
+      return true;
+    }
 
-      "CBG", "CHAMBERSBURG",
-      "FC",  "FRANKLIN COUNTY",
-      "HC",  "HUNTINGDON COUNTY",
-      "WC",  "WASHINGTON COUNTY"
-  });
+    @Override
+    public void parse(String field, Data data) {
+      if (!checkParse(field, data)) abort();
+    }
+  }
 
-  private static final Properties MA_CITY_CODES = buildCodeTable(new String[] {
-      "CBG", "CHAMBERSBURG",
-      "FC",  "FRANKLIN COUNTY",
-      "HC",  "HUNTINGDON COUNTY",
-      "WC",  "WASHINGTON COUNTY"
-  });
+  private static final Pattern ADDR_GPS_X_PTN = Pattern.compile("(.*?) ([-+]?\\d{2,3}\\.\\d{6,} +[-+]?\\d{2,3}\\.\\d{6,}|-361 +-361) *(.*)");
+  private class MyAddressCityField extends AddressCityField {
+    @Override
+    public void parse(String field, Data data) {
+      Matcher match = ADDR_GPS_X_PTN.matcher(field);
+      if (!match.matches()) abort();
+      super.parse(match.group(1).trim(), data);
+      data.strCity = stripFieldEnd(data.strCity, " BORO");
+      setGPSLoc(match.group(2), data);
+      data.strCross = match.group(3);
+    }
+
+    @Override
+    public String getFieldNames() {
+      return "ADDR APT CITY GPS X";
+    }
+  }
 }
