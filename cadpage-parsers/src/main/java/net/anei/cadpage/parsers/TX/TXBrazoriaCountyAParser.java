9ad987bb5c4df5c6d94612aa951c7346c8f5058f
Ken Corbin <ken@cadpage.org>
2023-09-05 20:44:35 -0700

    Fixed parsing problem with Brazoria County, TX (A)

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/TX/TXBrazoriaCountyAParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/TX/TXBrazoriaCountyAParser.java
@@ -1,29 +1,8 @@
 package net.anei.cadpage.parsers.TX;
 
-import java.util.Properties;
-import java.util.regex.Pattern;
+public class TXBrazoriaCountyAParser extends TXCyCreekCommCenterBParser {
 
-import net.anei.cadpage.parsers.dispatch.DispatchA22Parser;
-
-
-public class TXBrazoriaCountyAParser extends DispatchA22Parser {
-  
   public TXBrazoriaCountyAParser() {
-    super(CITY_CODES, "BRAZORIA COUNTY", "TX");
+    super("BRAZORIA COUNTY", "TX");
   }
-  
-  @Override
-  public String getFilter() {
-    return "page@manvelpd.org";
-  }
-  
-  @Override
-  public String adjustMapAddress(String addr) {
-    return BACKWARD_HWY_PTN.matcher(addr).replaceAll("TX $1");
-  }
-  private static final Pattern BACKWARD_HWY_PTN = Pattern.compile("\\b(\\d+) *SH", Pattern.CASE_INSENSITIVE);
-  
-  private static final Properties CITY_CODES = buildCodeTable(new String[]{
-      "BC",  "BRAZORIA COUNTY"
-  });
 }
