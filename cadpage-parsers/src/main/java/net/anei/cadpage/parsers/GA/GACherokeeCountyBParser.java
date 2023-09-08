613286f6fe7bf7ef1631437275845c415186540a
Ken Corbin <ken@cadpage.org>
2023-09-08 16:11:16 -0700

    Added city code table for Cherokee County, GA

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/GA/GACherokeeCountyBParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/GA/GACherokeeCountyBParser.java
@@ -1,15 +1,28 @@
 package net.anei.cadpage.parsers.GA;
 
+import java.util.Properties;
+
 import net.anei.cadpage.parsers.dispatch.DispatchH03Parser;
 
 public class GACherokeeCountyBParser extends DispatchH03Parser {
 
   public GACherokeeCountyBParser() {
-    super("CHEROKEE COUNTY", "GA");
+    super(CITY_CODES, "CHEROKEE COUNTY", "GA");
   }
 
   @Override
   public String getFilter() {
     return ".911@cherokeega.com";
   }
+
+  private static final Properties CITY_CODES = buildCodeTable(new String[] {
+      "AC", "ACWORTH",
+      "BG", "BALL GROUND",
+      "CA", "CANTON",
+      "CH", "CHEROKEE COUNTY",
+      "RO", "ROSWELL",
+      "WA", "WALESKA",
+      "WH", "WHITE",
+      "WO", "WOODSTOCK"
+  });
 }
