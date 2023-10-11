b871f1490e40b6d3d5d0afffd576803c3a957b5e
Ken Corbin <ken@cadpage.org>
2023-10-11 16:00:27 -0700

    Fixed parsing problem with Bergen County,NJ (F)

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/NJ/NJBergenCountyFParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/NJ/NJBergenCountyFParser.java
@@ -1,15 +1,57 @@
 package net.anei.cadpage.parsers.NJ;
 
-import net.anei.cadpage.parsers.dispatch.DispatchA27Parser;
+import java.text.SimpleDateFormat;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-public class NJBergenCountyFParser extends DispatchA27Parser {
-  
+import net.anei.cadpage.parsers.FieldProgramParser;
+import net.anei.cadpage.parsers.MsgInfo.Data;
+
+public class NJBergenCountyFParser extends FieldProgramParser {
+
   public NJBergenCountyFParser() {
-    super("BERGEN COUNTY", "NJ", "[A-Z]{1,6}|[A-Za-z]+\\d+");
+    super("BERGEN COUNTY", "NJ",
+          "CAD_No:ID! Time_of_Call:DATETIME! Location:ADDRCITY! Incident_Type:CALL! Notes:INFO INFO/N+");
   }
-  
+
   @Override
   public String getFilter() {
-    return "noreply@cisusa.org,donotreply@lawsoft-inc.com";
+    return "donotreply@lawsoftweb.onmicrosoft.com";
+  }
+
+  @Override
+  protected boolean parseMsg(String body, Data data) {
+    body = body.replace("EMS::Time of Call:", "Time of Call:");
+    return parseFields(body.split("\n"), data);
+  }
+
+  @Override
+  public Field getField(String name) {
+    if (name.equals("DATETIME")) return new MyDateTimeField();
+    if (name.equals("ADDRCITY")) return new MyAddressCityField();
+    return super.getField(name);
+  }
+
+  private static final Pattern DATE_TIME_PTN = Pattern.compile("(\\d\\d?/\\d\\d?/\\d{4}) (\\d\\d?:\\d\\d(?::\\d\\d)? [AP]M)");
+  private static final SimpleDateFormat TIME_SEC_FMT = new SimpleDateFormat("hh:mm:ss aa");
+  private static final SimpleDateFormat TIME_NOSEC_FMT = new SimpleDateFormat("hh:mm aa");
+  private class MyDateTimeField extends DateTimeField {
+    @Override
+    public void parse(String field, Data data) {
+      Matcher match = DATE_TIME_PTN.matcher(field);
+      if (!match.matches()) abort();
+      data.strDate = match.group(1);
+      String time = match.group(2);
+      SimpleDateFormat fmt = time.length() > 8 ? TIME_SEC_FMT : TIME_NOSEC_FMT;
+      setTime(fmt, time, data);
+    }
+  }
+
+  private class MyAddressCityField extends AddressCityField {
+    @Override
+    public void parse(String field, Data data) {
+      super.parse(field, data);
+      data.strCity = stripFieldEnd(data.strCity, " Boro");
+    }
   }
 }
