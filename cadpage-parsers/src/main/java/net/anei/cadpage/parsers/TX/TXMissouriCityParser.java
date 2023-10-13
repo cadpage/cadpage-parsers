06f95da24c495476bbe31f0f474c06a987ba58a2
Ken Corbin <ken@cadpage.org>
2023-10-13 15:00:10 -0700

    Fixed parsing problem with Missouri City, TX

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/TX/TXMissouriCityParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/TX/TXMissouriCityParser.java
@@ -1,7 +1,6 @@
 package net.anei.cadpage.parsers.TX;
 
-import net.anei.cadpage.parsers.SplitMsgOptions;
-import net.anei.cadpage.parsers.SplitMsgOptionsCustom;
+import net.anei.cadpage.parsers.MsgInfo.Data;
 import net.anei.cadpage.parsers.dispatch.DispatchOSSIParser;
 
 /**
@@ -10,23 +9,23 @@ import net.anei.cadpage.parsers.dispatch.DispatchOSSIParser;
 public class TXMissouriCityParser extends DispatchOSSIParser {
   public TXMissouriCityParser() {
     super("MISSOURI CITY", "TX",
-        "FYI? CALL ADDR UNIT? INFO! INFO/N+");
+        "FYI? CALL ADDR UNIT? INFO/N+? ID END");
   }
-  
-  
-  @Override
-  public SplitMsgOptions getActive911SplitMsgOptions() {
-    return new SplitMsgOptionsCustom();
-  }
-  
-  @Override
-  public Field getField(String name) {
-    if (name.equals("UNIT")) return new UnitField("[A-Z]+\\d+|\\S+,\\S+", true);
-    return super.getField(name);
-  }
-
 
   public String getFilter() {
     return "CAD@missouricitytx.gov";
+  }
+
+  @Override
+  protected boolean parseMsg(String body, Data data) {
+    if (!body.startsWith("CAD:")) body = "CAD:"+ body;
+    return super.parseMsg(body, data);
+  }
+
+  @Override
+  public Field getField(String name) {
+    if (name.equals("UNIT")) return new UnitField("[A-Z]+\\d+|\\S+,\\S+", true);
+    if (name.equals("ID")) return new IdField("\\d{13}", true);
+    return super.getField(name);
   }
 }
