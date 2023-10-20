b2ccd8665ccd005afb654ce4fbafbd0786a4c19a
Ken Corbin <ken@cadpage.org>
2023-10-20 14:08:09 -0700

    Fixed parsing problem with Moniteau County, MO

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/MO/MOMoniteauCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/MO/MOMoniteauCountyParser.java
@@ -1,5 +1,8 @@
 package net.anei.cadpage.parsers.MO;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import net.anei.cadpage.parsers.HtmlProgramParser;
 import net.anei.cadpage.parsers.MsgInfo.Data;
 
@@ -13,7 +16,7 @@ public class MOMoniteauCountyParser extends HtmlProgramParser {
   public MOMoniteauCountyParser(String defCity, String defState) {
     super(defCity, defState,
           "( SELECT/HTML Call:EMPTY! CALL! Sub:EMPTY! CALL/S Place:EMPTY! PLACE Address:EMPTY! ADDR City:EMPTY! CITY Apt:EMPTY! APT Cross_Streets:EMPTY! X Event#:EMPTY! ID! Reporting_Person:EMPTY! NAME Phone#:EMPTY! PHONE Initiated:EMPTY! SKIP! INFO/N+ Units%EMPTY! UNIT/C+?  COPYRIGHT " +
-          "| Call:CALL! Sub:CALL/S! Place:PLACE! Address:ADDR! ( City:CITY! Apt:APT! | Apt:APT! City:CITY! State:ST! Zip:ZIP! ) Cross_Streets:X! Event#:ID! Reporting_Person:NAME! Phone#:PHONE! ( Unit:UNIT! Initiated:SKIP! Notes:INFO! INFO/N+ | Initiated:SKIP! INFO/N+ Units%EMPTY! UNIT/C+ ) " +
+          "| Call:CALL! Sub:CALL/S! Place:PLACE! Address:ADDR! ( City:CITY! Apt:APT! | Apt:APT! City:CITY! State:ST! Zip:ZIP! ) Cross_Streets:X! Map_Coords:GPS? Event#:ID! Reporting_Person:NAME! Phone#:PHONE! ( Unit:UNIT! Initiated:SKIP! Notes:INFO! INFO/N+ | Initiated:SKIP! INFO/N+ Units%EMPTY! UNIT/C+ ) " +
           "| ADDR! Apt:APT! CITY! Cross_Streets:X! Event#:ID! Reporting_Person:NAME! Phone#:PHONE! INFO/N+ Units%EMPTY! UNIT/C+ )");
   }
 
@@ -49,6 +52,7 @@ public class MOMoniteauCountyParser extends HtmlProgramParser {
   public Field getField(String name) {
     if (name.equals("INFO")) return new MyInfoField();
     if (name.equals("COPYRIGHT")) return new SkipField("©.*", true);
+    if (name.equals("GPS")) return new MyGPSField();
     return super.getField(name);
   }
 
@@ -57,6 +61,17 @@ public class MOMoniteauCountyParser extends HtmlProgramParser {
     public void parse(String field, Data data) {
       if (field.startsWith("Call Received on ")) return;
       super.parse(field, data);
+    }
+  }
+
+  private static final Pattern GPS_PTN = Pattern.compile(".*\"([-+]?\\d{2,3}\\.\\d{3,})\".*\"([-+]?\\d{2,3}\\.\\d{3,})\".*");
+  private class MyGPSField extends GPSField {
+    @Override
+    public void parse(String field, Data data) {
+      Matcher match = GPS_PTN.matcher(field);
+      if (match.matches()) {
+        setGPSLoc(match.group(1)+','+match.group(2), data);
+      }
     }
   }
 }
