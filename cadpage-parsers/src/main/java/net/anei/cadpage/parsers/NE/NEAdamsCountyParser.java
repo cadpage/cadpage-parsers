1245f145c7a322f6b57efd2993946dc8789df61a
Ken Corbin <ken@cadpage.org>
2023-11-03 12:24:48 -0700

    Fixed parsing problem with Adams County, NE

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/NE/NEAdamsCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/NE/NEAdamsCountyParser.java
@@ -18,6 +18,8 @@ public class NEAdamsCountyParser extends DispatchA38Parser {
   protected boolean parseMsg(String body, Data data) {
     int pt = body.indexOf("\nThe information contained ");
     if (pt >= 0) body = body.substring(0,pt).trim();
-    return super.parseMsg(body, data);
+    if (!super.parseMsg(body, data)) return false;
+    if (data.strState.equals("NB")) data.strState = "NE";
+    return true;
   }
 }
