cd322cfce25809128d9d6d5b1b53ec162cc47b9f
Ken Corbin <ken@cadpage.org>
2023-11-10 09:38:49 -0800

    Fixed parsing problem with Washington City, MO

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/MO/MOWashingtonCityParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/MO/MOWashingtonCityParser.java
@@ -1,5 +1,8 @@
 package net.anei.cadpage.parsers.MO;
 
+import java.util.regex.Pattern;
+
+import net.anei.cadpage.parsers.MsgInfo.Data;
 import net.anei.cadpage.parsers.dispatch.DispatchA33Parser;
 
 public class MOWashingtonCityParser extends DispatchA33Parser {
@@ -15,7 +18,15 @@ public class MOWashingtonCityParser extends DispatchA33Parser {
 
   @Override
   public String getFilter() {
-    return "DISPATCH@OMNIGO.COM,DISPATCH@CI.WASHINGTON.MO";
+    return "DISPATCH@OMNIGO.COM,DISPATCH@CI.WASHINGTON.MO,noreply@omnigo.com";
+  }
+
+  private static Pattern ST_EXT_PTN = Pattern.compile("\\b(MO)\\d\\b");
+
+  @Override
+  protected boolean parseMsg(String body, Data data) {
+    body = ST_EXT_PTN.matcher(body).replaceAll("$1");
+    return super.parseMsg(body, data);
   }
 
 }
