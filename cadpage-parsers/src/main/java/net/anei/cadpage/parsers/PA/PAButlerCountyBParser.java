d7ad30148a20b6098d7e100ad7f78af9734b6b81
Ken Corbin <ken@cadpage.org>
2023-11-17 10:48:36 -0800

    Fixed mapping problem with Butler County, PA

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/PA/PAButlerCountyBParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/PA/PAButlerCountyBParser.java
@@ -7,30 +7,30 @@ import net.anei.cadpage.parsers.FieldProgramParser;
 import net.anei.cadpage.parsers.MsgInfo.Data;
 
 public class PAButlerCountyBParser extends FieldProgramParser {
-  
+
   public PAButlerCountyBParser() {
-    super("BUTLER COUNTY", "PA", 
+    super("BUTLER COUNTY", "PA",
 
         "DASHES! INCIDENT:EMPTY! Inc_Type:CALL! Mod_Circum:CALL/SDS! Priority:PRI! Area:MAP! County:CITY! LOCATION:EMPTY! Loc_Name:PLACE! Loc_Descr:PLACE/SDS! Location:ADDR! Municipality:CITY! Building:APT! Floor:APT! Apt/Unit:APT! Cross_Strs:X! PREMISE_HAZARD:HAZARD! COMMENTS:INFO! INFO/N+? UNITS_DISPATCHED:UNIT! UNIT/S+? Caller:NAME! Phone:PHONE! Created:SKIP! Inc_#:ID! END");
   }
-  
+
   @Override
   public String getFilter() {
     return "10C@RCAD911.com";
   }
-  
+
   @Override
   protected boolean parseMsg(String subject, String body, Data data) {
     if (!subject.startsWith("Dispatch Notification for Incident ")) return false;
     data.strCallId = subject.substring(35).trim();
     return parseFields(body.split("\n"), data);
   }
-  
+
   @Override
   public String getProgram() {
     return "ID " + super.getProgram();
   }
-  
+
   @Override
   public Field getField(String name) {
     if (name.equals("DASHES")) return new SkipField("-{20,}", true);
@@ -40,7 +40,7 @@ public class PAButlerCountyBParser extends FieldProgramParser {
     if (name.equals("HAZARD")) return new MyHazardField();
     return super.getField(name);
   }
-  
+
   private static final Pattern PLACE_GPS_PTN = Pattern.compile("LAT: <([-+]?[.0-9]+)>  LONG: <([-+]?[.0-9]+)>");
   private class MyPlaceField extends PlaceField {
     @Override
@@ -52,13 +52,13 @@ public class PAButlerCountyBParser extends FieldProgramParser {
         super.parse(field, data);
       }
     }
-    
+
     @Override
     public String getFieldNames() {
       return "GPS " + super.getFieldNames();
     }
   }
-  
+
   private class MyCityField extends CityField {
     @Override
     public void parse(String field, Data data) {
@@ -67,7 +67,7 @@ public class PAButlerCountyBParser extends FieldProgramParser {
       super.parse(field, data);
     }
   }
-  
+
   private class MyHazardField extends InfoField {
     @Override
     public void parse(String field, Data data) {
@@ -75,7 +75,7 @@ public class PAButlerCountyBParser extends FieldProgramParser {
       super.parse("Hazard: " + field, data);
     }
   }
-  
+
   private class MyCrossField extends CrossField {
     @Override
     public void parse(String field, Data data) {
@@ -84,14 +84,16 @@ public class PAButlerCountyBParser extends FieldProgramParser {
       super.parse(field, data);
     }
   }
-  
+
   private static final Pattern STREET_NO_PTN = Pattern.compile("^\\d+ ++(?!(?:AVE|RD|ST)\\b)");
-  
+  private static final Pattern HICKORY_KNLS_PTN = Pattern.compile("\\bHICKORY KNLS\\b", Pattern.CASE_INSENSITIVE);
+
   @Override
   public String adjustMapAddress(String addr, boolean cross) {
     if (cross) {
       addr = STREET_NO_PTN.matcher(addr).replaceFirst("");
     }
+    addr = HICKORY_KNLS_PTN.matcher(addr).replaceAll("HICKORY KNOLLS LN");
     return addr;
   }
 }
