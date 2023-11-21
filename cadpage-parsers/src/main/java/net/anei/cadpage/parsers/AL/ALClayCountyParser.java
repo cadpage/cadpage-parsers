47fd705263787a87b33727aa38567fbfdd99463a
Ken Corbin <ken@cadpage.org>
2023-11-20 17:10:17 -0800

    Fixed parasing problem with Clay COunty, AL

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/AL/ALClayCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/AL/ALClayCountyParser.java
@@ -1,36 +1,21 @@
 package net.anei.cadpage.parsers.AL;
 
-import net.anei.cadpage.parsers.FieldProgramParser;
-import net.anei.cadpage.parsers.MsgInfo.Data;
+import net.anei.cadpage.parsers.dispatch.DispatchA71Parser;
 
-public class ALClayCountyParser extends FieldProgramParser {
+public class ALClayCountyParser extends DispatchA71Parser {
 
   public ALClayCountyParser() {
-    super("CLAY COUNTY", "AL",
-          "CALL ADDR/iSXP INFO! INFO/N+? GPS END");
+    super("CLAY COUNTY", "AL");
   }
 
   @Override
   public String getFilter() {
     return "claye911@gmail.com";
   }
-
+  
   @Override
-  protected boolean parseMsg(String subject, String body, Data data) {
-    if (!subject.equals("CLAY E911")) return false;
-
-    body = body.replace('\n', ' ');
-    if (body.startsWith("// ")) body = ' ' + body;
-    if (!parseFields(body.split(" /{2,}"), data)) return false;
-    if (data.strCall.isEmpty()) {
-      data.strCall = data.strSupp;
-      data.strSupp = "";
-    }
-    if (data.strPlace.startsWith("MM ")) {
-      data.strAddress = append(data.strAddress, " ", data.strPlace);
-      data.strPlace = "";
-    }
-    return true;
+  public int getMapFlags() {
+    return MAP_FLG_PREFER_GPS;
   }
 
 }
