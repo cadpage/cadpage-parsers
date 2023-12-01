e5e03bfaf11a5ea12f699ae0489efef20b710332
Ken Corbin <ken@cadpage.org>
2023-12-01 10:26:33 -0800

    New Location: Lincoln County, WV (B)

---
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/WV/WVLincolnCountyBParser.java
@@ -0,0 +1,77 @@
+package net.anei.cadpage.parsers.WV;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import net.anei.cadpage.parsers.FieldProgramParser;
+import net.anei.cadpage.parsers.MsgInfo.Data;
+import net.anei.cadpage.parsers.MsgInfo.MsgType;
+
+public class WVLincolnCountyBParser extends FieldProgramParser {
+
+  public WVLincolnCountyBParser() {
+    super("LINCOLN COUNTY", "WV",
+          "CALL ADDR_X ( SELECT/1 NAME PHONE ID! | CITY NAME_PHONE EMPTY ID EMPTY UNIT_TIMES:INFO/R! ) INFO/RN+");
+  }
+
+  @Override
+  public String getFilter() {
+    return "lincowv911@e911.org";
+  }
+
+  @Override
+  protected boolean parseMsg(String body, Data data) {
+    if (body.startsWith("Lincoln 911:")) {
+      setSelectValue("2");
+      data.msgType = MsgType.RUN_REPORT;
+      body = body.substring(12).trim();
+    } else {
+      setSelectValue("1");
+    }
+    return parseFields(body.split("\n"), data);
+  }
+
+  @Override
+  public Field getField(String name) {
+    if (name.equals("ADDR_X")) return new MyAddressCrossField();
+    if (name.equals("NAME_PHONE")) return new MyNamePhoneField();
+    return super.getField(name);
+  }
+
+  private static final Pattern ADDR_X_PTN = Pattern.compile("(.*)(?: {2,}| +XS: *)(.*)");
+
+  private class MyAddressCrossField extends AddressField {
+    @Override
+    public void parse(String field, Data data) {
+      Matcher match = ADDR_X_PTN.matcher(field);
+      if (match.matches()) {
+        field = match.group(1);
+        data.strCross = match.group(2);
+      }
+      super.parse(field, data);
+    }
+    
+    @Override
+    public String getFieldNames() {
+      return super.getFieldNames() + " X";
+    }
+  }
+
+  private class MyNamePhoneField extends Field {
+
+    @Override
+    public void parse(String field, Data data) {
+      int pt = field.indexOf(" (");
+      if (pt >= 0) {
+        data.strPhone = field.substring(pt+1);
+        field = field.substring(0, pt).trim();
+      }
+      data.strName = cleanWirelessCarrier(field);
+    }
+
+    @Override
+    public String getFieldNames() {
+      return "NAME PHONE";
+    }
+  }
+}
--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/WV/WVLincolnCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/WV/WVLincolnCountyParser.java
@@ -1,62 +1,10 @@
 package net.anei.cadpage.parsers.WV;
 
-import java.util.regex.Pattern;
+import net.anei.cadpage.parsers.GroupBestParser;
 
-import net.anei.cadpage.parsers.FieldProgramParser;
-import net.anei.cadpage.parsers.MsgInfo.Data;
-
-
-public class WVLincolnCountyParser extends FieldProgramParser {
+public class WVLincolnCountyParser extends GroupBestParser {
 
   public WVLincolnCountyParser() {
-    super("LINCOLN COUNTY", "WV",
-          "Unit:UNIT! Incident:ID! Location:ADDR Place:PLACE! Apt/Lot:APT! Run_Zone:MAP! City:CITY! Cross_Street_1:X! Cross_Street_2:X! Complaint:CALL! Map:GPS!");
-  }
-
-  @Override
-  public String getFilter() {
-    return "lincowv911@e911.org";
-  }
-
-  @Override
-  public int getMapFlags() {
-    return MAP_FLG_PREFER_GPS;
-  }
-
-  @Override
-  protected boolean parseMsg(String subject, String body, Data data) {
-    if (!subject.equals("Dispatched Unit Email Notification")) return false;
-    return parseFields(body.split("\n"), data);
-  }
-
-  @Override
-  public Field getField(String name) {
-    if (name.equals("ADDR")) return new MyAddressField();
-    if (name.equals("PLACE")) return new MyPlaceField();
-    if (name.equals("GPS")) return new GPSField("https://maps.google.com/\\?q=(-\\d+\\.\\d+,\\d+\\.\\d+)", false);
-    return super.getField(name);
-  }
-
-  private static final Pattern MSPACE_PTN = Pattern.compile(" {2,}");
-  private class MyAddressField extends AddressField {
-    @Override
-    public void parse(String field, Data data) {
-      field = MSPACE_PTN.matcher(field).replaceAll(" ");
-      super.parse(field, data);
-    }
-  }
-
-  private class MyPlaceField extends PlaceField {
-    @Override
-    public void parse(String field, Data data) {
-      if (field.equals(data.strAddress)) return;
-      super.parse(field, data);
-    }
-  }
-
-  @Override
-  public String adjustMapCity(String city) {
-    if (city.equalsIgnoreCase("FEZ")) city = "BRANCHLAND";
-    return city;
+    super(new WVLincolnCountyAParser(), new WVLincolnCountyBParser());
   }
 }
