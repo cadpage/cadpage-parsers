13345cdf1cc24610608cbd8955cffbc25e79cfb8
Ken Corbin <ken@cadpage.org>
2023-11-30 17:38:09 -0800

    Fixed parsing problem with Lincoln County, WV

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/WV/WVLincolnCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/WV/WVLincolnCountyParser.java
@@ -2,199 +2,61 @@ package net.anei.cadpage.parsers.WV;
 
 import java.util.regex.Pattern;
 
-import net.anei.cadpage.parsers.CodeSet;
+import net.anei.cadpage.parsers.FieldProgramParser;
 import net.anei.cadpage.parsers.MsgInfo.Data;
-import net.anei.cadpage.parsers.dispatch.DispatchB2Parser;
 
 
+public class WVLincolnCountyParser extends FieldProgramParser {
 
-public class WVLincolnCountyParser extends DispatchB2Parser {
-  
   public WVLincolnCountyParser() {
-    super("LINCOLN 911:", CITY_LIST, "LINCOLN COUNTY", "WV", B2_OPT_CALL_CODE);
-    setupMultiWordStreets(
-        "A PARK",
-        "BALL PARK",
-        "BIG LAUREL",
-        "BLUE BIRD",
-        "CHARLIE NELSON",
-        "COAL RIVER",
-        "DR STEELE FARM",
-        "EAST LYNN",
-        "ELLIS FARM",
-        "END OF MONDAY",
-        "FOUR MILE",
-        "HILL VIEW",
-        "INDIAN VALLEY",
-        "JOSH BRANCH CEMETARY",
-        "LADY BUG",
-        "LEES LAKE",
-        "LINCOLN PANTHER",
-        "LITTLE COAL RIVER",
-        "LITTLE LAUREL",
-        "LOW GAP",
-        "LOWER MUD RIVER",
-        "MARY JANE",
-        "MIDWAY SCHOOL",
-        "MUD RIVER",
-        "NINE MILE",
-        "ONE MILE",
-        "PIN OAK",
-        "ROBERT C BYRD",
-        "SAND LICK", 
-        "SILVER MINE",
-        "SIX MILE",
-        "STONES TRAILER",
-        "SUGAR TREE",
-        "TEN MILE",
-        "TIMBER WOLF",
-        "TWO MILE",
-        "UPPER MUD RIVER",
-        "WEST PARK"
-    );
-    setupCallList(CALL_LIST);
-    setupDoctorNames("STEELE");
+    super("LINCOLN COUNTY", "WV",
+          "Unit:UNIT! Incident:ID! Location:ADDR Place:PLACE! Apt/Lot:APT! Run_Zone:MAP! City:CITY! Cross_Street_1:X! Cross_Street_2:X! Complaint:CALL! Map:GPS!");
   }
-  
+
   @Override
   public String getFilter() {
-    return "LINCOLN911@e911.org";
+    return "lincowv911@e911.org";
   }
-  
+
   @Override
-  public String adjustMapAddress(String addr) {
-    addr = STRAIGHT_FRK_PTN.matcher(addr).replaceAll("STRAIGHT FORK RD");
-    addr = SLASH_BR_BELLOMY_RD_PTN.matcher(addr).replaceAll("BELLOMY RD");
-    return addr;
+  public int getMapFlags() {
+    return MAP_FLG_PREFER_GPS;
   }
-  private static final Pattern STRAIGHT_FRK_PTN = Pattern.compile("\\bSTRAIGHT +FO?RK\\b", Pattern.CASE_INSENSITIVE);
-  private static final Pattern SLASH_BR_BELLOMY_RD_PTN = Pattern.compile("\\bSLASH BR BELLOMY RD\\b", Pattern.CASE_INSENSITIVE);
-  
-  
+
   @Override
-  protected boolean parseMsg(String body, Data data) {
-    if (!super.parseMsg(body, data)) return false;
-    data.strCross = TO_PTN.matcher(data.strCross).replaceAll(" / ");
-    return true;
+  protected boolean parseMsg(String subject, String body, Data data) {
+    if (!subject.equals("Dispatched Unit Email Notification")) return false;
+    return parseFields(body.split("\n"), data);
   }
-  private static final Pattern TO_PTN = Pattern.compile(" +TO +", Pattern.CASE_INSENSITIVE);
-  
+
+  @Override
+  public Field getField(String name) {
+    if (name.equals("ADDR")) return new MyAddressField();
+    if (name.equals("PLACE")) return new MyPlaceField();
+    if (name.equals("GPS")) return new GPSField("https://maps.google.com/\\?q=(-\\d+\\.\\d+,\\d+\\.\\d+)", false);
+    return super.getField(name);
+  }
+
+  private static final Pattern MSPACE_PTN = Pattern.compile(" {2,}");
+  private class MyAddressField extends AddressField {
+    @Override
+    public void parse(String field, Data data) {
+      field = MSPACE_PTN.matcher(field).replaceAll(" ");
+      super.parse(field, data);
+    }
+  }
+
+  private class MyPlaceField extends PlaceField {
+    @Override
+    public void parse(String field, Data data) {
+      if (field.equals(data.strAddress)) return;
+      super.parse(field, data);
+    }
+  }
+
   @Override
   public String adjustMapCity(String city) {
     if (city.equalsIgnoreCase("FEZ")) city = "BRANCHLAND";
     return city;
   }
-  
-  private static CodeSet CALL_LIST = new CodeSet(
-      "ABDOMINAL PAIN",
-      "ALLERGIC REACTION",
-      "ANIMAL COMPLAINT",
-      "AUTO ACCIDENT NO INJURIES",
-      "AUTO ACCIDENT UNKNOWN INJURIES",
-      "AUTO ACCIDENT W/ENTRAPMENT",
-      "AUTO ACCIDENT WITH INJURIES",
-      "BACK PAIN",
-      "BOX ALARM",
-      "BLEEDING NON TRAUMA",
-      "BREATHING DIFFICULTY",
-      "BRUSH FIRE",
-      "CARDIAC ARREST",
-      "CHEST PAINS",
-      "CHOKING",
-      "CODE 3 CHEST PAIN",
-      "CODE 3 DIFFICULTY BREATHING",
-      "CODE 3 SEIZURE",
-      "CODE 3 SYNCOPE/FAINTING",
-      "CODE 3 TRAUMA",
-      "CODE3 UNCONSCIOUS/UNRESPONSIVE",
-      "DISTURBANCE",
-      "FIGHT",
-      "FIRE INVESTIGATION",
-      "FLASH FLOOD WARNING",
-      "FLOOD WARNING",
-      "GAS LEAK",
-      "GUNSHOTS",
-      "HOUSE FIRE",
-      "LANDING ZONE SECURED",
-      "LINE DOWN",
-      "LOCK OUT",
-      "LOCK OUT WITH ENTRAPMENT",
-      "MENTAL PATIENT",
-      "MUTUAL AID TO OTHER CNTY/DEPT",
-      "OTHER FIRE CALL",
-      "OTHER POLICE CALL",
-      "OVERDOSE",
-      "PEDESTRIAN HIT",
-      "PUBLIC ASSISTANCE NON EMERGENC",
-      "RELEASED FROM",
-      "SEIZURES",
-      "SHOOTING",
-      "SICK UNKNOWN",
-      "SMOKE INVESTIGATION",
-      "STORM DAMAGE",
-      "STROKE",
-      "STRUCTURE FIRE",
-      "SUICIDE ATTEMPT",
-      "TEST",
-      "TRAFFIC PROBLEM",
-      "TRAILER FIRE",
-      "TRASH FIRE",
-      "TRAUMA",
-      "TREE DOWN",
-      "VEHICLE FIRE",
-      "WATER RESCUE"
-  );
-  
-  private static final String[] CITY_LIST = new String[]{
-    
-    // Incorporated cities and towns
-
-    "HAMLIN",
-    "WEST HAMLIN",
-
-    // Unincorporated communities
-
-    "ALKOL",
-    "ALUM CREEK",
-    "ATENVILLE",
-    "BIG UGLY",
-    "BRANCHLAND",
-    "DOLLIE",
-    "EDEN PARK",
-    "FERRELLSBURG",
-    "FEZ",
-    "FOURTEEN",
-    "FRY",
-    "GILL",
-    "GREEN SHOAL",
-    "GRIFFITHSVILLE",
-    "HARTS",
-    "LEET",
-    "MIDKIFF",
-    "MYRA",
-    "PALERMO",
-    "PLEASANT VIEW",
-    "RANGER",
-    "RECTOR",
-    "SIAS",
-    "SOD",
-    "SPURLOCKVILLE",
-    "SUMERCO",
-    "SWEETLAND",
-    "WARREN",
-    "WEWANTA",
-    "WOODVILLE",
-    "YAWKEY",
-    
-    "CABELL COUNTY",
-      "CULLODEN",
-    
-    "LOGAN COUNTY",
-    
-    "PUTNAM COUNTY",
-    
-    "KANAWHA COUNTY",
-      "ALUM CRK",
-      "TORNADO"
-  };
 }
