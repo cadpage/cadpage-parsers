5712ea7b823aef36cb1065c35d92ff2370871234
Ken Corbin <ken@cadpage.org>
2023-12-05 16:44:29 -0800

    Fixed old parsin gproblem with Perry County, PA

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/PA/PAPerryCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/PA/PAPerryCountyParser.java
@@ -1,64 +1,94 @@
 package net.anei.cadpage.parsers.PA;
 
-import java.util.Properties;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-import net.anei.cadpage.parsers.dispatch.DispatchArchonixParser;
+import net.anei.cadpage.parsers.MsgInfo.Data;
+import net.anei.cadpage.parsers.dispatch.DispatchH05Parser;
 
+public class PAPerryCountyParser extends DispatchH05Parser {
 
-
-public class PAPerryCountyParser extends DispatchArchonixParser {
-  
   public PAPerryCountyParser() {
-    super(CITY_CODES, "PERRY COUNTY", "PA");
+    super("PERRY COUNTY", "PA",
+          "( SELECT/1 CALL:CALL! PLACE:PLACE! ADDR:ADDRCITYGPS1! " +
+                 "( CRSS:X! MAP:GPS! CFS:SKIP! ID:ID! PRI:PRI1! DATE:DATETIME! UNIT:UNIT! TIMES:EMPTY! TIMES+ " +
+                 "| CROSS_STREETS:X! ID:ID! PRI:PRI1! DATE:DATETIME! MAP:MAP! UNIT:UNIT! INFO:EMPTY! TIMES+? CALLER_NAME:NAME! FIRE_BOX:BOX! END ) " +
+          "| CALL_ADDR_CITY2 X PLACE APT GPS! PN:PHONE! MAP UNIT! INFO/N+ )");
   }
-  
+
   @Override
   public String getFilter() {
-    return "@perryco.org";
+    return "noreply@jcpc911.com";
   }
-  
 
-  private static final Properties CITY_CODES = buildCodeTable(new String[]{
-      
-      "01",   "BLAIN",
-      "02",   "BUFFALO TWP",
-      "03",   "CARROLL TWP",
-      "04",   "CENTRE TWP",
-      "05",   "DUNCANNON",
-      "06",   "GREENWOOD TWP",
-      "07",   "HOWE TWP",
-      "08",   "JACKSON TWP",
-      "09",   "JUNIATA TWP",
-      "10",   "LANDISBURG",
-      "11",   "LIVERPOOL",
-      "12",   "LIVERPOOL TWP",
-      "13",   "MARYSVILLE",
-      "14",   "MILLER TWP",
-      "15",   "MILLERSTOWN",
-      "16",   "NEW BLOOMFIELD",
-      "17",   "NEW BUFFALO",
-      "18",   "NEWPORT",
-      "19",   "N.E. MADISON TWP",
-      "20",   "OLIVER TWP",
-      "21",   "PENN TWP",
-      "22",   "RYE TWP",
-      "23",   "SAVILLE TWP",
-      "24",   "S.W. MADISON TWP",
-      "25",   "SPRING TWP",
-      "26",   "TOBOYNE TWP",
-      "27",   "TUSCARORA TWP",
-      "28",   "TYRONE TWP",
-      "29",   "WATTS TWP",
-      "30",   "WHEATFIELD TWP",
-      "32",   "DELAWARE TWP JC",
-      "33",   "GREENWOOD TWP JC",
-      "34",   "SUSQUEHANNA TWP JC",
+  @Override
+  public int getMapFlags() {
+    return MAP_FLG_PREFER_GPS;
+  }
 
-      "51",   "",
-      "52",   "",
-      "55",   ""
-      
-      
-  });
+  @Override
+  protected boolean parseHtmlMsg(String subject, String body, Data data) {
+    if (subject.equals("Do Not Reply")) {
+      setSelectValue("2");
+      return parseFields(body.split("\n"), data);
+    } else {
+      setSelectValue("1");
+      return super.parseHtmlMsg(subject, body, data);
+    }
+  }
 
+  @Override
+  public Field getField(String name) {
+    if (name.equals("ADDRCITYGPS1")) return new MyAddressCityGPS1Field();
+    if (name.equals("PRI1")) return new MyPriority1Field();
+
+    if (name.equals("CALL_ADDR_CITY2")) return new MyCallAddressCity2Field();
+    return super.getField(name);
+  }
+
+  private static final Pattern GPS_PTN = Pattern.compile("(\\d+\\.\\d{6,})(-\\d+\\.\\d{6,})");
+
+  private class MyAddressCityGPS1Field extends AddressCityField {
+    @Override
+    public void parse(String field, Data data) {
+      int pt = field.lastIndexOf(',');
+      if (pt >= 0) {
+        String gps = field.substring(pt+1).trim();
+        Matcher match = GPS_PTN.matcher(gps);
+        if (match.matches()) {
+          setGPSLoc(match.group(1)+','+match.group(2), data);
+          field = field.substring(0,pt).trim();
+        }
+        else if (gps.isEmpty()) {
+          field = field.substring(0,pt).trim();
+        }
+      }
+      field = stripFieldEnd(field, ",");
+      super.parse(field, data);
+    }
+
+    @Override
+    public String getFieldNames() {
+      return super.getFieldNames() + " GPS?";
+    }
+  }
+
+  private class MyPriority1Field extends PriorityField {
+    @Override
+    public void parse(String field, Data data) {
+      int pt = field.indexOf('-');
+      if (pt >= 0) field = field.substring(0,pt).trim();
+      super.parse(field, data);
+    }
+  }
+
+  private class MyCallAddressCity2Field extends AddressCityField {
+    @Override
+    public void parse(String field, Data data) {
+      int pt = field.indexOf('@');
+      if (pt < 0) abort();
+      data.strCall = field.substring(0,pt).trim();
+      super.parse(field.substring(pt+1).trim(), data);
+    }
+  }
 }
