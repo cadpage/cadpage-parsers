1094b160dbeb2add8aa92d3bc848872af3631e17
Ken Corbin <ken@cadpage.org>
2023-12-07 10:32:20 -0800

    Fixed parsing problem with Tolland County, CT (C)

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/CT/CTTollandCountyCParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/CT/CTTollandCountyCParser.java
@@ -4,35 +4,36 @@ import net.anei.cadpage.parsers.FieldProgramParser;
 import net.anei.cadpage.parsers.MsgInfo.Data;
 
 public class CTTollandCountyCParser extends FieldProgramParser {
-  
+
   public CTTollandCountyCParser() {
-    super("TOLLAND COUNTY", "CT", 
-          "Address:ADDRCITY! Location_Info:INFO! INFO/N+ Call_Type:CALL! Nature:CALL/SDS X-Sts:X! Date/Time:DATETIME Incident_#:ID! ID+");
+    super("TOLLAND COUNTY", "CT",
+          "Address:ADDRCITY! Location_Info:INFO! INFO/N+ Call_Type:CALL! Nature:CALL/SDS X-Sts:X! Date/Time:DATETIME " +
+              "Incident_#:ID! ID+ Latitude:GPS1 Longitude:GPS2");
   }
-  
+
   @Override
   public String getFilter() {
     return "messaging@iamresponding.com,777";
   }
-  
+
   @Override
   protected boolean parseMsg(String subject, String body, Data data) {
     data.strSource = subject;
     return parseFields(body.split("\n"), data);
   }
-  
+
   @Override
   public String getProgram() {
     return "SRC " + super.getProgram();
   }
-  
+
   @Override
   public Field getField(String name) {
     if (name.equals("DATETIME")) return new DateTimeField("\\d\\d?/\\d\\d?/\\d{4} \\d\\d?:\\d\\d:\\d\\d", true);
     if (name.equals("ID")) return new MyIdField();
     return super.getField(name);
   }
-  
+
   private class MyIdField extends IdField {
     @Override
     public void parse(String field, Data data) {
