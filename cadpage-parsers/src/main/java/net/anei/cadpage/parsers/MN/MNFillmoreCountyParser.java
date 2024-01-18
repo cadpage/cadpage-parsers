8d9d965d471243b8544696e300dd038aa12ea611
Ken Corbin <ken@cadpage.org>
2024-01-17 23:54:15 -0800

    Added GPS lookup for Fillmore County, MN

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/MN/MNFillmoreCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/MN/MNFillmoreCountyParser.java
@@ -1,10 +1,17 @@
 package net.anei.cadpage.parsers.MN;
 
+import java.util.Properties;
+
 import net.anei.cadpage.parsers.dispatch.DispatchA43Parser;
 
 public class MNFillmoreCountyParser extends DispatchA43Parser {
 
   public MNFillmoreCountyParser() {
     super("FILLMORE COUNTY", "MN");
+    setupGpsLookupTable(GPS_LOOKUP_TABLE);
   }
+
+  private static final Properties GPS_LOOKUP_TABLE = buildCodeTable(new String[] {
+      "1864 FIRST MINNESOTA",                 "+43.681830,-92.094260"
+  });
 }
