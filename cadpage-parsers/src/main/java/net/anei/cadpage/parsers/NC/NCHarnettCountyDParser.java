330217bf9001c6dd505162716746c5c88d60ddab
Ken Corbin <ken@cadpage.org>
2024-02-14 18:39:27 -0800

    Fixed parsing problem with Harnett County,NC (D)

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/NC/NCHarnettCountyDParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/NC/NCHarnettCountyDParser.java
@@ -2,6 +2,8 @@ package net.anei.cadpage.parsers.NC;
 
 import java.util.regex.Pattern;
 
+import net.anei.cadpage.parsers.CodeTable;
+import net.anei.cadpage.parsers.StandardCodeTable;
 import net.anei.cadpage.parsers.FieldProgramParser;
 import net.anei.cadpage.parsers.MsgInfo.Data;
 
@@ -9,10 +11,16 @@ public class NCHarnettCountyDParser extends FieldProgramParser {
 
   public NCHarnettCountyDParser() {
     super("HARNETT COUNTY", "NC",
-          "( AD:ADDR! PN:CALL! CTC:DATETIME! " +
-          "| PN:CALL! ADD:ADDR! ( NOTES:INFO! OCA:ID! " +
-                               "| CITY:CITY! TAC:CH! XST:X! INC#:ID LT/LNG:GPS/d UNIT(S):UNIT TIME:TIME CMT:INFO " +
-                               ") " +
+          "( PN:CALL! UNIT(S):UNIT ADD:ADDR! ( NOTES:INFO! OCA:ID! " +
+                                            "| CITY:CITY! XST:X! LT/LNG:GPS/d TAC:CH INC#:ID ( UNITS:UNIT | UNIT(S):UNIT ) TIME:TIME NOTES:INFO RA:CH/L " +
+                                            ") " +
+          "| RA:SRC! ( UNIT(S):UNIT! PN:CALL! ADD:ADDR! CITY:CITY! ( XST:X! TAC:CH! | TAC:CH! XST:X! | ) INC#:ID! TIME:TIME! ( NOTES:INFO! | CMT:INFO! ) " +
+                    "| PN:CALL! ADD:ADDR! CITY:CITY! TAC:CH! XST:X! INC#:ID! LT/LNG:GPS/d! UNIT(S):UNIT! TIME:TIME! CMT:INFO! " +
+                    ") " +
+          "| UNITS:UNIT! PN:CALL! ADD:ADDR! APT:APT! CITY:CITY! XST:X! TAC:CH! INC#:ID! TIME:TIME! NOTES:INFO! " +
+          "| CALL! ( RA:SRC! UNIT(S):UNIT! ADD:ADDR! CITY:CITY! ( TAC:CH! XST:X! | XST:X! TAC:CH! ) INC#:ID! TIME:TIME! ( NOTES:INFO! | CMT:INFO! ) " +
+                  "| UNITS:UNIT! ADD:ADDR! APT:APT! CITY:CITY! XST:X! TAC:CH! INC#:ID! NOTES:INFO! " +
+                  ") " +
           ") END");
   }
 
@@ -26,6 +34,9 @@ public class NCHarnettCountyDParser extends FieldProgramParser {
     return MAP_FLG_PREFER_GPS;
   }
 
+  private static final Pattern MISSING_BLNK_PTN = Pattern.compile("(?<=\\S)(?=(?:TIME|CMT|NOTES|UNIT\\(S\\)|PN):)");
+  private static final Pattern MISSING_COLON_PTN = Pattern.compile("(?<=INC#)(?!:)");
+
   @Override
   protected boolean parseMsg(String body, Data data) {
     if (body.startsWith("Received:")) {
@@ -33,16 +44,36 @@ public class NCHarnettCountyDParser extends FieldProgramParser {
       if (pt < 0) return false;
       body = body.substring(pt+2).trim();
     }
-    body = body.replace("TIME:", " TIME:").replace("CMT:", " CMT:").replace("NOTES:", " NOTES:");
-    return super.parseMsg(body, data);
+    body = MISSING_BLNK_PTN.matcher(body).replaceAll(" ");
+    body = MISSING_COLON_PTN.matcher(body).replaceAll(":");
+    if (!super.parseMsg(body, data)) return false;
+    String call = CALL_CODES.getCodeDescription(data.strCall);
+    if (call != null) {
+      data.strCode = data.strCall;
+      data.strCall = call;
+    }
+    return true;
+  }
+
+  @Override
+  public String getProgram() {
+    return super.getProgram().replace("CALL", "CODE CALL");
   }
 
   @Override
   public Field getField(String name) {
-    if (name.equals("DATETIME")) return new DateTimeField("\\d\\d/\\d\\d/\\d\\d +\\d\\d:\\d\\d:\\d\\d", true);
+    if (name.equals("SRC")) return new MySourceField();
     if (name.equals("TIME")) return new TimeField("\\d\\d?:\\d\\d:\\d\\d", true);
     if (name.equals("INFO")) return new MyInfoField();
     return super.getField(name);
+  }
+
+  private class MySourceField extends SourceField {
+    @Override
+    public void parse(String field, Data data) {
+      field = stripFieldEnd(field, ",");
+      super.parse(field, data);
+    }
   }
 
   private static Pattern INFO_BRK_PTN = Pattern.compile(",? *\\[\\d{1,2}\\]");
@@ -55,4 +86,6 @@ public class NCHarnettCountyDParser extends FieldProgramParser {
       }
     }
   }
+
+  private static final CodeTable CALL_CODES = new StandardCodeTable();
 }
