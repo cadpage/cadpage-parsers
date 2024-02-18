7d4b7be32fe6fd3becf4ec73f5ac99105c06fda8
Ken Corbin <ken@cadpage.org>
2024-02-18 13:37:11 -0800

    Updated sender filter for Clark County, KY

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/KY/KYClarkCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/KY/KYClarkCountyParser.java
@@ -6,14 +6,14 @@ import net.anei.cadpage.parsers.dispatch.DispatchA27Parser;
  * Clark County, KY
  */
 public class KYClarkCountyParser extends DispatchA27Parser {
-  
+
   public KYClarkCountyParser() {
     super("CLARK COUNTY", "KY","[A-Z]{1,3}FD|[-A-Z]+\\d+|\\d{8}");
   }
-  
+
   @Override
   public String getFilter() {
-    return "noreply@cisusa.org,grant.wyatt@active911.com";
+    return "noreply@cisusa.org,grant.wyatt@active911.com,winchesterpd@cissystem.com";
   }
-  
+
 }
