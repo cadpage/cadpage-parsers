23d502a6fe1f44a424ff5fa66fb3975b839850cc
Ken Corbin <ken@cadpage.org>
2024-02-26 11:14:51 -0800

    Fixed parsing problem with Orange Countyh, NC (B)

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/NC/NCOrangeCountyBParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/NC/NCOrangeCountyBParser.java
@@ -3,28 +3,33 @@ package net.anei.cadpage.parsers.NC;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import net.anei.cadpage.parsers.CodeTable;
+import net.anei.cadpage.parsers.StandardCodeTable;
 import net.anei.cadpage.parsers.FieldProgramParser;
 import net.anei.cadpage.parsers.MsgInfo.Data;
 
 
 
 public class NCOrangeCountyBParser extends FieldProgramParser {
-  
+
   private static final Pattern SUBJECT_PTN = Pattern.compile("([-0-9]+) *[-\\(]([A-Z ]+)\\)?");
-  
+
   public NCOrangeCountyBParser() {
     super("ORANGE COUNTY", "NC",
-           "URL EMPTY? CH ADDR APT? CALL! INFO+ OCPC:SKIP SUBD:PLACE INFO+");
+           "URL EMPTY? ( TG:CH! | CH! | CODE! ) EMPTY? ADDRCITYST EMPTY? ( District:SRC! | APT? CALL! INFO+ OCPC:SKIP SUBD:PLACE INFO/N+ District:SRC )");
   }
-  
+
   @Override
   public String getFilter() {
     return "ptpage@pgtrksrv01.net";
   }
-  
+
   @Override
   protected boolean parseMsg(String subject, String body, Data data) {
     do {
+      if (subject.equals("CHFD")) {
+        break;
+      }
       if (subject.equals("EMS")) {
         data.strCall = subject;
         break;
@@ -36,9 +41,14 @@ public class NCOrangeCountyBParser extends FieldProgramParser {
         break;
       }
     } while (false);
-    return parseFields(body.split("\n"), 4, data);
+    if (!parseFields(body.split("\n"), 4, data)) return false;
+    if (!data.strCode.isEmpty()) {
+      String call = CALL_CODES.getCodeDescription(data.strCode);
+      if (call !=  null) data.strCall = call;
+    }
+    return true;
   }
-  
+
   @Override
   public String getProgram() {
     return "ID " + super.getProgram();
@@ -46,8 +56,9 @@ public class NCOrangeCountyBParser extends FieldProgramParser {
 
   @Override
   protected Field getField(String name) {
-    if (name.equals("URL")) return new InfoUrlField("http://[A-Za-z0-9]+\\.net/.*", true);
+    if (name.equals("URL")) return new InfoUrlField("https?://[A-Za-z0-9]+\\.net/.*", true);
     if (name.equals("CH")) return new ChannelField("OPS.*", true);
+    if (name.equals("CODE")) return new CodeField("\\d{1,3}[A-E]\\d{1,2}[A-Z]?", true);
     if (name.equals("APT")) return new MyAptField();
     if (name.equals("CALL")) return new MyCallField();
     if (name.equals("INFO")) return new MyInfoField();
@@ -75,13 +86,13 @@ public class NCOrangeCountyBParser extends FieldProgramParser {
       }
       return true;
     }
-    
+
     @Override
     public void parse(String field, Data data) {
       if (!checkParse(field, data)) abort();
     }
   }
-  
+
   private static final Pattern CALL_CODE_PTN = Pattern.compile("^(\\d+[A-Z]\\d+[A-Z]?) +");
   private class MyCallField extends CallField {
     @Override
@@ -93,17 +104,19 @@ public class NCOrangeCountyBParser extends FieldProgramParser {
       }
       data.strCall = append(data.strCall, " - ", field);
     }
-    
+
     @Override
     public String getFieldNames() {
       return "CODE CALL";
     }
   }
-  
+
   private class MyInfoField extends InfoField {
     @Override
     public void parse(String field, Data data) {
       data.strSupp = append(data.strSupp, "\n", field);
     }
   }
+  
+  private CodeTable CALL_CODES = new StandardCodeTable();
 }
