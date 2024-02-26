6dcd4bcd13515146de388957d89569b72719884b
Ken Corbin <ken@cadpage.org>
2024-02-26 13:32:26 -0800

    Fixed parsing problem with Blair County, PA (B)

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/PA/PABlairCountyBParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/PA/PABlairCountyBParser.java
@@ -10,7 +10,7 @@ public class PABlairCountyBParser extends FieldProgramParser {
 
   public PABlairCountyBParser() {
     super(CITY_LIST, "BLAIR COUNTY", "PA",
-          "CALL ADDRCITY PLACE PLACE! Due:UNIT! END");
+          "CALL ADDRCITY PLACE PLACE! PLACE+ Due:UNIT! END");
   }
 
   @Override
@@ -18,12 +18,10 @@ public class PABlairCountyBParser extends FieldProgramParser {
     return "alerts@centre.ealert911.com";
   }
 
-  private static final Pattern DELIM = Pattern.compile("\n| +(?=Due:)");
-
   @Override
   protected boolean parseMsg(String subject, String body, Data data) {
-    if (!subject.equals(".")) return false;
-    return parseFields(DELIM.split(body), data);
+    if (!subject.equals("| !!")) return false;
+    return parseFields(body.split("\n"), data);
   }
 
   @Override
@@ -37,16 +35,23 @@ public class PABlairCountyBParser extends FieldProgramParser {
   private class MyAddressCityField extends AddressCityField {
     @Override
     public void parse(String field, Data data) {
-      super.parse(field, data);
-
-      Matcher match = ADDR_APT_PTN.matcher(data.strAddress);
-      if (match.matches()) {
-        data.strAddress = match.group(1).trim();
-        data.strApt =  match.group(2).trim();
+      if (field.endsWith(" INTERSECTION")) {
+        field = stripFieldStart(field, "BL - ");
+        field = field.substring(0, field.length()-13).trim();
+        super.parse(field, data);
       }
+      else {
+        super.parse(field, data);
 
-      parseAddress(StartType.START_OTHER, FLAG_ONLY_CITY | FLAG_ANCHOR_END, data.strCity, data);
-      data.strCity = stripFieldEnd(data.strCity, " BORO");
+        Matcher match = ADDR_APT_PTN.matcher(data.strAddress);
+        if (match.matches()) {
+          data.strAddress = match.group(1).trim();
+          data.strApt =  match.group(2).trim();
+        }
+
+        parseAddress(StartType.START_OTHER, FLAG_ONLY_CITY | FLAG_ANCHOR_END, data.strCity, data);
+        data.strCity = stripFieldEnd(data.strCity, " BORO");
+      }
     }
   }
 
@@ -54,19 +59,25 @@ public class PABlairCountyBParser extends FieldProgramParser {
   private class MyPlaceField extends PlaceField {
     @Override
     public void parse(String field, Data data) {
-      Matcher match = APT_PTN.matcher(field);
-      if (match.matches()) {
-        String tmp = match.group(1);
-        if (tmp != null) field = tmp;
-        if (!field.equals(data.strApt)) data.strApt = append(data.strApt, "-", field);
-      } else {
-        data.strPlace = append(data.strPlace, " - ", field);
+      if (field.endsWith(" INTERSECTION")) return;
+      if (field.startsWith("BL - ")) {
+        data.strPlace = append(data.strPlace, " - ", field.substring(4).trim());
+      }
+      else {
+        Matcher match = APT_PTN.matcher(field);
+        if (match.matches()) {
+          String tmp = match.group(1);
+          if (tmp != null) field = tmp;
+          if (!field.equals(data.strApt)) data.strApt = append(data.strApt, "-", field);
+        } else {
+          data.strPlace = append(data.strPlace, " - ", field);
+        }
       }
     }
 
     @Override
     public String getFieldNames() {
-      return "APT PLACE";
+      return "PLACE APT";
     }
   }
 
