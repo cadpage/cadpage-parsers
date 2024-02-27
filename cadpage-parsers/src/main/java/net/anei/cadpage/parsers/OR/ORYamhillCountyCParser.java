cbc4d6d9c450638da32548053db824b2cbd137a9
Ken Corbin <ken@cadpage.org>
2024-02-26 18:34:05 -0800

    Fixed parsing problem with Yamhill County, OR (C)

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/OR/ORYamhillCountyCParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/OR/ORYamhillCountyCParser.java
@@ -10,18 +10,18 @@ import net.anei.cadpage.parsers.SplitMsgOptions;
 import net.anei.cadpage.parsers.SplitMsgOptionsCustom;
 
 public class ORYamhillCountyCParser extends FieldProgramParser {
-  
+
   public ORYamhillCountyCParser() {
-    super("YAMHILL COUNTY", "OR", 
-          "( SELECT/1 CALL:CALL! PLACE:PLACE! ADDR:ADDR! CITY:CITY! ID:ID! Cross_Street:X? MAP:MAP% UNIT:UNIT%  NARR:INFO/N " + 
+    super("YAMHILL COUNTY", "OR",
+          "( SELECT/1 CALL:CALL! PLACE:PLACE! ADDR:ADDR! CITY:CITY! ID:ID! Cross_Street:X? MAP:MAP% UNIT:UNIT%  NARR:INFO/N " +
           "| CALL ADDR PLACE! Caller:NAME! Caller_#:PHONE! Units:UNIT! ) END");
   }
-  
+
   @Override
   public String getFilter() {
     return "ycom@ycom911.org";
   }
-  
+
   @Override
   public SplitMsgOptions getActive911SplitMsgOptions() {
     return new SplitMsgOptionsCustom(){
@@ -33,7 +33,7 @@ public class ORYamhillCountyCParser extends FieldProgramParser {
   private static final Pattern RUN_REPORT_PTN = Pattern.compile("\\* ?CALL TIMES ?\\* ?Run #:((?:[A-Z]+-)?[-0-9]*) +Add: *?(.*?) +?((?:Call Rec:|Call Received:|Disp:).*)");
   private static final Pattern RR_BRK_PTN = Pattern.compile("(?:[* ]+|(?<!(?:^|[* \n])))(?=(?:Disp|Enr|Onscene|Avail|Unit):)");
   private static final Pattern DELIM = Pattern.compile("\\* | +(?=NARR:)");
-  
+
   @Override
   protected boolean parseMsg(String subject, String body, Data data) {
     Matcher match = RUN_REPORT_PTN.matcher(body);
@@ -45,24 +45,33 @@ public class ORYamhillCountyCParser extends FieldProgramParser {
       data.strSupp = RR_BRK_PTN.matcher(match.group(3)).replaceAll("\n");
       return true;
     }
-    
+
     if (body.startsWith("*CALL INFORMATION*")) {
       setSelectValue("2");
       body = body.substring(18).trim();
       return parseFields(body.split(";"), data);
     }
-    
+
     setSelectValue("1");
     body = body.replace("*Cross Street:", "* Cross Street:");
     return parseFields(DELIM.split(body), data);
   }
-  
+
   @Override
   public Field getField(String name) {
+    if (name.equals("CITY")) return new MyCityField();
     if (name.equals("INFO")) return new MyInfoField();
     return super.getField(name);
   }
-  
+
+  private class MyCityField extends CityField {
+    @Override
+    public void parse(String field, Data data) {
+      if (field.equals("YAMHILL COUN")) field = "YAMHILL COUNTY";
+      super.parse(field, data);
+    }
+  }
+
   private static final Pattern INFO_BRK_PTN = Pattern.compile("(?=\\[\\d{1,2}\\] )");
   private static final Pattern INFO_JUNK_PTN = Pattern.compile("\\[\\d{1,2}\\]");
   private class MyInfoField extends InfoField {
