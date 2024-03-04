9697916251b08da69477eafc842b8eb61d4a25d8
Ken Corbin <ken@cadpage.org>
2024-03-04 13:36:48 -0800

    Fixed parsing problem with Liberty County, GA

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/GA/GALibertyCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/GA/GALibertyCountyParser.java
@@ -2,130 +2,69 @@ package net.anei.cadpage.parsers.GA;
 
 import java.util.regex.Pattern;
 
-import net.anei.cadpage.parsers.CodeSet;
-import net.anei.cadpage.parsers.dispatch.DispatchB2Parser;
+import net.anei.cadpage.parsers.FieldProgramParser;
+import net.anei.cadpage.parsers.MsgInfo.Data;
 
 
-public class GALibertyCountyParser extends DispatchB2Parser {
+public class GALibertyCountyParser extends FieldProgramParser {
 
   public GALibertyCountyParser() {
-    super("911COMMUNICATIONS:", CITY_LIST,"LIBERTY COUNTY", "GA", B2_FORCE_CALL_CODE);
-    setupMultiWordStreets(
-        "BARRY MCCAFFREY",
-        "BILL CARTER",
-        "CHARLIE BUTLER",
-        "CHERRIE MURRELL",
-        "COUNTY LINE",
-        "CROSS CREEK",
-        "DUTCHMANS COVE",
-        "EB COOPER",
-        "EG MILES",
-        "GENERAL SCREVEN",
-        "GENERAL STEWART",
-        "GLENN BRYANT",
-        "FORT MORRIS",
-        "INTERSTATE PAPER",
-        "JOHN WELLS",
-        "LEROY COFFER",
-        "LEWIS FRASIER",
-        "LIVE OAK CHURCH",
-        "LONG FRASIER",
-        "MAGNOLIA PLANTATION",
-        "MILES CROSSING",
-        "OAK CREST",
-        "PATE ROGERS",
-        "RYE PATCH",
-        "SEABROOK ISLAND",
-        "SEABROOK SCHOOL",
-        "SHADOW WALK",
-        "WEEPING WILLOW",
-        "WELLS CEMETERY"
-    );
-    addRoadSuffixTerms("HIGHWAY");
-    removeWords("PLACE");
+    super("LIBERTY COUNTY", "GA",
+          "DATETIME CALL ADDRCITY X EMPTY UNIT! ID INFO/N+");
   }
-  
+
+  @Override
+  protected boolean parseMsg(String subject, String body, Data data) {
+    if (!subject.equals("!")) return false;
+    return parseFields(body.split("\n"), data);
+  }
+
+  @Override
+  public Field getField(String name) {
+    if (name.equals("DATE")) return new DateField("\\d\\d?/\\d\\d?/\\d{4} +\\d\\d:\\d\\d:\\d\\d", true);
+    if (name.equals("ADDRCITY")) return new MyAddressCityField();
+    if (name.equals("X")) return new MyCrossField();
+    return super.getField(name);
+  }
+
+  private class MyAddressCityField extends AddressCityField {
+    @Override
+    public void parse(String field, Data data) {
+      int pt = field.indexOf(" / ");
+      if (pt >= 0) {
+        data.strPlace = field.substring(pt+3).trim();
+        field = field.substring(0, pt).trim();
+      } else {
+        field = stripFieldEnd(field, "/");
+      }
+      field = field.replace('@', '&');
+      super.parse(field, data);
+    }
+
+    @Override
+    public String getFieldNames() {
+      return super.getFieldNames() + " PLACE";
+    }
+  }
+
+  private class MyCrossField extends CrossField {
+    @Override
+    public void parse(String field, Data data) {
+      if (field.equals("No Cross Streets Found")) return;
+      super.parse(field, data);
+    }
+  }
+
   @Override
   public String adjustMapAddress(String addr) {
     addr = EG_MILES_PTN.matcher(addr).replaceAll("ELMA G MILES");
     return super.adjustMapAddress(addr);
   }
   private static final Pattern EG_MILES_PTN = Pattern.compile("\\bE ?G MILES\\b", Pattern.CASE_INSENSITIVE);
-  
+
   @Override
   public String adjustMapCity(String city) {
     if (city.equalsIgnoreCase("FAIRHAVEN ACRES TP")) city = "MIDWAY";
     return super.adjustMapCity(city);
   }
-  
-  private static final String[] CITY_LIST = new String[]{
-    "ALLENHURST",
-    "FLEMINGTON",
-    "FORT STEWART",
-    "GUMBRANCH",
-    "HINESVILLE",
-    "MIDWAY",
-    "SUNBURY",
-    "RICEBORO",
-    "WALTHOURVILLE",
-    
-    "FAIRHAVEN ACRES TP"
-  };
-  
-  @Override
-  protected CodeSet buildCallList() {
-    return new CodeSet(
-        "ABDOMINAL/BACK PAIN",
-        "ANIMAL BITE",
-        "ANIMAL CALL",
-        "APPLIANCE FIRE",
-        "ASSIST CITIZEN",
-        "BRUSH FIRE",
-        "BROKEN BONE",
-        "CHEST PAIN/CARDIAC SYMPTOMS",
-        "DIABETIC EMERGENCY",
-        "DISTURBANCE",
-        "DOMESTIC DISPUTE IN PROGRESS",
-        "DOMESTIC RESCUE",
-        "DOWNED WIRES",
-        "DWELLING FIRE(SFH)",
-        "EMS MISCELLANEOUS",
-        "FAILURE TO STOP",
-        "FALL VICTIM",
-        "FIRE TEST CALL",
-        "GAS LEAK/OUTSIDE",
-        "HAZMAT INCIDENT",
-        "HEADACHE/STROKE",
-        "HEMORRHAGE/BLEEDING",
-        "INJURY OTHER THAN FALL",
-        "JUVENILE DELIQUENCY",
-        "LABOR/EMINENT BIRTH",
-        "LACERATION",
-        "MEDICAL ALARM",
-        "MESSAGE DELIVERY",
-        "MISSING PERSON",
-        "PSYCHIATRIC EMERGENCY",
-        "RESPIRATORY DISTRESS",
-        "SICK/UNKNOWN ILLNESS",
-        "SEIZURE",
-        "SEXUAL ASSAULT/RAPE",
-        "SUICIDE THREAT",
-        "STORM DAMAGE",
-        "SUICIDE ATTEMPT",
-        "SYNCOPAL EPISODE",
-        "TRAFFIC ACCIDENT NO INJURY",
-        "TRAFFIC ACCIDENT WITH INJURIES",
-        "TRAFFIC STOP (CLI)",
-        "TRANSPORT",
-        "TRUCK FIRE",
-        "UNCONSCIOUS",
-        "UNKNOWN PROBLEM",
-        "UNRESPONSIVE",
-        "VEHICLE FIRE",
-        "VEHICLE THEFT",
-        "VIN VERIFICATION"
-
-    );
-  }
-  
 }
