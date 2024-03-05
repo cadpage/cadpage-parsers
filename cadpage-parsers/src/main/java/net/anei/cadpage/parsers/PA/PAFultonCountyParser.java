bd249fd51aa4de19145370eba253013e096466bd
Ken Corbin <ken@cadpage.org>
2024-03-04 18:44:58 -0800

    Fixed parsing problem with Fulton County, PA

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/PA/PAFultonCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/PA/PAFultonCountyParser.java
@@ -1,6 +1,5 @@
 package net.anei.cadpage.parsers.PA;
 
-import java.util.Properties;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -8,17 +7,16 @@ import net.anei.cadpage.parsers.FieldProgramParser;
 import net.anei.cadpage.parsers.MsgInfo.Data;
 
 
-
 public class PAFultonCountyParser extends FieldProgramParser {
 
   public PAFultonCountyParser() {
     super("FULTON COUNTY", "PA",
-          "Units:UNIT! UNIT? CALL! Location:EMPTY! ADDRCITY! Narrative:INFO! INFO/N+");
+          "Units:EMPTY! UNIT! CALL! Location:EMPTY! ADDRCITY! X! Narrative:INFO! INFO/N+");
   }
 
   @Override
   public String getFilter() {
-    return "alerts@centre.ealert911.com";
+    return "dispatch@centrecountypa.gov";
   }
 
   @Override
@@ -28,51 +26,41 @@ public class PAFultonCountyParser extends FieldProgramParser {
 
   @Override
   public boolean parseMsg(String subject, String body, Data data) {
-    if (!subject.equals(".")) return false;
+    if (!subject.startsWith("| ")) return false;
+    data.strSource = subject.substring(2).trim();
     return parseFields(body.split("\n"), data);
   }
 
   @Override
+  public String getProgram() {
+    return "SRC " + super.getProgram();
+  }
+
+  @Override
   public Field getField(String name) {
-    if (name.equals("UNIT")) return new MyUnitField();
     if (name.equals("ADDRCITY")) return new MyAddressCityField();
     return super.getField(name);
   }
 
-  private class MyUnitField extends UnitField {
-    @Override
-    public boolean canFail() {
-      return true;
-    }
-
-    @Override
-    public boolean checkParse(String field, Data data) {
-      if (!data.strUnit.isEmpty()) return false;
-      data.strUnit = field;
-      return true;
-    }
-
-    @Override
-    public void parse(String field, Data data) {
-      if (!checkParse(field, data)) abort();
-    }
-  }
-
-  private static final Pattern ADDR_GPS_X_PTN = Pattern.compile("(.*?) ([-+]?\\d{2,3}\\.\\d{6,} +[-+]?\\d{2,3}\\.\\d{6,}|-361 +-361) *(.*)");
+  private static final Pattern ADDR_GPS_X_PTN = Pattern.compile("(.*?) ([-+]?\\d{2,3}\\.\\d{6,} +[-+]?\\d{2,3}\\.\\d{6,}|-361 +-361)");
   private class MyAddressCityField extends AddressCityField {
     @Override
     public void parse(String field, Data data) {
+      if (field.equals("<UNKNOWN>")) {
+        data.strAddress = field;
+        return;
+      }
+
       Matcher match = ADDR_GPS_X_PTN.matcher(field);
       if (!match.matches()) abort();
       super.parse(match.group(1).trim(), data);
       data.strCity = stripFieldEnd(data.strCity, " BORO");
       setGPSLoc(match.group(2), data);
-      data.strCross = match.group(3);
     }
 
     @Override
     public String getFieldNames() {
-      return "ADDR APT CITY GPS X";
+      return "ADDR APT CITY GPS";
     }
   }
 }
