0c59b68a9c939c3414408352fc418441e7782a8f
Ken Corbin <ken@cadpage.org>
2024-03-06 15:44:08 -0800

    Fixed parsing problem witih Reno County, KS

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/KS/KSRenoCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/KS/KSRenoCountyParser.java
@@ -4,131 +4,118 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import net.anei.cadpage.parsers.MsgInfo.Data;
-import net.anei.cadpage.parsers.SmartAddressParser;
+import net.anei.cadpage.parsers.dispatch.DispatchH05Parser;
 
 /**
  * Reno County, KS
  */
-public class KSRenoCountyParser extends SmartAddressParser {
-  
+public class KSRenoCountyParser extends DispatchH05Parser {
+
   public KSRenoCountyParser() {
-    super(CITY_LIST, "RENO COUNTY", "KS");
-    setFieldList("CALL ADDR APT CITY GPS INFO ID");
+    super("RENO COUNTY", "KS",
+          "( SELECT/1 ID1 ADDRCITY1 PLACE CALL UNIT! INFO/N+ " +
+          "| CALL2 ADDRCITY2 NAME_PHONE? DATETIME2! INFO_BLK/Z+? UNIT2! TIMES+? ID FINAL END )");
   }
-  
+
   @Override
   public String getFilter() {
-    return "NWSPage@renolec.com";
-  }  
-  
+    return "Dispatch@renolec.com";
+  }
+
   @Override
   public int getMapFlags() {
     return MAP_FLG_PREFER_GPS;
   }
-  
-  private static final Pattern MASTER = Pattern.compile("(.*?)  -  (.*?)(?:  (\\d{4}-\\d{8}))?");
-  private static final Pattern GPS_PTN = Pattern.compile(" (\\d+\\.\\d{6,}|-361) / (-\\d+\\.\\d{6,}|-361)(?:  |$)");
-  private static final Pattern AVE_X_PTN = Pattern.compile("(.*\\b[EW] AVE) & ([A-Z])");
-  private static final Pattern KDD_HWY_PTN = Pattern.compile("\\bK(\\d+) HWY\\b");
-  private static final Pattern E911_INFO_PTN = Pattern.compile(" *\\bE911 Info - Class of Service: [A-Za-z0-9]+ Special Response Info: UNC=.* Uncertainty: +Confidence: *");
 
   @Override
-  public boolean parseMsg(String subject, String body, Data data) {
-    
-    if (!subject.equals("Page Notification")) return false;
-    
-    body = body.replace("\n ", "").replace("\n", "");
-    Matcher match = MASTER.matcher(body);
-    if (!match.matches()) return false;
-    data.strCall = match.group(1).trim();
-    body = match.group(2).trim();
-    data.strCallId = getOptGroup(match.group(3));
-    
-    match = GPS_PTN.matcher(body);
-    if (match.find()) {
-      setGPSLoc(match.group(1)+','+match.group(2), data);
-      String addr = body.substring(0,match.start()).trim();
-      parseAddress(StartType.START_ADDR, FLAG_IMPLIED_INTERSECT | FLAG_RECHECK_APT | FLAG_ANCHOR_END, addr, data);
-      data.strSupp = body.substring(match.end()).trim();
-    } else {
-      parseAddress(StartType.START_ADDR, FLAG_IMPLIED_INTERSECT | FLAG_RECHECK_APT, body, data);
-      data.strSupp = getLeft();
-    }
-    
-    // Weird E AVE X street names get picked up by the implied intersection logic and
-    // have to be corrected
-    match = AVE_X_PTN.matcher(data.strAddress);
-    if (match.matches()) {
-      data.strAddress = match.group(1) + ' ' + match.group(2);
-    }
-    
-    // Kxx HWY needs to turn into KS xx
-    data.strAddress = KDD_HWY_PTN.matcher(data.strAddress).replaceAll("KS $1");
-    
-    data.strSupp = E911_INFO_PTN.matcher(data.strSupp).replaceAll(" ").trim();
-    
-    if (data.strCity.equalsIgnoreCase("COUNTY")) data.strCity = "";
+  protected boolean keepLeadBreak() {
     return true;
   }
-  
-  private static final String[] CITY_LIST = new String[]{
 
-    // Cities
-    "ABBYVILLE",
-    "ARLINGTON",
-    "BUHLER",
-    "HAVEN",
-    "HUTCHINSON",
-    "LANGDON",
-    "NICKERSON",
-    "PARTRIDGE",
-    "PLEVNA",
-    "PRETTY PRAIRIE",
-    "SOUTH HUTCHINSON",
-    "SYLVIA",
-    "TURON",
-    "WILLOWBROOK",
+  private static final Pattern DELIM = Pattern.compile(",?\n");
+  @Override
+  public boolean parseHtmlMsg(String subject, String body, Data data) {
 
-    // Unincorporated communities
-    "CASTLETON",
-    "DARLOW",
-    "MEDORA",
-    "PLEASANTVIEW",
-    "ST JOE",
-    "YODER",
-   
-    // Townships
-    "ALBION TWP",
-    "BELL TWP",
-    "CASTLETON TWP",
-    "CENTER TWP",
-    "CLAY TWP",
-    "ENTERPRISE TWP",
-    "GRANT TWP",
-    "GROVE TWP",
-    "HAVEN TWP",
-    "HAYES TWP",
-    "HUNTSVILLE TWP",
-    "LANGDON TWP",
-    "LINCOLN TWP",
-    "LITTLE REVIER TWP",
-    "LODA TWP",
-    "MEDFORD TWP",
-    "MEDORA TWP",
-    "MIAMI TWP",
-    "NINNESCAH TWP",
-    "PLEVNA TWP",
-    "RENO TWP",
-    "ROSCOE TWP",
-    "SALT CREEK TWP",
-    "SUMNER TWP",
-    "SYLVIA TWP",
-    "TROY TWP",
-    "VALLEY TWP",
-    "WALNUT TWP",
-    "WESTMINISTER TWP",
-    "YODER TWP",
-    
-    "COUNTY"
-  };
+    if (subject.equals("Page Notification")) {
+      setSelectValue("1");
+      return parseFields(DELIM.split(body), data);
+    } else {
+      setSelectValue("2");
+      return super.parseHtmlMsg(subject, body, data);
+    }
+  }
+
+  public Field getField(String name) {
+    if (name.equals("ID1")) return new IdField("\\d{4}-\\d{8}\\b.*|", true);
+    if (name.equals("ADDRCITY1")) return new MyAddressCity1Field();
+    if (name.equals("CALL2")) return new MyCall2Field();
+    if (name.equals("ADDRCITY2")) return new MyAddressCity2Field();
+    if (name.equals("NAME_PHONE")) return new MyNamePhoneField();
+    if (name.equals("DATETIME2")) return new DateTimeField("\\d\\d?/\\d\\d?/\\d{4} +\\d\\d:\\d\\d:\\d\\d", true);
+    if (name.equals("UNIT2")) return new UnitField("(?:\\b(?:[A-Z]+\\d+|DC-\\d+|\\S+ ?NOTIFY)\\b[, ]*)+", true);
+    if (name.equals("FINAL")) return new SkipField("Final", true);
+    return super.getField(name);
+  }
+
+  private class MyAddressCity1Field extends AddressCityField {
+    @Override
+    public void parse(String field, Data data) {
+      super.parse(field, data);
+      int pt = data.strCity.indexOf(',');
+      if (pt >= 0) data.strCity = data.strCity.substring(0,pt).trim();
+    }
+  }
+
+  private static final Pattern CALL2_HDR_PTN = Pattern.compile("\\d\\d?/\\d\\d?/\\d{4} \\d\\d:\\d\\d:\\d\\d +(.*)");
+
+  private class MyCall2Field extends CallField {
+    @Override
+    public void parse(String field, Data data) {
+      Matcher match = CALL2_HDR_PTN.matcher(field);
+      if (!match.matches()) abort();
+      field = match.group(1);
+      int len = Math.min(field.length()/2, 10);
+      int pt = field.indexOf(field.substring(0, len), len);
+      if (pt >= 0) field = field.substring(0, pt).trim();
+      super.parse(field, data);
+    }
+  }
+
+  private static final Pattern ADDR_CITY2_DELIM = Pattern.compile(" *, *");
+
+  private class MyAddressCity2Field extends AddressCityField {
+    @Override
+    public void parse(String field, Data data) {
+      String[] parts = ADDR_CITY2_DELIM.split(field, -1);
+      if (parts.length < 3) abort();
+      parseAddress(parts[0].replace('@', '&'), data);
+      data.strCity = parts[1];
+      data.strApt = append(data.strApt, "-", parts[2]);
+      for (int ndx = 3; ndx < parts.length; ndx++)
+      data.strCross = append(data.strCross, " / ", parts[ndx]);
+    }
+
+    @Override
+    public String getFieldNames() {
+      return "ADDR CITY APT X";
+    }
+  }
+
+  private class MyNamePhoneField extends Field {
+    @Override
+    public void parse(String field, Data data) {
+      int pt = field.indexOf('(');
+      if (pt >= 0) {
+        data.strPhone = field.substring(pt);
+        field = field.substring(0, pt);
+      }
+      data.strName = field;
+    }
+
+    @Override
+    public String getFieldNames() {
+      return "NAME PHONE";
+    }
+
+  }
 }
