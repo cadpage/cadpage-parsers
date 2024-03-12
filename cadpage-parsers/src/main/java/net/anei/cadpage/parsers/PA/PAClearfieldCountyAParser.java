defaf697122386396509b590cd517c30a8398834
Ken Corbin <ken@cadpage.org>
2024-03-11 20:28:40 -0700

    Fixed parsing problem with Clearfield County, PA (A)

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/PA/PAClearfieldCountyAParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/PA/PAClearfieldCountyAParser.java
@@ -1,51 +1,34 @@
 package net.anei.cadpage.parsers.PA;
 
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.Properties;
-
-import net.anei.cadpage.parsers.FieldProgramParser;
 import net.anei.cadpage.parsers.MsgInfo.Data;
+import net.anei.cadpage.parsers.dispatch.DispatchH05Parser;
 
 /**
  * Clearfield County, PA
  */
-public class PAClearfieldCountyAParser extends FieldProgramParser {
+public class PAClearfieldCountyAParser extends DispatchH05Parser {
 
   public PAClearfieldCountyAParser() {
     super("CLEARFIELD COUNTY", "PA",
-           "Inc:CALL! Add:ADDR! City:CITY! Units:UNIT SRC DATETIME END");
+           "Inc_Code:CALL! Address:ADDRCITY! Common_Name:PLACE! Units:UNIT! Cross_Streets:X! Alert_Code:CODE! END");
   }
 
   @Override
   public String getFilter() {
-    return "alerts@clearfieldalerts.com,Clearfield Alerts";
-  }
-
-  @Override
-  protected boolean parseMsg(String subject, String body, Data data) {
-    if (!subject.equals("Clearfield Alert")) return false;
-    if (!parseFields(body.split("\n"), 3, data)) return false;
-    data.strCity = convertCodes(data.strCity, CITY_CODES);
-    return true;
+    return "noreply@ntr911sa.com";
   }
 
   @Override
   public Field getField(String name) {
-    if (name.equals("DATETIME")) return new DateTimeField(DATE_FMT);
+    if (name.equals("ADDRCITY")) return new MyAddressCityField();
     return super.getField(name);
   }
-  private static final DateFormat DATE_FMT = new SimpleDateFormat("MM/dd/yyyy hh:mm:ss aa");
 
-  private static final Properties CITY_CODES = buildCodeTable(new String[] {
-      "BURN_TWP",   "BURNSIDE TWP",
-      "CHEST_CAM",  "CHEST TWP",
-      "FALLS_JEF",  "FALLS CREEK",
-      "HORTN_ELK",  "HORTON TWP",
-      "READE_CAM",  "READE TWP",
-      "SNDER_JEF",  "SNYDER TWP",
-      "WASH_JEFF",  "WASHINGTON TWP",
-      "WHTE_CAM",   "WHITE TWP",
-      "WINSL_JEF",  "WINSLOW TWP"
-  });
+  private class MyAddressCityField extends AddressCityField {
+    @Override
+    public void parse(String field, Data data) {
+      super.parse(field, data);
+      data.strCity = stripFieldEnd(data.strCity, " BORO");
+    }
+  }
 }
