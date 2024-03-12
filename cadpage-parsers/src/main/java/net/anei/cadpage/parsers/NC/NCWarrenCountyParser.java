4496576f8aca21d0812e83bacd7a8068a33aeb5c
Ken Corbin <ken@cadpage.org>
2024-03-12 13:41:34 -0700

    Fixed parsing problem with Warren County, NC

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/NC/NCWarrenCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/NC/NCWarrenCountyParser.java
@@ -1,97 +1,23 @@
 
 package net.anei.cadpage.parsers.NC;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
-import net.anei.cadpage.parsers.MsgInfo.Data;
-import net.anei.cadpage.parsers.dispatch.DispatchSouthernParser;
+import net.anei.cadpage.parsers.dispatch.DispatchA71Parser;
 
 
 
-public class NCWarrenCountyParser extends DispatchSouthernParser {
+public class NCWarrenCountyParser extends DispatchA71Parser {
 
   public NCWarrenCountyParser() {
-    super(CITY_LIST, "WARREN COUNTY", "NC", 
-          DSFLG_ADDR|DSFLG_OPT_CODE|DSFLG_OPT_X|DSFLG_OPT_ID|DSFLG_TIME);
+    super("WARREN COUNTY", "NC");
   }
-  
+
   @Override
   public String getFilter() {
     return "@warrencountync.com";
   }
-  
-  private static final Pattern DISPATCH_PTN = Pattern.compile(" +[A-Za-z]+\\d+$");
 
   @Override
-  protected boolean parseMsg(String body, Data data) {
-
-    Matcher match = DISPATCH_PTN.matcher(body);
-    if (match.find()) body = body.substring(0, match.start());
-    
-    if (!super.parseMsg(body, data)) return false;
-    if (data.strSupp.length() > 0) {
-      data.strCall = data.strCall + data.strSupp;
-      data.strSupp = "";
-    }
-    return true;
+  public int getMapFlags() {
+    return MAP_FLG_PREFER_GPS;
   }
-  
-  @Override
-  protected boolean isNotExtraApt(String apt) {
-    if (apt.endsWith(" MM")) return true;
-    return super.isNotExtraApt(apt);
-  }
-
-  private static final String[] CITY_LIST = new String[]{
-    "ALBERTA",
-    "AURELIAN SPRINGS",
-    "BOYDTON",
-    "BRINKLEYVILLE",
-    "BRODNAX",
-    "BUNN",
-    "CENTERVILLE",
-    "CHASE CITY",
-    "CLARKSVILLE",
-    "CONWAY",
-    "ENFIELD",
-    "FRANKLINTON",
-    "GARYSBURG",
-    "GASBURG",
-    "GASTON",
-    "HALIFAX",
-    "HEATHSVILLE",
-    "HENDERSON",
-    "HENRICO",
-    "HOBGOOD",
-    "HOLLISTER",
-    "JACKSON",
-    "KITTRELL",
-    "LA CROSSE",
-    "LASKER",
-    "LAWRENCEVILLE",
-    "LITTLETON",
-    "LOUISBURG",
-    "MACON",
-    "MANSON",
-    "MIDDLEBURG",
-    "NORLINA",
-    "RICH SQUARE",
-    "ROANOKE RAPIDS",
-    "SCOTLAND NECK",
-    "SEABOARD",
-    "SEVERN",
-    "SOUTH HENDERSON",
-    "SOUTH HILL",
-    "SOUTH ROSEMARY",
-    "SOUTH WELDON",
-    "WAKE FOREST",
-    "WARRENTON",
-    "WELDON",
-    "WHITE PLAINS",
-    "WISE",
-    "WOODLAND",
-    "YOUNGSVILLE",
-    "ZEBULON"
-  };
 }
