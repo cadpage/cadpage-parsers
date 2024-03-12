dc484004be8702cf80d7a06fc61182d101bc133c
Ken Corbin <ken@cadpage.org>
2024-03-11 21:48:31 -0700

    Fixed parsing problem with Calhoun County, FL

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/FL/FLCalhounCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/FL/FLCalhounCountyParser.java
@@ -1,35 +1,32 @@
 package net.anei.cadpage.parsers.FL;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 import net.anei.cadpage.parsers.FieldProgramParser;
 import net.anei.cadpage.parsers.MsgInfo.Data;
 
 public class FLCalhounCountyParser extends FieldProgramParser {
 
   public FLCalhounCountyParser() {
-    super("CALHOUN COUNTY", "FL", 
-          "CALL:CALL! PLACE:PLACE! ADDR:ADDR! CITY:CITY! ID:ID! PRI:PRI! DATE:DATE! TIME:TIME! MAP:MAP! UNIT:UNIT! INFO:INFO!");
+    super("CALHOUN COUNTY", "FL",
+          "CALL:CALL! PLACE:PLACE! ADDR:ADDR! CITY:CITY! ID:ID! PRI:PRI! DATE:DATE! TIME:TIME! MAP:MAP? UNIT:UNIT! INFO:INFO!");
   }
-  
+
   @Override
   public String getFilter() {
     return "woody@usa-software.com,calhouncad@gmail.com,CalhounCAD@CalhounSheriff.com";
   }
-  
+
   @Override
   protected boolean parseMsg(String subject, String body, Data data) {
     return parseFields(body.split("\n"), data);
   }
-  
+
   @Override
   public Field getField(String name) {
     if (name.equals("ADDR")) return new MyAddressField();
-    if (name.equals("TIME")) return new MyTimeField();
+    if (name.equals("TIME")) return new TimeField("\\d\\d:\\d\\d:\\d\\d", true);
     return super.getField(name);
   }
-  
+
   private class MyAddressField extends AddressField {
     @Override
     public void parse(String field, Data data) {
@@ -43,21 +40,11 @@ public class FLCalhounCountyParser extends FieldProgramParser {
       }
       super.parse(field, data);
     }
-    
+
     private String adjustAddress(String  addr) {
       addr = stripFieldEnd(addr, " 0");
       addr = addr.replace("HAYES S/D RD", "HAYES SUBDIVISION RD");
       return addr;
-    }
-  }
-  
-  private static final Pattern TIME_PTN = Pattern.compile("(\\d\\d)(\\d\\d)(\\d\\d)");
-  private class MyTimeField extends TimeField {
-    @Override
-    public void parse(String field, Data data) {
-      Matcher match = TIME_PTN.matcher(field);
-      if (!match.matches()) abort();
-      data.strTime = match.group(1) + ':' + match.group(2) + ':' + match.group(3);
     }
   }
 }
