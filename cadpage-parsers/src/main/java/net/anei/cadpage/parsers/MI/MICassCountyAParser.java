9e0f3db3e9f52c623c6a1885ff8e08e41498b1f0
Ken Corbin <ken@cadpage.org>
2024-03-17 12:08:32 -0700

    Fixed parsing problem with Cass County, MI (A)

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/MI/MICassCountyAParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/MI/MICassCountyAParser.java
@@ -1,15 +1,15 @@
 package net.anei.cadpage.parsers.MI;
 
-import net.anei.cadpage.parsers.dispatch.DispatchA1Parser;
+import net.anei.cadpage.parsers.dispatch.DispatchA24Parser;
 
-public class MICassCountyAParser extends DispatchA1Parser {
-  
+public class MICassCountyAParser extends DispatchA24Parser {
+
   public MICassCountyAParser() {
     super("CASS COUNTY", "MI");
   }
-  
+
   @Override
   public String getFilter() {
-    return "Dispatcher@cassco.org,911_Copier@cassco.org,cpritchard@alertpss.com,dispatcher@porterfire.org,forwarding-noreply@google.com,riprun@cassco.org";
+    return "@traumasoft.com";
   }
 }
