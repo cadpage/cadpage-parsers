164ab4c023cc2469a55876246ea6eb06220146a3
Ken Corbin <ken@cadpage.org>
2024-03-22 12:23:59 -0700

    Fixed parsing problem with Harford County, MD

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/MD/MDHarfordCountyCParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/MD/MDHarfordCountyCParser.java
@@ -1,5 +1,6 @@
 package net.anei.cadpage.parsers.MD;
 
+import net.anei.cadpage.parsers.CodeSet;
 import net.anei.cadpage.parsers.MsgInfo.Data;
 import net.anei.cadpage.parsers.dispatch.DispatchA48Parser;
 
@@ -7,6 +8,8 @@ public class MDHarfordCountyCParser extends DispatchA48Parser {
 
   public MDHarfordCountyCParser() {
     super(MDHarfordCountyParser.CITY_LIST, "HARFORD COUNTY", "MD", FieldType.PLACE, A48_NO_CODE);
+    setupCallList(CALL_LIST);
+    setupMultiWordStreets(MWORD_STREET_LIST);
   }
 
   @Override
@@ -23,5 +26,37 @@ public class MDHarfordCountyCParser extends DispatchA48Parser {
     }
     return true;
   }
+
+  private static final String[] MWORD_STREET_LIST = new String[] {
+      "AYRES CHAPEL",
+      "DEEP RUN",
+      "EAGLES GROVE",
+      "EMORY CHURCH",
+      "FAWN GROVE",
+      "GRIER NURSERY",
+      "SAINT ANNE"
+  };
+
+  private static final CodeSet CALL_LIST = new CodeSet(
+      "BRUSH/WOODS/2 BRUSH",
+      "BRUSH/WOODS FIRE",
+      "BUILDING FIRE",
+      "CARDIAC ARREST ALS",
+      "CHIMNEY FIRE",
+      "FALL BLS",
+      "MOTOR VEH COLLISION ALS",
+      "MOTOR VEH COLLISION BLS",
+      "MOTOR VEH COLL/RESCUE ALS",
+      "MVC RESCUE/MULT PT",
+      "RESID STRUCT FIRE",
+      "SEIZURE BLS",
+      "SICK PERSON ALS",
+      "SICK PERSON BLS",
+      "SMALL STRUCTURE FIRE",
+      "STROKE ALS",
+      "TEST INCIDENT",
+      "TRAUMA BLS",
+      "UNCONSCIOUS ALS"
+  );
 
 }
