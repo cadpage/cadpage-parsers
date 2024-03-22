e21fb32a26931c9f72db0bc6430d8793ac19eaac
Ken Corbin <ken@cadpage.org>
2024-03-22 16:15:09 -0700

    Update sender filter for Umatilla County, OR (B)

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/OR/ORUmatillaCountyBParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/OR/ORUmatillaCountyBParser.java
@@ -6,23 +6,23 @@ import net.anei.cadpage.parsers.HtmlProgramParser;
 import net.anei.cadpage.parsers.MsgInfo.Data;
 
 public class ORUmatillaCountyBParser extends HtmlProgramParser {
-  
+
   public ORUmatillaCountyBParser() {
-    super("UMATILLA COUNTY", "OR", 
+    super("UMATILLA COUNTY", "OR",
           "CALL_DATE_/_TIME:DATETIME! FIRE_CALL_TYPE:CALL! EMS_CALL_TYPE:CALL! COMMON_NAME:PLACE! ADDRESS:ADDRCITY/S6! APT/SUITE:APT! CROSS_STREET:X! FIRE_QUAD:MAP! NATURE_OF_CALL:CALL/SDS! DISPATCH_COMMENTS:EMPTY! INFO/N+ LOCAL_INFO:INFO/N! INFO/N+ ALERTS:INFO/N! INFO/N+ INCIDENT_#:ID! UNITS:UNIT! STATUS_TIMES:EMPTY! SKIP+ CALLER_NAME:NAME! CALLER_PHONE_#:PHONE! CALLER_ADDRESS:SKIP!");
   }
-  
+
   @Override
   public String getFilter() {
-    return "@milton-freewater-or.gov";
+    return "@milton-freewater-or.gov,RipnRunReport@wallawallawa.gov";
   }
-  
+
   @Override
   protected boolean parseHtmlMsg(String subject, String body, Data data) {
     if (!subject.startsWith("Automatic R&R Notification:")) return false;
     return super.parseHtmlMsg(subject, body, data);
   }
-  
+
   @Override
   public Field getField(String name) {
     if (name.equals("DATETIME")) return new DateTimeField("\\d\\d?/\\d\\d?/\\d{4} \\d\\d?:\\d\\d:\\d\\d", true);
@@ -30,7 +30,7 @@ public class ORUmatillaCountyBParser extends HtmlProgramParser {
     if (name.equals("INFO")) return new MyInfoField();
     return super.getField(name);
   }
-  
+
   private class MyAptField extends AptField {
     @Override
     public void parse(String field, Data data) {
@@ -38,12 +38,12 @@ public class ORUmatillaCountyBParser extends HtmlProgramParser {
       super.parse(field,  data);;
     }
   }
-  
+
   private static final Pattern INFO_HEADER_PTN = Pattern.compile("\\*{3}\\d\\d?/\\d\\d?/\\d{4}\\*{3}|\\d\\d?:\\d\\d:\\d\\d");
   private class MyInfoField extends InfoField {
-    
+
     private boolean suppressHeader = false;
-    
+
     @Override
     public void parse(String field, Data data) {
       if (!suppressHeader) {
@@ -55,7 +55,7 @@ public class ORUmatillaCountyBParser extends HtmlProgramParser {
         if (field.equals("-")) suppressHeader = false;
         return;
       }
-      
+
       data.strSupp = append(data.strSupp, "\n", field);
     }
   }
