eadf958cdac690b8c50ed934ed020c441f4a2e2e
Ken Corbin <ken@cadpage.org>
2024-03-28 18:35:28 -0700

    Fixed mapping problem with Benton County, AR (I)

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/AR/ARBentonCountyIParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/AR/ARBentonCountyIParser.java
@@ -1,5 +1,9 @@
 package net.anei.cadpage.parsers.AR;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 import net.anei.cadpage.parsers.MsgInfo.Data;
 import net.anei.cadpage.parsers.dispatch.DispatchA71Parser;
 
@@ -12,12 +16,21 @@ public class ARBentonCountyIParser extends DispatchA71Parser {
   @Override
   protected boolean parseMsg(String subject, String body, Data data) {
     if (subject.equals("Active 911")) return false;
-    return super.parseMsg(subject, body, data);
+    if (!super.parseMsg(subject, body, data)) return false;
+    if (OK_CITY_SET.contains(data.strCity)) data.strState = "OK";
+    return true;
+  }
+
+  @Override
+  public String getProgram() {
+    return super.getProgram().replace("CITY", "CITY ST");
   }
 
   @Override
   public int getMapFlags( ) {
     return MAP_FLG_PREFER_GPS;
   }
+
+  private static final Set<String> OK_CITY_SET = new HashSet<>(Arrays.asList("COLCORD", "WATTS"));
 
 }
