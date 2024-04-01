55abe35fbd306ce571abe93c1bc42ce2e7bf33eb
Ken Corbin <ken@cadpage.org>
2024-04-01 11:43:10 -0700

    Fixed parsin gproblem with Archuleta County, CO

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/CO/COArchuletaCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/CO/COArchuletaCountyParser.java
@@ -1,8 +1,6 @@
 package net.anei.cadpage.parsers.CO;
 
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import net.anei.cadpage.parsers.FieldProgramParser;
@@ -12,60 +10,49 @@ public class COArchuletaCountyParser extends FieldProgramParser {
 
   public COArchuletaCountyParser() {
     super("ARCHULETA COUNTY", "CO",
-          "GPS1 GPS2 CALL ADDR ID INFO! INFO+");
+          "ID DATETIME SRC ADDR CITY ST ZIP PRI CODE CALL GPS1 GPS2! INFO/N+");
   }
 
   @Override
   public String getFilter() {
-    return "directorac911@gmail.com,ArchuletaCountyCAD@outlook.com,noreply@echoitconsult.com";
+    return "noreply@archuletacounty.org";
   }
 
   @Override
-  protected boolean parseMsg(String body, Data data) {
-    return parseFields(body.split("\\^", -1), data);
+  public int getMapFlags() {
+    return MAP_FLG_PREFER_GPS;
+  }
+
+  @Override
+  protected boolean parseMsg(String subject, String body, Data data) {
+    if (!subject.startsWith("Active911: ")) return false;
+    return parseFields(body.split(";", -1), data);
+  }
+
+  @Override
+  protected boolean parseFields(String[] flds, Data data) {
+    for (int ii = 0; ii < flds.length; ii++) {
+      if (flds[ii].trim().equals("null")) flds[ii] = "";
+    }
+    return super.parseFields(flds, data);
   }
 
   @Override
   public Field getField(String name) {
-    if (name.equals("ADDR")) return new MyAddressField();
-    if (name.equals("ID")) return new IdField("#([A-Z]{0,3}\\d\\d-\\d{5,6})", true);
+    if (name.equals("ID")) return new IdField("\\d{8}-\\d{5}|", true);
+    if (name.equals("DATETIME")) return new MyDateTimeField();
+    if (name.equals("PRI")) return new PriorityField("\\d", true);
     return super.getField(name);
   }
 
-  private static final Pattern ADDR_ST_PTN = Pattern.compile("[A-Z]{2}");
-  private class MyAddressField extends AddressField {
+  private static final Pattern DATE_TIME_PTN = Pattern.compile("(\\d{4})-(\\d\\d)-(\\d\\d)T(\\d\\d:\\d\\d:\\d\\d)\\.\\d+Z");
+  private class MyDateTimeField extends DateTimeField {
     @Override
     public void parse(String field, Data data) {
-      if (GPS_PATTERN.matcher(field).matches()) {
-        data.strAddress = field;
-        return;
-      }
-      Parser p = new Parser(field);
-      String city = p.getLastOptional(',');
-      if (ADDR_ST_PTN.matcher(city).matches()) {
-        data.strState = city;
-        city = p.getLastOptional(',');
-      }
-      city = city.toUpperCase();
-      if (COUNTY_NAME_SET.contains(city)) city += " COUNTY";
-      data.strCity = city;
-      super.parse(p.get(), data);
-    }
-
-    @Override
-    public String getFieldNames() {
-      return super.getFieldNames() + " CITY ST";
+      Matcher match = DATE_TIME_PTN.matcher(field);
+      if (!match.matches()) abort();
+      data.strDate = match.group(2)+'/'+match.group(3)+'/'+match.group(1);
+      data.strTime = match.group(4);
     }
   }
-
-  private static final Set<String> COUNTY_NAME_SET = new HashSet<String>(Arrays.asList(
-      "ARCHULETA",
-      "CONEJOS",
-      "HINSDALE",
-      "LA PLATA",
-      "MINERAL",
-      "RIO ARRIBA",
-      "RIO GRANDE",
-      "SAN JUAN"
-  ));
 }
