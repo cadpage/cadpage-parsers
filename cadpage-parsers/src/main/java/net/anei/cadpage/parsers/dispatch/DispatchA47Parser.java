08de17a3a349a5d3e8d8693822b56609d1878e8f
Ken Corbin <ken@cadpage.org>
2024-04-09 12:29:01 -0700

    Fixed text problems

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/dispatch/DispatchA47Parser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/dispatch/DispatchA47Parser.java
@@ -20,7 +20,7 @@ public class DispatchA47Parser extends FieldProgramParser {
   public DispatchA47Parser(String subjectMarker, String[] cityList, String defCity, String defState, String unitPtn) {
     super(cityList, defCity, defState,
           "( SELECT/NEW Reported:DATETIME! ( Priority:PRI! | Priorities:PRI1! ) CFS:ID! Type:CALL! Loc:ADDRCITYST/S! Cross:X! Prem:PLACE! ( RP:NAME! RP_Cell:PHONE! | ) " +
-                "Units:UNIT! ProQA:INFO INFO/N+ Call_Details:INFO/N INFO/N+ " +
+                "Units:UNIT! ProQA:INFO? INFO/N+ Call_Details:INFO/N? INFO/N+ " +
           "| ( Reported:DATETIME! ID_CALL! Loc:ADDR/S! | ID_CALL! Reported:DATETIME? ADDR/S! ) X? ( PLACE2 UNITQ | UNIT2 | PLACE2 END | ) " +
           ") INFO/N+");
     this.subjectMarker = subjectMarker;
