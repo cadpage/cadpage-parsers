bc242fdbd2789210066c32c1d9ef957a98b0436c
Ken Corbin <ken@cadpage.org>
2024-04-14 14:38:48 -0700

    Fixed parsing problem with Adams County, PA (D)

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/PA/PAAdamsCountyDParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/PA/PAAdamsCountyDParser.java
@@ -1,5 +1,8 @@
 package net.anei.cadpage.parsers.PA;
 
+import java.util.regex.Pattern;
+
+import net.anei.cadpage.parsers.MsgInfo.Data;
 import net.anei.cadpage.parsers.dispatch.DispatchA57Parser;
 
 public class PAAdamsCountyDParser extends DispatchA57Parser {
@@ -11,5 +14,31 @@ public class PAAdamsCountyDParser extends DispatchA57Parser {
   @Override
   public String getFilter() {
     return "CAD@adams911.com";
+  }
+
+  private static final Pattern NATURE_OF_CALL_PTN = Pattern.compile("N ?a ?t ?u ?r ?e  ?o ?f  ?C ?a ?l ?l ?:");
+
+  @Override
+  protected boolean parseMsg(String body, Data data) {
+
+    // Try to unscramble this mess ...
+    if (body.length() < 65) return false;
+    if (body.charAt(64) == ' ') {
+      int len = body.length();
+      body = NATURE_OF_CALL_PTN.matcher(body).replaceFirst("Nature of Call:");
+      if (body.length() != len) {
+        body = stripBlank(body, 193);
+        body = stripBlank(body, 127);
+      }
+    }
+    body = body.replace("0A", "\n");
+    return super.parseMsg(body, data);
+  }
+
+  private String stripBlank(String body, int pos) {
+    if (body.length() > pos && body.charAt(pos) == ' ') {
+      body = body.substring(0, pos) + body.substring(pos+1);
+    }
+    return body;
   }
 }
