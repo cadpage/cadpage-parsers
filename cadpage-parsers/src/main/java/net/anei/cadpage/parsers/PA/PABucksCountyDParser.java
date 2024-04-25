9c656b666b2d73d2e336141178d33e1f3c6e6aa8
Ken Corbin <ken@cadpage.org>
2024-04-25 10:03:14 -0700

    Fixed parsing probem with Bucks County, PA (D)

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/PA/PABucksCountyDParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/PA/PABucksCountyDParser.java
@@ -1,5 +1,8 @@
 package net.anei.cadpage.parsers.PA;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import net.anei.cadpage.parsers.FieldProgramParser;
 import net.anei.cadpage.parsers.MsgInfo.Data;
 import net.anei.cadpage.parsers.MsgInfo.MsgType;
@@ -8,10 +11,12 @@ public class PABucksCountyDParser extends FieldProgramParser {
 
   public PABucksCountyDParser() {
     super("BUCKS COUNTY", "PA",
-          "CALL! Inc#:ID! ( Box:BOX! Alrm_Lvl:PRI! ( Place_Name:PLACE! Addr:ADDR! | Addr:ADDR! Place_Name:PLACE! ) btwn:X! Pri_Remarks:INFO ( Call_Text:INFO/N! | Text:INFO/N! ) Channel:CH! ( Units:UNIT! | Run:UNIT! ) GPS! TIME! Caller:NAME Phone:PHONE Alt:PHONE/L " +
-                         "| Units:UNIT! Place_Name:PLACE Addr:ADDR! btwn:X! GPS! ( Box:BOX! | FIRE_Box:BOX! EMS_Box:BOX/L! ) ( Pri_Remarks:PRI! | PRI_Remarks:PRI! ) Call_Text:INFO! Caller:NAME! Phone:PHONE! Alt_Ph:PHONE/L! Alrm_Lvl:PRI! Channel:CH! ( Time_Out:TIME! | TIME! ) G:SKIP D:SKIP " +
-                         "| Sector:MAP! Place_Name:PLACE! Addr:ADDR! btwn:X! Pri_Remarks:PRI! Call_Text:INFO! GPS! TIME! Units:UNIT! Caller:NAME Phone:PHONE Alt:PHONE/L " +
-                         ") END ");
+          "CALL! ( Inc#:ID! ( Box:BOX! Alrm_Lvl:PRI! ( Place_Name:PLACE! Addr:ADDR! | Addr:ADDR! Place_Name:PLACE! ) btwn:X! Pri_Remarks:INFO ( Call_Text:INFO/N! | Text:INFO/N! ) Channel:CH! ( Units:UNIT! | Run:UNIT! ) GPS! TIME! Caller:NAME Phone:PHONE Alt:PHONE/L " +
+                           "| Units:UNIT! Place_Name:PLACE Addr:ADDR! btwn:X! GPS! ( Box:BOX! | FIRE_Box:BOX! EMS_Box:BOX/L! | Fire_Box#:BOX! EMS_Box#:BOX/L! | Fire_Box:BOX! EMS_Box:BOX/L! | Sector:MAP! ) ( Pri_Remarks:PRI! | PRI_Remarks:PRI! ) Call_Text:INFO! ( Caller:NAME! | Caller_Name:NAME! Caller_Adr:SKIP! Caller_Apt#:SKIP! ) Phone:PHONE! Alt_Ph:PHONE/L! Alrm_Lvl:PRI! Channel:CH! ( Time_Out:TIME! | TIME! ) G:SKIP D:SKIP " +
+                           "| Sector:MAP! Place_Name:PLACE! Addr:ADDR! btwn:X! Pri_Remarks:PRI! Call_Text:INFO! GPS! TIME! Units:UNIT! Caller:NAME Phone:PHONE Alt:PHONE/L " +
+                           ") END " +
+                "| Box:BOX! ADDR! btwn:X! GPS! Text:INFO! TIME_ID! Run:UNIT! " +
+                ")");
   }
 
   @Override
@@ -59,6 +64,7 @@ public class PABucksCountyDParser extends FieldProgramParser {
   @Override
   public Field getField(String name) {
     if (name.equals("TIME")) return new TimeField("\\d\\d:\\d\\d:\\d\\d", true);
+    if (name.equals("TIME_ID"))  return new MyTimeIdField();
     if (name.equals("PHONE")) return new MyPhoneField();
     return super.getField(name);
   }
@@ -68,6 +74,22 @@ public class PABucksCountyDParser extends FieldProgramParser {
     public void parse(String field, Data data) {
       if (field.equals("-")) return;
       super.parse(field, data);
+    }
+  }
+
+  private static Pattern TIME_ID_PTN = Pattern.compile("(\\d\\d?:\\d\\d:\\d\\d) +(.*)");
+  private class MyTimeIdField extends Field {
+    @Override
+    public void parse(String field, Data data) {
+      Matcher match = TIME_ID_PTN.matcher(field);
+      if (!match.matches()) abort();
+      data.strTime = match.group(1);
+      data.strCallId = match.group(2);
+    }
+
+    @Override
+    public String getFieldNames() {
+      return "TIME ID";
     }
   }
 }
