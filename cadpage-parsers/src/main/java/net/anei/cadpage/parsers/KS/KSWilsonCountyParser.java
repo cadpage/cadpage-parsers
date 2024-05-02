0e76666f11717687fbca0e0a5c6b55c4aea2333c
Ken Corbin <ken@cadpage.org>
2024-05-02 09:50:06 -0700

    Update sender filter for Wilson County, KS

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/KS/KSWilsonCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/KS/KSWilsonCountyParser.java
@@ -10,7 +10,7 @@ public class KSWilsonCountyParser extends DispatchA25Parser {
 
   @Override
   public String getFilter() {
-    return "wilco911@twinmounds.com";
+    return "wilco911@twinmounds.com,dispatch@wilsoncountykansas.org";
   }
 
 }
