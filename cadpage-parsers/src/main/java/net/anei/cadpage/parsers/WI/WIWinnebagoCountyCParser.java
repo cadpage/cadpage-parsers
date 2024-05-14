1399372b5d396b76cbdddd61c677c7a18d9f095a
Ken Corbin <ken@cadpage.org>
2024-05-13 20:34:37 -0700

    Fixed parsing problem with Winnebago County, WI (C)

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/WI/WIWinnebagoCountyCParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/WI/WIWinnebagoCountyCParser.java
@@ -1,6 +1,5 @@
 package net.anei.cadpage.parsers.WI;
 
-import net.anei.cadpage.parsers.MsgInfo.Data;
 import net.anei.cadpage.parsers.dispatch.DispatchH03Parser;
 
 public class WIWinnebagoCountyCParser extends DispatchH03Parser {
@@ -13,11 +12,4 @@ public class WIWinnebagoCountyCParser extends DispatchH03Parser {
   public String getFilter() {
     return ".WI@co.winnebago.wi.us";
   }
-
-  @Override
-  protected boolean parseHtmlMsg(String subject, String body, Data data) {
-    if (!subject.endsWith(" Active911 Incident") && !subject.endsWith(" Active911 Notification")) return false;
-    return super.parseHtmlMsg(subject, body, data);
-  }
-
 }
