cfb41ee8e1c6fbe78b9b9c435202b8ebaca1ca16
Ken Corbin <ken@cadpage.org>
2024-05-13 21:11:26 -0700

    Fixed parsing problem with Richland County, OH

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/OH/OHRichlandCountyAParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/OH/OHRichlandCountyAParser.java
@@ -1,24 +1,33 @@
 package net.anei.cadpage.parsers.OH;
 
-import java.util.Properties;
+import net.anei.cadpage.parsers.MsgInfo.Data;
+import net.anei.cadpage.parsers.dispatch.DispatchH05Parser;
 
-import net.anei.cadpage.parsers.dispatch.DispatchH02Parser;
+public class OHRichlandCountyAParser extends DispatchH05Parser {
 
-public class OHRichlandCountyAParser extends DispatchH02Parser {
-  
   public OHRichlandCountyAParser() {
-    super(CITY_CODES, "RICHLAND COUNTY", "OH");
+    super("RICHLAND COUNTY", "OH",
+          "Call_Number:SKIP! Call_Date/Time:DATETIME! Address:ADDRCITY! Units:UNIT! " +
+              "Status_Times:EMPTY! TIMES+ Narrative:EMPTY! INFO_BLK+ Incident_Number:ID! Latitude/Longitude:GPS!");
   }
-  
+
   @Override
   public String getFilter() {
-    return "911-DISPATCHERS@RICHLANDCOUNTYOH.US";
+    return "richlandcounty911@richlandcountyoh.gov";
   }
-  
-  private static final Properties CITY_CODES = buildCodeTable(new String[]{
-      "BEL", "BELLVILLE",
-      "LUC", "LUCAS",
-      "MANS", "MANSFIELD",
-      "PERR", "PERRY"
-  });
+
+  @Override
+  public Field getField(String name) {
+    if (name.equals("DATETIME")) return new DateTimeField("\\d\\d?/\\d\\d?/\\d{4} \\d\\d?:\\d\\d:\\d\\d", true);
+    if (name.equals("GPS")) return new MyGPSField();
+    return super.getField(name);
+  }
+
+  private class MyGPSField extends GPSField {
+    @Override
+    public void parse(String field, Data data) {
+      field = field.replace("-", ",-");
+      super.parse(field, data);
+    }
+  }
 }
