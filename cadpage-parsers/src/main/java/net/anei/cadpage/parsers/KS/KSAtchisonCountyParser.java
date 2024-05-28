5a450151af9fc109896c9339aae53419f2532de4
Ken Corbin <ken@cadpage.org>
2024-05-28 13:11:13 -0700

    Fixed parsing problem with Atchison County, KS

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/KS/KSAtchisonCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/KS/KSAtchisonCountyParser.java
@@ -1,18 +1,58 @@
 package net.anei.cadpage.parsers.KS;
 
-import net.anei.cadpage.parsers.dispatch.DispatchA25Parser;
+import java.util.regex.Pattern;
 
+import net.anei.cadpage.parsers.FieldProgramParser;
+import net.anei.cadpage.parsers.MsgInfo.Data;
 
-public class KSAtchisonCountyParser extends DispatchA25Parser {
-  
+public class KSAtchisonCountyParser extends FieldProgramParser {
+
   public KSAtchisonCountyParser() {
-    super("ATCHISON COUNTY", "KS");
+    super("ATCHISON COUNTY", "KS",
+          "Call_number:ID! Call_type:CALL! Time:DATETIME! Address:ADDRCITYST! Location_Details:INFO! Location_Name:PLACE! Map:GPS! Details:INFO! Agencies:UNIT! END");
   }
-  
+
   @Override
   public String getFilter() {
-    return "EnterpolAlerts@atchisonlec.org";
+    return "no-reply@atcosheriffks.org";
   }
 
-  
+  @Override
+  public int getMapFlags() {
+    return MAP_FLG_PREFER_GPS;
+  }
+
+  @Override
+  protected boolean parseMsg(String subject, String body, Data data) {
+    if (!subject.equals("Please Respond")) return false;
+    return super.parseMsg(body, data);
+  }
+
+  @Override
+  public Field getField(String name) {
+    if (name.equals("DATETIME")) return new DateTimeField("\\d\\d/\\d\\d/\\d\\d \\d\\d:\\d\\d", true);
+    if (name.equals("INFO")) return new MyInfoField();
+    if (name.equals("UNIT")) return new MyUnitField();
+    return super.getField(name);
+  }
+
+  private static final Pattern INFO_BRK_PTN = Pattern.compile("[; ]*\\b\\d\\d/\\d\\d/\\d\\d \\d\\d:\\d\\d:\\d\\d - *");
+
+  private class MyInfoField extends InfoField {
+    @Override
+    public void parse(String field, Data data) {
+      if (field.equals("None")) return;
+      for (String line : INFO_BRK_PTN.split(field)) {
+        data.strSupp = append(data.strSupp, "\n", line);
+      }
+    }
+  }
+
+  private class MyUnitField extends UnitField {
+    @Override
+    public void parse(String field, Data data) {
+      field = field.replace("; ", ",");
+      super.parse(field, data);
+    }
+  }
 }
