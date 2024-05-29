4db454fb0461e746689d13497ca6781725c04bea
Ken Corbin <ken@cadpage.org>
2024-05-28 21:45:58 -0700

    Fixed parsing problem with Jasper County, GA

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/GA/GAJasperCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/GA/GAJasperCountyParser.java
@@ -1,100 +1,50 @@
 package net.anei.cadpage.parsers.GA;
 
-import net.anei.cadpage.parsers.CodeSet;
-import net.anei.cadpage.parsers.MsgInfo.Data;
-import net.anei.cadpage.parsers.dispatch.DispatchB2Parser;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-public class GAJasperCountyParser extends DispatchB2Parser {
+import net.anei.cadpage.parsers.MsgInfo.Data;
+import net.anei.cadpage.parsers.MsgParser;
+
+public class GAJasperCountyParser extends MsgParser {
 
   public GAJasperCountyParser() {
-    super("JASPERCO911:", null, "JASPER COUNTY", "GA", B2_FORCE_CALL_CODE | B2_CROSS_FOLLOWS);
-    setupCallList(CALL_LIST);
-    setupMultiWordStreets(
-        "ALCOVY SHORES",
-        "BALL GROUND",
-        "BEAR CREEK",
-        "BECOMES POST",
-        "BETHEL CHURCH",
-        "BLUE HERON",
-        "CONCORD CHURCH",
-        "COUNTY LINE",
-        "DAVIS FORD",
-        "HENDERSON MILL",
-        "HERRING GULL",
-        "JACKSON LAKE",
-        "LAKE SHORE",
-        "LIBERTY CHURCH",
-        "MCELHENEY STILES",
-        "MIDWAY CHURCH",
-        "MISS SARAH",
-        "MOURNING DOVE",
-        "PITTS CHAPEL E POST",
-        "ROCK EAGLE",
-        "SMITH BROCK",
-        "STAG RUN",
-        "WHIP POOR WILL WENDY HILL",
-        "WILD TURKEY"
-    );
-    setupSpecialStreets("BEAR CREEK PT");
+    super("JASPER COUNTY", "GA");
+    setFieldList("CALL ID ADDR APT CITY ST INFO");
   }
-  
+
   @Override
-  public String getProgram() {
-    return super.getProgram().replace("NAME", "INFO"); 
+  public String getFilter() {
+    return "911paging@jasperema911ga.com";
   }
-  
+
+  private static final Pattern INFO_BRK_PTN = Pattern.compile("[; ]*\\b\\d\\d/\\d\\d/\\d\\d \\d\\d:\\d\\d:\\d\\d - *");
+  private static final Pattern ID_ADDR_PTN = Pattern.compile("(CFS\\d{4}-\\d{8}) +([^,]*?)(?:, *([-A-Z ]+)(?:, *([A-Z]{2})(?: +\\d{5})?)?)?");
+
   @Override
-  protected boolean parseAddrField(String field, Data data) {
-    field = field.replace('@',  '/');
-    if (!super.parseAddrField(field, data)) return false;
-    
-    // They do not use the normal name/phone convention.  If we find a
-    // parsed name, it really should be something else
-    if (data.strName.length() > 0) {
-      if (data.strName.startsWith("/")) {
-        String saveCross = data.strCross;
-        data.strCross = "";
-        parseAddress(StartType.START_ADDR, FLAG_ONLY_CROSS, data.strName.substring(1).trim(), data);
-        if (saveCross.length() == 0) {
-          data.strAddress = append(data.strAddress, " & ", data.strCross);
-          data.strCross = "";
-        } else {
-          data.strCross = append(saveCross, " / ", data.strCross);
-        }
-        data.strSupp = getLeft();
-      } else {
-        data.strSupp = data.strName;
-      }
-      data.strName = "";
+  protected boolean parseMsg(String subject, String body, Data data) {
+
+    if (subject.isEmpty()) return false;
+    if (!body.startsWith(subject+' ')) return false;
+    data.strCall = subject;
+    body = body.substring(subject.length()+1).trim();
+    body = stripFieldEnd(body, " None");
+    body = stripFieldEnd(body, " 0.0");
+
+    String[] parts = INFO_BRK_PTN.split(body);
+    if (parts.length < 2) return false;
+
+    Matcher match = ID_ADDR_PTN.matcher(parts[0]);
+    if (!match.matches()) return false;
+
+    data.strCallId = match.group(1);
+    parseAddress(match.group(2).trim(), data);
+    data.strCity = getOptGroup(match.group(3));
+    data.strState = getOptGroup(match.group(4));
+
+    for (int ii = 1; ii<parts.length; ii++) {
+      data.strSupp = append(data.strSupp, "\n", parts[ii]);
     }
     return true;
   }
-
-  private static final CodeSet CALL_LIST = new CodeSet(
-      "ACCIDENT W/POSSIBLE INJURIES",
-      "AUTO ACCIDENT/FATALITY",
-      "BLEEDING,HEMORI,HEMORH",
-      "CARDIAC ARREST / HEART ATTACK",
-      "CHEST PAIN",
-      "CVA/STROKE",
-      "EXPLOSION",
-      "FALL",
-      "FIRE",
-      "FIRE ALARM",
-      "FIRE/OTHER",
-      "FIRE / BRUSH",
-      "FIRE / STRUCTURE",
-      "FIRE/UNKNOWN",
-      "FIRE/VEHICLE",
-      "HIT AND RUN",
-      "LACERATION",
-      "LIFT ASSIST/ASSIST INVALID",
-      "MISSING PERSON",
-      "OVERDOSE",
-      "PREGNANCY",
-      "SHORT/BREATH/ASTHMA",
-      "UNCONSCIOUS",
-      "VEHICLE ACCIDENT/ENTRAPMENT",
-      "VEHICLE ACC W/INJURIES"
-  );
 }
