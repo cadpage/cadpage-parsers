2a14992078bb4ac68ca0bc55e580f9330dfeb2cb
Ken Corbin <ken@cadpage.org>
2024-06-04 11:13:17 -0700

    Fixed parsing problem with Washington County, AR (B)

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/AR/ARWashingtonCountyBParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/AR/ARWashingtonCountyBParser.java
@@ -1,74 +1,78 @@
 package net.anei.cadpage.parsers.AR;
 
-import net.anei.cadpage.parsers.FieldProgramParser;
-import net.anei.cadpage.parsers.MsgInfo.Data;
+import net.anei.cadpage.parsers.MsgParser;
 
-public class ARWashingtonCountyBParser extends FieldProgramParser {
-  
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import net.anei.cadpage.parsers.MsgInfo.Data;
+import net.anei.cadpage.parsers.MsgInfo.MsgType;
+
+public class ARWashingtonCountyBParser extends MsgParser {
+
   public ARWashingtonCountyBParser() {
     this("WASHINGTON COUNTY", "AR");
   }
-  
+
   protected ARWashingtonCountyBParser(String defCity, String defState) {
-    super(defCity, defState,
-          "CALL:CALL! PLACE:PLACE! ADDR:ADDR! APT:APT! City:CITY! TIME:TIME! CROSSSTREET:X! NOTE:INFO! LAT:GPS1! LON:GPS2!");
+    super(defCity, defState);
   }
-  
+
   @Override
   public String getAliasCode() {
     return "ARWashingtonCountyB";
   }
-  
+
   @Override
   public String getFilter() {
-    return "noreplyActive911@centralems.org";
+    return "tritech@centralems.org";
   }
-  
+
   @Override
   public int getMapFlags() {
     return MAP_FLG_PREFER_GPS;
   }
-  
+
+  private static final Pattern MASTER = Pattern.compile("(?:(\\d{6}-\\d{6}) +(?:SPACE)?)?Call Information: *(\\S+) +response reference (.*?) +at +(.*?) +Ops Channel *(.*?) +Time (\\d\\d:\\d\\d)Lat/Long: *(\\d\\d)(\\d{6}) +(\\d\\d)(\\d{6}) +Location Name: *(.*?) +City: *(.*?) +(.*)");
+  private static final Pattern GEN_ALERT_MASTER = Pattern.compile("(\\d{6}-\\d{6}) +,(\\S+) +,(?:True|False) \\d+\\) \\d\\d/\\d\\d/\\d{4} \\d\\d:\\d\\d:\\d\\d-\\[\\d+\\] +(.*)");
+
   @Override
   protected boolean parseMsg(String subject, String body, Data data) {
-    
-    if (!subject.equals("Active 911 Alert")) return false;
 
-    return parseFields(body.split("\n"), data);
+    if (!subject.equals("Inform CAD Paging")) return false;
+
+    Matcher match = MASTER.matcher(body);
+    if (match.matches()) {
+      setFieldList("ID PRI CALL ADDR APT CH TIME GPS PLACE CITY UNIT");
+      data.strCallId = getOptGroup(match.group(1));
+      data.strPriority = match.group(2);
+      data.strCall = match.group(3);
+      parseAddress(match.group(4), data);
+      data.strChannel = match.group(5);
+      data.strTime = match.group(6);
+      setGPSLoc(match.group(7)+'.'+match.group(8)+','+match.group(9)+'.'+match.group(10), data);
+      data.strPlace = match.group(11);
+      data.strCity = match.group(12);
+      data.strUnit = match.group(13);
+      return true;
+    }
+
+    match = GEN_ALERT_MASTER.matcher(body);
+    if (match.matches()) {
+      setFieldList("ID PRI INFO");
+      data.msgType = MsgType.GEN_ALERT;
+      data.strCallId = match.group(1);
+      data.strPriority =  match.group(2);
+      data.strSupp = match.group(3);
+      return true;
+    }
+    return false;
   }
-  
+
+  private static final Pattern WC_NN_PTN = Pattern.compile("\\bWC \\d+\\b", Pattern.CASE_INSENSITIVE);
+
   @Override
-  public Field getField(String name) {
-    if (name.equals("CALL")) return new MyCallField();
-    if (name.equals("X")) return new MyCrossField();
-    return super.getField(name);
-  }
-  
-  private class MyCallField extends Field {
-
-    @Override
-    public void parse(String field, Data data) {
-      String[] parts = field.split(" - ", 3);
-      if (parts.length == 1) {
-        data.strCall = field;
-      } else {
-        data.strCode = parts[0].trim();
-        data.strCall = parts[parts.length-1].trim();
-      }
-    }
-
-    @Override
-    public String getFieldNames() {
-      return "CODE CALL";
-    }
-  }
-  
-  private class MyCrossField extends CrossField {
-    @Override
-    public void parse(String field, Data data) {
-      for (String part : field.split(";")) {
-        data.strCross = append(data.strCross, " / ", part);
-      }
-    }
+  public String adjustMapAddress(String addr) {
+    return WC_NN_PTN.matcher(addr).replaceAll("").trim();
   }
 }
