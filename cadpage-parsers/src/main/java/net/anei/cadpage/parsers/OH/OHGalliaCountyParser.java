aaafb2f50e63a4f93083721db3ef76331b18f563
Ken Corbin <ken@cadpage.org>
2024-06-10 20:12:41 -0700

    Fixed parsing problem with Gallia County, OH

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/OH/OHGalliaCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/OH/OHGalliaCountyParser.java
@@ -1,96 +1,17 @@
 package net.anei.cadpage.parsers.OH;
 
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
-
-import net.anei.cadpage.parsers.MsgInfo.Data;
-import net.anei.cadpage.parsers.dispatch.DispatchEmergitechParser;
+import net.anei.cadpage.parsers.dispatch.DispatchA19Parser;
 
 
 
-public class OHGalliaCountyParser extends DispatchEmergitechParser {
-  
+public class OHGalliaCountyParser extends DispatchA19Parser {
+
   public OHGalliaCountyParser() {
-    super("Gallia911:", 60, CITY_LIST, "GALLIA COUNTY", "OH");
-    addSpecialWords("LINCOLN");
+    super("GALLIA COUNTY", "OH");
   }
 
   @Override
-  protected boolean parseMsg(String body, Data data) {
-    if (!super.parseMsg(body, data)) return false;
-    
-    // If there is no city, see if we can find it in the info section
-    if (data.strCity.length() == 0) {
-      Result res = parseAddress(StartType.START_ADDR, FLAG_ONLY_CITY, data.strSupp);
-      if (res.isValid()) res.getData(data);
-    }
-    
-    // If city ends with " CO", expand it
-    if (data.strCity.endsWith(" CO")) data.strCity += "UNTY";
-    
-    // ANd finally, see if this is in West Virginia
-    if (WV_CITY_TABLE.contains(data.strCity)) data.strState = "WV";
-    
-    // See if there is a callback phone number in the APT field
-    if (data.strApt.startsWith("CALLBK=")) {
-      data.strPhone = data.strApt.substring(7).trim();
-      data.strApt = "";
-    }
-    
-    return true;
+  public String getFilter() {
+    return "motorolaflex@galliacounty911.org";
   }
-  
-  @Override
-  public String getProgram() {
-    return super.getProgram().replace("CITY", "CITY ST").replace("APT", "APT PHONE");
-  }
-
-  private static final String[] CITY_LIST = new String[]{
-    // Villages
-    "CENTERVILLE",
-    "CHESHIRE",
-    "CROWN CITY",
-    "GALLIPOLIS",
-    "RIO GRANDE",
-    "VINTON",
-
-    // Townships
-    "ADDISON TWP",
-    "CHESHIRE TWP",
-    "CLAY TWP",
-    "GALLIPOLIS TWP",
-    "GREEN TWP",
-    "GREENFIELD TWP",
-    "GUYAN TWP",
-    "HARRISON TWP",
-    "HUNTINGTON TWP",
-    "MORGAN TWP",
-    "OHIO TWP",
-    "PERRY TWP",
-    "RACCOON TWP",
-    "SPRINGFIELD TWP",
-    "WALNUT TWP",
-
-    // Unincorporated communities
-    "BIDWELL",
-    "KERR",
-    "PATRIOT",
-    
-    // Jackson County
-    "JACKSON CO",
-    "JACKSON COUNTY",
-    
-    // Mason County, WV
-    "MASON CO",
-    "MASON COUNTY",
-    "GALLIPOLIS FERRY",
-    "POINT PLEASANT"
-  };
-  
-  private static Set<String> WV_CITY_TABLE = new HashSet<String>(Arrays.asList(
-      "GALLIPOLIS FERRY",
-      "MASON COUNTY",
-      "POINT PLEASANT"
-  ));
 }
