0b13ef85d333aef52a5d1c4af07a62ac0cfba782
Ken Corbin <ken@cadpage.org>
2024-06-26 19:55:32 -0700

    Fixed parsing problem with Fond D Lac County, WI

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/WI/WIFondDuLacCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/WI/WIFondDuLacCountyParser.java
@@ -1,35 +1,24 @@
 package net.anei.cadpage.parsers.WI;
 
-import java.util.Properties;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import net.anei.cadpage.parsers.MsgInfo.Data;
-import net.anei.cadpage.parsers.dispatch.DispatchA41Parser;
+import net.anei.cadpage.parsers.dispatch.DispatchA19Parser;
 
-public class WIFondDuLacCountyParser extends DispatchA41Parser {
+public class WIFondDuLacCountyParser extends DispatchA19Parser {
 
   public WIFondDuLacCountyParser() {
-    super(CITY_CODES, "FOND DU LAC COUNTY", "WI", "F[AFPS]|S[EFO]");
+    super("FOND DU LAC COUNTY", "WI");
   }
 
   @Override
   public String getFilter() {
-    return "cadmail@fdlco.wi.gov";
+    return "fdlco@fdlco.wi.gov";
   }
 
   @Override
-  protected boolean parseMsg(String body, Data data) {
-    if (body.startsWith("CALLALERT:")) body = "DISPATCH:" + body.substring(10);
-    if (!super.parseMsg(body, data)) return false;
-    data.strSource = data.strChannel;
-    data.strChannel = "";
-    return true;
-  }
-
-  @Override
-  public String getProgram() {
-    return super.getProgram().replace("CH", "SRC");
+  public int getMapFlags() {
+    return MAP_FLG_PREFER_GPS;
   }
 
   private static final Pattern DIR_NUMBER_ADDR = Pattern.compile("([NSEW])(\\d+ .*)");
@@ -51,33 +40,4 @@ public class WIFondDuLacCountyParser extends DispatchA41Parser {
     if (saveDir != null) addr = saveDir + addr;
     return super.postAdjustMapAddress(addr);
   }
-
-  private static final Properties CITY_CODES = buildCodeTable(new String[]{
-      "BRND", "BRANDON",
-      "BRV",  "BROWNSVILLE",
-      "BYRN", "BYRON",
-      "CAMP", "CAMPBELLSPORT",
-      "CASC", "CASCADE",
-      "CHIL", "CHILTON",
-      "EDEN", "EDEN",
-      "ELD",  "ELDORADO",
-      "FAIR", "FAIRWATER",
-      "FDL",  "FOND DU LAC",
-      "GLEN", "GLENBEULAH",
-      "KEW",  "KEWASKUM",
-      "LOM",  "LOMIRE",
-      "MAL",  "MALONE",
-      "MARK", "MARKESAN",
-      "MTCA", "MT CALVARY",
-      "NFDL", "N FOND DU LAC",
-      "NHOL", "NEW HOLSTEIN",
-      "OAK",  "OAKFIELD",
-      "OSH",  "OSHKOSH",
-      "PICK", "PICKETT",
-      "RIP",  "RIPON",
-      "ROS",  "ROSENDALE",
-      "STC",  "ST CLOUD",
-      "VDYN", "VAN DYNE",
-      "WPN",  "WAUPUN"
-  });
 }
