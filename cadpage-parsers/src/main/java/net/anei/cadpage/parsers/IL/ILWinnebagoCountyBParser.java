818dd790ce7db8ff7595a819e5fa5b99331fac30
Ken Corbin <ken@cadpage.org>
2024-07-08 12:57:31 -0700

    Fixed parsing problem with Winnebago County, IL (B)

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/IL/ILWinnebagoCountyBParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/IL/ILWinnebagoCountyBParser.java
@@ -2,6 +2,7 @@ package net.anei.cadpage.parsers.IL;
 
 import net.anei.cadpage.parsers.MsgInfo.Data;
 
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import net.anei.cadpage.parsers.SmartAddressParser;
@@ -10,7 +11,6 @@ public class ILWinnebagoCountyBParser extends SmartAddressParser {
 
   public ILWinnebagoCountyBParser() {
     super(CITY_LIST, "WINNEBAGO COUNTY", "IL");
-    setFieldList("ADDR APT CITY PLACE CALL");
   }
 
   @Override
@@ -18,18 +18,42 @@ public class ILWinnebagoCountyBParser extends SmartAddressParser {
     return "DoNotReply@rockfordil.gov";
   }
 
-  private static final Pattern DELIM = Pattern.compile(" +\\*(?=\\S)");
+  private static final Pattern MASTER1 = Pattern.compile("(.+?) (HR\\d\\d-\\d{6}) (.+?)(?: ([A-Z][A-Z0-9]*\\dS?))?");
+  private static final Pattern MASTER2 = Pattern.compile("(.+?) (HR\\d\\d-\\d{5,6})(\\d\\d/\\d\\d/\\d{4})([A-Z][A-Z0-9]*\\dS?|) (\\[1\\].*)");
+  private static final Pattern INFO_BRK_PTN = Pattern.compile(" *, *(?=\\[\\d{1,2}\\])");
 
   @Override
   protected boolean parseMsg(String subject, String body, Data data) {
     if (!subject.equals("CAD Alert")) return false;
-    String[] parts = DELIM.split(body);
-    if (parts.length != 2) return false;
-    data.strCall = parts[1];
-    parseAddress(StartType.START_ADDR, parts[0], data);
-    if (data.strCity.isEmpty()) return false;
-    data.strPlace = getLeft();
-    return true;
+    Matcher match = MASTER1.matcher(body);
+    if (match.matches()) {
+      setFieldList("CALL ID ADDR APT CITY UNIT");
+      data.strCall = match.group(1).trim();
+      data.strCallId = match.group(2);
+      parseAddress(StartType.START_ADDR, FLAG_ANCHOR_END, match.group(3).trim(), data);
+      if (data.strCity.isEmpty()) return false;
+      data.strUnit = getOptGroup(match.group(4));
+      return true;
+    }
+
+    match = MASTER2.matcher(body);
+    if (match.matches()) {
+      setFieldList("CALL ADDR APT CITY ID DATE UNIT INFO");
+      parseAddress(StartType.START_CALL, FLAG_ANCHOR_END, match.group(1).trim(), data);
+      if (data.strCity.isEmpty()) return false;
+      data.strCallId = match.group(2);
+      data.strDate =  match.group(3);
+      data.strUnit = match.group(4);
+      String info = match.group(5);
+      for (String line : INFO_BRK_PTN.split(info)) {
+        line = stripFieldEnd(line, ",");
+        line = stripFieldEnd(line, "[Shared]");
+        data.strSupp = append(data.strSupp, "\n", line);
+      }
+      return true;
+    }
+
+    return false;
   }
 
   private static final String[] CITY_LIST = new String[] {
@@ -81,7 +105,11 @@ public class ILWinnebagoCountyBParser extends SmartAddressParser {
       "WINNEBAGO",
 
       // Former Settlement
-      "CAMP GRANT"
+      "CAMP GRANT",
+
+      // Counties
+      "BOONE COUNTY",
+      "WINNEBAGO COUNTY"
   };
 
 }
