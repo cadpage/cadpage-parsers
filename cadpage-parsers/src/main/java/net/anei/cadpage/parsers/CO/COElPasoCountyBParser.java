457063357131b8083038caa0288043616e2e2c49
Ken Corbin <ken@cadpage.org>
2024-08-26 16:28:36 -0700

    Fixed parsing problem with El Paso County, CO (B)

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/CO/COElPasoCountyBParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/CO/COElPasoCountyBParser.java
@@ -1,5 +1,8 @@
 package net.anei.cadpage.parsers.CO;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import net.anei.cadpage.parsers.FieldProgramParser;
 import net.anei.cadpage.parsers.MsgInfo.Data;
 
@@ -7,24 +10,74 @@ public class COElPasoCountyBParser extends FieldProgramParser {
 
   public COElPasoCountyBParser() {
     super("EL PASO COUNTY", "CO",
-          "ADDRCITY X? UNIT CALL CH? DATETIME! END");
+          "CODE_UNIT ADDR PLACE? ( MAP_TIME_ID! | MAP CH? CALL ID TIME! ) END");
   }
 
   @Override
   public String getFilter() {
-    return "no_reply@coloradosprings.gov,CAD@eptpaging.info";
+    return "CAD@eptpaging.info";
   }
 
   @Override
   protected boolean parseMsg(String subject, String body, Data data) {
-    if (!subject.equals("CAD Page")) return false;
     return super.parseFields(body.split("\n"), data);
   }
 
   @Override
   public Field getField(String name) {
-    if (name.equals("X")) return new CrossField(".*/.*|", true);
-    if (name.equals("DATETIME")) return new DateTimeField("\\d\\d?/\\d\\d?/\\d{4} \\d\\d:\\d\\d:\\d\\d", true);
+    if (name.equals("CODE_UNIT")) return new MyCodeUnitField();
+    if (name.equals("MAP_TIME_ID")) return new MyMapTimeIdField();
+    if (name.equals("MAP")) return new MapField("Map +(.+)", true);
+    if (name.equals("CH")) return new ChannelField("Radio +(.+)", true);
+    if (name.equals("ID")) return new IdField("Report +(.+)", true);
+    if (name.equals("TIME")) return new TimeField("Time +(\\d\\d:\\d\\d:\\d\\d)", true);
     return super.getField(name);
+  }
+
+  private static final Pattern CODE_UNIT_PTN = Pattern.compile("([A-Z0-9 ]+?) +([A-Z0-9,]+)");
+  private class MyCodeUnitField extends Field {
+    @Override
+    public void parse(String field, Data data) {
+      Matcher match = CODE_UNIT_PTN.matcher(field);
+      if (!match.matches()) abort();
+      data.strCode = match.group(1);
+      data.strUnit =  match.group(2);
+    }
+
+    @Override
+    public String getFieldNames() {
+      return "CALL? CODE UNIT";
+    }
+  }
+
+  private static final Pattern MAP_TIME_ID_PTN = Pattern.compile("(?:Map +(\\S+) +)?(\\d\\d:\\d\\d:\\d\\d)(?: +Call +(\\S+))?");
+  private class MyMapTimeIdField extends Field {
+    @Override
+    public boolean canFail() {
+      return true;
+    }
+
+    @Override
+    public boolean checkParse(String field, Data data) {
+      Matcher match = MAP_TIME_ID_PTN.matcher(field);
+      if (!match.matches()) return false;
+      data.strMap = getOptGroup(match.group(1));
+      data.strTime = match.group(2);
+      data.strCallId = getOptGroup(match.group(3));
+      data.strCall = data.strCode;
+      data.strCode = "";
+      return true;
+    }
+
+    @Override
+    public void parse(String field, Data data) {
+      if (!checkParse(field, data)) abort();
+    }
+
+    @Override
+    public String getFieldNames() {
+      return "MAP TIME ID";
+    }
+
   }
 }
