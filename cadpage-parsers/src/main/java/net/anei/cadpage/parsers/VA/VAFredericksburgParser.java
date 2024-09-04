fdef9fbb0d089091ea4927be4a6631f612c67e97
Ken Corbin <ken@cadpage.org>
2024-09-04 15:13:09 -0700

    Fixed parsing problem with Fredericksburg, VA

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/VA/VAFredericksburgParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/VA/VAFredericksburgParser.java
@@ -1,18 +1,52 @@
 package net.anei.cadpage.parsers.VA;
 
+import java.util.Properties;
+import java.util.regex.Pattern;
+
+import net.anei.cadpage.parsers.MsgInfo.Data;
 import net.anei.cadpage.parsers.dispatch.DispatchOSSIParser;
 
 public class VAFredericksburgParser extends DispatchOSSIParser {
-  
+
   public VAFredericksburgParser() {
-    super("FREDERICKSBURG", "VA", 
-          "( CANCEL ADDR! SKIP INFO+ | FYI? ( ADDR/Z ID UNIT? CALL2! X+? INFO+ | PLACE ADDR/Z ID UNIT? CALL2! X+? INFO+ | PLACE? CALL ADDR/Z END ) )");
+    super(CITY_CODES, "FREDERICKSBURG", "VA",
+          "( CANCEL ADDR! CITY INFO+ " +
+          "| FYI? ADDR PLACE? CITY GPS1 GPS2! CALL2 ID2 UNIT? X X END " +
+          ")");
   }
-  
+
+  @Override
+  public int getMapFlags() {
+    return MAP_FLG_PREFER_GPS;
+  }
+
+  @Override
+  protected boolean parseMsg(String body, Data data) {
+    if (!body.startsWith("CAD:")) body = "CAD:" + body;
+    return super.parseMsg(body, data);
+  }
+
   @Override
   public Field getField(String name) {
-    if (name.equals("ID")) return new IdField("\\d{10}", true);
+    if (name.equals("ID2")) return new IdField("\\d+");
     if (name.equals("CALL2")) return new CallField("[A-Z]+", true);
+    if (name.equals("GPS1")) return new MyGPSField(1);
+    if (name.equals("GPS2")) return new MyGPSField(2);
+    if (name.equals("UNIT")) return new UnitField("[A-Z]+\\d+|MEDI", true);
     return super.getField(name);
   }
+
+  private static final Pattern GPS_PTN = Pattern.compile("[-+]?\\d{2,3}\\.\\d{6,}");
+  private class MyGPSField extends GPSField {
+    public MyGPSField(int type) {
+      super(type);
+      setPattern(GPS_PTN, true);
+    }
+  }
+
+  private static Properties CITY_CODES = buildCodeTable(new String[] {
+      "FRE",  "FREDERICKSBURG",
+      "MAF",  "FREDERICKSBURG",
+      "SPO",  "FREDERICKSBURG"
+  });
 }
