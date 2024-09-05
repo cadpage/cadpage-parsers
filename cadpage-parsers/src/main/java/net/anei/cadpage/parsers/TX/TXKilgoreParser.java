8105fa1b257ecd51442fb2f5d0650641b637034d
Ken Corbin <ken@cadpage.org>
2024-09-04 17:59:22 -0700

    Turn on GPS trust flag for Kilgore, TXx

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/TX/TXKilgoreParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/TX/TXKilgoreParser.java
@@ -5,13 +5,18 @@ import net.anei.cadpage.parsers.dispatch.DispatchA19Parser;
  * Kilgore, TX
  */
 public class TXKilgoreParser extends DispatchA19Parser {
- 
+
   public TXKilgoreParser() {
     super("KILGORE", "TX");
   }
-  
+
   @Override
   public String getFilter() {
     return "kfddispatch@cityofkilgore.com";
+  }
+
+  @Override
+  public int getMapFlags() {
+    return MAP_FLG_PREFER_GPS | MAP_FLG_SUPPR_LA;
   }
 }
