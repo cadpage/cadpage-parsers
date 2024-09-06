5d56942f81469453fe97046d736137f8de4a955b
Ken Corbin <ken@cadpage.org>
2024-09-05 17:04:49 -0700

    Fixed parsing problem with Brusnwick County, VA

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/VA/VABrunswickCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/VA/VABrunswickCountyParser.java
@@ -8,16 +8,17 @@ import net.anei.cadpage.parsers.dispatch.DispatchOSSIParser;
 
 
 public class VABrunswickCountyParser extends DispatchOSSIParser {
-    
-  
+
+
   public VABrunswickCountyParser() {
     super(CITY_CODES, "BRUNSWICK COUNTY", "VA",
           "( CANCEL ADDR CITY! INFO/N+ " +
-          "| FYI ( ID DATETIME CALL PLACE? ADDR/Z CITY/Y! X/Z+? ( UNIT END | END ) " +
-                "| DATETIME ID CALL UNIT PLACE? ADDR/Z CITY/Y! X/+? NAME PHONE " +
-                "| CALL ADDR CITY/Y! INFO/N+ ) )");
+          "| FYI? ( ID DATETIME CALL PLACE? ADDR/Z CITY/Y! X/Z+? ( UNIT END | END ) " +
+                 "| DATETIME ID CALL UNIT PLACE? ADDR/Z CITY/Y! X/+? NAME PHONE " +
+                 "| CALL ADDR CITY/Y! INFO/N+ ) )");
+    setupCityValues(CITY_CODES);
   }
-  
+
   @Override
   public String getFilter() {
     return "CAD@brunswickso.org";
@@ -25,21 +26,20 @@ public class VABrunswickCountyParser extends DispatchOSSIParser {
 
   @Override
   protected boolean parseMsg(String subject, String body, Data data) {
-    if (!subject.equals("Text Message")) return false;
     if (!body.startsWith("CAD:")) body = "CAD:" +  body;
     return super.parseMsg(body, data);
   }
-  
+
   @Override
   public Field getField(String name) {
-    if (name.equals("ID")) return new IdField("\\d{8}", true);
+    if (name.equals("ID")) return new IdField("\\d{5,}", true);
     if (name.equals("DATETIME")) return new DateTimeField("\\d\\d?/\\d\\d/\\d{4} \\d\\d:\\d\\d:\\d\\d", true);
     if (name.equals("SRC")) return new SourceField("DOLPHIN VOLUNTEER FIRE DEPARME|", true);
     if (name.equals("UNIT")) return new UnitField("(?:\\b(?:\\d{1,4}[A-Z]*|[A-Z]{4}|[A-Z]{1,2}\\d+|FRSTRY)\\b *)+");
     if (name.equals("INFO")) return new MyInfoField();
     return super.getField(name);
   }
-  
+
   private class MyInfoField extends InfoField {
     @Override
     public void parse(String field, Data data) {
@@ -47,7 +47,7 @@ public class VABrunswickCountyParser extends DispatchOSSIParser {
       super.parse(field, data);
     }
   }
-  
+
   private static final Properties CITY_CODES = buildCodeTable(new String[]{
       "ALBE",   "ALBERTA",
       "BLAC",   "BLACKSTONE",
