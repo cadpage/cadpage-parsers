bb00ce78f50f470940109dcd33793929a430d8e3
Ken Corbin <ken@cadpage.org>
2024-09-09 19:42:51 -0700

    Fixed parsing problem with Jefferson County, TN

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/TN/TNJeffersonCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/TN/TNJeffersonCountyParser.java
@@ -7,27 +7,27 @@ import net.anei.cadpage.parsers.dispatch.DispatchSouthernParser;
 
 
 public class TNJeffersonCountyParser extends DispatchSouthernParser {
-  
-  
+
+
   public TNJeffersonCountyParser() {
-    super(CITY_LIST, "JEFFERSON COUNTY", "TN", 
-          DSFLG_OPT_DISP_ID|DSFLG_ADDR|DSFLG_ADDR_TRAIL_PLACE2|DSFLG_OPT_ID|DSFLG_TIME);
+    super(CITY_LIST, "JEFFERSON COUNTY", "TN",
+          DSFLG_OPT_DISP_ID|DSFLG_ADDR|DSFLG_ADDR_TRAIL_PLACE2|DSFLG_OPT_ID|DSFLG_OPT_TIME);
     removeWords("CIRCLE", "COURT", "COVE", "PLACE", "ROAD", "TERRACE");
   }
-  
+
   @Override
   public String getFilter() {
     return "DISPATCH@jeffersoncountytn911.org,911@jeffersoncountytn911.org";
   }
-  
+
   @Override
   public Field getField(String name) {
     if (name.equals("ADDR")) return new MyAddressField();
     return super.getField(name);
   }
-  
+
   private static final Pattern HIGHWAY_25_PTN = Pattern.compile("\\b(HIGHWAY|HWY|US|RT) 25 (32|70)\\b");
-  
+
   private class MyAddressField extends BaseAddressField {
     @Override
     public void parse(String field, Data data) {
@@ -52,10 +52,10 @@ public class TNJeffersonCountyParser extends DispatchSouthernParser {
     "STRAWBERRY PLAINS",
     "TALBOTT",
     "WHITE PINE",
-    
+
     // Hamblen County
     "MORRISTOWN",
-    
+
     // Sevior County
     "GATLINBURG",
     "PITTMAN CENTER"
