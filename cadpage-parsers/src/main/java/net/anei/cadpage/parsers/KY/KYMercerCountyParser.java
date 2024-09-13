4d081e3a43a12cda7a0a5833fe31bdbc59cfbe88
Ken Corbin <ken@cadpage.org>
2024-09-12 18:13:32 -0700

    Updated sender filter for Mercer County, KY

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/KY/KYMercerCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/KY/KYMercerCountyParser.java
@@ -10,7 +10,7 @@ public class KYMercerCountyParser extends DispatchA24Parser {
 
   @Override
   public String getFilter() {
-    return "paging@10-8systems.com";
+    return "paging@10-8systems.com,noreply@10-8systems.com";
   }
 
 }
