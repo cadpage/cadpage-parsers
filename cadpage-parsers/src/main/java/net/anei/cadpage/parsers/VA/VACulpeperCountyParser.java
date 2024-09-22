af3939d746ae3a484eb5205a0ff4ca5509132742
Ken Corbin <ken@cadpage.org>
2024-09-22 10:04:23 -0700

    Fixed parsing problem with Culpeper County, VA

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/VA/VACulpeperCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/VA/VACulpeperCountyParser.java
@@ -8,12 +8,12 @@ import net.anei.cadpage.parsers.dispatch.DispatchOSSIParser;
 
 
 public class VACulpeperCountyParser extends DispatchOSSIParser {
-  
+
   public VACulpeperCountyParser() {
     super("CULPEPER COUNTY", "VA",
            "( CANCEL ADDR | FYI? BOX CH? CALL ADDR MAP? ) INFO/N+");
   }
-  
+
   @Override
   public String getFilter() {
     return "CAD@culpepercounty.gov";
@@ -21,12 +21,12 @@ public class VACulpeperCountyParser extends DispatchOSSIParser {
 
   @Override
   protected Field getField(String name) {
-    if (name.equals("BOX"))  return new BoxField("\\d{4}", true);
+    if (name.equals("BOX"))  return new BoxField("(?:Text Message)?(\\d{4})", true);
     if (name.equals("CH")) return new ChannelField("\\d+[- ]?[A-Z]", true);
-    if (name.equals("MAP")) return new MyMapField(); 
+    if (name.equals("MAP")) return new MyMapField();
     return super.getField(name);
   }
-  
+
   // And we need a special MAP field that will append two map data fields
   private static final Pattern MAP_PTN = Pattern.compile("(?:(.*?)[/ ]+?)?(\\d{1,2}-?[A-Z]\\d)(?: - MAP PAGE)?");
   private static final Pattern SPECIAL_MAP_PTN = Pattern.compile("(?:(.*?)[ /]+)?(?:([A-Z]+) POST OFFICE MAP|MAP PAGE|MP)");
@@ -35,14 +35,14 @@ public class VACulpeperCountyParser extends DispatchOSSIParser {
     public boolean canFail() {
       return true;
     }
-    
+
     @Override
     public boolean checkParse(String field, Data data) {
       Matcher match = MAP_PTN.matcher(field);
       if (!match.matches()) return false;
       String place = getOptGroup(match.group(1));
       data.strMap = match.group(2);
-      
+
       match = SPECIAL_MAP_PTN.matcher(place);
       if (match.matches()) {
         data.strPlace = getOptGroup(match.group(1));
@@ -52,12 +52,12 @@ public class VACulpeperCountyParser extends DispatchOSSIParser {
       }
       return true;
     }
-    
+
     @Override
     public void parse(String field, Data data) {
       if (!checkParse(field, data)) abort();
     }
-    
+
     @Override
     public String getFieldNames() {
       return "PLACE CITY MAP";
