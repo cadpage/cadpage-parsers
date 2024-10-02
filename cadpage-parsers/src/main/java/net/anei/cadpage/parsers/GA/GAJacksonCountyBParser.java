9eaa31683ba34036fd1718d1f6423dc0196723b9
Ken Corbin <ken@cadpage.org>
2024-10-02 16:34:43 -0700

    Fixed parsing problem with Jackson County, GA (B)

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/GA/GAJacksonCountyBParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/GA/GAJacksonCountyBParser.java
@@ -5,12 +5,14 @@ import java.util.regex.Pattern;
 
 import net.anei.cadpage.parsers.FieldProgramParser;
 import net.anei.cadpage.parsers.MsgInfo.Data;
+import net.anei.cadpage.parsers.SplitMsgOptions;
+import net.anei.cadpage.parsers.SplitMsgOptionsCustom;
 
 public class GAJacksonCountyBParser extends FieldProgramParser {
 
   public GAJacksonCountyBParser() {
     super("JACKSON COUNTY", "GA",
-          "CALL ADDRCITY PHONE PLACE! ID INFO/N+");
+          "DATETIME NAME_PHONE CODE_CALL ADDRCITY PLACE! INFO/N+? ID END");
   }
 
   @Override
@@ -18,29 +20,76 @@ public class GAJacksonCountyBParser extends FieldProgramParser {
     return "cadpage@jacksoncountygov.com";
   }
 
-  private static final Pattern MARKER = Pattern.compile("CAD Page:(\\d\\d?/\\d\\d?/\\d{4}) (\\d\\d:\\d\\d:\\d\\d) {3,}");
-  private static final Pattern DELIM = Pattern.compile(" /+|\n");
+  @Override
+  public SplitMsgOptions getActive911SplitMsgOptions() {
+    return new SplitMsgOptionsCustom();
+  }
+
+  private static final Pattern DELIM = Pattern.compile(" /|\n");
 
   @Override
   protected boolean parseMsg(String subject, String body, Data data) {
 
-    if (!subject.equals("Text Message")) return false;
-
-    Matcher match = MARKER.matcher(body);
-    if (!match.lookingAt()) return false;
-    data.strDate = match.group(1);
-    data.strTime = match.group(2);
+    if (subject.equals("Text Message") && body.startsWith("CAD Page:")) {
+      body = body.substring(9).trim();
+    } else if (!subject.equals("CAD Page")) return false;
     return parseFields(DELIM.split(body), data);
   }
 
   @Override
-  public String getProgram() {
-    return "DATE TIME " + super.getProgram();
+  public Field getField(String name) {
+    if (name.equals("DATETIME")) return new DateTimeField("\\d\\d?/\\d\\d?/\\d{4} +\\d\\d:\\d\\d:\\d\\d", true);
+    if (name.equals("NAME_PHONE")) return new MyNamePhoneField();
+    if (name.equals("CODE_CALL")) return new MyCodeCallField();
+    if (name.equals("PLACE")) return new MyPlaceField();
+    if (name.equals("ID")) return new IdField("\\d{4}-\\d{8}\\b.*", true);
+    return super.getField(name);
   }
 
-  @Override
-  public Field getField(String name) {
-    if (name.equals("PHONE")) return new PhoneField("\\d{7,10}|", true);
-    return super.getField(name);
+  private static final Pattern NAME_PHONE_PTN = Pattern.compile("(.*?) +(\\d{10})");
+  private class MyNamePhoneField extends Field {
+    @Override
+    public void parse(String field, Data data) {
+      Matcher match = NAME_PHONE_PTN.matcher(field);
+      if (match.matches()) {
+        field = match.group(1);
+        data.strPhone = match.group(2);
+      }
+      data.strName = field;
+    }
+
+    @Override
+    public String getFieldNames() {
+      return "NAME PHONE";
+    }
+  }
+
+  private class MyCodeCallField extends Field {
+    @Override
+    public void parse(String field, Data data) {
+      int pt = field.indexOf("/ ");
+      if (pt < 0) abort();
+      data.strCode = field.substring(0,pt).trim();
+      data.strCall = field.substring(pt+2).trim();
+    }
+
+    @Override
+    public String getFieldNames() {
+      return "CODE CALL";
+    }
+  }
+
+  private class MyPlaceField extends PlaceField {
+    @Override
+    public void parse(String field, Data data) {
+      if (field.endsWith("/")) {
+        data.strPlace = field.substring(0, field.length()-1).trim();
+      } else {
+        int pt = field.indexOf("/ ");
+        if (pt < 0) abort();
+        data.strPlace = field.substring(0, pt).trim();
+        data.strSupp = field.substring(pt+2).trim();
+      }
+    }
   }
 }
