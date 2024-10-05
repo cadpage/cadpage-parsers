0731ff99334bd650adf1577d1ff6a71088be825f
Ken Corbin <ken@cadpage.org>
2024-10-05 10:04:52 -0700

    Update sender filter for Dallas County, TX (D)

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/TX/TXDallasCountyEParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/TX/TXDallasCountyEParser.java
@@ -3,16 +3,16 @@ package net.anei.cadpage.parsers.TX;
 import net.anei.cadpage.parsers.dispatch.DispatchA27Parser;
 
 public class TXDallasCountyEParser extends DispatchA27Parser {
-  
+
   public TXDallasCountyEParser() {
     super("DALLAS COUNTY", "TX");
   }
-  
+
   @Override
   public String getFilter() {
-    return "rfd@cor.gov";
+    return "rfd@cor.gov,richardsonpdtx@cissystem.com";
   }
-  
+
   @Override
   public int getMapFlags() {
     return MAP_FLG_PREFER_GPS | MAP_FLG_SUPPR_LA;
