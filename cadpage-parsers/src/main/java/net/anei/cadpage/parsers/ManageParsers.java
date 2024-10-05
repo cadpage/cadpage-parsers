82c9811e49a2c2d2521c77e006002a546db6f67e
Ken Corbin <ken@cadpage.org>
2024-10-05 10:02:04 -0700

    Fixed test problems

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/ManageParsers.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/ManageParsers.java
@@ -221,7 +221,7 @@ public class ManageParsers {
         "ARPulaskiCountyB",   "General",
         "OHClarkCountyA",     "General",                 // 01/02/2024
         "OHClarkCountyB",     "General",
-        "OHClarkCountyC",     "General",
+        "OHClarkCountyD",     "General",
         "OHSummitCountyF",    "General",                 // 01/08/2024
         "PALackawannaCountyA","General",                 // 01/29/2024
         "NCHarnettCountyB",   "General",                 // 02/16/2024
