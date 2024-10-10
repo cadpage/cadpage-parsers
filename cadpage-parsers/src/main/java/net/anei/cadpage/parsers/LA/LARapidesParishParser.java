5cc3c55c2a69290d84b3d943a29d99b9d94acaad
Ken Corbin <ken@cadpage.org>
2024-10-10 14:22:58 -0700

    Update sender filter for Rapides Parish, LA

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/LA/LARapidesParishParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/LA/LARapidesParishParser.java
@@ -7,39 +7,39 @@ import net.anei.cadpage.parsers.FieldProgramParser;
 import net.anei.cadpage.parsers.MsgInfo.Data;
 
 public class LARapidesParishParser extends FieldProgramParser {
-  
+
   public LARapidesParishParser() {
-    super("RAPIDES PARISH", "LA", 
+    super("RAPIDES PARISH", "LA",
           "Addr:ADDR! Cross:X! Inc_Type:CALL! REMARKS! INFO/N+");
   }
-  
+
   @Override
   public String getFilter() {
-    return "mailout@kbpobox.com";
+    return "mailout@kbpobox.com,cad@rapides911.org";
   }
-  
+
   private static final Pattern MARKER = Pattern.compile("(\\d\\d?/\\d\\d/\\d{4}) : (\\d\\d:\\d\\d) +");
   private static final Pattern DELIM = Pattern.compile("\n:?");
-  
+
   @Override
   protected boolean parseMsg(String subject, String body, Data data) {
-    
+
     if (!subject.equals("CAD Alert Recieved")) return false;
-    
+
     Matcher match = MARKER.matcher(body);
     if (!match.lookingAt()) return false;
     data.strDate = match.group(1);
     data.strTime = match.group(2);
     body = body.substring(match.end());
-    
+
     return parseFields(DELIM.split(body), data);
   }
-  
+
   @Override
   public String getProgram() {
     return "DATE TIME " + super.getProgram();
   }
-  
+
   @Override
   public Field getField(String name) {
     if (name.equals("REMARKS")) return new InfoField("Remarks\\b *(.*)");
