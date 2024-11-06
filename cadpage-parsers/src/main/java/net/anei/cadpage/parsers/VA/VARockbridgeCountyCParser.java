4408edc57954aed1d8ceff1a9f2cbae505498627
Ken Corbin <ken@cadpage.org>
2024-11-05 18:08:48 -0800

    Fixed parsing problem with Rockbriddge County, VA (C)

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/VA/VARockbridgeCountyCParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/VA/VARockbridgeCountyCParser.java
@@ -1,49 +1,10 @@
 package net.anei.cadpage.parsers.VA;
 
-import net.anei.cadpage.parsers.FieldProgramParser;
-import net.anei.cadpage.parsers.MsgInfo.Data;
+import net.anei.cadpage.parsers.dispatch.DispatchA71Parser;
 
-public class VARockbridgeCountyCParser extends FieldProgramParser {
+public class VARockbridgeCountyCParser extends DispatchA71Parser {
 
   public VARockbridgeCountyCParser() {
-    super("ROCKBRIDGE COUNTY", "VA",
-          "DATETIME! CFS_Number:SKIP! Nature_of_Call:CALL! Address:ADDRCITY! Narrative:INFO! INFO/N+ Additional_Location_Info:PLACE! " +
-              "Assigned_Units:UNIT! Radio_Channel:CH! Incident_#:ID! Closest_Intersection:X! Call_Type:CALL/SDS! END");
+    super("ROCKBRIDGE COUNTY", "VA");
   }
-
-  @Override
-  public String getFilter() {
-    return "dispatch@lexingtonva.gov";
-  }
-
-  @Override
-  protected boolean parseMsg(String subject, String body, Data data) {
-    if (!subject.equals("!")) return false;
-    return parseFields(body.split("\n"), data);
-  }
-
-  @Override
-  public Field getField(String name) {
-    if (name.equals("DATETIME")) return new DateTimeField("\\d\\d?/\\d\\d?/\\d{4} \\d\\d:\\d\\d:\\d\\d", true);
-    if (name.equals("ADDRCITY")) return new MyAddressCityField();
-    return super.getField(name);
-  }
-
-  private class MyAddressCityField extends AddressCityField {
-    @Override
-    public void parse(String field, Data data) {
-      int pt = field.lastIndexOf(':');
-      if (pt >= 0) {
-        data.strPlace = stripFieldStart(field.substring(pt+1).trim(), "-");
-        field = field.substring(0,pt).trim();
-      }
-      super.parse(field, data);
-    }
-
-    @Override
-    public String getFieldNames() {
-      return super.getFieldNames() + " PLACE";
-    }
-  }
-
 }
