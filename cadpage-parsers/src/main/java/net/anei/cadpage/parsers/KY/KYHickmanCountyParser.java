ff2ea399a65ce042777591e5fb0ef11fab48856b
Ken Corbin <ken@cadpage.org>
2024-11-08 20:19:22 -0800

    Fixed parsing problem with Hickman COunty, KY

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/KY/KYHickmanCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/KY/KYHickmanCountyParser.java
@@ -1,23 +1,65 @@
 package net.anei.cadpage.parsers.KY;
 
-import net.anei.cadpage.parsers.MsgInfo.Data;
-import net.anei.cadpage.parsers.dispatch.DispatchA48Parser;
+import java.util.regex.Pattern;
 
-public class KYHickmanCountyParser extends DispatchA48Parser {
+import net.anei.cadpage.parsers.FieldProgramParser;
+import net.anei.cadpage.parsers.MsgInfo.Data;
+
+public class KYHickmanCountyParser extends FieldProgramParser {
 
   public KYHickmanCountyParser() {
-    super(CITY_LIST, "HICKMAN COUNTY", "KY", FieldType.PLACE, A48_OPT_CODE);
+    super("HICKMAN COUNTY", "KY",
+          "ID PRI CALL PLACE+? ADDRCITYST/Z UNIT EMPTY! INFO/N+");
   }
 
   @Override
-  protected boolean parseMsg(String subject, String body, Data data) {
-    return super.parseMsg(subject, ": " + body, data);
+  public String getFilter() {
+    return "paging@10-8systems.com";
   }
 
-  private static final String[] CITY_LIST = new String[] {
-      "CLINTON",
-      "COLUMBUS",
-      "HICKMAN"
-  };
+  private static final Pattern DELIM = Pattern.compile(" /(?= )");
 
+  @Override
+  protected boolean parseMsg(String subject, String body, Data data) {
+    if (!subject.equals("NEW DISPATCH")) return false;
+    return parseFields(DELIM.split(body), data);
+  }
+
+  @Override
+  public Field getField(String name) {
+    if (name.equals("ID")) return new IdField("INC +(.*)", true);
+    if (name.equals("PRI")) return new PriorityField("Pri (\\d)", true);
+    if (name.equals("PLACE")) return new MyPlaceField();
+    if (name.equals("ADDRCITYST")) return new MyAddressCityStateField();
+    if (name.equals("INFO")) return new MyInfoField();
+    return super.getField(name);
+  }
+
+  private class MyPlaceField extends PlaceField {
+    @Override
+    public void parse(String field, Data data) {
+      if (getRelativeField(+1).startsWith(field)) return;
+      super.parse(field, data);
+    }
+  }
+
+  private class MyAddressCityStateField extends AddressCityStateField {
+
+    @Override
+    public void parse(String field, Data data) {
+      field = stripFieldEnd(field, ", USA");
+      super.parse(field, data);
+    }
+  }
+
+  private static final Pattern MSPACE_PTN = Pattern.compile(" {3,}");
+  private class MyInfoField extends InfoField {
+    @Override
+    public void parse(String field, Data data) {
+      int pt = field.indexOf(", USA");
+      if (pt >= 0) field = field.substring(pt+5).trim();
+      field = MSPACE_PTN.matcher(field).replaceAll("\n");
+      super.parse(field, data);
+    }
+  }
 }
