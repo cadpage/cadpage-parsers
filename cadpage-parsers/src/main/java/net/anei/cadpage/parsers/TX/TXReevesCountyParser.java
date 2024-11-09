0bd9e1aa34d79326c20a21942750fc1d9e901792
Ken Corbin <ken@cadpage.org>
2024-11-09 13:55:36 -0800

    Fixed parsin gproblem with Reeves County, TX

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/TX/TXReevesCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/TX/TXReevesCountyParser.java
@@ -1,16 +1,54 @@
 package net.anei.cadpage.parsers.TX;
 
-import net.anei.cadpage.parsers.dispatch.DispatchA72Parser;
+import java.util.regex.Pattern;
 
-public class TXReevesCountyParser extends DispatchA72Parser {
-  
+import net.anei.cadpage.parsers.FieldProgramParser;
+import net.anei.cadpage.parsers.MsgInfo.Data;
+
+public class TXReevesCountyParser extends FieldProgramParser {
+
   public TXReevesCountyParser() {
-    super("REEVES COUNTY", "TX");
+    super("REEVES COUNTY", "TX",
+          "CALL ADDRCITYST PLACE PLACE GPS1 GPS2 INFO DATETIME UNIT SRC X SKIP SKIP ID EMPTY END");
   }
-  
+
   @Override
   public String getFilter() {
-    return "TPS_Service@tylertech.com";
+    return "RCESDCAD@reevescounty-esd.com";
   }
 
+  private static final Pattern DELIM = Pattern.compile(" *\\| *");
+
+  @Override
+  protected boolean parseMsg(String subject, String body, Data data) {
+    if (!subject.endsWith("|")) return false;
+    body = subject + ' ' + body;
+    return parseFields(DELIM.split(body, -1), data);
+  }
+
+  @Override
+  public Field getField(String name) {
+    if (name.equals("PLACE")) return new MyPlaceField();
+    if (name.equals("INFO")) return new MyInfoField();
+    if (name.equals("DATETIME")) return new DateTimeField("\\d\\d?/\\d\\d/\\d\\d \\d\\d:\\d\\d", true);
+    if (name.equals("ID")) return new IdField("CFS\\d+", true);
+    return super.getField(name);
+  }
+
+  private class MyPlaceField extends PlaceField {
+    @Override
+    public void parse(String field, Data data) {
+      if (field.equals("None")) return;
+      super.parse(field, data);
+    }
+  }
+
+  private static final Pattern INFO_BRK_PTN = Pattern.compile("[; ]*\\b\\d\\d?/\\d\\d/\\d\\d \\d\\d:\\d\\d:\\d\\d - *");
+  private class MyInfoField extends InfoField {
+    @Override
+    public void parse(String field, Data data) {
+      field = INFO_BRK_PTN.matcher(field).replaceAll("\n").trim();
+      super.parse(field, data);
+    }
+  }
 }
