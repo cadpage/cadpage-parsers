c76f2910499e4caf60f00a4672ccf1ee0d90ea83
Ken Corbin <ken@cadpage.org>
2024-11-13 18:07:49 -0800

    Fixed parsing problem with Jefferson County, IN

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/IN/INJeffersonCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/IN/INJeffersonCountyParser.java
@@ -1,76 +1,42 @@
 package net.anei.cadpage.parsers.IN;
 
-import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import net.anei.cadpage.parsers.FieldProgramParser;
 import net.anei.cadpage.parsers.MsgInfo.Data;
-import net.anei.cadpage.parsers.MsgInfo.MsgType;
-import net.anei.cadpage.parsers.SplitMsgOptions;
-import net.anei.cadpage.parsers.SplitMsgOptionsCustom;
-import net.anei.cadpage.parsers.dispatch.DispatchEmergitechParser;
 
 
-public class INJeffersonCountyParser extends DispatchEmergitechParser {
-  
+public class INJeffersonCountyParser extends FieldProgramParser {
+
   public INJeffersonCountyParser() {
-    super(true, CITY_LIST, "JEFFERSON COUNTY", "IN", TrailAddrType.PLACE);
+    super("JEFFERSON COUNTY", "IN",
+          "CFS_location:ADDRCITY! Nature:CALL! Details:INFO! END");
   }
 
   @Override
   public String getFilter() {
-    return "@jeffersoncounty.in.gov";
+    return "no-reply-zuercher@jeffersoncounty.in.gov";
   }
-  
-  @Override
-  public SplitMsgOptions getActive911SplitMsgOptions() {
-    return new SplitMsgOptionsCustom();
-  }
-  
-  private static final Pattern SUBJECT_GEN_ALERT_PTN = Pattern.compile("Text Message(?:|(\\S+))");
-  private static final Pattern GEN_ALERT_TRIM_PTN = Pattern.compile("^[- ]+");
 
   @Override
-  protected boolean parseMsg(String subject, String body, Data data) {
-    
-    Matcher match =  SUBJECT_GEN_ALERT_PTN.matcher(subject);
-    if (match.matches()) {
-      setFieldList("UNIT INFO");
-      data.msgType = MsgType.GEN_ALERT;
-      data.strUnit = getOptGroup(match.group(1));
-      data.strSupp = GEN_ALERT_TRIM_PTN.matcher(body).replaceFirst("");
-      return true;
-    }
-    
-    if (subject.length() > 0) {
-      body = subject + ": " + body;
-      if (!body.contains("Nature:") && body.contains(" Location")) body = "Nature:" + body;
-    }
+  protected boolean parseMsg(String body, Data data) {
+    if (!body.endsWith(" Please respond immediately.")) return false;
+    body = body.substring(0, body.length()-28).trim();
     return super.parseMsg(body, data);
   }
 
-  private static final String[] CITY_LIST = new String[]{
+  @Override
+  public Field getField(String name) {
+    if (name.equals("INFO")) return new MyInfoField();
+    return super.getField(name);
+  }
 
-    // Cities and towns
-    "BROOKSBURG",
-    "DUPONT",
-    "HANOVER",
-    "MADISON",
-
-    // Unincorporated towns
-    "CANAAN",
-    "KENT",
-    "DEPUTY",
-
-    // Townships
-    "GRAHAM TWP",
-    "HANOVER TWP",
-    "LANCASTER TWP",
-    "MADISON TWP",
-    "MILTON TWP",
-    "MONROE TWP",
-    "REPUBLICAN TWP",
-    "SALUDA TWP",
-    "SHELBY TWP",
-    "SMYRNA TWP"
-  };
+  private static final Pattern INFO_BRK_PTN = Pattern.compile("[; ]*\\b\\d\\d?/\\d\\d?/\\d\\d \\d\\d:\\d\\d:\\d\\d - *");
+  private class MyInfoField extends InfoField {
+    @Override
+    public void parse(String field, Data data) {
+      field = INFO_BRK_PTN.matcher(field).replaceAll("\n").trim();
+      super.parse(field, data);
+    }
+  }
 }
