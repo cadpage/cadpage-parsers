e2b1b5c10aa83d209c0c1fc1efa98d0b3564c8ee
Ken Corbin <ken@cadpage.org>
2024-11-18 18:18:28 -0800

    Fixed parsing problem with Union County, OH (B)

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/OH/OHUnionCountyBParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/OH/OHUnionCountyBParser.java
@@ -7,8 +7,7 @@ public class OHUnionCountyBParser extends FieldProgramParser {
 
   public OHUnionCountyBParser() {
     super("UNION COUNTY", "OH",
-          "Call_Type:CODE_CALL! Address:ADDRCITY! Lat/Lon:GPS! Common_Name:PLACE! ( Cross_Streets:X! | Closest_Intersection:X! ) Fire_Box:BOX! " +
-              "Assigned_Units:UNIT! Radio_Channel:CH! Primary_Inc_#:ID! CFS_#:SKIP! Narrative:INFO! INFO/N+");
+          "Call_Type:CODE_CALL! Status:SKIP? Address:ADDRCITY! Cross_Streets:X Lat/Lon:GPS Fire_Box:BOX Radio_Channel:CH EMPTY+? ( Dispatched_Units:UNIT! | Units:UNIT! ) Primary_Incident_Number:ID! Narrative:INFO! INFO/N+");
   }
 
   @Override
@@ -29,22 +28,42 @@ public class OHUnionCountyBParser extends FieldProgramParser {
   @Override
   public Field getField(String name) {
     if (name.equals("CODE_CALL")) return new MyCodeCallField();
+    if (name.equals("ADDRCITY")) return new MyAddressCityField();
     return super.getField(name);
   }
 
   private class MyCodeCallField extends Field {
     @Override
     public void parse(String field, Data data) {
+      if (field.isEmpty()) return;
       int pt = field.indexOf(" - ");
-      if (pt < 0) abort();
-      data.strCode = field.substring(0, pt).trim();
-      data.strCall = field.substring(pt+3).trim();
+      if (pt >= 0) {
+        data.strCode = field.substring(0, pt).trim();
+        field = field.substring(pt+3).trim();
+      }
+      data.strCall = field;
     }
 
     @Override
     public String getFieldNames() {
       return "CODE CALL";
     }
+  }
 
+  private class MyAddressCityField extends AddressCityField {
+    @Override
+    public void parse(String field, Data data) {
+      int pt = field.indexOf(" - ");
+      if (pt >= 0) {
+        data.strPlace = field.substring(pt+3).trim();
+        field = field.substring(0,pt).trim();
+      }
+      super.parse(field, data);
+    }
+
+    @Override
+    public String getFieldNames() {
+      return super.getFieldNames() + " PLACE";
+    }
   }
 }
