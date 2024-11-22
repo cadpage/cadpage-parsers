31dbce1eb18eb4ed76d43757d7a0be28d739f4b5
Ken Corbin <ken@cadpage.org>
2024-11-22 15:22:26 -0800

    Fixed minor issue with Maury County, TN

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/TN/TNMauryCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/TN/TNMauryCountyParser.java
@@ -1,5 +1,6 @@
 package net.anei.cadpage.parsers.TN;
 
+import net.anei.cadpage.parsers.MsgInfo.Data;
 import net.anei.cadpage.parsers.dispatch.DispatchH03Parser;
 
 public class TNMauryCountyParser extends DispatchH03Parser {
@@ -12,4 +13,12 @@ public class TNMauryCountyParser extends DispatchH03Parser {
   public String getFilter() {
     return "911@P1CAD.CommandCentral.com";
   }
+
+  @Override
+  protected boolean parseHtmlMsg(String subject, String body, Data data) {
+    if (!super.parseHtmlMsg(subject, body, data)) return false;
+    if (data.strCity.equals("CNTY")) data.strCity = "";
+    return true;
+  }
+
 }
