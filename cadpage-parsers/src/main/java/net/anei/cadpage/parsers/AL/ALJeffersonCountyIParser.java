3a061c6b21b96699b4f4c5987cf8d0b3db6249f8
Ken Corbin <ken@cadpage.org>
2024-11-26 11:10:44 -0800

    Fixed parsing problem with Jefferson Co9unty, AL (I)

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/AL/ALJeffersonCountyIParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/AL/ALJeffersonCountyIParser.java
@@ -5,24 +5,32 @@ import java.text.SimpleDateFormat;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import net.anei.cadpage.parsers.FieldProgramParser;
 import net.anei.cadpage.parsers.MsgInfo.Data;
+import net.anei.cadpage.parsers.dispatch.DispatchH05Parser;
 
-public class ALJeffersonCountyIParser extends FieldProgramParser {
+public class ALJeffersonCountyIParser extends DispatchH05Parser {
 
   public ALJeffersonCountyIParser() {
     super("JEFFERSON COUNTY", "AL",
-          "CALL:CALL! ADDR:GPS! ADDR1:ADDR! ID:ID! ( GRID2640:MAP! ( Date/Time:DATETIME! MAP:SKIP! UNITS:UNIT! " +
-                                                                  "| MAP:SKIP! UNITS:UNIT! Date/Time:DATETIME! " +
-                                                                  ") " +
-                                                   "| Date/Time:DATETIME GRID2640:MAP? MAP:SKIP! UNITS:UNIT! " +
-                                                   "| MAP:SKIP! UNITS:UNIT! Date/Time:DATETIME! " +
-                                                   ") INFO/N+");
+          "( SELECT/2 ADDRCITY CALL SKIP! X:X! Units:UNIT! Created:DATETIME2! Pri_Inc:ID! END " +
+          "| SELECT/H1  ( CALL:CALL! X/Y:GPS! ADDR1:ADDRCITY! ID:ID! DATE/TIME:DATETIME! CALL_TAKER:SKIP! " +
+                              "GRID2640:MAP! GOOGLE_MAP:EMPTY! MAP:MAP/L! XSTREETS:X! UNITS:UNIT! NARR:EMPTY INFO_BLK+ " +
+                       "| Address:ADDRCITY! Common_Name:PLACE! Cross_Streets:X! https:SKIP! Call_Date/Time:DATETIME! " +
+                              "Call_Type:CALL_PRI! Fire_Area:MAP! Alerts:ALERT! Incident_Numbers:ID! Units:UNIT! Times:EMPTY! TIMES+ " +
+                       ") " +
+          "| CALL:CALL! ADDR:GPS! ADDR1:ADDR! ID:ID! EMPTY+? ( GRID2640:MAP! ( Date/Time:DATETIME! MAP:SKIP! UNITS:UNIT! " +
+                                                                            "| MAP:SKIP! UNITS:UNIT! Date/Time:DATETIME! " +
+                                                                            ") " +
+                                                            "| Date/Time:DATETIME GRID2640:MAP? MAP:SKIP! UNITS:UNIT! " +
+                                                            "| MAP:SKIP! UNITS:UNIT! Date/Time:DATETIME! " +
+                                                            "| DATE:DATETIME! MAP:SKIP! UNIT:UNIT! " +
+                                                            ") INFO/N+ " +
+         ")");
   }
 
   @Override
   public String getFilter() {
-    return "FireDesk@JeffCoal911.org";
+    return "FireDesk@JeffCoal911.org,dispatcher@jeffcoal911.org";
   }
 
   @Override
@@ -36,16 +44,40 @@ public class ALJeffersonCountyIParser extends FieldProgramParser {
   }
 
   @Override
+  protected boolean parseHtmlMsg(String subject, String body, Data data) {
+    if (subject.startsWith("Automatic R&R Notification:")) {
+      setSelectValue("H1");
+      return super.parseHtmlMsg(subject, body, data);
+    } else {
+      return parseMsg(subject, body, data);
+    }
+  }
+
+  @Override
   protected boolean parseMsg(String subject, String body, Data data) {
-    if (!subject.equals("ACTIVE 9-1-1")) return false;
-    body = body.replace("GRID2640:", "\nGRID2640:");
-    return parseFields(body.split("\n"), data);
+    if (subject.equals("D I S P A T C H")) {
+      if (body.startsWith("CALL:")) {
+        subject = "ACTIVE 9-1-1";
+      } else {
+        setSelectValue("2");
+        return parseFields(body.split("\n"), data);
+      }
+    }
+    if (subject.equals("ACTIVE 9-1-1")) {
+      setSelectValue("1");
+      body = body.replace("GRID2640:", "\nGRID2640:");
+      return parseFields(body.split("\n"), data);
+    }
+
+    return false;
   }
 
   @Override
   public Field getField(String name) {
     if (name.equals("MAP")) return new MyMapField();
+    if (name.equals("CALL_PRI")) return new MyCallPriorityField();
     if (name.equals("DATETIME")) return new MyDateTimeField();
+    if (name.equals("DATETIME2")) return new DateTimeField("(\\d\\d?/\\d\\d?/\\d{4} \\d\\d?:\\d\\d:\\d\\d) #\\d+", true);
     if (name.equals("UNIT")) return new MyUnitField();
     if (name.equals("INFO"))  return new MyInfoField();
     return super.getField(name);
@@ -63,6 +95,23 @@ public class ALJeffersonCountyIParser extends FieldProgramParser {
         data.strMap = field;
       }
     }
+  }
+
+  private static final Pattern CALL_PRIORITY_PTN = Pattern.compile("([^,]+)[, ]+Call Priority: *([^,]*)[, ]*");
+  private class MyCallPriorityField extends Field {
+    @Override
+    public void parse(String field, Data data) {
+      Matcher match = CALL_PRIORITY_PTN.matcher(field);
+      if (!match.matches()) abort();
+      data.strCall = match.group(1).trim();
+      data.strPriority = match.group(2).trim();
+    }
+
+    @Override
+    public String getFieldNames() {
+      return "CALL PRI";
+    }
+
   }
 
   private static final Pattern DATE_TIME_PTN = Pattern.compile("(\\d\\d?/\\d\\d?/\\d{4}) (\\d\\d?:\\d\\d:\\d\\d(?: [AP]M)?)");
