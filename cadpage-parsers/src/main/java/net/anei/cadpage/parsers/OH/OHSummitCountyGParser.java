ce09829d4e23a69438fa89fe1016358d593501ca
Ken Corbin <ken@cadpage.org>
2024-11-26 15:21:53 -0800

    Update sender filter for Summit County, OH (G)

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/OH/OHSummitCountyGParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/OH/OHSummitCountyGParser.java
@@ -3,7 +3,7 @@ package net.anei.cadpage.parsers.OH;
 import net.anei.cadpage.parsers.dispatch.DispatchA39Parser;
 
 public class OHSummitCountyGParser  extends DispatchA39Parser {
-  
+
   public OHSummitCountyGParser() {
     super(OHSummitCountyParser.CITY_LIST, "SUMMIT COUNTY", "OH");
     removeWords("HTS");
@@ -11,9 +11,9 @@ public class OHSummitCountyGParser  extends DispatchA39Parser {
 
   @Override
   public String getFilter() {
-    return "dispatch@twinsburg.local,@richfieldpd.us,Dispatch@macedonia.oh.us,macedonia@tacpaging.com";
+    return "dispatch@twinsburg.local,@richfieldpd.us,Dispatch@macedonia.oh.us,macedonia@tacpaging.com,dispatch@twinsburg.oh.us";
   }
-  
+
   @Override
   public String adjustMapCity(String city) {
     return OHSummitCountyParser.fixMapCity(city);
