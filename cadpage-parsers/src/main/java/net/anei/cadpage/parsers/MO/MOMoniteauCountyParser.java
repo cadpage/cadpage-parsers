0ee685b3c266b5eae023eea7a4edeefda50ef399
Ken Corbin <ken@cadpage.org>
2024-12-02 12:56:25 -0800

    Fixed parsing problem with Moniteau County, MO

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/MO/MOMoniteauCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/MO/MOMoniteauCountyParser.java
@@ -3,11 +3,11 @@ package net.anei.cadpage.parsers.MO;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import net.anei.cadpage.parsers.HtmlProgramParser;
+import net.anei.cadpage.parsers.FieldProgramParser;
 import net.anei.cadpage.parsers.MsgInfo.Data;
 
 
-public class MOMoniteauCountyParser extends HtmlProgramParser {
+public class MOMoniteauCountyParser extends FieldProgramParser {
 
   public MOMoniteauCountyParser() {
     this("MONITEAU COUNTY", "MO");
@@ -15,9 +15,9 @@ public class MOMoniteauCountyParser extends HtmlProgramParser {
 
   public MOMoniteauCountyParser(String defCity, String defState) {
     super(defCity, defState,
-          "( SELECT/HTML Call:EMPTY! CALL! Sub:EMPTY! CALL/S Place:EMPTY! PLACE Address:EMPTY! ADDR City:EMPTY! CITY Apt:EMPTY! APT Cross_Streets:EMPTY! X Event#:EMPTY! ID! Reporting_Person:EMPTY! NAME Phone#:EMPTY! PHONE Initiated:EMPTY! SKIP! INFO/N+ Units%EMPTY! UNIT/C+?  COPYRIGHT " +
-          "| Call:CALL! Sub:CALL/S! Place:PLACE! Address:ADDR! ( City:CITY! Apt:APT! | Apt:APT! City:CITY! State:ST! Zip:ZIP! ) Cross_Streets:X! ( Latitude:GPS1 Longitude:GPS2! | Map_Coords:GPS | MapCoordinates:GPS | ) Event#:ID! Reporting_Person:NAME! Phone#:PHONE! ( Unit:UNIT! Initiated:SKIP! Notes:INFO! INFO/N+ | ( Initiated:SKIP! | Initiated_Date:SKIP! Initiated_Time:SKIP! ) INFO/N+ Units%EMPTY! UNIT/C+ ) " +
-          "| ADDR! Apt:APT! CITY! Cross_Streets:X! Event#:ID! Reporting_Person:NAME! Phone#:PHONE! INFO/N+ Units%EMPTY! UNIT/C+ )");
+          "Call:CALL! Sub:CALL/S! Place:PLACE! Address:ADDR! ( City:CITY! Apt:APT! | Apt:APT! City:CITY! State:ST! Zip:ZIP! ) Cross_Streets:X! " +
+              "( Latitude:GPS1 Longitude:GPS2! | Map_Coords:GPS | MapCoordinates:GPS | ) Event#:ID! Reporting_Person:NAME? Phone#:PHONE? " +
+              "( Unit:UNIT! Initiated:SKIP! ( Notes:INFO! | ProQANote:INFO! | ) INFO/N+ | ( Initiated:SKIP! | Initiated_Date:SKIP! Initiated_Time:SKIP! ) INFO/N+ Units%EMPTY UNIT/C+ ) ");
   }
 
   @Override
@@ -27,20 +27,7 @@ public class MOMoniteauCountyParser extends HtmlProgramParser {
 
   @Override
   public String getFilter() {
-    return "CAD@MONITEAU911.COM,CADMONITEAU911@OMNIGO.COM,DISPATCH@OMNIGO.COM,noreply@omnigo.com";
-  }
-
-  @Override
-  protected boolean parseHtmlMsg(String subject, String body, Data data) {
-    if (body.startsWith("<html>")) {
-      setSelectValue("HTML");
-      int pt = body.indexOf("\n©");
-      if (pt >= 0) body  = body.substring(0,pt).trim();
-      return super.parseHtmlMsg(subject, body, data);
-    } else {
-      setSelectValue("");
-      return parseMsg(body, data);
-    }
+    return "noreply@omnigo.com";
   }
 
   @Override
@@ -52,7 +39,6 @@ public class MOMoniteauCountyParser extends HtmlProgramParser {
   public Field getField(String name) {
     if (name.equals("ST")) return new MyStateField();
     if (name.equals("INFO")) return new MyInfoField();
-    if (name.equals("COPYRIGHT")) return new SkipField("©.*", true);
     if (name.equals("GPS")) return new MyGPSField();
     return super.getField(name);
   }
