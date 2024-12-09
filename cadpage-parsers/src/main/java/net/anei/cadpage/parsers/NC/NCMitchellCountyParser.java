154882e53d9f060cbdac4c88f406b0e7a96fb387
Ken Corbin <ken@cadpage.org>
2024-12-09 13:17:50 -0800

    Fixed parsing problem with Mitchell County, NC"

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/NC/NCMitchellCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/NC/NCMitchellCountyParser.java
@@ -1,25 +1,25 @@
 package net.anei.cadpage.parsers.NC;
 
 import net.anei.cadpage.parsers.MsgInfo.Data;
-import net.anei.cadpage.parsers.dispatch.DispatchA3Parser;
+import net.anei.cadpage.parsers.dispatch.DispatchA71Parser;
 
 
-public class NCMitchellCountyParser extends DispatchA3Parser {
-  
+public class NCMitchellCountyParser extends DispatchA71Parser {
+
   public NCMitchellCountyParser() {
-    super(1, "Mitchell911:", "MITCHELL COUNTY", "NC");
-  }
-  
-  @Override
-  public String getFilter() {
-    return "DISPATCH@main.nc.us";
+    super("MITCHELL COUNTY", "NC");
   }
 
   @Override
-  protected boolean parseMsg(String body, Data data, boolean splitField) {
-    if (!super.parseMsg(body, data, splitField)) return false;
+  public int getMapFlags() {
+    return MAP_FLG_PREFER_GPS;
+  }
+
+  @Override
+  protected boolean parseMsg(String body, Data data) {
+    if (!super.parseMsg(body, data)) return false;
     if (data.strPhone.equals("828- -")) data.strPhone = "";
     return true;
   }
-  
+
 }
