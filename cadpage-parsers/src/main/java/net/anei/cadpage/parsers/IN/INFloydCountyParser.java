058fb55f8be87c8a4c37b902baf3f5cc3d3cd705
Ken Corbin <ken@cadpage.org>
2024-12-13 14:26:16 -0800

    Update sender filter for Floyd County, IN

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/IN/INFloydCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/IN/INFloydCountyParser.java
@@ -3,13 +3,13 @@ package net.anei.cadpage.parsers.IN;
 import net.anei.cadpage.parsers.dispatch.DispatchA19Parser;
 
 public class INFloydCountyParser extends DispatchA19Parser {
-  
+
   public INFloydCountyParser() {
     super("FLOYD COUNTY", "IN");
   }
-  
+
   @Override
   public String getFilter() {
-    return "911@FCSDIN.NET";
+    return "911@FCSDIN.NET,@NAPD.COM";
   }
 }
