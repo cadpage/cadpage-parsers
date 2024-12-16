fb1de735e382ef1e9d50b247e8bb1c1693ee60b4
Ken Corbin <ken@cadpage.org>
2024-12-16 12:17:10 -0800

    Fixed parsing problelm with Jackson County, IL

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/IL/ILJacksonCountyBParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/IL/ILJacksonCountyBParser.java
@@ -6,12 +6,12 @@ public class ILJacksonCountyBParser extends DispatchH05Parser {
 
   public ILJacksonCountyBParser() {
     super("JACKSON COUNTY", "IL",
-          "ID DATETIME ADDRCITY INFO_BLK/Z+? SRC GPS1 GPS2! https:SKIP! TIMES+? UNIT! END");
+          "ID DATETIME ADDRCITY INFO_BLK/Z+? SRC! SKIP+? GPS! TIMES+? UNIT! END");
   }
 
   @Override
   public String getFilter() {
-    return "avafire@applecityfd.org";
+    return "@applecityfd.org";
   }
 
   @Override
@@ -23,6 +23,7 @@ public class ILJacksonCountyBParser extends DispatchH05Parser {
   public Field getField(String name) {
     if (name.equals("SRC")) return new SourceField("[A-Z]{1,4}[FP]D|MPS", true);
     if (name.equals("DATETIME")) return new DateTimeField("\\d\\d?/\\d\\d?/\\d{4} \\d\\d?:\\d\\d:\\d\\d", true);
+    if (name.equals("GPS")) return new GPSField("https?:.*query=(.*)", true);
     return super.getField(name);
   }
 }
