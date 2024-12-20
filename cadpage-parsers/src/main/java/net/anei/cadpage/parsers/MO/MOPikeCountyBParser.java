788697adf046cf334265d698e556ef0738cc50d7
Ken Corbin <ken@cadpage.org>
2024-12-19 18:22:36 -0800

    Update sender filter for Pike County, MO (B)

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/MO/MOPikeCountyBParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/MO/MOPikeCountyBParser.java
@@ -2,13 +2,13 @@ package net.anei.cadpage.parsers.MO;
 
 
 public class MOPikeCountyBParser extends MOLincolnCountyAParser {
-  
+
   public MOPikeCountyBParser() {
     super("PIKE COUNTY", "MO");
   }
-  
+
   @Override
   public String getFilter() {
-    return "EnterpolAlerts@PikeCountySO.org";
+    return "EnterpolAlerts@PikeCountySO.org,relay@pikemo911.org";
   }
 }
