a3ad73eecaf1d9e8ffadc1380ea57059d4be7796
Ken Corbin <ken@cadpage.org>
2024-12-20 18:15:47 -0800

    Update sender filter for Stevens County, WA

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/WA/WAStevensCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/WA/WAStevensCountyParser.java
@@ -5,13 +5,13 @@ import net.anei.cadpage.parsers.dispatch.DispatchA19Parser;
 
 
 public class WAStevensCountyParser extends DispatchA19Parser {
-  
+
   public WAStevensCountyParser() {
     super("STEVENS COUNTY", "WA");
   }
-  
+
   @Override
   public String getFilter() {
-    return "spillmancad@co.stevens.wa.us,Dispatch";
+    return "spillmancad@co.stevens.wa.us,Dispatch,@lifeflight.org";
   }
 }
