fcbd43c39b46b972dadb1887a2423e80321fb41c
Ken Corbin <ken@cadpage.org>
2024-12-23 13:51:10 -0800

    Fixed parsing problem with Story County, IA

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/IA/IAStoryCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/IA/IAStoryCountyParser.java
@@ -2,17 +2,15 @@ package net.anei.cadpage.parsers.IA;
 
 import java.util.Properties;
 
-import net.anei.cadpage.parsers.MsgInfo.Data;
 import net.anei.cadpage.parsers.dispatch.DispatchOSSIParser;
 
 public class IAStoryCountyParser extends DispatchOSSIParser {
 
   public IAStoryCountyParser() {
     super(CITY_CODES, "STORY COUNTY", "IA",
-          "( CANCEL ADDR CITY! INFO/N+" +
-          "| FYI UNIT1/C+? ( ADDR/Z CITY! CALL | CALL ADDR/Z CITY! |  CALL ADDR | ADDR CALL ) INFO/N+? ID END" +
-          "| UNIT2 CALL PRI ADDR CITY! INFO/N+ " +
-          "| ADDR? INFO/N+? DATETIME! UNIT1! CITY! END " +
+          "( CANCEL ADDR! CITY? INFO/N+" +
+          "| FYI ( ADDR/Z CITY/Y! CALL | CALL ADDR/Z CITY! |  CALL ADDR | ADDR CALL ) PHONE? DATETIME? INFO/N+? ID END" +
+          "| CALL2 ( UNIT ADDR/Z CITY! | ADDR/Z CITY! | UNIT? ADDR ) Radio_Channel:CH? INFO/N+ END " +
           ")");
   }
 
@@ -22,24 +20,11 @@ public class IAStoryCountyParser extends DispatchOSSIParser {
   }
 
   @Override
-  public boolean parseMsg(String subject, String body, Data data) {
-    if (!subject.equals("Text Message")) {
-      if (subject.length() > 0  || body.startsWith("CANCEL;")) {
-        body = stripFieldStart(body, "CAD:");
-        body = "CAD:" + append(subject, ": ", body);
-      }
-    }
-    return super.parseMsg(body, data);
-  }
-
-  private static final String UNIT_PTN_STR = "(?:[A-Z]{3}[A-Z0-9])";
-  @Override
   public Field getField(String name) {
-    if (name.equals("UNIT1")) return new UnitField(UNIT_PTN_STR, true);
-    if (name.equals("UNIT2")) return new UnitField("(?:\\b" + UNIT_PTN_STR + "\\b,?)+", true);
-    if (name.equals("PRI")) return new PriorityField("F\\d", true);
-    if (name.equals("ID")) return new IdField("\\d{9}", true);
+    if (name.equals("CALL2")) return new CallField("[A-Z]{4,5}", true);
+    if (name.equals("PHONE")) return new PhoneField("\\d{10}", true);
     if (name.equals("DATETIME")) return new DateTimeField("\\d\\d/\\d\\d/\\d{4} \\d\\d:\\d\\d:\\d\\d", true);
+    if (name.equals("ID")) return new IdField("\\d{9}", true);
     return super.getField(name);
   }
 
