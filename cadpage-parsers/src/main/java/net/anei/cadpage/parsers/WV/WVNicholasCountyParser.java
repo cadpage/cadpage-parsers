d5df8fae4f8d860aef66694bee3570745e124b60
Ken Corbin <ken@cadpage.org>
2024-12-23 13:12:26 -0800

    Fixed parsing problem with Nicholas County, WV

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/WV/WVNicholasCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/WV/WVNicholasCountyParser.java
@@ -1,73 +1,15 @@
 package net.anei.cadpage.parsers.WV;
 
-import net.anei.cadpage.parsers.MsgInfo.Data;
-import net.anei.cadpage.parsers.dispatch.DispatchEmergitechParser;
+import net.anei.cadpage.parsers.dispatch.DispatchA19Parser;
 
-public class WVNicholasCountyParser extends DispatchEmergitechParser {
-  
+public class WVNicholasCountyParser extends DispatchA19Parser {
+
   public WVNicholasCountyParser() {
-    super(CITY_LIST, "NICHOLAS COUNTY", "WV");
+    super("NICHOLAS COUNTY", "WV");
   }
-  
+
   @Override
   public String getFilter() {
-    return "NCDHSEM@shentel.net";
+    return "smtp.flex@nicholas911.com";
   }
-  
-  @Override
-  protected boolean parseMsg(String subject, String body, Data data) {
-    if (!subject.startsWith("CAD TEXT|")) return false;
-    subject = subject.substring(9);
-    return super.parseMsg(subject, body, data);
-  }
-
-  private static final String[] CITY_LIST = new String[]{
-      
-      // Cities
-      "RICHWOOD",
-      "SUMMERSVILLE",
-
-      // Magisterial districts
-      "BEAVER",
-      "GRANT",
-      "HAMILTON",
-      "JEFFERSON",
-      "KENTUCKY",
-      "SUMMERSVILLE",
-      "WILDERNESS",
-
-      // Census-designated places
-      "BELVA",
-      "BIRCH RIVER",
-      "CRAIGSVILLE",
-      "DIXIE",
-      "FENWICK",
-      "NETTIE",
-      "TIOGA",
-
-      // Unincorporated communities
-      "BENTREE",
-      "CALVIN",
-      "CAMBRIA",
-      "CANVAS",
-      "COTTLE",
-      "DRENNEN",
-      "ENON",
-      "GILBOA",
-      "HOLCOMB",
-      "HOOKERSVILLE",
-      "KESSLERS CROSS LANES",
-      "LEIVASY",
-      "LOCKWOOD",
-      "MOUNT NEBO",
-      "MOUNT LOOKOUT",
-      "MUDDLETY",
-      "NEW HOPE",
-      "ODELL TOWN",
-      "PERSINGER",
-      "POOL",
-      "SWISS",
-      "WERTH",
-      "ZELA"
-  };
 }
