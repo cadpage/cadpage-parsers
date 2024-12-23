4e7cae0bc9d083734582a48e1151f73e2b25ef68
Ken Corbin <ken@cadpage.org>
2024-12-23 11:21:08 -0800

    Fixed parsing problem with Florence County, SC

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/SC/SCFlorenceCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/SC/SCFlorenceCountyParser.java
@@ -1,6 +1,10 @@
 package net.anei.cadpage.parsers.SC;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import net.anei.cadpage.parsers.MsgInfo.Data;
+import net.anei.cadpage.parsers.MsgInfo.MsgType;
 import net.anei.cadpage.parsers.dispatch.DispatchSouthernParser;
 
 
@@ -8,7 +12,7 @@ import net.anei.cadpage.parsers.dispatch.DispatchSouthernParser;
 public class SCFlorenceCountyParser extends DispatchSouthernParser {
 
   public SCFlorenceCountyParser() {
-    super(CITY_LIST, "FLORENCE COUNTY", "SC", DSFLG_ADDR | DSFLG_ADDR_TRAIL_PLACE | DSFLG_OPT_X | DSFLG_OPT_CODE | DSFLG_ID | DSFLG_TIME);
+    super(CITY_LIST, "FLORENCE COUNTY", "SC", DSFLG_PROC_EMPTY_FLDS | DSFLG_ADDR | DSFLG_ADDR_TRAIL_PLACE | DSFLG_OPT_X | DSFLG_OPT_CODE | DSFLG_ID | DSFLG_TIME);
     setupMultiWordStreets("I M GRAHAM");
     removeWords("COURT", "HEIGHTS", "PLACE", "STREET");
   }
@@ -18,10 +22,31 @@ public class SCFlorenceCountyParser extends DispatchSouthernParser {
     return MAP_FLG_PREFER_GPS;
   }
 
+  private static final Pattern GEN_ALERT_PTN = Pattern.compile("(?:32;)?500 +(.*)");
+  private static final Pattern MARKER = Pattern.compile("(2|65536); *");
+  private static final Pattern ID_PTN = Pattern.compile(";\\d{4}-\\d+;");
+
   @Override
   protected boolean parseMsg(String body, Data data) {
 
-    body = stripFieldStart(body, "CFS Location:");
+    Matcher match = GEN_ALERT_PTN.matcher(body);
+    if (match.matches()) {
+      setFieldList("INFO");
+      data.msgType = MsgType.GEN_ALERT;
+      data.strSupp = match.group(1);
+      return true;
+    }
+
+    match = MARKER.matcher(body);
+    if (match.lookingAt()) {
+      body = body.substring(match.end());
+      if (!match.group(1).equals("2")) {
+        match = ID_PTN.matcher(body);
+        if (!match.find()) return false;
+        int pt = match.start();
+        body = body.substring(0, pt) + ";;;;" + body.substring(pt);
+      }
+    }
     return super.parseMsg(body, data);
   }
 
