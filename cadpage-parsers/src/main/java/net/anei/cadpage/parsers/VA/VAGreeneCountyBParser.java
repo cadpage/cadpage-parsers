45113da7f0ed2fca2075ee28220a69ffb78dc631
Ken Corbin <ken@cadpage.org>
2024-12-23 12:42:05 -0800

    Fixed parsing problem with Green County, VA (B)

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/VA/VAGreeneCountyBParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/VA/VAGreeneCountyBParser.java
@@ -1,40 +1,20 @@
 package net.anei.cadpage.parsers.VA;
 
-import java.util.regex.Pattern;
+import net.anei.cadpage.parsers.dispatch.DispatchA19Parser;
 
-import net.anei.cadpage.parsers.FieldProgramParser;
-import net.anei.cadpage.parsers.MsgInfo.Data;
-
-public class VAGreeneCountyBParser extends FieldProgramParser {
+public class VAGreeneCountyBParser extends DispatchA19Parser {
 
   public VAGreeneCountyBParser() {
-    super("GREENE COUNTY", "VA",
-          "ID! Nat:CALL! Add:ADDR! Resp_Units:UNIT! Comments:INFO! INFO/N+");
+    super("GREENE COUNTY", "VA");
   }
 
   @Override
   public String getFilter() {
-    return "40808-N9hZUEFtuQYvYVRd@alert.active911.com";
+    return "gcsoadmin@gcvasheriff.us";
   }
 
   @Override
-  protected boolean parseMsg(String body, Data data) {
-    return parseFields(body.split("\n"), data);
-  }
-
-  @Override
-  public Field getField(String name) {
-    if (name.equals("ID")) return new IdField("INC # +(.*)", true);
-    if (name.equals("INFO")) return new MyInfoField();
-    return super.getField(name);
-  }
-
-  private Pattern INFO_JUNK_PTN = Pattern.compile("\\d\\d:\\d\\d:\\d\\d \\d\\d/\\d\\d/\\d{4} - .*");
-  private class MyInfoField extends InfoField {
-    @Override
-    public void parse(String field, Data data) {
-      if (INFO_JUNK_PTN.matcher(field).matches()) return;
-      super.parse(field, data);
-    }
+  public int getMapFlags() {
+    return MAP_FLG_PREFER_GPS;
   }
 }
