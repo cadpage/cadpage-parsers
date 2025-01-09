4955158c3166026c56450bcefcf609344b46b9ac
Ken Corbin <ken@cadpage.org>
2025-01-09 11:04:48 -0800

    Fixed parsing problem with Cherokee County, OK (B)

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/OK/OKCherokeeCountyBParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/OK/OKCherokeeCountyBParser.java
@@ -1,8 +1,8 @@
 package net.anei.cadpage.parsers.OK;
 
-import net.anei.cadpage.parsers.dispatch.DispatchA38Parser;
+import net.anei.cadpage.parsers.dispatch.DispatchA19Parser;
 
-public class OKCherokeeCountyBParser extends DispatchA38Parser {
+public class OKCherokeeCountyBParser extends DispatchA19Parser {
 
   public OKCherokeeCountyBParser() {
     super("CHEROKEE COUNTY", "OK");
