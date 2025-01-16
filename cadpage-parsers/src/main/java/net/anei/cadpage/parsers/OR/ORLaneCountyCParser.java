cf77a3cc341be762f8ced6671449667557e3af71
Ken Corbin <ken@cadpage.org>
2025-01-16 09:51:34 -0800

    Cleanup Lane County, OR (C)

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/OR/ORLaneCountyCParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/OR/ORLaneCountyCParser.java
@@ -3,6 +3,7 @@ package net.anei.cadpage.parsers.OR;
 
 import java.util.Properties;
 
+import net.anei.cadpage.parsers.MsgInfo.Data;
 import net.anei.cadpage.parsers.dispatch.DispatchA98Parser;
 
 
@@ -21,6 +22,13 @@ public class ORLaneCountyCParser extends DispatchA98Parser {
   @Override
   public int getMapFlags() {
     return MAP_FLG_PREFER_GPS | MAP_FLG_SUPPR_LA;
+  }
+
+  @Override
+  protected boolean parseMsg(String subject, String body, Data data) {
+    if (!super.parseMsg(subject, body, data)) return false;
+    if (data.strPlace.equals(data.strAddress)) data.strPlace = "";
+    return true;
   }
 
   private static final Properties CITY_CODES = buildCodeTable(new String[]{
