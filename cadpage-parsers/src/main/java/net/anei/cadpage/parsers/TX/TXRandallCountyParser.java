943e6240c74e0f8efe2f5584ae3f257becce5950
Ken Corbin <ken@cadpage.org>
2025-01-28 20:51:45 -0800

    Added city code table to Randall County, TX

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/TX/TXRandallCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/TX/TXRandallCountyParser.java
@@ -1,16 +1,29 @@
 package net.anei.cadpage.parsers.TX;
 
+import java.util.Properties;
+
 import net.anei.cadpage.parsers.dispatch.DispatchH03Parser;
 
 public class TXRandallCountyParser extends DispatchH03Parser {
 
   public TXRandallCountyParser() {
-    super("RANDALL COUNTY", "TX");
+    super(CITY_CODES, "RANDALL COUNTY", "TX");
   }
 
   @Override
   public String getFilter() {
     return "RCSO@rc-sheriff.com";
   }
+
+  private static final Properties CITY_CODES = buildCodeTable(new String[] {
+      "AMAR", "AMARILLO",
+      "BUSH", "BUSHLAND",
+      "CNYN", "CANYON",
+      "LKTW", "LAKE TANGLEWOOD",
+      "POTT", "POTTER CO",
+      "RAND", "RANDALL",
+      "TIMB", "TIMBERCREEK"
+
+  });
 
 }
