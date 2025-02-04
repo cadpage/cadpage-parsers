f6cc58f2a1a6f958edea8ccc95d1fcd561dd1af4
Ken Corbin <ken@cadpage.org>
2025-02-03 16:27:13 -0800

    Fixed parsing problem with Coffee COunty, TN

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/TN/TNCoffeeCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/TN/TNCoffeeCountyParser.java
@@ -2,55 +2,22 @@ package net.anei.cadpage.parsers.TN;
 
 import java.util.Properties;
 
-import net.anei.cadpage.parsers.dispatch.DispatchA74Parser;
+import net.anei.cadpage.parsers.dispatch.DispatchA95Parser;
 
 
-public class TNCoffeeCountyParser extends DispatchA74Parser {
+public class TNCoffeeCountyParser extends DispatchA95Parser {
 
   public TNCoffeeCountyParser() {
-    super(CITY_LIST, "COFFEE COUNTY", "TN");
+    super("COFFEE COUNTY", "TN");
     setupGpsLookupTable(GPS_LOOKUP_TABLE);
   }
 
   @Override
   public String getFilter() {
-    return "dispatch@coffeetne911.info";
+    return "COFFEECO911@BENLOMAND.NET";
   }
 
   private static final Properties GPS_LOOKUP_TABLE = buildCodeTable(new String[] {
       "21 POWELL RD",                         "+35.407222,-86.173306"
   });
-
-  private static final String[] CITY_LIST = new String[]{
-
-      "BEECHGROVE",
-      "BEECH GROVE",
-      "BELMONT",
-      "FARRAR HILL",
-      "FUDGEAROUND",
-      "HILLSBORO",
-      "LAKEWOOD PARK",
-      "MANCHESTER",
-      "MORRISON",
-      "NEW UNION",
-      "NOAH",
-      "POCAHONTAS",
-      "SHADY GROVE",
-      "SUMMITVILLE",
-      "SUMMITTVILLE",
-      "TULLAHOMA",
-
-      // Bedford County
-      "NORMANDY",
-      "WARTRACE",
-
-      // Cannon County
-      "BRADYVILLE",
-
-      // Franklin County
-      "ESTILL SPRINGS",
-
-      // Warren County
-      "MCMINNVILLE"
-  };
 }
