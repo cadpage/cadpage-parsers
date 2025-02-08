6d5e31870a8e8e6dd6833c8b676afe444dd4f0f5
Ken Corbin <ken@cadpage.org>
2025-02-07 19:30:27 -0800

    Update sender filter for Rockingham County, NC

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/NC/NCRockinghamCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/NC/NCRockinghamCountyParser.java
@@ -11,21 +11,21 @@ import net.anei.cadpage.parsers.dispatch.DispatchOSSIParser;
  * Buncombe county, NC
  */
 public class NCRockinghamCountyParser extends DispatchOSSIParser {
-  
+
   public NCRockinghamCountyParser() {
     super(CITY_CODES, "ROCKINGHAM COUNTY", "NC",
           "ID?: ( SELECT/1 Incident#:ID1! Report#:EMPTY! Date:DATE! Time_Out:TIME! Nature:CALL! MP:CODE! Business:PLACE! Address:ADDR! City:CITY! Addt_Address:ADDADDR! Cross:X! X+ Subdivision:SKIP! Neighborhood:SKIP Notes:INFO/N! INFO/N+ Units:UNIT! INFO/N+ " +
                "| FYI? ( ADDR/Z CITY X/Z+? CALL2 UNIT2 | CALL2 ADDR APT? X/Z+? ( CITY ID? APT2? ( CODE2 UNIT2? CH? | UNIT2 CH? | CH | PLACE CODE2 UNIT2? CH? | PLACE UNIT2 CH? | PLACE CH | ) | ID CODE2? UNIT2? CH? | CODE2 UNIT2? CH? | UNIT2 CH? | CH ) ) INFO+ )");
   }
-  
+
   @Override
   public String getFilter() {
-    return "@co.rockingham.nc.us,14101,messaging@iamresponding.com";
+    return "@co.rockingham.nc.us,14101,messaging@iamresponding.com,CAD@rockinghamcountync.gov";
   }
-  
+
   private static final Pattern SPEC_MARKER = Pattern.compile("Communications\n\nDispatch\n\n");
   private static final Pattern DELIM_PTN = Pattern.compile("\n| (?=Report#:|Time Out:|MP:|City:)");
-  
+
   @Override
   protected boolean parseMsg(String body, Data data) {
     Matcher match = SPEC_MARKER.matcher(body);
@@ -47,7 +47,7 @@ public class NCRockinghamCountyParser extends DispatchOSSIParser {
     if (name.equals("DATE")) return new DateField("\\d\\d/\\d\\d/\\d\\d", true);
     if (name.equals("TIME")) return new TimeField("\\d\\d:\\d\\d", true);
     if (name.equals("ADDADDR")) return new MyAdditionalAddressField();
-    
+
     if (name.equals("CALL2")) return new MyCallField();
     if (name.equals("APT")) return new AptField("[A-Z]|\\d{1,3}", true);
     if (name.equals("ID")) return new IdField("\\d{8}", true);
@@ -57,20 +57,20 @@ public class NCRockinghamCountyParser extends DispatchOSSIParser {
     if (name.equals("CH")) return new ChannelField("(?:Radio Channel: *)?(TAC.*)", true);
     return super.getField(name);
   }
-  
+
   private class MyAdditionalAddressField extends InfoField {
     @Override
     public void parse(String field, Data data) {
       if (field.length() == 0) return;
       super.parse("Addt Address: " + field, data);
     }
-    
+
     @Override
     public String getFieldNames() {
       return "";
     }
   }
-  
+
   private class MyCallField extends  CallField {
     @Override
     public void parse(String field, Data data) {
@@ -83,13 +83,13 @@ public class NCRockinghamCountyParser extends DispatchOSSIParser {
       }
       super.parse(field, data);
     }
-    
+
     @Override
     public String getFieldNames() {
       return "UNIT CALL";
     }
   }
-  
+
   private static final Properties CITY_CODES = buildCodeTable(new String[]{
       "EDEN", "EDEN",
       "MAD",  "MADISON",
