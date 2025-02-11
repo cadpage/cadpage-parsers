b4599542bd8779af1a46bb1318840ea62a9cfe86
Ken Corbin <ken@cadpage.org>
2025-02-11 13:08:17 -0800

    Fixed parsing problem with Morris County, NJ (B)

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/NJ/NJMorrisCountyBParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/NJ/NJMorrisCountyBParser.java
@@ -1,5 +1,7 @@
 package net.anei.cadpage.parsers.NJ;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -7,48 +9,40 @@ import net.anei.cadpage.parsers.FieldProgramParser;
 import net.anei.cadpage.parsers.MsgInfo.Data;
 
 public class NJMorrisCountyBParser extends FieldProgramParser {
-  
+
   public NJMorrisCountyBParser() {
-    super("MORRIS COUNTY", "NJ", 
-          "Case#:ID! CFS_Description:CALL! Location:ADDR! Common_Place:PLACE! Map_ID:MAP! Incident_Recv_DateTime:DATETIME! Contain_DateTime:SKIP! INFO/N+");
-  }
-  
-  @Override
-  public String getFilter() {
-    return "DataExport@jeffersonpolice.com,donotreply@lawsoftweb.onmicrosoft.com";
-  }
-  
-  private static final Pattern SUBJECT_PTN = Pattern.compile("Incident# : *\\S+ Resource# : *(\\S+) For Activity Dispatch Resource");
-  private static final Pattern DELIM = Pattern.compile("\\n+|\\|");
-  
-  @Override
-  public boolean parseMsg(String subject, String body, Data data) {
-    Matcher match = SUBJECT_PTN.matcher(subject);
-    if (!match.matches()) return false;
-    data.strUnit = match.group(1);
-    body = body.replace(" :- ", ":");
-    return parseFields(DELIM.split(body), data);
-  }
-  
-  @Override
-  public String getProgram() {
-    return "UNIT " + super.getProgram();
-  }
-  
-  @Override
-  public Field getField(String name) {
-    if (name.equals("DATETIME")) return new DateTimeField("\\d\\d?/\\d\\d/\\d{4} \\d\\d:\\d\\d:\\d\\d", true);
-    if (name.equals("INFO")) return new MyInfoField();
-    return super.getField(name);
-  }
-  
-  private class MyInfoField extends InfoField {
-    @Override
-    public void parse(String field, Data data) {
-      if (!field.startsWith("Comment:")) return;
-      field = field.substring(8).trim();
-      super.parse(field, data);
-    }
+    super("MORRIS COUNTY", "NJ",
+          "CAD:ID! Dt:DATETIME! Loc:ADDR! Cross:X! Apt:APT! Mun:CITY! CFS:CALL! Cmnts:INFO! INFO/N+");
   }
 
+  @Override
+  public String getFilter() {
+    return "donotreply@lawsoftweb.onmicrosoft.com";
+  }
+
+  @Override
+  public boolean parseMsg(String subject, String body, Data data) {
+    return parseFields(body.split("\n"), data);
+  }
+
+  @Override
+  public Field getField(String name) {
+    if (name.equals("DATETIME")) return new MyDateTimeField();
+    return super.getField(name);
+  }
+
+  private static final Pattern DATE_TIME_PTN = Pattern.compile("(\\d\\d?/\\d\\d?/\\d{4}) (\\d\\d?:\\d\\d(:\\d\\d)? [AP]M)");
+  private static final DateFormat TIME_FMT1 = new SimpleDateFormat("hh:mm:ss aa");
+  private static final DateFormat TIME_FMT2 = new SimpleDateFormat("hh:mm aa");
+
+  private class MyDateTimeField extends DateTimeField {
+    @Override
+    public void parse(String field, Data data) {
+      Matcher match = DATE_TIME_PTN.matcher(field);
+      if (!match.matches()) abort();
+      data.strDate = match.group(1);
+      DateFormat fmt = match.group(3) != null ? TIME_FMT1 : TIME_FMT2;
+      setTime(fmt, match.group(2), data);
+    }
+  }
 }
