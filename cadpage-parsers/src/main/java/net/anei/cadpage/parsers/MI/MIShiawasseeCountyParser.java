06961a000ca755f12416143f8b75b4cfbc69bc8d
Ken Corbin <ken@cadpage.org>
2025-02-11 09:33:50 -0800

    Cleaned up Shiawassee County, MI

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/MI/MIShiawasseeCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/MI/MIShiawasseeCountyParser.java
@@ -19,6 +19,11 @@ public class MIShiawasseeCountyParser extends DispatchOSSIParser {
   }
 
   @Override
+  public int getMapFlags() {
+    return MAP_FLG_PREFER_GPS;
+  }
+
+  @Override
   protected boolean parseMsg(String body, Data data) {
     if (!body.startsWith("CAD:")) body = "CAD:" + body;
     return super.parseMsg(body, data);
