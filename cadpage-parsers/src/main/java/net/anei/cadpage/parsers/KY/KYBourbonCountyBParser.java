47875b87094876796d086bce89ada5bc45b32548
Ken Corbin <ken@cadpage.org>
2025-02-10 20:37:28 -0800

    Fixed parsing problem with Bourbon County, KY (B)

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/KY/KYBourbonCountyBParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/KY/KYBourbonCountyBParser.java
@@ -1,18 +1,18 @@
 package net.anei.cadpage.parsers.KY;
 
-import net.anei.cadpage.parsers.dispatch.DispatchA19Parser;
+import net.anei.cadpage.parsers.dispatch.DispatchA24Parser;
 /**
  * Bourbon County, KY
  */
-public class KYBourbonCountyBParser extends DispatchA19Parser {
-  
+public class KYBourbonCountyBParser extends DispatchA24Parser {
+
   @Override
   public String getFilter() {
     return "@alert.active911.com";
   }
-  
+
   public KYBourbonCountyBParser() {
     super("BOURBON COUNTY", "KY");
   }
-  
+
 }
