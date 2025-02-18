7134fb68a8a93dcc0c09fbb76eda6b593506d3f4
Ken Corbin <ken@cadpage.org>
2025-02-18 14:44:32 -0800

    Fixed parsing problem with Mercer County, MO

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/MO/MOMercerCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/MO/MOMercerCountyParser.java
@@ -1,11 +1,16 @@
 package net.anei.cadpage.parsers.MO;
 
-import net.anei.cadpage.parsers.TX.TXArcherCountyParser;
+import net.anei.cadpage.parsers.dispatch.DispatchBCParser;
 
-public class MOMercerCountyParser extends TXArcherCountyParser {
+public class MOMercerCountyParser extends DispatchBCParser {
   
   public MOMercerCountyParser() {
     super("MERCER COUNTY", "MO");
+  }
+  
+  @Override
+  public String getFilter() {
+    return "hcso@omnigo.com";
   }
 
 }
