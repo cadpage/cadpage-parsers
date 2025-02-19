6f576a1c147599f4cbc2162f0636767f16a49995
Ken Corbin <ken@cadpage.org>
2025-02-18 18:28:54 -0800

    Fixed parsing problem with ARcher COunty, TX

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/TX/TXArcherCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/TX/TXArcherCountyParser.java
@@ -1,20 +1,15 @@
 package net.anei.cadpage.parsers.TX;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import net.anei.cadpage.parsers.dispatch.DispatchA71Parser;
 
-import net.anei.cadpage.parsers.FieldProgramParser;
-import net.anei.cadpage.parsers.MsgInfo.Data;
-
-public class TXArcherCountyParser extends FieldProgramParser {
+public class TXArcherCountyParser extends DispatchA71Parser {
 
   public TXArcherCountyParser() {
     this("ARCHER COUNTY", "TX");
   }
 
   public TXArcherCountyParser(String defCity, String defState) {
-    super(defCity, defState,
-          "CALL ADDR CITY! INFO/N+");
+    super(defCity, defState);
   }
 
   @Override
@@ -22,34 +17,8 @@ public class TXArcherCountyParser extends FieldProgramParser {
     return "cadalerts@kologik.com";
   }
 
-  private static Pattern SUBJECT_PTN = Pattern.compile("(\\d\\d:\\d\\d) @ .*");
   @Override
-  protected boolean parseMsg(String subject, String body, Data data) {
-    Matcher match = SUBJECT_PTN.matcher(subject);
-    if (!match.matches()) return false;
-    data.strTime = match.group(1);
-    return parseFields(body.split(";"), data);
+  public int getMapFlags() {
+    return MAP_FLG_PREFER_GPS | MAP_FLG_SUPPR_LA;
   }
-
-  @Override
-  public String getProgram() {
-    return "TIME " + super.getProgram();
-  }
-
-  @Override
-  public Field getField(String name) {
-    if (name.equals("INFO")) return new MyInfoField();
-    return super.getField(name);
-  }
-
-  private static final Pattern INFO_PTN = Pattern.compile("\\w+ - +");
-  private class MyInfoField extends InfoField {
-    @Override
-    public void parse(String field, Data data) {
-      Matcher match = INFO_PTN.matcher(field);
-      if (match.lookingAt()) field = field.substring(match.end());
-      super.parse(field, data);
-    }
-  }
-
 }
