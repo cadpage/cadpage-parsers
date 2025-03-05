da6ad3669dc545fb39511348df828376005c9516
Ken Corbin <ken@cadpage.org>
2025-03-05 13:45:15 -0800

    New Location: Clark County, NV (E)

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/NV/NVClarkCountyEParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/NV/NVClarkCountyEParser.java
@@ -3,58 +3,61 @@ package net.anei.cadpage.parsers.NV;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import net.anei.cadpage.parsers.FieldProgramParser;
 import net.anei.cadpage.parsers.MsgInfo.Data;
-import net.anei.cadpage.parsers.MsgParser;
 
-public class NVClarkCountyEParser extends MsgParser {
-  
+public class NVClarkCountyEParser extends FieldProgramParser {
+
   public NVClarkCountyEParser() {
-    super("CLARK COUNTY", "NV");
+    super("CLARK COUNTY", "NV",
+          "ID CODE_CALL SRC MAP ADDR UNIT INFO! INFO+ END");
   }
-  
+
   @Override
   public String getFilter() {
     return "countydsp@clarkcountynv.gov";
   }
-  
+
   @Override
   protected boolean parseMsg(String subject, String body, Data data) {
     if (!subject.equals("CAD")) return false;
-    
-    FParser fp = new FParser(body);
-    if (fp.checkAhead(10, "INC  #")) {
-      setFieldList("ID CODE CALL ADDR APT MAP");
-      fp.skip(16);
-      data.strCallId = fp.get(16);
-      if (!fp.check("  PROBLEM")) return false;
-      parseCall(fp.get(30), data);
-      if (!fp.check("ADDR")) return false;
-      parseAddress(fp.get(40), data);
-      if (!fp.check("BatBat ")) return false;
-      data.strMap = fp.get(2);
-      if (!fp.check("  DivDiv ")) return false;
-      data.strMap = append(data.strMap, "-", fp.get());
+
+    String[] flds = body.split("\\|", -1);
+    if (flds.length > 5) {
+      return parseFields(flds, data);
+    }
+
+    else {
+      setFieldList("ADDR APT CITY X CODE CALL PRI MAP ID UNIT INFO");
+      FParser fp = new FParser(body);
+      parseAddress(fp.get(30), data);
+
+      if (!fp.check("City: ")) return false;
+      data.strCity = fp.get(35);
+
+      if (!fp.check("X Street: ")) return false;
+      data.strCross = fp.get(60);
+
+      parseCall(fp.get(30), data);;
+
+      if (!fp.check("Pri: ")) return false;
+      data.strPriority = fp.get(30);
+
+      if (!fp.check("Dist/Phan: ")) return false;
+      data.strMap = fp.get(10);
+
+      if (!fp.check("Inc #")) return false;
+      data.strCallId = fp.get(20);
+
+      if (!fp.check("Units: ")) return false;
+      data.strUnit = fp.get(30);
+
+      if (!fp.check("Comments:")) return false;
+      parseExtra(fp.get(), data);
       return true;
     }
-    setFieldList("ADDR APT X CODE CALL PRI MAP ID UNIT INFO");
-    parseAddress(fp.get(30), data);
-    if (!fp.check("X Street")) return false;
-    data.strCross = fp.get(60);
-    parseCall(fp.get(30), data);
-    if (!fp.check("Pri:")) return false;
-    data.strPriority = fp.get(30);
-    if (!fp.check("Dist/Phan")) return false;
-    data.strMap = append(data.strMap, "/", fp.get(10));
-    if (!fp.check("Inc #")) return false;
-    data.strCallId = fp.get(19);
-    fp.setOptional();
-    if (!fp.check(" ")) return false;
-    
-    // Rest is free form and mostly junk
-    parseExtra(fp.get(), data);
-    return true;
   }
-  
+
   private static final Pattern CODE_CALL_PTN = Pattern.compile("([A-Z0-9]+)- *(.*)");
   private void parseCall(String field, Data data) {
     Matcher match = CODE_CALL_PTN.matcher(field);
@@ -64,43 +67,63 @@ public class NVClarkCountyEParser extends MsgParser {
     }
     data.strCall = field;
   }
-  
-  private static final Pattern EXTRA_UNIT_PTN = Pattern.compile("Units;(.*?)comments;");
+
   private void parseExtra(String field, Data data) {
-    Matcher match = EXTRA_UNIT_PTN.matcher(field);
-    if (match.lookingAt()) {
-      data.strUnit = match.group(1).trim();
-      field = field.substring(match.end()).trim();
-    }
-    else {
-      field = stripFieldStart(field, "Comments");
-    }
-    
+    field = stripFieldStart(field, "Comments");
+
     // Throw away anything between square brackets, and process
     // anything outside of them
+    int pCnt = 0;
     int last = 0;
     for (int pt = 0; pt<field.length(); pt++) {
       int chr = field.charAt(pt);
-      if (last>=0 && chr ==  '[') {
-        parseExtraField(field.substring(last, pt), data);
-        last = -1;
-      }
-      else if (last < 0 && chr == ']') {
-        last = pt+1;
+      if (chr == '[') {
+        if (pCnt++ == 0) parseExtraField(field.substring(last, pt), data);
+      } else if (chr == ']') {
+        if (--pCnt == 0) last = pt+1;
       }
     }
-    if (last >= 0) {
+
+    if (pCnt == 0) {
       parseExtraField(field.substring(last), data);
     }
   }
-  
+
   private static final Pattern EXTRA_FLD_PTN = Pattern.compile("FZ[A-Z0-9]+, *(.*)");
   private void parseExtraField(String field, Data data) {
     field = field.trim();
+    field = stripFieldEnd(field, ",");
     if (field.length() == 0) return;
-    if (field.startsWith("Class of service:")) return;
+    if (field.startsWith("Class of Service")) return;
     Matcher match = EXTRA_FLD_PTN.matcher(field);
     if (match.matches()) field = match.group(1);
     data.strSupp = append(data.strSupp, "\n", field);
+  }
+
+  @Override
+  public Field getField(String name) {
+    if (name.equals("ID")) return new IdField("\\d{8}-\\d{7}", true);
+    if (name.equals("CODE_CALL")) return new MyCodeCallField();
+    if (name.equals("INFO")) return new MyInfoField();
+    return super.getField(name);
+  }
+
+  private class MyCodeCallField extends Field {
+    @Override
+    public void parse(String field, Data data) {
+      parseCall(field, data);
+    }
+
+    @Override
+    public String getFieldNames() {
+      return "CODE CALL";
+    }
+  }
+
+  private class MyInfoField extends InfoField {
+    @Override
+    public void parse(String field, Data data) {
+      parseExtra(field, data);
+    }
   }
 }
