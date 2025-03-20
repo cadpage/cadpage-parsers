81385e860e64cb3a1010567461d4c510088deb6a
Ken Corbin <ken@cadpage.org>
2025-03-20 15:58:46 -0700

    Fixed minor problem with Cape Girardeau County, MO (E)

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/MO/MOCapeGirardeauCountyEParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/MO/MOCapeGirardeauCountyEParser.java
@@ -7,25 +7,35 @@ import net.anei.cadpage.parsers.FieldProgramParser;
 import net.anei.cadpage.parsers.MsgInfo.Data;
 
 public class MOCapeGirardeauCountyEParser extends FieldProgramParser {
-  
+
   public MOCapeGirardeauCountyEParser() {
-    super("CAPE GIRARDEAU COUNTY", "MO", 
+    super("CAPE GIRARDEAU COUNTY", "MO",
           "CFS_Number:ID! Address:ADDRCITY/S6! Common_Name:PLACE! Additional_Location_Information:INFO1! Incident_Type:CALL! Notes:INFO! Cross_Streets:X! Fire_Zone:MAP! Latitude:GPS1! Longitude:GPS2!");
   }
-  
+
   @Override
   public String getFilter() {
-    return "no-reply@zuercherportal.com>";
+    return "no-reply@zuercherportal.com,no-replyzuercher@cityofcapegirardeau.org";
   }
-  
+
   @Override
   public Field getField(String name) {
+    if (name.equals("ID")) return new MyIdField();
     if (name.equals("ADDRCITY")) return new MyAddressCityField();
     if (name.equals("INFO1")) return new MyInfo1Field();
     if (name.equals("INFO")) return new MyInfoField();
     return super.getField(name);
   }
-  
+
+  private class MyIdField extends IdField {
+    @Override
+    public void parse(String field, Data data) {
+      int pt = field.indexOf(' ');
+      if (pt >= 0) field = field.substring(0,pt).trim();
+      super.parse(field, data);
+    }
+  }
+
   private static final Pattern ADDR_ST_ZIP_PTN = Pattern.compile("(.*?), *([A-Z]{2})(?: (\\d{5}))?");
   private class MyAddressCityField extends AddressCityField {
     @Override
@@ -40,13 +50,13 @@ public class MOCapeGirardeauCountyEParser extends FieldProgramParser {
       super.parse(field,  data);
       if (data.strCity.length() == 0 && zip != null) data.strCity = zip;
     }
-    
+
     @Override
     public String getFieldNames() {
       return super.getFieldNames() + " ST";
     }
   }
-  
+
   private class MyInfo1Field extends InfoField {
     @Override
     public void parse(String field, Data data) {
@@ -54,8 +64,8 @@ public class MOCapeGirardeauCountyEParser extends FieldProgramParser {
       super.parse(field, data);
     }
   }
-  
-  private static final Pattern INFO_BRK_PTN = Pattern.compile(" *\\b\\d\\d/\\d\\d/\\d\\d \\d\\d?:\\d\\d:\\d\\d - +"); 
+
+  private static final Pattern INFO_BRK_PTN = Pattern.compile(" *\\b\\d\\d/\\d\\d/\\d\\d \\d\\d?:\\d\\d:\\d\\d - +");
   private class MyInfoField extends InfoField {
     @Override
     public void parse(String field, Data data) {
