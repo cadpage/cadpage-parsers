c29a39b599294235f6a56b73e914bc6f99af3e66
Ken Corbin <ken@cadpage.org>
2025-03-24 13:46:11 -0700

    Fixed parsing problem with Isanti County, MN

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/MN/MNIsantiCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/MN/MNIsantiCountyParser.java
@@ -1,70 +1,61 @@
 package net.anei.cadpage.parsers.MN;
 
-import net.anei.cadpage.parsers.dispatch.DispatchA43Parser;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import net.anei.cadpage.parsers.FieldProgramParser;
+import net.anei.cadpage.parsers.MsgInfo.Data;
 
 /**
  * Isanti County, MN
  */
 
-public class MNIsantiCountyParser extends DispatchA43Parser {
+public class MNIsantiCountyParser extends FieldProgramParser {
 
   public MNIsantiCountyParser() {
-    super(CITY_LIST, "ISANTI COUNTY", "MN");
+    super("ISANTI COUNTY", "MN",
+          "ID DATETIME SRC ADDR CITY ST ZIP PRI CODE CALL GPS1 GPS2 INFO UNIT! END");
   }
-  
-  private static final String[] CITY_LIST = new String[]{
-  
-      // Cities
-      "BRAHAM",
-      "CAMBRIDGE",
-      "ISANTI",
-  
-      // Townships
-      "ATHENS",
-      "BRADFORD",
-      "CAMBRIDGE",
-      "DALBO",
-      "ISANTI",
-      "MAPLE RIDGE",
-      "NORTH BRANCH",
-      "OXFORD",
-      "SPENCER BROOK",
-      "SPRINGVALE",
-      "STANCHFIELD",
-      "STANFORD",
-      "WYANETT",
-  
-      // Unincorporated communities
-      "ANDREE",
-      "ATHENS",
-      "BLOMFORD",
-      "BODUM",
-      "BRADFORD",
-      "CARMODY",
-      "CROWN",
-      "DALBO",
-      "DAY",
-      "EDGEWOOD",
-      "ELM PARK",
-      "GRANDY",
-      "OXLIP",
-      "PINE BROOK",
-      "SPENCER BROOK",
-      "SPRING LAKE",
-      "SPRINGVALE",
-      "STANLEY",
-      "STANCHFIELD",
-      "STANFORD",
-      "WALBO",
-      "WEBER",
-      "WEST POINT",
-      "WYANETT",
-      
-      // Mile Lacs County
-      "PRINCETON",
-      "SPENCER BROOK",
-      
-      // Not sure what this is, but we need to get rid of it
-      "UPPER"
-  };
+
+  @Override
+  protected boolean parseMsg(String subject, String body, Data data) {
+    if (!subject.startsWith("Active911:")) return false;
+    return parseFields(body.split(";"), data);
+  }
+
+  @Override
+  public Field getField(String name) {
+    if (name.equals("DATETIME")) return new MyDateTimeField();
+    if (name.equals("ADDR")) return new MyAddressField();
+    if (name.equals("PRI")) return new PriorityField("\\d", true);
+    return super.getField(name);
+  }
+
+  private static final Pattern DATE_TIME_PTN = Pattern.compile("(\\d{4})-(\\d\\d)-(\\d\\d)T(\\d\\d:\\d\\d:\\d\\d)\\.\\d+Z");
+  private class MyDateTimeField extends DateTimeField {
+    @Override
+    public void parse(String field, Data data) {
+      Matcher match = DATE_TIME_PTN.matcher(field);
+      if (!match.matches()) abort();
+      data.strDate = match.group(2)+'/'+match.group(3)+'/'+match.group(1);
+      data.strTime = match.group(4);
+
+    }
+  }
+
+  private class MyAddressField extends AddressField {
+    @Override
+    public void parse(String field, Data data) {
+      int pt = field.indexOf(" - ");
+      if (pt < 0) abort();
+      super.parse(field.substring(0, pt).trim(), data);
+      String place = field.substring(pt+3).trim();
+      if (!place.equals("null")) data.strPlace = place;
+    }
+
+    @Override
+    public String getFieldNames() {
+      return super.getFieldNames() + " PLACE";
+    }
+  }
 }
