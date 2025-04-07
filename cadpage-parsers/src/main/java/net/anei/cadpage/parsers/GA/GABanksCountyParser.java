418feab55551226f32962d07e0d2d8024a6fc42e
Ken Corbin <ken@cadpage.org>
2025-04-07 16:41:15 -0700

    Fixed parsing problem with Banks County, GA

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/GA/GABanksCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/GA/GABanksCountyParser.java
@@ -1,48 +1,14 @@
 package net.anei.cadpage.parsers.GA;
 
-import net.anei.cadpage.parsers.MsgInfo.Data;
-import net.anei.cadpage.parsers.MsgInfo.MsgType;
-import net.anei.cadpage.parsers.dispatch.DispatchSouthernParser;
+import net.anei.cadpage.parsers.dispatch.DispatchA71Parser;
 
-public class GABanksCountyParser extends DispatchSouthernParser {
+public class GABanksCountyParser extends DispatchA71Parser {
 
   public GABanksCountyParser() {
-    super(CITY_LIST, "BANKS COUNTY", "GA",
-          DSFLG_ADDR | DSFLG_ID | DSFLG_TIME);
+    super("BANKS COUNTY", "GA");
   }
 
-  @Override
-  public String getFilter() {
-    return "911@co.banks.ga.us";
+  public int getMapFlags() {
+    return MAP_FLG_PREFER_GPS;
   }
-
-  @Override
-  protected boolean parseMsg(String body, Data data) {
-    int pt = body.indexOf("\n----");
-    if (pt >= 0) body = body.substring(0,pt).trim();
-    if (super.parseMsg(body, data)) return true;
-    if (pt < 0) return false;
-    data.msgType = MsgType.GEN_ALERT;
-    data.strSupp = body;
-    return true;
-  }
-
-
-  private static final String[] CITY_LIST = new String[] {
-
-      // Cities
-      "BALDWIN",
-      "COMMERCE",
-      "GILLSVILLE",
-      "LULA",
-
-      // Towns
-      "ALTO",
-      "HOMER",
-      "MAYSVILLE",
-
-      // Unincorporated communities
-      "HOLLINGSWORTH",
-      "NARROWS"
-  };
 }
