63a2315904e6ebd5085bb5873b1865bd4d26534a
Ken Corbin <ken@cadpage.org>
2025-04-16 15:41:44 -0700

    Release v2.2.11-48

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/MD/MDBaltimoreCountyEParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/MD/MDBaltimoreCountyEParser.java
@@ -9,24 +9,24 @@ import java.util.regex.Pattern;
 import net.anei.cadpage.parsers.MsgParser;
 
 public class MDBaltimoreCountyEParser extends MsgParser {
-  
+
   public MDBaltimoreCountyEParser() {
     super("BALTIMORE COUNTY", "MD");
     setFieldList("MAP ID UNIT ADDR APT GPS INFO CALL");
   }
-  
+
   @Override
   public String getFilter() {
     return "cadsys@baltimorecountmd.gov";
   }
-  
-  private static final Pattern MASTER = 
-      Pattern.compile("CodeMessaging Message: b (\\d{3}-\\d{2}) +cc# (F\\d{4}-\\d{9}) +unit (\\S+) l (.*?) +#J? g (\\S+) (.*)? title (.*)");
-  
+
+  private static final Pattern MASTER =
+      Pattern.compile("CodeMessaging Message: b (\\d{3}-\\d{2}) +cc# (F\\d{4}-\\d{9}) +unit (\\S+) l (.*?) +#(\\S*) g (\\S+) (.*)? title (.*)");
+
   @Override
   protected boolean parseMsg(String subject, String body, Data data) {
     if (!subject.equals("CodeMessaging Unit Cleared")) return false;
-    
+
     body = body.replace('\t', ' ');
     Matcher match = MASTER.matcher(body);
     if (!match.matches()) return false;
@@ -35,9 +35,10 @@ public class MDBaltimoreCountyEParser extends MsgParser {
     data.strCallId = match.group(2);
     data.strUnit = match.group(3);
     parseAddress(match.group(4).trim(), data);
-    setGPSLoc(match.group(5).trim(), data);
-    data.strSupp = match.group(6).trim();
-    data.strCall = match.group(7);
+    data.strApt = append(data.strApt, "-", match.group(5));
+    setGPSLoc(match.group(6).trim(), data);
+    data.strSupp = match.group(7).trim();
+    data.strCall = match.group(8);
     return true;
   }
 }
