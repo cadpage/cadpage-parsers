a070a3c161ddcea1dfbfc64ac732e67ea1383f84
Ken Corbin <ken@cadpage.org>
2025-04-17 17:12:07 -0700

    Update sender filter for Shelby County, MO

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/MO/MOShelbyCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/MO/MOShelbyCountyParser.java
@@ -5,16 +5,16 @@ import java.util.regex.Pattern;
 import net.anei.cadpage.parsers.dispatch.DispatchA62Parser;
 
 public class MOShelbyCountyParser extends DispatchA62Parser {
-  
+
   public MOShelbyCountyParser() {
     super("SHELBY COUNTY", "MO");
   }
-  
+
   @Override
   public String getFilter() {
-    return "sc911alerts@gmail.com";
+    return "sc911alerts@gmail.com,@idsapplications.com";
   }
-  
+
   @Override
   public String adjustMapAddress(String addr) {
     addr = SHELBYNNN_PTN.matcher(addr).replaceAll("COUNTY ROAD $1");
