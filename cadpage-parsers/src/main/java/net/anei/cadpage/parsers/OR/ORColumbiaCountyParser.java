5d729e5de7babcdf0e334b7d1ae5398c5b060c93
Ken Corbin <ken@cadpage.org>
2025-05-19 12:57:59 -0700

    Fixed parsing problem with Columbia County, OR

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/OR/ORColumbiaCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/OR/ORColumbiaCountyParser.java
@@ -10,54 +10,38 @@ public class ORColumbiaCountyParser extends FieldProgramParser {
 
   public ORColumbiaCountyParser() {
     super("COLUMBIA COUNTY", "OR",
-          "ID_CALL_UNIT_CITY ADDR APT! MAP? X:X! COMMENTS:INFO? INFO/N+");
+          "ID_CALL_UNIT CITY ADDR APT! MAP? X:X! COMMENTS:INFO? INFO/N+");
   }
 
-  private static Pattern MUTUAL_AID_FORMAT = Pattern.compile("([,A-Z0-9]+) (mutual aid to [A-Z0-9]+) at (.*?)(?:; *(.*?))? (for .*?)\\.");
   private static final Pattern DELIM = Pattern.compile(" ;| (?=Map |X:|COMMENTS:)|[ ,](?=\\[\\d{1,2}\\] )");
 
   @Override
   protected boolean parseMsg(String body, Data data) {
-
-    // "mutual aid to" format
-    Matcher match = MUTUAL_AID_FORMAT.matcher(body);
-    if (match.matches()) {
-      setFieldList("UNIT CALL ADDR APT CITY PLACE INFO");
-      data.strUnit = match.group(1);
-      data.strCall = match.group(2);
-      parseAddress(match.group(3).trim(), data);
-      data.strPlace = getOptGroup(match.group(4));
-      String g5 = match.group(5);
-      if (g5.length() > 4) data.strSupp = g5; //min length is 4. ("for ")
-      return true;
-    }
-
     return parseFields(DELIM.split(body), data);
   }
 
   @Override
   public Field getField(String name) {
-    if (name.equals("ID_CALL_UNIT_CITY")) return new MyIdCallUnitCityField();
+    if (name.equals("ID_CALL_UNIT")) return new MyIdCallUnitCityField();
     if (name.equals("APT")) return new AptField("Apartment:? *(.*)", true);
     if (name.equals("MAP")) return new MapField("Map +(.*)", true);
     if (name.equals("X")) return new MyCrossField();
     return super.getField(name);
   }
 
-  private static Pattern ID_CALL_UNIT_CITY_PTN = Pattern.compile("([A-Z]{0,5}\\d{7,9}) +([A-Z0-9]+) - ([-/ A-Z0-9]+) :([A-Z0-9,]+) *(.*)");
+  private static Pattern ID_CALL_UNIT_PTN = Pattern.compile("([A-Z]{0,5}\\d{7,9}) +([A-Z0-9]+) - ([-/ A-Z0-9]+) :([A-Z0-9,]+)");
 
   private class MyIdCallUnitCityField extends Field {
 
     @Override
     public void parse(String field, Data data) {
-      Matcher match = ID_CALL_UNIT_CITY_PTN.matcher(field);
+      Matcher match = ID_CALL_UNIT_PTN.matcher(field);
       if (!match.matches()) abort();
 
       data.strCallId = match.group(1);
       data.strCode = match.group(2);
       data.strCall = match.group(3).trim();
       data.strUnit = match.group(4).trim();
-      data.strCity = match.group(5);
     }
 
     @Override
