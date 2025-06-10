f10d372619a194d33ebbb3708f6a83bbfdc24060
Ken Corbin <ken@cadpage.org>
2025-06-09 21:18:47 -0700

    Fixed parsing problem with Benton County, AR (C)

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/AR/ARBentonCountyCParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/AR/ARBentonCountyCParser.java
@@ -1,19 +1,34 @@
 package net.anei.cadpage.parsers.AR;
 
-import net.anei.cadpage.parsers.dispatch.DispatchA9Parser;
+import net.anei.cadpage.parsers.MsgInfo.Data;
+import net.anei.cadpage.parsers.dispatch.DispatchH06Parser;
 
 /**
  *Bentonville, AR
  */
-public class ARBentonCountyCParser extends DispatchA9Parser {
-  
+public class ARBentonCountyCParser extends DispatchH06Parser {
+
   public ARBentonCountyCParser() {
     super("BENTON COUNTY", "AR");
   }
-  
+
   @Override
   public String getFilter() {
-    return "BentonvillePD@bentonville.arkansas.gov";
+    return "noreply@bentonville.arkansas.gov";
+  }
+  
+  @Override
+  public int getMapFlags() {
+    return MAP_FLG_PREFER_GPS;
+  }
+
+  @Override
+  protected boolean parseHtmlMsg(String subject, String body, Data data) {
+    if (!super.parseHtmlMsg(subject, body, data)) return false;
+    if (!data.strApt.isEmpty()) {
+      data.strAddress = stripFieldEnd(data.strAddress, ' '+data.strApt);
+    }
+    return true;
   }
 }
 
