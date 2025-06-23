53faef26b6459166d3a40aa89e8e01b1de5f250e
Ken Corbin <ken@cadpage.org>
2025-06-23 12:00:16 -0700

    Fixed parsing problem with Miami County, KS (A)

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/KS/KSMiamiCountyAParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/KS/KSMiamiCountyAParser.java
@@ -1,18 +1,17 @@
 package net.anei.cadpage.parsers.KS;
 
 import net.anei.cadpage.parsers.MsgInfo.Data;
-import net.anei.cadpage.parsers.ReverseCodeSet;
+import net.anei.cadpage.parsers.CodeSet;
+import net.anei.cadpage.parsers.SmartAddressParser;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-
-import net.anei.cadpage.parsers.SmartAddressParser;
 
 public class KSMiamiCountyAParser extends SmartAddressParser {
 
   public KSMiamiCountyAParser() {
     super(CITY_LIST, "MIAMI COUNTY", "KS");
-    setFieldList("ADDR APT CITY ST PLACE X CALL UNIT ID");
+    setFieldList("CALL ADDR APT CITY ST PLACE X ID");
     setupMultiWordStreets(MWORD_STREETS);
     setupCallList(CALL_LIST);
     removeWords("PLAZA");
@@ -23,93 +22,82 @@ public class KSMiamiCountyAParser extends SmartAddressParser {
     return "noreply@sheriffmiamicountyks.gov";
   }
 
-  private static final Pattern TRAIL_ID_PTN = Pattern.compile("\\b\\d{4}-\\d{6}$");
-  private static final Pattern TRAIL_UNIT_PTN = Pattern.compile("(?:\\b(?:[A-Z]+PAGER|\\d{3,4}|[A-Z]{1,3}\\d{1,3})(?:; )?)+$");
-  private static final Pattern TRAIL_CALL_PTN = Pattern.compile("\\b([ A-Z]+(?: \\(FIRE\\))?)$");
-  private static final Pattern LEAD_CITY_ST_PTN = Pattern.compile("([A-Z ]+), *([A-Z]{2})(?: +\\d{5})? +");
-  private static final Pattern LEAD_CITY_ZIP_PTN = Pattern.compile("([A-Z ]+?) +\\d{5}\\b *");
+  private static final Pattern MASTER = Pattern.compile("Incident in progress (.*) (\\d{4}-\\d{6})");
+  private static final Pattern CITY_ST_ZIP_PTN = Pattern.compile("([A-Z ]+), ([A-Z]{2})(?: \\d{5})?\\b *(.*)");
+  private static final Pattern CITY_ZIP_PTN = Pattern.compile("([A-Z ]+) \\d{5}\\b *(.*)");
 
   @Override
   protected boolean parseMsg(String subject, String body, Data data) {
 
-    if (!subject.equals("DISPATCHED INCIDENT")) return false;
+    Matcher match = MASTER.matcher(body);
+    if (!match.matches()) return false;
+    body = match.group(1).trim();
+    data.strCallId = match.group(2);
 
-    Matcher match = TRAIL_ID_PTN.matcher(body);
-    if (!match.find()) return false;
-    body = body.substring(0,match.start()).trim();
-    data.strCallId = match.group();
-
-    match = TRAIL_UNIT_PTN.matcher(body);
-    if (match.find()) {
-      body = body.substring(0,match.start()).trim();
-      data.strUnit = match.group().replace("; ", ",");
+    Parser p = new Parser(body);
+    String callAddr = p.get(',');
+    String cityX = p.get();
+    parseAddress(StartType.START_CALL, FLAG_ANCHOR_END, callAddr.replace(" AT ", " & "), data);
+    if (data.strAddress.isEmpty()) {
+      data.strAddress = data.strCall;
+      data.strCall = "ALERT";
+    }
+    else if (data.strCall.endsWith("/") || data.strCall.endsWith("&")) {
+      data.strAddress = append(data.strCall.substring(0, data.strCall.length()-1).trim(), " & ", data.strAddress);
+      data.strCall = "ALERT";
+    }
+    else if (data.strCall.isEmpty()) {
+      data.strCall = "ALERT";
     }
 
-    String call = CALL_LIST.getCode(body);
-    if (call != null) {
-      data.strCall = call;
-      body = body.substring(0, body.length()-call.length()).trim();
-    } else {
-      match = TRAIL_CALL_PTN.matcher(body);
-      if (match.find()) {
-        data.strCall = match.group();
-        body = body.substring(0, match.start()).trim();
-      }
-      else data.strCall = "ALERT";
+    if ((match = CITY_ST_ZIP_PTN.matcher(cityX)).matches()) {
+      data.strCity = match.group(1).trim();
+      data.strState = match.group(2);
+      cityX = match.group(3);
     }
-
-    int pt = body.indexOf(',');
-    if (pt >= 0) {
-      parseAddress(body.substring(0, pt).trim(), data);
-      body = body.substring(pt+1).trim();
-
-      match = LEAD_CITY_ST_PTN.matcher(body);
-      if (match.lookingAt()) {
-        data.strCity = match.group(1).trim();
-        data.strState = match.group(2);
-        body = body.substring(match.end());
-      }
-      else if ((match = LEAD_CITY_ZIP_PTN.matcher(body)).lookingAt()) {
-        data.strCity =  match.group(1).trim();
-        body = body.substring(match.end());
-      }
-      else {
-        parseAddress(StartType.START_ADDR, FLAG_ONLY_CITY, body, data);
-        body = getLeft();
-      }
-
-      if (body.endsWith(")")) {
-        pt = body.indexOf('(');
-        if (pt < 0) return false;
-        parseAddress(StartType.START_PLACE, FLAG_ONLY_CROSS | FLAG_ANCHOR_END, body.substring(0,pt).trim(), data);
-        if (data.strCross.isEmpty()) {
-          data.strCross = data.strPlace;
-          data.strPlace = "";
-        }
-        data.strCross = append(data.strCross, " ", body.substring(pt));
-      } else {
-        data.strPlace = body;
-      }
+    else if ((match = CITY_ZIP_PTN.matcher(cityX)).matches()) {
+      data.strCity = match.group(1).trim();
+      cityX = match.group(2).trim();
     }
-
     else {
-      parseAddress(body, data);
+      parseAddress(StartType.START_ADDR, FLAG_ONLY_CITY, cityX, data);
+      cityX = getLeft();
+    }
+
+    if (!cityX.isEmpty()) {
+      String xExt = "";
+      int pt = cityX.lastIndexOf('(');
+      if (pt >= 0) {
+        xExt = cityX.substring(pt);
+        cityX = cityX.substring(0,pt).trim();
+      }
+      parseAddress(StartType.START_PLACE, FLAG_ONLY_CROSS | FLAG_ANCHOR_END, cityX, data);
+      data.strCross = append(data.strCross, " ", xExt);
     }
     return true;
   }
 
   private static final String[] MWORD_STREETS = new String[] {
+      "BETHEL CHURCH",
       "BROWN CIRCLE",
+      "CEDAR NILES",
+      "COLD WATER SPRINGS",
+      "HEDGE LANE",
       "INDUSTRIAL PARK",
+      "KANSAS CITY",
+      "LAKE MIOLA",
       "MAIN BUILDING",
       "MISSION BELLEVIEW",
+      "OSAWATOMIE PARKER",
       "PLEASANT VALLEY",
+      "PLUM CREEK",
+      "ROHRER HEIGHTS",
       "STATE HOSPITAL",
       "STATE LINE",
       "STATE PARK CAMPGROUND"
   };
 
-  private static final ReverseCodeSet CALL_LIST = new ReverseCodeSet(new String[] {
+  private static final CodeSet CALL_LIST = new CodeSet(new String[] {
       "911 MISDIAL / HANGUP",
       "ALARM CALL",
       "ALERT",
@@ -123,16 +111,23 @@ public class KSMiamiCountyAParser extends SmartAddressParser {
       "CITIZEN ASSIST",
       "CIVIL MATTER",
       "CIVIL PAPER",
+      "CIVIL STANDBY",
+      "DEBRIS IN ROADWAY",
       "DISTURBANCE",
       "EMS ASSIST",
       "EMS ASSIST (FIRE)",
+      "EXTRA PATROL",
+      "FIELD/GRASS FIRE (FIRE)",
+      "FOLLOW UP",
       "FUNERAL ESCORT",
       "GAS LEAK (FIRE)",
       "GRASS FIRE (FIRE)",
       "HARASSMENT",
       "HOUSE FIRE",
       "INJURY ACCIDENT (FIRE)",
+      "INVESTIGATION (FIRE)",
       "JUVENILE ACTIVITY",
+      "LINES DOWN (FIRE)",
       "LIVESTOCK OUT",
       "MISCELLANEOUS",
       "MOTORIST ASSIST",
@@ -146,12 +141,15 @@ public class KSMiamiCountyAParser extends SmartAddressParser {
       "SUPICIOUS ACTIVITY",
       "SUSPICIOUS PERSON",
       "SUSPICIOUS VEHICLE",
+      "TEST CALL",
       "THEFT",
       "TRAFFIC HAZARD",
       "TRAFFIC STOP",
       "Traffic Stop",
       "UNWANTED SUBJECT",
+      "VEHICLE CHECK",
       "VEHICLE FIRE",
+      "VEHICLE LOCKOUT",
       "WARRANT"
   });
 
@@ -175,6 +173,9 @@ public class KSMiamiCountyAParser extends SmartAddressParser {
       "STANTON",
       "WAGSTAFF",
       "WEA",
+
+      // Franklin County
+      "LANE",
 
       // Linn County
       "LINN COUNTY",
