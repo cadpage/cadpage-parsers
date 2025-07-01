7cfd7034bd2acd400e1886f95bc8738dccbcc75d
Ken Corbin <ken@cadpage.org>
2025-06-30 18:11:37 -0700

    Fixed parsing problem with Lewis County, KY

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/KY/KYLewisCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/KY/KYLewisCountyParser.java
@@ -1,18 +1,11 @@
 package net.anei.cadpage.parsers.KY;
 
-import net.anei.cadpage.parsers.SplitMsgOptions;
-import net.anei.cadpage.parsers.SplitMsgOptionsCustom;
-import net.anei.cadpage.parsers.dispatch.DispatchA65Parser;
+import net.anei.cadpage.parsers.dispatch.DispatchA74Parser;
 
-public class KYLewisCountyParser extends DispatchA65Parser {
+public class KYLewisCountyParser extends DispatchA74Parser {
 
   public KYLewisCountyParser() {
-    super(CITY_LIST, "LEWIS COUNTY", "KY");
-  }
-  
-  @Override
-  public SplitMsgOptions getActive911SplitMsgOptions() {
-    return new SplitMsgOptionsCustom();
+    super("LEWIS COUNTY", "KY");
   }
 
   @Override
@@ -20,76 +13,8 @@ public class KYLewisCountyParser extends DispatchA65Parser {
     return "dispatch@LewisKYE911.org";
   }
 
-  private static final String[] CITY_LIST = new String[]{
-
-      // Cities
-      "CONCORD",
-      "VANCEBURG",
-
-      // Census-designated place
-      "GARRISON",
-
-      // Other unincorporated places
-      "ALBURN",
-      "AWE",
-      "BLACK OAK",
-      "BEECHY CREEK",
-      "BUENA VISTA",
-      "BURTONVILLE",
-      "CABIN CREEK",
-      "CAMP DIX",
-      "CARRS",
-      "CHARTERS",
-      "CLARKSBURG",
-      "COTTAGEVILLE",
-      "COVEDALE",
-      "CRUM",
-      "EMERSON",
-      "EPWORTH",
-      "ESCULAPIA SPRINGS",
-      "FEARIS",
-      "FIREBRICK",
-      "FRUIT",
-      "GLENN",
-      "GLENN SPRINGS",
-      "GUN POWDER GAP",
-      "HARRIS",
-      "HEAD OF GRASSY",
-      "HESELTON",
-      "IRWIN",
-      "JACKTOWN",
-      "KINNICONICK",
-      "KIRKVILLE",
-      "LAUREL",
-      "LIBBIE",
-      "MARTIN",
-      "MCDOWELL CREEK",
-      "MCKENZIE",
-      "MONTGOMERY CREEK",
-      "NASHTOWN",
-      "NOAH",
-      "OAK RIDGE",
-      "PENCE",
-      "PETERSVILLE",
-      "POPLAR FLAT",
-      "QUICKS RUN",
-      "RANDVILLE",
-      "RECORDS",
-      "REXTON",
-      "RIBOLT",
-      "RUGLESS",
-      "SAINT PAUL",
-      "SALT LICK",
-      "SAND HILL",
-      "STRICKLETT",
-      "SULLIVAN",
-      "TANNERY",
-      "TEUTONIA",
-      "THOR",
-      "TOLLESBORO",
-      "TRINITY",
-      "UPPER BRUCE",
-      "VALLEY",
-      "WADSWORTH"
-  };
+  @Override
+  public int getMapFlags() {
+    return MAP_FLG_PREFER_GPS;
+  }
 }
