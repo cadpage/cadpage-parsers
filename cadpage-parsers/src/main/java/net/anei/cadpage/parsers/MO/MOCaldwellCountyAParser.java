2caf885a3108e5504e9c56eea631e2b8f7094238
Ken Corbin <ken@cadpage.org>
2025-06-30 17:19:56 -0700

    Fixed parsing problem with Caldwell County, MO (Add B)

---
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/MO/MOCaldwellCountyAParser.java
@@ -0,0 +1,37 @@
+package net.anei.cadpage.parsers.MO;
+
+import net.anei.cadpage.parsers.MsgInfo.Data;
+import net.anei.cadpage.parsers.dispatch.DispatchBCParser;
+
+public class MOCaldwellCountyAParser extends DispatchBCParser {
+  
+  public MOCaldwellCountyAParser() {
+    super("CALDWELL COUNTY", "MO");
+  }
+  
+  @Override
+  public String getFilter() {
+    return "CALDWELL003@CALDWELLCO.MISSOURI.ORG,DISPATCH@PUBLICSAFETYSOFTWARE.NET,DISPATCH@OMNIGO.COM,CALDWELLSHERIFF@OMNIGO.COM";
+  }
+
+  @Override
+  protected boolean parseHtmlMsg(String subject, String body, Data data) {
+    
+    if (body.startsWith("WARNING:")) {
+      int pt = body.indexOf("\n\n");
+      if (pt < 0) return false;
+      body = body.substring(pt+2).trim();
+    }
+    
+    if (!super.parseHtmlMsg(subject, body, data)) return false;
+    
+    // Subject usually has a more informative call description.
+    // then what is in the alert text
+    if (subject.length() > 0) {
+      if (subject.startsWith("STEALING INVESTIGATION")) subject = "STEALING INVESTIGATION";
+      data.strCall = subject;
+    }
+    return true;
+  }
+  
+}
--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/MO/MOCaldwellCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/MO/MOCaldwellCountyParser.java
@@ -1,37 +1,11 @@
 package net.anei.cadpage.parsers.MO;
 
-import net.anei.cadpage.parsers.MsgInfo.Data;
-import net.anei.cadpage.parsers.dispatch.DispatchBCParser;
+import net.anei.cadpage.parsers.GroupBestParser;
 
-public class MOCaldwellCountyParser extends DispatchBCParser {
-  
+
+public class MOCaldwellCountyParser extends GroupBestParser {
+
   public MOCaldwellCountyParser() {
-    super("CALDWELL COUNTY", "MO");
+    super(new MOCaldwellCountyAParser(), new MOCaldwellCountyBParser());
   }
-  
-  @Override
-  public String getFilter() {
-    return "CALDWELL003@CALDWELLCO.MISSOURI.ORG,DISPATCH@PUBLICSAFETYSOFTWARE.NET,DISPATCH@OMNIGO.COM,CALDWELLSHERIFF@OMNIGO.COM";
-  }
-
-  @Override
-  protected boolean parseHtmlMsg(String subject, String body, Data data) {
-    
-    if (body.startsWith("WARNING:")) {
-      int pt = body.indexOf("\n\n");
-      if (pt < 0) return false;
-      body = body.substring(pt+2).trim();
-    }
-    
-    if (!super.parseHtmlMsg(subject, body, data)) return false;
-    
-    // Subject usually has a more informative call description.
-    // then what is in the alert text
-    if (subject.length() > 0) {
-      if (subject.startsWith("STEALING INVESTIGATION")) subject = "STEALING INVESTIGATION";
-      data.strCall = subject;
-    }
-    return true;
-  }
-  
 }
