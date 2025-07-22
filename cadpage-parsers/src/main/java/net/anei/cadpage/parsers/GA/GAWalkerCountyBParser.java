a0194bc79c46697fe2950e6c11743f3bfd3b0264
Ken Corbin <ken@cadpage.org>
2025-07-21 20:12:56 -0700

    Fixed parsing problem with Walker County, GA (B)

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/GA/GAWalkerCountyBParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/GA/GAWalkerCountyBParser.java
@@ -1,5 +1,7 @@
 package net.anei.cadpage.parsers.GA;
 
+import java.util.regex.Pattern;
+
 import net.anei.cadpage.parsers.FieldProgramParser;
 import net.anei.cadpage.parsers.MsgInfo.Data;
 
@@ -7,7 +9,7 @@ public class GAWalkerCountyBParser extends FieldProgramParser {
 
   public GAWalkerCountyBParser() {
     super("WALKER COUNTY", "GA",
-          "CALL CALL_EXT/CS? ADDR CITY X X/CS? UNIT/Z! Quadrant:MAP! GPS! ID! END");
+          "CALL CALL_EXT/CS? ADDR/S6 CITY X X/ZCS+? PLACE UNIT/Z GPS1 GPS2 PHONE INFO/CS+? ID EMPTY END");
   }
 
   @Override
@@ -16,20 +18,41 @@ public class GAWalkerCountyBParser extends FieldProgramParser {
   }
 
   @Override
+  public int getMapFlags() {
+    return MAP_FLG_PREFER_GPS;
+  }
+
+  @Override
   protected boolean parseMsg(String subject, String body, Data data) {
     if (!subject.equals("Dispatch Information")) return false;
     body = body.replace(" Quadrant:", ", Quadrant:");
-    return parseFields(body.split(", "), data);
+    if (body.endsWith(",")) {
+      setSelectValue("1");
+      body += ' ';
+    } else {
+      setSelectValue("2");
+    }
+    return parseFields(body.split(", ", -1), data);
   }
 
   @Override
   public Field getField(String name) {
     if (name.equals("CALL_EXT")) return new CallField("Leak or Odor");
+    if (name.equals("ADDR")) return new MyAddressField();
     if (name.equals("X")) return new MyCrossField();
     if (name.equals("UNIT")) return new UnitField("[A-Z0-9,]*", true);
-    if (name.equals("GPS")) return new MyGPSField();
-    if (name.equals("ID")) return new IdField("#(\\S+?)(?: \\([A-Z]*\\d+\\))?", true);
+    if (name.equals("GPS1")) return new MyGPSField(1);
+    if (name.equals("GPS2")) return new MyGPSField(2);
+    if (name.equals("ID")) return new IdField("#?(\\d{4}-\\d+)(?: \\([A-Z]*\\d+\\))?", true);
     return super.getField(name);
+  }
+  
+  private class MyAddressField extends AddressField {
+    @Override
+    public void parse(String field, Data data) {
+      field = field.replace('@', '&');
+      super.parse(field, data);
+    }
   }
 
   private class MyCrossField extends CrossField {
@@ -40,10 +63,12 @@ public class GAWalkerCountyBParser extends FieldProgramParser {
     }
   }
 
+  private static final Pattern GPS1_PTN = Pattern.compile("[-+]?\\d{2}.\\d+");
   private class MyGPSField extends GPSField {
-    @Override
-    public void parse(String field, Data data) {
-      super.parse(field.replace('/', ','), data);
+
+    public MyGPSField(int type) {
+      super(type);
+      setPattern(GPS1_PTN);
     }
   }
 }
