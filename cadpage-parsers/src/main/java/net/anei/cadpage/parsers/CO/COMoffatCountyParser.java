08b0387b804e651b47d680c853c5e852b7c74951
Ken Corbin <ken@cadpage.org>
2025-07-24 18:01:29 -0700

    Fixed parsing problem with Moffat County,CO

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/CO/COMoffatCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/CO/COMoffatCountyParser.java
@@ -1,6 +1,5 @@
 package net.anei.cadpage.parsers.CO;
 
-import net.anei.cadpage.parsers.MsgInfo.Data;
 import net.anei.cadpage.parsers.dispatch.DispatchH03Parser;
 
 public class COMoffatCountyParser extends DispatchH03Parser {
@@ -13,11 +12,4 @@ public class COMoffatCountyParser extends DispatchH03Parser {
   public String getFilter() {
     return "CG@state.co.us,CG@csp.noreply";
   }
-
-  @Override
-  protected boolean parseHtmlMsg(String subject, String body, Data data) {
-    if (!subject.equals("Craig Ambulance MRH Notification")) return false;
-    return super.parseHtmlMsg(subject, body, data);
-  }
-
 }
