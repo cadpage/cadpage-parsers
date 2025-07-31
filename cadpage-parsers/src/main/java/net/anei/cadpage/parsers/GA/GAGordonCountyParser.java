46100f288bc0da65d5f52a09897d42829bb40e7b
Ken Corbin <ken@cadpage.org>
2025-07-31 13:42:08 -0700

    Update sender filter for Gordon County, GA

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/GA/GAGordonCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/GA/GAGordonCountyParser.java
@@ -4,14 +4,14 @@ import net.anei.cadpage.parsers.dispatch.DispatchA19Parser;
 
 
 public class GAGordonCountyParser extends DispatchA19Parser {
-  
+
   public GAGordonCountyParser() {
     super("GORDON COUNTY", "GA");
   }
-  
+
   @Override
   public String getFilter() {
-    return "gordone911@spillman.com,@ALERT.ACTIVE911.COM";
+    return "gordone911@spillman.com,@ALERT.ACTIVE911.COM,gordone911@gordoncountyga.gov";
   }
 
 }
