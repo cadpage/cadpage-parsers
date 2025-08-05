84d259c2cbecf73da8578171fb54c864b21af3f7
Ken Corbin <ken@cadpage.org>
2025-08-04 17:52:55 -0700

    Parsing problem with Jackson County, MO (C)

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/MO/MOJacksonCountyCParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/MO/MOJacksonCountyCParser.java
@@ -1,7 +1,5 @@
 package net.anei.cadpage.parsers.MO;
 
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -12,60 +10,67 @@ public class MOJacksonCountyCParser extends FieldProgramParser {
 
   public MOJacksonCountyCParser() {
     super("JACKSON COUNTY", "MO",
-          "( Sent:DATETIME1! INFO/G! INFO/N+ " +
-          "| Date:DATETIME2! ID! Type:CALL? Location:ADDR! CITY! ( Latitude:GPS1! Longitude:GPS2! | ) Mapgrid:MAP? Units_Responding:UNIT UNIT/C+ " +
-          ") END");
+          "Date:DATETIME! ID! Location:ADDR! CITY! ST ( Latitude:GPS1! Longitude:GPS2! | ) Map_Grid:MAP! Units_Responding:UNIT! CFS_#:ID/L! " +
+              "Nearest_Intersection:X! Subdivision:LINFO! Response_Zone:LINFO! Hazmat:LINFO! Other_Hazardous:LINFO! END");
   }
 
   @Override
   public String getFilter() {
-    return "firecomm@cityofls.net";
+    return "zmail.relay@cityofls.net";
   }
+
+  private static final Pattern SPC_SUBJECT_PTN = Pattern.compile("CFS - .* - #([A-Z]{2}\\d{6}-\\d{3})");
+  private static final Pattern SPC_MASTER = Pattern.compile("There is a (.*) at ([^,]+), *([ A-Z]+), ([A-Z]{2})\\b *\\d*");
 
   @Override
   protected boolean parseMsg(String subject, String body, Data data) {
-    if (!subject.equals("Incident")) return false;
-    return parseFields(body.split("[,\n]"), data);
+    Matcher match = SPC_SUBJECT_PTN.matcher(subject);
+    if (match.matches()) {
+      setFieldList("ID CODE CALL ADDR APT CITY ST");
+      data.strCallId = match.group(1);
+
+      if (body.startsWith("Incident Code:")) {
+        int pt = body.indexOf("\n", 14);
+        if (pt < 0) return false;
+        data.strCode = body.substring(14, pt).trim();
+        pt = body.indexOf("\nThere is a ");
+        if (pt < 0) return false;
+        body = body.substring(pt+1);
+        pt = body.indexOf("\nCFS:");
+        if (pt >= 0) body = body.substring(0,pt).trim();
+      }
+
+      match = SPC_MASTER.matcher(body);
+      if (!match.matches()) return false;
+      data.strCall = match.group(1);
+      parseAddress(match.group(2), data);
+      data.strCity = match.group(3).trim();
+      data.strState = match.group(4);
+      return true;
+    }
+
+    data.strCall = subject;
+    return parseFields(body.split("[,\\|]"), data);
   }
 
-  private static final DateFormat DATE_TIME1_FMT = new SimpleDateFormat("yyyy/MMM/dd HH:mm");
+  @Override
+  public String getProgram() {
+    return "CALL " + super.getProgram();
+  }
 
   @Override
   public Field getField(String name) {
-    if (name.equals("DATETIME1")) return new DateTimeField("\\d{4}/[A-Z][a-z]{2}/\\d\\d \\d\\d:\\d\\d", DATE_TIME1_FMT, true);
-    if (name.equals("DATETIME2")) return new MyDateTime2Field();
-    if (name.equals("CALL")) return new MyCallField();
-    if (name.equals("ID")) return new IdField("#(\\d*)", true);
-    if (name.equals("MAP")) return new MapField("\\d{3}[A-Z]", true);
+    if (name.equals("DATETIME")) return new DateTimeField("\\d\\d/\\d\\d/\\d\\d \\d\\d:\\d\\d", true);
+    if (name.equals("ST")) return new StateField("([A-Z]{2})\\b.*");
+    if (name.equals("UNIT")) return new MyUnitField();
     return super.getField(name);
   }
 
-  private static final Pattern DATE_TIME_PTN = Pattern.compile("(\\d{4})-(\\d\\d-\\d\\d) (\\d\\d:\\d\\d:\\d\\d)");
-  private class MyDateTime2Field extends DateTimeField {
+  private class MyUnitField extends UnitField {
     @Override
     public void parse(String field, Data data) {
-      Matcher match = DATE_TIME_PTN.matcher(field);
-      if (!match.matches()) abort();
-      data.strDate = match.group(2).replace('-', '/') + '/' + match.group(1);
-      data.strTime = match.group(3);
-    }
-  }
-
-  private static final Pattern CODE_CALL_PTN = Pattern.compile("(\\S+) - (.*)");
-  private class MyCallField extends Field {
-    @Override
-    public void parse(String field, Data data) {
-      Matcher match = CODE_CALL_PTN.matcher(field);
-      if (match.matches()) {
-        data.strCode = match.group(1);
-        field = match.group(2);
-      }
-      data.strCall = field;
-    }
-
-    @Override
-    public String getFieldNames() {
-      return "CODE CALL";
+      field = field.replace("; ", ",");
+      super.parse(field, data);
     }
   }
 }
