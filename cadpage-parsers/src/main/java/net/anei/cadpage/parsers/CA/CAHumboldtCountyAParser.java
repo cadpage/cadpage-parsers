a0006b7afceff946a456a425fa92861dce5c1832
Ken Corbin <ken@cadpage.org>
2025-08-08 18:02:02 -0700

    Fixed parsin gproblem with Humboldt County, CA (A)

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/CA/CAHumboldtCountyAParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/CA/CAHumboldtCountyAParser.java
@@ -10,19 +10,19 @@ import net.anei.cadpage.parsers.MsgParser;
  * Humbolt County, CA
  */
 public class CAHumboldtCountyAParser extends MsgParser {
-  
+
   private static final Pattern MASTER = Pattern.compile("RA: +([A-Z0-9]+); +([^;]+); +([^;]+?) +,([A-Z][^ ]*?) +(?:; *(.*?))? +X: +(-?[ \\d\\.]+?) +Y: +(-?[ \\d\\.]+?) +Inc# +(\\d+); +([^;]+?) *; *(?:Descr: *)?(.*)", Pattern.DOTALL);
-  
+
   public CAHumboldtCountyAParser() {
     super("HUMBOLDT COUNTY", "CA");
     setFieldList("SRC CALL ADDR APT CITY GPS ID UNIT INFO CH MAP");
   }
-  
+
   @Override
   public String getFilter() {
     return "huucad@fire.ca.gov";
   }
-  
+
   @Override
   public int getMapFlags() {
     return MAP_FLG_SUPPR_LA | MAP_FLG_PREFER_GPS;
@@ -36,7 +36,7 @@ public class CAHumboldtCountyAParser extends MsgParser {
     data.strSource = match.group(fld++);
     data.strCall = match.group(fld++);
     parseAddress(match.group(fld++), data);
-    data.strCity = match.group(fld++).replace('_', ' ');
+    data.strCity = stripFieldEnd(match.group(fld++).replace('_', ' '), " STN");
     data.strCall = append(data.strCall, " - ", getOptGroup(match.group(fld++)));
     setGPSLoc(match.group(fld++)+','+match.group(fld++), data);
     data.strCallId = match.group(fld++);
@@ -46,30 +46,30 @@ public class CAHumboldtCountyAParser extends MsgParser {
     if (pt >= 0) info = info.substring(0,pt).trim();
     for (String part : info.split(";")) {
       part = part.trim();
-      
+
       pt = part.indexOf("Map:");
       if (pt >= 0) {
         data.strMap = append(data.strMap, "/", part.substring(pt+4).trim());
         part = part.substring(0,pt).trim();
       }
-      
+
       String channel = "";
       pt = part.indexOf("Tac:");
       if (pt >= 0) {
         channel = part.substring(pt+4).trim();
         part = part.substring(0,pt).trim();
       }
-      
+
       pt = part.indexOf("Cmd:");
       if (pt >= 0) {
         channel = append(part.substring(pt+4).trim(), "/", channel);
         part = part.substring(0,pt).trim();
       }
-      
+
       data.strChannel = append(data.strChannel, "/", channel);
-      
+
       if ("Cmd:".startsWith(part) || "Tac:".startsWith(part) || "Map:".startsWith(part)) continue;
-      
+
       data.strSupp = append(data.strSupp, "\n", part);
     }
     return true;
