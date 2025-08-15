6b8090aedca8368bde68be9a5b097826c0d44165
Ken Corbin <ken@cadpage.org>
2025-08-14 18:02:51 -0700

    Update sender filter for Sand SPrings, OK

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/OK/OKSandSpringsParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/OK/OKSandSpringsParser.java
@@ -3,13 +3,13 @@ package net.anei.cadpage.parsers.OK;
 import net.anei.cadpage.parsers.dispatch.DispatchA19Parser;
 
 public class OKSandSpringsParser extends DispatchA19Parser {
-  
+
   public OKSandSpringsParser() {
     super("SAND SPRINGS", "OK");
   }
-  
+
   @Override
   public String getFilter() {
-    return "@sandspringsok.org";
+    return "@sandspringsok.org,@sandspringsok.gov";
   }
 }
