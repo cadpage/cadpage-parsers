6a6221b07cfce9bbcd1e8afce02dfccbaf1dabf2
Ken Corbin <ken@cadpage.org>
2025-08-25 19:43:55 -0700

    Fixed parsing problem with Middletown, OH

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/OH/OHMiddletownParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/OH/OHMiddletownParser.java
@@ -1,16 +1,28 @@
 package net.anei.cadpage.parsers.OH;
 
-import net.anei.cadpage.parsers.dispatch.DispatchA71Parser;
+import net.anei.cadpage.parsers.FieldProgramParser;
+import net.anei.cadpage.parsers.MsgInfo.Data;
 
-public class OHMiddletownParser extends DispatchA71Parser {
+public class OHMiddletownParser extends FieldProgramParser {
 
   public OHMiddletownParser() {
-    super("MIDDLETOWN", "OH");
+    super("MIDDLETOWN", "OH",
+          "dispatch:UNIT! location:ADDR! city:CITY! state:ST! callType:CALL! category:SKIP! narrative:CALL/L! coordinates:GPS! incidentId:ID! timestamp:SKIP! END");
   }
 
   @Override
   public String getFilter() {
-    return "cad@cityofmiddletown.org";
+    return "no-reply@bryx.com";
   }
 
+  @Override
+  public int getMapFlags() {
+    return MAP_FLG_PREFER_GPS;
+  }
+
+  @Override
+  protected boolean parseMsg(String subject, String body, Data data) {
+    if (!subject.startsWith("[BRYX Dispatch]")) return false;
+    return parseFields(body.split("\n"), data);
+  }
 }
