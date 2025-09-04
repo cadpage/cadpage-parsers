d07890e018b50728a1218304e4364d3710023df0
Ken Corbin <ken@cadpage.org>
2025-09-04 16:50:14 -0700

    Update sender filter for Todd County, KY

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/KY/KYToddCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/KY/KYToddCountyParser.java
@@ -4,13 +4,13 @@ import net.anei.cadpage.parsers.dispatch.DispatchA27Parser;
 
 
 public class KYToddCountyParser extends DispatchA27Parser {
-  
+
   public KYToddCountyParser() {
     super("TODD COUNTY", "KY","[A-Z]{1,3}FD|[A-Z]+\\d+|\\d{8}");
   }
-  
+
   @Override
   public String getFilter() {
-    return "noreply@cisusa.org,noreply@toddcogov.com";
+    return "noreply@cisusa.org,noreply@toddcogov.com,dispactive911@toddcogov.com";
   }
 }
