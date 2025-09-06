f7629b671983f55a141966d1166dce442723e50d
Ken Corbin <ken@cadpage.org>
2025-09-06 13:28:49 -0700

    Update GPS lookup table for Sanduskky County, OH

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/OH/OHSanduskyCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/OH/OHSanduskyCountyParser.java
@@ -1,11 +1,14 @@
 package net.anei.cadpage.parsers.OH;
 
+import java.util.Properties;
+
 import net.anei.cadpage.parsers.dispatch.DispatchA77Parser;
 
 public class OHSanduskyCountyParser extends DispatchA77Parser {
 
   public OHSanduskyCountyParser() {
     super("Fire Call", "SANDUSKY COUNTY", "OH");
+    setupGpsLookupTable(GPS_LOOKUP_TABLE);
   }
 
   @Override
@@ -17,5 +20,9 @@ public class OHSanduskyCountyParser extends DispatchA77Parser {
   public int getMapFlags() {
     return MAP_FLG_PREFER_GPS;
   }
+
+  private static final Properties GPS_LOOKUP_TABLE = buildCodeTable(new String[] {
+      "888 N CR 260",                         "+41.366375,-82.954555"
+  });
 
 }
