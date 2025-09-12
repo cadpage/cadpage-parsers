eb67b7ee71602f156ebcabd2e3ace425f738f7b5
Ken Corbin <ken@cadpage.org>
2025-09-11 18:29:33 -0700

    Fixed parsing problem with Clinton County, MO (A)

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/MO/MOClintonCountyAParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/MO/MOClintonCountyAParser.java
@@ -1,42 +1,73 @@
 package net.anei.cadpage.parsers.MO;
 
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import net.anei.cadpage.parsers.MsgInfo.Data;
-import net.anei.cadpage.parsers.dispatch.DispatchA25Parser;
+import net.anei.cadpage.parsers.MsgParser;
 
 
-public class MOClintonCountyAParser extends DispatchA25Parser {
-  
-  private static final Pattern TOWN_AND_COUNTRY_PTN0 = Pattern.compile("\\b(TOWN) +& +(COUNTRY)\\b", Pattern.CASE_INSENSITIVE);
-  private static final Pattern TOWN_AND_COUNTRY_PTN1 = Pattern.compile("\\b(TOWN) +(AND) +(COUNTRY)\\b", Pattern.CASE_INSENSITIVE);
-  private static final Pattern TOWN_AND_COUNTRY_PTN2 = Pattern.compile("\\b(TOWN)_(AND)_(COUNTRY)\\b", Pattern.CASE_INSENSITIVE);
+public class MOClintonCountyAParser extends MsgParser {
   
   public MOClintonCountyAParser() {
     super("CLINTON COUNTY", "MO");
+    setFieldList("CALL CODE ADDR APT CITY ST PLACE INFO");
   }
   
   @Override
   public String getFilter() {
-    return "@clintoncosheriff.org,@plattsburgfire.org";  
+    return "centralsquare@cameronmo.com";  
   }
+  
+  private static final Pattern INFO_BRK_PTN = Pattern.compile("[; ]+\\d\\d/\\d\\d/\\d\\d \\d\\d:\\d\\d:\\d\\d - *");
+  private static final Pattern MASTER = Pattern.compile("(?:([A-Z]+) )?None +(.*)");
+  private static final Pattern ST_ZIP_PTN = Pattern.compile("([A-Z]{2})(?: +\\d{5})?");
   
   @Override
   protected boolean parseMsg(String subject, String body, Data data) {
-    body = TOWN_AND_COUNTRY_PTN0.matcher(body).replaceAll("$1 and $2");
-    body = adjustMapAddress(body);
-    if (!super.parseMsg(subject, body, data)) return false;
-    data.strAddress = postAdjustMapAddress(data.strAddress);
+    
+    if (subject.isEmpty()) return false;
+    data.strCall = subject;
+    
+    String[] parts = INFO_BRK_PTN.split(body);
+    body = parts[0];
+    
+    Matcher match = MASTER.matcher(body);
+    if (!match.matches()) return false;
+    data.strCode = getOptGroup(match.group(1));
+    body =  match.group(2);
+    
+    String addr;
+    int brk = findDuplicate(body);
+    if (brk >= 0) {
+      addr = body.substring(0, brk).trim();
+      data.strPlace = body.substring(brk+addr.length()).trim();
+    } else return false;
+    
+    Parser p = new Parser(addr);
+    String city = p.getLastOptional(',');
+    match = ST_ZIP_PTN.matcher(city);
+    if (match.matches()) {
+      data.strState = match.group(1);
+      city = p.getLastOptional(',');
+    }
+    parseAddress(p.get(), data);
+    data.strCity = city;
+    
+    for (int j = 1; j < parts.length; j++) {
+      data.strSupp = append(data.strSupp, "\n", parts[j]);
+    }
     return true;
   }
-
-  @Override
-  public String adjustMapAddress(String addr) {
-    return TOWN_AND_COUNTRY_PTN1.matcher(addr).replaceAll("$1_$2_$3");
-  }
   
-  @Override
-  public String postAdjustMapAddress(String addr) {
-    return TOWN_AND_COUNTRY_PTN2.matcher(addr).replaceAll("$1 $2 $3");
+  private int findDuplicate(String field) {
+    int k = (field.length()+1)/2;
+    for (int j = k; j > 0; j--) {
+      if (field.charAt(j) != ' ') k = j;
+      if (field.charAt(j) == ' ' && field.charAt(j-1) != ' ') {
+        if (field.substring(k).startsWith(field.substring(0, j))) return k;
+      }
+    }
+    return -1;
   }
 }
