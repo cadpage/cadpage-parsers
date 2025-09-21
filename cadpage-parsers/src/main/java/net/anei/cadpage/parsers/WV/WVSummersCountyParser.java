00d54fe71e656f6fe5ecf3f7bc4d66dd9a3e976b
Ken Corbin <ken@cadpage.org>
2025-09-20 18:53:36 -0700

    Parsing problem with Summers COunty, WV

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/WV/WVSummersCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/WV/WVSummersCountyParser.java
@@ -1,58 +1,20 @@
 package net.anei.cadpage.parsers.WV;
 
-import net.anei.cadpage.parsers.dispatch.DispatchA46Parser;
+import net.anei.cadpage.parsers.dispatch.DispatchA19Parser;
 
-public class WVSummersCountyParser extends DispatchA46Parser {
+public class WVSummersCountyParser extends DispatchA19Parser {
 
   public WVSummersCountyParser() {
-    super(CITY_LIST, "SUMMERS COUNTY", "WV");
+    super("SUMMERS COUNTY", "WV");
   }
 
   @Override
   public String getFilter() {
-    return "summerscounty911@pagingpts.com";
+    return "FlexRapidNotification@dccnotify.com";
   }
 
   @Override
   public int getMapFlags() {
     return MAP_FLG_PREFER_GPS;
   }
-
-  private static final String[] CITY_LIST = new String[] {
-
-      // City
-      "HINTON",
-
-      // Magisterial districts
-      "BLUESTONE RIVER",
-      "FOREST HILL",
-      "GREEN SULPHUR",
-      "GREENBRIER",
-      "GREENBRIER RIVER",
-      "JUMPING BRANCH",
-      "NEW RIVER",
-      "PIPESTEM",
-      "TALCOTT",
-
-      // Other places
-      "BROOKS",
-      "BUCK",
-      "ELTON",
-      "FOREST HILL",
-      "GREEN SULPHUR SPRINGS",
-      "HIX",
-      "INDIAN MILLS",
-      "JUMPING BRANCH",
-      "LOWELL",
-      "MARIE",
-      "NIMITZ",
-      "PENCE SPRINGS",
-      "PIPESTEM",
-      "SANDSTONE",
-      "TALCOTT",
-
-      //  Greenbriar County
-      "ALDERSON"
-  };
-
 }
