40eb3128046a5f0d0447bf7734ad5261016bd274
Ken Corbin <ken@cadpage.org>
2025-10-28 15:13:41 -0700

    Update sender filter for Bingham County, ID (B)

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/ID/IDBinghamCountyBParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/ID/IDBinghamCountyBParser.java
@@ -3,13 +3,13 @@ package net.anei.cadpage.parsers.ID;
 import net.anei.cadpage.parsers.dispatch.DispatchA19Parser;
 
 public class IDBinghamCountyBParser extends DispatchA19Parser {
-  
+
   public IDBinghamCountyBParser() {
     super("BINGHAM COUNTY", "ID");
   }
-  
+
   @Override
   public String getFilter() {
-    return "FlexRapidNotification@dccnotify.com";
+    return "FlexRapidNotification@dccnotify.com,@email.getrave.com";
   }
 }
