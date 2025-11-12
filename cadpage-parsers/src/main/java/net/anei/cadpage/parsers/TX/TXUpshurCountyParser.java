7e9e0c1c606095769c3c303f4b8d3e5e77c589ad
Ken Corbin <ken@cadpage.org>
2025-11-12 15:15:41 -0800

    Fixed parsing problem with Upshur County, TX

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/TX/TXUpshurCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/TX/TXUpshurCountyParser.java
@@ -10,27 +10,33 @@ public class TXUpshurCountyParser extends FieldProgramParser {
 
   public TXUpshurCountyParser() {
     super("UPSHUR COUNTY", "TX",
-          "CALL GPS1 GPS2 ADDRCITYST! INFO/N+");
+          "CALL GPS1 GPS2 ADDRCITYST! INFO/N+? ID DATETIME END");
   }
 
   @Override
   public String getFilter() {
     return "no-reply.zuercher@countyofupshur.com";
   }
+  
+  @Override
+  public int getMapFlags() {
+    return MAP_FLG_PREFER_GPS | MAP_FLG_SUPPR_LA;
+  }
 
   private static final Pattern DELIM = Pattern.compile(" *; +");
 
   @Override
   protected boolean parseMsg(String subject, String body, Data data) {
+    body = stripFieldStart(body, "None ");
     if (body.endsWith(";")) body += ' ';
-    String[] flds = DELIM.split(body);
-    if (!subject.equals(flds[0])) return false;
-    return parseFields(flds, data);
+    return parseFields(DELIM.split(body), data);
   }
 
   @Override
   public Field getField(String name) {
     if (name.equals("INFO")) return new MyInfoField();
+    if (name.equals("ID")) return new IdField("CFS\\d{3,}", true);
+    if (name.equals("DATETIME")) return new DateTimeField("\\d\\d/\\d\\d/\\d\\d \\d\\d:\\d\\d:\\d\\d", true);
     return super.getField(name);
   }
 
