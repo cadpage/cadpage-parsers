d332ab8b7e18d0d14349c2be349077a099c1b7a8
Ken Corbin <ken@cadpage.org>
2025-11-13 13:39:26 -0800

    Fixed parsing problem with Warren County, NJ

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/NJ/NJWarrenCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/NJ/NJWarrenCountyParser.java
@@ -14,7 +14,7 @@ public class NJWarrenCountyParser extends SmartAddressParser {
 
   public NJWarrenCountyParser() {
     super("WARREN COUNTY", "NJ");
-    setFieldList("SRC CALL UNIT PLACE ADDR APT X ID INFO");
+    setFieldList("SRC CALL UNIT PLACE ADDR APT X ID GPS INFO");
   }
 
   @Override
@@ -22,7 +22,8 @@ public class NJWarrenCountyParser extends SmartAddressParser {
     return "WC911@co.warren.nj.us,root@co.warren.nj.us";
   }
 
-  private static final Pattern MASTER = Pattern.compile("([A-Z0-9]+) ALERT: (.*?) \\(([A-Z0-9]+) *\\) *([^,]*?), NEAR: ([^:]*?):([^:]*) (\\d{8})(?: :: (.*))?");
+  private static final Pattern MASTER = Pattern.compile("([A-Z0-9]+)(?: ALERT)?: (.*?) \\(([A-Z0-9]+) *\\) *([^,]*?),(?: NEAR: ([^:]*?):)?([^:]*) (\\d{8})(?: :: (.*))?");
+  private static final Pattern GPS_PTN = Pattern.compile("([-+]?\\d{2}\\.\\d{6} [-+]?\\d{2}\\.\\d{6})\\b[/ ]*");
 
   @Override
   public boolean parseMsg(String body, Data data) {
@@ -38,10 +39,16 @@ public class NJWarrenCountyParser extends SmartAddressParser {
       addr = addr.substring(pt+1).trim();
     }
     parseAddress(StartType.START_ADDR, FLAG_RECHECK_APT | FLAG_ANCHOR_END, addr, data);
-    data.strCross = match.group(5).trim();
+    data.strCross = getOptGroup(match.group(5));
     data.strCall = append(data.strCall, " - ", match.group(6).trim());
     data.strCallId = match.group(7);
-    data.strSupp = getOptGroup(match.group(8));
+    String info = getOptGroup(match.group(8));
+    match  = GPS_PTN.matcher(info);
+    if (match.lookingAt()) {
+      setGPSLoc(match.group(1), data);
+      info = info.substring(match.end());
+    }
+    data.strSupp = info;
     return true;
   }
 }
