01069eca2731451843cabe8c075979190838d764
Ken Corbin <ken@cadpage.org>
2025-11-21 10:51:53 -0800

    Update A911 parser table

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/Active911ParserTable.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/Active911ParserTable.java
@@ -2408,6 +2408,7 @@ public class Active911ParserTable {
       "US/PA/OxfordFire",                 "PAChesterCountyO",
       "US/PA/Padoh",                      "PABethlehem",
       "US/PA/PennBoroVFD",                "PAWestmorelandCountyA",
+      "US/PA/PennHillsPolice",            "PAAlleghenyCountyA",
       "US/PA/PennHillsSta227",            "PAAlleghenyCountyA",
       "US/PA/PennsylvaniaAmbulance",      "PALackawannaCountyB",
       "US/PA/PerryCounty",                "PAPerryCounty",
