22abbdd483fa31035041c1295a40da8466fcc069
Ken Corbin <ken@cadpage.org>
2025-11-24 18:22:56 -0800

    Fixed parsing problem with Armstrong COunty, PA (E)

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/PA/PAArmstrongCountyEParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/PA/PAArmstrongCountyEParser.java
@@ -9,13 +9,18 @@ public class PAArmstrongCountyEParser extends DispatchH05Parser {
     super("ARMSTRONG COUNTY", "PA",
           "EMS_CFS_Type:SKIP! Fire_CFS_Type:SKIP! Nature_of_Call:CALL! Narrative:EMPTY! INFO_BLK+ Location:ADDRCITY! ( Latitude:GPS1! Longitude:GPS2! | ) " +
               "Cross_Streets:X? Common_Name:PLACE? Additional_Info:INFO/N? Info:INFO/N? Call_Date/Time:DATETIME! Call_Number:ID! Units:UNIT? " +
-              "( Fire_Quadrant:MAP! | Fire_Quandrant:MAP! ) Ems_District:MAP/L! Status_Times:EMPTY TIMES+ Caller_Name:NAME Caller_Phone:PHONE");
+              "( Fire_Quadrant:MAP! | Fire_Quandrant:MAP! ) ( Ems_District:MAP/L! | EMS_District:MAP/L! ) Status_Times:EMPTY TIMES+ Caller_Name:NAME Caller_Phone:PHONE");
     setAccumulateUnits(true);
   }
 
   @Override
   public String getFilter() {
     return "911dispatch@co.armstrong.pa.us,noreply@co.armstrong.pa.us";
+  }
+  
+  @Override
+  public int getMapFlags() {
+    return MAP_FLG_PREFER_GPS;
   }
 
   @Override
