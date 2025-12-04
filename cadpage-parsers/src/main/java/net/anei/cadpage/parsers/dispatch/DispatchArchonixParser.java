72b3547a4016091e93626036eaa162457675d9a9
Ken Corbin <ken@cadpage.org>
2025-12-03 16:46:22 -0800

    Fixed parsin gproblem with Upshur County, TX

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/dispatch/DispatchArchonixParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/dispatch/DispatchArchonixParser.java
@@ -34,7 +34,8 @@ public class DispatchArchonixParser extends FieldProgramParser {
   public DispatchArchonixParser(Properties cityCodes, Properties maCityCodes, String defCity, String defState, int flags) {
     super(defCity, defState,
           "( ADDRCITY/ZaSXP! ( #:APT MI#:ID! | MI#:ID! ) TIMES! TIMES+? RES#:UNIT " +
-          "| CALL ADDRCITY/aSXP! #:APT X:X BOX:BOX? ZN:MAP? CP:PLACE UNIT Time:DATETIME? MI#:ID Lat/Lon:GPS? RES#:UNIT )");
+          "| CALL ADDRCITY/aSXP! #:APT X:X BOX:BOX? ZN:MAP? CP:PLACE UNIT Time:DATETIME? MI#:ID Lat/Lon:GPS? RES#:UNIT " + 
+          ") INFO/N+");
     this.cityCodes = cityCodes;
     this.maCityCodes = maCityCodes;
     twoPartCity = (flags & ARCH_FLG_TWO_PART_CITY) != 0;
