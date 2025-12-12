521914de2ef4a987132cd8906de629ddaa6c5646
Ken Corbin <ken@cadpage.org>
2025-12-11 17:23:19 -0800

    Update sender filter for Rabun County, GA

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/GA/GARabunCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/GA/GARabunCountyParser.java
@@ -5,30 +5,30 @@ import net.anei.cadpage.parsers.MsgInfo.Data;
 
 
 public class GARabunCountyParser extends FieldProgramParser {
- 
+
   public GARabunCountyParser() {
-    super("RABUN COUNTY", "GA", 
-          "CFS_Number:ID! Incident_Type:CALL! Location_Details:PLACE! Address:ADDRCITYST! Address_Name:PLACE! Cross_Streets:X! " + 
+    super("RABUN COUNTY", "GA",
+          "CFS_Number:ID! Incident_Type:CALL! Location_Details:PLACE! Address:ADDRCITYST! Address_Name:PLACE! Cross_Streets:X! " +
               "CFS_Latitude:GPS1! CFS_Longitude:GPS2! Please_respond_immediately.%EMPTY! END");
   }
-  
+
   @Override
   public String getFilter() {
-    return "no-reply@zuercherportal.com";
+    return "no-reply@zuercherportal.com,rabun911@rabuncounty.ga.gov";
   }
-  
+
   @Override
   public boolean parseMsg(String subject, String body, Data data) {
     if (subject.isEmpty()) return false;
     return parseFields(body.split(" // "), data);
   }
-  
+
   @Override
   public Field getField(String name) {
     if (name.equals("PLACE")) return new MyPlaceField();
     return super.getField(name);
   }
-  
+
   private class MyPlaceField extends PlaceField {
     @Override
     public void parse(String field, Data data) {
