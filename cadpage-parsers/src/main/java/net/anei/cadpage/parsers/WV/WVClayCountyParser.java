802c897659f3c8fe09f2f8e0f3293a6bfbb6e310
Ken Corbin <ken@cadpage.org>
2025-12-11 17:21:06 -0800

    Update sender filter for Clay County, WV

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/WV/WVClayCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/WV/WVClayCountyParser.java
@@ -3,14 +3,14 @@ package net.anei.cadpage.parsers.WV;
 import net.anei.cadpage.parsers.dispatch.DispatchSPKParser;
 
 public class WVClayCountyParser extends DispatchSPKParser {
-  
+
   public WVClayCountyParser() {
     super("CLAY COUNTY", "WV");
   }
-  
+
   @Override
   public String getFilter() {
-    return "text.clay911@gmail.com,ClayCoCAD@911.com";
+    return "text.clay911@gmail.com,ClayCoCAD@911.com,ClayCoCAD@lightrr.com";
   }
 
 }
