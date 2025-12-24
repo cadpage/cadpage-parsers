ce2f2fad0f763f08fabd934f153bffb131de9721
Ken Corbin <ken@cadpage.org>
2025-12-23 18:11:06 -0800

    Minor fixes for Dauphin County, PA (C)

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/PA/PADauphinCountyCParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/PA/PADauphinCountyCParser.java
@@ -1,5 +1,8 @@
 package net.anei.cadpage.parsers.PA;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import net.anei.cadpage.parsers.FieldProgramParser;
 import net.anei.cadpage.parsers.MsgInfo.Data;
 
@@ -25,6 +28,7 @@ public class PADauphinCountyCParser extends FieldProgramParser {
     if (name.equals("ADDR")) return new MyAddressField();
     if (name.equals("DATE")) return new DateField("(\\d\\d?/\\d\\d?/\\d{4})\\b.*", true);
     if (name.equals("TIME")) return new TimeField("\\d\\d?:\\d\\d:\\d\\d", true);
+    if (name.equals("INFO")) return new MyInfoField();
     return super.getField(name);
   }
 
@@ -36,6 +40,21 @@ public class PADauphinCountyCParser extends FieldProgramParser {
       String apt = p.getLastOptional(" - ");
       super.parse(p.get(), data);
       data.strApt = append(data.strApt, "-", apt);
+    }
+
+    @Override
+    public String getFieldNames() {
+      return super.getFieldNames() + " PLACE";
+    }
+  }
+
+  private static final Pattern INFO_HDR_PTN = Pattern.compile("\\d\\d?/\\d\\d?/\\d\\d(?:\\d\\d)? \\d\\d?:\\d\\d:\\d\\d\\b *");
+  private class MyInfoField extends InfoField {
+    @Override
+    public void parse(String field, Data data) {
+      Matcher match = INFO_HDR_PTN.matcher(field);
+      if (match.lookingAt()) field = field.substring(match.end());
+      super.parse(field, data);
     }
   }
 }
