35c744a6ceac059cb0071662d9c191b0ac99b27f
Ken Corbin <ken@cadpage.org>
2026-01-06 16:08:22 -0800

    Update sender filter for Comal County, TX (A&B)

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/TX/TXComalCountyBParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/TX/TXComalCountyBParser.java
@@ -3,16 +3,16 @@ package net.anei.cadpage.parsers.TX;
 import net.anei.cadpage.parsers.dispatch.DispatchA19Parser;
 
 public class TXComalCountyBParser extends DispatchA19Parser {
-  
+
   public TXComalCountyBParser() {
     super("COMAL COUNTY", "TX");
   }
 
   @Override
   public String getFilter() {
-    return "FlexRapidNotification@dccnotify.com";
+    return "FlexRapidNotification@dccnotify.com,@getrave.com";
   }
-  
+
   @Override
   public int getMapFlags() {
     return MAP_FLG_PREFER_GPS | MAP_FLG_SUPPR_LA;
