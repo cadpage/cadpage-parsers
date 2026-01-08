ef56caee9edb22077b94b0b2b4be1298b9b0b024
Ken Corbin <ken@cadpage.org>
2026-01-08 15:50:55 -0800

    Update msg doc

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/WV/WVWyomingCountyBParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/WV/WVWyomingCountyBParser.java
@@ -3,11 +3,11 @@ package net.anei.cadpage.parsers.WV;
 import net.anei.cadpage.parsers.dispatch.DispatchA19Parser;
 
 public class WVWyomingCountyBParser extends DispatchA19Parser {
-  
+
   public WVWyomingCountyBParser() {
     super("WYOMING COUNTY", "WV");
   }
-  
+
   @Override
   public String getFilter() {
     return "FlexRapidNotification@dccnotify.com";
