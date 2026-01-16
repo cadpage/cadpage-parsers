935103410c7538e23a128ff63f212d2a306efcc4
Ken Corbin <ken@cadpage.org>
2026-01-15 18:00:28 -0800

    Fixed parsing problem with Claiborne County, TN

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/dispatch/DispatchC04Parser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/dispatch/DispatchC04Parser.java
@@ -11,7 +11,15 @@ public class DispatchC04Parser extends FieldProgramParser {
 
   public DispatchC04Parser(String defCity, String defState) {
     super(defCity, defState,
-          "CFS:ID! CALL! ADDRCITYST! ( GPS! | X_PLACE GPS! | ) ( X_PLACE UNIT_TIMES! | UNIT_TIMES! | X_PLACE PHONE! NAME FAIL Received:TIMES! | PHONE? NAME FAIL Received:TIMES! ) TIMES/N+");
+          "CFS:ID! CALL! ADDRCITYST! ADDR2? ( GPS! | X_PLACE GPS! | ) " +
+              "( X_PLACE UNIT_TIMES! | UNIT_TIMES! " +
+              "| Received:TIMES! " +
+              "| X_PLACE PHONE! NAME FAIL Received:TIMES! " +
+              "| PHONE! NAME FAIL Received:TIMES! " +
+              "| X_PLACE NAME! FAIL Received:TIMES! " +
+              "| NAME FAIL Received:TIMES! " +
+              ") TIMES/N+");
+
   }
 
   @Override
@@ -37,10 +45,12 @@ public class DispatchC04Parser extends FieldProgramParser {
   @Override
   public Field getField(String name) {
     if (name.equals("ADDRCITYST")) return new BaseAddressCityStateField();
+    if (name.equals("ADDR2")) return new BaseAddress2Field();
     if (name.equals("UNIT_TIMES")) return new SkipField("Unit Times", true);
     if (name.equals("X_PLACE")) return new BaseCrossPlaceField();
     if (name.equals("GPS")) return new GPSField("\\d+\\.\\d+, -\\d+\\.\\d+", true);
     if (name.equals("PHONE")) return new PhoneField("\\d{10}", true);
+    if (name.equals("NAME")) return new BaseNameField();
     if (name.equals("TIMES")) return new BaseTimesField();
     return super.getField(name);
   }
@@ -55,6 +65,15 @@ public class DispatchC04Parser extends FieldProgramParser {
       Matcher match = SHORT_ZIP_PTN.matcher(field);
       if (match.matches()) field = match.group(1);
       super.parse(field, data);
+    }
+  }
+
+  private class BaseAddress2Field extends AddressField {
+    @Override
+    public boolean checkParse(String field, Data data) {
+      if (!data.strAddress.isEmpty()) return false;
+      super.parse(field, data);
+      return true;
     }
   }
 
@@ -75,7 +94,26 @@ public class DispatchC04Parser extends FieldProgramParser {
     public String getFieldNames() {
       return "PLACE X";
     }
+  }
 
+  private class BaseNameField extends NameField {
+    @Override
+    public boolean canFail() {
+      return true;
+    }
+
+    @Override
+    public boolean checkParse(String field, Data data) {
+      if (!field.contains(",")) return false;
+      parse(field, data);
+      return true;
+    }
+
+    @Override
+    public void parse(String field, Data data) {
+      field = stripFieldEnd(field, ",");
+      super.parse(field, data);
+    }
   }
 
   private class BaseTimesField extends InfoField {
