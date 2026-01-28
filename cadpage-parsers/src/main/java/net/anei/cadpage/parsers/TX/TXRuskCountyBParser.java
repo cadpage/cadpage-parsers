e76e225d4b112b0f548742d79c216dc5619865ad
Ken Corbin <ken@cadpage.org>
2026-01-28 12:00:38 -0800

    Update sender filter for Rusk County, TX (B)

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/TX/TXRuskCountyBParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/TX/TXRuskCountyBParser.java
@@ -9,12 +9,12 @@ public class TXRuskCountyBParser extends DispatchA19Parser {
   public TXRuskCountyBParser() {
     super("RUSK COUNTY", "TX");
   }
-  
+
   @Override
   public String getFilter() {
-    return "FlexRapidNotification@dccnotify.com";
+    return "FlexRapidNotification@dccnotify.com,@email.getrave.com";
   }
-  
+
   @Override
   public int getMapFlags() {
     return MAP_FLG_PREFER_GPS | MAP_FLG_SUPPR_LA;
