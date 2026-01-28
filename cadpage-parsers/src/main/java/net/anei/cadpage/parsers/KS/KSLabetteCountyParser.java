070a679ac28ef9dcf90fc38b7261286ebe097495
Ken Corbin <ken@cadpage.org>
2026-01-27 17:52:07 -0800

    Update sender filter for Labette County, KS

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/KS/KSLabetteCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/KS/KSLabetteCountyParser.java
@@ -3,13 +3,13 @@ package net.anei.cadpage.parsers.KS;
 import net.anei.cadpage.parsers.dispatch.DispatchBCParser;
 
 public class KSLabetteCountyParser extends DispatchBCParser {
-  
+
   public KSLabetteCountyParser() {
     super("LABETTE COUNTY", "KS");
   }
-  
+
   @Override
   public String getFilter() {
-    return "DISPATCH@LABETTECOUNTY.COM,LABETTECOUNTY@OMNIGO.COM";
+    return "DISPATCH@LABETTECOUNTY.COM,LABETTECOUNTY@OMNIGO.COM,noreply@omnigo.com";
   }
 }
