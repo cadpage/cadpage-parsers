6b6e160a23358b8f04f11f9025f79709ba3fe1c8
Ken Corbin <ken@cadpage.org>
2026-02-05 15:13:53 -0800

    Fixed parsing problem with Walla Walla County, WA

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/OR/ORUmatillaCountyBParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/OR/ORUmatillaCountyBParser.java
@@ -8,8 +8,23 @@ import net.anei.cadpage.parsers.MsgInfo.Data;
 public class ORUmatillaCountyBParser extends HtmlProgramParser {
 
   public ORUmatillaCountyBParser() {
-    super("UMATILLA COUNTY", "OR",
-          "CALL_DATE_/_TIME:DATETIME! FIRE_CALL_TYPE:CALL! EMS_CALL_TYPE:CALL! COMMON_NAME:PLACE! ADDRESS:ADDRCITY/S6! APT/SUITE:APT! CROSS_STREET:X! FIRE_QUAD:MAP! NATURE_OF_CALL:CALL/SDS! DISPATCH_COMMENTS:EMPTY! INFO/N+ LOCAL_INFO:INFO/N! INFO/N+ ALERTS:INFO/N! INFO/N+ INCIDENT_#:ID! UNITS:UNIT! STATUS_TIMES:EMPTY! SKIP+ CALLER_NAME:NAME! CALLER_PHONE_#:PHONE! CALLER_ADDRESS:SKIP!");
+    this("UMATILLA COUNTY", "OR");
+  }
+
+  public ORUmatillaCountyBParser(String defCity, String defState) {
+    super(defCity, defState,
+          "CALL_DATE_/_TIME:DATETIME! " +
+                "( FIRE_CALL_TYPE:CALL! EMS_CALL_TYPE:CALL! COMMON_NAME:PLACE! ADDRESS:ADDRCITY/S6! APT/SUITE:APT! CROSS_STREET:X! " +
+                    "FIRE_QUAD:MAP! NATURE_OF_CALL:CALL/SDS! DISPATCH_COMMENTS:EMPTY! INFO/N+ LOCAL_INFO:INFO/N! INFO/N+ ALERTS:ALERT! INFO/N+ " +
+                    "INCIDENT_#:ID! UNITS:UNIT! STATUS_TIMES:EMPTY! SKIP+ CALLER_NAME:NAME! CALLER_PHONE_#:PHONE! CALLER_ADDRESS:SKIP! " +
+                "| COMMON_NAME:PLACE! ADDRESS:ADDRCITY/S6! APT/SUITE:APT! CROSS_STREET:X! LATITUDE:GPS! " +
+                    "FIRE_CALL_TYPE:CALL/SDS? FIRE_CALL_DESCRIPTION:CALL/SDS? EMS_CALL_TYPE:CALL/SDS? EMS_CALL_DESCRIPTION:CALL/SDS? CALL_TYPE:CALL/SDS? NATURE_OF_CALL:CALL/SDS! " +
+                    "DISPATCH_COMMENTS:EMPTY! INFO/N+ LOCAL_INFO:INFO/N? INFO/N+ ALERTS:ALERT! INCIDENT_#:ID! SKIP+ " +
+                    "CALLER_NAME:NAME! CALLER_PHONE_#:PHONE! CALLER_ADDRESS:SKIP! FIRE_CALL_TYPE:CALL/SDS? FIRE_CALL_DESCRIPTION:CALL/SDS? " +
+                    "EMS_CALL_TYPE:CALL/SDS? EMS_CALL_DESCRIPTION:CALL/SDS? FIRE_QUADRANT:MAP? EMS_DISTRICT:MAP/L? " +
+                    "FIRE_RADIO_CHANNEL:CH? EMS_RADIO_CHANEL:CH/L? " +
+                ")");
+
   }
 
   @Override
@@ -18,8 +33,13 @@ public class ORUmatillaCountyBParser extends HtmlProgramParser {
   }
 
   @Override
+  public int getMapFlags() {
+    return MAP_FLG_PREFER_GPS;
+  }
+
+  @Override
   protected boolean parseHtmlMsg(String subject, String body, Data data) {
-    if (!subject.startsWith("Automatic R&R Notification:")) return false;
+    if (!subject.startsWith("Automatic R&R Notification")) return false;
     return super.parseHtmlMsg(subject, body, data);
   }
 
@@ -27,6 +47,8 @@ public class ORUmatillaCountyBParser extends HtmlProgramParser {
   public Field getField(String name) {
     if (name.equals("DATETIME")) return new DateTimeField("\\d\\d?/\\d\\d?/\\d{4} \\d\\d?:\\d\\d:\\d\\d", true);
     if (name.equals("APT")) return new MyAptField();
+    if (name.equals("GPS")) return new MyGPSField();
+    if (name.equals("ID")) return new MyIdField();
     if (name.equals("INFO")) return new MyInfoField();
     return super.getField(name);
   }
@@ -36,6 +58,22 @@ public class ORUmatillaCountyBParser extends HtmlProgramParser {
     public void parse(String field, Data data) {
       if (field.equals(data.strApt)) return;
       super.parse(field,  data);;
+    }
+  }
+
+  private class MyIdField extends IdField {
+    @Override
+    public void parse(String field, Data data) {
+      if (field.startsWith("[Incident not yet created")) return;
+      super.parse(field, data);
+    }
+  }
+
+  private class MyGPSField extends GPSField {
+    @Override
+    public void parse(String field, Data data) {
+      field = field.replace(" LONGITUDE:", ",");
+      super.parse(field, data);
     }
   }
 
--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/WA/WAWallaWallaCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/WA/WAWallaWallaCountyParser.java
@@ -1,34 +1,13 @@
 package net.anei.cadpage.parsers.WA;
 
-import java.util.Properties;
-
-import net.anei.cadpage.parsers.MsgInfo.Data;
-import net.anei.cadpage.parsers.dispatch.DispatchA5Parser;
+import net.anei.cadpage.parsers.OR.ORUmatillaCountyBParser;
 
 /**
  * Walla Walla County, WA
  */
-public class WAWallaWallaCountyParser extends DispatchA5Parser {
-  
-  public WAWallaWallaCountyParser() {
-    super(CITY_CODES, "WALLA WALLA COUNTY", "WA");
-  }
-  
-  @Override
-  public String getFilter() {
-    return "@WALLAWALLAWA.GOV";
-  }
-  
-  @Override
-  protected boolean parseMsg(String subject, String body, Data data) {
-    if (!super.parseMsg(subject, body, data)) return false;
-    if (data.strCall.equals("E ProQA")) data.strCall = "E Medical";
-    return true;
-  }
+public class WAWallaWallaCountyParser extends ORUmatillaCountyBParser {
 
-  private static final Properties CITY_CODES = buildCodeTable(new String[]{
-      "College Pl",     "COLLEGE PLACE",
-      "WW City",        "WALLA WALLA",
-      "WW County",      "WALLA WALLA COUNTY"
-  });
+  public WAWallaWallaCountyParser() {
+    super("WALLA WALLA COUNTY", "WA");
+  }
 }
