535ab74fa9e9a483018f4be178ed681d55f6ba9d
Ken Corbin <ken@cadpage.org>
2026-02-05 11:41:21 -0800

    Fixe dparsing problem with Nueces County, TX

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/TX/TXNuecesCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/TX/TXNuecesCountyParser.java
@@ -4,164 +4,81 @@ import java.util.Properties;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import net.anei.cadpage.parsers.FieldProgramParser;
 import net.anei.cadpage.parsers.MsgInfo.Data;
-import net.anei.cadpage.parsers.SplitMsgOptions;
-import net.anei.cadpage.parsers.SplitMsgOptionsCustom;
+import net.anei.cadpage.parsers.MsgInfo.MsgType;
+import net.anei.cadpage.parsers.SmartAddressParser;
 
 
-public class TXNuecesCountyParser extends FieldProgramParser {
-  
+public class TXNuecesCountyParser extends SmartAddressParser {
+
   public TXNuecesCountyParser() {
-    super(CITY_CODES, "NUECES COUNTY", "TX",
-          "CALL! ALRM:SKIP! PRI:PRI! ESZ:UNIT! TIME:TIME? EV:ID");
+    super(CITY_CODES, "NUECES COUNTY", "TX");
   }
-  
-  
-  @Override
-  public SplitMsgOptions getActive911SplitMsgOptions() {
-    return new SplitMsgOptionsCustom(){
-      @Override public boolean splitBlankIns() { return false; }
-      @Override public int splitBreakLength() { return 150; }
-      @Override public int splitBreakPad() { return 1; }
-    };
-  }
-
 
   @Override
   public String getFilter() {
-    return "ccpdpaging@cctexas.com,CCPDPaging@corpuschristi.onmicrosoft.com";
+    return "mispublicsafety@cctexas.com";
   }
-  
+
   @Override
   public int getMapFlags() {
     return MAP_FLG_SUPPR_LA;
   }
-  
-  private static final Pattern SRC_ID_PTN = Pattern.compile("([ A-Z]+)[: ] +(\\d{10}) +");
- 
+
+  private static final Pattern MASTER = Pattern.compile("Unit (\\S+) was (cleared from|dispatched to) event (\\d+) at (.*?) NUECES (?:\\| (.*?) )?for (.*)");
+  private static final Pattern MASTER2 = Pattern.compile("Event (\\d+) \\((.*?)\\) at (.*?) NUECES (?:\\| (.*) )?was created");
+
   @Override
   public boolean parseMsg(String subject, String body, Data data) {
-    do {
-      if (subject.equals("CAD Notify")) break;
-      
-      if (body.startsWith("CAD Notify ")) {
-        body = body.substring(11).trim();
-        break;
-      }
-      return false;
-    } while (false);
-    
-    Matcher match = SRC_ID_PTN.matcher(body);
-    if (match.lookingAt()) {
-      data.strSource = match.group(1).trim();
-      data.strCallId = match.group(2);
-      body = body.substring(match.end());
+    if (!subject.equals("Device Notification")) return false;
+    int pt = body.indexOf("|MESS Unit ");
+    if (pt >= 0) {
+      body = body.substring(pt+6).trim();
+      body = stripFieldEnd(body, " .|SEND|QUIT");
+    } else {
+      pt = body.indexOf("Message:\t");
+      if (pt < 0) return false;
+      body = body.substring(pt+9);
+      body = stripFieldEnd(body, ".");
+      body = body + " for ALERT";
     }
-    
-    body = body.replace("TIME:", " TIME:");
-    if (!super.parseMsg(body, data)) return false;
-    return data.strAddress.length() > 0;
-  }
-  
-  @Override
-  public String getProgram() {
-    return "SRC ID " + super.getProgram();
-  }
-  
-  @Override
-  public Field getField(String name) {
-    if (name.equals("UNIT")) return new MyUnitField();
-    if (name.equals("TIME")) return new MyTimeField();
-    return super.getField(name);
-  }
-  
-  private static final Pattern UNIT_PTN = Pattern.compile("(?:(\\d+)|-1)(?:[ /]+(.*))?");
-  private class MyUnitField extends MyAddressField {
-    @Override
-    public void parse(String field, Data data) {
-      Matcher match = UNIT_PTN.matcher(field);
-      if (!match.matches()) abort();
-      data.strUnit = getOptGroup(match.group(1));
-      String addr = match.group(2);
-      if (addr != null) super.parse(addr, data);
-    }
-    
-    @Override
-    public String getFieldNames() {
-      return "UNIT " + super.getFieldNames();
-    }
-  }
-  
-  private static final Pattern TIME_PTN = Pattern.compile("(\\d\\d:\\d\\d:\\d\\d) +(.*)");
-  private class MyTimeField extends MyAddressField {
-    @Override
-    public void parse(String field, Data data) {
-      Matcher match = TIME_PTN.matcher(field);
-      if (!match.matches()) abort();
-      data.strTime = match.group(1);
-      super.parse(match.group(2), data);
-    }
-    
-    @Override
-    public String getFieldNames() {
-      return "TIME " + super.getFieldNames();
-    }
-  }
-  
-  private static final Pattern ADDR_APT_PTN = Pattern.compile("(.*)[,:;] *([-A-Z0-9]+)");
-  private abstract class MyAddressField extends AddressField {
 
-    @Override
-    public void parse(String field, Data data) {
-      int pt = field.indexOf(": @");
-      if (pt >= 0) {
-        data.strPlace = field.substring(pt+3).trim().replace(": @", " - ");
-        field = field.substring(0,pt);
+    Matcher match = MASTER.matcher(body);
+    if (match.matches()) {
+      setFieldList("UNIT ID ADDR APT CITY PLACE CALL");
+      data.strUnit = match.group(1);
+      if (match.group(2).startsWith("cleared")) data.msgType = MsgType.RUN_REPORT;
+      data.strCallId = match.group(3);
+      parseAddress(StartType.START_ADDR, FLAG_ANCHOR_END, match.group(4).trim(), data);
+      data.strPlace = getOptGroup(match.group(5));
+      data.strCall = match.group(6).trim();
+    }
+
+    else {
+      body = stripFieldEnd(body, " for ALERT");
+      if ((match = MASTER2.matcher(body)).matches()) {
+        setFieldList("ID CALL ADDR APT CITY PLACE");
+        data.strCallId = match.group(1);
+        data.strCall = match.group(2).trim();
+        parseAddress(StartType.START_ADDR, FLAG_ANCHOR_END, match.group(3).trim(), data);
+        data.strPlace = getOptGroup(match.group(4));
       }
-      String apt = "";
-      Matcher match = ADDR_APT_PTN.matcher(field);
-      if (match.matches()) {
-        field = match.group(1).trim();
-        apt = match.group(2);
-      }
-      field = stripFieldEnd(field, " NUECS");
-      parseAddress(StartType.START_OTHER, FLAG_ONLY_CITY | FLAG_ANCHOR_END, field, data);
-      field = stripFieldEnd(getStart(), " OCL");
-      String alias = null;
-      pt = field.indexOf(": alias ");
-      if (pt >= 0) {
-        alias = field.substring(pt+8).trim();
-        field = field.substring(0,pt).trim();
-        field = stripFieldEnd(field, " NUECS");
-        parseAddress(StartType.START_OTHER, FLAG_ONLY_CITY | FLAG_ANCHOR_END, field, data);
-        field = stripFieldEnd(getStart(), " OCL");
-      }
-      super.parse(field, data);
-      data.strApt = append(data.strApt, "-", apt);
-      if (alias != null) {
-        data.strAddress = append(data.strAddress, " ", '(' + alias + ')');
+
+      else {
+        setFieldList("INFO");
+        data.msgType = MsgType.GEN_ALERT;
+        data.strSupp = body;
       }
     }
-    
-    @Override
-    public String getFieldNames() {
-      return "ADDR CITY APT PLACE";
-    }
+    return true;
   }
-  
-  private static final Pattern HWY_N_FWY_PTN = Pattern.compile("\\b(HWY \\d+) FWY\\b");
-  
-  @Override
-  public String adjustMapAddress(String addr) {
-    return HWY_N_FWY_PTN.matcher(addr).replaceAll("$1");
-  }
-  
+
   private static final Properties CITY_CODES = buildCodeTable(new String[]{
+      "BANQ",   "BANQUETE",
       "BISH",   "BISHOP",
       "CC",     "CORPUS CHRISTI",
       "KLEBG",  "KLEBERG COUNTY",
+      "OCL",    "",  // ???
       "ROBS",   "ROBSTOWN",
       "TIER",   "TIERRA GRANDE"
-  });
-}
+  });}
