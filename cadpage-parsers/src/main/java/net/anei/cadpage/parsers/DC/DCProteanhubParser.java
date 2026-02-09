67c727ab8a78d9dc2303454764788579646df7de
Ken Corbin <ken@cadpage.org>
2026-02-08 16:34:34 -0800

    Fixed parsing problem with Proteanhub, DC

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/DC/DCProteanhubParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/DC/DCProteanhubParser.java
@@ -12,45 +12,51 @@ import net.anei.cadpage.parsers.MsgInfo.Data;
 
 public class DCProteanhubParser extends FieldProgramParser {
 
-  private static final Pattern MISSING_NL_PTN = Pattern.compile(" (?=ADDR:|CITY:|ST:|CNTY:|GPS:)");
- 
   public DCProteanhubParser() {
     super("DC", "",
-          "CAD:ID? ( CALL:CALL! Sending_Facility:FACILITY? LOCATION:PLACE? ( ADDR:ADDR! CITY:CITY! ST:ST! CNTY:CITY! GPS:GPS! PRI:PRI! UNIT:UNIT! | PRI:PRI? UNIT:UNIT? ) | " + 
-                    "Call_Type:CALL! Sending_Facility:FACILITY? ( Priority:PRI | Pri:PRI? ) Unit:UNIT ) INFO/N+", FLDPROG_IGNORE_CASE);
+          "CAD:ID? CALL:CALL! Sending_Facility:FACILITY? LOCATION:PLACE? " +
+                "( ADDR:ADDR! CITY:CITY! ST:ST? CNTY:CITY? Call_Back_Number:PHONE? GPS:GPS? PRI:PRI! UNIT:UNIT! " +
+//                "| PRI:PRI? UNIT:UNIT? " +
+                ") INFO/N+", FLDPROG_IGNORE_CASE);
   }
-  
+
   @Override
   public String getFilter() {
-    return "donotreply@proteanhub.com,no-reply@cnmc.proteanhub.com";
+    return "donotreply@proteanhub.com,FlightVectorSvc@childrensnational.org";
   }
 
   @Override
   public String getLocName() {
     return "Protean Hub, DC";
   }
-  
+
   @Override
   public int getMapFlags() {
     return MAP_FLG_PREFER_GPS;
   }
 
+  private static final Pattern MISSING_NL_PTN = Pattern.compile(" (?=ADDR:|CITY:|ST:|CNTY:|GPS:)");
+
   @Override
   protected boolean parseMsg(String subject, String body, Data data) {
-    
-    if (!subject.equals("Alert") && !subject.startsWith("Z - Active 911 Page")) return false;
+
+    if (!subject.equals("Alert") &&
+        !subject.startsWith("Z - Active 911 Page") &&
+        !subject.equals("FlightVector")) return false;
+    int pt = body.indexOf("\n\nConfidentiality Notice:");
+    if (pt >= 0) body = body.substring(0,pt).trim();
     body = body.replace('\t', ' ');
     body = MISSING_NL_PTN.matcher(body).replaceAll("\n");
     return parseFields(body.split("\n+"), data);
   }
-  
+
   @Override
   public Field getField(String name) {
     if (name.equals("FACILITY")) return new MyFacilityField();
     if (name.equals("CITY")) return new MyCityField();
     return super.getField(name);
   }
-  
+
   private class MyFacilityField extends InfoField {
     @Override
     public void parse(String field, Data data) {
@@ -58,7 +64,7 @@ public class DCProteanhubParser extends FieldProgramParser {
       super.parse(field, data);
     }
   }
-  
+
   private class MyCityField extends CityField {
     @Override
     public void parse(String field, Data data) {
