8efebac935dc0838e1346e403a6d632654e699fd
Ken Corbin <ken@cadpage.org>
2026-02-12 13:35:39 -0800

    Fixed parsing problem with Umatilla County, OR (B)

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/OR/ORUmatillaCountyBParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/OR/ORUmatillaCountyBParser.java
@@ -13,17 +13,23 @@ public class ORUmatillaCountyBParser extends HtmlProgramParser {
 
   public ORUmatillaCountyBParser(String defCity, String defState) {
     super(defCity, defState,
-          "CALL_DATE_/_TIME:DATETIME! " +
-                "( FIRE_CALL_TYPE:CALL! EMS_CALL_TYPE:CALL! COMMON_NAME:PLACE! ADDRESS:ADDRCITY/S6! APT/SUITE:APT! CROSS_STREET:X! " +
-                    "FIRE_QUAD:MAP! NATURE_OF_CALL:CALL/SDS! DISPATCH_COMMENTS:EMPTY! INFO/N+ LOCAL_INFO:INFO/N! INFO/N+ ALERTS:ALERT! INFO/N+ " +
-                    "INCIDENT_#:ID! UNITS:UNIT! STATUS_TIMES:EMPTY! SKIP+ CALLER_NAME:NAME! CALLER_PHONE_#:PHONE! CALLER_ADDRESS:SKIP! " +
-                "| COMMON_NAME:PLACE! ADDRESS:ADDRCITY/S6! APT/SUITE:APT! CROSS_STREET:X! LATITUDE:GPS! " +
-                    "FIRE_CALL_TYPE:CALL/SDS? FIRE_CALL_DESCRIPTION:CALL/SDS? EMS_CALL_TYPE:CALL/SDS? EMS_CALL_DESCRIPTION:CALL/SDS? CALL_TYPE:CALL/SDS? NATURE_OF_CALL:CALL/SDS! " +
-                    "DISPATCH_COMMENTS:EMPTY! INFO/N+ LOCAL_INFO:INFO/N? INFO/N+ ALERTS:ALERT! INCIDENT_#:ID! SKIP+ " +
-                    "CALLER_NAME:NAME! CALLER_PHONE_#:PHONE! CALLER_ADDRESS:SKIP! FIRE_CALL_TYPE:CALL/SDS? FIRE_CALL_DESCRIPTION:CALL/SDS? " +
-                    "EMS_CALL_TYPE:CALL/SDS? EMS_CALL_DESCRIPTION:CALL/SDS? FIRE_QUADRANT:MAP? EMS_DISTRICT:MAP/L? " +
-                    "FIRE_RADIO_CHANNEL:CH? EMS_RADIO_CHANEL:CH/L? " +
-                ")");
+          "JUNK+? " +
+            "( CALL_DATE_/_TIME:DATETIME! " +
+                    "( FIRE_CALL_TYPE:CALL! EMS_CALL_TYPE:CALL! COMMON_NAME:PLACE! ADDRESS:ADDRCITY/S6! APT/SUITE:APT! CROSS_STREET:X! " +
+                        "FIRE_QUAD:MAP! NATURE_OF_CALL:CALL/SDS! DISPATCH_COMMENTS:EMPTY! INFO/N+ LOCAL_INFO:INFO/N! INFO/N+ ALERTS:ALERT! INFO/N+ " +
+                        "INCIDENT_#:ID! UNITS:UNIT! STATUS_TIMES:EMPTY! SKIP+ CALLER_NAME:NAME! CALLER_PHONE_#:PHONE! CALLER_ADDRESS:SKIP! " +
+                    "| COMMON_NAME:PLACE! ADDRESS:ADDRCITY/S6! APT/SUITE:APT! CROSS_STREET:X! LATITUDE:GPS? " +
+                        "FIRE_CALL_TYPE:CALL/SDS? FIRE_CALL_DESCRIPTION:CALL/SDS? EMS_CALL_TYPE:CALL/SDS? EMS_CALL_DESCRIPTION:CALL/SDS? " +
+                        "CALL_TYPE:CALL/SDS? CALL_DESCRIPTON:CALL/SDS? NATURE_OF_CALL:CALL/SDS! " +
+                        "DISPATCH_COMMENTS:EMPTY! INFO/N+ LOCAL_INFO:INFO/N? INFO/N+ ALERTS:ALERT! INCIDENT_#:ID! SKIP+ " +
+                        "CALLER_NAME:NAME! CALLER_PHONE_#:PHONE! CALLER_ADDRESS:SKIP! FIRE_CALL_TYPE:CALL/SDS? FIRE_CALL_DESCRIPTION:CALL/SDS? " +
+                        "EMS_CALL_TYPE:CALL/SDS? EMS_CALL_DESCRIPTION:CALL/SDS? FIRE_QUADRANT:MAP? EMS_DISTRICT:MAP/L? " +
+                        "FIRE_RADIO_CHANNEL:CH? EMS_RADIO_CHANEL:CH/L? " +
+                    ") " +
+            "| Incident_Number:ID! Apt/Suite:APT! Call_Date_and_Time:SKIP! Common_Name:PLACE! Fire_Call_Type:CODE! Address:ADDRCITY/S6! Cross_Streets:X! Fire_Quadrant:MAP! " +
+                "Caller_Name:NAME! Caller_Phone:PHONE! Caller_Address:SKIP! Narative:INFO! INFO/N+ Alerts:ALERT! ALERT/N+ Unit_Assigned:UNIT! Assigned_Station:SRC? " +
+                "Unit:SKIP? Dispatched:DATETIME? Map_Link:EMPTY! GPS2! " +
+            ")");
 
   }
 
@@ -37,17 +43,22 @@ public class ORUmatillaCountyBParser extends HtmlProgramParser {
     return MAP_FLG_PREFER_GPS;
   }
 
+  private static final Pattern MISSED_BRK_PTN = Pattern.compile(" +(?=Apt/Suite:|Call Date and Time:|Fire Quadrant:|Caller Phone:)");
+
   @Override
   protected boolean parseHtmlMsg(String subject, String body, Data data) {
     if (!subject.startsWith("Automatic R&R Notification")) return false;
+    body = MISSED_BRK_PTN.matcher(body).replaceAll("</div><div>");
     return super.parseHtmlMsg(subject, body, data);
   }
 
   @Override
   public Field getField(String name) {
+    if (name.equals("JUNK")) return new SkipField("(?!FIRE CALL TYPE:|Incident Number:).*", true);
     if (name.equals("DATETIME")) return new DateTimeField("\\d\\d?/\\d\\d?/\\d{4} \\d\\d?:\\d\\d:\\d\\d", true);
     if (name.equals("APT")) return new MyAptField();
     if (name.equals("GPS")) return new MyGPSField();
+    if (name.equals("GPS2")) return new GPSField("https://www.google.com.*query=(.*)", true);
     if (name.equals("ID")) return new MyIdField();
     if (name.equals("INFO")) return new MyInfoField();
     return super.getField(name);
