9c06cf7aab2bc81f3152448ed9e93a8560c82ac6
Ken Corbin <ken@cadpage.org>
2026-02-19 20:49:03 -0800

    Fixed parsing problem with Kane County, IL (C)

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/IL/ILKaneCountyCParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/IL/ILKaneCountyCParser.java
@@ -3,106 +3,17 @@ package net.anei.cadpage.parsers.IL;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import net.anei.cadpage.parsers.CodeSet;
+import net.anei.cadpage.parsers.FieldProgramParser;
 import net.anei.cadpage.parsers.MsgInfo.Data;
-import net.anei.cadpage.parsers.SmartAddressParser;
 
 
-public class ILKaneCountyCParser extends SmartAddressParser {
+public class ILKaneCountyCParser extends FieldProgramParser {
 
   public ILKaneCountyCParser() {
-    super(ILKaneCountyParser.CITY_LIST, "KANE COUNTY", "IL");
-    setFieldList("CALL ADDR APT CITY PLACE X INFO GPS");
-    setupCallList(CALL_LIST);
-    setupSaintNames("CHARLES");
-    setupMultiWordStreets(
-        "ABBEY GLEN",
-        "ARBOR CREEK",
-        "ARMY TRAIL",
-        "BAR HARBOR",
-        "BARB HILL",
-        "BAY SHORE",
-        "BIG TIMBER",
-        "BILLY BURNS",
-        "BOWES BEND",
-        "BREWSTER CREEK",
-        "BRIDLE CREEK",
-        "BRIER HILL",
-        "CAMPTON HILLS",
-        "CAMPTON WOOD",
-        "CAPE COD",
-        "CARL LEE",
-        "CLOVER FIELD",
-        "COUNTRY CLUB",
-        "COUNTY LINE",
-        "CREEK VIEW",
-        "CURLING POND",
-        "DEER RUN",
-        "DIAMOND HEAD",
-        "DUNHAM TRAILS",
-        "EAST LAURA INGALLS WILDER",
-        "EAST MARY",
-        "EDGAR LEE MASTERS",
-        "EDNA ST VINCENT MILLAY",
-        "EMILY DICKINSON",
-        "FERSON CREEK",
-        "FOX BEND",
-        "FOX BLUFF",
-        "FOX MILL",
-        "FOX RIVER",
-        "FOX RUN",
-        "FRANCIS BRET HARTE",
-        "GLEN COVE",
-        "GREY BARN",
-        "HAPPY HILLS",
-        "HIDDEN HILL",
-        "HIDDEN LAKES",
-        "HIDDEN OAKS",
-        "HOMEWARD HILL",
-        "JJC LINCOLN",
-        "JOYCE KILMER",
-        "LAKE BLUFF",
-        "LONG VIEW",
-        "LOON LAKE",
-        "NORTH JAMES",
-        "OAK RIDGE",
-        "OLIVER WENDELL HOLMES",
-        "PADRE ISLAND",
-        "PINE HILLS",
-        "PINE HILLS",
-        "PRAIRIE HILL",
-        "PRAIRIE LAKES",
-        "RAMM WOODS",
-        "RED BARN",
-        "RED GATE",
-        "RED HAWK",
-        "RED LEAF",
-        "RICHARD J BROWN",
-        "RIDGE LINE",
-        "RIVER GRANGE",
-        "RIVERSIDE",
-        "ROLLING OAKS",
-        "SETTLERS GROVE",
-        "SILVER GLEN",
-        "SILVER LAKE",
-        "SORRENTOS MCGOUGH",
-        "TALL PINES",
-        "TAYLOR CALDWELL",
-        "TIMBER RIDGE",
-        "TOMS TRAIL",
-        "TOWN PLACE",
-        "VACHEL LINDSAY",
-        "VALLEY STREAM",
-        "WALT WHITMAN",
-        "WEST LAURA INGALLS WILDER",
-        "WEST MARY",
-        "WEST VIEW",
-        "WHISPERING WILLOWS",
-        "WHITE PINE",
-        "WILD ROSE",
-        "WILLIAM CULLEN BRYANT",
-        "WINDING HILL"
-    );
+    super("KANE COUNTY", "IL",
+          "( CALL_TIME:DATETIME! CALL_TYPE:CALL! COMMON_NAME:PLACE! ADDRESS:ADDRCITY1/S6! CROSS_STREETS:X! UNITS_DUE:UNIT! NARRATIVE:INFO! INFO/N+ LAT/LON:GPSX1! CALLERS_TX:PHONE! " +
+          "| DATETIME_CALL ADDRCITY2/S6 UNIT_MAP_ID_INFO! INFO/N+? GPSX2! " +
+          ") END");
   }
 
   @Override
@@ -110,145 +21,115 @@ public class ILKaneCountyCParser extends SmartAddressParser {
     return "shfradio@co.kane.il.us";
   }
 
-  private static final Pattern DIR_STREET_NO_PTN = Pattern.compile(" (\\d+[NSEW]\\d? ?\\d+) ");
-  private static final Pattern MIXED_CASE_PTN = Pattern.compile("[A-Z][a-z].*");
-  private static final Pattern DIR_STREET_NO_PTN2 = Pattern.compile("(\\d+[NSEW]\\d? \\d+) ");
-  private static final Pattern X_ST_PTN1 = Pattern.compile("([. A-Z0-9]+ ?/ ?[. ,A-Z0-9]+?)(?: {2,}|$)");
-  private static final Pattern X_ST_PTN2 = Pattern.compile("([. A-Z0-9]+ ?/ ?[.A-Z0-9]+?)(?: |$)");
-
   @Override
   protected boolean parseMsg(String subject, String body, Data data) {
 
     if (!subject.equals("!")) return false;
-
-    // They have some weird street numbers that throw the smart address parser
-    // for a loop.  So check for them first.
-    StartType st = StartType.START_CALL;
-    int flags = FLAG_START_FLD_REQ;
-    String dirStreetNo = null;
-    Matcher match = DIR_STREET_NO_PTN.matcher(body);
-    if (match.find()) {
-      String call = body.substring(0,match.start()).trim();
-      Result tmpRes = parseAddress(StartType.START_OTHER, FLAG_ONLY_CITY, call);
-      String city = tmpRes.getCity();
-      if (!MIXED_CASE_PTN.matcher(city).matches()) {
-        st = StartType.START_ADDR;
-        flags = 0;
-        data.strCall = call;
-        dirStreetNo = match.group(1);
-        body = "999999" + body.substring(match.end(1));
-      }
-    }
-
-    else {
-      int pt = body.indexOf(" <UNKNOWN> ");
-      if (pt >= 0) {
-        data.strCall = body.substring(0,pt).trim();
-        data.strAddress = "<UNKNOWN>";
-        data.strSupp = body.substring(11).trim();
-        return true;
-      }
-    }
-
-    // They like verbose highway names
-    body = body.replace(" UNITED STATES HIGHWAY ", " US ");
-    body = body.replaceAll(" ILLIONIS ", " ILLINOIS ");
-    body = body.replace(" ILLINOIS ROUTE ", " IL ");
-
-    match = GPS_PATTERN.matcher(body);
-    if (match.find()) {
-      setGPSLoc(match.group(), data);
-      data.strSupp = body.substring(match.end()).trim();
-      body = body.substring(0,match.start()).trim();
-    }
-
-
-    // Lets see what we can do...
-    parseAddress(st, flags | FLAG_IMPLIED_INTERSECT | FLAG_IGNORE_AT| FLAG_CROSS_FOLLOWS | FLAG_RECHECK_APT, body, data);
-    if (!isValidAddress() && data.strCity.length() == 0) return false;
-    body = getLeft();
-    if (dirStreetNo != null && data.strAddress.startsWith("999999 ")) {
-      data.strAddress = dirStreetNo + data.strAddress.substring(6);
-    }
-
-    // Next is the cross street. But it is a bit chancy.  Let's see what we can do
-    // with this.
-    do {
-
-      if (body.startsWith("No Cross Streets Found ")) {
-        data.strSupp = body.substring(23).trim();
-        break;
-      }
-
-      match = X_ST_PTN1.matcher(body);
-      if (match.lookingAt()) {
-        data.strCross = match.group(1).trim();
-        data.strSupp = body.substring(match.end()).trim();
-        break;
-      }
-
-      match = X_ST_PTN2.matcher(body);
-      if (match.lookingAt()) {
-        data.strCross = match.group(1).trim();
-        data.strSupp = body.substring(match.end()).trim();
-        break;
-      }
-
-      Result res = parseAddress(StartType.START_ADDR, FLAG_ONLY_CROSS | FLAG_NO_CITY, body);
-      if (res.isValid()) {
-        res.getData(data);
-        data.strSupp = res.getLeft();
-        if (data.strSupp.length() == 0) {
-          data.strSupp = data.strCross;
-          data.strCross = "";
-        }
-        break;
-      }
-
-      data.strSupp = body;
-
-    } while (false);
-
-    // Split out place name from cross street
-    String cross = data.strCross;
-    data.strCross = "";
-    String crossExt = null;
-    int pt = cross.indexOf(',');
-    if (pt >= 0) {
-      crossExt = cross.substring(pt);
-      cross = cross.substring(0, pt).trim();
-    }
-    parseAddress(StartType.START_PLACE, FLAG_ONLY_CROSS | FLAG_ANCHOR_END | FLAG_NO_CITY, cross, data);
-    if (data.strCross.length() == 0) {
-      data.strPlace = "";
-      data.strCross = cross;
-    }
-    if (crossExt != null) data.strCross += crossExt;
-
-    return true;
+    return parseFields(body.split("\n"), data);
   }
 
   @Override
-  public String adjustMapAddress(String addr) {
-    Matcher match = DIR_STREET_NO_PTN2.matcher(addr);
-    if (match.lookingAt()) {
-      addr = match.group(1).replace(" ", "") + addr.substring(match.end(1));
-    }
-    while (addr.startsWith("0")) addr = addr.substring(1).trim();
-    return addr;
+  public Field getField(String name) {
+    if (name.equals("DATETIME")) return new DateTimeField("\\d\\d?/\\d\\d?/\\d{4} +\\d\\d:\\d\\d:\\d\\d", true);
+    if (name.equals("ADDRCITY1")) return new MyAddressCity1Field();
+    if (name.equals("GPSX1")) return new MyGPSX1Field();
+    if (name.equals("GPSX2")) return new GPSField("\\d\\d\\.\\d+ +-\\d\\d\\.\\d+", true);
+    if (name.equals("DATETIME_CALL")) return new MyDateTimeCallField();
+    if (name.equals("ADDRCITY2")) return new MyAddressCity2Field();
+    if (name.equals("UNIT_MAP_ID_INFO")) return new MyUnitMapIdInfoField();
+    return super.getField(name);
   }
 
+  private class MyGPSX1Field extends GPSField {
+    public MyGPSX1Field() {
+      super("\\d\\d\\.\\d+-\\d\\d\\.\\d+", true);
+    }
 
-  private static final CodeSet CALL_LIST = new CodeSet(
-      "AMBULANCE CALL",
-      "CALLBACK",
-      "CARBON MONOXIDE DETECTOR",
-      "FIELD FIRE",
-      "FIRE ALARM",
-      "FIRE CALL",
-      "MABAS",
-      "MUTUAL AID REQUEST",
-      "STRUCTURE FIRE",
-      "VEHICLE FIRE"
-  );
+    @Override
+    public void parse(String field, Data data) {
+      field = field.replace("-", ",-");
+      super.parse(field, data);
+    }
+  }
+
+  private static final Pattern DATE_TIME_CALL_PTN = Pattern.compile("(\\d\\d?/\\d\\d?/\\d{4}) +(\\d\\d:\\d\\d:\\d\\d) +(.*)");
+  private class MyDateTimeCallField extends Field {
+    @Override
+    public void parse(String field, Data data) {
+      Matcher match = DATE_TIME_CALL_PTN.matcher(field);
+      if (!match.matches()) abort();
+      data.strDate = match.group(1);
+      data.strTime = match.group(2);
+      data.strCall = match.group(3);
+    }
+
+    @Override
+    public String getFieldNames() {
+      return "DATE TIME CALL";
+    }
+  }
+
+  private class MyAddressCity1Field extends AddressCityField {
+    @Override
+    public void parse(String field, Data data) {
+      field = field.replace('@', '&');
+      super.parse(field, data);
+    }
+  }
+
+  private static final Pattern ADDRCITY_PLACE_X_PTN = Pattern.compile("(.*?)    (.*?)(?:    (.*))?");
+  private static final Pattern ADDRCITY_PLACE_X_PTN2 = Pattern.compile("(.*?)  (.*?)(?:  (.*))?");
+  private class MyAddressCity2Field extends AddressCityField {
+    @Override
+    public void parse(String field, Data data) {
+      Matcher match = ADDRCITY_PLACE_X_PTN.matcher(field);
+      boolean good = match.matches();
+      if (!good) {
+        match = ADDRCITY_PLACE_X_PTN2.matcher(field);
+        good = match.matches();
+      }
+      if (good) {
+        field = match.group(1);
+        data.strPlace = match.group(2).trim();
+        data.strCross = getOptGroup(match.group(3));
+      }
+      field = field.replace('@', '&');
+      super.parse(field, data);
+    }
+
+    @Override
+    public String getFieldNames() {
+      return super.getFieldNames() + " PLACE X";
+    }
+  }
+
+  private static final Pattern UNIT_MAP_ID_INFO_PTN1 = Pattern.compile("(?:(\\S+) +)?([A-Z]+-\\d+)(?: +(\\d{4}-\\d{8} \\([A-Z]{2}\\d+\\)))? *(.*)");
+  private static final Pattern UNIT_MAP_ID_INFO_PTN2 = Pattern.compile("(?:(\\S+) +)?(\\d{4}-\\d{8} \\([A-Z]{2}\\d+\\)) *(.*)");
+  private class MyUnitMapIdInfoField extends Field {
+    @Override
+    public void parse(String field, Data data) {
+      Matcher match = UNIT_MAP_ID_INFO_PTN1.matcher(field);
+      if (match.matches()) {
+        data.strUnit = getOptGroup(match.group(1));
+        data.strMap = match.group(2);
+        data.strCallId = getOptGroup(match.group(3));
+        data.strSupp = getOptGroup(match.group(4));
+        return;
+      }
+      match = UNIT_MAP_ID_INFO_PTN2.matcher(field);
+      if (match.matches()) {
+        data.strUnit = getOptGroup(match.group(1));
+        data.strCallId = getOptGroup(match.group(2));
+        data.strSupp = getOptGroup(match.group(3));
+        return;
+      }
+      abort();
+    }
+
+    @Override
+    public String getFieldNames() {
+      return "UNIT MAP ID INFO";
+    }
+
+  }
 }
