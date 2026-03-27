9c2b21d2ddfe95cc60dea8320b1d61eee509e9ab
Ken Corbin <ken@cadpage.org>
2026-03-27 16:58:20 -0700

    Fixed parsing problem with Jefferson County, OH

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/OH/OHJeffersonCountyBParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/OH/OHJeffersonCountyBParser.java
@@ -12,26 +12,26 @@ public class OHJeffersonCountyBParser extends MsgParser {
     super("JEFFERSON COUNTY", "OH");
     setFieldList("CALL DATE TIME ADDR CITY ST GPS INFO X");
   }
-  
+
   @Override
   public String getFilter() {
     return "no-reply@zuercherportal.com";
   }
-  
-  private static final Pattern MASTER = Pattern.compile("(\\d\\d/\\d\\d/\\d\\d) (\\d\\d:\\d\\d) (.*?) ([-+]?\\d{2}\\.\\d{6} [-+]?\\d{2}\\.\\d{6}|None None)");
+
+  private static final Pattern MASTER = Pattern.compile("(\\d\\d/\\d\\d/\\d\\d) (\\d\\d:\\d\\d) (.*?) ([-+]?\\d{2}\\.\\d{6,} [-+]?\\d{2}\\.\\d{6,}|None None)");
   private static final Pattern STATE_ZIP_PTN = Pattern.compile("([A-Z]{2})(?: +(\\d{5}))?");
   private static final Pattern INFO_BRK_PTN = Pattern.compile(" \\d\\d/\\d\\d/\\d\\d \\d\\d:\\d\\d:\\d\\d - ");
-  
+
   @Override
   protected boolean parseMsg(String subject, String body, Data data) {
-    
+
     data.strCall = subject;
     int pt = body.lastIndexOf(" Cross Streets:");
     if (pt >= 0) {
       data.strCross = body.substring(pt+15).trim();
       body = body.substring(0,pt).trim();
     }
-    
+
     String[] lines = INFO_BRK_PTN.split(body);
     body = lines[0];
     if (lines.length == 1) {
@@ -41,15 +41,15 @@ public class OHJeffersonCountyBParser extends MsgParser {
         data.strSupp = append(data.strSupp, "\n", lines[ndx].trim());
       }
     }
-    
+
     Matcher match = MASTER.matcher(body);
     if (!match.matches()) return false;
-    
+
     data.strDate = match.group(1);
     data.strTime = match.group(2);
     String addr = match.group(3).trim();
     setGPSLoc(match.group(4), data);
-    
+
     Parser p = new Parser(addr);
     String city = p.getLastOptional(',');
     if (city.length() > 0) {
@@ -64,7 +64,7 @@ public class OHJeffersonCountyBParser extends MsgParser {
       data.strCity = city;
     }
     parseAddress(p.get(), data);
-    
+
     return true;
   }
 }
