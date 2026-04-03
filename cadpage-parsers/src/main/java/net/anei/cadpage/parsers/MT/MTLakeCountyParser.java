2acfcbce518c763c050d48fb76ce218d306d4800
Ken Corbin <ken@cadpage.org>
2026-04-02 22:32:39 -0700

    Fixed parsaing problem with Lake County, MT

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/MT/MTLakeCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/MT/MTLakeCountyParser.java
@@ -1,22 +1,25 @@
 package net.anei.cadpage.parsers.MT;
 
-import java.util.Properties;
+import java.util.regex.Pattern;
 
-import net.anei.cadpage.parsers.dispatch.DispatchA49Parser;
+import net.anei.cadpage.parsers.FieldProgramParser;
+import net.anei.cadpage.parsers.MsgInfo.Data;
 
-public class MTLakeCountyParser extends DispatchA49Parser {
+public class MTLakeCountyParser extends FieldProgramParser {
 
   public MTLakeCountyParser() {
     this("LAKE COUNTY", "MT");
   }
 
   MTLakeCountyParser(String defCity, String defState) {
-    super(defCity, defState, CALL_CODES);
+    super(defCity, defState,
+          "Address:ADDRCITYST! Address_Name:PLACE! Location_Details:PLACE! CFS_Number:ID! Incident_Type:CALL! Caller:NAME! Dispatcher:SKIP! " +
+              "Call_Time:DATETIME! Call_Location:SKIP! Responding_Units:UNIT! Call_Notes:INFO! Message:EMPTY! CFS_Latitude:GPS1! CFS_Longitude:GPS2! END");
   }
 
   @Override
   public String getFilter() {
-    return "cadpage@e9.com";
+    return "no-reply@csprosuite.centralsquarecloudgov.com";
   }
 
   @Override
@@ -24,41 +27,47 @@ public class MTLakeCountyParser extends DispatchA49Parser {
     return "MTLakeCounty";
   }
 
-  private static final Properties CALL_CODES = buildCodeTable(new String[] {
-      "ALARMBUR", "ALARM BURGLAR",
-      "ALARMFIR", "ALARM FIRE",
-      "ALARMMED", "ALARM MEDICAL",
-      "AMB",      "AMBULANCE",
-      "AMBASST",  "AMBULANCE ASSIST",
-      "AMBFLU",   "AMBULANCE COVID SYMPTOMS",
-      "AMBTRANS", "AMBULANCE TRANSPORT",
-      "ASSAULT",  "ASSAULT",
-      "CRISIS",   "SUICIDE ATTEMPT",
-      "FIREBUIL", "FIRE BUILDING",
-      "FIREDEB",  "FIRE DEBRIS",
-      "FIREGRAS", "FIRE GRASS",
-      "FIREMISC", "FIRE MISC",
-      "FIREVEH",  "FIRE VEHICLE",
-      "FIREWOOD", "FIRE WOODS",
-      "GOVASST",  "GOVERNMENT ASSIST",
-      "LAWASST",  "LAW ENFORCEMENT ASSIST",
-      "MISSING",  "MISSING PERSON",
-      "PAGETEST", "PAGER TEST",
-      "PFMAIP",   "LAW ENFORCEMENT",
-      "PROPNGAS", "PROPANE GAS",
-      "PUBASST",  "PUBLIC ASSIST",
-      "RDDEBRIS", "ROAD DEBRIS",
-      "SINKWATR", "WATER RESCUE - BOAT SINKING",
-      "SUICATT",  "SUICIDE ATTEMPT",
-      "UNK-F",    "VEHICLE CRASH UNK INJURY",
-      "UTILITY",  "UTILITY LINE",
-      "VEHCRASH", "VEHICLE CRASH",
-      "VEHINJUR", "VEHICLE CRASH WITH INJURY",
-      "VEHPROP",  "VEHICLE CRASH NO INJURY",
-      "VEHSLID",  "VEHICLE SLID OFF ROAD",
-      "VOIDTEST", "PAGER TEST",
-      "WTROTHER", "WATER OTHER",
-      "WTRRESCU", "WATER RESCUE"
-  });
+  @Override
+  public Field getField(String name) {
+    if (name.equals("PLACE")) return new MyPlaceField();
+    if (name.equals("NAME")) return new MyNameField();
+    if (name.equals("DATETIME")) return new DateTimeField("\\d\\d/\\d\\d/\\d\\d +\\d\\d:\\d\\d", true);
+    if (name.equals("UNIT")) return new MyUnitField();
+    if (name.equals("INFO")) return new MyInfoField();
+    return super.getField(name);
+  }
 
+  private class MyPlaceField extends PlaceField {
+    @Override
+    public void parse(String field, Data data) {
+      if (field.equals("None")) return;
+      super.parse(field, data);
+    }
+  }
+
+  private class MyNameField extends NameField {
+    @Override
+    public void parse(String field, Data data) {
+      if (field.equals("None")) return;
+      super.parse(field, data);
+    }
+  }
+
+  private class MyUnitField extends UnitField {
+    @Override
+    public void parse(String field, Data data) {
+      field = field.replace("; ", ",");
+      super.parse(field, data);
+    }
+  }
+
+  private static final Pattern INFO_BRK_PTN = Pattern.compile("[; ]*\\b\\d\\d/\\d\\d/\\d\\d \\d\\d:\\d\\d:\\d\\d - *");
+  private class MyInfoField extends InfoField {
+    @Override
+    public void parse(String field, Data data) {
+      if (field.equals("None")) return;
+      field = INFO_BRK_PTN.matcher(field).replaceAll("\n").trim();
+      data.strSupp = field;
+    }
+  }
 }
