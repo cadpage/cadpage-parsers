e82a3d0f22e4edbecc12d659e8d0444832d2d9e2
Ken Corbin <ken@cadpage.org>
2026-04-05 16:57:39 -0700

    Fixed parsing problem with Marin County, CA

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/CA/CAMarinCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/CA/CAMarinCountyParser.java
@@ -1,56 +1,119 @@
 package net.anei.cadpage.parsers.CA;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.Properties;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import net.anei.cadpage.parsers.MsgInfo.Data;
-import net.anei.cadpage.parsers.MsgParser;
+import net.anei.cadpage.parsers.MsgInfo.MsgType;
+import net.anei.cadpage.parsers.FieldProgramParser;
 
 /**
  * Marin County, CA
  */
-public class CAMarinCountyParser extends MsgParser {
-  
-  private static final Pattern MASTER = Pattern.compile("(\\w+): (\\d\\d/\\d\\d/\\d\\d) (\\d\\d:\\d\\d:\\d\\d) From: CNSL (\\d):(.*?)\\[(\\d+) ?\\](.*?)(?:,([A-Z]+))? btwn (.*)");
-  
+public class CAMarinCountyParser extends FieldProgramParser {
+
   public CAMarinCountyParser() {
-    super("MARIN COUNTY", "CA");
-    setFieldList("SRC DATE TIME PRI CALL ID ADDR ADDR CITY X");
+    super(CITY_CODES, "MARIN COUNTY", "CA",
+          "Location:ADDR/S? Beat:MAP! TYPE_CODE:CALL! Alarm_Lev:PRI! Typecode:SKIP! Time:TIME! Dgroup:CH! MapGrid:GRID/L! MapPage:MAP/L! END");
   }
-  
+
   @Override
   public String getFilter() {
     return "CAD@marinsheriff.org";
   }
-  
+
+  private static final Pattern GEN_ALERT_PTN = Pattern.compile("Original message from terminal .*? (\\d\\d?/\\d\\d?/\\d{4}) (\\d\\d?:\\d\\d:\\d\\d [AP]M) *.*? \\(\\d+\\): *(.*)");
+  private static final DateFormat TIME_FMT = new SimpleDateFormat("hh:mm:ss aa");
+
   @Override
-  public boolean parseMsg(String body, Data data) {
-    
+  public boolean parseMsg(String subject, String body, Data data) {
+
+    if (!subject.equals("CAD PAGE")) return false;
+
     int pt = body.indexOf("\n\n");
     if (pt >= 0) body = body.substring(0,pt).trim();
-    
-    Matcher match = MASTER.matcher(body);
-    if (!match.matches()) return false;
-    
-    data.strSource = match.group(1);
-    data.strDate = match.group(2);
-    data.strTime = match.group(3);
-    data.strPriority = match.group(4);
-    data.strCall = match.group(5).trim();
-    data.strCallId = match.group(6);
-    parseAddress(match.group(7).trim(), data);
-    String city = match.group(8);
-    if (city != null) data.strCity = convertCodes(city, CITY_CODES);
-    data.strCross = match.group(9).trim();
-    return true;
+
+    Matcher match = GEN_ALERT_PTN.matcher(body);
+    if (match.matches()) {
+      setFieldList("DATE TIME INFO");
+      data.msgType = MsgType.GEN_ALERT;
+      data.strDate = match.group(1);
+      setTime(TIME_FMT, match.group(2), data);
+      data.strSupp = match.group(3);
+      return true;
+    }
+
+    else {
+      return super.parseMsg(body, data);
+    }
   }
-  
+
+  @Override
+  public Field getField(String name) {
+    if (name.equals("ADDR")) return new MyAddressField();
+    if (name.equals("CALL")) return new MyCallField();
+    if (name.equals("TIME")) return new TimeField("\\d\\d:\\d\\d:\\d\\d", true);
+    if (name.equals("GRID")) return new MapField("Grid (.*)|()", true);
+    return super.getField(name);
+  }
+
+  private class MyAddressField extends AddressField {
+    @Override
+    public void parse(String field, Data data) {
+      Parser p = new Parser(field);
+      data.strPlace = p.getLastOptional(": @");
+      String apt = p.getLastOptional(',');
+      super.parse(p.get(), data);
+      data.strApt = append(data.strApt, "-", apt);
+    }
+
+    @Override
+    public String getFieldNames() {
+      return "ADDR CITY APT PLACE";
+    }
+  }
+
+  private class MyCallField extends Field {
+    @Override
+    public void parse(String field, Data data) {
+      data.strCode = field;
+      data.strCall = convertCodes(field, CALL_CODES);
+    }
+
+    @Override
+    public String getFieldNames() {
+      return "CODE CALL";
+    }
+  }
+
+  private static final Properties CALL_CODES = buildCodeTable(new String[]{
+      "22",     "Vehicle Accident / Extrication Assignment",
+      "29",     "Vehicle Accident / Extrication Assignment",
+      "BF",     "Boat Fire",
+      "EXPL",   "Explosion",
+      "HBF",    "Houseboat Fire",
+      "MED",    "Medical",
+      "NATGAS", "Natural Gas Leak",
+      "PLANE",  "Plane Crash",
+      "ROPE",   "Rope Rescue",
+      "SC",     "Structure Collapse",
+      "SF",     "Structure Fire",
+      "SMOKEB", "Smoke in a Building",
+      "VEG",    "Vegetation Fire",
+      "VFB",    "Vehicle Fire near a Building",
+      "VFC",    "Vehicle Fire (Commercial)",
+      "VFF",    "Vehicle Fire (Freeway)",
+      "WATER",  "Water Rescue"
+  });
+
   private static final Properties CITY_CODES = buildCodeTable(new String[]{
+      "GGN", "GOLDEN GATE NATIONAL RECREATION AREA",
       "MVP", "MILL VALLEY",
       "SAS", "SAUSALITO",
-      "SBY", "",        // Belvedere Tiburon
+      "SBY", "STRAWBERRY",
       "TAM", "TAMALPAIS-HOMESTEAD VALLEY"
-      
   });
 }
