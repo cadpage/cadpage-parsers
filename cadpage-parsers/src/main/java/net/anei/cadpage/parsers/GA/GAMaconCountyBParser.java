e5a5ffbae5167ea911dc0801b11ce64c3b689614
Ken Corbin <ken@cadpage.org>
2026-04-09 15:24:57 -0700

    Fixed parsing problem with lots of counties in GA

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/GA/GAMaconCountyBParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/GA/GAMaconCountyBParser.java
@@ -1,27 +1,29 @@
 package net.anei.cadpage.parsers.GA;
 
-import net.anei.cadpage.parsers.dispatch.DispatchA73Parser;
+import net.anei.cadpage.parsers.dispatch.DispatchA19Parser;
 
-public class GAMaconCountyBParser extends DispatchA73Parser {
+public class GAMaconCountyBParser extends DispatchA19Parser {
 
   public GAMaconCountyBParser() {
     this("MACON COUNTY");
   }
 
   GAMaconCountyBParser(String county) {
-    super(new String[] {"CHATTAHOOCHEE", "CRISP", "DOOLY", "HARRIS", "HOUSTON", "MACON", "MARION", "MERIWETHER", "MUSCOGEE",
-                        "OGLETHORPE", "PEACH", "PULASKI", "RANDOLPH", "SCHLEY", "SUMTER", "STEWARD", "TALBOT", "TERRELL",
-                        "TAYLOR", "UPSON", "WEBSTER", "WILCOX"},
-          county, "GA");
+    super(county, "GA");
   }
 
   @Override
   public String getFilter() {
-    return "@alert.active911.com,middleflint@mfre911.com";
+    return "dispatch@mfre911.com";
   }
 
   @Override
   public String getAliasCode() {
     return "GAMaconCountyB";
+  }
+
+  @Override
+  public int getMapFlags() {
+    return MAP_FLG_PREFER_GPS;
   }
 }
