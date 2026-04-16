04c2b1af014f375ce41e84f3df0cc7c528e842cd
Ken Corbin <ken@cadpage.org>
2026-04-15 17:12:23 -0700

    WOrking on Lewis County, NY (C)

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/NY/NYLewisCountyCParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/NY/NYLewisCountyCParser.java
@@ -31,9 +31,27 @@ public class NYLewisCountyCParser extends FieldProgramParser {
 
   @Override
   public Field getField(String name) {
+    if (name.equals("ADDR")) return new MyAddressField();
     if (name.equals("DATETIME")) return new DateTimeField(DATETIME_FMT, true);
     if (name.equals("INFO")) return new MyInfoField();
     return super.getField(name);
+  }
+
+  private class MyAddressField extends AddressField {
+    @Override
+    public void parse(String field, Data data) {
+      int pt = field.indexOf(';');
+      if (pt >= 0) {
+        data.strPlace = field.substring(pt+1).trim();
+        field = field.substring(0,pt).trim();
+      }
+      super.parse(field, data);
+    }
+
+    @Override
+    public String getFieldNames() {
+      return super.getFieldNames() + " PLACE";
+    }
   }
 
   private static final Pattern INFO_JUNK_PTN = Pattern.compile("\\d\\d/\\d\\d/\\d\\d \\d\\d:\\d\\d:\\d\\d .*:");
