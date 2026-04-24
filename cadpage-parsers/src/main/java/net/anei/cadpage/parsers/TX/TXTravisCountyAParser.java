0ae9be9adffdd7bc1345a711babab4f4570c7ce6
Ken Corbin <ken@cadpage.org>
2026-04-23 21:57:36 -0700

    Fixed parsing problem with Travis County, TX (A)

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/TX/TXTravisCountyAParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/TX/TXTravisCountyAParser.java
@@ -28,17 +28,6 @@ public class TXTravisCountyAParser extends MsgParser {
 
   private static final Pattern COMMENT_PTN = Pattern.compile("Comment: (.*?), ?(From -.*?)(?: From -.*)?");
 
-  private static final Pattern MASTER1 =
-      Pattern.compile("From -([A-Z0-9]+) Dispatch - ?\\d?ALARM -(.*?) - BOX -([-A-Z0-9]*) ?On -([ A-Z0-9]*) - AT -(.+?) - INC# =>(\\d+) Case Num:([- A-Z0-9]*) For -([A-Z0-9,]+)");
-
-  private static final Pattern MASTER2 =
-      Pattern.compile("From - ?(NON - ADVI|[A-Z0-9]+) ?- ?\\d ?Alarm / ?(.*?) (?:Pri (\\d+) +)?(?:Box|BOX|\\| RAP) - ?([-A-Z0-9]*) ?@ ?(.*?) (?:\\| )?XStreets: *(.*?)[ \\|]+?On - ?([ A-Z0-9]*)\\|? Time:[ \\|]*(?:(\\d\\d:\\d\\d:\\d\\d)|(\\d\\d:\\d\\d [AP]M))[ \\|]+Inc# ?(\\d+)(?: Case Num:([-A-Z0-9]*))?[ \\|]+For - ?([A-Z0-9,]*)(?: Lat: ?(\\d{8}) Lon: ?(\\d{8}))?");
-
-  private static final Pattern MASTER3 =
-      Pattern.compile("(?:INCIDENT ASSIGNED TO YOU!!*|Response Info from Dispatch) - ?\\d?ALARM -(.*?) - BOX -([-A-Z0-9]*) FC=(.+?)- AT -(.*?) - ASSIGNED UNITS=>([A-Z0-9,]+) ?- INC# =>(\\d+)");
-
-  private static final Pattern CROSS_PTN = Pattern.compile("(?:(?:N?o )?CrossStreet Found)?/?(.*?)/?(?:No CrossStreet Found)?");
-
   private static final DateFormat TIME_FMT = new SimpleDateFormat("hh:mm aa");
 
   @Override
@@ -48,65 +37,129 @@ public class TXTravisCountyAParser extends MsgParser {
       data.strSupp = match.group(1).trim();
       body = match.group(2).trim();
     }
-    match = MASTER1.matcher(body);
-    if (match.matches()) {
-      setFieldList("INFO SRC CALL MAP CH ADDR CITY APT ID UNIT");
-
-      data.strSource = match.group(1);
-      data.strCall = match.group(2).trim();
-      data.strMap = match.group(3).trim();
-      data.strChannel = match.group(4).trim();
-      parseAddress(match.group(5).trim(), data);
-      data.strCallId = append(match.group(7), "/", match.group(6));
-      data.strUnit = match.group(8);
-
-      return true;
-    }
-
-    match = MASTER2.matcher(body);
-    if (match.matches()) {
-      setFieldList("INFO SRC CALL PRI MAP ADDR CITY APT X CH TIME ID UNIT GPS");
-
-      data.strSource = match.group(1);
-      data.strCall = match.group(2).trim();
-      data.strPriority = getOptGroup(match.group(3));
-      data.strMap = match.group(4).trim();
-      parseAddress(match.group(5).trim(), data);
-      String cross  = match.group(6).trim();
-      data.strChannel = match.group(7);
-      String time = match.group(8);
-      if (time != null) data.strTime = time;
-      else setTime(TIME_FMT, match.group(9), data);
-      data.strCallId = append(match.group(10), "/", getOptGroup(match.group(11)));
-      data.strUnit = match.group(12);
-      String gps1 = match.group(13);
-      String gps2 = match.group(14);
-
-      match = CROSS_PTN.matcher(cross);
-      if (match.matches()) cross = match.group(1);
-      data.strCross = cross;
-
-      if (gps1 != null) {
-        setGPSLoc(fixGPS(gps1)+','+fixGPS(gps2), data);
-      }
-
-      return true;
-
-    }
-
-    match = MASTER3.matcher(body);
-    if (match.matches()) {
-      setFieldList("CALL MAP CH ADDR CITY APT UNIT ID");
-      data.strCall = match.group(1);
-      data.strMap = match.group(2);
-      data.strChannel = match.group(3).trim();
-      parseAddress(match.group(4).trim(), data);
-      data.strUnit = match.group(5).trim();
-      data.strCallId = match.group(6);
-      return true;
-    }
-
+    if (parseFmt1(body, data)) return true;
+    if (parseFmt2(body, data)) return true;
+    if (parseFmt3(body, data)) return true;
+    if (parseFmt4(body, data)) return true;
+    if (parseFmt5(body, data)) return true;
+    if (parseFmt6(body, data)) return true;
     return false;
+  }
+
+  private static final Pattern MASTER1 =
+      Pattern.compile("From -([A-Z0-9]+) Dispatch - ?\\d?ALARM -(.*?) - BOX -([-A-Z0-9]*) ?On -([ A-Z0-9]*) - AT -(.+?) - INC# =>(\\d+) Case Num:([- A-Z0-9]*) For -([A-Z0-9,]+)");
+
+  private boolean parseFmt1(String body, Data data) {
+    Matcher match;
+    match = MASTER1.matcher(body);
+    if (!match.matches()) return false;
+    setFieldList("INFO SRC CALL MAP CH ADDR CITY APT ID UNIT");
+
+    data.strSource = match.group(1);
+    data.strCall = match.group(2).trim();
+    data.strMap = match.group(3).trim();
+    data.strChannel = match.group(4).trim();
+    parseAddress(match.group(5).trim(), data);
+    data.strCallId = append(match.group(7), "/", match.group(6));
+    data.strUnit = match.group(8);
+    return true;
+  }
+
+  private static final Pattern MASTER2 =
+      Pattern.compile("From - ?(NON - ADVI|[A-Z0-9]+) ?- ?\\d? ?Alarm / ?(.*?) (?:Pri (\\d+) +)?(?:Box|BOX|\\| RAP) - ?([-A-Z0-9]*) ?@ ?(.*?) (?:\\| )?XStreets: *(.*?)[ \\|]+?On - ?([ A-Z0-9]*)\\|? Time:[ \\|]*(?:(\\d\\d:\\d\\d:\\d\\d)|(\\d\\d:\\d\\d [AP]M))[ \\|]+Inc# ?(\\d+)(?: Case Num:([-A-Z0-9]*))?[ \\|]+For - ?([A-Z0-9,]*)(?: Lat: ?(\\d+) Lon: ?(\\d+))?");
+  private static final Pattern CROSS_PTN = Pattern.compile("(?:(?:N?o )?CrossStreet Found)?/?(.*?)/?(?:No CrossStreet Found)?");
+
+  private boolean parseFmt2(String body, Data data) {
+    Matcher match = MASTER2.matcher(body);
+    if (!match.matches()) return false;
+    setFieldList("INFO SRC CALL PRI MAP ADDR CITY APT X CH TIME ID UNIT GPS");
+
+    data.strSource = match.group(1);
+    data.strCall = match.group(2).trim();
+    data.strPriority = getOptGroup(match.group(3));
+    data.strMap = match.group(4).trim();
+    parseAddress(match.group(5).trim(), data);
+    String cross  = match.group(6).trim();
+    data.strChannel = match.group(7);
+    String time = match.group(8);
+    if (time != null) data.strTime = time;
+    else setTime(TIME_FMT, match.group(9), data);
+    data.strCallId = append(match.group(10), "/", getOptGroup(match.group(11)));
+    data.strUnit = match.group(12);
+    String gps1 = match.group(13);
+    String gps2 = match.group(14);
+
+    match = CROSS_PTN.matcher(cross);
+    if (match.matches()) cross = match.group(1);
+    data.strCross = cross;
+
+    if (gps1 != null) {
+      setGPSLoc(fixGPS(gps1)+','+fixGPS(gps2), data);
+    }
+    return true;
+  }
+
+  private static final Pattern MASTER3 =
+      Pattern.compile("INCIDENT ASSIGNED - ?\\d?Alarm /(.*?) \\| RAP - ?([-A-Z0-9]*) ?@ ?(.*?) \\| On - ?(.*?) \\| Inc#(\\d+) \\| For - ?(.*)");
+
+  private boolean parseFmt3(String body, Data data) {
+    Matcher match = MASTER3.matcher(body);
+    if (!match.matches()) return false;
+    setFieldList("CALL MAP CH ADDR CITY APT CH ID UNIT");
+    data.strCall = match.group(1);
+    data.strMap = match.group(2);
+    parseAddress(match.group(3), data);
+    data.strChannel = match.group(4).trim();
+    data.strCallId = match.group(5);
+    data.strUnit =  match.group(6).trim();
+    return true;
+  }
+
+  private static final Pattern MASTER4 =
+      Pattern.compile("DISPATCH ALERT!! Inc #:(\\S*) Map:(\\S*) Location:(.*?) Prem:(.*?) Bldg:(\\S*) Apt:(\\S*) City:(.*?) Zip:\\S* Problem:(.*?) (?:Pri (\\S*) )?Unit:(\\S*)");
+
+  private boolean parseFmt4(String body, Data data) {
+    setFieldList("INFO ID MAP ADDR PLACE APT CITY CALL PRI UNIT");
+    Matcher match = MASTER4.matcher(body);
+    if (!match.matches()) return false;
+    data.strCallId = match.group(1);
+    data.strMap = match.group(2);
+    parseAddress(match.group(3).trim(), data);
+    data.strPlace = getOptGroup(match.group(4));
+    data.strApt = append(data.strApt, "-", match.group(5));
+    data.strApt = append(data.strApt, "-", match.group(6));
+    data.strCity = match.group(7).trim();
+    data.strCall =  match.group(8).trim();
+    data.strPriority = getOptGroup(match.group(9));
+    data.strUnit =  match.group(10);
+    return true;
+  }
+
+  private static final Pattern MASTER5 =
+      Pattern.compile("(Hospital Times): Inc# (\\S*) \\| Add:(.*?) \\| Zip:\\S* \\| (.*?) \\| Unit: (\\S*)");
+
+  private boolean parseFmt5(String body, Data data) {
+    setFieldList("CALL ID ADDR APT INFO");
+    Matcher match = MASTER5.matcher(body);
+    if (!match.matches()) return false;
+    data.strCall = match.group(1);
+    data.strCallId = match.group(2);
+    parseAddress(match.group(3).trim(), data);
+    data.strSupp = match.group(4).trim().replace(" | ", "\n");
+    return true;
+  }
+
+  private static final Pattern MASTER6 =
+      Pattern.compile("Closest \\*+TRAUMA FACILITY\\*+ to (.*?): +(.*)");
+
+  private boolean parseFmt6(String body, Data data) {
+    setFieldList("CALL ADDR CITY APT INFO");
+    Matcher match = MASTER6.matcher(body);
+    if (!match.matches()) return false;
+    data.strCall = "Closest Trauma Facility";
+    parseAddress(match.group(1).trim(), data);
+    data.strSupp = stripFieldEnd(match.group(2).trim(), "[Shared]").replace(" | ", "\n");
+    return true;
   }
 
   @Override
