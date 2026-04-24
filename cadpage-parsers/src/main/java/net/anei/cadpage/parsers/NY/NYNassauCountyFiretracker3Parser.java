e50eb6c5f32f6e52c90d3b4d68432a33ef78b758
Ken Corbin <ken@cadpage.org>
2026-04-23 18:11:48 -0700

    Fixed parsing problem with Nassau County, NY (FireTracker)

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/NY/NYNassauCountyFiretracker3Parser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/NY/NYNassauCountyFiretracker3Parser.java
@@ -1,57 +1,32 @@
 package net.anei.cadpage.parsers.NY;
 
-import net.anei.cadpage.parsers.FieldProgramParser;
 import net.anei.cadpage.parsers.MsgInfo.Data;
+import net.anei.cadpage.parsers.dispatch.DispatchH05Parser;
 
-public class NYNassauCountyFiretracker3Parser extends FieldProgramParser {
-  
+public class NYNassauCountyFiretracker3Parser extends DispatchH05Parser {
+
   public NYNassauCountyFiretracker3Parser() {
-    super("NASSAU COUNTY", "NY", 
-          "Call_Type:CALL! Additional_Info:CALL/SDS Address:ADDR! Between:X CountyNum:ID! DOA:DATE! TOA:TIME! FDID:SRC!");
+    super("NASSAU COUNTY", "NY",
+          "FDID:SRC! DATETIME! Call_Type:CALL! Address:ADDRCITY! bet:X! Additional_Information:INFO! Alerts:ALERT! " +
+              "Narrative:EMPTY! INFO_BLK+ CFS_Number:SKIP! 1st_Unit:SKIP! ID! TIMES+ https:GPS! END");
+    setAccumulateUnits(true);
   }
-  
+
   @Override
   public String getFilter() {
-    return "dispatch@firetracker.net";
-  }
-  
-  @Override
-  protected boolean parseMsg(String subject, String body, Data data) {
-    if (!subject.equals("FirePage")) return false;
-    if (!body.startsWith("FPC FD Call ")) return false;
-    if (!body.endsWith("[FireTracker]")) return false;
-    body = body.substring(12, body.length()-13).trim();
-    
-    body = body.replace('\n', ' ');
-    return super.parseMsg(body, data);
-  }
-  
-  @Override
-  public Field getField(String name) {
-    if (name.equals("CALL")) return new MyCallField();
-    if (name.equals("X")) return new MyCrossField();
-    if (name.equals("ID")) return new IdField("\\d{12}", true);
-    if (name.equals("DATE")) return new DateField("\\d\\d?/\\d\\d?/\\d{4}", true);
-    if (name.equals("TIME")) return new TimeField("\\d\\d?:\\d\\d", true);
-    return super.getField(name);
-  }
-  
-  private class MyCallField extends CallField {
-    @Override
-    public void parse(String field, Data data) {
-      field = field.replace("] /[", "/");
-      field = stripFieldStart(field, "[");
-      field = stripFieldEnd(field, "]");
-      super.parse(field, data);
-    }
-  }
-  
-  private class MyCrossField extends CrossField {
-    @Override
-    public void parse(String field, Data data) {
-      field = field.replace(" and ", " / ");
-      super.parse(field, data);
-    }
+    return "NassauFireCall@nassaucountyny.gov";
   }
 
+  @Override
+  protected boolean parseHtmlMsg(String subject, String body, Data data) {
+    int pt = body.indexOf("CONFIDENTIALITY NOTICE:");
+    if (pt >= 0) body = body.substring(0,pt).trim();
+    return super.parseHtmlMsg(subject, body, data);
+  }
+
+  @Override
+  public Field getField(String name) {
+    if (name.equals("DATETIME")) return new DateTimeField("\\d\\d?/\\d\\d?/\\d{4} +\\d\\d:\\d\\d:\\d\\d", true);
+    return super.getField(name);
+  }
 }
