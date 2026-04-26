412a457173609c79fb0fd994bfd8f6e4d2d470e5
Ken Corbin <ken@cadpage.org>
2026-04-26 14:06:06 -0700

    Fixed parsing problem with Patrick County, VA (B)

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/VA/VAPatrickCountyBParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/VA/VAPatrickCountyBParser.java
@@ -1,5 +1,8 @@
 package net.anei.cadpage.parsers.VA;
 
+import java.util.regex.Pattern;
+
+import net.anei.cadpage.parsers.MsgInfo.Data;
 import net.anei.cadpage.parsers.SplitMsgOptions;
 import net.anei.cadpage.parsers.SplitMsgOptionsCustom;
 import net.anei.cadpage.parsers.dispatch.DispatchA76Parser;
@@ -9,10 +12,10 @@ public class VAPatrickCountyBParser extends DispatchA76Parser {
   public VAPatrickCountyBParser() {
     super("PATRICK COUNTY", "VA");
   }
-  
+
   @Override
   public String getFilter() {
-    return "cad@sheriff.co.patrick.va.us";
+    return "cad@sheriff.co.patrick.va.us,tylerirvin93@gmail.com";
   }
 
   @Override
@@ -22,6 +25,23 @@ public class VAPatrickCountyBParser extends DispatchA76Parser {
       @Override public boolean splitBlankIns() { return false; }
       @Override public int splitBreakLength() { return 130; }
       @Override public int splitBreakPad() { return 1; }
+//      @Override public boolean splitKeepLeadBreak() { return true; }
     };
+  }
+
+  private static final Pattern BAD_PIPE_PTN = Pattern.compile("\\b(?:APT|APARTMENT|UNIT)\\|");
+
+  @Override
+  protected boolean parseMsg(String subject, String body, Data data) {
+    body = body.replace("\nText STOP to opt out", "");
+    int pt = body.indexOf("\n-- \n");
+    if (pt >= 0) body = body.substring(0,pt).trim();
+    body = BAD_PIPE_PTN.matcher(body).replaceAll("");
+    body = body.replace('\n',' ');
+    if (subject.contains("SMSForwarder")) {
+      if (body.length() >= 128 && body.length() <= 131) data.expectMore = true;
+    }
+    body = body.replace("| ", " | ");
+    return super.parseMsg(body, data);
   }
 }
