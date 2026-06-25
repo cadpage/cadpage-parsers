cce3bad33ab112028ea3d58c00c648ebae57de6b
Ken Corbin <ken@cadpage.org>
2026-06-25 16:26:40 -0700

    Fixed parsing problem with Greene County, PA

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/PA/PAGreeneCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/PA/PAGreeneCountyParser.java
@@ -1,138 +1,57 @@
 package net.anei.cadpage.parsers.PA;
 
-import java.util.Properties;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import net.anei.cadpage.parsers.CodeSet;
 import net.anei.cadpage.parsers.MsgInfo.Data;
-import net.anei.cadpage.parsers.SmartAddressParser;
+import net.anei.cadpage.parsers.FieldProgramParser;
 
-public class PAGreeneCountyParser extends SmartAddressParser {
+public class PAGreeneCountyParser extends FieldProgramParser {
 
   public PAGreeneCountyParser() {
-    super("GREENE COUNTY", "PA");
-    setFieldList("CALL ADDR CITY APT INFO X MAP PLACE DATE TIME ID UNIT");
-    setupCallList(CALL_LIST);
-    setupMultiWordStreets(MW_STREET_LIST);
+    super("GREENE COUNTY", "PA",
+          "SKIP+ DATE/TIME:DATETIME! CALL_TYPE:CODE_CALL! LOCATION:ADDRCITY! ADDITIONAL_LOCATION:PLACE! CROSS_STREETS:X! COMMON_NAME:PLACE! UNITS:UNIT! NARRATIVE:INFO! INFO/N+ Latitude:GPS1! Longitude:GPS2! ");
   }
-  
+
   @Override
   public String getFilter() {
-    return "911_Call_Center@co.greene.pa.us";
+    return "dispatch@co.greene.pa.us";
   }
 
-  private static Pattern MASTER = Pattern.compile("(.*?)(?: ,(\\d+) [A-Z]*)?#:(.*?)X:/?(.*?)/?GD:(.*?)ZN:(.*?)CP:(.*?)(?:(\\d{2}/\\d{2}/\\d{4}) (\\d{1,2}:\\d{2}:\\d{2}))?MI#:(\\d{9})(.*)");
+  @Override
+  public int getMapFlags() {
+    return MAP_FLG_PREFER_GPS;
+  }
 
   protected boolean parseMsg(String subject, String body, Data data) {
-    
-    if (!subject.equals("ALERT")) return false;
-    
-    //cut off all from the first \n on
-    int ni = body.indexOf("\n");
-    if (ni >= 0) body = body.substring(0, ni);
-    
-    Matcher mat = MASTER.matcher(body);
-    if (!mat.matches()) return false;
 
-    parseAddress(StartType.START_CALL,  FLAG_ANCHOR_END | FLAG_START_FLD_NO_DELIM, mat.group(1).trim(), data);
-    data.strCity = getOptGroup(CITY_LIST.getProperty(getOptGroup(mat.group(2))));
-    data.strApt = mat.group(3).trim();
-    parseAddress(StartType.START_ADDR,  FLAG_ANCHOR_END | FLAG_ONLY_CROSS, mat.group(4), data);
-    data.strMap = append(mat.group(5).trim(), " ", mat.group(6).trim());
-    data.strPlace = mat.group(7).trim();
-    data.strDate = getOptGroup(mat.group(8));
-    data.strTime = getOptGroup(mat.group(9));
-    data.strCallId = mat.group(10);
-    data.strUnit = getOptGroup(mat.group(11));
-    return true;
+    if (!subject.equals("Dispatch")) return false;
+    int pt = body.indexOf("\n________");
+    if (pt >= 0) body = body.substring(0,pt).trim();
+    return parseFields(body.split("\n"), data);
   }
-  
-  private static final Properties CITY_LIST = buildCodeTable(new String[]{
-      "03", "CENTER TWP",
-      "05", "CUMBERLAND TWP",
-      "06", "DUNKARD TWP",
-      "07", "FRANKLIN TWP",
-      "13", "JACKSON TWP",
-      "14", "JEFFERSON",
-      "15", "JEFFERSON TWP",
-      "17", "WAYNESBURG",
-      "19", "PERRY TWP",
-      "20", "RICES LANDING",
-      "21", "RICHHILL TWP",
-      "23", "WASHINGTON TWP",
-      "24", "WAYNESBURG",
-      "26", "WAYNESBURG",
-      "29", "WHITELEY TWP"
-  });
-  
-  private static final CodeSet CALL_LIST = new CodeSet(
-      "ABDOMINAL PAIN ALS",
-      "ABDOMINAL PAIN BLS",
-      "ASSAULT ALS",
-      "ASSAULT BLS",
-      "BACK PAIN ALS",
-      "BACK PAIN BLS",
-      "CARD ARREST ADULT",
-      "CHEST PAIN ALS",
-      "CHEST PAIN BLS",
-      "CHIMNEY FIRE",
-      "DIABETIC ALS",
-      "DIABETIC BLS",
-      "DIFF BREATHING ALS",
-      "DIFF BREATHING BLS",
-      "FALLS ALS",
-      "FALLS BLS",
-      "FIRE ALARM COMMERC",
-      "FIRE ALARM RESIDEN",
-      "FIRE CARBON MONO",
-      "FIRE FIELD",
-      "FIRE HAZMAT",
-      "FIRE MUTUAL AID",
-      "FIRE REKINDLE",
-      "FIRE STRUCTURE RES",
-      "FIRE STRUCTURE UNK",
-      "FIRE VEHICLE",
-      "FIRE/ODOR INVEST",
-      "GENERAL SICK ALS",
-      "GENERAL SICK BLS",
-      "HEADACHE ALS",
-      "HEADACHE BLS",
-      "HEART PROBLEMS ALS",
-      "HEART PROBLEMS BLS",
-      "MVA NO INJURY",
-      "MVA W/ INJURY ALS",
-      "MVA W/ INJURY BLS",
-      "MVA W/ INTRAP",
-      "MVA W/ UNK INJURY",
-      "OVERDOSE ALS",
-      "OVERDOSE BLS",
-      "PSYCH EMERG ALS",
-      "PSYCH EMERG BLS",
-      "PUBLIC SERVICE EMS",
-      "PUBLIC SERVICE FIR",
-      "SEIZURES ALS",
-      "SEIZURES BLS",
-      "STROKE CVA ALS",
-      "STROKE CVA BLS",
-      "SUICIDE ATEMPT ALS",
-      "SUICIDE ATEMPT BLS",
-      "TRASH FIRE",
-      "TRAMTIC INJURY ALS",
-      "TRAMTIC INJURY BLS",
-      "UNCONSCIOUS ALS",
-      "UNCONSCIOUS BLS",
-      "UNKNOWN FIRE",
-      "UNK PROBLEM ALS",
-      "UNK PROBLEM BLS");
-  
-  private static final String[] MW_STREET_LIST = new String[]{
-      "GRIMES RUN",
-      "HUNTINGTON WOODS",
-      "MT VIEW GARDENS",
-      "ROY FURMAN",
-      "SMITH CREEK",
-      "SOUTH BRANCH MUDDY CREEK"
-  };
 
+  public Field getField(String name) {
+    if (name.equals("DATETIME")) return new DateTimeField("\\d\\d?/\\d\\d?/\\d{4} +\\d\\d:\\d\\d:\\d\\d", true);
+    if (name.equals("CODE_CALL")) return new MyCodeCallField();
+    return super.getField(name);
+  }
+
+  private static final Pattern CODE_CALL_PTN = Pattern.compile("([A-Z0-9]+)- *(.*)");
+  private class MyCodeCallField extends Field {
+    @Override
+    public void parse(String field, Data data) {
+      Matcher match = CODE_CALL_PTN.matcher(field);
+      if (match.matches()) {
+        data.strCode =  match.group(1);
+        field = match.group(2);
+      }
+      data.strCall = field;
+    }
+
+    @Override
+    public String getFieldNames() {
+      return "CODE CALL";
+    }
+  }
 }
