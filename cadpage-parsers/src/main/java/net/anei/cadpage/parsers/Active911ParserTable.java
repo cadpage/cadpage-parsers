d7610c602f1cb27be58a40b84349a83d49776af5
Ken Corbin <ken@cadpage.org>
2026-07-22 20:33:48 -0700

    Update A911 parser table

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/Active911ParserTable.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/Active911ParserTable.java
@@ -2264,6 +2264,7 @@ public class Active911ParserTable {
       "US/OH/TallmadgeFire",              "OHSummitCountyI",
       "US/OH/TheBeachWaterpark",          "OHWarrenCountyD",
       "US/OH/Tiffin",                     "OHSenecaCounty",
+      "US/OH/TriTwp",                     "OHDelawareCountyD",
       "US/OH/TrumbullCounty",             "OHTrumbullCounty",
       "US/OH/TrumbullCoWarrenTwpPD",      "OHTrumbullCountyB",
       "US/OH/UnionCounty",                "OHUnionCounty,OHMadisonCounty,OHCrawfordCounty",
