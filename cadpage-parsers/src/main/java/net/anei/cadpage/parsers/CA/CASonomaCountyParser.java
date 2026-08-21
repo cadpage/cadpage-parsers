b6bd8e01b9ede6ee7bda1985c301f0dc415e6c46
Ken Corbin <ken@cadpage.org>
2026-08-21 15:53:54 -0700

    Fixed parsing problem with Sonoma County, CA

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/CA/CASonomaCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/CA/CASonomaCountyParser.java
@@ -6,207 +6,183 @@ import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import net.anei.cadpage.parsers.CodeTable;
 import net.anei.cadpage.parsers.FieldProgramParser;
 import net.anei.cadpage.parsers.MsgInfo.Data;
+import net.anei.cadpage.parsers.StandardCodeTable;
 
 /**
  * Sonoma County, CA
  */
 public class CASonomaCountyParser extends FieldProgramParser {
-  
-  private String version;
-  
+
   public CASonomaCountyParser() {
-    super(CITY_LIST, 
+    super(CITY_CODES,
            "SONOMA COUNTY", "CA",
-           "( SELECT/1 Location:ADDR/S? EID:ID! TYPE_CODE:CALL? CALLER_NAME:NAME CALLER_ADDR:ADDR2/S TIME:TIME Comments:INFO | " +
-             "Loc:ADDR? BOX:BOX! TYP:CALL? CN:NAME! C#:PHONE TYP:CALL? TYPE_CODE:SKIP CALLER_NAME:NAME CALLER_ADDR:ADDR2/S TIME:TIME COM:INFO )");
+           "LOC:ADDRCITYST/S! ( BOX:BOX! CN:NAME! CLLR#:PHONE! TYPE:CALL! ALARM#:PRI! CALLER_ADDR:SKIP! DISP:UNIT! TIME:TIME! COM:INFO! INFO/N+ CASE:ID! " +
+                             "| TYP:CALL! REMARKS:INFO! INFO/N+ CN:NAME! CALLER_ADDR:SKIP! PHONE:PHONE! EVENT__#:ID! " +
+                             ") END",
+           FLDPROG_DOUBLE_UNDERSCORE);
   }
-  
+
   @Override
   public String getFilter() {
-    return "sclec@sonoma-county.org,bc71@srcity.org,ps-cst@Sonoma-county.org";
+    return "ps-cst@sonoma-county.org";
   }
-  
+
   @Override
   public int getMapFlags() {
     return MAP_FLG_SUPPR_LA;
   }
-  
+
   private Set<String> unitSet = new HashSet<String>();
+
+  private static final Pattern POST_MOVE_PTN = Pattern.compile("Unit (\\S*) Relocate to: @(?:(.*) - )?(.*)");
 
   @Override
   protected boolean parseMsg(String subject, String body, Data data) {
-    unitSet.clear();
-    if (body.startsWith("LOC:")) body = "Loc:" + body.substring(4);
-    body = body.replace(" CN:COM ", " CN: COM:").replace(" CN:COM:", " CN: COM:").replace("TYP:", " TYP:").replace("TIME:", " TIME:");
-    version = body.startsWith("Location:") || body.startsWith("EID") ? "1" : "2";
-    if (!super.parseMsg(body, data)) return false;
-    if (data.strAddress.startsWith("ID #") && data.strGPSLoc.length() > 0) {
-      data.strAddress = data.strAddress + " (" + data.strGPSLoc + ")";
+    Matcher match = POST_MOVE_PTN.matcher(body);
+    if (match.matches()) {
+      setFieldList("CALL UNIT PLACE ADDR");
+      data.strCall = "Post Move";
+      data.strUnit = match.group(1);
+      data.strPlace = getOptGroup(match.group(2));
+      parseAddress(match.group(3).trim(), data);
+      return true;
     }
-    return true;
-  }
-  
-  @Override
-  protected String getSelectValue() {
-    return version;
+    unitSet.clear();
+    return parseFields(body.split("\n"), data);
   }
 
   @Override
   public Field getField(String name) {
-    if (name.equals("ADDR")) return new MyAddressField();
-    if (name.equals("NAME")) return new MyNameField();
-    if (name.equals("ADDR2")) return new MyAddress2Field();
-    if (name.equals("TIME")) return new MyTimeField();
+    if (name.equals("ADDRCITYST")) return new MyAddressCityStateField();
+    if (name.equals("CALL")) return new MyCallField();
+    if (name.equals("TIME")) return new TimeField("\\d\\d:\\d\\d:\\d\\d", true);
     if (name.equals("INFO")) return new MyInfoField();
     return super.getField(name);
   }
-  
-  private static final Pattern COLON_MARKER = Pattern.compile(" *: *");
-  private static final Pattern SRC_CODE_PTN = Pattern.compile("[A-Z0-9]{0,3}");
-  private static final Pattern UNIT_PTN = Pattern.compile("@([A-Z]+\\d+)(?:[- ,]+(.*))?");
-  private static final Pattern UNIT2_PTN = Pattern.compile("@([A-Z]+\\d+)");
-  private class MyAddressField extends AddressField {
-    
+
+  private static final Pattern LEAD_STN_PTN1 = Pattern.compile("([A-Z]{2,3}):(?: @([A-Z0-9]+) *:)? *(.*)");
+  private static final Pattern LEAD_STN_PTN2 = Pattern.compile(".*? ([A-Z]{2,3}): @([A-Z0-9]+) *: *(.*)");
+  private static final Pattern COLON_MARKER = Pattern.compile(" *: @ *");
+  private static final Pattern TRAIL_ZIP_PTN = Pattern.compile("(.*?)(?: +\\d{5})+");
+  private static final Pattern NOT_APT_PTN = Pattern.compile("[A-Z ]{3,}|CA");
+
+  private class MyAddressCityStateField extends AddressCityStateField {
+
     @Override
-    public void parse(String fld, Data data) {
-      
-      // Onstar reports go strait into the address field
-      // we will append the GPS coordinates later
-      if (fld.startsWith("ID #")) {
-        data.strAddress = fld;
-        return;
+    public void parse(String field, Data data) {
+
+      String leadCity = null;
+      Matcher match = LEAD_STN_PTN1.matcher(field);
+      if (match.matches()) {
+        leadCity = match.group(1);
+        data.strUnit = match.group(2);
+        field = match.group(3);
       }
-      
+      else if ((match = LEAD_STN_PTN2.matcher(field)).matches()) {
+        leadCity =  match.group(1);
+        data.strUnit = match.group(2);
+        field = match.group(3);
+      }
+
+      String gps = null;
+      if (field.startsWith("LL(")) {
+        int pt = field.indexOf(")");
+        if (pt < 0) abort();
+        gps = field.substring(0,pt+1);
+        field = field.substring(pt+1).trim();
+        field = stripFieldStart(field, ": EST ");
+      }
+
       // Split everything by colon markers
-      String[] parts = COLON_MARKER.split(fld);
-      int ndx = 0;
-      
-      // Does the first part look like a source code?
-      if (ndx < parts.length) {
-        String src = parts[ndx];
-        if (SRC_CODE_PTN.matcher(src).matches()) {
-          data.strSource = src;
-          ndx++;
-        }
-      }
-      
-      // First part may be (or contain) a unit designation
-      if (ndx < parts.length) {
-        String unit = parts[ndx];
-        Matcher match = UNIT_PTN.matcher(unit);
-        if (match.matches()) {
-          unit = match.group(1);
-          String addr = match.group(2);
-          addUnit(unit, data);
-          if (addr != null) {
-            parts[ndx] = addr;
-          } else {
-            ndx++;
-          }
-        }
-      }
-      
-      // Followed by address, which will be parsed later
-      fld = (ndx < parts.length ? stripFieldStart(parts[ndx++], "@") : "");
-      
-      // first part is the address proper
-      // anything following that will either be a city or place
-      while (ndx < parts.length) {
-        String tmp = parts[ndx++];
-        if (tmp.equals("@UNIT AT")) continue;
-        Matcher match = UNIT2_PTN.matcher(tmp);
-        if (match.matches()) {
-          addUnit(match.group(1), data);
-        }
-        else if (data.strCity.length() == 0 && isCity(tmp)) {
-          data.strCity = tmp;
-        } else {
-          tmp = stripFieldStart(tmp, "@");
-          data.strPlace = append(data.strPlace, " - ", tmp);
+      // First part is address.  Everything else is a place name
+      String[] parts = COLON_MARKER.split(field);
+      field = parts[0];
+      for (int ndx = 1; ndx < parts.length; ndx++) {
+        String place = stripFieldStart(parts[ndx], "UNIT AT:");
+        if (!data.strPlace.contains(place)) {
+          data.strPlace = append(data.strPlace, "-", place);
         }
       }
 
-      // Strip off trailing apt
+      // Strip off trailing zip code(s)
+      match = TRAIL_ZIP_PTN.matcher(field);
+      if (match.matches()) field = match.group(1).trim();
+
+      // Strip off trailing apt, making sure it doesn't look like a city or state
       String apt = "";
-      int pt = fld.lastIndexOf(',');
+      int pt = field.lastIndexOf(',');
       if (pt >= 0) {
-        apt = fld.substring(pt+1).trim();
-        fld = fld.substring(0, pt);
-        if (data.strCity.length() == 0) {
-          parseAddress(StartType.START_OTHER, FLAG_ONLY_CITY | FLAG_ANCHOR_END, apt, data);
-          apt = stripFieldEnd(getStart(), "-"); 
+        apt = field.substring(pt+1).trim();
+        if (NOT_APT_PTN.matcher(apt).matches()) {
+          apt = "";
+        } else {
+          field = field.substring(0,pt).trim();
         }
       }
-      
-      // Strip off trailing place name
-      pt = fld.lastIndexOf('@');
-      if (pt >= 0) {
-        data.strPlace = append(fld.substring(pt+1).trim(), " - ", data.strPlace);
-        fld = fld.substring(0,pt).trim();
-      }
-      
-      
-      fld = fld.replace(" AT ", " & ");
-      parseAddress(StartType.START_ADDR, FLAG_ANCHOR_END, fld, data);
-      if (data.strCity.length() > 0) {
-        if (data.strSource.length() == 0) data.strSource = data.strCity;
-        data.strCity = convertCodes(data.strCity, CITY_CODES);
-      } else if (data.strSource.length() > 0) {
-        data.strCity = convertCodes(data.strSource, CITY_CODES);
-      }
-      if (data.strCity.length() <= 3) data.strCity = "";
-      
-      data.strAddress = stripFieldEnd(data.strAddress, "-");
-      data.strApt = stripFieldEnd(data.strApt, "-");
+
+      // Now parse whaever is left
+      field = field.replace(" X ", " & ");
+      super.parse(field, data);
+
+      // Final cleanup
       data.strApt = append(data.strApt, "-", apt);
+      if (leadCity != null && data.strCity.isEmpty()) {
+        data.strCity = convertCodes(leadCity, CITY_CODES);
+      }
+
+      if (gps != null) {
+        if (!data.strAddress.isEmpty()) data.strPlace = data.strAddress;
+        data.strAddress = gps;
+      }
     }
-    
+
     @Override
     public String getFieldNames() {
-      return "ADDR APT X CITY SRC PLACE UNIT";
+      return "ADDR APT CITY ST PLACE UNIT?";
     }
   }
-  
-  private class MyNameField extends NameField {
+
+  private static final Pattern MED_CODE_PTN = Pattern.compile("MED \\((\\S*)\\)");
+
+  private class MyCallField extends CallField {
     @Override
     public void parse(String field, Data data) {
-      field = stripFieldEnd(field, " CMCST");
+      Matcher match = MED_CODE_PTN.matcher(field);
+      if (match.matches()) {
+        String code = match.group(1);
+        String call = CALL_CODES.getCodeDescription(code);
+        if (call != null) {
+          data.strCode = code;
+          data.strCall = call;
+          return;
+        }
+      }
       super.parse(field, data);
     }
-  }
-  
-  private class MyAddress2Field extends MyAddressField {
+
     @Override
-    public void parse(String field, Data data) {
-      if (field.length() == 0) return;
-      if (data.strAddress.length() > 0) return;
-      super.parse(field, data);
+    public String getFieldNames() {
+      return "CODE CALL";
     }
   }
-  
-  private class MyTimeField extends TimeField {
-    @Override 
-    public void parse(String field, Data data) {
-      if (field.length() < 5) return;
-      if (field.length() < 8) field = field.substring(0,5);
-      super.parse(field, data);
-    }
-  }
-  
-  private static final Pattern GPS_PTN = Pattern.compile("^(?:N )?(-\\d{3}.\\d{4,}) (?:T )?(\\d{2}.\\d{4,})(?: METERS \\d+)? *");
+
+  private static final CodeTable CALL_CODES = new StandardCodeTable();
+
+  private static final Pattern GPS_PTN = Pattern.compile("^(?:N )?(-\\d{3}.\\d{4,}) (?:T )?(\\d{2}.\\d{4,})(?: METERS(?: \\d+)?)?(?: +WPH\\d)? *");
   private static final Pattern JUNK_PTN = Pattern.compile(" *(?:Unit ([A-Z0-9]+) (?:requested case number [A-Z0-9]+|.*)|\\*\\* Case number (?:[A-Z0-9]+ has been assigned for [:A-Z0-9]+|.*)|\\*\\* >>>> (?:by: [A-Z ]+ on terminal: [a-z0-9]+|.*)) *");
   private class MyInfoField extends InfoField {
     @Override
     public void parse(String field, Data data) {
-      
+
       Matcher match = GPS_PTN.matcher(field);
       if (match.find()) {
         setGPSLoc(match.group(1)+','+match.group(2), data);
         field = field.substring(match.end());
       }
-      
+
       match = JUNK_PTN.matcher(field);
       if (match.find()) {
         int  last = 0;
@@ -220,7 +196,7 @@ public class CASonomaCountyParser extends FieldProgramParser {
         result = append(result, " - ", field.substring(last));
         field = result;
       }
-      
+
       int pt = field.indexOf("**");
       if (pt >= 0) {
         String tail = field.substring(pt);
@@ -230,18 +206,18 @@ public class CASonomaCountyParser extends FieldProgramParser {
       }
       super.parse(field, data);
     }
-    
+
     @Override
     public String getFieldNames() {
       return "GPS INFO UNIT";
     }
   }
-  
+
   private void addUnit(String unit, Data data) {
     unit = unit.toUpperCase();
     if (unitSet.add(unit)) data.strUnit = append(data.strUnit, " ", unit);
   }
-  
+
   private static final Properties CITY_CODES = buildCodeTable(new String[]{
       "AGU", "AQUA CALIENTE",
       "ANP", "ANNAPOLIS",
@@ -306,7 +282,7 @@ public class CASonomaCountyParser extends FieldProgramParser {
       "SCH", "SCHELLVILLE",
       "SE",  "SEBATOPOL",
       "SEB", "SEBATOPOL",
-      "SNMA","SONOMA", 
+      "SNMA","SONOMA",
       "SO",  "SONOMA",
       "SOL", "SOLANO COUNTY",
       "SON", "SONOMA",
@@ -323,7 +299,7 @@ public class CASonomaCountyParser extends FieldProgramParser {
       "WI",  "WINDSOR",
       "WIN", "WINDSOR",
       "WSR", "SANTA ROSA",
-      
+
       "ANCHOR BAY",   "ANCHOR BAY",
       "CLSTGA",       "CALISTOGA",
       "GEYSERVILLE",  "GEYSERVILLE",
@@ -340,6 +316,4 @@ public class CASonomaCountyParser extends FieldProgramParser {
       "BDGA",      "BODEGA",
       "BDGA BAY",  "BODEGA BAY"
   });
-  
-  private static final String[] CITY_LIST = CITY_CODES.keySet().toArray(new String[CITY_CODES.size()]);
 }
