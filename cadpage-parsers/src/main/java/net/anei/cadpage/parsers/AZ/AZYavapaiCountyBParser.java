77ccdd8c9998c11f7341f148d5660f03e109adb3
Ken Corbin <ken@cadpage.org>
2026-08-21 18:47:28 -0700

    Fixed parsing problem with Yavapai County, AZ (B)

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/AZ/AZYavapaiCountyBParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/AZ/AZYavapaiCountyBParser.java
@@ -4,15 +4,16 @@ package net.anei.cadpage.parsers.AZ;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import net.anei.cadpage.parsers.FieldProgramParser;
 import net.anei.cadpage.parsers.MsgInfo.Data;
 import net.anei.cadpage.parsers.MsgParser;
 
 
-public class AZYavapaiCountyBParser extends MsgParser {
+public class AZYavapaiCountyBParser extends FieldProgramParser {
 
   public AZYavapaiCountyBParser() {
-    super("YAVAPAI COUNTY", "AZ");
-    setFieldList("CALL MAP DATE TIME ADDR APT SRC X UNIT INFO GPS");
+    super("YAVAPAI COUNTY", "AZ",
+          "PN:CALL! ADD:ADDR! XST:X! LAT:GPS1! LON:GPS2 END");
   }
 
   @Override
@@ -21,42 +22,45 @@ public class AZYavapaiCountyBParser extends MsgParser {
   }
 
   private static final Pattern MASTER =
-      Pattern.compile("(.*?) (?:Map|MAP:)(?:(\\d+)|NOT FOUND) +(?:(\\d\\d?/\\d\\d/\\d\\d) +)?(\\d\\d:\\d\\d:\\d\\d) *ADD:(.*?) *([A-Z]+) *XST:(.*)");
+      Pattern.compile("(?:PN: *)?(.*?) (?:Map|MAP:)(?:(\\d+)|NOT FOUND|NOT) +(?:(\\d\\d?/\\d\\d/\\d\\d) +)?(\\d\\d:\\d\\d:\\d\\d) *ADD:(.*?) *([A-Z]+) *XST:(.*)");
   private static final Pattern GPS_PTN = Pattern.compile("(.*)\\bLAT: *(\\d+) +LON: *(\\d+)");
   private static final Pattern UNIT_PTN = Pattern.compile("(.*)\\bUNITS:(\\S*)\\b *(.*)");
 
   @Override
   public boolean parseMsg(String body, Data data) {
-    body = stripFieldStart(body, "PN:");
     Matcher match = MASTER.matcher(body);
-    if (!match.matches()) return false;
-    data.strCall = match.group(1).trim();
-    data.strMap = getOptGroup(match.group(2));
-    data.strDate = getOptGroup(match.group(3));
-    data.strTime = match.group(4);
-    parseAddress(match.group(5).trim(), data);
-    data.strSource = match.group(6);
+    if (match.matches()) {
+      setFieldList("CALL MAP DATE TIME ADDR APT SRC X UNIT INFO GPS");
+      data.strCall = match.group(1).trim();
+      data.strMap = getOptGroup(match.group(2));
+      data.strDate = getOptGroup(match.group(3));
+      data.strTime = match.group(4);
+      parseAddress(match.group(5).trim(), data);
+      data.strSource = match.group(6);
 
-    body = match.group(7).trim();
-    match = GPS_PTN.matcher(body);
-    if (match.matches()) {
-      body = match.group(1).trim();
-      setGPSLoc(setDec(match.group(2))+','+setDec(match.group(3)), data);
-    }
-    match = UNIT_PTN.matcher(body);
-    if (match.matches()) {
-      body = match.group(1).trim();
-      data.strUnit = match.group(2).trim();
-      data.strSupp = stripFieldStart(match.group(3), "CMT:");
-    } else {
-      int pt = body.indexOf(" CMT:");
-      if (pt >= 0) {
-        data.strSupp = body.substring(pt+4).trim();
-        body = body.substring(0,pt).trim();
+      body = match.group(7).trim();
+      match = GPS_PTN.matcher(body);
+      if (match.matches()) {
+        body = match.group(1).trim();
+        setGPSLoc(setDec(match.group(2))+','+setDec(match.group(3)), data);
       }
+      match = UNIT_PTN.matcher(body);
+      if (match.matches()) {
+        body = match.group(1).trim();
+        data.strUnit = match.group(2).trim();
+        data.strSupp = stripFieldStart(match.group(3), "CMT:");
+      } else {
+        int pt = body.indexOf(" CMT:");
+        if (pt >= 0) {
+          data.strSupp = body.substring(pt+4).trim();
+          body = body.substring(0,pt).trim();
+        }
+      }
+      data.strCross = stripFieldEnd(body, "/");
+      return true;
+    } else {
+      return super.parseMsg(body, data);
     }
-    data.strCross = stripFieldEnd(body, "/");
-    return true;
   }
 
   private static String setDec(String fld) {
