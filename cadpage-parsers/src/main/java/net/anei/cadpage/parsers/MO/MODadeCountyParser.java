d13ebe062c0fb7b0f8d22e6aec62dcf8c09c8cd7
Ken Corbin <ken@cadpage.org>
2026-09-10 13:19:16 -0700

    Fixed pasing problem with Dade County, MO

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/MO/MODadeCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/MO/MODadeCountyParser.java
@@ -1,83 +1,10 @@
 package net.anei.cadpage.parsers.MO;
 
-import net.anei.cadpage.parsers.MsgInfo.Data;
-import net.anei.cadpage.parsers.dispatch.DispatchA48Parser;
+import net.anei.cadpage.parsers.dispatch.DispatchA27Parser;
 
-public class MODadeCountyParser extends DispatchA48Parser {
+public class MODadeCountyParser extends DispatchA27Parser {
 
   public MODadeCountyParser() {
-    super(CITY_LIST, "DADE COUNTY", "MO", FieldType.INFO, A48_NO_CODE);
-    setupCallList(CALL_LIST);
+    super("DADE COUNTY", "MO");
   }
-
-  @Override
-  protected boolean parseMsg(String subject, String body, Data data) {
-    subject = "As of 99/99/99 99:99:99";
-    body = body.replace(" CITY OF ", " ");
-    if (!super.parseMsg(subject, body, data)) return false;
-    data.strDate = data.strTime = "";
-    return true;
-  }
-
-  private static final String[] CALL_LIST = new String[] {
-      "911HUP",
-      "ASSIST OTHER AGENCY",
-      "C&I",
-      "FIRES",
-      "MEDICAL ALARM",
-      "MEDICAL EVENT",
-      "MVA",
-      "OUTSIDE FIRE",
-      "STRUCTURE FIRE",
-      "SUSPAC",
-      "TEST CALL",
-      "TEST CALL ;DCES TEST CAD FOR EMAIL DELIVERY",
-      "UNK",
-      "VIOLAT",
-  };
-
-  private static final String[] CITY_LIST = new String[] {
-
-      // Cities and towns
-      "ARCOLA",
-      "DADEVILLE",
-      "EVERTON",
-      "GREENFIELD",
-      "CITY OF GREENFIELD",
-      "LOCKWOOD",
-      "CITY OF LOCKWOOD",
-      "SOUTH GREENFIELD",
-
-      // Unincorporated communities
-      "BONA",
-      "CEDARVILLE",
-      "COMET",
-      "CORRY",
-      "DUDENVILLE",
-      "KINGS POINT",
-      "MEINERT",
-      "PENNSBORO",
-      "SYLVANIA",
-
-      // Barton County
-      "GOLDEN CITY",
-      "LAMAR",
-
-      // Polk County
-      "ALDRICH",
-
-      // Greene County
-      "ASH GROVE",
-      "WALNUT GROVE",
-
-      // Jasper County
-      "JASPER",
-
-      // Cedar County
-      "JERICO SPRINGS",
-      "STOCKTON",
-
-      // Lawrence County
-      "MILLER"
-  };
 }
