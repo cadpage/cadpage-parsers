19221a3032680e30895766c6715f57229a8589c5
Ken Corbin <ken@cadpage.org>
2026-09-17 15:40:22 -0700

    Fixed parsing problem with  El Paso County, CO (D)

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/CO/COElPasoCountyDParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/CO/COElPasoCountyDParser.java
@@ -1,15 +1,71 @@
 package net.anei.cadpage.parsers.CO;
 
-import net.anei.cadpage.parsers.dispatch.DispatchA55Parser;
+import java.util.Properties;
 
-public class COElPasoCountyDParser extends DispatchA55Parser {
+import net.anei.cadpage.parsers.MsgInfo.Data;
+import net.anei.cadpage.parsers.dispatch.DispatchA11Parser;
+
+public class COElPasoCountyDParser extends DispatchA11Parser {
 
   public COElPasoCountyDParser() {
-    super("EL PASO COUNTY", "CO");
+    super(CITY_CODES, "EL PASO COUNTY", "CO");
   }
 
   @Override
   public String getFilter() {
-    return "ereports@eforcesoftware.com";
+    return "PCSO_Comm@pueblocounty.us";
   }
+
+  @Override
+  protected boolean parseMsg(String body, Data data) {
+
+    // They seem to be dropping the PAGED identifier, so we have to put it back
+    if (!body.contains("\nPAGED\n")) {
+      int pt =  body.indexOf('\n');
+      if (pt < 0) return false;
+      pt = body.indexOf('\n', pt+1);
+      if (pt < 0) return false;
+      body = body.substring(0,pt) + "\nPAGED" + body.substring(pt);
+    }
+    return super.parseMsg(body, data);
+  }
+
+  private static final Properties CITY_CODES = buildCodeTable(new String[] {
+      "AVO", "Avondale",
+      "BEU", "Beulah",
+      "BOO", "Boone",
+      "CAN", "Canon City",
+      "COL", "Colorado City",
+      "COS", "Colo Springs",
+      "DEN", "Denver",
+      "FCO", "Fort Collins",
+      "FLO", "Florence",
+      "FOU", "Fountain",
+      "FOW", "Fowler",
+      "HOM", "Homeless",
+      "LAJ", "La Junta",
+      "LAM", "Lamar",
+      "LAV", "La Veta",
+      "MAN", "Manzanola",
+      "MID", "Midway",
+      "OLN", "Olney Springs",
+      "ORD", "Ordway",
+      "PB1", "Pueblo",
+      "PB2", "Pueblo PO Box",
+      "PB3", "Pueblo",
+      "PB4", "Pueblo",
+      "PB5", "Pueblo",
+      "PB6", "Pueblo",
+      "PB8", "Pueblo",
+      "PEN", "Penrose",
+      "PW",  "Pueblo West",
+      "RES", "Reservoir",
+      "ROC", "Rocky Ford",
+      "RYE", "Rye",
+      "SAN", "San Isabel",
+      "TRI", "Trinidad",
+      "WAL", "Walsenburg",
+      "WES", "Westcliffe",
+      "WET", "Wetmore"
+  });
 }
