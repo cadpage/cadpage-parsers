db2679e97305eb768a6f2c2f9eaae6d4dd2c0891
Ken Corbin <ken@cadpage.org>
2026-09-23 17:53:20 -0700

    Update sender filter for Sumner County, KS

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/KS/KSSumnerCountyParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/KS/KSSumnerCountyParser.java
@@ -15,17 +15,17 @@ public class KSSumnerCountyParser extends DispatchA25Parser {
 
   @Override
   public String getFilter() {
-    return "sumner911cad@co.sumner.ks.us,donotreply@mulvane.us,CAD@mulvane.us,notsowpdd1@hotmail.com";
+    return "sumner911cad@co.sumner.ks.us,donotreply@mulvane.us,CAD@mulvane.us,notsowpdd1@hotmail.com,harpercountyks@gmail.com";
   }
-  
+
   private static final Pattern APT_PTN = Pattern.compile("(?:APT|ROOM|RM|LOT)\\b[:# ]*(.*)", Pattern.CASE_INSENSITIVE);
 
   @Override
   protected boolean parseMsg(String subject, String body, Data data) {
     if (!super.parseMsg(subject, body, data)) return false;
-    
+
     if (data.strCity.equalsIgnoreCase("County")) data.strCity = "";
-    
+
     data.strApt = stripFieldStart(data.strApt, data.strPlace);
     Matcher match = APT_PTN.matcher(data.strApt);
     if (match.matches()) data.strApt = match.group(1);
