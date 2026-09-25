dabe24bfbb0e30a1dd3c2113016a26f182e3fc4c
Ken Corbin <ken@cadpage.org>
2026-09-24 20:28:26 -0700

    Fixed more parsing problems with Windham County, CT (A)

--- cadpage-parsers/src/main/java/net/anei/cadpage/parsers/CT/CTWindhamCountyAParser.java
+++ cadpage-parsers/src/main/java/net/anei/cadpage/parsers/CT/CTWindhamCountyAParser.java
@@ -1,10 +1,8 @@
 package net.anei.cadpage.parsers.CT;
 
-import java.util.Properties;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import net.anei.cadpage.parsers.ReverseCodeSet;
 import net.anei.cadpage.parsers.SmartAddressParser;
 import net.anei.cadpage.parsers.MsgInfo.Data;
 
@@ -34,8 +32,10 @@ public class CTWindhamCountyAParser extends SmartAddressParser {
   private static final Pattern MASTER =
       Pattern.compile("([-/A-Z0-9,]*) ?\\|(\\S+)\\|(?: PRI (\\d) \\|)? (.+?) \\* ([^\\*,]+)(?:, *([A-Za-z ]*))? \\* +(.*?) (\\d\\d?/\\d\\d?/\\d{4}) (\\d\\d:\\d\\d:\\d\\d)");
 
+  private static final Pattern PLACE_X_PTN = Pattern.compile("(.*[^- ].*)[- ] (.*)");
   private static final Pattern CLEAN_PLACE_PTN = Pattern.compile("[-*/| ]*(.*?)[-*/| ]*");
-  private static final Pattern APT_PTN = Pattern.compile("(.*?)[-*/| ]*\\b(?:APT|ROOM|RM|^LOT|UNIT)[ #]+(.+)", Pattern.CASE_INSENSITIVE);
+  private static final Pattern APT_PTN = Pattern.compile("(.*?)[-*/| ]*\\b(?:APT|ROOM|RM|^LOT|UNIT)\\.?[ #]+(.+)", Pattern.CASE_INSENSITIVE);
+  private static final Pattern PLACE_APT_X_PTN = Pattern.compile("(.*) *\\b(?:APT|ROOM|RM|^LOT|UNIT)\\.?[ #](\\S+)\\b *(.*)", Pattern.CASE_INSENSITIVE);
 
   @Override
   public boolean parseMsg(String subject, String body, Data data) {
@@ -56,22 +56,50 @@ public class CTWindhamCountyAParser extends SmartAddressParser {
     data.strCall =  stripFieldStart(match.group(4), "/");
     parseAddress(match.group(5).trim(), data);
     data.strCity = getOptGroup(match.group(6));
-    String cross = match.group(7);
+    String cross = match.group(7).trim();
     data.strDate = match.group(8);
     data.strTime = match.group(9);
 
-    int pt = cross.indexOf("  ");
-    if (pt >= 0) {
-      String place = cross.substring(0,pt).trim();
-      cross = cross.substring(pt+2).trim();
+    String place = null;
+    cross = stripFieldStart(cross, "/");
+    match = PLACE_X_PTN.matcher(cross);
+    if (match.matches()) {
+      place = match.group(1).trim();
+      cross = match.group(2).trim();
       match = CLEAN_PLACE_PTN.matcher(place);
       if (match.matches()) place = match.group(1);
+    }
+    else {
+      match = CLEAN_PLACE_PTN.matcher(cross);
+      if (match.matches()) {
+        place = match.group(1);
+        if (place.equals(cross)) {
+          if (cross.equalsIgnoreCase("Area of")) {
+            place = cross;
+            cross = "";
+          } else {
+            place = null;
+          }
+        } else {
+          cross = "";
+        }
+      }
+    }
+    if (place != null) {
       match = APT_PTN.matcher(place);
       if (match.matches()) {
         place = match.group(1).trim();
         data.strApt = match.group(2);
       }
       data.strPlace = place;
+    }
+    else {
+      match = PLACE_APT_X_PTN.matcher(cross);
+      if (match.matches()) {
+        data.strPlace = match.group(1).trim();
+        data.strApt = match.group(2);
+        cross = match.group(3);
+      }
     }
     data.strCross = cross;
 
